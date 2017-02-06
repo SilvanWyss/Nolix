2526edc409501bfeeca952ce378f1c23308296d8
Silvan Wyss <silvan.wyss@gmail.com>
2017-02-06 10:38:23 +0100

    Created DataProviderTutorial.

---
+++ Source/ch/nolix/commonTutorial/financeTutorial/DataProviderTutorial.java
@@ -0,0 +1,39 @@
+//package declaration
+package ch.nolix.commonTutorial.financeTutorial;
+
+//own imports
+import ch.nolix.common.finance.DataProvider;
+import ch.nolix.common.finance.NYSEProductSymbolManager;
+import ch.nolix.common.util.Time;
+
+//class
+/**
+ * This class provides a tutorial for the data provider class.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-02
+ * @lines 30
+ */
+public final class DataProviderTutorial {
+	
+	//main method
+	/**
+	 * Loads and prints out to the console the candle sticks per hour of BP from 2017-2-1 until 2017-2-3.
+	 * 
+	 * @param arguments
+	 */
+	public static void main(final String[] arguments) {
+		DataProvider.getCandleSticksPerHour(
+			NYSEProductSymbolManager.BP,
+			new Time(2017, 2, 1, 16, 0),
+			new Time(2017, 2, 3, 22, 0)
+		)
+		.forEach(cs -> System.out.println(cs));
+	}
+
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private DataProviderTutorial() {}
+}
