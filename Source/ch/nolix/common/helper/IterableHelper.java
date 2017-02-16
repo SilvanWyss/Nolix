666b9458f88264c28c6183751b4481cb7ed0919c
Silvan Wyss <silvan.wyss@gmail.com>
2017-02-16 12:53:46 +0100

    Added getSize method to IterableHelper.

--- Source/ch/nolix/common/helper/IterableHelper.java
+++ Source/ch/nolix/common/helper/IterableHelper.java
@@ -1,26 +1,45 @@
-/*
- * file:	IterableHelper.java
- * author:	Silvan Wyss
- * month:	02.03.2016
- * lines:	30
- */
-
 //package declaration
 package ch.nolix.common.helper;
+
+//Java import
+import java.util.Iterator;
 
 //class
 /**
  * This class provides some methods to handle iterable objects.
+ * 
+ * @author Silvan Wyss
+ * @month 2016-03
+ * @lines 50
  */
 public final class IterableHelper {
+	
+	//static method
+	/**
+	 * @param iterableObject
+	 * @return the number of elements of the given iterable object.
+	 */
+	public static int getSize(final Iterable<?> iterableObject) {
+		
+		int size = 0;
+		
+		//Iterates the given iterable object.
+		final Iterator<?> iterator = iterableObject.iterator();
+		while (iterator.hasNext()) {
+			size++;
+			iterator.next();
+		}
+		
+		return size;
+	}
 
 	//static method
 	/**
-	 * @param iterable
-	 * @return true if the given iterable object is empty
+	 * @param iterableObject
+	 * @return true if the given iterable object is empty.
 	 */
-	public static final boolean isEmpty(Iterable<?> iterable) {
-		return !iterable.iterator().hasNext();
+	public static boolean isEmpty(final Iterable<?> iterableObject) {
+		return !iterableObject.iterator().hasNext();
 	}
 	
 	//private constructor
