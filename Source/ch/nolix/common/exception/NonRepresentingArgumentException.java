56c5e67d7a3e45eb718e61743609b271a9469ccb
Silvan Wyss <silvan.wyss@gmail.com>
2017-03-05 10:21:32 +0100

    Created NonRepresentingArgumentException.

---
+++ Source/ch/nolix/common/exception/NonRepresentingArgumentException.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.common.exception;
+
+//class
+/**
+ * A non representing argument exception is an exception that is intended to be thrown when an argument represents undesired no object of a given type.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-03
+ * @lines 30
+ */
+@SuppressWarnings("serial")
+public final class NonRepresentingArgumentException extends InvalidArgumentException {
+
+	//constructor
+	/**
+	 * Creates new non representing argument exception for the given argument and the given type.
+	 * 
+	 * @param argument
+	 * @param type
+	 */
+	public NonRepresentingArgumentException(final Object argument, final Class<?> type) {
+		
+		//Calls constructor of the base class.
+		super(
+			new Argument(argument),
+			new ErrorPredicate("represents no " + type)
+		);
+	}
+}
