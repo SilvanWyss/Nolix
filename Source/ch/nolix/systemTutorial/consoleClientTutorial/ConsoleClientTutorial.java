2189ed90ca6295574e032114da90bfa7d10fd698
Silvan Wyss <silvan.wyss@gmail.com>
2017-08-20 13:50:08 +0200

    Created ConsoleClientTutorial.

---
+++ Source/ch/nolix/systemTutorial/consoleClientTutorial/ConsoleClientTutorial.java
@@ -0,0 +1,91 @@
+//package declaration
+package ch.nolix.systemTutorial.consoleClientTutorial;
+
+//own imports
+import ch.nolix.core.sequencer.Sequencer;
+import ch.nolix.system.client.Session;
+import ch.nolix.system.client.StandardApplication;
+import ch.nolix.system.consoleClient.ConsoleBackClient;
+import ch.nolix.system.consoleClient.ConsoleFrontClient;
+
+//class
+/**
+ * This class provides a tutorial for the ConsoleClient class.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-08
+ * @lines 90
+ */
+public final class ConsoleClientTutorial {
+
+	//main method
+	/**
+	 * 1. Creates an application for console back clients.
+	 * 2. Creates a console front client that connects to the application.
+	 * 
+	 * @param arguments
+	 */
+	public static void main(final String[] arguments) {
+		
+		//Creates application.
+		final StandardApplication<ConsoleBackClient> application
+		= new StandardApplication<ConsoleBackClient>(
+			"MyApplication",
+			MainSession.class
+		);
+		
+		//Creates console front client that connects to the application.
+		new ConsoleFrontClient(application);
+	}
+	
+	//inner class
+	private static final class MainSession extends Session<ConsoleBackClient> {
+		
+		//method
+		/**
+		 * Initializes this main session.
+		 */
+		public void initialize() {
+			
+			getRefClient().writeNextLineToInfoPanel(
+				"This is the info panel of the console.",
+				"On the info panel informatiosn can be displayed.",
+				"The info panel cannot be edited."
+			);
+			
+			Sequencer.waitForMilliseconds(100);
+			getRefClient().writeNextLineToConsole("Enter your name.");
+			final String name = getRefClient().readNextLineFromConsole();
+			
+			Sequencer.waitForMilliseconds(100);
+			getRefClient().writeNextLineToConsole("Hello " + name + ".");
+						
+			boolean answerIsValid = false;
+			do {
+				
+				Sequencer.waitForMilliseconds(100);
+				getRefClient().writeNextLineToConsole("Are you fine? (Press y for yes, press n for no.)");
+				
+				final char answer = getRefClient().readNextCharacterFromConsole();
+				
+				//Enumerates the received answer.
+				switch (answer) {
+					case 'y':
+						answerIsValid = true;
+						getRefClient().writeNextLineToConsole("Oh good!");
+						break;
+					case 'n':
+						answerIsValid = true;
+						getRefClient().writeNextLineToConsole("Oh sad!");
+						break;
+				}
+			} while (!answerIsValid);
+		}
+	}
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private ConsoleClientTutorial() {}
+}
