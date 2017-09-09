104f6e4bbe203b4871ca512c34cfc3c63b71fe74
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-09 13:48:50 +0200

    Created Refreshable interface.

---
+++ Source/ch/nolix/core/interfaces/Refreshable.java
@@ -0,0 +1,17 @@
+//package declaration
+package ch.nolix.core.interfaces;
+
+//interface
+/**
+ * @author Silvan Wyss
+ * @month 2017-09
+ * @lines 10
+ */
+public interface Refreshable {
+
+	//abstract method
+	/**
+	 * Refreshes this refreshable object.
+	 */
+	public abstract void refresh();
+}
