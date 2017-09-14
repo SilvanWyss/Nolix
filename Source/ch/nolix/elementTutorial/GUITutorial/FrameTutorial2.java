3c2e8e809704b2c7683a37906a6179277a48b694
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-14 21:45:38 +0200

    Edited FrameTutorial2.

--- Source/ch/nolix/elementTutorial/GUITutorial/FrameTutorial2.java
+++ Source/ch/nolix/elementTutorial/GUITutorial/FrameTutorial2.java
@@ -33,8 +33,7 @@ public final class FrameTutorial2 {
 		//Sets a yellow background color to the clock label.
 		clockLabel
 		.getRefNormalStructure()
-		//.setBackgroundColor(new Color(Color.YELLOW));
-		.setBackgroundColor(Color.AQUAMARINE);
+		.setBackgroundColor(Color.YELLOW);
 	
 		//Creates a frame that contains the clock caption label and the clock label.
 		final Frame frame
