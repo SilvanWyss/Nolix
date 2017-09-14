c113fb033b5e435563e4de4e3bfe21eab84494bd
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-14 21:14:46 +0200

    Created BlackRedConsoleDesign.

---
+++ Source/ch/nolix/system/consoleClient/BlackRedConsoleDesign.java
@@ -0,0 +1,52 @@
+//package declaration
+package ch.nolix.system.consoleClient;
+
+//own imports
+import ch.nolix.element.GUI.ContainerRole;
+import ch.nolix.element.configuration.DeepConfiguration;
+import ch.nolix.element.configuration.StandardConfiguration;
+
+//class
+/**
+ * @author Silvan Wyss
+ * @month 2017-09
+ * @lines 50
+ */
+public final class BlackRedConsoleDesign extends StandardConfiguration {
+
+	//constructor
+	/**
+	 * Creates new console client.
+	 */
+	public BlackRedConsoleDesign() {
+		
+		addAttachingAttribute("BackgroundColor(0x200000)");
+		
+		addConfiguration(
+			new DeepConfiguration()
+			.setSelectorRole(ContainerRole.MainContainer)
+			.addAttachingAttribute(
+				"NormalBackgroundColor(Black)",
+				"ElementMargin(20)"
+			),
+			new DeepConfiguration()
+			.setSelectorName(WidgetNameManager.INFO_PANEL_NAME)
+			.addAttachingAttribute(
+				"NormalWidth(1000)",
+				"NormalHeight(300)",
+				"NormalBackgroundColor(0x202020)",
+				"NormalPadding(10)",
+				"NormalTextColor(OrangeRed)"
+			),
+			new DeepConfiguration()
+			.setSelectorName(WidgetNameManager.CONSOLE_NAME)
+			.addAttachingAttribute(
+				"NormalWidth(1000)",
+				"NormalHeight(400)",
+				"NormalBackgroundColor(0x101010)",
+				"NormalPadding(10)",
+				"NormalTextColor(Red)"
+			)
+		);
+	}
+}
