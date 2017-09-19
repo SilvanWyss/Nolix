0e70ceccca425230736ef1b8633c8cb5fac75501
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-19 19:36:58 +0200

    Renamed WindowsEnvironmentVariablesManager to EnvironmentVariableManager.

---
+++ Source/ch/nolix/core/constants/EnvironmentVariableCatalogue.java
@@ -0,0 +1,22 @@
+//package declaration
+package ch.nolix.core.constants;
+
+//class
+/**
+ * The environment variable catalogue provides environment variables for Microsoft Windows.
+ * 
+ * @author Silvan Wyss
+ * @month 2016-09
+ * @lines 20
+ */
+public final class EnvironmentVariableCatalogue {
+
+	//environment variable
+	public static final String APP_DATA = "APPDATA";
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private EnvironmentVariableCatalogue() {}
+}
