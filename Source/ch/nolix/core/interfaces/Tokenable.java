a21064d8213cff5594858c6e7e89cfdc8dd04a84
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-19 19:26:12 +0200

    Edited Tokenable interface.

--- Source/ch/nolix/core/interfaces/Tokenable.java
+++ Source/ch/nolix/core/interfaces/Tokenable.java
@@ -3,12 +3,13 @@ package ch.nolix.core.interfaces;
 
 //interface
 /**
- * A tokenable object is a tokened object whose token can be set dynamically.
+ * A tokenable object is a tokened object
+ * whose token can be set programmatically.
  * 
  * @author Silvan Wyss
  * @month 2015-12
  * @lines 20
- * @param <T> - The concrete type of a tokenable object.
+ * @param <T> The type of a tokenable object.
  */
 public interface Tokenable<T extends Tokenable<T>>
 extends Tokened {
