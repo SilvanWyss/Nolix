c61c04f12baa3c5967e4a902c42a3e02609a7fd5
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-28 23:20:06 +0200

    Edited VerticalLine.

--- Source/ch/nolix/element/GUI/VerticalLine.java
+++ Source/ch/nolix/element/GUI/VerticalLine.java
@@ -1,58 +1,38 @@
-/*
- * file:	VerticalLine.java
- * author:	Silvan Wyss
- * month:	2015
- * lines:	50 
- */
-
 //package declaration
 package ch.nolix.element.GUI;
 
-//own imports
-import ch.nolix.core.container.List;
-import ch.nolix.core.specification.StandardSpecification;
-
 //class
+/**
+ * @author Silvan Wyss
+ * @month 2015-12
+ * @lines 30
+ */
 public final class VerticalLine extends Line<VerticalLine> {
 	
-	//constant
-	public static final String SIMPLE_CLASS_NAME = "VerticalLine";
+	//type name
+	public static final String TYPE_NAME = "VerticalLine";
 
 	//constructor
 	/**
-	 * Creates new vertical line that has default attributes.
+	 * Creates new vertical line.
 	 */
 	public VerticalLine() {		
 		resetConfiguration();
 	}
 	
-	//constructor
-	/**
-	 * Creates new vertical that has the given attributes.
-	 * @param attributes
-	 * @throws Exception if the given attributes are not valid
-	 */
-	public VerticalLine(List<StandardSpecification> attributes) {
-		
-		//Calls other constructor.
-		this();
-		
-		addOrChangeAttributes(attributes);
-	}
-	
 	//method
 	/**
-	 * @return the current height of this rectangle if it is not collapsed
+	 * @return the height of this line when it is not collapsed.
 	 */
-	public final int getHeightWhenNotCollapsed() {
+	public int getHeightWhenNotCollapsed() {
 		return getLength();
 	}
 	
 	//method
 	/**
-	 * @return the current width of this rectangle if it is not collapsed
+	 * @return the width of this line when it is not collapsed.
 	 */
-	public final int getWidthWhenNotCollapsed() {
+	public int getWidthWhenNotCollapsed() {
 		return getThickness();
 	}
 }
