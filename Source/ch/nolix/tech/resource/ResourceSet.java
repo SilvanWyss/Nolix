368f050f57b6fa1824806773e8cafbf026251d3b
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-28 21:45:18 +0200

    Created ResourceSet.

---
+++ Source/ch/nolix/tech/resource/ResourceSet.java
@@ -0,0 +1,33 @@
+//package declaration
+package ch.nolix.tech.resource;
+
+//class
+/**
+ * A resource set is a set of resources that depends on each other.
+ * Of this class no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-09
+ * @lines 30
+ */
+public class ResourceSet {
+
+	//resources
+	public static final Resource CARBON = new Resource("Carbon");
+	public static final Resource COLE = new Resource("Cole", CARBON);
+	public static final Resource CARBON_HYDRIDE = new Resource("Carbon Hydride");
+	public static final Resource METHAN = new Resource("Methan", CARBON_HYDRIDE);
+	public static final Resource SILICIUM = new Resource("Silicium");
+	public static final Resource METAL = new Resource("Metal");
+	public static final Resource ALUMINIUM = new Resource("Aluminium", METAL);
+	public static final Resource IRON = new Resource("Iron", METAL);
+	public static final Resource TITAN = new Resource("Titan", METAL);
+	public static final Resource SILVER = new Resource("Silver", METAL);
+	public static final Resource GOLD = new Resource("Gold", METAL);
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private ResourceSet() {}
+}
