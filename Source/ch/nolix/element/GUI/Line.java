7739d0dc3294c0095d5c4d4f0a119354d21b17e8
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-28 23:14:12 +0200

    Edited Line.

--- Source/ch/nolix/element/GUI/Line.java
+++ Source/ch/nolix/element/GUI/Line.java
@@ -1,42 +1,38 @@
-/*
- * file:	Line.java
- * author:	Silvan Wyss
- * month:	2015-12
- * lines:	250
- */
-
 //package declaration
 package ch.nolix.element.GUI;
 
 //Java import
 import java.awt.Graphics;
 
-import ch.nolix.core.container.AccessorContainer;
-
 //own imports
-
+import ch.nolix.core.container.AccessorContainer;
 import ch.nolix.core.container.List;
 import ch.nolix.core.invalidArgumentException.Argument;
 import ch.nolix.core.invalidArgumentException.ArgumentName;
 import ch.nolix.core.invalidArgumentException.ErrorPredicate;
 import ch.nolix.core.invalidArgumentException.InvalidArgumentException;
 import ch.nolix.core.specification.StandardSpecification;
+import ch.nolix.core.validator2.Validator;
 import ch.nolix.element.color.Color;
 import ch.nolix.element.data.Length;
 import ch.nolix.element.data.Thickness;
 
 //abstract class
 /**
- * A line is a rectangle that has a color and which length is clearly bigger than its thickness.
+ * The width of a line is bigger than its thickness.
  * 
  * @author Silvan Wyss
+ * @month 2015-12
+ * @lines 260
  * @param <L> The type of a line.
  */
 public abstract class Line<L extends Line<L>>
 extends Widget<L, LineStructure> {
 	
-	//constants
-	public static final String SIMPLE_CLASS_NAME = "Line";
+	//type name
+	public static final String TYPE_NAME = "Line";
+	
+	//min length to thickness ratio
 	public static final int MIN_LENGTH_TO_THICKNESS_RATIO = 4;
 	
 	//default values
@@ -45,20 +41,20 @@ extends Widget<L, LineStructure> {
 	public static final Color DEFAULT_COLOR = Color.BLACK;
 	
 	//attributes
-	private final Length length = new Length();
-	private final Thickness thickness = new Thickness();
-	private Color color = new Color();
+	private Length length;
+	private Thickness thickness;
+	private Color color;
 	
 	//method
 	/**
 	 * Adds or change the given attribute to this line.
 	 * 
-	 * @param attribute		The attribute to add or change to this line.
-	 * @throws Exception if the given attribute is not valid
+	 * @param attribute
+	 * @throws InvalidArgumentException if the given attribute is not valid.
 	 */
 	public final void addOrChangeAttribute(final StandardSpecification attribute) {
 		
-		//Enumerates the given attribute.
+		//Enumerates the header of the given attribute.
 		switch (attribute.getHeader()) {
 			case Length.TYPE_NAME:
 				setLength(attribute.getOneAttributeToInteger());
@@ -67,7 +63,7 @@ extends Widget<L, LineStructure> {
 				setThickness(attribute.getOneAttributeToInteger());
 				break;	
 			case Color.TYPE_NAME:
-				setColor(attribute.getOneAttributeToString());
+				setColor(new Color(attribute.getOneAttributeToString()));
 				break;
 			default:
 				
@@ -78,7 +74,7 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * @return the attributes of this line
+	 * @return the attributes of this line.
 	 */
 	public final List<StandardSpecification> getAttributes() {	
 		
@@ -93,7 +89,7 @@ extends Widget<L, LineStructure> {
 			attributes.addAtEnd(thickness.getSpecification());
 		}
 		
-		if (getColor().equals(DEFAULT_COLOR)) {
+		if (!getColor().equals(DEFAULT_COLOR)) {
 			attributes.addAtEnd(color.getSpecification());
 		}
 		
@@ -102,7 +98,7 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * @return the color of this line
+	 * @return the color of this line.
 	 */
 	public final Color getColor() {
 		return color;
@@ -110,7 +106,7 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * @return the length of this line
+	 * @return the length of this line.
 	 */
 	public final int getLength() {
 		return length.getValue();
@@ -118,7 +114,15 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * @return the thickness of this line
+	 * @return the widgets of this line.
+	 */
+	public final AccessorContainer<Widget<?, ?>> getRefWidgets() {
+		return new AccessorContainer<>();
+	}
+	
+	//method
+	/**
+	 * @return the thickness of this line.
 	 */
 	public final int getThickness() {
 		return thickness.getValue();
@@ -126,8 +130,8 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * @param role		The role this line is ask to have.
-	 * @return true if this line has the given role
+	 * @param role
+	 * @return true if this line has the given role.
 	 */
 	public final boolean hasRole(final String role) {
 		return false;
@@ -135,7 +139,7 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * Resets the confoguration of this line.
+	 * Resets the configuration of this line.
 	 */
 	public final void resetConfiguration() {
 		
@@ -151,30 +155,18 @@ extends Widget<L, LineStructure> {
 	/**
 	 * Sets the color of this line.
 	 * 
-	 * @param color		The color to set tot this line.
-	 * @return this line
-	 * @throws Exception if the given color is no true color
+	 * @param color
+	 * @return this line.
+	 * @throws NullArgumentException if the given color is null.
 	 */
 	@SuppressWarnings("unchecked")
 	public final L setColor(final Color color) {
 		
+		//Checks if the given color is not null.
+		Validator.supposeThat(color).thatIsInstanceOf(Color.class).isNotNull();
+		
+		//Sets the color of this line.
 		this.color = color;
-		
-		return (L)this;
-	}
-	
-	//method
-	/**
-	 * Sets the color of this line.
-	 * 
-	 * @param color		The color to set to this line.
-	 * @return this line
-	 * @throws Exception if the given color is no color name or no true color value
-	 */
-	@SuppressWarnings("unchecked")
-	public final L setColor(final String color) {
-		
-		this.color = new Color(color);
 		
 		return (L)this;
 	}
@@ -183,19 +175,25 @@ extends Widget<L, LineStructure> {
 	/**
 	 * Sets the length of this line.
 	 * 
-	 * @param length		The length to set to this line.
-	 * @return this line
-	 * @throws Exception if the given length is smaller than 4 times the thickness of this line
+	 * @param length
+	 * @return this line.
+	 * @throws InvalidArgumentException if the given length
+	 * is smaller than 4 times the thickness of this line.
 	 */
 	@SuppressWarnings("unchecked")
-	public final L setLength(int length) {
+	public final L setLength(final int length) {
 		
-		//Checks the given length.
+		//Checks if the given length is not smaller than 4 times the thickness of this line.
 		if (length < MIN_LENGTH_TO_THICKNESS_RATIO * getThickness()) {
 			throw new InvalidArgumentException(
 				new ArgumentName("length"),
 				new Argument(length),
-				new ErrorPredicate("is smaller than " + MIN_LENGTH_TO_THICKNESS_RATIO + "x the thickness " + getThickness() + ".")
+				new ErrorPredicate(
+					"is smaller than "
+					+ MIN_LENGTH_TO_THICKNESS_RATIO
+					+ "x the thickness "
+					+ getThickness() + "."
+				)
 			);
 		}
 		
@@ -206,11 +204,12 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * Sets the thickness of this line
+	 * Sets the thickness of this line.
 	 * 
-	 * @param thickness		The thickness to set to this line.
-	 * @return this line
-	 * @throws Exception if 4 times the given thickness is bigger than the length of this line
+	 * @param thickness
+	 * @return this line.
+	 * @throws InvalidArgumentException if 4 times the given thickness
+	 * is bigger than the length of this line.
 	 */
 	@SuppressWarnings("unchecked")
 	public final L setThickness(final int thickness) {
@@ -220,7 +219,13 @@ extends Widget<L, LineStructure> {
 			throw new InvalidArgumentException(
 				new ArgumentName("thickness"),
 				new Argument(thickness),
-				new ErrorPredicate(" multiplied with " + MIN_LENGTH_TO_THICKNESS_RATIO +  " is bigger than the length " + getLength() + ".")
+				new ErrorPredicate(
+					"multiplied with "
+					+ MIN_LENGTH_TO_THICKNESS_RATIO
+					+ "x is bigger than the length "
+					+ getLength()
+					+ "."
+				)
 			);
 		}
 		
@@ -231,7 +236,7 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * @return new widget structure for this line.
+	 * @return a new widget structure for this line.
 	 */
 	protected final LineStructure createWidgetStructure() {
 		return new LineStructure();
@@ -239,26 +244,21 @@ extends Widget<L, LineStructure> {
 	
 	//method
 	/**
-	 * Paints this rectangle using the given rectangle structure and graphics.
+	 * Paints this line using the given widget structure and graphics.
 	 * 
-	 * @param rectangleStructure
+	 * @param widgetStructure
 	 * @param graphics
 	 */
 	protected final void paint(
-		final LineStructure rectangleStructure,
+		final LineStructure widgetStructure,
 		final Graphics graphics
 	) {
 		graphics.setColor(color.getJavaColor());
 		graphics.fillRect(
 			getXPositionOnContainer(),
 			getYPositionOnContainer(),
-			getWidthWhenNotCollapsed(),
-			getHeightWhenNotCollapsed()
+			getWidth(),
+			getHeight()
 		);
-	}
-	
-	@Override
-	public AccessorContainer<Widget<?, ?>> getRefWidgets() {
-		return new AccessorContainer<>();
 	}
 }
