c0fca49ace89b60b4e4df1550636aa0af0b99304
Silvan Wyss <silvan.wyss@gmail.com>
2017-09-28 21:25:53 +0200

    Created ITwoElementTakerBooleanGetter interface.

---
+++ Source/ch/nolix/core/functionInterfaces/ITwoElementTakerBooleanGetter.java
@@ -0,0 +1,23 @@
+//package declaration
+package ch.nolix.core.functionInterfaces;
+
+//interface
+/**
+ * A two element taker boolean getter has a method
+ * that takes 2 elements of the same type and returns a boolean.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-09
+ * @lines 20
+ * @param <E> The type of the elements a two element taker boolean getter takes.
+ */
+public interface ITwoElementTakerBooleanGetter<E> {
+
+	//abstract method
+	/**
+	 * @param element1
+	 * @param element2
+	 * @return a boolean for the given two elements.
+	 */
+	public abstract boolean getOutput(E element1, E element2);
+}
