c2a085493e62299fbd5268e74d4fab5b1d685402
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-13 21:00:49 +0200

    Edited Label.

--- Source/ch/nolix/element/GUI/Label.java
+++ Source/ch/nolix/element/GUI/Label.java
@@ -1,27 +1,20 @@
-/*
- * file:	Label.java
- * author:	Silvan Wyss
- * month:	2015-12
- * lines:	180
- */
-
 //package declaration
 package ch.nolix.element.GUI;
-
-//Java import
-import java.awt.event.KeyEvent;
 
 //own imports
 import ch.nolix.core.container.List;
 import ch.nolix.core.invalidStateException.UnexistingAttributeException;
 import ch.nolix.core.specification.StandardSpecification;
-import ch.nolix.core.validator.Validator;
+import ch.nolix.core.validator2.Validator;
 
 //class
 /**
- * A label is a text line rectangle whose text cannot be changed by the user.
+ * A label is a text line rectangle
+ * that is supposed to show texts a user cannot change.
  * 
  * @author Silvan Wyss
+ * @month 2015-12
+ * @lines 150
  */
 public final class Label extends TextLineWidget<Label> {
 	
@@ -36,7 +29,7 @@ public final class Label extends TextLineWidget<Label> {
 	
 	//constructor
 	/**
-	 * Creates new label with default values.
+	 * Creates a new label with default values.
 	 */
 	public Label() {	
 		resetConfiguration();
@@ -44,33 +37,26 @@ public final class Label extends TextLineWidget<Label> {
 	
 	//constructor
 	/**
-	 * Creates new label with the given text.
+	 * Creates a new label with the given text.
 	 * 
 	 * @param text
-	 * @throws Exception if the given text is null
+	 * @throws NullArgumentException if the given text is null.
 	 */
 	public Label(final String text) {
-		resetConfiguration();
+		
+		//Calls other constructor.
+		this();
+		
+		//Sets the text of this label.
 		setText(text);
-	}
-	
-	//constructor
-	/**
-	 * Creates new label with the given attributes.
-	 * 
-	 * @param attributes
-	 * @throws Exception if the given attributes are not valid
-	 */
-	public Label(final Iterable<StandardSpecification> attributes) {
-		resetConfiguration();
-		addOrChangeAttributes(attributes);
 	}
 	
 	//method
 	/**
 	 * Adds or change the given attribute to this label.
 	 * 
-	 * @param attribute		The attribute to add or change to this label.
+	 * @param attribute
+	 * @throws InvalidArgumentException if the given attribute is not valid.
 	 */
 	public void addOrChangeAttribute(final StandardSpecification attribute) {
 		
@@ -78,7 +64,7 @@ public final class Label extends TextLineWidget<Label> {
 		switch (attribute.getHeader()) {
 			case ROLE_HEADER:
 				setRole(LabelRole.valueOf(attribute.getOneAttributeToString()));
-				break;	
+				break;
 			default:
 				
 				//Calls method of the base class.
@@ -88,15 +74,16 @@ public final class Label extends TextLineWidget<Label> {
 	
 	//method
 	/**
-	 * @return the attributes of this label
+	 * @return the attributes of this label.
 	 */
 	public List<StandardSpecification> getAttributes() {
 		
 		//Calls method of the base class.
 		final List<StandardSpecification> attributes = super.getAttributes();
 		
+		//Handles the option that this label has a role.
 		if (hasRole()) {
-			attributes.addAtEnd(new StandardSpecification(ROLE_HEADER, role.toString()));
+			attributes.addAtEnd(role.getSpecificationAs(ROLE_HEADER));
 		}
 		
 		return attributes;
@@ -104,22 +91,20 @@ public final class Label extends TextLineWidget<Label> {
 	
 	//method
 	/**
-	 * @return the role of this label
-	 * @throws UnexistingAttributeExcetpion if this label has no role
+	 * @return the role of this label.
+	 * @throws UnexistingAttributeExcetpion if this label has no role.
 	 */
 	public LabelRole getRole() {
 		
 		//Checks if this label has a role.
-		if (!hasRole()) {
-			throw new UnexistingAttributeException(this, ROLE_HEADER);
-		}
+		supposeHasRole();
 		
 		return role;
 	}
 	
 	//method
 	/**
-	 * @return true if this label has a role
+	 * @return true if this label has a role.
 	 */
 	public boolean hasRole() {
 		return (role != null);
@@ -127,54 +112,46 @@ public final class Label extends TextLineWidget<Label> {
 	
 	//method
 	/**
-	 * @param role		The role this label is requested to have.
-	 * @return true of this label has the given role
+	 * @param role
+	 * @return true of this label has the given role.
 	 */
 	public boolean hasRole(final String role) {
 		
+		//Handles the case if this label has no role.
 		if (!hasRole()) {
 			return false;
 		}
 		
-		return this.role.toString().equals(role);
+		//Handles the case if this label has a role.
+		return getRole().equals(LabelRole.valueOf(role));
 	}
-	
-	//method
-	/**
-	 * Lets this label note a pressed key.
-	 * 
-	 * @param keyEvent
-	 */
-	public void noteKeyPress(final KeyEvent keyEvent) {}
-	
-	//method
-	/**
-	 * Lets this label note a right mouse button press.
-	 */
-	public void noteRightMouseButtonPress() {}
-	
-	/**
-	 * Lets this label note a typed key.
-	 * 
-	 * @param keyEvent
-	 */
-	public void noteKeyTyping(final KeyEvent keyEvent) {}
 	
 	//method
 	/**
 	 * Sets the role of this label.
 	 * 
-	 * @param role		The role that is set to this label.
-	 * @return this label
-	 * @throws Exception if the given role is null
+	 * @param role
+	 * @return this label.
+	 * @throws NullArgumentException if the given role is null.
 	 */
 	public final Label setRole(final LabelRole role) {
 		
-		//Checks the given role.
-		Validator.throwExceptionIfValueIsNull("role", role);
+		//Checks if the given role is not null.
+		Validator.suppose(role).thatIsNamed("role").isNotNull();
 
+		//Sets the role of this label.
 		this.role = role;
 		
 		return this;
-	}	
+	}
+	
+	//method
+	/**
+	 * @throws UnexistingAttributeException if this label has no role.
+	 */
+	private void supposeHasRole() {
+		if (!hasRole()) {
+			throw new UnexistingAttributeException(this, ROLE_HEADER);
+		}
+	}
 }
