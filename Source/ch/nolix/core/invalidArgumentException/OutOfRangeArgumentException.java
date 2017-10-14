7e042a95db09da551f04febf8639d97b74503342
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-15 01:47:48 +0200

    Extended OutOfRangeArgumentException.

--- Source/ch/nolix/core/invalidArgumentException/OutOfRangeArgumentException.java
+++ Source/ch/nolix/core/invalidArgumentException/OutOfRangeArgumentException.java
@@ -3,40 +3,99 @@ package ch.nolix.core.invalidArgumentException;
 
 //class
 /**
- * An out of range exception is an invalid argument exception that is intended to be thrown when a value is undesired not in a given range.
+ * An out of range exception is an invalid argument exception
+ * that is supposed to be thrown when a value is undesired not in a given range.
+ * 
+ * An out of range exception is not mutable.
  * 
  * @author Silvan Wyss
  * @month 2016-02
- * @lines 50
+ * @lines 110
  */
 @SuppressWarnings("serial")
 public final class OutOfRangeArgumentException extends InvalidArgumentException {
 	
 	//constructor
 	/**
-	 * Creates new out of range argument exception for the given argument and for the range defined by the given min and max.
+	 * Creates new out of range argument exception
+	 * for the given argument and for the range defined by the given min and max.
 	 * 
 	 * @param argument
 	 * @param min
 	 * @param max
 	 */
-	public OutOfRangeArgumentException(final long argument, final long min, final long max) {
-		
+	public OutOfRangeArgumentException(
+		final double argument,
+		final double min,
+		final double max
+	) {		
 		//Calls constructor of the base class.
 		super(
 			new Argument(argument),
-			new ErrorPredicate("is not between [" + min + ", " + max + "]")
+			new ErrorPredicate("is not in [" + min + ", " + max + "]")
 		);
 	}
 	
 	//constructor
 	/**
-	 * Creates new out of range argument exception for the given argument, that has the given argument name, and for the ragne defined by the given min and max.
+	 * Creates new out of range argument exception
+	 * for the given argument, that has the given argument name,
+	 * and for the range defined by the given min and max.
 	 * 
 	 * @param argumentName
 	 * @param argument
 	 * @param min
 	 * @param max
+	 * @throws RuntimeException if the given argument name is null.
+	 * @throws RuntimeException if the given argument name is empty.
+	 */
+	public OutOfRangeArgumentException(
+		final String argumentName,
+		final double argument,
+		final double min,
+		final double max
+	) {
+		//Calls constructor of the base class.
+		super(
+			new ArgumentName(argumentName),
+			new Argument(argument),
+			new ErrorPredicate("is not in [" + min + ", " + max + "]")
+		);
+	}
+	
+	//constructor
+	/**
+	 * Creates new out of range argument exception
+	 * for the given argument and for the range defined by the given min and max.
+	 * 
+	 * @param argument
+	 * @param min
+	 * @param max
+	 */
+	public OutOfRangeArgumentException(
+		final long argument,
+		final long min,
+		final long max
+	) {		
+		//Calls constructor of the base class.
+		super(
+			new Argument(argument),
+			new ErrorPredicate("is not in [" + min + ", " + max + "]")
+		);
+	}
+	
+	//constructor
+	/**
+	 * Creates new out of range argument exception
+	 * for the given argument, that has the given argument name,
+	 * and for the range defined by the given min and max.
+	 * 
+	 * @param argumentName
+	 * @param argument
+	 * @param min
+	 * @param max
+	 * @throws RuntimeException if the given argument name is null.
+	 * @throws RuntimeException if the given argument name is empty.
 	 */
 	public OutOfRangeArgumentException(
 		final String argumentName,
@@ -44,12 +103,11 @@ public final class OutOfRangeArgumentException extends InvalidArgumentException 
 		final long min,
 		final long max
 	) {
-		
 		//Calls constructor of the base class.
 		super(
 			new ArgumentName(argumentName),
 			new Argument(argument),
-			new ErrorPredicate("is not between [" + min + ", " + max + "]")
+			new ErrorPredicate("is not in [" + min + ", " + max + "]")
 		);
 	}
 }
