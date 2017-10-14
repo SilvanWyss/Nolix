886cbe0cf4ccc3e60ff8e9a239f70bf4aa772aff
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-15 01:29:45 +0200

    Edited FalseArgumentException.

--- Source/ch/nolix/core/invalidArgumentException/FalseArgumentException.java
+++ Source/ch/nolix/core/invalidArgumentException/FalseArgumentException.java
@@ -3,14 +3,15 @@ package ch.nolix.core.invalidArgumentException;
 
 //class
 /**
- * A false argument exception is an argument exception that is intended to be thrown when an argument is undesired false.
+ * A false argument exception is an argument exception
+ * that is supposed to be thrown when an argument is undesired false.
  * 
  * @author Silvan Wyss
  * @month 2017-01
- * @lines 30
+ * @lines 40
  */
 @SuppressWarnings("serial")
-public class FalseArgumentException extends InvalidArgumentException {
+public final class FalseArgumentException extends InvalidArgumentException {
 
 	//constant
 	private static final String ERROR_PREDICATE = "is false";
@@ -27,7 +28,8 @@ public class FalseArgumentException extends InvalidArgumentException {
 	
 	//constructor
 	/**
-	 * Creates new false argument exception for an argument with the given argument name.
+	 * Creates new false argument exception
+	 * for an argument with the given argument name.
 	 * 
 	 * @param argumentName
 	 * @throws RuntimeException if the given argument name is null.
@@ -36,6 +38,9 @@ public class FalseArgumentException extends InvalidArgumentException {
 	public FalseArgumentException(final String argumentName) {
 		
 		//Calls constructor of the base class.
-		super(new ArgumentName(argumentName), new ErrorPredicate(ERROR_PREDICATE));
+		super(new ArgumentName(
+			argumentName),
+			new ErrorPredicate(ERROR_PREDICATE)
+		);
 	}
 }
