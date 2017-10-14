82ddc7019bf05e4f2817a6ba63e1e9b10cb338f3
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-15 01:38:48 +0200

    Extended PositiveArgumentException.

--- Source/ch/nolix/core/invalidArgumentException/PositiveArgumentException.java
+++ Source/ch/nolix/core/invalidArgumentException/PositiveArgumentException.java
@@ -3,11 +3,14 @@ package ch.nolix.core.invalidArgumentException;
 
 //class
 /**
- * A positive argument exception is an argument exception that is intended to be thrown when an argument is undesired positive.
+ * A positive argument exception is an argument exception
+ * that is supposed to be thrown when an argument is undesired positive.
+ * 
+ * A positive argument exception is not mutable.
  * 
  * @author Silvan Wyss
  * @month 2016-12
- * @lines 60
+ * @lines 80
  */
 @SuppressWarnings("serial")
 public final class PositiveArgumentException extends InvalidArgumentException {
@@ -41,15 +44,40 @@ public final class PositiveArgumentException extends InvalidArgumentException {
 	
 	//constructor
 	/**
-	 * Creates new positive argument exception for the given argument that has the given argument name.
+	 * Creates new positive argument exception
+	 * for the given argument that has the given argument name.
 	 * 
 	 * @param argumentName
 	 * @param argument
 	 * @throws RuntimeException if the given argument name is null.
 	 * @throws RuntimeException if the given argument name is empty.
 	 */
-	public PositiveArgumentException(final String argumentName, final double argument) {
-		
+	public PositiveArgumentException(
+		final String argumentName,
+		final double argument
+	) {		
+		//Calls constructor of the base class.
+		super(
+			new ArgumentName(argumentName),
+			new Argument(argument),
+			new ErrorPredicate(ERROR_PREDICATE)
+		);
+	}
+	
+	//constructor
+	/**
+	 * Creates new positive argument exception
+	 * for the given argument that has the given argument name.
+	 * 
+	 * @param argumentName
+	 * @param argument
+	 * @throws RuntimeException if the given argument name is null.
+	 * @throws RuntimeException if the given argument name is empty.
+	 */
+	public PositiveArgumentException(
+		final String argumentName,
+		final long argument
+	) {
 		//Calls constructor of the base class.
 		super(
 			new ArgumentName(argumentName),
