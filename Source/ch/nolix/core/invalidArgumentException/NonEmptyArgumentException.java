676e0bc990b546d5696541896b12dcef88fc2846
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-14 23:36:46 +0200

    Extended NonEmptyArgumentException.

--- Source/ch/nolix/core/invalidArgumentException/NonEmptyArgumentException.java
+++ Source/ch/nolix/core/invalidArgumentException/NonEmptyArgumentException.java
@@ -3,11 +3,12 @@ package ch.nolix.core.invalidArgumentException;
 
 //class
 /**
- * A non empty argument exception is an argument exception that is intended to be thrown when an argument is undesired not empty.
+ * A non empty argument exception is an argument exception
+ * that is supposed to be thrown when an argument is undesired not empty.
  * 
  * @author Silvan Wyss
  * @month 2017-01
- * @lines 40
+ * @lines 60
  */
 @SuppressWarnings("serial")
 public final class NonEmptyArgumentException extends InvalidArgumentException {
@@ -34,10 +35,29 @@ public final class NonEmptyArgumentException extends InvalidArgumentException {
 	 * 
 	 * @param argumentName
 	 * @throws RuntimeException if the given argument name is null.
+	 * @throws RuntimeException if the given argument name is empty.
 	 */
 	public NonEmptyArgumentException(final ArgumentName argumentName) {
 
 		//Calls constructor of the base class.
 		super(argumentName, new ErrorPredicate(PREDICATE));
+	}
+	
+	//constructor
+	/**
+	 * Creates new non empty argument exception for the given argument with the given argumet name.
+	 * @param argumentName
+	 * @param argument
+	 * @throws RuntimeException if the given argument name is null.
+	 * @throws RuntimeException if the given argument name is empty.
+	 */
+	public NonEmptyArgumentException(final String argumentName, final Object argument) {
+		
+		//Calls constructor of the base class.
+		super(
+			new ArgumentName(argumentName),
+			new Argument(argument),
+			new ErrorPredicate(PREDICATE)
+		);
 	}
 }
