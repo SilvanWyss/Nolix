5910f5f7d17e470eafd923c1704fc4821d8248fe
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-15 02:13:01 +0200

    Edited ClosedStateException.

--- Source/ch/nolix/core/invalidStateException/ClosedStateException.java
+++ Source/ch/nolix/core/invalidStateException/ClosedStateException.java
@@ -3,8 +3,10 @@ package ch.nolix.core.invalidStateException;
 
 //class
 /**
- * A closed state exception is an invalid state exception that intended to be thrown
- * when an object is undesired closed.
+ * A closed state exception is an invalid state exception
+ * that is supposed to be thrown when an object is undesired closed.
+ * 
+ * A close state exception is not mutable.
  * 
  * @author Silvan Wyss
  * @month 2017-09
@@ -13,7 +15,7 @@ package ch.nolix.core.invalidStateException;
 @SuppressWarnings("serial")
 public final class ClosedStateException extends InvalidStateException {
 
-	//error predicate
+	//constant
 	private static final String ERROR_PREDICATE = "is closed";
 
 	//constructor
@@ -21,10 +23,11 @@ public final class ClosedStateException extends InvalidStateException {
 	 * Creates new closed state exception for the given object.
 	 * 
 	 * @param object
+	 * @throws NullArgumentException if the given object is null.
 	 */
 	public ClosedStateException(final AutoCloseable object) {
 		
 		//Calls constructor of the base class.
 		super(object, ERROR_PREDICATE);
-	}	
+	}
 }
