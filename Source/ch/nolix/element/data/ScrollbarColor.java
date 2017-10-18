f3a142c3cd2a2323b094eafc4bd8c011568de675
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-18 22:07:46 +0200

    Created ScrollbarColor.

---
+++ Source/ch/nolix/element/data/ScrollbarColor.java
@@ -0,0 +1,58 @@
+//package declaration
+package ch.nolix.element.data;
+
+//own import
+import ch.nolix.element.color.Color;
+
+//class
+/**
+ * A scrollbar color is not mutable.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-10
+ * @lines 50
+ */
+public final class ScrollbarColor extends Color {
+
+	//constant
+	public static final String TYPE_NAME = "ScrollbarColor";
+	
+	//default value
+	public static int DEFAULT_VALUE = Color.GREY_INT;
+	
+	//constructor
+	/**
+	 * Creates new scrollbar color with a default value.
+	 */
+	public ScrollbarColor() {
+		
+		//Calls constructor of the base class.
+		super(DEFAULT_VALUE);
+	}
+	
+	//constructor
+	/**
+	 * Creates new scrollbar color with the given value.
+	 * 
+	 * @param value
+	 * @throws OutOfRangeArgumentException if the given value is no true color value.
+	 */
+	public ScrollbarColor(final int value) {
+		
+		//Calls constructor of the base class.
+		super(value);
+	}
+	
+	//constructor
+	/**
+	 * Creates new scrollbar color with the given value.
+	 * 
+	 * @param value
+	 * @throws InvalidArgumentException if the given value is no color name or no true color value.
+	 */
+	public ScrollbarColor(final String value) {
+		
+		//Calls constructor of the base class.
+		super(value);
+	}
+}
