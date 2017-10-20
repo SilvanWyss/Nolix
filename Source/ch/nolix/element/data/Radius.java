1ebdd62d7d9d8cec61466c121ccebc97e2135399
Silvan Wyss <silvan.wyss@gmail.com>
2017-10-20 22:43:02 +0200

    Created Radius.

---
+++ Source/ch/nolix/element/data/Radius.java
@@ -0,0 +1,38 @@
+//package declaration
+package ch.nolix.element.data;
+
+//own import
+import ch.nolix.element.core.PositiveFloatingPointNumber;
+
+//class
+/**
+ * A radius is not mutable.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-10
+ * @lines 30
+ */
+public final class Radius extends PositiveFloatingPointNumber {
+
+	//constant
+	public static final String TYPE_NAME = "Radius";
+	
+	//constructor
+	/**
+	 * Creates new radius with a default value.
+	 */
+	public Radius() {}
+	
+	//constructor
+	/**
+	 * Creates new radius with the given value.
+	 * 
+	 * @param value
+	 * @throws NonPositiveArgumentException if the given value is not positive.
+	 */
+	public Radius(final double value) {
+		
+		//Calls constructor of the base class.
+		super(value);
+	}
+}
