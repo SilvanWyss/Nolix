ed8fa97fbd61460b160f8c8f4c8282e57c8015f0
Silvan Wyss <silvan.wyss@gmail.com>
2017-11-13 21:44:55 +0100

    Created VariableNameCatalogue.

---
+++ Source/ch/nolix/core/constants/VariableNameCatalogue.java
@@ -0,0 +1,61 @@
+//package declaration
+package ch.nolix.core.constants;
+
+//class
+/**
+ * Of this class no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-11
+ * @lines 60
+ */
+public class VariableNameCatalogue {
+	
+	//constants
+	public static final String AGE = "age";
+	public static final String AMOUNT = "amount";
+	public static final String CAPACITY = "capacity";
+	public static final String CAPTION = "caption";
+	public static final String CREATION_DATE = "date";
+	public static final String CREATOR = "creator";
+	public static final String DATE = "date";
+	public static final String DATE_OF_BIRTH = "date of birth";
+	public static final String DEFAULT_CAPACITY = "default capacity";
+	public static final String DEFAULT_NAME = "default name";
+	public static final String DESCRIPTION = "description";
+	public static final String DISTANCE = "distance";
+	public static final String DURATION = "duration";
+	public static final String DURACTION_IN_MILLISECONDS = "duration in milliseconds";
+	public static final String DURATION_IN_SECONDS = "duration in seconds";
+	public static final String END_DATE = "end date";
+	public static final String END_INDEX = "end index";
+	public static final String HASH_CODE = "hash code";
+	public static final String HEIGHT = "height";
+	public static final String ID = "id";
+	public static final String INDEX = "index";
+	public static final String LAST_NAME = "last name";
+	public static final String MAXIMUM = "maximum";
+	public static final String MINIMUM = "minimum";
+	public static final String NAME = "name";
+	public static final String NODE = "node";
+	public static final String PRICE = "price";
+	public static final String RADIUS = "radius";
+	public static final String REASON = "reason";
+	public static final String ROOT = "root";
+	public static final String ROOT_NODE = "root node";
+	public static final String START_DATE = "start date";
+	public static final String START_INDEX = "start index";
+	public static final String STATE = "state";
+	public static final String SUMMARY = "summary";
+	public static final String SURNAME = "surname";
+	public static final String TEXT = "text";
+	public static final String TIMEOUT = "timeout";
+	public static final String WEIGHT = "weight";
+	public static final String YEAR = "year";
+
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private VariableNameCatalogue() {}
+}
