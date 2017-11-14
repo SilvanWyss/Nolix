15e4b1e286304e006120f84d06d0982f8cd4103a
Silvan Wyss <silvan.wyss@gmail.com>
2017-11-14 21:07:59 +0100

    Created Launcher for core tests.

---
+++ Source/ch/nolix/elementTest/coreTest/Launcher.java
@@ -0,0 +1,29 @@
+//package declaration
+package ch.nolix.elementTest.coreTest;
+
+//class
+/**
+ * The launcher provides a main method to create and run a core test pool.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-11
+ * @lines 20
+ */
+public final class Launcher {
+
+	//main method
+	/**
+	 * Creates a new core test pool and runs it.
+	 * 
+	 * @param args
+	 */
+	public static final void main(String[] args) {
+		new CoreTestPool().run();
+	}
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private Launcher() {}
+}
