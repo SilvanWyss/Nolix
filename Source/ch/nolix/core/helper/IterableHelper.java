9f2b3f5edcc8f936470fa9b90d3bfef24f7c0cfc
Silvan Wyss <silvan.wyss@gmail.com>
2017-11-26 18:14:49 +0100

    Added getElementAt method to IterableHelper.

--- Source/ch/nolix/core/helper/IterableHelper.java
+++ Source/ch/nolix/core/helper/IterableHelper.java
@@ -4,6 +4,11 @@ package ch.nolix.core.helper;
 //Java import
 import java.util.Iterator;
 
+//own imports
+import ch.nolix.core.constants.VariableNameCatalogue;
+import ch.nolix.core.sequencer.Sequencer;
+import ch.nolix.core.validator2.Validator;
+
 //class
 /**
  * This class provides methods to handle iterable objects.
@@ -11,9 +16,27 @@ import java.util.Iterator;
  * 
  * @author Silvan Wyss
  * @month 2016-03
- * @lines 50
+ * @lines 70
  */
 public final class IterableHelper {
+	
+	//static method
+	/**
+	 * @param iterableObject
+	 * @param index
+	 * @return the element at the given index of the given iterable object
+	 * @throws NonPositiveArgumentException if the given index is not positive.
+	 * @throws NoSuchElementException if the given iterable object has no element at the given index.
+	 */
+	public static <E> E getElementAt(final Iterable<E> iterableObject, final int index) {
+		
+		//Checks if the given index is positive.
+		Validator.suppose(index).thatIsNamed(VariableNameCatalogue.INDEX).isPositive();
+		
+		final Iterator<E> iterator = iterableObject.iterator();
+		Sequencer.forCount(index - 1).run(() -> iterator.next());	
+		return iterator.next();
+	}
 	
 	//static method
 	/**
