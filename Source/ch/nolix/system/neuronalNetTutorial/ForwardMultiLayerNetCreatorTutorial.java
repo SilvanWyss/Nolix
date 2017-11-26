ed73d935dc82eb9eca87537aef2d5d646a68179b
Silvan Wyss <silvan.wyss@gmail.com>
2017-11-26 21:23:38 +0100

    Edited ForwardMultiLayerNetCreatorTutorial.

--- Source/ch/nolix/system/neuronalNetTutorial/ForwardMultiLayerNetCreatorTutorial.java
+++ Source/ch/nolix/system/neuronalNetTutorial/ForwardMultiLayerNetCreatorTutorial.java
@@ -22,36 +22,37 @@ public final class ForwardMultiLayerNetCreatorTutorial {
 
 	//main method
 	/**
-	 * 1. Creates neuronal net creator.
-	 * 2. Lets the neuronal net creator create a neuronal net.
+	 * 1. Creates a forward multi layer net creator.
+	 * 2. Lets the forward multi layer net creator create a neuronal net.
 	 * 3. Creates the inputs for the neuronal net.
-	 * 4. Triggers the neuronal net and prints out its output to the console.
+	 * 4. Lets the neuronal net fire.
+	 * 5. Prints out the output of the neuronal net to the console.
 	 * 
 	 * @param args
 	 */
 	public static void main(final String[] args) {
 		
-		//Creates neuronal net creator.
-		final ForwardMultiLayerNetCreator<Double> neuronalNetCreator =
+		//Creates a forward multi layer net creator.
+		final ForwardMultiLayerNetCreator<Double> forwardMultiLayerNetCreator =
 		new ForwardMultiLayerNetCreator<Double>()
 		.setLayerCount(4)
 		.setNeuronsPerLayer(5)
 		.setOutputFunction(Calculator.DOUBLE_SUM);
 		
-		//Lets the neuronal net creator create a neuronal net.
-		final NeuronalNet<Double> neuronalNet = neuronalNetCreator.createNeuronalNet();
+		//Lets the forward multi layer net creator create a neuronal net.
+		final NeuronalNet<Double> neuronalNet = forwardMultiLayerNetCreator.createNeuronalNet();
 		
-		//Creates inputs
+		//Creates inputs for the neuronal net.
 		final List<Double> inputs = new List<Double>();
 		Sequencer.forCount(5).run(() -> inputs.addAtEnd(0.01));
 		
 		//Sets the inputs to the neuronal net.
 		neuronalNet.addInputNeuron(new SourceNeuron<Iterable<Double>>(inputs));
 		
-		//Triggers the neuronal net.
+		//Lets the neuronal net fire.
 		neuronalNet.fire();
 		
-		//Prints the output of the neuronal net out to the console.
+		//Prints out the output of the neuronal net to the console.
 		System.out.println("output values:");
 		neuronalNet.getRefOutput().forEach(v -> System.out.println(v));
 	}
