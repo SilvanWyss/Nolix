0d1fede45cdfac7515ade1b6fdf6dd4878ca9bc9
Silvan Wyss <silvan.wyss@gmail.com>
2017-12-08 17:56:52 +0100

    Created ILongTakerLongGetter interface.

---
+++ Source/ch/nolix/core/functionInterfaces/ILongTakerLongGetter.java
@@ -0,0 +1,20 @@
+//package declaration
+package ch.nolix.core.functionInterfaces;
+
+//functional interface
+/**
+ * A long taker long getter has a method that takes a long and returns a long.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-12
+ * @lines 20
+ */
+public interface ILongTakerLongGetter {
+
+	//abstract method
+	/**
+	 * @param value
+	 * @return a long for the given value.
+	 */
+	public abstract long getOutput(long value);
+}
