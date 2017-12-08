29dfa9a4e47fa182883355390f678080cb95c297
Silvan Wyss <silvan.wyss@gmail.com>
2017-12-08 17:55:58 +0100

    Created NumberCatalogue.

---
+++ Source/ch/nolix/core/constants/NumberCatalogue.java
@@ -0,0 +1,24 @@
+//package declaration
+package ch.nolix.core.constants;
+
+//class
+/**
+ * Of this class no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2017-12
+ * @lines 20
+ */
+public final class NumberCatalogue {
+	
+	//numbers
+	public static final double EULERS_NUMBER = Math.E;
+	public static final double LOGARITHMUS_DECIMALIS_OF_2 = Math.log(2.0);
+	public static final double PI = Math.PI;
+
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private NumberCatalogue() {}
+}
