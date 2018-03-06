b55e22ca36441887445aba518e7c752ae24cee0c
Silvan Wyss <silvan.wyss@gmail.com>
2018-03-06 18:56:34 +0100

    Created Tableau.

---
+++ Source/ch/nolix/element/task/Tableau.java
@@ -0,0 +1,117 @@
+//package declaration
+package ch.nolix.element.task;
+
+//own imports
+import ch.nolix.core.container.ReadContainer;
+import ch.nolix.core.entity.MultiProperty;
+import ch.nolix.core.interfaces.Clearable;
+import ch.nolix.element.bases.NamableElement;
+
+//class
+/**
+ * A tableau can contain tasks.
+ * A tableau is clearable.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-03
+ * @lines 110
+ */
+public final class Tableau
+extends NamableElement<Tableau>
+implements Clearable<Tableau> {
+
+	//attribute
+	private final MultiProperty<Task> tasks =
+	new MultiProperty<Task>(
+		Task.TYPE_NAME,
+		t -> addTask(t),
+		s -> Task.createFromSpecification(s)
+	);
+		
+	//method
+	/**
+	 * Adds the given task to this tableau.
+	 * 
+	 * @param task
+	 * @return this tableau.
+	 * @throws NullArgumentException
+	 * if the given task is null.
+	 */
+	public Tableau addTask(final Task task) {
+		
+		tasks.addValue(task);
+		
+		return this;
+	}
+	
+	//method
+	/**
+	 * Adds the given tasks to this tableau.
+	 * 
+	 * @param tasks
+	 * @return this Tableau.
+	 * @throws NullArgumentException
+	 * if one of the given tasks is null.
+	 */
+	public Tableau addTask(final Task...tasks) {
+		
+		//Iterates the given tasks.
+		for (final Task t : tasks) {
+			addTask(t);
+		}
+		
+		return this;
+	
+	}
+
+	//method
+	/**
+	 * Removes all tasks of this tableau.
+	 */
+	public Tableau clear() {
+		
+		tasks.clear();
+		
+		return this;
+	}
+	
+	//method
+	/**
+	 * @return the tasks of this tableau.
+	 */
+	public ReadContainer<Task> getRefTasks() {
+		return tasks.getRefValues();
+	}
+
+	//method
+	/**
+	 * @return true if this tableau contains no task.
+	 */
+	public boolean isEmpty() {
+		return tasks.isEmpty();
+	}
+
+	//method
+	/**
+	 * Removes the given task from this tableau.
+	 * 
+	 * @param task
+	 * @throws InvalidArgumentException
+	 * if this tableau does not contain the given task.
+	 */
+	public void removeTask(final Task task) {
+		tasks.removeValue(task);
+	}
+	
+	//method
+	/**
+	 * Resets this tableau.
+	 */
+	public void reset() {
+		
+		//Calls method of the base class.
+		super.reset();
+		
+		clear();
+	}
+}
