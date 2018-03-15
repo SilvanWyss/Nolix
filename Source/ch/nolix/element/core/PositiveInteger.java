2053a167223b7c2f4683de69de6499eef6801fda
Silvan Wyss <silvan.wyss@gmail.com>
2018-03-15 21:39:49 +0100

    Extended PositiveInteger.

--- Source/ch/nolix/element/core/PositiveInteger.java
+++ Source/ch/nolix/element/core/PositiveInteger.java
@@ -1,28 +1,48 @@
 //package declaration
 package ch.nolix.element.core;
 
+//own imports
+import ch.nolix.core.constants.VariableNameCatalogue;
+import ch.nolix.core.specification.Specification;
+import ch.nolix.primitive.validator2.Validator;
+
 //class
 /**
  * @author Silvan Wyss
  * @month 2015-12
- * @lines 30
+ * @lines 60
  */
 public class PositiveInteger extends Integer {
 	
-	//type name
+	//constant
 	public static final String TYPE_NAME = "PositiveInteger";
 	
 	//default value
 	public static final int DEFAULT_VALUE = 1;
 	
+	//static method
+	/**
+	 * @param specification
+	 * @return a new positive integer from the given specification.
+	 * @throws InvalidArgumentException
+	 * if the given specification is not valid.
+	 */
+	public static PositiveInteger createFromSpecification(
+		final Specification specification
+	) {
+		return new PositiveInteger(
+			specification.getOneAttributeAsInt()
+		);
+	}
+	
 	//constructor
 	/**
-	 * Creates a new positive integer with default attributes.
+	 * Creates a new positive integer with a default value.
 	 */
 	public PositiveInteger() {
 		
-		//Calls constructor of the base class.
-		super(DEFAULT_VALUE);
+		//Calls other constructor.
+		this(DEFAULT_VALUE);
 	}
 	
 	//constructor
@@ -30,9 +50,18 @@ public class PositiveInteger extends Integer {
 	 * Creates a new positive integer with the given value.
 	 * 
 	 * @param value
-	 * @throws Exception if the given value is not positive
+	 * @throws NonPositiveArgumentException
+	 * if the given value is not positive.
 	 */
-	public PositiveInteger(int value) {
+	public PositiveInteger(final int value) {
+		
+		//Calls constructor of the base class.
 		super(value);
+		
+		//Checks if the given value is positive.
+		Validator
+		.suppose(value)
+		.thatIsNamed(VariableNameCatalogue.VALUE)
+		.isPositive();
 	}
 }
