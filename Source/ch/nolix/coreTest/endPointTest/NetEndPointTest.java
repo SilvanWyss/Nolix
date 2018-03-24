514fed86faa960ac9e30f970974f68d686f9446e
Silvan Wyss <silvan.wyss@gmail.com>
2018-03-24 11:12:14 +0100

    Edited NetEndPointTest.

--- Source/ch/nolix/coreTest/endPointTest/NetEndPointTest.java
+++ Source/ch/nolix/coreTest/endPointTest/NetEndPointTest.java
@@ -2,54 +2,91 @@
 package ch.nolix.coreTest.endPointTest;
 
 //own imports
+import ch.nolix.core.communicationInterfaces.IReceiver;
+import ch.nolix.core.constants.VariableNameCatalogue;
 import ch.nolix.core.endPoint.EndPoint;
 import ch.nolix.core.endPoint.IEndPointTaker;
 import ch.nolix.core.endPoint.NetServer;
+import ch.nolix.core.sequencer.Sequencer;
+import ch.nolix.primitive.invalidStateException.InvalidStateException;
 import ch.nolix.primitive.test2.Test;
+import ch.nolix.primitive.validator2.Validator;
 import ch.nolix.core.endPoint.NetEndPoint;
 
 //test class
 /**
- * This class is a test class for the net end point class.
- * A net end point test is not mutable.
+ * This class is a test class for the {@link NetEndPoint} class.
  * 
  * @author Silvan Wyss
  * @month 2017-05
- * @lines 50
+ * @lines 90
  */
 public final class NetEndPointTest extends Test {
 
 	//test method
 	public void test_send() {
 		
-		//test parameter
-		final int port = 29000;
+		//test parameters
+		final int port = 50000;
 		final String message = "Hello World!";
 		
 		//setup	
-			final ReceiverMock receiverMock = new ReceiverMock();
+			final ReceiverFake receiverFake = new ReceiverFake();
 		
 			final NetServer netServer
 			= new NetServer(
 				port,
-				new IEndPointTaker() {public void takeEndPoint(EndPoint endPoint) {endPoint.setReceiver(receiverMock);}}	
+				new IEndPointTaker() {					
+					public void takeEndPoint(final EndPoint endPoint) {
+						endPoint.setReceiver(receiverFake);
+					}
+				}
 			);
 			
 			final NetEndPoint netEndPoint = new NetEndPoint(port);
 			
 		//execution			
-			netEndPoint.send(message);
-			
-			try {
-				Thread.sleep(500);
-			}
-			catch (InterruptedException exception) {}
+		netEndPoint.send(message);
+		Sequencer.waitForMilliseconds(500);
 		
 		//verification
-		expect(receiverMock.getLastReceivedMessage()).isEqualTo(message);
+		expect(receiverFake.getLastReceivedMessage()).isEqualTo(message);
 		
 		//cleanup
 		netServer.close();
 		netEndPoint.close();
+	}
+	
+	//inner class
+	private final class ReceiverFake implements IReceiver {
+		
+		//optional attribute
+		private String lastReceivedMessage;
+		
+		//method
+		public String getLastReceivedMessage() {
+			
+			if (!hasReceivedMessage()) {
+				throw new InvalidStateException(this, "received no message");
+			}
+			
+			return lastReceivedMessage;
+		}
+		
+		//method
+		public boolean hasReceivedMessage() {
+			return (lastReceivedMessage != null);
+		}
+		
+		//method
+		public void receive(final String message) {
+			
+			Validator
+			.suppose(message)
+			.thatIsNamed(VariableNameCatalogue.MESSAGE)
+			.isNotNull();
+			
+			lastReceivedMessage = message;
+		}
 	}
 }
