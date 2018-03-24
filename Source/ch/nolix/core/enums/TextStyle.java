ee2e0d8d62ba56e6cf978791c6e57a52323b5e16
Silvan Wyss <silvan.wyss@gmail.com>
2018-03-24 10:05:58 +0100

    Create TextStyle enum.

---
+++ Source/ch/nolix/core/enums/TextStyle.java
@@ -0,0 +1,17 @@
+//package declaration
+package ch.nolix.core.enums;
+
+//own import
+import ch.nolix.core.specificationInterfaces.ISpecifiedEnum;
+
+//enum
+/**
+ * @author Silvan Wyss
+ * @month 2018-03
+ * @lines 10
+ */
+public enum TextStyle implements ISpecifiedEnum {
+	Bold,
+	BoldAndItalic,
+	Italic
+}
