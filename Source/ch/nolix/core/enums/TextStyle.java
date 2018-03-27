55c47a67c920de601b82df05088aa4e4676b9a0c
Silvan Wyss <silvan.wyss@gmail.com>
2018-03-27 18:27:14 +0200

    Extended TextStyle enum.

--- Source/ch/nolix/core/enums/TextStyle.java
+++ Source/ch/nolix/core/enums/TextStyle.java
@@ -1,18 +1,38 @@
 //package declaration
 package ch.nolix.core.enums;
 
-//own import
+//own imports
+import ch.nolix.core.specification.Specification;
 import ch.nolix.core.specificationInterfaces.ISpecifiedEnum;
 
 //enum
 /**
+ * A {@link TextStyle} is not mutable.
+ * 
  * @author Silvan Wyss
  * @month 2018-03
- * @lines 10
+ * @lines 30
  */
 public enum TextStyle implements ISpecifiedEnum {
 	Default,
 	Bold,
 	BoldAndItalic,
-	Italic
+	Italic;
+	
+	//constant
+	public static String TYPE_NAME = "TextStyle";
+	
+	//static method
+	/**
+	 * @param specification
+	 * @return a new {@link TextStyle} from the given specification.
+	 * @throws NullArgumentException
+	 * if the given specification is not valid.
+	 */
+	public static TextStyle createFromSpecification(
+		final Specification specification
+	) {
+		return
+		TextStyle.valueOf(specification.getOneAttributeAsString());
+	}
 }
