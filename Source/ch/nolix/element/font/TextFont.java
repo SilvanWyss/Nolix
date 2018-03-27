4ef6fab455583540d2136282faddc0fafc661368
Silvan Wyss <silvan.wyss@gmail.com>
2018-03-27 21:33:03 +0200

    Extended TextFont enum.

--- Source/ch/nolix/element/font/TextFont.java
+++ Source/ch/nolix/element/font/TextFont.java
@@ -1,34 +1,53 @@
 //package declaration
 package ch.nolix.element.font;
 
-//own import
+//own imports
+import ch.nolix.core.specification.Specification;
 import ch.nolix.core.specificationInterfaces.ISpecifiedEnum;
 
 //enum
 /**
+ * A {@link TextFont} is not mutable.
+ * 
  * @author Silvan Wyss
  * @month 2017-08
- * @lines 30
+ * @lines 50
  */
 public enum TextFont implements ISpecifiedEnum {
 	Arial,
+	ComicSansMS,
 	Console,
 	Papyrus,
 	Verdana;
 	
-	//type name
+	//constant
 	public static final String TYPE_NAME = "TextFont";
+	
+	//static method
+	/**
+	 * @param specification
+	 * @return a new {@link TextFont} from the given specification.
+	 * @throws InvalidArgumentException
+	 * if the given specification is not valid.
+	 */
+	public static TextFont createFromSpecification(
+		final Specification specification
+	) {
+		return TextFont.valueOf(specification.getOneAttributeAsString());
+	}
 	
 	//method
 	/**
-	 * @return the Java font family of this font family.
+	 * @return the Java font family of this {@link TextFont}.
 	 */
 	public String getJavaFontFamily() {
 		
 		//Enumerates this text font.
 		switch (this) {
+			case ComicSansMS:
+				return "Comic Sans MS";
 			case Console:
-				return java.awt.Font.MONOSPACED;
+				return "Monospaced";
 			default:
 				return toString();
 		}
