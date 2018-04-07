5081916f5e7731a2976b72166f1d44cfd6daaacf
Silvan Wyss <silvan.wyss@gmail.com>
2018-04-07 22:25:14 +0200

    Created PascalCaseNameCatalogue.

---
+++ Source/ch/nolix/core/constants/PascalCaseNameCatalogue.java
@@ -0,0 +1,73 @@
+//package declaration
+package ch.nolix.core.constants;
+
+//class
+/**
+ * Of the {@link PascalCaseNameCatalogue} no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-04
+ * @lines 70
+ */
+public class PascalCaseNameCatalogue {
+
+	//constants
+	public static final String AGE = "Age";
+	public static final String AMOUNT = "Amount";
+	public static final String BACKGROUND_COLOR = "BackgroundColor";
+	public static final String BORDER_COLOR = "BorderColor";
+	public static final String CAPACITY = "Capacity";
+	public static final String CAPTION = "Caption";
+	public static final String COLOR = "Color";
+	public static final String COLUMN = "Column";
+	public static final String CONDITION = "Condition";
+	public static final String CONFIGURATION = "Configuration";
+	public static final String CONTAINER = "Container";
+	public static final String CREATION_DATE = "CreationDate";
+	public static final String CREATOR = "Creator";
+	public static final String DATABASE = "Database";
+	public static final String DATE = "Date";
+	public static final String DATE_OF_BIRTH = "DateOfBirth";
+	public static final String DESCRIPTION = "Description";
+	public static final String DISTANCE = "Distance";
+	public static final String DURATION = "Duration";
+	public static final String DURACTION_IN_MILLISECONDS = "DurationInMilliseconds";
+	public static final String DURATION_IN_SECONDS = "DurationInSeconds";
+	public static final String END_DATE = "EndDate";
+	public static final String ENTITY = "Entity";
+	public static final String HEADER = "Header";
+	public static final String HEIGHT = "Height";
+	public static final String ID = "Id";
+	public static final String LENGTH = "Length";
+	public static final String MESSAGE = "Message";
+	public static final String NAME = "Name";
+	public static final String PRICE = "Price";
+	public static final String RADIUS = "Radius";
+	public static final String REASON = "Reason";
+	public static final String RESOURCE = "Resource";
+	public static final String ROLE = "Role";
+	public static final String ROOT = "Root";
+	public static final String ROW = "Row";
+	public static final String SEPARATOR = "Separator";
+	public static final String START_DATE = "StartDate";
+	public static final String START_INDEX = "StartIndex";
+	public static final String STATE = "State";
+	public static final String SUMMARY = "Summary";
+	public static final String SURNAME = "Surname";
+	public static final String TEXT = "Text";
+	public static final String TEXT_COLOR = "TextColor";
+	public static final String TIMEOUT = "Timeout";
+	public static final String TIMEOUT_IN_MICROSECONDS = "TimeoutMicroseconds";
+	public static final String TIMEOUT_IN_MILLISECONDS = "TimeoutMilliseconds";
+	public static final String TIMEOUT_IN_NANOSECONDS = "TimeoutNanoseconds";
+	public static final String TIMEOUT_IN_SECONDS = "TimeoutInSeconds";
+	public static final String WEIGHT = "Weight";
+	public static final String WIDTH = "Width";
+	public static final String YEAR = "Year";
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of this class can be created.
+	 */
+	private PascalCaseNameCatalogue() {}
+}
