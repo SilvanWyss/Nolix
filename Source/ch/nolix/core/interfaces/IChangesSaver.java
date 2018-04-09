4a535f0b774f217c4e74c88ac600f173668ec101
Silvan Wyss <silvan.wyss@gmail.com>
2018-04-09 22:00:05 +0200

    Created IChangesSaver interface.

---
+++ Source/ch/nolix/core/interfaces/IChangesSaver.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.core.interfaces;
+
+//interface
+/**
+ * A {@link IChangesSaver} can save its changes.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-04
+ * @lines 20
+ */
+public interface IChangesSaver {
+	
+	//abstract method
+	/**
+	 * @return true if the current {@link IChangesSaver} has changes.
+	 */
+	public abstract boolean hasChanges();
+
+	//abstract method
+	/**
+	 * Saves the changes of the current {@link IChangesSaver}.
+	 */
+	public abstract void saveChanges();
+}
