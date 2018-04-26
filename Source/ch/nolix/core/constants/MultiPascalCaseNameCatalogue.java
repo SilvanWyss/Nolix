805324688cb3d8cd92c025bde8a5a64ca13bdafb
Silvan Wyss <silvan.wyss@gmail.com>
2018-04-26 22:13:06 +0200

    Created MultiPascalCaseNameCatalogue.

---
+++ Source/ch/nolix/core/constants/MultiPascalCaseNameCatalogue.java
@@ -0,0 +1,22 @@
+//package declaration
+package ch.nolix.core.constants;
+
+//class
+/**
+ * Of the {@link MultiPascalCaseNameCatalogue} no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-04
+ * @lines 20
+ */
+public class MultiPascalCaseNameCatalogue {
+	
+	//constants
+	public static final String ENTITIES = "Entities";
+
+	//private constructor
+	/**
+	 *  Avoids that an instance of the {@link MultiPascalCaseNameCatalogue} can be created.
+	 */
+	private MultiPascalCaseNameCatalogue() {}
+}
