adafa52f44a63d0111088377b3963855e238fb29
Silvan Wyss <silvan.wyss@gmail.com>
2018-04-28 23:51:57 +0200

    Extended MatrixTest.

--- Source/ch/nolix/coreTest/containerTest/MatrixTest.java
+++ Source/ch/nolix/coreTest/containerTest/MatrixTest.java
@@ -7,27 +7,63 @@ import ch.nolix.primitive.test2.Test;
 
 //test class
 /**
- * This class is a test class for the matrix class.
+ * A {@link MatrixTest} is a {@link Test} for the {@link Matrix} class.
  * 
  * @author Silvan Wyss
  * @month 2016-08
- * @lines 30
+ * @lines 60
  */
 public final class MatrixTest extends Test {
 
 	//test method
-	public final void testAddRow() {
+	public void test_addRow() {
 		
 		//setup
-		final Matrix<String> matrix	= new Matrix<String>()
+		final var matrix = new Matrix<String>();
 				
 		//execution
-		.addRow("apple", "banana", "cerish");
+		matrix.addRow("apple", "banana", "cerish");
 		
 		//verification
 		expect(matrix.getElementCount()).isEqualTo(3);
 		expect(matrix.getRowCount()).isEqualTo(1);
 		expect(matrix.getColumnCount()).isEqualTo(3);
 		expect(matrix.toString()).isEqualTo("apple,banana,cerish");
+	}
+	
+	//test method
+	public void test_getColumn() {
+		
+		//setup
+			final var matrix = new Matrix<String>();
+		
+			matrix.addRow("apple", "banana", "cerish");
+			matrix.addRow("elephant", "lion", "monkey");
+			matrix.addRow("flower", "tree", "palm");
+			
+		//execution
+		final var column = matrix.getColumn(3);
+		
+		//verification
+		expect(column.getElementCount()).isEqualTo(3);
+		expect(column.toString()).isEqualTo("cerish,monkey,palm");
+	}
+	
+	//test method
+	public void test_getRow() {
+		
+		//setup
+			final var matrix = new Matrix<String>();
+		
+			matrix.addRow("apple", "banana", "cerish");
+			matrix.addRow("elephant", "lion", "monkey");
+			matrix.addRow("flower", "tree", "palm");
+			
+		//execution
+		final var column = matrix.getRow(3);
+		
+		//verification
+		expect(column.getElementCount()).isEqualTo(3);
+		expect(column.toString()).isEqualTo("flower,tree,palm");
 	}
 }
