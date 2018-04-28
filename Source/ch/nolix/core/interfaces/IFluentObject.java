95fae73aab05f2f87ff50a2bd2fe5a557bff4e90
Silvan Wyss <silvan.wyss@gmail.com>
2018-04-28 15:52:13 +0200

    Edited IFluentObject interface.

--- Source/ch/nolix/core/interfaces/IFluentObject.java
+++ Source/ch/nolix/core/interfaces/IFluentObject.java
@@ -3,19 +3,18 @@ package ch.nolix.core.interfaces;
 
 //interface
 /**
- * A fluent object is an object that
- * can return itself casted to its concrete type.
+ * A {@link IFluentObject} can return itself casted to its concrete type.
  * 
  * @author Silvan Wyss
  * @month 2017-10
  * @lines 20
- * @param <FO> The type of a fluent object.
+ * @param <FO> The type of a {@link IFluentObject}.
  */
 public interface IFluentObject<FO> {
 
 	//default method
 	/**
-	 * @return this object
+	 * @return the current {@link IFluentObject}.
 	 */
 	@SuppressWarnings("unchecked")
 	public default FO getInstance() {
