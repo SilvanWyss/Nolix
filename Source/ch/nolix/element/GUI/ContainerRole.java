2b3a40275f7146b9cbff6d8fc7011838da8816c2
Silvan Wyss <silvan.wyss@gmail.com>
2018-04-28 16:11:23 +0200

    Extended ContainerRole enum.

--- Source/ch/nolix/element/GUI/ContainerRole.java
+++ Source/ch/nolix/element/GUI/ContainerRole.java
@@ -1,16 +1,26 @@
 //package declaration
 package ch.nolix.element.GUI;
 
-//own import
+//own imports
+import ch.nolix.core.specification.Specification;
 import ch.nolix.core.specificationInterfaces.ISpecifiedEnum;
 
 //enum
 /**
  * @author Silvan Wyss
  * @month 2016-08
- * @lines 10
+ * @lines 20
  */
 public enum ContainerRole implements ISpecifiedEnum {
 	OverallContainer,
-	MainContainer
+	MainContainer;
+	
+	//static method
+	/**
+	 * @param specification
+	 * @return a new {@link ContainerRole} from the given specification.
+	 */
+	public static ContainerRole createFromSpecification(final Specification specification) {
+		return ContainerRole.valueOf(specification.getOneAttributeAsString());
+	}
 }
