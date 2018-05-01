40302be8f58063559474a58861c809ad376ef2c5
Silvan Wyss <silvan.wyss@gmail.com>
2018-05-01 20:47:36 +0200

    Created IdentifiedElement.

---
+++ Source/ch/nolix/element/bases/IdentifiedElement.java
@@ -0,0 +1,50 @@
+//package declaration
+package ch.nolix.element.bases;
+
+//own imports
+import ch.nolix.core.constants.VariableNameCatalogue;
+import ch.nolix.core.container.List;
+import ch.nolix.core.interfaces.Identified;
+import ch.nolix.core.specification.StandardSpecification;
+import ch.nolix.element.core.Element;
+import ch.nolix.primitive.validator2.Validator;
+
+//abstract class
+public abstract class IdentifiedElement
+extends Element
+implements Identified {
+
+	//attribute
+	private final int id;
+	
+	//constructor
+	public IdentifiedElement(final int id) {
+		
+		Validator
+		.suppose(id)
+		.thatIsNamed(VariableNameCatalogue.ID)
+		.isPositive();
+		
+		this.id = id;
+	}
+	
+	//method
+	public List<StandardSpecification> getAttributes() {
+		
+		final var attributes = super.getAttributes();
+		
+		attributes.addAtEnd(
+			new StandardSpecification(
+				VariableNameCatalogue.ID,
+				StandardSpecification.createFromInt(getId())
+			)
+		);
+		
+		return attributes;
+	}
+	
+	//method
+	public final int getId() {
+		return id;
+	}
+}
