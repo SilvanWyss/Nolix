5de171b5cc5536aad1ab64c9f8543534635522f5
Silvan Wyss <silvan.wyss@gmail.com>
2018-05-08 23:56:34 +0200

    Created HeaderableElement.

---
+++ Source/ch/nolix/element/bases/HeaderableElement.java
@@ -0,0 +1,36 @@
+//package declaration
+package ch.nolix.element.bases;
+
+//own imports
+import ch.nolix.core.constants.PascalCaseNameCatalogue;
+import ch.nolix.core.entity.MutableProperty;
+import ch.nolix.core.interfaces.Headerable;
+import ch.nolix.element.core.MutableElement;
+import ch.nolix.element.core.NonEmptyText;
+
+//abstract class
+public abstract class HeaderableElement<HE extends HeaderableElement<HE>>
+extends MutableElement<HE>
+implements Headerable<HE> {
+	
+	//attribute
+	private final MutableProperty<NonEmptyText> headerProperty =
+	new MutableProperty<NonEmptyText>(
+		PascalCaseNameCatalogue.HEADER,
+		h -> setHeader(h.getValue()),
+		s -> NonEmptyText.createFromSpecification(s)
+	);
+	
+	//method
+	public final String getHeader() {
+		return headerProperty.getValue().getValue();
+	}
+
+	//method
+	public final HE setHeader(final String header) {
+		
+		headerProperty.setValue(new NonEmptyText(header));
+		
+		return getInstance();
+	}	
+}
