2684cb10ea8e1a5d80a9a67da340d480fc58e886
Silvan Wyss <silvan.wyss@gmail.com>
2018-05-10 16:18:32 +0200

    Created TabContainerTutorial.

---
+++ Source/ch/nolix/elementTutorial/GUITutorial/TabContainerTutorial.java
@@ -0,0 +1,73 @@
+//package declaration
+package ch.nolix.elementTutorial.GUITutorial;
+
+//own imports
+import ch.nolix.element.GUI.Area;
+import ch.nolix.element.GUI.Frame;
+import ch.nolix.element.GUI.TabContainer;
+import ch.nolix.element.GUI.TabContainerTab;
+import ch.nolix.element.color.Color;
+
+//class
+/**
+ * The {@link TabContainerTutorial} provides a tutorial for the {@link TabContainer} class.
+ * Of the {@link TabContainerTutorial} no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-4
+ * @lines 70
+ */
+public final class TabContainerTutorial {
+
+	//main method
+	/**
+	 * Creates a {@link Frame} with a {@link TabContainer}.
+	 * 
+	 * @param args
+	 */
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		//Creates a tab container.
+		final var tabContainer =
+		new TabContainer(
+			new TabContainerTab(
+				"Tab1",
+				new Area()
+				.setBackgroundColor(Color.LIGHT_GREEN)
+			),
+			new TabContainerTab(
+				"Tab2",
+				new Area()
+				.setBackgroundColor(Color.LIGHT_BLUE)
+			),
+			new TabContainerTab(
+				"Tab3",
+				new Area()
+				.setBackgroundColor(Color.LIGHT_CORAL)
+			),
+			new TabContainerTab(
+				"Tab4",
+				new Area()
+				.setBackgroundColor(Color.LIGHT_PINK)
+			)
+		);
+		
+		//Configures the tab container.
+		tabContainer
+		.getRefBaseLook()
+		.setBorderThicknesses(1)
+		.setPaddings(10);
+		
+		//Creates a frame with the tab container.
+		new Frame()
+		.setTitle("Tab Container Tutorial")
+		.setRootWidget(tabContainer);
+	}
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of the {@link TabContainerTutorial} can be created.
+	 */
+	private TabContainerTutorial() {}
+}
