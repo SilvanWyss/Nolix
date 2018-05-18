609cab2a54d40b80d05be45f0249c1b8d4ed8f64
Silvan Wyss <silvan.wyss@gmail.com>
2018-05-18 23:36:25 +0200

    Added role to Button.

--- Source/ch/nolix/element/GUI/Button.java
+++ Source/ch/nolix/element/GUI/Button.java
@@ -1,19 +1,29 @@
 //package declaration
 package ch.nolix.element.GUI;
 
-//own import
+//own imports
+import ch.nolix.core.constants.PascalCaseNameCatalogue;
+import ch.nolix.core.constants.VariableNameCatalogue;
+import ch.nolix.core.container.List;
+import ch.nolix.core.specification.Specification;
+import ch.nolix.core.specification.StandardSpecification;
 import ch.nolix.element.color.Color;
+import ch.nolix.primitive.invalidStateException.UnexistingAttributeException;
+import ch.nolix.primitive.validator2.Validator;
 
 //class
 /**
  * @author Silvan Wyss
  * @month 2015-12
- * @lines 60
+ * @lines 190
  */
 public final class Button extends TextLineWidget<Button> {
 	
 	//constant
 	public static final String TYPE_NAME = "Button";
+	
+	//optional attribute
+	private ButtonRole role;
 	
 	//constructor
 	/**
@@ -30,7 +40,8 @@ public final class Button extends TextLineWidget<Button> {
 	 * Creates a new {@link Button} with the given text.
 	 * 
 	 * @param text
-	 * @throws NullArgumentException if the given text is null.
+	 * @throws NullArgumentException
+	 * if the given text is null.
 	 */
 	public Button(final String text) {
 		
@@ -42,11 +53,110 @@ public final class Button extends TextLineWidget<Button> {
 	
 	//method
 	/**
+	 * {@inheritDoc}
+	 */
+	public void addOrChangeAttribute(final Specification attribute) {
+		
+		//Enumerates the header of the given attribute.
+		switch (attribute.getHeader()) {
+			case PascalCaseNameCatalogue.ROLE:
+				setRole(ButtonRole.createFromSpecification(attribute));
+				break;
+			default:
+				
+				//Calls method of the base class.
+				super.addOrChangeAttribute(attribute);
+		}
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<StandardSpecification> getAttributes() {
+		
+		//Calls method of the base class.
+		final var attributes = super.getAttributes();
+		
+		//Handles the case that the current button has a role.
+		if (hasRole()) {
+			attributes.addAtEnd(
+				getRole().getSpecificationAs(PascalCaseNameCatalogue.ROLE)
+			);
+		}
+		
+		return attributes;
+	}
+	
+	//method
+	/**
+	 * @return the role of the current {@link Button}.
+	 * @throws UnexistingAttributeException
+	 * if the current {@link Button} has no role.
+	 */
+	public ButtonRole getRole() {
+		
+		supposeHasRole();
+		
+		return role;
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean hasRole() {
+		return (role != null);
+	}
+
+	//method
+	/**
 	 * @param role
-	 * @return true if the current {@link Button} has the given role.
+	 * @return true
+	 * if the current {@link Button} has the given role.
 	 */
 	public boolean hasRole(final String role) {
-		return false;
+		
+		//Handles the case that the current link button has no role.
+		if (!hasRole()) {
+			return false;
+		}
+		
+		//Handles the case that the current link button has a role.
+		return getRole().toString().equals(role);
+	}
+	
+	/**
+	 * Removes the role of the current {@link Button}.
+	 * 
+	 * @return the current {@link Button}.
+	 */
+	public Button removeRole() {
+		
+		role = null;
+		
+		return this;
+	}
+	
+	/**
+	 * Sets the role of the current {@link Button}.
+	 * 
+	 * @param role
+	 * @return the current {@link Button}.
+	 * @throws NullArgumentException if the given role is null.
+	 */
+	public Button setRole(final ButtonRole role) {
+		
+		//Checks if the given role is not null.
+		Validator
+		.suppose(role)
+		.thatIsNamed(VariableNameCatalogue.ROLE)
+		.isNotNull();
+		
+		//Sets the role of the current button.
+		this.role = role;
+		
+		return this;
 	}
 
 	//method
@@ -65,5 +175,23 @@ public final class Button extends TextLineWidget<Button> {
 		
 		getRefHoverLook()
 		.setBackgroundColor(Color.GREY);
+	}
+	
+	
+	//method
+	/**
+	 * @throws UnexistingAttributeException
+	 * if the current {@link Button} has no role.
+	 */
+	private void supposeHasRole() {
+		
+		//Checks if the current button has a role.
+		if (!hasRole()) {
+			throw 
+			new UnexistingAttributeException(
+				this,
+				VariableNameCatalogue.ROLE
+			);
+		}
 	}
 }
