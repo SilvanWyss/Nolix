dbb1649092093f17232d96509ebc9611f4c9c868
Silvan Wyss <silvan.wyss@gmail.com>
2018-05-23 23:20:27 +0200

    Edited CreateEntitySession.

--- Source/ch/nolix/system/databaseApplication/CreateEntitySession.java
+++ Source/ch/nolix/system/databaseApplication/CreateEntitySession.java
@@ -5,11 +5,14 @@ package ch.nolix.system.databaseApplication;
 import ch.nolix.core.container.List;
 import ch.nolix.core.databaseAdapter.Entity;
 import ch.nolix.core.databaseAdapter.EntitySet;
+import ch.nolix.core.databaseAdapter.Property;
+import ch.nolix.core.databaseAdapter.ReferenceProperty;
 import ch.nolix.element.GUI.Button;
 import ch.nolix.element.GUI.ButtonRole;
 import ch.nolix.element.GUI.Grid;
 import ch.nolix.element.GUI.HorizontalStack;
 import ch.nolix.element.GUI.Label;
+import ch.nolix.element.GUI.SelectionMenu;
 import ch.nolix.element.GUI.TextBox;
 import ch.nolix.element.GUI.VerticalStack;
 import ch.nolix.primitive.validator2.Validator;
@@ -43,13 +46,40 @@ public final class CreateEntitySession extends HeaderedSession {
 	public void CreateEntity() {
 		
 		final var entity = getRefEntitySet().createEmptyEntity();
+		getRefEntitySet().addEntity(entity);
 		
-		for (final var p : entity.getRefProperties().getRefSelected(p -> p.isDataProperty())) {		
-			final TextBox dataTextBox = getRefClient().getRefGUI().getRefWidgetByNameRecursively(p.getHeader());
-			p.setGenericValue(dataTextBox.getText());
+		for (final var p : entity.getRefProperties()) {	
+			
+			switch (p.getPropertyKind()) {
+				case DATA:
+					
+					final var property = (Property<?>)p;
+			
+					final TextBox dataTextBox =
+					getRefClient().getRefGUI().getRefWidgetByNameRecursively(p.getHeader());
+					
+					property.setGenericValue(dataTextBox.getText());
+					
+					break;
+					
+				case REFERENCE:
+					
+					final var referenceProperty = (ReferenceProperty<Entity>)p;
+					
+					final SelectionMenu referenceSelectionMenu =
+					getRefClient().getRefGUI().getRefWidgetByNameRecursively(referenceProperty.getHeader());
+					
+					referenceProperty.set(
+						referenceProperty
+						.getReferencedEntitySet()
+						.getRefEntityById(referenceSelectionMenu.getSelectedItemId())
+					);
+					
+					break;
+			}
 		}
 		
-		getRefEntitySet().addEntity(entity);
+		
 		getRefDatabaseAdapter().saveChanges();
 		
 		openEntitySetSession();
@@ -79,28 +109,67 @@ public final class CreateEntitySession extends HeaderedSession {
 	//method
 	private Grid createDataEntryGrid() {
 		
-		final var dataEntryGrid = new Grid();
+		final var dataGrid = new Grid();
+		
+		var emptyEntity = getRefEntitySet().createEmptyEntity();
+		getRefEntitySet().addEntity(emptyEntity);
 		
 		int rowIndex = 1;
-		for (final var c : getRefEntitySet().getColumns().getRefSelected(c -> c.isDataColumn())) {
+		for (final var p : emptyEntity.getRefProperties()) {
 			
-			dataEntryGrid
-			.setWidget(
-				rowIndex,
-				1,
-				new Label(c.getHeader())
-			)
-			.setWidget(
-				rowIndex,
-				2,
-				new TextBox()
-				.setName(c.getHeader())
-			);
-			
-			rowIndex++;
+			switch (p.getPropertyKind()) {
+				case DATA:
+					
+					final var property = (Property<?>)p;
+					
+					dataGrid
+					.setWidget(
+						rowIndex,
+						1,
+						new Label(p.getHeader())
+					)
+					.setWidget(
+						rowIndex,
+						2,
+						new TextBox()
+						.setName(property.getHeader())
+					);
+					
+					rowIndex++;
+					
+					break;
+				case REFERENCE:
+					
+					final var referenceProperty = (ReferenceProperty<Entity>)p;
+					
+					dataGrid
+					.setWidget(
+						rowIndex,
+						1,
+						new Label(p.getHeader())
+					);
+					
+					final var referencesSelectionMenu = new SelectionMenu().setName(referenceProperty.getHeader());
+					
+					for (final var e : referenceProperty.getReferencedEntitySet().getRefEntities()) {
+						referencesSelectionMenu.addItem(e.getId(), e.getParentEntitySet().getName() + e.getId());
+					}
+					
+					dataGrid.setWidget(
+						rowIndex,
+						2,
+						referencesSelectionMenu
+					);
+					
+					rowIndex++;
+					
+					break;
+			}
 		}
 		
-		return dataEntryGrid;
+		getRefDatabaseAdapter().reset();
+		
+		return dataGrid;
 	}
 	
 	//method
