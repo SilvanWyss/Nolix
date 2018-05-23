e2c4e44c4eda2c53591f0cc046c9fe2415a5db8a
Silvan Wyss <silvan.wyss@gmail.com>
2018-05-23 23:53:57 +0200

    Extended DataGenerator.

--- Source/ch/nolix/tutorialApplication/movieManager/DataGenerator.java
+++ Source/ch/nolix/tutorialApplication/movieManager/DataGenerator.java
@@ -8,5 +8,54 @@ import ch.nolix.core.databaseAdapter.DatabaseAdapter;
 public final class DataGenerator {
 
 	//method
-	public void generateAndSaveSampleData(final DatabaseAdapter movieManagerDatabaseAdapter) {}
+	public void generateAndSaveSampleData(final DatabaseAdapter movieManagerDatabaseAdapter) {
+		
+		final var actionGenre = new Genre();
+		actionGenre.Name.setValue("Action");
+		
+		final var dramaGenre = new Genre();
+		dramaGenre.Name.setValue("Drama");
+		
+		final var fantasyGenre = new Genre();
+		fantasyGenre.Name.setValue("Fantasy");
+		
+		final var horrorGenre = new Genre();
+		horrorGenre.Name.setValue("Horror");
+		
+		movieManagerDatabaseAdapter
+		.getRefEntitySet(Genre.class)
+		.addEntity(
+			actionGenre,
+			dramaGenre,
+			fantasyGenre,
+			horrorGenre
+		);
+		
+		movieManagerDatabaseAdapter.saveChanges();
+		
+		final var battleRoyaleMovie = new Movie();
+		battleRoyaleMovie.Title.setValue("Battle Royale");
+		battleRoyaleMovie.Genre.set(
+			movieManagerDatabaseAdapter
+			.getRefEntitySet(Genre.class)
+			.getRefEntities()
+			.getRefFirst(g -> g.Name.getValue().equals("Drama")));
+		
+		final var hobbitMovie = new Movie();
+		hobbitMovie.Title.setValue("Hobbit");
+		hobbitMovie.Genre.set(
+			movieManagerDatabaseAdapter
+			.getRefEntitySet(Genre.class)
+			.getRefEntities()
+			.getRefFirst(g -> g.Name.getValue().equals("Fantasy")));
+		
+		movieManagerDatabaseAdapter
+		.getRefEntitySet(Movie.class)
+		.addEntity(
+			battleRoyaleMovie,
+			hobbitMovie
+		);
+		
+		movieManagerDatabaseAdapter.saveChanges();
+	}
 }
