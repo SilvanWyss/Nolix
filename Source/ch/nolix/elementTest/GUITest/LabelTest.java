79f01d09d61a9d99c6b27f979e78bb6cc9043ed1
Silvan Wyss <silvan.wyss@gmail.com>
2018-05-28 22:03:08 +0200

    Refactored LabelTest.

--- Source/ch/nolix/elementTest/GUITest/LabelTest.java
+++ Source/ch/nolix/elementTest/GUITest/LabelTest.java
@@ -1,51 +1,43 @@
-/*
- * file:	LabelTest.java
- * author:	Silvan Wyss
- * month:	2015-12
- * lines:	50
- */
-
 //package declaration
 package ch.nolix.elementTest.GUITest;
 
+//own imports
 import ch.nolix.element.GUI.Label;
 import ch.nolix.primitive.test2.Test;
 
 //test class
 /**
- * This class is a test class for the label class.
+ * A {@link LabelTest} is a test for a {@link Label}.
+ * 
+ * @author Silvan Wyss
+ * @month 2015-12
+ * @lines 40
  */
 public final class LabelTest extends Test {
 	
 	//test method
-	public void testSetNormalTextSize() {
-		
-		//parameter definition
-		final int textSize = 25;
+	public void test_getRefBaseLook_setTextSize() {
 		
 		//setup
-		Label label = new Label();
+		final var label = new Label();
 		
 		//execution
-		label.getRefBaseLook().setTextSize(textSize);
+		label.getRefBaseLook().setTextSize(25);
 		
 		//verification
-		expect(label.getRefBaseLook().getRecursiveOrDefaultTextSize()).isEqualTo(textSize);
+		expect(label.getRefBaseLook().getRecursiveOrDefaultTextSize()).isEqualTo(25);
 	}
 	
 	//test method
-	public void testSetText() {
-		
-		//parameter definition
-		final String text = "Test";
+	public void test_setText() {
 		
 		//setup
-		Label label = new Label();
+		final var label = new Label();
 		
 		//execution
-		label.setText(text);
+		label.setText("Helix");
 		
 		//verification
-		expect(label.getText()).isEqualTo(text);
+		expect(label.getText()).isEqualTo("Helix");
 	}
 }
