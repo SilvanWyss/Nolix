383614bb306a384cb973951888bf13aa966fe486
Silvan Wyss <silvan.wyss@gmail.com>
2018-06-12 13:25:32 +0200

    Improved DataGenerator.

--- Source/ch/nolix/tutorialApplication/movieManager/DataGenerator.java
+++ Source/ch/nolix/tutorialApplication/movieManager/DataGenerator.java
@@ -10,52 +10,54 @@ public final class DataGenerator {
 	//method
 	public void generateAndSaveSampleData(final DatabaseAdapter movieManagerDatabaseAdapter) {
 		
-		final var actionGenre = new Genre();
-		actionGenre.Name.setValue("Action");
-		
-		final var dramaGenre = new Genre();
-		dramaGenre.Name.setValue("Drama");
-		
-		final var fantasyGenre = new Genre();
-		fantasyGenre.Name.setValue("Fantasy");
-		
-		final var horrorGenre = new Genre();
-		horrorGenre.Name.setValue("Horror");
-		
-		movieManagerDatabaseAdapter
-		.getRefEntitySet(Genre.class)
-		.addEntity(
-			actionGenre,
-			dramaGenre,
-			fantasyGenre,
-			horrorGenre
-		);
-		
-		movieManagerDatabaseAdapter.saveChanges();
-		
-		final var battleRoyaleMovie = new Movie();
-		battleRoyaleMovie.Title.setValue("Battle Royale");
-		battleRoyaleMovie.Genre.set(
+		//Creates and saves genres.
+			final var actionGenre = new Genre();
+			actionGenre.Name.setValue("Action");
+			
+			final var dramaGenre = new Genre();
+			dramaGenre.Name.setValue("Drama");
+			
+			final var fantasyGenre = new Genre();
+			fantasyGenre.Name.setValue("Fantasy");
+			
+			final var horrorGenre = new Genre();
+			horrorGenre.Name.setValue("Horror");
+			
 			movieManagerDatabaseAdapter
 			.getRefEntitySet(Genre.class)
-			.getRefEntities()
-			.getRefFirst(g -> g.Name.getValue().equals("Drama")));
+			.addEntity(
+				actionGenre,
+				dramaGenre,
+				fantasyGenre,
+				horrorGenre
+			);
+			
+			movieManagerDatabaseAdapter.saveChanges();
 		
-		final var hobbitMovie = new Movie();
-		hobbitMovie.Title.setValue("Hobbit");
-		hobbitMovie.Genre.set(
+		//Creates and saves movies.
+			final var battleRoyaleMovie = new Movie();
+			battleRoyaleMovie.Title.setValue("Battle Royale");
+			battleRoyaleMovie.Genre.set(
+				movieManagerDatabaseAdapter
+				.getRefEntitySet(Genre.class)
+				.getRefEntities()
+				.getRefFirst(g -> g.Name.hasEqualValue("Drama")));
+			
+			final var hobbitMovie = new Movie();
+			hobbitMovie.Title.setValue("Hobbit");
+			hobbitMovie.Genre.set(
+				movieManagerDatabaseAdapter
+				.getRefEntitySet(Genre.class)
+				.getRefEntities()
+				.getRefFirst(g -> g.Name.hasEqualValue("Fantasy")));
+			
 			movieManagerDatabaseAdapter
-			.getRefEntitySet(Genre.class)
-			.getRefEntities()
-			.getRefFirst(g -> g.Name.getValue().equals("Fantasy")));
-		
-		movieManagerDatabaseAdapter
-		.getRefEntitySet(Movie.class)
-		.addEntity(
-			battleRoyaleMovie,
-			hobbitMovie
-		);
-		
-		movieManagerDatabaseAdapter.saveChanges();
+			.getRefEntitySet(Movie.class)
+			.addEntity(
+				battleRoyaleMovie,
+				hobbitMovie
+			);
+			
+			movieManagerDatabaseAdapter.saveChanges();
 	}
 }
