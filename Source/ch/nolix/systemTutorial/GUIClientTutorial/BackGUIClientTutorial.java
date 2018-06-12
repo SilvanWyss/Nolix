cc15b08b26d9a62bc1bc0199c6367715002af652
Silvan Wyss <silvan.wyss@gmail.com>
2018-06-12 12:28:18 +0200

    Edited BackGUIClientTutorial.

--- Source/ch/nolix/systemTutorial/GUIClientTutorial/BackGUIClientTutorial.java
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/BackGUIClientTutorial.java
@@ -8,13 +8,14 @@ import ch.nolix.element.GUI.CursorIcon;
 import ch.nolix.element.GUI.ValueCatalogue;
 import ch.nolix.element.color.Color;
 import ch.nolix.system.GUIClient.BackGUIClient;
+import ch.nolix.system.GUIClient.BackGUISession;
 import ch.nolix.system.GUIClient.FrontGUIClient;
 import ch.nolix.system.client.Application;
-import ch.nolix.system.client.Session;
 
 //class
 /**
- * This class provides a tutorial for the GUI client class.
+ * The {@link BackGUIClientTutorial} is a tutorial for a {@link BackGUIClient}.
+ * Of the {@link BackGUIClientTutorial} no instance can be created.
  * 
  * @author Silvan Wyss
  * @month 2017-02
@@ -24,13 +25,13 @@ public final class BackGUIClientTutorial {
 
 	//main method
 	/**
-	 * 1. Creates an application for dialog clients.
-	 * 2. Creates a front dialog client that will connect to the application.
+	 * 1. Creates a an application for back GUI clients.
+	 * 2. Creates a front GUI client that will connect to the application.
 	 * 
 	 * @param arguments
 	 */
 	@SuppressWarnings("resource")
-	public static void main(final String[] arguments) {
+	public static void main(String[] args) {
 		
 		//Creates an application.
 		final var application
@@ -40,23 +41,20 @@ public final class BackGUIClientTutorial {
 			MainSession.class
 		);
 		
-		//Creates a front dialog client that will to the application.
+		//Creates a front GUI client that will to the application.
 		new FrontGUIClient(application);
 	}
 	
 	//inner class
-	private static final class MainSession extends Session<BackGUIClient> {
+	private static final class MainSession extends BackGUISession {
 
 		//attribute
 		private int counter = 1;
 		
 		//method
-		/**
-		 * Initializes this main session.
-		 */
 		public void initialize() {			
 			
-			final Button button =
+			final var button =
 			new Button()
 			.setText("Change color")
 			.setLeftMouseButtonPressCommand(() -> changeColor())
@@ -76,22 +74,16 @@ public final class BackGUIClientTutorial {
 			.getRefHoverFocusLook()
 			.setTextSize(ValueCatalogue.BIG_TEXT_SIZE);
 			
-			getParentClient()
-			.getRefGUI()
-			.setTitle("Dialog Client Tutorial")
-			.removeConfiguration()
+			getRefGUI()
+			.setTitle("Back GUI Client Tutorial")
 			.setBackgroundColor(Color.GREEN)
 			.setContentPosition(ContentPosition.Center)
 			.setRootWidget(button);	
 		}
 		
 		//method
-		/**
-		 * Changes the color of the GUI of the client of this main session.
-		 */
-		@SuppressWarnings("unused")
 		public void changeColor() {
-			
+						
 			if (counter % 2 == 0) {
 				getParentClient().getRefGUI().setBackgroundColor(Color.GREEN);
 			}
@@ -105,7 +97,7 @@ public final class BackGUIClientTutorial {
 	
 	//private constructor
 	/**
-	 * Avoids that an instance of this class can be created.
+	 * Avoids that an instance of the {@link BackGUIClientTutorial} can be created.
 	 */
 	private BackGUIClientTutorial() {}
 }
