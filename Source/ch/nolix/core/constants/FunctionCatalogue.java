4735544bde6fc537881690c124fe92a97d8dcbc1
Silvan Wyss <silvan.wyss@gmail.com>
2018-06-14 14:52:20 +0200

    Edited FunctionCatalogue.

--- Source/ch/nolix/core/constants/FunctionCatalogue.java
+++ Source/ch/nolix/core/constants/FunctionCatalogue.java
@@ -9,11 +9,12 @@ import ch.nolix.core.functionInterfaces.IFunction;
 
 //class
 /**
- * Of this class no instance can be created.
+ * The {@link FunctionCatalogue} provides functions.
+ * Of the {@link FunctionCatalogue} no instance can be created.
  * 
  * @author Silvan Wyss
  * @month 2016-12
- * @lines 90
+ * @lines 80
  */
 public final class FunctionCatalogue {
 	
@@ -26,37 +27,31 @@ public final class FunctionCatalogue {
 	
 	//function
 	/**
-	 * This function returns false for a given input object.
+	 * This function returns false for a given object.
 	 */
-	public static final IElementTakerBooleanGetter<Object> FALSE_FUNCTION
-	= (o) -> {
-		return false;
-	};
+	public static final IElementTakerBooleanGetter<Object> FALSE_FUNCTION =
+	o -> false;
 
 	//function
 	/**
-	 * This function returns a given input object.
+	 * This function returns a given object.
 	 */
-	public static final IElementTakerElementGetter<Object, Object> IDENTITY_FUNCTION
-	= (o) -> {
-		return o;
-	};
+	public static final IElementTakerElementGetter<Object, Object> IDENTITY_FUNCTION =
+	o -> o;
 	
 	//function
 	/**
-	 * This function returns null for a given input object.
+	 * This function returns null for a given object.
 	 */
-	public static final IElementTakerElementGetter<Object, Object> NULL_FUNCTION
-	= (o) -> {
-		return null;
-	};
+	public static final IElementTakerElementGetter<Object, Object> NULL_FUNCTION =
+	o -> null;
 	
 	//function
 	/**
-	 * This function returns the string representation for a given input object.
+	 * This function returns a string that represents the given object.
 	 */
-	public static final IElementTakerElementGetter<Object, String> TO_STRING_FUNCTION
-	= (o) -> {
+	public static final IElementTakerElementGetter<Object, String> TO_STRING_FUNCTION =
+	o -> {
 		
 		//Handles the case that the given object is null.
 		if (o == null) {
@@ -69,25 +64,21 @@ public final class FunctionCatalogue {
 	
 	//function
 	/**
-	 * This function returns true for a given input object.
+	 * This function returns true for a given object.
 	 */
-	public static final IElementTakerBooleanGetter<Object> TRUE_FUNCTION
-	= (o) -> {
-		return true;
-	};
+	public static final IElementTakerBooleanGetter<Object> TRUE_FUNCTION =
+	o -> true;
 	
 	//function
 	/**
 	 * This function returns 0.0 for a given input object.
 	 */
-	public static final IElementTakerDoubleGetter<Object> ZERO_FUNCTION
-	= (o) -> {
-		return 0.0;
-	};
+	public static final IElementTakerDoubleGetter<Object> ZERO_FUNCTION =
+	o -> 0.0;
 	
 	//private constructor
 	/**
-	 * Avoids that an instance of this class can be created.
+	 * Avoids that an instance of the {@link FunctionCatalogue} can be created.
 	 */
 	private FunctionCatalogue() {}
 }
