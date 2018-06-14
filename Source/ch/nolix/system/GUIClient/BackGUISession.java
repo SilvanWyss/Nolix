7f22803ed129e22442d47525a9a536ae08ce5021
Silvan Wyss <silvan.wyss@gmail.com>
2018-06-14 19:13:44 +0200

    Edited BackGUISession.

--- Source/ch/nolix/system/GUIClient/BackGUISession.java
+++ Source/ch/nolix/system/GUIClient/BackGUISession.java
@@ -6,16 +6,15 @@ import ch.nolix.element.GUI.GUI;
 import ch.nolix.element.GUI.InvisibleGUI;
 import ch.nolix.system.client.Session;
 
-//TODO
 //abstract class
 public abstract class BackGUISession
 extends Session<BackGUIClient> {
 	
 	//attribute
-	private final InvisibleGUI GUI = new InvisibleGUI();
+	private final GUI<?> GUI_ = new InvisibleGUI();
 	
 	//method
 	public final GUI<?> getRefGUI() {
-		return GUI;
+		return GUI_;
 	}
 }
