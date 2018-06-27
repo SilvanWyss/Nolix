af88d8e8e496f2563615a7a7875a70c2566bc753
Silvan Wyss <silvan.wyss@gmail.com>
2018-06-27 22:02:18 +0200

    Edited MultiVariableNameCatalogue.

--- Source/ch/nolix/core/constants/MultiVariableNameCatalogue.java
+++ Source/ch/nolix/core/constants/MultiVariableNameCatalogue.java
@@ -3,13 +3,13 @@ package ch.nolix.core.constants;
 
 //class
 /**
- * Of this class no instance can be created.
+ * Of the {@link MultiVariableNameCatalogue} no instance can be created.
  * 
  * @author Silvan Wyss
  * @month 2018-03
  * @lines 20
  */
-public class MultiVariableNameCatalogue {
+public final class MultiVariableNameCatalogue {
 	
 	//constants
 	public static final String INPUT_VALUES = "input values";
@@ -18,7 +18,7 @@ public class MultiVariableNameCatalogue {
 
 	//private constructor
 	/**
-	 * Avoids that an instance of this class can be created.
+	 * Avoids that an instance of the {@link MultiVariableNameCatalogue} can be created.
 	 */
 	private MultiVariableNameCatalogue() {}
 }
