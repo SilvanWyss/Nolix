3852e804692980a18fbb85e95a1e8f163f95f60e
Silvan Wyss <silvan.wyss@gmail.com>
2018-07-07 15:13:37 +0200

    Created DownloaderTutorial.

---
+++ Source/ch/nolix/elementTutorial/GUITutorial/DownloaderTutorial.java
@@ -0,0 +1,61 @@
+//package declaration
+package ch.nolix.elementTutorial.GUITutorial;
+
+//own imports
+import ch.nolix.core.fileSystem.FileAccessor;
+import ch.nolix.core.fileSystem.FileSystemAccessor;
+import ch.nolix.element.GUI.Downloader;
+import ch.nolix.element.GUI.Frame;
+import ch.nolix.element.color.Color;
+
+//class
+/**
+ * The {@link DownloaderTutorial} provides a tutorial for a {@link Downloader}.
+ * Of the {@link DownloaderTutorial} no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-07
+ * @lines 60
+ */
+public final class DownloaderTutorial {
+	
+	//main method
+	/**
+	 * Creates a {@link Frame} with a {@link Downloader}.
+	 * 
+	 * @param args
+	 */
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		//Creates a file getter for the downloader.
+		new FileSystemAccessor().overwriteFile("tutorial.txt", "Tutorial");
+		
+		//Creates a downloader
+		final var downloader =
+		new Downloader()
+		.setText("Download file")
+		.setFileGetter(() -> new FileAccessor("tutorial.txt").readFileToBytes()) ;
+		
+		//Configures the look of the downloader.
+		downloader
+		.getRefBaseLook()
+		.setTextSize(50)
+		.setTextColor(Color.DARK_BLUE);
+		
+		downloader
+		.getRefHoverLook()
+		.setTextColor(Color.BLUE);
+		
+		//Creates a frame with the downloader.
+		new Frame()
+		.setTitle("Downloader Tutorial")
+		.setRootWidget(downloader);
+	}
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of the {@link DownloaderTutorial} can be created.
+	 */
+	private DownloaderTutorial() {}
+}
