53ba08d1aa69329a112f02202e710d06b050b81b
Silvan Wyss <silvan.wyss@gmail.com>
2018-07-08 12:14:47 +0200

    Edited HomeSession.

--- Source/ch/nolix/system/databaseApplication/HomeSession.java
+++ Source/ch/nolix/system/databaseApplication/HomeSession.java
@@ -4,14 +4,18 @@ package ch.nolix.system.databaseApplication;
 //own imports
 import ch.nolix.core.container.List;
 import ch.nolix.element.GUI.Button;
+import ch.nolix.element.GUI.ButtonRole;
 import ch.nolix.element.GUI.Grid;
 
 //class
 public final class HomeSession extends HeaderedSession {
-
+	
+	//constant
+	public static final String TITLE = "Home";
+	
 	//constructor
 	public HomeSession() {
-		super("Home");
+		super(TITLE);
 	}
 	
 	//method
@@ -24,14 +28,15 @@ public final class HomeSession extends HeaderedSession {
 		
 		final var entitySetsGrid = new Grid();
 		
-		int row = 1;
-		int column = 1;
+		var row = 1;
+		var column = 1;
 		for (final var es : getRefDatabaseAdapter().getRefEntitySets()) {
 			
 			entitySetsGrid.setWidget(
 				row,
 				column,
 				new Button(es.getName())
+				.setRole(ButtonRole.LinkButton)
 				.setLeftMouseButtonPressCommand(
 					() -> openEntitySetSession(es.getName())
 				)
