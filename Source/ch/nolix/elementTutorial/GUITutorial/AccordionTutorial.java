01bf1cc94cfba5496ab70e0fab0b243f708a4be7
Silvan Wyss <silvan.wyss@gmail.com>
2018-08-13 21:30:17 +0200

    Created AccordionTutorial.

---
+++ Source/ch/nolix/elementTutorial/GUITutorial/AccordionTutorial.java
@@ -0,0 +1,82 @@
+//package declaration
+package ch.nolix.elementTutorial.GUITutorial;
+
+//own imports
+import ch.nolix.element.GUI.Area;
+import ch.nolix.element.GUI.Frame;
+import ch.nolix.element.GUI.Accordion;
+import ch.nolix.element.GUI.AccordionTab;
+import ch.nolix.element.color.Color;
+
+//class
+/**
+ * The {@link AccordionTutorial} provides a tutorial for a {@link Accordion}.
+ * Of the {@link AccordionTutorial} no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-08
+ * @lines 80
+ */
+public final class AccordionTutorial {
+
+	//main method
+	/**
+	 * Creates a {@link Frame} with a {@link Accordion}.
+	 * 
+	 * @param args
+	 */
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		//Creates an accordion.
+		final var accordion =
+		new Accordion(
+			new AccordionTab(
+				"Tab1",
+				new Area()
+				.setBackgroundColor(Color.DARK_BLUE)
+				.setWidth(500)
+				.setHeight(200)
+			),
+			new AccordionTab(
+				"Tab2",
+				new Area()
+				.setBackgroundColor(Color.BLUE)
+				.setWidth(200)
+				.setHeight(100)
+			),
+			new AccordionTab(
+				"Tab3",
+				new Area()
+				.setBackgroundColor(Color.DARK_BLUE)
+				.setWidth(500)
+				.setHeight(200)
+			),
+			new AccordionTab(
+				"Tab4",
+				new Area()
+				.setBackgroundColor(Color.BLUE)
+				.setWidth(200)
+				.setHeight(100)
+			)
+		);
+		
+		//Configures the look of the accordion.
+		accordion
+		.getRefBaseLook()
+		.setBorderThicknesses(5)
+		.setBorderColors(Color.DARK_BLUE)
+		.setBackgroundColor(Color.ALICE_BLUE);
+		
+		//Creates a frame with the accordion.
+		new Frame()
+		.setTitle("Accordion Container Tutorial")
+		.setRootWidget(accordion);
+	}
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of the {@link AccordionTutorial} can be created.
+	 */
+	private AccordionTutorial() {}
+}
