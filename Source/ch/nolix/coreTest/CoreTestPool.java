df6757b522727611e251beba2635d4bd4dda4bd7
Silvan Wyss <silvan.wyss@gmail.com>
2018-08-18 20:38:41 +0200

    Extended CoreTestPool.

--- Source/ch/nolix/coreTest/CoreTestPool.java
+++ Source/ch/nolix/coreTest/CoreTestPool.java
@@ -1,32 +1,41 @@
 //package declaration
 package ch.nolix.coreTest;
 
+//own imports
 import ch.nolix.coreTest.containerTest.ContainerTestPool;
+import ch.nolix.coreTest.databaseAdapter.DatabaseAdapterTestPool;
 import ch.nolix.coreTest.enumTest.EnumTestPool;
+import ch.nolix.coreTest.helperTest.HelperTestPool;
 import ch.nolix.coreTest.mathematicsTest.MathematicsTestPool;
+import ch.nolix.coreTest.sequencerTest.SequencerTestPool;
 import ch.nolix.coreTest.specificationTest.SpecificationTestPool;
+import ch.nolix.coreTest.utilTest.UtilTestPool;
 import ch.nolix.primitive.testoid.TestPool;
 
 //class
 /**
  * @author Silvan Wyss
  * @month 2016-08
- * @lines 30
+ * @lines 40
  */
 public final class CoreTestPool extends TestPool {
 
 	//constructor
 	/**
-	 * Creates a new core test pool.
+	 * Creates a new {@link CoreTestPool}.
 	 */
 	public CoreTestPool() {
 		addTestPool(
 			new ContainerTestPool(),
+			new DatabaseAdapterTestPool(),
 			new ch.nolix.coreTest.endPointTest.EndPointTestPool(),
 			new ch.nolix.coreTest.endPoint3Test.EndPointTestPool(),
 			new EnumTestPool(),
+			new HelperTestPool(),
 			new MathematicsTestPool(),
-			new SpecificationTestPool()
+			new SequencerTestPool(),
+			new SpecificationTestPool(),
+			new UtilTestPool()
 		);
 	}
 }
