1aa181897af0be1a56d2c5322a3458f18f96937c
Silvan Wyss <silvan.wyss@gmail.com>
2018-08-18 18:05:35 +0200

    Created HelperTestPool.

---
+++ Source/ch/nolix/coreTest/helperTest/HelperTestPool.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.coreTest.helperTest;
+
+//own import
+import ch.nolix.primitive.testoid.TestPool;
+
+//class
+/**
+ * @author Silvan Wyss
+ * @month 2018-08
+ * @lines 20
+ */
+public final class HelperTestPool extends TestPool {
+	
+	//constructor
+	/**
+	 * Creates a new {@link HelperTestPool}.
+	 */
+	public HelperTestPool() {
+		addTest(
+			new CharacterHelperTest(),
+			new StringHelperTest()
+		);
+	}
+}
