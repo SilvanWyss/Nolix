5521871b940c59fd042c7b7ca216a66ffcf19a0a
Silvan Wyss <silvan.wyss@gmail.com>
2018-08-18 18:06:17 +0200

    Refactored StringHelperTest.

--- Source/ch/nolix/coreTest/helperTest/StringHelperTest.java
+++ Source/ch/nolix/coreTest/helperTest/StringHelperTest.java
@@ -1,84 +1,81 @@
-/*
- * file:	StringHelperTest.java
- * author:	Silvan Wyss
- * month:	2015
- * lines:	80
- */
-
 //package declaration
 package ch.nolix.coreTest.helperTest;
 
 //own imports
 import ch.nolix.core.helper.StringHelper;
-import ch.nolix.primitive.test.Test;
+import ch.nolix.primitive.test2.Test;
 
 //test class
 /**
- * This class is a test class for the string helper class.
+ * A {@link StringHelperTest} is a test for the {@link StringHelper} class.
+ * 
+ * @author Silvan Wyss
+ * @month 2015-12
+ * @lines 80
  */
 public class StringHelperTest extends Test {
+	
+	//test method
+	public void test_toInt_1() {
+		expect(StringHelper.toInt("1")).isEqualTo(1);
+		expect(StringHelper.toInt("10")).isEqualTo(10);
+		expect(StringHelper.toInt("100")).isEqualTo(100);
+		expect(StringHelper.toInt("1000")).isEqualTo(1000);
+	}
+	
+	//test method
+	public void test_toInt_2() {
+		expect(StringHelper.toInt("-1")).isEqualTo(-1);
+		expect(StringHelper.toInt("-10")).isEqualTo(-10);
+		expect(StringHelper.toInt("-100")).isEqualTo(-100);
+		expect(StringHelper.toInt("-1000")).isEqualTo(-1000);
+	}
+	
+	//test method
+	public void test_toInt_3() {
+		expect(StringHelper.toInt("5")).isEqualTo(5);
+		expect(StringHelper.toInt("55")).isEqualTo(55);
+		expect(StringHelper.toInt("555")).isEqualTo(555);
+		expect(StringHelper.toInt("5555")).isEqualTo(5555);
+	}
+	
+	//test method
+	public void test_toInt_4() {
+		expect(StringHelper.toInt("-5")).isEqualTo(-5);
+		expect(StringHelper.toInt("-55")).isEqualTo(-55);
+		expect(StringHelper.toInt("-555")).isEqualTo(-555);
+		expect(StringHelper.toInt("-5555")).isEqualTo(-5555);
+	}
+	
+	//test method
+	public void test_toInt_5() {
+		expect(StringHelper.toInt("0x1")).isEqualTo(0x1);
+		expect(StringHelper.toInt("0x10")).isEqualTo(0x10);
+		expect( StringHelper.toInt("0x100")).isEqualTo(0x100);
+		expect(StringHelper.toInt("0x1000")).isEqualTo(0x1000);
+	}
+	
+	//test method
+	public void test_toInt6() {
+		expect(StringHelper.toInt("-0x1")).isEqualTo(-0x1);
+		expect(StringHelper.toInt("-0x10")).isEqualTo(-0x10);
+		expect( StringHelper.toInt("-0x100")).isEqualTo(-0x100);
+		expect(StringHelper.toInt("-0x1000")).isEqualTo(-0x1000);
+	}
 
 	//test method
-	public void testToInteger1() {
-		expectEquality(1, StringHelper.toInt("1"));
-		expectEquality(10, StringHelper.toInt("10"));
-		expectEquality(100, StringHelper.toInt("100"));
-		expectEquality(1000, StringHelper.toInt("1000"));
+	public void test_toInt_7() {
+		expect(StringHelper.toInt("0x5")).isEqualTo(0x5);
+		expect(StringHelper.toInt("0x55")).isEqualTo(0x55);
+		expect(StringHelper.toInt("0x555")).isEqualTo(0x555);
+		expect(StringHelper.toInt("0x5555")).isEqualTo(0x5555);
 	}
 	
 	//test method
-	public void testToInteger2() {
-		expectEquality(2, StringHelper.toInt("2"));
-		expectEquality(24, StringHelper.toInt("24"));
-		expectEquality(246, StringHelper.toInt("246"));
-		expectEquality(2468, StringHelper.toInt("2468"));
-	}
-	
-	//test method
-	public void testToInteger3() {
-		expectEquality(-1, StringHelper.toInt("-1"));
-		expectEquality(-10, StringHelper.toInt("-10"));
-		expectEquality(-100, StringHelper.toInt("-100"));
-		expectEquality(-1000, StringHelper.toInt("-1000"));
-	}
-	
-	//test method
-	public void testToInteger4() {
-		expectEquality(-2, StringHelper.toInt("-2"));
-		expectEquality(-24, StringHelper.toInt("-24"));
-		expectEquality(-246, StringHelper.toInt("-246"));
-		expectEquality(-2468, StringHelper.toInt("-2468"));
-	}
-	
-	//test method
-	public void testToInteger5() {
-		expectEquality(1, StringHelper.toInt("0x1"));
-		expectEquality(16, StringHelper.toInt("0x10"));
-		expectEquality(256, StringHelper.toInt("0x100"));
-		expectEquality(4096, StringHelper.toInt("0x1000"));
-	}
-	
-	//test method
-	public void testToInteger6() {
-		expectEquality(2, StringHelper.toInt("0x2"));
-		expectEquality(36, StringHelper.toInt("0x24"));
-		expectEquality(582, StringHelper.toInt("0x246"));
-		expectEquality(9320, StringHelper.toInt("0x2468"));
-	}
-
-	//test method
-	public void testToInteger7() {
-		expectEquality(-1, StringHelper.toInt("-0x1"));
-		expectEquality(-16, StringHelper.toInt("-0x10"));
-		expectEquality(-256, StringHelper.toInt("-0x100"));
-		expectEquality(-4096, StringHelper.toInt("-0x1000"));
-	}
-	
-	//test method
-	public void testToInteger8() {
-		expectEquality(-2, StringHelper.toInt("-0x2"));
-		expectEquality(-36, StringHelper.toInt("-0x24"));
-		expectEquality(-582, StringHelper.toInt("-0x246"));
-		expectEquality(-9320, StringHelper.toInt("-0x2468"));
+	public void test_toInt_8() {
+		expect(StringHelper.toInt("-0x5")).isEqualTo(-0x5);
+		expect(StringHelper.toInt("-0x55")).isEqualTo(-0x55);
+		expect(StringHelper.toInt("-0x555")).isEqualTo(-0x555);
+		expect(StringHelper.toInt("-0x5555")).isEqualTo(-0x5555);
 	}
 }
