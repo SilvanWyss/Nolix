e62a92ec9a3c9b4c36ab16d6aa0201e92e873e26
Silvan Wyss <silvan.wyss@gmail.com>
2018-08-23 21:29:21 +0200

    Created ImageWidget.

---
+++ Source/ch/nolix/element/GUI/ImageWidget.java
@@ -0,0 +1,110 @@
+//package declaration
+package ch.nolix.element.GUI;
+
+//own imports
+import ch.nolix.core.container.List;
+import ch.nolix.core.entity.MutableOptionalProperty;
+import ch.nolix.core.interfaces.Clearable;
+import ch.nolix.element.image.Image;
+import ch.nolix.element.painter.IPainter;
+
+//class
+public final class ImageWidget
+extends BorderWidget<ImageWidget, ImageWidgetLook>
+implements Clearable<ImageWidget> {
+	
+	//attribute
+	private final MutableOptionalProperty<Image> image =
+	new MutableOptionalProperty<>(
+		Image.TYPE_NAME,
+		i -> setImage(i),
+		s -> Image.createFromSpecification(s)
+	);
+	
+	//constructor
+	public ImageWidget() {
+		reset();
+		approveProperties();
+		applyUsableConfiguration();
+	}
+	
+	//constructor
+	public ImageWidget(final Image image) {
+		
+		this();
+		
+		setImage(image);
+	}
+	
+	//method
+	public ImageWidget clear() {
+		
+		image.clear();
+		
+		return this;
+	}
+	
+	//method
+	public CursorIcon getContentAreaCursorIcon() {
+		return getCustomCursorIcon();
+	}
+	
+	//method
+	public Image getRefImage() {
+		return image.getValue();
+	}
+
+	//method
+	public boolean hasRole(final String role) {
+		return false;
+	}
+	
+	//method
+	public boolean isEmpty() {
+		return image.isEmpty();
+	}
+	
+	//method
+	public boolean keepsFocus() {
+		return false;
+	}
+	
+	//method
+	public ImageWidget setImage(final Image image) {
+		
+		this.image.setValue(image);
+		
+		return this;
+	}
+	
+	//method
+	protected void applyUsableConfigurationWhenConfigurationIsReset() {}
+	
+	//method
+	protected ImageWidgetLook createWidgetLook() {
+		return new ImageWidgetLook();
+	}
+	
+	//method
+	protected void fillUpWidgets(final List<Widget<?, ?>> list) {}
+	
+	//method
+	protected int getContentAreaHeight() {
+		return (isEmpty() ? 0 : getRefImage().getHeight());
+	}
+
+	//method
+	protected int getContentAreaWidth() {
+		return (isEmpty() ? 0 : getRefImage().getWidth());
+	}
+
+	//method
+	protected void paintContentArea(
+		final ImageWidgetLook borderWidgetLook,
+		final IPainter painter
+	) {
+		if (containsAny()) {
+			painter.paintImage(getRefImage());
+		}
+	}
+}
---
+++ Source/ch/nolix/element/GUI/ImageWidgetLook.java
@@ -0,0 +1,5 @@
+//package declaration
+package ch.nolix.element.GUI;
+
+//class
+public final class ImageWidgetLook extends BorderWidgetLook<ImageWidgetLook> {}
