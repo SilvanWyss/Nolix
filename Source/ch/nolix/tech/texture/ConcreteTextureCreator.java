5afc33d3b9e529a0ec213748aa34153e82301c08
Silvan Wyss <silvan.wyss@gmail.com>
2018-08-25 16:28:09 +0200

    Created texture creators.

---
+++ Source/ch/nolix/tech/texture/ConcreteTextureCreator.java
@@ -0,0 +1,34 @@
+//package declaration
+package ch.nolix.tech.texture;
+
+//own imports
+import ch.nolix.element.color.Color;
+import ch.nolix.element.image.Image;
+import ch.nolix.techAPI.textureAPI.ITextureCreator;
+
+//class
+public final class ConcreteTextureCreator implements ITextureCreator {
+
+	//method
+	public Image createTexture(final int width,	final int height) {
+				
+		final var image = new Image(width, height);
+		
+		for (var x = 1; x <= image.getWidth(); x++) {
+			for (var y = 1; y <= image.getHeight(); y++) {		
+				
+				image.setPixel(x, y, new Color(0xA0A0A0));
+				
+				if ((x * x + y * y) % 11 == (x * y) % 13) {
+					image.setPixel(x, y, new Color(0xC0C0C0));
+				}
+				
+				if ((y + 1) * (x * y - y % 3) % 7== (x * y - x % 5 + y % 2) % 11) {
+					image.setPixel(x, y, new Color(0x808080));
+				}				
+			}
+		}
+		
+		return image;
+	}
+}
---
+++ Source/ch/nolix/tech/texture/JuteTextureCreator.java
@@ -0,0 +1,37 @@
+//package declaration
+package ch.nolix.tech.texture;
+
+//own imports
+import ch.nolix.element.color.Color;
+import ch.nolix.element.image.Image;
+import ch.nolix.techAPI.textureAPI.ITextureCreator;
+
+//class
+public final class JuteTextureCreator implements ITextureCreator {
+
+	//method
+	public Image createTexture(
+		final int width,
+		final int height
+	) {
+		
+		final var image = new Image(width, height);
+		
+		for (var x = 1; x <= image.getWidth(); x++) {
+			for (var y = 1; y <= image.getHeight(); y++) {		
+				
+				image.setPixel(x, y, new Color(0xA0A080));
+								
+				if ((x * y) % 3 == (x + y) % 2) {
+					image.setPixel(x, y, new Color(0xC0C0A0));
+				}
+				
+				if ((x * x + y * y) % 5 == (x * y) % 7) {
+					image.setPixel(x, y, new Color(0x808060));
+				}
+			}
+		}
+		
+		return image;
+	}
+}
