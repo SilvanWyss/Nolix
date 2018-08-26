65893ecd6276f173f4e98a382f77e6114b42ee4e
Silvan Wyss <silvan.wyss@gmail.com>
2018-08-26 20:00:38 +0200

    Created IIntTaker.

---
+++ Source/ch/nolix/core/functionInterfaces/IIntTaker.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.core.functionInterfaces;
+
+//functional interface
+/**
+ * A {@link IIntTaker} has a method that takes an integer.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-08
+ * @lines 10
+ */
+public interface IIntTaker {
+
+	//abstract method
+	/**
+	 * Lets the current {@link IIntTaker} take the given value.
+	 * 
+	 * @param value
+	 */
+	public abstract void run(int value);
+}
