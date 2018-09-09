b557af73c5a990d35c0df5e4baae52230d103a01
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-09 16:35:44 +0200

    Created Valued.

--- Source/ch/nolix/core/skillInterfaces/Valueable.java
+++ Source/ch/nolix/core/skillInterfaces/Valueable.java
@@ -7,46 +7,12 @@ package ch.nolix.core.skillInterfaces;
  * 
  * @author Silvan Wyss
  * @month 2018-09
- * @lines 50
+ * @lines 20
  * @param <VA> The type of a {@link Valueable}.
  * @param <V> The type of the value of a {@link Valueable}.
  */
-public interface Valueable<VA extends Valueable<VA, V>, V> {
-	
-	//abstract method
-	/**
-	 * @return the value of the current {@link Valueable}.
-	 */
-	public abstract V getValue();
-	
-	//default method
-	/**
-	 * @return a string representation of the value
-	 * of the current {@link Valueable}.
-	 */
-	public default String getValueAsString() {
-		return getValue().toString();
-	}
-	
-	//default method
-	/**
-	 * @param value
-	 * @return  true if the current {@link Valueable}
-	 * has a value that equals the given value.
-	 */
-	public default boolean hasEqualValue(final Object value) {
-		return getValue().equals(value);
-	}
-	
-	//default method
-	/**
-	 * @param value
-	 * @return true
-	 * if the current {@link Valueable} has the given value.
-	 */
-	public default boolean hasValue(final Object value) {
-		return (getValue() == value);
-	}
+public interface Valueable<VA extends Valueable<VA, V>, V>
+extends Valued<VA, V> {
 	
 	//abstract method
 	/**
