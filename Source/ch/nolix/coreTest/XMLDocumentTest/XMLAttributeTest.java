dc1aa6c60177b8cb4c7bf03523d4c7049d650d0d
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-09 23:39:09 +0200

    Created XMLAttributeTest.

---
+++ Source/ch/nolix/coreTest/XMLDocumentTest/XMLAttributeTest.java
@@ -0,0 +1,39 @@
+//package declaration
+package ch.nolix.coreTest.XMLDocumentTest;
+
+//own imports
+import ch.nolix.core.XMLDocument.XMLAttribute;
+import ch.nolix.core.constants.StringCatalogue;
+import ch.nolix.primitive.test2.Test;
+
+//test class
+public final class XMLAttributeTest extends Test {
+	
+	//test case
+	public void testCase_constructor() {
+		
+		//execution & verification
+		expect(() -> new XMLAttribute("x", StringCatalogue.EMPTY_STRING));
+	}
+	
+	//test case
+	public void testCase_constructor_2() {
+		
+		//execution
+		final var XMLAttribute = new XMLAttribute("color", "green");
+		
+		//verification
+		expect(XMLAttribute.getName()).isEqualTo("color");
+		expect(XMLAttribute.getValue()).isEqualTo("green");
+	}
+	
+	//test case
+	public void testCase_toString() {
+		
+		//setup
+		final var XMLAttribute = new XMLAttribute("color", "green");
+		
+		//execution & verification
+		expect(XMLAttribute.toString()).isEqualTo("color='green'");
+	}
+}
