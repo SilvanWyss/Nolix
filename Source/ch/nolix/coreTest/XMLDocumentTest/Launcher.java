cfdf56987dfe917b7392de3749c70dea148c58fe
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-09 23:39:30 +0200

    Created XMLDocumentTestPool and Launcher.

---
+++ Source/ch/nolix/coreTest/XMLDocumentTest/Launcher.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.coreTest.XMLDocumentTest;
+
+//launcher class
+public final class Launcher {
+
+	//main method
+	public static void main(String[] args) {
+		new XMLDocumentTestPool().run();
+	}
+	
+	//private constructor
+	private Launcher() {}
+}
---
+++ Source/ch/nolix/coreTest/XMLDocumentTest/XMLDocumentTestPool.java
@@ -0,0 +1,13 @@
+//package declaration
+package ch.nolix.coreTest.XMLDocumentTest;
+
+import ch.nolix.primitive.testoid.TestPool;
+
+//class
+public final class XMLDocumentTestPool extends TestPool {
+
+	//constructor
+	public XMLDocumentTestPool() {
+		addTestClass(XMLAttributeTest.class);
+	}
+}
