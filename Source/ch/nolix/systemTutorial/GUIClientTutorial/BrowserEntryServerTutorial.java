b0bca1b1a6143461d4f347a0096c756ea59d8b1d
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-12 22:48:44 +0200

    Created BrowserEntryServerTutorial.

---
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/BrowserEntryServerTutorial.java
@@ -0,0 +1,32 @@
+//package declaration
+package ch.nolix.systemTutorial.GUIClientTutorial;
+
+//own import
+import ch.nolix.system.GUIClientoid.BrowserEntryServer;
+
+//class
+/**
+ * The {@link BrowserEntryServerTutorial} is a tutorial for a {@link BrowserEntryServer}.
+ * Of the {@link BrowserEntryServerTutorial} no instance can be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-09
+ * @lines 30
+ */
+public final class BrowserEntryServerTutorial {
+	
+	//main method
+	/**
+	 * Creates a browser entry server that is listening to browser clients on the port 8000.
+	 * Open your browser and choose the URL '127.0.01:8000'.
+	 * This URL will connect to the browser entry server.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new BrowserEntryServer(8000);
+	}
+	
+	//private constructor
+	private BrowserEntryServerTutorial() {}
+}
