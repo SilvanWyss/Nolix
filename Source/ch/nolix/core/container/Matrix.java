7830bba0df408efea8d52a72ac510eef075a2b3e
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-16 11:32:02 +0200

    Edited description of Matrix.

--- Source/ch/nolix/core/container/Matrix.java
+++ Source/ch/nolix/core/container/Matrix.java
@@ -11,9 +11,7 @@ import ch.nolix.primitive.validator2.Validator;
 
 //class
 /**
- * A {@link Matrix} is a {@link IContainer}
- * that stores its elements in rows and columns.
- * 
+ * A {@link Matrix} is a {@link IContainer} that stores its elements in rows and columns.
  * A {@link Matrix} is clearable.
  * 
  * @author Silvan Wyss
