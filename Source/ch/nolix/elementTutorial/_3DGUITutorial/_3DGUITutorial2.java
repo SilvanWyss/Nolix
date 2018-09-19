9e2503e2c6cdaa06551cf0d112ad52128c6050c4
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-19 21:13:00 +0200

    Edited _3DGUITutorial2.

--- Source/ch/nolix/elementTutorial/_3DGUITutorial/_3DGUITutorial2.java
+++ Source/ch/nolix/elementTutorial/_3DGUITutorial/_3DGUITutorial2.java
@@ -4,43 +4,49 @@ package ch.nolix.elementTutorial._3DGUITutorial;
 //own imports
 import ch.nolix.element.JMonkeyGUI.JMonkeyMainFrame;
 import ch.nolix.element._3DGUI.Cuboid;
+import ch.nolix.element._3DGUI.Cylinder;
 import ch.nolix.element._3DGUI.MultiShape;
 import ch.nolix.element._3DGUI.Sphere;
 import ch.nolix.element.color.Color;
 
 //class
 /**
- * The 3D GUI tutorial 2 is a tutorial for the 3D GUI class.
+ * The {@link _3DGUITutorial2} is a tutorial for a {@link JMonkeyMainFrame}.
  * 
  * @author Silvan Wyss
  * @month 2017-10
- * @lines 30
+ * @lines 60
  */
 public final class _3DGUITutorial2 {
 
 	//main method
 	/**
-	 * Creates a new main frame and adds a sphere to it as root shape.
+	 * Creates a new {@link JMonkeyMainFrame} and adds shapes to it.
 	 * 
 	 * @param args
 	 */
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
-		final JMonkeyMainFrame mainFrame = new JMonkeyMainFrame();
+		final var mainFrame = new JMonkeyMainFrame("3D GUI Tutorial");
 		
 		mainFrame.setRootShape(
 			new MultiShape(
 				new Cuboid()
-					.setPosition(0.0, -2.0, 0.0)
-					.setXLength(2.0)
-					.setYLength(1.0)
-					.setZLength(0.5)
-					.setColor(Color.BLUE),
+				.setPosition(0.0, -2.0, 0.0)
+				.setXLength(2.0)
+				.setYLength(1.0)
+				.setZLength(0.5)
+				.setColor(Color.BLUE),
 				new Sphere()
-					.setPosition(4.0, -2.0, 0.0)
-					.setDiameter(1.0)
-					.setColor(Color.RED)
+				.setPosition(4.0, -2.0, 0.0)
+				.setDiameter(1.0)
+				.setColor(Color.RED),
+				new Cylinder()
+				.setPosition(4.0, 2.0, 0.0)
+				.setRadius(2.0)
+				.setHeight(4.0)
+				.setColor(Color.GREEN)
 			)
 		);
 		
@@ -49,7 +55,7 @@ public final class _3DGUITutorial2 {
 	
 	//private constructor
 	/**
-	 * Avoids that an instance of this class can be created.
+	 * Avoids that an instance of the {@link _3DGUITutorial2} can be created.
 	 */
 	private _3DGUITutorial2() {}
 }
