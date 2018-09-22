3d69d9fb76bfbd54343d7c02c8608ac1fb523b84
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-22 22:43:13 +0200

    Edited StringValidationTutorial.

--- Source/ch/nolix/coreTutorial/validator2Tutorial/StringValidationTutorial.java
+++ Source/ch/nolix/coreTutorial/validator2Tutorial/StringValidationTutorial.java
@@ -1,11 +1,12 @@
 //package declaration
 package ch.nolix.coreTutorial.validator2Tutorial;
 
+//own import
 import ch.nolix.primitive.validator2.Validator;
 
 //class
 /**
- * This class provides a tutorial how the zeta validator validates a string.
+ * The {@link StringValidationTutorial} is a tutorial for the {@link Validator}.
  * 
  * @author Silvan
  * @month 2017-03
@@ -15,20 +16,20 @@ public final class StringValidationTutorial {
 
 	//main method
 	/**
-	 * Lets the zeta validator suppose properties of the string 'Hello World!'.
+	 * Lets the {@link Validator} validate the maximum length of a {@link String}.
 	 */
 	public static void main(String[] args) {
 		
-		//Supposes that the string 'Hello World!' is not null and not empty, what makes that the zeta validator does not complain.
+		//Lets the validator validate that the string 'Hello World!' is not null and not empty.
 		Validator.suppose("Hello World!").isNotEmpty();
 		
-		//Supposes that the string 'Hello World' has the max length 10, what makes that the zeta validator throws an InvalidArgumentException.
-		Validator.suppose("Hello World!").hasMaxLength(10);
+		//Lets the validator validate that he string 'Hello World!' has the max length 20.
+		Validator.suppose("Hello World!").hasMaxLength(12);
 	}
 	
 	//private constructor
 	/**
-	 * Avoids that an instance of this class can be created.
+	 * Avoids that an instance of the {@link Validator} can be created.
 	 */
 	private StringValidationTutorial() {}
 }
