f2c128ddf08dcdc662f14c77d2b73476a21d4f43
Silvan Wyss <silvan.wyss@gmail.com>
2018-09-23 23:07:03 +0200

    Edited BaseShape.

--- Source/ch/nolix/element/_3DGUI/BaseShape.java
+++ Source/ch/nolix/element/_3DGUI/BaseShape.java
@@ -4,24 +4,21 @@ package ch.nolix.element._3DGUI;
 //own imports
 import ch.nolix.core.container.ReadContainer;
 import ch.nolix.core.constants.PascalCaseNameCatalogue;
-import ch.nolix.core.container.List;
 import ch.nolix.core.entity.MutableOptionalProperty;
 import ch.nolix.core.entity.MutableProperty;
-import ch.nolix.core.specification.StandardSpecification;
 import ch.nolix.element.color.Color;
 import ch.nolix.element.image.Image;
 
 //abstract class
 /**
- * A base shape contains no other shapes.
+ * A {@link BaseShape} is a {@link Shape} that does not contain other shapes.
  * 
  * @author Silvan Wyss
  * @month 2017-11
- * @lines 150
- * @param <BS> The type of a base shape.
+ * @lines 130
+ * @param <BS> The type of a {@link BaseShape}.
  */
-public abstract class BaseShape<BS extends BaseShape<BS>>
-extends Shape<BS> {
+public abstract class BaseShape<BS extends BaseShape<BS>> extends Shape<BS> {
 
 	//default value
 	public static final Color DEFAULT_COLOR = Color.WHITE;
@@ -46,21 +43,7 @@ extends Shape<BS> {
 	
 	//method
 	/**
-	 * @return the attributes of this base shape.
-	 */
-	public List<StandardSpecification> getAttributes() {
-		
-		//Calls method of the base class.
-		final List<StandardSpecification> attributes = super.getAttributes();
-		
-		attributes.addAtEnd(getDefaultColor().getSpecification());
-		
-		return attributes;
-	}
-	
-	//method
-	/**
-	 * @return the default color of this base shape.
+	 * @return the default color of the current {@link BaseShape}.
 	 */
 	public final Color getDefaultColor() {
 		return defaultColor.getValue();
@@ -68,8 +51,8 @@ extends Shape<BS> {
 	
 	//method
 	/**
-	 * @return the default texture of this base shape.
-	 * @throws UnexistingAttribute if this base shape has no default texture.
+	 * @return the default texture of the current {@link BaseShape}.
+	 * @throws UnexistingAttribute if the current {@link BaseShape} has no default texture.
 	 */
 	public final Image getDefaultTexture() {
 		return defaultTexture.getValue();
@@ -77,15 +60,15 @@ extends Shape<BS> {
 	
 	//method
 	/**
-	 * @return the shapes of this base shape.
+	 * @return the shapes of the current {@link BaseShape}.
 	 */
 	public final ReadContainer<Shape<?>> getRefShapes() {
-		return new ReadContainer<>();
+		return new ReadContainer<Shape<?>>();
 	}
 	
 	//method
 	/**
-	 * @return true of this base shape has a default texture.
+	 * @return true of the current {@link BaseShape} has a default texture.
 	 */
 	public final boolean hasDefaultTexture() {
 		return defaultTexture.containsAny();
@@ -93,9 +76,9 @@ extends Shape<BS> {
 	
 	//method
 	/**
-	 * Removes the default texture of this base shape.
+	 * Removes the default texture of the current {@link BaseShape}.
 	 * 
-	 * @return this base shape.
+	 * @return the current {@link BaseShape}.
 	 */
 	public final BS removeDefaultTexture() {
 		
@@ -106,11 +89,9 @@ extends Shape<BS> {
 	
 	//method
 	/**
-	 * Resets the configuration of this base shape.
-	 * 
-	 * @return this base shape.
+	 * {@inheritDoc}
 	 */
-	public final BS resetConfiguration() {
+	public BS resetConfiguration() {
 		
 		setDefaultColor(DEFAULT_COLOR);
 		removeDefaultTexture();
@@ -120,25 +101,25 @@ extends Shape<BS> {
 
 	//method
 	/**
-	 * Sets the default color of this base shape.
+	 * Sets the default color of the current {@link BaseShape}.
 	 * 
-	 * @param color
-	 * @return this base shape.
-	 * @throws NullArgumentException if the given color is not an instance.
+	 * @param defaultColor
+	 * @return the current {@link BaseShape}.
+	 * @throws NullArgumentException if the given default color is not an instance.
 	 */
-	public final BS setDefaultColor(final Color color) {
+	public final BS setDefaultColor(final Color defaultColor) {
 	
-		this.defaultColor.setValue(color);
+		this.defaultColor.setValue(defaultColor);
 		
 		return getInstance();
 	}
 	
 	//method
 	/**
-	 * Sets the default texture of this base shape.
+	 * Sets the default texture of the current {@link BaseShape}.
 	 * 
 	 * @param defaultTexture
-	 * @return this base shape.
+	 * @return the current {@link BaseShape}.
 	 * @throws NullArgumentException if the given default texture is not an instance.
 	 */
 	public final BS setDefaultTexture(final Image defaultTexture) {
