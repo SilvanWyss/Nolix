d75c5b31fde6f793a09332f898e41066db6212a5
Silvan Wyss <silvan.wyss@gmail.com>
2018-10-13 18:17:17 +0200

    Edited IGenericReplier.

--- Source/ch/nolix/core/communicationAPI/IGenericReplier.java
+++ Source/ch/nolix/core/communicationAPI/IGenericReplier.java
@@ -3,19 +3,20 @@ package ch.nolix.core.communicationAPI;
 
 //interface
 /**
- * A generic replier can receive messages of a certain type and return replies of a certain type to them.
+ * A {@link IGenericReplier} can receive messages of a certain type
+ * and can return replies of a certain type to the messages.
  * 
  * @author Silvan Wyss
  * @month 2015-12
  * @lines 20
- * @param <M> - The type of the messages a generic replier can reply to.
- * @param <R> - The type of the replies a generic replier can reply.
+ * @param <M> The type of the messages a {@link IGenericReplier} can reply to.
+ * @param <R> The type of the replies a {@link IGenericReplier} can return.
  */
 public interface  IGenericReplier<M, R> {
 
 	//abstract method
 	/**
-	 * Lets this replier receive the given message.
+	 * Lets the current {@link IGenericReplier} receive the given message.
 	 * 
 	 * @param message
 	 * @return the reply to the given message.
