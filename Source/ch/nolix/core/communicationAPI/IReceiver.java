f194f5f38a1ebe48bf0f7122be4afc1638c352b8
Silvan Wyss <silvan.wyss@gmail.com>
2018-10-13 22:20:22 +0200

    Edited IReceiver.

--- Source/ch/nolix/core/communicationAPI/IReceiver.java
+++ Source/ch/nolix/core/communicationAPI/IReceiver.java
@@ -3,7 +3,7 @@ package ch.nolix.core.communicationAPI;
 
 //interface
 /**
- * A receiver can receive messages.
+ * A {@link IReceiver} can receive messages.
  * 
  * @author Silvan Wyss
  * @month 2015-12
@@ -13,7 +13,7 @@ public interface IReceiver {
 
 	//abstract method
 	/**
-	 * Lets this receiver receive the given message.
+	 * Lets the current {@link IReceiver} receive the given message.
 	 * 
 	 * @param message
 	 */
