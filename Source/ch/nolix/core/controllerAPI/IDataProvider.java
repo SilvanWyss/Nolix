8d80b46c4a857cec6b05fbc1dee1287c1d2ab04d
Silvan Wyss <silvan.wyss@gmail.com>
2018-10-18 22:09:36 +0200

    Edited IDataProvider.

--- Source/ch/nolix/core/controllerAPI/IDataProvider.java
+++ Source/ch/nolix/core/controllerAPI/IDataProvider.java
@@ -1,6 +1,7 @@
 //package declaration
 package ch.nolix.core.controllerAPI;
 
+//own imports
 import ch.nolix.core.documentNode.DocumentNode;
 import ch.nolix.core.documentNode.Statement;
 
@@ -11,18 +12,18 @@ import ch.nolix.core.documentNode.Statement;
  * @lines 30
  */
 public interface IDataProvider {
-
+	
 	//abstract method
 	/**
 	 * @param request
-	 * @return the data the given request requests from this data controller.
+	 * @return the data the given request requests from the current {@link IDataProvider}.
 	 */
 	public abstract DocumentNode getData(Statement request);
 	
 	//default method
 	/**
 	 * @param request
-	 * @return the data the given request requests from this data controller.
+	 * @return the data the given request requests from the current {@link IDataProvider}.
 	 */
 	public default DocumentNode getData(final String request) {
 		return getData(new Statement(request));
