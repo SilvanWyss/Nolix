6ccd5a0f240113482aa7c6cc83531e07b82cfc1d
Silvan Wyss <silvan.wyss@gmail.com>
2018-10-20 18:47:35 +0200

    Made IGUI castable.

--- Source/ch/nolix/element/GUI/IGUI.java
+++ Source/ch/nolix/element/GUI/IGUI.java
@@ -3,6 +3,7 @@ package ch.nolix.element.GUI;
 
 //own imports
 import ch.nolix.core.container.IContainer;
+import ch.nolix.core.skillInterfaces.Castable;
 import ch.nolix.core.skillInterfaces.Clearable;
 import ch.nolix.core.skillInterfaces.Closable;
 import ch.nolix.core.skillInterfaces.IRequestableContainer;
@@ -12,6 +13,7 @@ import ch.nolix.core.specificationAPI.Specifiable;
 //interface
 public interface IGUI<G extends IGUI<G>>
 extends
+Castable,
 Clearable<G>,
 Closable,
 IRequestableContainer,
