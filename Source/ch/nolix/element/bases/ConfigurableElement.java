bb1d7243c07e36aeb33a644c51927f43b1322160
Silvan Wyss <silvan.wyss@gmail.com>
2018-10-28 23:13:11 +0100

    Refactored ConfigurableElement.

--- Source/ch/nolix/element/bases/ConfigurableElement.java
+++ Source/ch/nolix/element/bases/ConfigurableElement.java
@@ -1,10 +1,3 @@
-/*
- * file:	ClassableNamableElement.java
- * author:	Silvan Wyss
- * month:	2015
- * lines:	130
- */
-
 //package declaration
 package ch.nolix.element.bases;
 
@@ -17,96 +10,33 @@ import ch.nolix.core.documentNode.DocumentNodeoid;
 import ch.nolix.core.invalidStateException.UnexistingAttributeException;
 import ch.nolix.core.skillInterfaces.OptionalTokenable;
 import ch.nolix.core.specificationAPI.Configurable;
-import ch.nolix.element.core.NonEmptyText;
+import ch.nolix.core.validator2.Validator;
 
-//class
+//abstract class
 /**
- * A classable namable element is a namable element that can have a class.
+ * A {@link ConfigurableElement} is a {@link OptionalNamableElement}
+ * that is configurable.
+ * 
+ * @author Silvan Wyss
+ * @month 2015-12
+ * @lines 160
  */
 public abstract class ConfigurableElement<CE extends ConfigurableElement<CE>>
 extends OptionalNamableElement<CE>
 implements Configurable<CE>, OptionalTokenable<CE> {	
 	
 	//optional attribute
-	private NonEmptyText token;
+	private String token;
 	
 	//method
 	/**
-	 * @return the attributes of this specifiable object
-	 */
-	public List<DocumentNode> getAttributes() {
-		
-		//Calls method of the base class.
-		List<DocumentNode> attributes = super.getAttributes();
-		
-		if (hasToken()) {
-			attributes.addAtEnd(token.getSpecificationAs(PascalCaseNameCatalogue.TOKEN));
-		}
-		return attributes;
-	}
-	
-	//method
-	/**
-	 * @return the class of this configurable element
-	 * @throws UnexistingAttribute if this configurable element has no class
-	 */
-	public final String getToken() {
-		if (!hasToken()) {
-			throw new UnexistingAttributeException(getType(), VariableNameCatalogue.TOKEN);
-		}
-		return token.getValue();
-	}
-	
-	//method
-	/**
-	 * @return true if this configurable element has a class
-	 */
-	public final boolean hasToken() {
-		return (token != null);
-	}
-	
-	//method
-	/**
-	 * @param class0
-	 * @return true if this configurable element has the given class
-	 */
-	public final boolean hasToken(String class0) {
-		if (hasToken()) {
-			return this.token.hasValue(class0);
-		}
-		return false;
-	}
-	
-	//method
-	/**
-	 * @return true if this configurable element has the given type or super type.
-	 */
-	public final boolean hasTypeOrSuperType(final String type) {
-		
-		//Calls method of the base class.
-		return super.hasTypeOrSuperType(type);
-	}
-	
-	//method
-	/**
-	 * Removes the class of this configurable element.
-	 */
-	public final CE removeToken() {
-		
-		token = null;
-		
-		return getInstance();
-	}
-	
-	//method
-	/**
-	 * Sets the given attribute.
-	 * @param attribute
-	 * @throws Exception if the given attribute is not valid
+	 * {@inheritDoc}
 	 */
 	public void addOrChangeAttribute(final DocumentNodeoid attribute) {
+		
+		//Enumerates the header of the given attribute.
 		switch (attribute.getHeader()) {
-			case "Token":
+			case VariableNameCatalogue.TOKEN:
 				setToken(attribute.getOneAttributeAsString());
 				break;
 			default:
@@ -118,7 +48,88 @@ implements Configurable<CE>, OptionalTokenable<CE> {
 	
 	//method
 	/**
-	 * Sets the token of this configurable element.
+	 * {@inheritDoc}
+	 */
+	public List<DocumentNode> getAttributes() {
+		
+		//Calls method of the base class.
+		final var attributes = super.getAttributes();
+		
+		//Handles the case that the current configurbale element has token.
+		if (hasToken()) {
+			attributes.addAtEnd(
+				new DocumentNode(PascalCaseNameCatalogue.TOKEN, getToken())
+			);
+		}
+		return attributes;
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public final String getToken() {
+		
+		//Checks if the current configurable element has a token.
+		supposeHasToken();
+		
+		return token;
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public final boolean hasToken() {
+		return (token != null);
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public final boolean hasToken(String token) {		
+		return OptionalTokenable.super.hasToken(token);
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public final boolean hasTypeOrSuperType(final String type) {
+		
+		//Calls method of the base class.
+		return super.hasTypeOrSuperType(type);
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public final CE removeToken() {
+		
+		token = null;
+		
+		return getInstance();
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	public CE reset() {
+		
+		//Calls method of the base class.
+		super.reset();
+		
+		resetConfiguration();
+		
+		return getInstance();
+	}
+	
+	//method
+	/**
+	 * Sets the token of the current {@link ConfigurableElement}.
 	 * 
 	 * @param token
 	 * @throws NullArgumentException if the given token is null.
@@ -126,8 +137,28 @@ implements Configurable<CE>, OptionalTokenable<CE> {
 	 */
 	public final CE setToken(final String token) {
 		
-		this.token = new NonEmptyText(token);
+		//Checks if the given token is not null and not empty.
+		Validator
+		.suppose(token)
+		.thatIsNamed(VariableNameCatalogue.TOKEN)
+		.isNotEmpty();
+		
+		//Sets the token of the current configurable element.
+		this.token = token;
 		
 		return getInstance();
+	}
+	
+	//method
+	/**
+	 * @throws UnexistingAttributeException
+	 * if the current {@link ConfigurableElement} does not have a token.
+	 */
+	private void supposeHasToken() {
+		
+		//Checks if the current configurable element has a token.
+		if (!hasToken()) {
+			throw new UnexistingAttributeException(this, VariableNameCatalogue.TOKEN);
+		}
 	}
 }
