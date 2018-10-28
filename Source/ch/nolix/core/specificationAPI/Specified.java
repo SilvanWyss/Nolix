4135573487dedfed81521e43ebae62ca9e992621
Silvan Wyss <silvan.wyss@gmail.com>
2018-10-28 21:25:07 +0100

    Edited Specified.

--- Source/ch/nolix/core/specificationAPI/Specified.java
+++ Source/ch/nolix/core/specificationAPI/Specified.java
@@ -5,27 +5,29 @@ package ch.nolix.core.specificationAPI;
 import ch.nolix.core.XMLDocument.XMLNode;
 import ch.nolix.core.container.List;
 import ch.nolix.core.documentNode.DocumentNode;
+import ch.nolix.core.invalidArgumentException.InvalidArgumentException;
 import ch.nolix.core.skillInterfaces.Typed;
 
 //interface
 /**
- * A specified object is a typed object whose official attributes can be got together.
+ * A {@link Specified} is a {@link Typed}
+ * whose official attributes can be requested together.
  * 
  * @author Silvan Wyss
  * @month 2015-12
- * @lines 90
+ * @lines 130
  */
 public interface Specified extends Typed {
-
+	
 	//abstract method
 	/**
-	 * @return the attributes of this specified object.
+	 * @return the attributes of the current {@link Specified}.
 	 */
 	public abstract List<DocumentNode> getAttributes();
 	
 	//default method
 	/**
-	 * @return the specification of this specified object.
+	 * @return the specification of the current {@link Specified}.
 	 */
 	public default DocumentNode getSpecification() {
 		return new DocumentNode(getType(), getAttributes());
@@ -34,7 +36,7 @@ public interface Specified extends Typed {
 	//default method
 	/**
 	 * @param type
-	 * @return the specification of this specified as the given type.
+	 * @return the specification of the current specified as the given type.
 	 * @throws NullArgumentException if the given type is not an instance.
 	 * @throws EmptyArgumentException if the given type is empty.
 	 */
@@ -44,7 +46,7 @@ public interface Specified extends Typed {
 	
 	//default method
 	/**
-	 * @return the specification of this specified object without header.
+	 * @return the specification of the current {@link Specified} without header.
 	 */
 	public default DocumentNode getSpecificationWithoutHeader() {
 		return new DocumentNode(getAttributes());
@@ -52,24 +54,68 @@ public interface Specified extends Typed {
 	
 	//default method
 	/**
-	 * Saves this specified object to the file or directory with the given path.
+	 * Saves the current {@link Specified} as the given type
+	 * to the file with the given file path.
 	 * 
-	 * @param path
-	 * @throws Exception if an error occurs
+	 * @param type
+	 * @param filePath
+	 * @throws NullArgumentException if the given type is null.
+	 * @throws EmptyArgumentException if the given type is empty.
+	 * @throws InvalidArgumentException
+	 * if a file system item with the given file path exists already.
 	 */
-	public default void saveTo(final String path) {
-		getSpecification().saveToFile(path);
+	public default void saveAsTo(final String type, final String filePath) {
+		
+		//Calls other method.
+		saveAsTo(type, filePath, false);
 	}
 	
 	//default method
 	/**
-	 * Saves this specified object as the given type to the file or directory with the given path.
+	 * Saves the current {@link Specified} as the given type
+	 * to the file with the given path.
 	 * 
 	 * @param type
-	 * @param path
+	 * @param filePath
+	 * @param overwrite
+	 * @throws NullArgumentException if the given type is null.
+	 * @throws EmptyArgumentException if the given type is empty.
+	 * @throws InvalidArgumentException if the given overwrite flag is false
+	 * and a file system item with the given file path exists already.
 	 */
-	public default void saveAsTo(final String type, final String path) {
-		getSpecificationAs(type).saveToFile(path);
+	public default void saveAsTo(
+		final String type,
+		final String filePath,
+		final boolean overwrite
+	) {
+		getSpecificationAs(type).saveToFile(filePath, overwrite);
+	}
+	
+	//default method
+	/**
+	 * Saves the current {@link Specified} to the file with the given file path.
+	 * 
+	 * @param filePath
+	 * @throws InvalidArgumentException
+	 * if a file system item with the given file path exists already.
+	 */
+	public default void saveTo(final String filePath) {
+		
+		//Calls other method.
+		saveTo(filePath, false);
+	}
+	
+	//default method
+	/**
+	 * Saves the current {@link Specified} to the file with the given file path.
+	 * 
+	 * @param filePath
+	 * @param overwrite
+	 * @throws InvalidArgumentException if the given overwrite flag is false
+	 * and a file system item with the given file path exists already.
+	 */
+	public default void saveTo(final String filePath, final boolean overwrite) {
+		getSpecification().saveToFile(filePath, overwrite);
 	}
 	
 	//default method
@@ -84,6 +130,8 @@ public interface Specified extends Typed {
 	/**
 	 * @return a XML representation of the current {@link Specified} as the given type.
 	 * @param type
+	 * @throws NullArgumentException if the given type is null.
+	 * @throws EmptyArgumentException if the given type is empty.
 	 */
 	public default XMLNode toXMLAs(final String type) {
 		return getSpecificationAs(type).toXML();
