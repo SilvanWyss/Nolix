94b1f155c6f670a32784242ee680ee95da5c02b7
Silvan Wyss <silvan.wyss@gmail.com>
2018-11-10 17:06:13 +0100

    Created Identified2.

---
+++ Source/ch/nolix/core/skillInterfaces/Identified2.java
@@ -0,0 +1,38 @@
+//package declaration
+package ch.nolix.core.skillInterfaces;
+
+//interface
+/**
+ * A {@link Identified2} has an id.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-11
+ * @lines 30
+ */
+public interface Identified2 {
+	
+	//abstract method
+	/**
+	 * @return the id of the current {@link Identified2}.
+	 */
+	public abstract long getId();
+	
+	//default method
+	/**
+	 * @param object
+	 * @return true if the current {@link Identified}
+	 * has the same id as the given object.
+	 */
+	public default boolean hasId(final Identified2 object) {
+		return hasId(object.getId());
+	}
+	
+	//default method
+	/**
+	 * @return true if the current {@link Identified2}
+	 * has the given id.
+	 */
+	public default boolean hasId(final long id) {
+		return (getId() == id);
+	}
+}
