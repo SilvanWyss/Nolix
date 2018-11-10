98ad64c3adf03a48090f0c2c652c537b0d04ec70
Silvan Wyss <silvan.wyss@gmail.com>
2018-11-10 17:05:44 +0100

    Edited Identified.

--- Source/ch/nolix/core/skillInterfaces/Identified.java
+++ Source/ch/nolix/core/skillInterfaces/Identified.java
@@ -19,20 +19,20 @@ public interface Identified {
 	
 	//default method
 	/**
-	 * @return true if the current {@link Identified}
-	 * has the given id.
-	 */
-	public default boolean hasId(final long id) {
-		return (getId() == id);
-	}
-	
-	//default method
-	/**
 	 * @param object
 	 * @return true if the current {@link Identified}
 	 * has the same id as the given object.
 	 */
 	public default boolean hasId(final Identified object) {
 		return hasId(object.getId());
+	}
+	
+	//default method
+	/**
+	 * @return true if the current {@link Identified}
+	 * has the given id.
+	 */
+	public default boolean hasId(final long id) {
+		return (getId() == id);
 	}
 }
