d2cf14feea739cca30d2ddffc3af9b55106ee373
Silvan Wyss <silvan.wyss@gmail.com>
2018-11-25 21:59:32 +0100

    Created SafetyAwareness enum.

---
+++ Source/ch/nolix/core/safety/SafetyAwareness.java
@@ -0,0 +1,13 @@
+//package declaration
+package ch.nolix.core.safety;
+
+//enum
+/**
+ * @author Silvan Wyss
+ * @month 2018-11
+ * @lines 10
+ */
+public enum SafetyAwareness {
+	I_DO_NOT_KNOW_WHAT_I_DO,
+	I_KNOW_WHAT_I_DO
+}
