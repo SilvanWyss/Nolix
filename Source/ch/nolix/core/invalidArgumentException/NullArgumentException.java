a0010d5cc72374de1919bffbf6e906e3ea5642f4
Silvan Wyss <silvan.wyss@gmail.com>
2018-12-09 12:14:13 +0100

    Extended NullArgumentException.

--- Source/ch/nolix/core/invalidArgumentException/NullArgumentException.java
+++ Source/ch/nolix/core/invalidArgumentException/NullArgumentException.java
@@ -3,26 +3,55 @@ package ch.nolix.core.invalidArgumentException;
 
 //class
 /**
- * A null argument exception is an exception that is intended to be thrown when an argument is undesired null.
+ * A {@link NullArgumentException} is a {@link InvalidArgumentException}
+ * that is intended to be thrown when an argument is undesired null.
  * 
  * @author Silvan Wyss
  * @month 2016-04
- * @lines 40
+ * @lines 70
  */
 @SuppressWarnings("serial")
 public final class NullArgumentException extends InvalidArgumentException {
-
+	
 	//constant
 	private static final String ERROR_PREDICATE = "is null";
 	
+	//static method
+	/**
+	 * @return a safe argument name for the given argument type.
+	 * @throws RuntimeException if the given argument type is null.
+	 */
+	private static String createSafeArgumentName(final Class<?> argumentType) {
+		
+		//Checks if the given argument type is not null.
+		if (argumentType == null) {
+			throw new RuntimeException("The given argument type is null.");
+		}
+		
+		return argumentType.getSimpleName();
+	}
+	
 	//constructor
 	/**
-	 * Creates a new null argument exception.
+	 * Creates a new {@link NullArgumentException}.
 	 */
 	public NullArgumentException() {
 		
 		//Calls constructor of the base class.
 		super(null, ERROR_PREDICATE);
+	}
+	
+	//constructor
+	/**
+	 * Creates a new {@link NullArgumentException} for an argument of the given argument type.
+	 * 
+	 * @param argumentType
+	 * @throws RuntimeException if the given argument type is null.
+	 */
+	public NullArgumentException(final Class<?> argumentType) {
+		
+		//Calls constructor of the base class.
+		super(createSafeArgumentName(argumentType), null, ERROR_PREDICATE);
 	}
 	
 	//constructor
@@ -36,6 +65,6 @@ public final class NullArgumentException extends InvalidArgumentException {
 	public NullArgumentException(final String argumentName) {
 		
 		//Calls constructor of the base class.
-		super(null, ERROR_PREDICATE);
+		super(argumentName, null, ERROR_PREDICATE);
 	}
 }
