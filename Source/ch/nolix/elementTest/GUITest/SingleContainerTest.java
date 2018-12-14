7f664e352cabccca26c79bbbac6f806f3c1201b8
Silvan Wyss <silvan.wyss@gmail.com>
2018-12-14 20:49:41 +0100

    Refactored SingleContainerTest.

--- Source/ch/nolix/elementTest/GUITest/SingleContainerTest.java
+++ Source/ch/nolix/elementTest/GUITest/SingleContainerTest.java
@@ -1,23 +1,52 @@
-/*
- * file:	SingleContainerTest.java
- * author:	Silvan Wyss
- * month:	2015
- * lines:	20
- */
-
 //package declaration
 package ch.nolix.elementTest.GUITest;
 
-import ch.nolix.core.test2.Test;
+//own imports
+import ch.nolix.element.GUI.Label;
+import ch.nolix.element.GUI.SingleContainer;
 
 //test class
 /**
- * This class is a test class for the single container class.
+ * The {@link SingleContainerTest} is a test for {@link SingleContainer}.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-12
+ * @lines 50
  */
-public class SingleContainerTest extends Test {
-
+public class SingleContainerTest extends BorderWidgetTest<SingleContainer> {
+	
 	//test case
-	public void testGetTypes() {
-		//expectEquality("SingleContainer,Container,BorderableRectangle,Rectangle,ConfigurableElement,OptionalNamableElement,Element", new SingleContainer().getTypes().toString());
+	public void testCase_reset() {
+		
+		//setup
+		final var singleContainer = new SingleContainer(new Label("Test"));
+		
+		//setup verification
+		expect(singleContainer.containsAny());
+		
+		//execution
+		singleContainer.reset();
+		
+		//verification
+		expect(singleContainer.isEmpty());
+	}
+	
+	//test case
+	public void testCase_setWidget() {
+		
+		//setup
+		final var singleContainer = new SingleContainer();
+		final var label = new Label("Test");
+		
+		//execution
+		singleContainer.setWidget(label);
+		
+		//verification
+		expect(singleContainer.getRefWidget()).isSameAs(label);
+	}
+	
+	@Override
+	protected SingleContainer createTestObject() {
+		return new SingleContainer();
 	}
 }
