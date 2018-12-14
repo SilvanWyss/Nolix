90613c857cb07668bd28d9f7d6d532b576dc03e4
Silvan Wyss <silvan.wyss@gmail.com>
2018-12-14 18:30:36 +0100

    Refactored CheckboxTest.

--- Source/ch/nolix/elementTest/GUITest/CheckboxTest.java
+++ Source/ch/nolix/elementTest/GUITest/CheckboxTest.java
@@ -1,20 +1,20 @@
 //package declaration
 package ch.nolix.elementTest.GUITest;
 
-import ch.nolix.core.test2.Test;
 //own imports
+import ch.nolix.core.test2.Test;
 import ch.nolix.element.GUI.Checkbox;
 
 //test class
 /**
- * A {@link CheckboxTest} is a test for a {@link Checkbox}.
+ * A {@link CheckboxTest} is a test for {@link Checkbox}.
  * 
  * @author Silvan Wyss
  * @month 2017-03
  * @lines 50
  */
 public final class CheckboxTest extends Test {
-
+	
 	//test case
 	public void testCase_constructor() {
 		
@@ -31,6 +31,9 @@ public final class CheckboxTest extends Test {
 		//setup
 		final var checkBox = new Checkbox();
 		
+		//setup verification
+		expectNot(checkBox.isChecked());
+		
 		//execution
 		checkBox.check();
 		
@@ -42,8 +45,10 @@ public final class CheckboxTest extends Test {
 	public void testCase_uncheck() {
 		
 		//setup
-		final var checkBox = new Checkbox();
-		checkBox.check();
+		final var checkBox = new Checkbox(true);
+		
+		//setup verification
+		expect(checkBox.isChecked());
 		
 		//execution		
 		checkBox.uncheck();
