2b5878c1eb484fed0f6ee4475972e1956fd04ae3
Silvan Wyss <silvan.wyss@gmail.com>
2018-12-16 19:41:54 +0100

    Created FileExtensionCatalogue.

---
+++ Source/ch/nolix/core/constants/FileExtensionCatalogue.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.core.constants;
+
+//class
+/**
+ * Of the {@link FileExtensionCatalogue} cannot be created an instance.
+ * 
+ * @author Silvan Wyss
+ * @month 2018-12
+ * @lines 30
+ */
+public final class FileExtensionCatalogue {
+	
+	//constants
+	public static final String CSS = "css";
+	public static final String JAVA = "java";
+	public static final String JAVA_CLASS = "class";
+	public static final String JPEG = "jpeg";
+	public static final String JS = "js";
+	public static final String PDF = "pdf";
+	public static final String PNG = "png";
+	public static final String TS = "ts";
+	public static final String TXT = "txt";
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of the {@link FileExtensionCatalogue} can be created.
+	 */
+	private FileExtensionCatalogue() {}
+}
