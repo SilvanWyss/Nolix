41ae3d78e9a38ba4eec0f5ade387dca04dadf736
Silvan Wyss <silvan.wyss@gmail.com>
2019-01-26 21:41:37 +0100

    Created ClosedArgumentException.

---
+++ Source/ch/nolix/core/invalidArgumentException/ClosedArgumentException.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.core.invalidArgumentException;
+
+//class
+/**
+ * A {@link ClosedArgumentException} is a {@link InvalidArgumentException}
+ * that is intended to be thrown when an argument is undesirably closed.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-01
+ * @lines 30
+ */
+@SuppressWarnings("serial")
+public final class ClosedArgumentException extends InvalidArgumentException {
+	
+	//constant
+	private static final String ERROR_PREDICATE = "is closed";
+	
+	//constructor
+	/**
+	 * Creates a new {@link ClosedArgumentException} for the given argument.
+	 * 
+	 * @param argument
+	 */
+	public ClosedArgumentException(final AutoCloseable argument) {
+		
+		//Calls constructor of the base class.
+		super(argument, ERROR_PREDICATE);
+	}
+}
