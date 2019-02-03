4d44e2f72be55d4c4e3f13f6f5e1c3d8718546de
Silvan Wyss <silvan.wyss@gmail.com>
2019-02-03 23:14:28 +0100

    Created SourceNeuronTest.

---
+++ Source/ch/nolix/systemTest/neuronTest/SourceNeuronTest.java
@@ -0,0 +1,44 @@
+//package declaration
+package ch.nolix.systemTest.neuronTest;
+
+//own imports
+import ch.nolix.core.invalidArgumentException.NullArgumentException;
+import ch.nolix.core.test2.Test;
+import ch.nolix.system.neuronoid.SourceNeuron;
+
+//test class
+public final class SourceNeuronTest extends Test {
+	
+	//test case
+	public void testCase_constructor() {
+		
+		//setup
+		final var output = "test output";
+		
+		//execution
+		final var sourceNeuron = new SourceNeuron<String>(output);
+		
+		//verification
+		expect(sourceNeuron.getRefOutput()).isEqualTo(output);
+	}
+	
+	//test case
+	public void testCase_constructor_whenTheGivenOutputIsNull() {
+		
+		//execution & verification
+		expect(() -> new SourceNeuron<String>(null))
+		.throwsException()
+		.ofType(NullArgumentException.class)
+		.withMessage("The given output is null.");
+	}
+	
+	//test case
+	public void testCase_getMaxInputNeuronCount() {
+		
+		//setup
+		final var sourceNeuron = new SourceNeuron<String>("test output");
+		
+		//execution & verification
+		expect(sourceNeuron.getMaxInputNeuronCount()).isZero();
+	}
+}
