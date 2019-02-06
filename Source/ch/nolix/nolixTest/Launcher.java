5b607c69b5719c39b66e572f8a23e0c0c23a1443
Silvan Wyss <silvan.wyss@gmail.com>
2019-02-06 23:26:52 +0100

    Edited Launcher.

--- Source/ch/nolix/nolixTest/Launcher.java
+++ Source/ch/nolix/nolixTest/Launcher.java
@@ -3,8 +3,8 @@ package ch.nolix.nolixTest;
 
 //class
 /**
- * This class provides a main method to run a Nolix test pool.
- * Of this class an instance cannot be created.
+ * The {@link Launcher} provides a main method to run a {@link NolixTestPool}.
+ * Of the {@link Launcher} an instance cannot be created.
  * 
  * @author Silvan Wyss
  * @month 2017-11
@@ -14,17 +14,17 @@ public final class Launcher {
 	
 	//main method
 	/**
-	 * Creates a new Nolix test pool and runs it.
+	 * Creates a new {@link NolixTestPool} and runs it.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		new NolixTestPool().run();
 	}
-
+	
 	//private constructor
 	/**
-	 * Avoids that an instance of this class can be created.
+	 * Avoids that an instance of the {@link NolixTestPool} can be created.
 	 */
 	private Launcher() {}
 }
