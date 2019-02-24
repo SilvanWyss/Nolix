861362f29f35469b6731d53f1171e9bd78f719b3
Silvan Wyss <silvan.wyss@gmail.com>
2019-02-24 19:42:39 +0100

    Renamed EnvironmentVariableCatalogue to WindowsEnvironmentVariableCatalogue.

---
+++ Source/ch/nolix/core/constants/WindowsEnvironmentVariableCatalogue.java
@@ -0,0 +1,23 @@
+//package declaration
+package ch.nolix.core.constants;
+
+//class
+/**
+ * The {@link WindowsEnvironmentVariableCatalogue} provides environment variables for Microsoft Windows.
+ * Of the {@link WindowsEnvironmentVariableCatalogue} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2016-09
+ * @lines 20
+ */
+public final class WindowsEnvironmentVariableCatalogue {
+	
+	//constant
+	public static final String APP_DATA = "APPDATA";
+	
+	//private constructor
+	/**
+	 * Avoids that an instance of the {@link WindowsEnvironmentVariableCatalogue} can be created.
+	 */
+	private WindowsEnvironmentVariableCatalogue() {}
+}
