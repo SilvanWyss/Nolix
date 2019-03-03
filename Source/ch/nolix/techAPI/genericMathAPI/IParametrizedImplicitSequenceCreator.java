f36a9554e731e7f6512f5f379d2a26b9f50d42ee
Silvan Wyss <silvan.wyss@gmail.com>
2019-03-03 10:34:23 +0100

    Edited IParametrizedImplicitSequenceCreator interface.

--- Source/ch/nolix/techAPI/genericMathAPI/IParametrizedImplicitSequenceCreator.java
+++ Source/ch/nolix/techAPI/genericMathAPI/IParametrizedImplicitSequenceCreator.java
@@ -1,11 +1,14 @@
 //package declaration
 package ch.nolix.techAPI.genericMathAPI;
 
+//Java import
+import java.util.ArrayList;
+
 //interface
 public interface IParametrizedImplicitSequenceCreator<N> {
 	
 	//abstract method
-	public abstract ISequence<N> createSequence(N startValue);
+	public abstract ISequence<N> createSequence(ArrayList<N> startValues);
 	
 	//abstract method
 	public abstract int getSequencesStartIndex();
