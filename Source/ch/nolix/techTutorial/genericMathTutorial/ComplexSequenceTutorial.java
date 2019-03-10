ed9b2f6c09fa6f3a400bb432d13e4ce36be57d17
Silvan Wyss <silvan.wyss@gmail.com>
2019-03-10 20:24:18 +0100

    Created ComplexSequenceTutorial.

---
+++ Source/ch/nolix/techTutorial/genericMathTutorial/ComplexSequenceTutorial.java
@@ -0,0 +1,43 @@
+package ch.nolix.techTutorial.genericMathTutorial;
+
+import ch.nolix.core.classProvider.ClassProvider;
+import ch.nolix.core.container.List;
+import ch.nolix.core.functionAPI.IIntTaker;
+import ch.nolix.tech.genericMath.GenericMathRegistrator;
+import ch.nolix.techAPI.genericMathAPI.IComplexNumber;
+import ch.nolix.techAPI.genericMathAPI.IComplexNumberFactory;
+import ch.nolix.techAPI.genericMathAPI.IImplicitComplexSequenceBuilder;
+
+public class ComplexSequenceTutorial {
+	
+	public static void main(String[] args) {
+		
+		//Registers an implementation for the GenericMathAPI at the ClassProvider.
+		GenericMathRegistrator.register();
+		
+		final var complexNumberFactory = ClassProvider.create(IComplexNumberFactory.class);
+		final var startValues = new List<IComplexNumber>(complexNumberFactory.create(0.0, 0.0));
+		final var c = complexNumberFactory.create(0.0, 1.0);
+		
+		//Creates a Sequence.
+		final var sequence =
+		ClassProvider
+		.create(IImplicitComplexSequenceBuilder.class)
+		.setStartIndex(1)
+		.setStartValues(startValues)
+		.setNextValueFunction(z -> z.get(0).getSquare().getSum(c))
+		.build();
+		
+		final IIntTaker function = (int i) -> System.out.println("a(" + i + ") = " + sequence.getValue(i));
+		
+		//Prints out to the console some values of the sequence.
+		function.run(1);
+		function.run(2);
+		function.run(3);
+		function.run(4);
+		function.run(5);
+		function.run(10);
+		function.run(100);
+		function.run(1000);
+	}
+}
