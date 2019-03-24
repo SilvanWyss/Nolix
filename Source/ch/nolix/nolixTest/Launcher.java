f5ae9b65e84f4edc2fad4a238f53a0ff99167560
Silvan Wyss <silvan.wyss@gmail.com>
2019-03-24 20:27:37 +0100

    Added measureSizeOfNolixLibrary static method to Launcher.

--- Source/ch/nolix/nolixTest/Launcher.java
+++ Source/ch/nolix/nolixTest/Launcher.java
@@ -1,14 +1,18 @@
 //package declaration
 package ch.nolix.nolixTest;
 
+//own imports
+import ch.nolix.core.fileSystem.FolderAccessor;
+import ch.nolix.core.invalidArgumentException.UninstantiableClassException;
+import ch.nolix.core.math.Calculator;
+
 //class
 /**
- * The {@link Launcher} provides a main method to run a {@link NolixTestPool}.
  * Of the {@link Launcher} an instance cannot be created.
  * 
  * @author Silvan Wyss
  * @month 2017-11
- * @lines 30
+ * @lines 80
  */
 public final class Launcher {
 	
@@ -19,12 +23,58 @@ public final class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		System.out.println("--- Nolix ---");
+		System.out.println();
+		
+		measureSizeOfNolixLibrary();
+		System.out.println();
+		
 		new NolixTestPool().run();
+	}
+	
+	//static method
+	private static void measureSizeOfNolixLibrary() {
+		
+		final var fileAccesssors =
+		new FolderAccessor().getParentFolderAccessor().getFolderAccessor("Source").getFileAccessorsRecursively();
+		
+		final var productiveClassCount =
+		Calculator.round(
+			fileAccesssors.getCount(
+				fa -> 
+				!fa.getName().endsWith("Test.java")
+				&& !fa.getName().endsWith("Launcher.java")
+				&& !fa.getName().endsWith("Tutorial.java")
+			)
+		)
+		.downToNext(10);
+		
+		System.out.println("number of productive classes: ca. " + productiveClassCount);
+		
+		final var testClassCount =
+		Calculator.round(fileAccesssors.getCount(fa -> fa.getName().endsWith("Test.java"))).downToNext(10);
+		
+		System.out.println("number of test classes: ca. " +  testClassCount);
+		
+		final var launcherClassCount =
+		Calculator.round(fileAccesssors.getCount(fa -> fa.getName().endsWith("Launcher.java"))).downToNext(10);
+		
+		System.out.println("number of launcher classes: ca. " + launcherClassCount);
+		
+		final var tutorialClassCount =
+		Calculator.round(fileAccesssors.getCount(fa -> fa.getName().endsWith("Tutorial.java"))).downToNext(10);
+		
+		System.out.println("number of tutorial classes: ca. " + tutorialClassCount);
 	}
 	
 	//private constructor
 	/**
 	 * Avoids that an instance of the {@link NolixTestPool} can be created.
+	 * 
+	 * @throws UninstantiableClassException
 	 */
-	private Launcher() {}
+	private Launcher() {
+		throw new UninstantiableClassException(Launcher.class);
+	}
 }
