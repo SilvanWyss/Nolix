0800cb5fb60a51d3f67c0c0664bfac88765d04f7
Silvan Wyss <silvan.wyss@gmail.com>
2019-03-25 20:15:01 +0100

    Created Recalculable interface.

---
+++ Source/ch/nolix/core/skillAPI/Recalculable.java
@@ -0,0 +1,22 @@
+//package declaration
+package ch.nolix.core.skillAPI;
+
+//interface
+/**
+ * A {@link Recalculable} can be recalculated.
+ * This means that the state of a {@Recalculable} can (!) be updated after several parameters have been reset.
+ * 
+ * There is a difference between a {@link Recalculable} and a {@link Refreshable}.
+ * 
+ * A {@link Recalculable} just recalculates its state
+ * whereas as a {@link Refreshable} refreshes also the display when it is shown.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-03
+ * @lines 20
+ */
+public interface Recalculable {
+	
+	//abstract method
+	public abstract void recalculate();
+}
