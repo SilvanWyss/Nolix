d1906b376f907d602e68113bd15b7cfdc300122e
Silvan Wyss <silvan.wyss@gmail.com>
2019-04-06 17:32:54 +0200

    Created BlackWhiteJuliaFractalTutorial and JuliaFractalTutorial.

---
+++ Source/ch/nolix/techTutorial/genericMathTutorial/BlackWhiteJuliaFractalTutorial.java
@@ -0,0 +1,53 @@
+package ch.nolix.techTutorial.genericMathTutorial;
+
+import ch.nolix.core.classProvider.ClassProvider;
+import ch.nolix.core.invalidArgumentException.UninstantiableClassException;
+import ch.nolix.core.sequencer.Sequencer;
+import ch.nolix.element.GUI.Frame;
+import ch.nolix.element.GUI.ImageWidget;
+import ch.nolix.element.color.Color;
+import ch.nolix.tech.genericMath.GenericMathRegistrator;
+import ch.nolix.techAPI.genericMathAPI.IComplexNumber;
+import ch.nolix.techAPI.genericMathAPI.IComplexNumberFactory;
+import ch.nolix.techAPI.genericMathAPI.IFractalBuilder;
+
+public final class BlackWhiteJuliaFractalTutorial {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		//Registers an implementation for the GenericMathAPI at the ClassProvider.
+		GenericMathRegistrator.register();
+		
+		final var maxIterationCount = 100;
+		final var j = ClassProvider.create(IComplexNumberFactory.class).create(-0.8, 0.15);
+		
+		//Creates a Frame that shows a realtime-generated image of a Fractal.
+		final var frame =
+		new Frame(
+			"Black White Julia Fractal Tutorial",
+			new ImageWidget(
+				ClassProvider.create(IFractalBuilder.class)
+				.setRealComponentInterval(-2.0, 2.0)
+				.setImaginaryComponentInterval(-1.5, 1.5)
+				.setWidthInPixel(800)
+				.setSequencesStartValuesFunction(c -> new IComplexNumber[]{c})
+				.setSequencesNextValueFunctionFor1Predecessor((p, c) -> p.getSquare().getSum(j))
+				.setSequencesMinDivergenceMagnitude(2.5)
+				.setSequencesMaxIterationCount(maxIterationCount)
+				.setColorFunction(i -> i < maxIterationCount ? Color.WHITE : Color.BLACK)
+				.setBigDecimalScale(20)
+				.build()
+				.startImageBuild()
+				.getRefImage()
+			)
+		);
+		
+		//Refreshes the frame as long as it is alive.
+		Sequencer.asLongAs(() -> frame.isAlive()).afterAllMilliseconds(100).run(() -> frame.refresh());
+	}
+	
+	private BlackWhiteJuliaFractalTutorial() {
+		throw new UninstantiableClassException(BlackWhiteJuliaFractalTutorial.class);
+	}
+}
---
+++ Source/ch/nolix/techTutorial/genericMathTutorial/JuliaFractalTutorial.java
@@ -0,0 +1,56 @@
+package ch.nolix.techTutorial.genericMathTutorial;
+
+import ch.nolix.core.classProvider.ClassProvider;
+import ch.nolix.core.invalidArgumentException.UninstantiableClassException;
+import ch.nolix.core.sequencer.Sequencer;
+import ch.nolix.element.GUI.Frame;
+import ch.nolix.element.GUI.ImageWidget;
+import ch.nolix.element.color.Color;
+import ch.nolix.tech.genericMath.GenericMathRegistrator;
+import ch.nolix.techAPI.genericMathAPI.IComplexNumber;
+import ch.nolix.techAPI.genericMathAPI.IComplexNumberFactory;
+import ch.nolix.techAPI.genericMathAPI.IFractalBuilder;
+
+public final class JuliaFractalTutorial {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		//Registers an implementation for the GenericMathAPI at the ClassProvider.
+		GenericMathRegistrator.register();
+		
+		final var maxIterationCount = 100;
+		final var j = ClassProvider.create(IComplexNumberFactory.class).create(-0.8, 0.15);
+		
+		//Creates a Frame that shows a realtime-generated image of a Fractal.
+		final var frame =
+		new Frame(
+			"Julia Fractal Tutorial",
+			new ImageWidget(
+				ClassProvider.create(IFractalBuilder.class)
+				.setRealComponentInterval(-2.0, 2.0)
+				.setImaginaryComponentInterval(-1.5, 1.5)
+				.setWidthInPixel(800)
+				.setSequencesStartValuesFunction(c -> new IComplexNumber[]{c})
+				.setSequencesNextValueFunctionFor1Predecessor((p, c) -> p.getSquare().getSum(j))
+				.setSequencesMinDivergenceMagnitude(2.5)
+				.setSequencesMaxIterationCount(maxIterationCount)
+				.setColorFunction(
+					i ->
+					i < maxIterationCount ?	new Color(i % 256, (10 * i) % 256, (2 * i) % 256) : Color.BLACK
+				)
+				.setBigDecimalScale(20)
+				.build()
+				.startImageBuild()
+				.getRefImage()
+			)
+		);
+		
+		//Refreshes the frame as long as it is alive.
+		Sequencer.asLongAs(() -> frame.isAlive()).afterAllMilliseconds(100).run(() -> frame.refresh());
+	}
+	
+	private JuliaFractalTutorial() {
+		throw new UninstantiableClassException(JuliaFractalTutorial.class);
+	}
+}
