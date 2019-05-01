63a25b5af884d93e14855b30c1b5e2c5f6d70dde
Silvan Wyss <silvan.wyss@gmail.com>
2019-05-01 23:13:24 +0200

    Edited NetEndPoint.

--- Source/ch/nolix/core/endPoint2/NetEndPoint.java
+++ Source/ch/nolix/core/endPoint2/NetEndPoint.java
@@ -16,16 +16,16 @@ import ch.nolix.core.validator.Validator;
 
 //class
 /**
- * A net end point can send messages to an other net end point that is on:
+ * A {@link NetEndPoint} can send messages to an other {@link NetEndPoint} that is on:
  * -the same process on the local computer
- * -an other process on the local computer
- * -an other process on an other computer
+ * -another process on the local computer
+ * -another process on another computer
  * 
  * @author Silvan Wyss
  * @month 2015-12
- * @lines 190
+ * @lines 370
  */
-public class NetEndPoint extends EndPoint {
+public final class NetEndPoint extends EndPoint {
 	
 	//attributes
 	private boolean receivedTargetInfo = false;
@@ -33,11 +33,11 @@ public class NetEndPoint extends EndPoint {
 	private final String HTTPMessage;
 	private final Socket socket;
 	private final PrintWriter printWriter;
-		
+	
 	//constructor
 	/**
-	 * Creates a new net end point that will connect to 
-	 * the default target on the given port on the local machine.
+	 * Creates a new {@link NetEndPoint} that will connect to 
+	 * the main target on the given port on the local machine.
 	 * 
 	 * @param port
 	 * @throws OutOfRangeException if the given port is not in [0, 65535].
@@ -45,31 +45,36 @@ public class NetEndPoint extends EndPoint {
 	public NetEndPoint(final int port) {
 		
 		//Calls other constructor.
-		this(
-			IPv6Catalogue.LOOP_BACK_ADDRESS,
-			port
-		);
+		this(IPv6Catalogue.LOOP_BACK_ADDRESS, port);
 	}
 	
-	public NetEndPoint(
-		int port,
-		String target
-	) {
+	//constructor
+	/**
+	 * Creates a new {@link NetEndPoint} that will connect to 
+	 * the given target on the given port on the local machine.
+	 * 
+	 * @param port
+	 * @param target
+	 * @throws OutOfRangeException if the given port is not in [0, 65535].
+	 * @throws NullArgumentException if the given target is null.
+	 * @throws InvalidArgumentException if the given target is blank.
+	 */
+	public NetEndPoint(final int port, final String target) {
+		
+		//Calls other constructor.
 		this(IPv6Catalogue.LOOP_BACK_ADDRESS, port, target);
 	}
 	
 	//constructor
 	/**
-	 * Creates a new net end point that connects to
-	 * the default target on the given port on the machine with the given ip.
+	 * Creates a new {@link NetEndPoint} that will connect to
+	 * the main target on the given port on the machine with the given ip.
 	 * 
 	 * @param ip
 	 * @param port
 	 * @throws OutOfRangeException if the given port is not in [0, 65535].
 	 */
-	public NetEndPoint(
-		final String ip,
-		final int port) {
+	public NetEndPoint(final String ip,	final int port) {
 		
 		//Calls constructor of the base class.
 		super(true);
@@ -77,31 +82,32 @@ public class NetEndPoint extends EndPoint {
 		//Checks if the given port is in [0, 65535]. 
 		Validator
 		.suppose(port)
-		.thatIsNamed("port")
+		.thatIsNamed(VariableNameCatalogue.PORT)
 		.isBetween(PortCatalogue.MIN_PORT, PortCatalogue.MAX_PORT);
 		
 		HTTPMessage = null;
 		
 		try {
 			
-			//Creates the socket of this net end point.
+			//Creates the socket of the current NetEndPoint.
 			socket = new Socket(ip, port);
 			
+			//Creates the printWriter of the current NetEndPoint.
 			printWriter = new PrintWriter(socket.getOutputStream());
 		}
-		catch (final IOException exception) {
-			throw new RuntimeException(exception);
+		catch (final IOException IOException) {
+			throw new RuntimeException(IOException);
 		}
 		
-		//Creates and starts the listener of this net end point.
+		//Creates and starts the listener of the current NetEndPoint.
 		new NetEndPointSubListener(this);
 		
-		send_internal("N");
+		send_internal(NetEndPointProtocol.MAIN_TARGET_PREFIX);
 	}
 	
 	//constructor
 	/**
-	 * Creates a new net end point that will connect to
+	 * Creates a new {@link NetEndPoint} that will connect to
 	 * the given target on the given port on the machine with the given ip.
 	 * 
 	 * @param ip
@@ -109,7 +115,7 @@ public class NetEndPoint extends EndPoint {
 	 * @param target
 	 * @throws OutOfRangeException if the given port is not in [0, 65535].
 	 * @throws NullArgumentException if the given target is null.
-	 * @throws EmptyArgumentException if the given target is empty.
+	 * @throws InvalidArgumentException if the given target is blank.
 	 */
 	public NetEndPoint(
 		final String ip,
@@ -124,26 +130,27 @@ public class NetEndPoint extends EndPoint {
 		//Checks if the given port is in [0, 65535]. 
 		Validator
 		.suppose(port)
-		.thatIsNamed("port")
+		.thatIsNamed(VariableNameCatalogue.PORT)
 		.isBetween(PortCatalogue.MIN_PORT, PortCatalogue.MAX_PORT);
 		
 		HTTPMessage = null;
 		
 		try {
 			
-			//Creates the socket of this net end point.
+			//Creates the socket of the current NetEndPoint.
 			socket = new Socket(ip, port);
 			
+			//Creates the printWriter of the current NetEndPoint.
 			printWriter = new PrintWriter(socket.getOutputStream());
 		}
-		catch (final IOException exception) {
-			throw new RuntimeException(exception);
+		catch (final IOException IOException) {
+			throw new RuntimeException(IOException);
 		}
 		
-		//Creates and starts the listener of this net end point.
+		//Creates and starts the listener of this {@link NetEndPoint}.
 		new NetEndPointSubListener(this);
 		
-		send_internal("T" + target);
+		send_internal(NetEndPointProtocol.TARGET_PREFIX + target);
 	}
 	
 	//package-visible constructor
@@ -153,6 +160,8 @@ public class NetEndPoint extends EndPoint {
 	 * @param socket
 	 * @param HTTPMessage
 	 * @throws NullArgumentException if the given socket is null.
+	 * @throws NullArgumentException if the given HTTPMessage is null.
+	 * @throws InvalidArgumentException if the given HTTPMessage is blank.
 	 */
 	NetEndPoint(final Socket socket, final String HTTPMessage) {
 		
@@ -162,22 +171,24 @@ public class NetEndPoint extends EndPoint {
 		//Checks if the given socket is not null.
 		Validator.suppose(socket).isOfType(Socket.class);
 		
-		//Checks if the given HTTP message is not null and not empty.
+		//Checks if the given HTTP message is not null or empty.
 		Validator.suppose(HTTPMessage).thatIsNamed("HTTP message").isNotEmpty();
 		
 		this.HTTPMessage = HTTPMessage;
 		
-		//Sets the socket of the current net end point.
+		//Sets the socket of the current NetEndPoint.
 		this.socket = socket;
 		
 		try {
+			
+			//Creates the printWriter of the current NetEndPoint.
 			printWriter = new PrintWriter(socket.getOutputStream());
 		}
-		catch (IOException e) {
-			throw new RuntimeException(e);
+		catch (IOException IOException) {
+			throw new RuntimeException(IOException);
 		}
 		
-		//Creates and starts the listener of the current net end point.
+		//Creates and starts the listener of the current NetEndPoint.
 		new NetEndPointSubListener(this);
 		
 		waitToTarget();
@@ -185,54 +196,79 @@ public class NetEndPoint extends EndPoint {
 	
 	//method
 	/**
-	 * @return true if this net end point is a net end point.
+	 * {@inheritDoc}
 	 */
 	@Override
 	public boolean isNetEndPoint() {
 		return true;
 	}
-
+	
 	//method
 	/**
-	 * @return the socket of this end point.
+	 * {@inheritDoc}
 	 */
-	protected Socket getRefSocket() {
-		return socket;
+	@Override
+	public void send(final String message) {
+		send_internal("M" + message);
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void noteClose() {
+		try {
+			socket.close();
+		}
+		catch (final IOException IOException) {}
 	}
 	
 	//package-visible method
 	/**
-	 * Lets this net end point receive the given message.
+	 * Lets the current {@link NetEndPoint} receive the given message.
 	 * 
 	 * @param message
-	 * @throws InvalidArgumentException if this net end point is aborted.
+	 * @throws InvalidArgumentException if the current {@link NetEndPoint} is not alive.
+	 * @throws NullArgumentException if the given message is null.
+	 * @throws InvalidArgumentException if the given message is blank.
 	 */
 	@Override
 	protected void receive(final String message) {
 		
+		//Checks if the given message is not null or blank.
 		Validator
 		.suppose(message)
 		.thatIsNamed(VariableNameCatalogue.MESSAGE)
-		.isNotEmpty();
+		.isNotBlank();
 		
 		//Enumerates the first character of the given message.
 		switch (message.charAt(0)) {
-			case 'M':
+			case NetEndPointProtocol.MESSAGE_PREFIX:
 				
+				//Checks if the current NetEndPoint is alive.
 				supposeIsAlive();
 			
 				//Calls method of the base class.
 				super.receive(message.substring(1));
 				
 				break;
-			case 'N':
+			case NetEndPointProtocol.MAIN_TARGET_PREFIX:
+				
+				//Checks if the current NetEndPoint is alive.
 				supposeIsAlive();
+				
 				receivedTargetInfo = true;
+				
 				break;
-			case 'T':
+			case NetEndPointProtocol.TARGET_PREFIX:
+				
+				//Checks if the current NetEndPoint is alive.
 				supposeIsAlive();
+				
 				receivedTargetInfo = true;
 				setTarget(message.substring(1));
+				
 				break;
 			case 'H':
 				isBrowserEndPoint = true;
@@ -252,17 +288,49 @@ public class NetEndPoint extends EndPoint {
 		}
 	}
 	
-	@Override
-	public void send(final String message) {
-		send_internal("M" + message);
+	//package-visible method
+	/**
+	 * @return the socket of the current {@link NetEndPoint}.
+	 */
+	Socket getRefSocket() {
+		return socket;
 	}
 	
+	//method
+	/**
+	 * @return true if the current {@link NetEndPoint} has received a target info.
+	 */
+	private boolean receivedTargetInfo() {
+		return receivedTargetInfo;
+	}
+	
+	//method
+	/**
+	 * Lets the current {@link NetEndPoint} send the given message.
+	 * 
+	 * @param message
+	 * @throws InvalidArgumentException if the current {@link NetEndPoint} is not alive.
+	 */
+	private void send_internal(final char message) {
+		
+		//Calls other method.
+		send_internal(String.valueOf(message));
+	}
+	
+	//method
+	/**
+	 * Lets the current {@link NetEndPoint} send the given message.
+	 * 
+	 * @param message
+	 * @throws NullArgumentException if the given message is null.
+	 * @throws InvalidArgumentException if the current {@link NetEndPoint} is not alive.
+	 */
 	private void send_internal(final String message) {
 		
 		//Checks if the given message is not null.
-		Validator.suppose(message).thatIsNamed("message").isNotNull();
+		Validator.suppose(message).thatIsNamed(VariableNameCatalogue.MESSAGE).isNotNull();
 		
-		//Checks if this net end point is not stopped.
+		//Checks if the current NetEndPoint is alive.
 		supposeIsAlive();
 		
 		printWriter.println(message);
@@ -271,29 +339,14 @@ public class NetEndPoint extends EndPoint {
 	
 	//method
 	/**
-	 * Lets this net end point note an abort.
-	 */
-	@Override
-	protected void noteClose() {
-		try {
-			socket.close();
-		} catch (final IOException exception) {}
-	}
-	
-	private boolean receivedTargetInfo() {
-		return receivedTargetInfo;
-	}
-
-	//method
-	/**
-	 * Lets this net end point wait to the target.
+	 * Lets the current {@link NetEndPoint} wait to the target.
 	 * 
-	 * @throws RuntimeException if this net end point reaches the timeout before it receives the target.
-	 * @throws InvalidArgumentException if this net end point is aborted.
+	 * @throws RuntimeException if the current {@link NetEndPoint} reaches the timeout before it receives a target.
+	 * @throws InvalidArgumentException if the current {@link NetEndPoint} is not alive.
 	 */
 	private void waitToTarget() {
 		
-		//Checks if this net end point is not stopped.
+		//Checks if the current NetEndPoint is alive.
 		supposeIsAlive();
 		
 		final long startTimeInMilliseconds = System.currentTimeMillis();
@@ -310,7 +363,7 @@ public class NetEndPoint extends EndPoint {
 			}
 			
 			if (System.currentTimeMillis() - startTimeInMilliseconds > 5000) {
-				throw new RuntimeException("Zeta end point reached timeout while waiting to target application.");
+				throw new InvalidArgumentException("reached timeout while waiting to target.");
 			}
 		}
 	}
---
+++ Source/ch/nolix/core/endPoint2/NetEndPointProtocol.java
@@ -0,0 +1,16 @@
+//package declaration
+package ch.nolix.core.endPoint2;
+
+//package-visible class
+/**
+ * @author Silvan Wyss
+ * @month 2019-05
+ * @lines 10
+ */
+final class NetEndPointProtocol {
+	
+	//constants
+	public static final char MESSAGE_PREFIX = 'M';
+	public static final char TARGET_PREFIX = 'T';
+	public static final char MAIN_TARGET_PREFIX = 'A';
+}
