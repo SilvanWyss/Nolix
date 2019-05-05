0d574bb8d94a78f53fba4ce6a983a64c077dc564
Silvan Wyss <silvan.wyss@gmail.com>
2019-05-05 13:37:51 +0200

    Created FileConcatenator.

---
+++ Source/ch/nolix/core/util/FileConcatenator.java
@@ -0,0 +1,57 @@
+//package declaration
+package ch.nolix.core.util;
+
+//own imports
+import ch.nolix.core.fileSystem.FolderAccessor;
+import ch.nolix.core.invalidArgumentException.FrozenArgumentException;
+import ch.nolix.core.skillAPI.Freezable;
+
+//class
+public final class FileConcatenator implements Freezable<FileConcatenator> {
+	
+	//attributes
+	private boolean frozen = false;
+	private final FolderAccessor rootFolderAccessor;
+	private final StringBuilder stringBuilder = new StringBuilder();
+	
+	//constructor
+	public FileConcatenator(final String rootFolderPath) {
+		rootFolderAccessor = new FolderAccessor(rootFolderPath);
+	}
+	
+	//method
+	public FileConcatenator addFile(final String relativeFilePath) {
+		return addFileContent(rootFolderAccessor.readFile(relativeFilePath));
+	}
+	
+	//method
+	public FileConcatenator addFileContent(final String fileContent) {
+		
+		if (frozen) {
+			throw new FrozenArgumentException(this);
+		}
+		
+		stringBuilder.append(fileContent);
+		
+		return this;
+	}
+	
+	//method
+	@Override
+	public FileConcatenator freeze() {
+		
+		frozen = true;
+		
+		return this;
+	}
+	
+	//method
+	public String getConcatenatatedContent() {
+		return stringBuilder.toString();
+	}
+	
+	@Override
+	public boolean isFrozen() {
+		return frozen;
+	}
+}
