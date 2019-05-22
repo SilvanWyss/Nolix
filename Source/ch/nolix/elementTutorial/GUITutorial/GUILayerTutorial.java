111c094ae3bb1ba206a972b7430d55eb5f4e845c
Silvan Wyss <silvan.wyss@gmail.com>
2019-05-22 22:07:17 +0200

    Created GUILayerTutorial.

---
+++ Source/ch/nolix/elementTutorial/GUITutorial/GUILayerTutorial.java
@@ -0,0 +1,69 @@
+package ch.nolix.elementTutorial.GUITutorial;
+
+import ch.nolix.core.elementEnums.ExtendedContentPosition;
+import ch.nolix.element.GUI.Frame;
+import ch.nolix.element.GUI.GUILayer;
+import ch.nolix.element.color.Color;
+import ch.nolix.element.widget.Button;
+import ch.nolix.element.widget.HorizontalStack;
+import ch.nolix.element.widget.VerticalStack;
+
+/**
+ * The {@link GUILayerTutorial} provides a tutorial for {@link GUILayer}s.
+ * Of the {@link GUILayerTutorial} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-05
+ */
+public class GUILayerTutorial {
+	
+	/**
+	 * Creates a {@link Frame} with a {@link Button}.
+	 * The button will add a second {@link GUILayer} to the frame when it is clicked on.
+	 * 
+	 * @param args
+	 */
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		//Creates a Frame.
+		final var frame = new Frame("GUILayer Tutorial");
+		
+		//Configures the look of the frame.
+		frame.setBackgroundColor(Color.ALICE_BLUE);
+		
+		//Creates dialogLayer.
+		final var dialogLayer = new GUILayer(
+			ExtendedContentPosition.Center,
+			new VerticalStack(
+				new HorizontalStack(
+					new Button("Close").setLeftMouseButtonReleaseCommand(() -> frame.removeTopLayer()),
+					new Button("Close").setLeftMouseButtonReleaseCommand(() -> frame.removeTopLayer())
+				)
+				.setElementMargin(200)
+			)
+			.applyOnBaseLook(
+				bl ->
+				bl
+				.setBorderThicknesses(5)
+				.setBorderColors(Color.DARK_BLUE)
+				.setBackgroundColor(Color.WHITE)
+				.setLeftPadding(10)
+				.setRightPadding(10)
+				.setTopPadding(200)
+				.setBottomPadding(10)
+			)
+		);
+		
+		//Creates showButton.
+		final var showButton = new Button("Show");
+		showButton.setLeftMouseButtonReleaseCommand(
+			() -> {
+				frame.addLayerOnTop(dialogLayer);
+			}
+		);
+		
+		//Adds the showButton to the frame.
+		frame.addLayerOnTop(ExtendedContentPosition.Center, showButton);
+	}
+}
