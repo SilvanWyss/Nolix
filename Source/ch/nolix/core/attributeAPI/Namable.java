c2d92830e53cd9adfab195208fa4cee7d761881b
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 22:27:19 +0200

    Edited Namable interface.

--- Source/ch/nolix/core/attributeAPI/Namable.java
+++ Source/ch/nolix/core/attributeAPI/Namable.java
@@ -3,22 +3,21 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * A namable object is a named object
- * whose name can be set programmatically.
+ * A {@link Namable} is a {@link Named} whose name can be set programmatically.
  * 
  * @author Silvan Wyss
  * @month 2015-12
- * @lines 30
- * @param <N> The type of a namable object.
+ * @lines 20
+ * @param <N> The type of a {@link Namable}.
  */
 public interface Namable<N extends Namable<N>> extends Named {
 	
 	//abstract method
 	/**
-	 * Sets the name of this namable object.
+	 * Sets the name of the current {@link Namable}.
 	 * 
 	 * @param name
-	 * @return this namable object.
+	 * @return the current {@link Namable}.
 	 */
 	public abstract N setName(String name);
 }
