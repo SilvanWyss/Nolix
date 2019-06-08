a188b1df8f72b51acbe80eca081186528c239d2d
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 23:26:03 +0200

    Edited Versioned interface.

--- Source/ch/nolix/core/attributeAPI/Versioned.java
+++ Source/ch/nolix/core/attributeAPI/Versioned.java
@@ -3,17 +3,17 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * A versioned object has a single version number.
+ * A {@link Versioned} has a single version number.
  * 
  * @author Silvan Wyss
  * @month 2015-12
  * @lines 10
  */
 public interface Versioned {
-
+	
 	//abstract method
 	/**
-	 * @return the version number of this versioned object.
+	 * @return the version number of the current {@link Versioned}.
 	 */
 	public abstract int getVersion();
 }
