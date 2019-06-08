0d77190b9e0693d8fded9cb4f7f26bff5545bdaf
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 23:12:45 +0200

    Edited Signed interface.

--- Source/ch/nolix/core/attributeAPI/Signed.java
+++ Source/ch/nolix/core/attributeAPI/Signed.java
@@ -8,30 +8,39 @@ package ch.nolix.core.attributeAPI;
  * A signature is a string representation of an object,
  * that can identify the object, but is mostly shorter than the common string representation.
  * 
- * Example: Given is a person
- * (surname: Peter, name: Muster, date of birth: 01.01.2000, hair color: brown, hobby: fishing).
+ * Example:
+ * There is given a person (surname: Peter, name: Muster, date of birth: 01.01.2000, hair color: brown, hobby: fishing).
  * -string representation: "Peter, Muster, 2000-01-01, brown, fishing"
  * -signature: "Peter Muster, 2001-01-01"
  * 
- * Example: Given is a package (index: 25000, content: HelloWorld)
+ * Example:
+ * There is given a package (index: 25000, content: HelloWorld).
  * -string representation: "25000, HelloWorld"
  * -signature: "25000"
  * 
  * @author Silvan Wyss
  * @month 2018-11
- * @lines 40
+ * @lines 50
  */
 public interface Signed {
 	
 	//abstract method
 	/**
-	 * @return the signature of the current {@Link Signed}.
+	 * @return the signature of the current {@link Signed}.
 	 */
 	public abstract String getSignature();
 	
 	//default method
 	/**
-	 * @return the signature of the current {@Link Signed} in quotes.
+	 * @return the signature of the current {@link Signed} in brackets.
+	 */
+	public default String getSignatureInBrackets() {
+		return ("(" + getSignature() + ")");
+	}
+	
+	//default method
+	/**
+	 * @return the signature of the current {@link Signed} in quotes.
 	 */
 	public default String getSignatureInQuotes() {
 		return ("'" + getSignature() + "'");
@@ -39,7 +48,7 @@ public interface Signed {
 	
 	//default method
 	/**
-	 * @return true if he current {@Link Signed} has the given signature.
+	 * @return true if he current {@link Signed} has the given signature.
 	 */
 	public default boolean hasSignature(final String signature) {
 		return getSignature().equals(signature);
