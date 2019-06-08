ff1388b3cf28f9841fa9f173500c1f703958c8f7
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 22:31:41 +0200

    Edited Named interface.

--- Source/ch/nolix/core/attributeAPI/Named.java
+++ Source/ch/nolix/core/attributeAPI/Named.java
@@ -3,23 +3,31 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * A named object has a name.
+ * A {@link Named} has a name.
  * 
  * @author Silvan Wyss
  * @month 2015-12
- * @lines 40
+ * @lines 60
  */
 public interface Named {
 	
 	//abstract method
 	/**
-	 * @return the name of this named object.
+	 * @return the name of the current {@link Named}.
 	 */
 	public abstract String getName();
 	
 	//default method
 	/**
-	 * @return the name of this named object in quotes.
+	 * @return the name of the current {@link Named} in brackets.
+	 */
+	public default String getNameInBrackets() {
+		return ("(" + getName() + ")");
+	}
+	
+	//default method
+	/**
+	 * @return the name of the current {@link Named} in quotes.
 	 */
 	public default String getNameInQuotes() {
 		return ("'" + getName() + "'");
@@ -28,7 +36,7 @@ public interface Named {
 	//default method
 	/**
 	 * @param name
-	 * @return true if this named object has the given name.
+	 * @return true if the current {@link Named} has the given name.
 	 */
 	public default boolean hasName(final String name) {
 		return getName().equals(name);
@@ -37,9 +45,16 @@ public interface Named {
 	//default method
 	/**
 	 * @param object
-	 * @return true if this named object has the same name as the given object.
+	 * @return true if the current {@link Named} has the same name as the given object.
 	 */
 	public default boolean hasSameNameAs(final Named object) {
+		
+		//Handles the case that the given object is null.
+		if (object == null) {
+			return false;
+		}
+		
+		//Handles the case that the given object is not null.
 		return hasName(object.getName());
 	}
 }
