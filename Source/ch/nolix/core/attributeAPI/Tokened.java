278d456146105e7fd28ab09a901fb3e85bc2dca7
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 23:19:47 +0200

    Edited Tokened interface.

--- Source/ch/nolix/core/attributeAPI/Tokened.java
+++ Source/ch/nolix/core/attributeAPI/Tokened.java
@@ -3,26 +3,58 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * A tokened object has a token.
+ * A {@link Tokened} has a token.
  * 
  * @author Silvan Wyss
  * @month 2016-12
- * @lines 20
+ * @lines 60
  */
 public interface Tokened {
-
+	
 	//abstract method
 	/**
-	 * @return the token of this tokened object.
+	 * @return the token of the current {@link Tokened}.
 	 */
 	public abstract String getToken();
 	
 	//default method
 	/**
+	 * @return the token of the current {@link Tokened} in brackets.
+	 */
+	public default String getTokenInBrackets() {
+		return ("(" + getToken() + ")");
+	}
+	
+	//default method
+	/**
+	 * @return the token of the current {@link Tokened} in quotes.
+	 */
+	public default String getTokenInQuotes() {
+		return ("'" + getToken() + "'");
+	}
+	
+	//default method
+	/**
 	 * @param token
-	 * @return true if this tokened object has the given token.
+	 * @return true if the current {@link Tokened} has the given token.
 	 */
 	public default boolean hasToken(final String token) {
 		return getToken().equals(token);
+	}
+	
+	//default method
+	/**
+	 * @param object
+	 * @return true if the current {@link Tokened} has the same token as the given object.
+	 */
+	public default boolean hasSameHeaderAs(final Tokened object) {
+		
+		//Handles the case that the given object is null.
+		if (object == null) {
+			return false;
+		}
+		
+		//Handles the case that the given object is not null.
+		return getToken().equals(object.getToken());
 	}
 }
