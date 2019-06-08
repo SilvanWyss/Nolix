8d9dbff9d4e2185704eb3e7aa35c298501088f86
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 22:58:15 +0200

    Edited OptionalNamable interface.

--- Source/ch/nolix/core/attributeAPI/OptionalNamable.java
+++ Source/ch/nolix/core/attributeAPI/OptionalNamable.java
@@ -3,61 +3,64 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * An optional namable object is a namable object
- * whose name can be set and removed programmatically.
+ * An {@link OptionalNamable} is a {@link Namable} whose name can be removed programmatically.
  * 
  * @author Silvan Wyss
  * @month 2015-12
  * @lines 60
- * @param <ON> The type of an optional namable object.
+ * @param <ON> The type of a {@link OptionalNamable}.
  */
-public interface OptionalNamable<ON extends OptionalNamable<ON>>
-extends Namable<ON>{
-
+public interface OptionalNamable<ON extends OptionalNamable<ON>> extends Namable<ON>{
+	
 	//abstract method
 	/**
-	 * @return true if this optional namable object has a name.
+	 * @return true if the current {@link OptionalNamable} has a name.
 	 */
 	public abstract boolean hasName();
 	
 	//default method
 	/**
 	 * @param name
-	 * @return true if this optional namable object has the given name.
+	 * @return true if the current {@link OptionalNamable} has the given name.
 	 */
 	@Override
 	public default boolean hasName(final String name) {
 		
-		//Handles the case that this optional namable object does not have a name.
+		//Handles the case that the current OptionalNamable does not have a name.
 		if (!hasName()) {
 			return false;
 		}
 		
-		//Handles the case that this optional namable object has a name.
+		//Handles the case that the current OptionalNamable has a name.
 		return getName().equals(name);
 	}
 	
 	//default method
 	/**
 	 * @param object
-	 * @return true if this optional namable object has the same name as the given object.
+	 * @return true if the current {@link OptionalNamable} has the same name as the given object.
 	 */
 	public default boolean hasSameNameAs(final OptionalNamable<?> object) {
+		
+		//Handles the case that the given object is null.
+		if (object == null) {
+			return false;
+		}
 		
 		//Handles the case that the given object does not have a name.
 		if (!object.hasName()) {
 			return false;
 		}
 		
-		//Handles the case that the given object has a name.
+		//Calls other method.
 		return hasName(object.getName());
 	}
 	
 	//abstract method
 	/**
-	 * Removes the name of this optional namable object.
+	 * Removes the name of the current {@link OptionalNamable}.
 	 * 
-	 * @return this optional namable object.
+	 * @return the current {@link OptionalNamable}.
 	 */
 	public abstract ON removeName();
 }
