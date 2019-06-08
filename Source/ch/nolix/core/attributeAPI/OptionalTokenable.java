89f4df02610c16ea05a05fb946423da45db4e4bd
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 23:04:26 +0200

    Edited OptionalTokenable interface.

--- Source/ch/nolix/core/attributeAPI/OptionalTokenable.java
+++ Source/ch/nolix/core/attributeAPI/OptionalTokenable.java
@@ -3,45 +3,64 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * An optional tokenable object is a tokenable object
- * whose token can be removed programmatically.
+ * A {@link OptionalTokenable} is a {@link Tokenable} whose token can be removed programmatically.
  * 
  * @author Silvan Wyss
  * @month 2015-12
  * @lines 30
- * @param <OT> The type of an optional tokenable object.
+ * @param <OT> The type of a {@link OptionalTokenable}.
  */
-public interface OptionalTokenable<OT extends OptionalTokenable<OT>>
-extends Tokenable<OT> {
+public interface OptionalTokenable<OT extends OptionalTokenable<OT>> extends Tokenable<OT> {
 	
 	//abstract method
 	/**
-	 * @return true if this optional tokenable object has a token.
+	 * @return true if the current {@link OptionalTokenable} has a token.
 	 */
 	public abstract boolean hasToken();
 	
 	//default method
 	/**
+	 * @param object
+	 * @return true if current {@link OptionalTokenable} has the same token as the given object.
+	 */
+	public default boolean hasSameTokenAs(final OptionalTokenable<?> object) {
+		
+		//Handles the case that the given object is null.
+		if (object == null) {
+			return false;
+		}
+		
+		//Handles the case that the given object does not have a token.
+		if (!object.hasToken()) {
+			return false;
+		}
+		
+		//Calls other method.
+		return hasToken(object.getToken());
+	}
+	
+	//default method
+	/**
 	 * @param token
-	 * @return true if this optional tokenable object has the given token.
+	 * @return true if the current {@link OptionalTokenable} has the given token.
 	 */
 	@Override
 	public default boolean hasToken(final String token) {
 		
-		//Handles the case that this optional tokenable object does not have a token.
+		//Handles the case that the current OptionalTokenable does not have a token.
 		if (!hasToken()) {
 			return false;
 		}
 		
-		//Handles the case that this optional tokenable object has a token.
+		//Handles the case that the current OptionalTokenable has a token.
 		return getToken().equals(token);
 	}
 	
 	//abstract method
 	/**
-	 * Removes the token of this optional tokenable object.
+	 * Removes the token of the current {@link OptionalTokenable}.
 	 * 
-	 * @return this optional tokenable object.
+	 * @return the current {@link OptionalTokenable}.
 	 */
 	public abstract OT removeToken();
 }
