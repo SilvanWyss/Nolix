5e8e9d93df69514e57bbb9d31c591d81f641caad
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-08 20:33:08 +0200

    Edited Indexed interface.

--- Source/ch/nolix/core/attributeAPI/Indexed.java
+++ Source/ch/nolix/core/attributeAPI/Indexed.java
@@ -3,26 +3,66 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * An indexed object has an index.
+ * A {@link Indexed} has an index.
  * 
  * @author Silvan Wyss
  * @month 2015-12
  * @lines 20
  */
 public interface Indexed {
-
+	
 	//abstract method
 	/**
-	 * @return the index of this indexed object.
+	 * @return the index of the current {@link Indexed}.
 	 */
 	public abstract int getIndex();
 	
 	//default method
 	/**
+	 * @return the index of the current {@link Indexed} as {@link String}.
+	 */
+	public default String getIndexAsString() {
+		return String.valueOf(getIndex());
+	}
+	
+	//default method
+	/**
+	 * @return the index of the current {@link Indexed} as {@link String} in brackets.
+	 */
+	public default String getIndexAsStringInBrackets() {
+		return ("(" + getIndexAsString() + ")");
+	}
+	
+	//default method
+	/**
+	 * @return the index of the current {@link Indexed} as {@link String} in quotes.
+	 */
+	public default String getIndexAsStringInQuotes() {
+		return ("'" + getIndexAsString() + "'");
+	}
+	
+	//default method
+	/**
 	 * @param index
-	 * @return true if this indexed object has the given index.
+	 * @return true if the current {@link Indexed} has the given index.
 	 */
 	public default boolean hasIndex(final int index) {
 		return (getIndex() == index);
+	}
+	
+	//default method
+	/**
+	 * @param object
+	 * @return true if the current {@link Indexed} has the same index as the given object.
+	 */
+	public default boolean hasSameIndexAs(final Indexed object) {
+		
+		//Handles the case that the given object is null.
+		if (object == null) {
+			return false;
+		}
+		
+		//Handles the case that the given object is not null.
+		return hasIndex(object.getIndex());
 	}
 }
