989d53eefbefa43b79d6756a7a728142d683249d
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-20 17:58:21 +0200

    Edited TextureCreatorTutorial.

--- Source/ch/nolix/templatesTutorial/textureCreatorTutorial/TextureCreatorTutorial.java
+++ Source/ch/nolix/templatesTutorial/textureCreatorTutorial/TextureCreatorTutorial.java
@@ -1,6 +1,8 @@
 package ch.nolix.templatesTutorial.textureCreatorTutorial;
 
+import ch.nolix.core.invalidArgumentException.UninstantiableClassException;
 import ch.nolix.element.GUI.Frame;
+import ch.nolix.element.color.Color;
 import ch.nolix.element.containerWidget.Accordion;
 import ch.nolix.element.containerWidget.AccordionTab;
 import ch.nolix.element.widget.ImageWidget;
@@ -8,12 +10,12 @@ import ch.nolix.templates.textureCreators.ConcreteTextureCreator;
 import ch.nolix.templates.textureCreators.JuteTextureCreator;
 
 /**
- * The {@link TextureCreatorTutorial} provides a tutorial for {@link JuteTextureCreator}.
+ * The {@link TextureCreatorTutorial} is a tutorial for Texture creators.
  * Of the {@link TextureCreatorTutorial} an instance cannot be created.
  * 
  * @author Silvan Wyss
  * @month 2018-08
- * @lines 40
+ * @lines 50
  */
 public final class TextureCreatorTutorial {
 	
@@ -24,26 +26,25 @@ public final class TextureCreatorTutorial {
 		final var concreteTexture = new ConcreteTextureCreator().createTexture(1000, 500);
 		final var juteTexture = new JuteTextureCreator().createTexture(1000, 500);
 		
-		//Creates a frame that will show the textures.
-		new Frame()
-		.setTitle("Texture Creator Tutorial")
-		.addLayerOnTop(
+		//Creates a Frame that will show the textures.
+		new Frame(
+			"Texture Creator Tutorial",
 			new Accordion(
 				new AccordionTab(
 					"Concrete",
-					new ImageWidget(
-						concreteTexture
-					)
+					new ImageWidget(concreteTexture	)
+					.applyOnBaseLook(bl -> bl.setBackgroundColor(Color.WHITE_SMOKE).setPaddings(10))
 				),
 				new AccordionTab(
 					"Jute",
-					new ImageWidget(
-						juteTexture
-					)
+					new ImageWidget(juteTexture)
+					.applyOnBaseLook(bl -> bl.setBackgroundColor(Color.WHITE_SMOKE).setPaddings(10))
 				)
-			)	
+			)
 		);
 	}
 	
-	private TextureCreatorTutorial() {}
+	private TextureCreatorTutorial() {
+		throw new UninstantiableClassException(getClass());
+	}
 }
