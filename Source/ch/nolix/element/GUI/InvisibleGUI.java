6ec6eee1013323235dbe8408a9143d032583eec7
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-23 10:33:35 +0200

    Created InvisibleGUI.

---
+++ Source/ch/nolix/element/GUI/InvisibleGUI.java
@@ -0,0 +1,62 @@
+//package declaration
+package ch.nolix.element.GUI;
+
+//class
+public final class InvisibleGUI extends GUI<InvisibleGUI> {
+		
+	//method
+	@Override
+	public int getCursorXPosition() {
+		//TODO
+		return 0;
+	}
+	
+	//method
+	@Override
+	public int getCursorYPosition() {
+		//TODO
+		return 0;
+	}
+	
+	//method
+	@Override
+	public int getHeight() {
+		//TODO
+		return 0;
+	}
+	
+	//method
+	@Override
+	public int getWidth() {
+		//TODO
+		return 0;
+	}
+	
+	//method
+	@Override
+	public int getViewAreaHeight() {
+		//TODO
+		return 0;
+	}
+	
+	//method
+	@Override
+	public int getViewAreaWidth() {
+		//TODO
+		return 0;
+	}
+	
+	//method
+	@Override
+	public boolean isRootGUI() {
+		return true;
+	}
+	
+	//method
+	@Override
+	public void paint() {}
+	
+	//method
+	@Override
+	protected void noteClosing() {}
+}
