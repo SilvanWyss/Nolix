b93a2232164ea50035caab0ae173125372c6b5c8
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-28 21:31:32 +0200

    Removed inheritence of OptionalValueable interface.

--- Source/ch/nolix/core/attributeAPI/OptionalValueable.java
+++ Source/ch/nolix/core/attributeAPI/OptionalValueable.java
@@ -3,15 +3,22 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * A {@link OptionalValueable} is a {@link Valueable} whose value can be removed programmatically.
+ * A {@link OptionalValueable} can have a value that can be set and removed programmatically.
  * 
  * @author Silvan Wyss
  * @month 2018-09
- * @lines 60
+ * @lines 70
  * @param <OV> The type of a {@link OptionalValueable}.
  * @param <V> The type of the value of a {@link OptionalValueable}.
  */
-public interface OptionalValueable<OV extends OptionalValueable<OV, V>, V> extends Valueable<OV, V> {
+public interface OptionalValueable<OV extends OptionalValueable<OV, V>, V> {
+	
+	//abstract method
+	/**
+	 * @return the value of the current {@link OptionalValueable}.
+	 * @throws Exception if the current {@link OptionalValueable} does not have a value.
+	 */
+	public abstract V getValue();
 	
 	//abstract method
 	/**
@@ -24,7 +31,6 @@ public interface OptionalValueable<OV extends OptionalValueable<OV, V>, V> exten
 	 * @param value
 	 * @return true if the current {@link OptionalValueable} has a value that equals the given value.
 	 */
-	@Override
 	public default boolean hasEqualValue(final Object value) {
 		
 		//Handles the case that the current OptionalValueable does not have a value.
@@ -41,7 +47,6 @@ public interface OptionalValueable<OV extends OptionalValueable<OV, V>, V> exten
 	 * @param value
 	 * @return true if the current {@link OptionalValueable} has the given value.
 	 */
-	@Override
 	public default boolean hasValue(final Object value) {
 		
 		//Handles the case that the current OptionalValueable does not have a value.
@@ -60,4 +65,13 @@ public interface OptionalValueable<OV extends OptionalValueable<OV, V>, V> exten
 	 * @return the current {@link OptionalValueable}.
 	 */
 	public abstract OV removeValue();
+	
+	//abstract method
+	/**
+	 * Sets the value of the current {@link OptionalValueable}.
+	 * 
+	 * @param value
+	 * @return the current {@link OptionalValueable}.
+	 */
+	public abstract OV setValue(V value);
 }
