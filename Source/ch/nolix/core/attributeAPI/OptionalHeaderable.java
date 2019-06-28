539839b9ec28a8a844f5eaa954538e56a6c9b5f7
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-28 21:07:33 +0200

    Updated description of OptionalHeaderable interface.

--- Source/ch/nolix/core/attributeAPI/OptionalHeaderable.java
+++ Source/ch/nolix/core/attributeAPI/OptionalHeaderable.java
@@ -3,7 +3,7 @@ package ch.nolix.core.attributeAPI;
 
 //interface
 /**
- * A {@link OptionalHeaderable} has a header that can be set and removed programmatically.
+ * A {@link OptionalHeaderable} can have a header that can be set and removed programmatically.
  * 
  * @author Silvan Wyss
  * @month 2019-02
