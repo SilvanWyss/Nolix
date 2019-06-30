5bf217e3b7c816a4c6c13ede0d2fc0666fa3d42c
Silvan Wyss <silvan.wyss@gmail.com>
2019-06-30 14:08:20 +0200

    Edited CalculatorTest.

--- Source/ch/nolix/coreTest/mathTest/CalculatorTest.java
+++ Source/ch/nolix/coreTest/mathTest/CalculatorTest.java
@@ -2,6 +2,7 @@
 package ch.nolix.coreTest.mathTest;
 
 //own import
+import ch.nolix.core.math.Calculator;
 import ch.nolix.core.test.Test;
 
 //test class
@@ -10,6 +11,27 @@ import ch.nolix.core.test.Test;
  * 
  * @author Silvan Wyss
  * @month 2016-08
- * @lines 10
+ * @lines 30
  */
-public final class CalculatorTest extends Test {}
+public final class CalculatorTest extends Test {
+	
+	//test case
+	public void testCase_getMax() {
+		
+		//execution
+		final var min = Calculator.getMax(-2.0, -1.0, 0.0, 1.0, 2.0);
+		
+		//validation
+		expect(min).isEqualTo(2.0);
+	}
+	
+	//test case
+	public void testCase_getMin() {
+		
+		//execution
+		final var min = Calculator.getMin(-2.0, -1.0, 0.0, 1.0, 2.0);
+		
+		//validation
+		expect(min).isEqualTo(-2.0);
+	}
+}
