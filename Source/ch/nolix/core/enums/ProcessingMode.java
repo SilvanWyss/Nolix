11cd5fde7036966488348dcca30a38def8114bd1
Silvan Wyss <silvan.wyss@gmail.com>
2019-07-14 17:49:32 +0200

    Created ProcessingMode enum.

---
+++ Source/ch/nolix/core/enums/ProcessingMode.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.core.enums;
+
+//enum
+/**
+ * A {@link ProcessingMode} defines if a process should be run single-threaded or multi-threaded.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-07
+ * @lines 10
+ */
+public enum ProcessingMode {
+	SINGLE_THREADED,
+	MULTI_THREADER
+}
