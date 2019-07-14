9329fe8d369c481e07a419cb5daeafc38eed977b
Silvan Wyss <silvan.wyss@gmail.com>
2019-07-14 17:40:18 +0200

    Fixed package info of elementEnums package.

--- Source/ch/nolix/element/elementEnums/package-info.java
+++ Source/ch/nolix/element/elementEnums/package-info.java
@@ -1,6 +1,6 @@
 //package info
 /**
- * The {@link elementEnums} contains {@link ElementEnum}s.
+ * The {@link elementEnums} package contains {@link ch.nolix.element.baseAPI.IElementEnum}s.
  * 
  * @author Silvan Wyss
  * @month 2019-04
