c3fafe208b820b046256d37c5d0f7da459298a35
Silvan Wyss <silvan.wyss@gmail.com>
2019-07-21 18:21:00 +0200

    Deleted util package.

--- Source/ch/nolix/core/util/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.util} package defines several unique types, helpers and providers.
- * 
- * @author Silvan Wyss
- * @month 2019-07
- * @lines 10
- */
-package ch.nolix.core.util;
