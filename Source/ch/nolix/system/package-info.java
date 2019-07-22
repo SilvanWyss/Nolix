0bf67f00412c6947645a6a65e53140eb6c270f27
Silvan Wyss <silvan.wyss@gmail.com>
2019-07-22 11:40:47 +0200

    Created package info file for system package.

---
+++ Source/ch/nolix/system/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system} package defines types for general purpose.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-07
+ * @lines 10
+ */
+package ch.nolix.system;
