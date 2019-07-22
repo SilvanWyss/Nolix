b71e9bccf2c86cb5fef334e88e75474aa3d4211f
Silvan Wyss <silvan.wyss@gmail.com>
2019-07-22 11:35:31 +0200

    Created Launcher for techTest package.

---
+++ Source/ch/nolix/techTest/Launcher.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.techTest;
+
+//class
+public final class Launcher {
+	
+	//main method
+	public static void main(String[] args) {
+		new TechTestPool().run();
+	}
+	
+	//private constructor
+	private Launcher() {}
+}
