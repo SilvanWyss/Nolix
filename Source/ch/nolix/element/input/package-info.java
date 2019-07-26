1e38f52c0941b09fa187a0d2ce668a0497853603
Silvan Wyss <silvan.wyss@gmail.com>
2019-07-26 20:41:14 +0200

    Created Key enum.

---
+++ Source/ch/nolix/element/input/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.element.input} package defines a key enum.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-07
+ * @lines 10
+ */
+package ch.nolix.element.input;
