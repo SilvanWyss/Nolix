8e7d2fd4b7da6ac3f88a86ce31ddd93a87dd0911
Silvan Wyss <silvan.wyss@gmail.com>
2019-07-28 17:30:17 +0200

    Created rasterAPI package.

---
+++ Source/ch/nolix/core/rasterAPI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.rasterAPI} package declares types that are in a raster.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-07
+ * @lines 10
+ */
+package ch.nolix.core.rasterAPI;
