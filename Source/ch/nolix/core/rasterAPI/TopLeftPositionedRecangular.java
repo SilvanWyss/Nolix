de8a28aa4a96fd988a21f3be8110279934b97bc8
Silvan Wyss <silvan.wyss@gmail.com>
2019-08-10 19:40:38 +0200

    Added containsPointRelatively default method to TopLeftPositionedRecangular interface.

--- Source/ch/nolix/core/rasterAPI/TopLeftPositionedRecangular.java
+++ Source/ch/nolix/core/rasterAPI/TopLeftPositionedRecangular.java
@@ -5,7 +5,7 @@ package ch.nolix.core.rasterAPI;
 /**
  * @author Silvan Wyss
  * @month 2019-09
- * @lines 20
+ * @lines 40
  */
 public interface TopLeftPositionedRecangular extends Rectangular, TopLeftPositioned {
 	
@@ -18,9 +18,24 @@ public interface TopLeftPositionedRecangular extends Rectangular, TopLeftPositio
 	 */
 	public default boolean containsPoint(final int xPosition, final int yPosition) {
 		return
-		getXPosition() <= xPosition
-		&& getYPosition() <= yPosition
-		&& getXPosition() + getWidth() > xPosition
-		&& getYPosition() + getHeight() > yPosition;
+		getXPosition() < xPosition
+		&& getYPosition() < yPosition
+		&& getXPosition() + getWidth() >= xPosition
+		&& getYPosition() + getHeight() >= yPosition;
+	}
+	
+	//default method
+	/**
+	 * @param xPosition
+	 * @param yPosition
+	 * @return true if the current {@link TopLeftPositionedRecangular}
+	 * contains the point with the given relativeXPosition and relativeYPosition.
+	 */
+	public default boolean containsPointRelatively(final int relativeXPosition, final int relativeYPosition) {
+		return
+		relativeXPosition >= 1
+		&& relativeYPosition >= 1
+		&& relativeXPosition <= getWidth()
+		&& relativeYPosition <= getHeight();
 	}
 }
