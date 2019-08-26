3195ae6c14be6e5d82e7a9b2fc7017bf30dd86dd
Silvan Wyss <silvan.wyss@gmail.com>
2019-08-26 20:49:55 +0200

    Edited FrameVisualizerResizeListener.

--- Source/ch/nolix/element/frameVisualizer/FrameVisualizerResizeListener.java
+++ Source/ch/nolix/element/frameVisualizer/FrameVisualizerResizeListener.java
@@ -6,36 +6,49 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 //own import
+import ch.nolix.core.sequencer.Sequencer;
+import ch.nolix.core.validator.Validator;
 import ch.nolix.element.GUI.GUI;
 
 //class
 public class FrameVisualizerResizeListener implements ComponentListener {
 	
 	//attributes
-	private GUI<?> frameVisualizer;
+	private GUI<?> mParentGUI;
+	private boolean isNew = true;
 	
 	//constructor
-	public FrameVisualizerResizeListener(GUI<?> frameVisualizer) {
-		this.frameVisualizer = frameVisualizer;
+	public FrameVisualizerResizeListener(GUI<?> mParentGUI) {
+		
+		Validator.suppose(mParentGUI).thatIsNamed("parent GUI").isNotNull();
+		
+		this.mParentGUI = mParentGUI;
 	}
-
+	
 	//method
 	@Override
 	public void componentHidden(ComponentEvent arg0) {}
-
+	
 	//method
 	@Override
 	public void componentMoved(ComponentEvent arg0) {}
-
+	
 	//method
 	@Override
 	public void componentResized(ComponentEvent arg0) {
-		this.frameVisualizer.noteResize(
+		
+		//This is important because events can be fired before the GUI is created completely.
+		if (isNew) {
+			Sequencer.waitForMilliseconds(500);
+			isNew = false;
+		}
+		
+		this.mParentGUI.noteResize(
 			arg0.getComponent().getWidth(),
 			arg0.getComponent().getHeight()
 		);
 	}
-
+	
 	//method
 	@Override
 	public void componentShown(ComponentEvent arg0) {}
