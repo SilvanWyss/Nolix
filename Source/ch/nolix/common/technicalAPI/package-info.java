88afd9f60a0c056eeed748b1a0a89f624f95eedf
Silvan Wyss <silvan.wyss@gmail.com>
2019-09-03 23:44:26 +0200

    Created technicalAPI package.

---
+++ Source/ch/nolix/common/technicalAPI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.technicalAPI} package declares technical properties of types.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-09
+ * @lines 10
+ */
+package ch.nolix.common.technicalAPI;
