c67a03e8c09aeccfe7e7164a8074bf230b59e85d
Silvan Wyss <silvan.wyss@gmail.com>
2019-09-03 23:45:00 +0200

    Created PurposeRequestable interface.

---
+++ Source/ch/nolix/common/technicalAPI/PurposeRequestable.java
@@ -0,0 +1,38 @@
+//package declaration
+package ch.nolix.common.technicalAPI;
+
+//own import
+import ch.nolix.common.technicalProperties.Purpose;
+
+//interface
+/**
+ * A {@link PurposeRequestable} can requested for its purpose.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-09
+ * @lines 30
+ */
+public interface PurposeRequestable {
+	
+	//abstract method
+	/**
+	 * @return the purpose of the current {@link PurposeRequestable}.
+	 */
+	public abstract Purpose getPurpose();
+	
+	//abstract method
+	/**
+	 * @return true if the current {@link PurposeRequestable} is functional.
+	 */
+	public default boolean isFunctional() {
+		return (getPurpose() == Purpose.FUNCTIONAL);
+	}
+	
+	//abstract method
+	/**
+	 * @return true if the current {@link PurposeRequestable} is technical.
+	 */
+	public default boolean isTechnical() {
+		return (getPurpose() == Purpose.TECHNICAL);
+	}
+}
