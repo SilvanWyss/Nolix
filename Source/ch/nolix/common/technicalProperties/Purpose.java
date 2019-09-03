660fa4627ba394ed8567b06821db0fbb6684ecb0
Silvan Wyss <silvan.wyss@gmail.com>
2019-09-03 23:40:19 +0200

    Created Purpose enum.

---
+++ Source/ch/nolix/common/technicalProperties/Purpose.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.common.technicalProperties;
+
+//enum
+/**
+ * A {@link Purpose} defines for what kind of requirement a given object is for.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-09
+ * @lines 10
+ */
+public enum Purpose {
+	FUNCTIONAL,
+	TECHNICAL
+}
