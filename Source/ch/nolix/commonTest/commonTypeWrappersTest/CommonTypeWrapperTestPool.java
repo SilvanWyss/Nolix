fb3a1b2ca7b5cf085f49e0f2f34699f22b84d022
Silvan Wyss <silvan.wyss@gmail.com>
2019-09-06 21:17:59 +0200

    Created commonTypeWrappersTest package.

---
+++ Source/ch/nolix/commonTest/commonTypeWrappersTest/CommonTypeWrapperTestPool.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.commonTest.commonTypeWrappersTest;
+
+//own import
+import ch.nolix.common.baseTest.TestPool;
+
+//class
+public final class CommonTypeWrapperTestPool extends TestPool {
+	
+	//constructor
+	public CommonTypeWrapperTestPool() {}
+}
---
+++ Source/ch/nolix/commonTest/commonTypeWrappersTest/Launcher.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.commonTest.commonTypeWrappersTest;
+
+//class
+public final class Launcher {
+	
+	//main method
+	public static void main(String[] args) {
+		new CommonTypeWrapperTestPool().run();
+	}
+	
+	//private constructor
+	private Launcher() {}
+}
