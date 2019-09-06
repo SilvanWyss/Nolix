304a9b4c010135a60a062d7b7698025c146be879
Silvan Wyss <silvan.wyss@gmail.com>
2019-09-06 21:06:37 +0200

    Edited package info file.

--- Source/ch/nolix/commonTest/commonTypeHelpersTest/package-info.java
+++ Source/ch/nolix/commonTest/commonTypeHelpersTest/package-info.java
@@ -1,6 +1,7 @@
 //package info
 /**
- * The {@link ch.nolix.commonTest.commonTypeHelpersTest} package defines tests for the {@link ch.nolix.common.commonTypeHelpers} package.
+ * The {@link ch.nolix.commonTest.commonTypeHelpersTest} package
+ * defines tests for the {@link ch.nolix.common.commonTypeHelpers} package.
  * 
  * @author Silvan Wyss
  * @month 2019-07
