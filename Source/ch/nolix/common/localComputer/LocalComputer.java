ba16af63315c0466535eda9cb09da26de5cffde5
Silvan Wyss <silvan.wyss@gmail.com>
2019-09-22 13:01:07 +0200

    Created LocalComputer.

---
+++ Source/ch/nolix/common/localComputer/LocalComputer.java
@@ -0,0 +1,23 @@
+//package declaration
+package ch.nolix.common.localComputer;
+
+//own imports
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+//class
+public final class LocalComputer {
+	
+	//static method
+	public static String getLANIP() {
+		try {
+			return InetAddress.getLocalHost().getHostAddress();
+		}
+		catch (final UnknownHostException unknownHostException) {
+			throw new RuntimeException(unknownHostException);
+		}
+	}
+	
+	//private constructor
+	private LocalComputer () {}
+}
