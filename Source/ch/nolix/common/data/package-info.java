a2ce3f11438b41b451b7815859ad29ec544ec7b7
Silvan Wyss <silvan.wyss@gmail.com>
2019-10-14 22:27:13 +0200

    Created data package.

---
+++ Source/ch/nolix/common/data/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.data} package defines data types.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-10
+ * @lines 10
+ */
+package ch.nolix.common.data;
