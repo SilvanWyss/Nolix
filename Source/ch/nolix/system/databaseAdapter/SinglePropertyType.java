95ce5b59c93a0f25c6967daea640168588e023e6
Silvan Wyss <silvan.wyss@gmail.com>
2019-10-26 17:06:27 +0200

    Edited SinglePropertyType.

--- Source/ch/nolix/system/databaseAdapter/SinglePropertyType.java
+++ Source/ch/nolix/system/databaseAdapter/SinglePropertyType.java
@@ -2,9 +2,8 @@
 package ch.nolix.system.databaseAdapter;
 
 //abstract class
-public abstract class SinglePropertyType<V>
-extends DataPropertyoidType<V> {
-
+public abstract class SinglePropertyType<V> extends DataPropertyoidType<V> {
+	
 	//constructor
 	public SinglePropertyType(final Class<V> valueClass) {
 		super(valueClass);
