937f899f1fdede38cbf533be3688f44ab1754469
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-02 17:52:37 +0100

    Edited ArgumentMediator.

--- Source/ch/nolix/common/validator/ArgumentMediator.java
+++ Source/ch/nolix/common/validator/ArgumentMediator.java
@@ -1,33 +1,29 @@
 //package declaration
 package ch.nolix.common.validator;
 
+//own imports
 import ch.nolix.common.constants.VariableNameCatalogue;
 import ch.nolix.common.functionAPI.IElementTakerBooleanGetter;
-import ch.nolix.common.invalidArgumentExceptions.EmptyArgumentException;
 import ch.nolix.common.invalidArgumentExceptions.InvalidArgumentException;
 import ch.nolix.common.invalidArgumentExceptions.ArgumentIsNullException;
 
 //class
 /**
- * A argument mediator is a mediator for an argument with a name.
- * A argument mediator is not mutable.
+ * A {@link ArgumentMediator} is not mutable.
  * 
  * @author Silvan Wyss
  * @month 2016-12
- * @lines 140
- * @param <A> The type of the argument of an argument mediator.
+ * @lines 130
+ * @param <A> The type of the argument of an {@link ArgumentMediator}.
  */
 public class ArgumentMediator<A> extends Mediator {
-
-	//constant
-	public static final String DEFAULT_ARGUMENT_NAME = "argument";
-	
+		
 	//attribute
 	private final A argument;
 	
 	//package-visible constructor
 	/**
-	 * Creates a new argument mediator for the given argument.
+	 * Creates a new {@link ArgumentMediator} for the given argument.
 	 * 
 	 * @param argument
 	 */
@@ -39,19 +35,19 @@ public class ArgumentMediator<A> extends Mediator {
 	
 	//package-visible constructor
 	/**
-	 * Creates a new argument mediator for the given argument with the given argument name.
+	 * Creates a new {@link ArgumentMediator} for the given argument, that has the given argument name.
 	 * 
 	 * @param argumentName
 	 * @param argument
-	 * @throws ArgumentIsNullException if the given argument name is null.
-	 * @throws EmptyArgumentException if the given argument name is empty.
+	 * @throws ArgumentIsNullException if the given argumentName is null.
+	 * @throws InvalidArgumentException if the given argument name is blank.
 	 */
 	ArgumentMediator(final String argumentName, final A argument) {
 		
 		//Calls constructor of the base class.
 		super(argumentName);
 		
-		//Sets the argument of this argument mediator.
+		//Sets the argument of the current ArgumentMediator.
 		this.argument = argument;
 	}
 	
@@ -59,39 +55,33 @@ public class ArgumentMediator<A> extends Mediator {
 	/**
 	 * @param condition
 	 * @throws ArgumentIsNullException if the given condition is null.
-	 * @throws InvalidArgumentException if the argument of this argument mediator is null.
+	 * @throws InvalidArgumentException
+	 * if the argument of the current {@link ArgumentMediator} does not fulfill the given condition.
 	 */
-	public final void fulfils(IElementTakerBooleanGetter<A> condition) {
+	public final void fulfills(IElementTakerBooleanGetter<A> condition) {
 		
 		//Checks if the given condition is not null.
 		if (condition == null) {
 			throw new ArgumentIsNullException(VariableNameCatalogue.CONDITION);
 		}
-		
-		//Checks if the argument of this argument mediator is not null.
-		isNotNull();
-		
+				
+		//Checks if the argument of the current ArgumentMediator fulfills the given condition.
 		if (!condition.getOutput(getRefArgument())) {
-			throw new InvalidArgumentException(
-				getArgumentName(),
-				getRefArgument(),
-				"does not fulfil the given condition"
-			);
+			throw
+			new InvalidArgumentException(getArgumentName(), getRefArgument(), "does not fulfil the given condition");
 		}
 	}
 	
 	//method
 	/**
-	 * @return a new terminal argument mediator for the argument of this argument mediator.
-	 * @throws InvalidArgumentException if the argument of this argument mediator is the given object.
+	 * @return a new {@link TerminalArgumentMediator} for the argument of the current {@link ArgumentMediator}.
+	 * @throws InvalidArgumentException if the argument of the current {@link ArgumentMediator} is the given object.
 	 */
 	public final TerminalArgumentMediator<A> isNot(final Object object) {
 		
-		//Checks if the argument of this argument mediator is not the given object.
+		//Checks if the argument of the current ArgumentMediator is not the given object.
 		if (argument == object) {
-			
-			//TODO: Create ArgumentIsSameException.
-			throw new InvalidArgumentException(argument, "is the given object");
+			throw new InvalidArgumentException(getArgumentName(), getRefArgument(), "is the given object");
 		}
 		
 		return new TerminalArgumentMediator<>(getRefArgument());
@@ -99,12 +89,12 @@ public class ArgumentMediator<A> extends Mediator {
 	
 	//method
 	/**
-	 * @return a new terminal argument mediator for the argument of this argument mediator.
-	 * @throws ArgumentIsNullException if the argument of this argument mediator is null.
+	 * @return a new {@link TerminalArgumentMediator} for the argument of the current {@link ArgumentMediator}.
+	 * @throws ArgumentIsNullException if the argument of the current {@link ArgumentMediator} is null.
 	 */
 	public TerminalArgumentMediator<A> isNotNull() {
 		
-		//Checks if the argument of this argument mediator is not null.
+		//Checks if the argument of the current ArgumentMediator is not null.
 		if (argument == null) {
 			throw new ArgumentIsNullException(getArgumentName());
 		}
@@ -115,28 +105,24 @@ public class ArgumentMediator<A> extends Mediator {
 	//method
 	/**
 	 * @param type
-	 * @throws ArgumentIsNullException if the argument of this argument mediator is null.
+	 * @throws ArgumentIsNullException if the argument of the current {@link ArgumentMediator} is null.
 	 * @throws InvalidArgumentException
-	 * if the argument of this argument mediator is null of the given type.
+	 * if the argument of the current {@link ArgumentMediator} is not of the given type.
 	 */
 	public final void isOfType(final Class<?> type) {
 		
-		//Checks if the argument of this argument mediator is not null.
+		//Checks if the argument of the current ArgumentMediator is not null.
 		isNotNull();
 		
-		//Checks if the argument of this argument mediator is of the given type.
+		//Checks if the argument of the current ArgumentMediator is of the given type.
 		if (!getRefArgument().getClass().getClass().isAssignableFrom(type.getClass())) {
-			throw new InvalidArgumentException(
-				getArgumentName(),
-				getRefArgument(),
-				"is not a " + type
-			);
+			throw new InvalidArgumentException(getArgumentName(), getRefArgument(), "is not a " + type);
 		}
 	}
 	
 	//method
 	/**
-	 * @return the argument of this argument mediator.
+	 * @return the argument of the current {@link ArgumentMediator}.
 	 */
 	protected A getRefArgument() {
 		return argument;
