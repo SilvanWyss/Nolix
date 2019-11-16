8764a9256698bb9ef295d63af3815aa1700af7ef
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-16 13:50:08 +0100

    Created BooleanHelper.

---
+++ Source/ch/nolix/common/commonTypeHelpers/BooleanHelper.java
@@ -0,0 +1,17 @@
+//package declaration
+package ch.nolix.common.commonTypeHelpers;
+
+//own import
+import ch.nolix.common.constants.StringCatalogue;
+
+//class
+public final class BooleanHelper {
+	
+	//static method
+	public static String toString(final boolean pBoolean) {
+		return (pBoolean ? StringCatalogue.TRUE_HEADER : StringCatalogue.FALSE_HEADER);
+	}
+	
+	//private constructor
+	private BooleanHelper() {}
+}
