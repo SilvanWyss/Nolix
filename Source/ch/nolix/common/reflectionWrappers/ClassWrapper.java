51a5a6ad65ad84bf5c622f2732ef72f09f6a7a02
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-16 23:58:57 +0100

    Created ClassWrapper.

---
+++ Source/ch/nolix/common/reflectionWrappers/ClassWrapper.java
@@ -0,0 +1,63 @@
+//package declaration
+package ch.nolix.common.reflectionWrappers;
+
+//Java import
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+//own imports
+import ch.nolix.common.constants.VariableNameCatalogue;
+import ch.nolix.common.validator.Validator;
+
+//class
+public final class ClassWrapper<T> {
+	
+	//attribute
+	private final Class<T> mClass;
+	
+	//constructor
+	public ClassWrapper(final Class<T> pClass) {
+		
+		Validator.suppose(pClass).thatIsNamed(VariableNameCatalogue.CLASS).isNotNull();
+		
+		mClass = pClass;
+	}
+	
+	//method
+	public T createInstance(final Object... arguments) {
+		try {
+			return getRefConstructor(getParameterTypes(arguments)).newInstance(arguments);
+		}
+		catch (
+			final
+			InstantiationException
+			| IllegalAccessException
+			| IllegalArgumentException
+			| InvocationTargetException
+			exception
+		) {
+			throw new RuntimeException(exception);
+		}
+	}
+	
+	//method
+	public Constructor<T> getRefConstructor(final Class<?>... parameterTypes) {
+		try {
+			return mClass.getConstructor(parameterTypes);
+		}
+		catch (final NoSuchMethodException | SecurityException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+	
+	//method
+	private Class<?>[] getParameterTypes(final Object... arguments) {
+		
+		final var parameterTypes = new Class<?>[arguments.length];
+		for (var i = 0; i < arguments.length; i++) {
+			parameterTypes[i] = arguments[i].getClass();
+		}
+		
+		return parameterTypes;
+	}
+}
