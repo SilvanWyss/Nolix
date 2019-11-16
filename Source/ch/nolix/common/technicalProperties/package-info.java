227fdb4b3ee90e42ca25cf32e5dbef6d06a82d00
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-16 11:39:18 +0100

    Deleted technicalProperties package.

--- Source/ch/nolix/common/technicalProperties/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.common.technicalProperties} package defines technical properties.
- * 
- * @author Silvan Wyss
- * @month 2019-09
- * @lines 10
- */
-package ch.nolix.common.technicalProperties;
