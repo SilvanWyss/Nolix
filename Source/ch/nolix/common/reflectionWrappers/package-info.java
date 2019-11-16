2f272200a0a6ef854e95d5eb3b0e9db2440f2e60
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-16 23:58:16 +0100

    Created reflectionWrappers package.

---
+++ Source/ch/nolix/common/reflectionWrappers/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.reflectionWrappers} package defines wrappers for reflection types.
+ * 
+ * @author Silvan Wyss
+ * @month 2019-11
+ * @lines 10
+ */
+package ch.nolix.common.reflectionWrappers;
