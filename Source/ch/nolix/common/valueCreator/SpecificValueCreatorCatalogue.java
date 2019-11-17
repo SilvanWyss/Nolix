feedc96f2f887bae4263743e55a70c9455bdad57
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-17 10:05:34 +0100

    Fixed SpecificValueCreatorCatalogue.

--- Source/ch/nolix/common/valueCreator/SpecificValueCreatorCatalogue.java
+++ Source/ch/nolix/common/valueCreator/SpecificValueCreatorCatalogue.java
@@ -13,14 +13,14 @@ public final class SpecificValueCreatorCatalogue {
 	
 	//constant
 	public static final SpecificValueCreator<BigDecimal> BIG_DECIMAL_CREATOR =
-	new SpecificValueCreator<>(BigDecimal.class, s -> new BigDecimal(s), s -> new BigDecimal(s.toString()));
+	new SpecificValueCreator<>(BigDecimal.class, s -> new BigDecimal(s), s -> new BigDecimal(s.getHeader()));
 	
 	//constant
 	public static final SpecificValueCreator<BinaryObject> BINARY_OBJECT_CREATOR =
 	new SpecificValueCreator<>(
 		BinaryObject.class,
 		s -> BinaryObject.fromString(s),
-		s -> BinaryObject.fromString(s.toString())
+		s -> BinaryObject.fromString(s.getHeader())
 	);
 	
 	//constant
@@ -33,7 +33,7 @@ public final class SpecificValueCreatorCatalogue {
 	
 	//constant
 	public static final SpecificValueCreator<String> STRING_CREATOR =
-	new SpecificValueCreator<>(String.class, s -> s, s -> s.toString());
+	new SpecificValueCreator<>(String.class, s -> s, s -> s.getHeader());
 	
 	//private constructor
 	private SpecificValueCreatorCatalogue() {}
