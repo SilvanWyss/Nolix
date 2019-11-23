b90fc5cd147f7e9d9a99848a6020403deb8eca42
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-23 13:07:50 +0100

    Fixed IDatabaseAdapter interface.

--- Source/ch/nolix/system/entity/IDatabaseAdapter.java
+++ Source/ch/nolix/system/entity/IDatabaseAdapter.java
@@ -5,11 +5,9 @@ package ch.nolix.system.entity;
 import ch.nolix.common.containers.IContainer;
 import ch.nolix.common.node.BaseNode;
 import ch.nolix.common.skillAPI.IChangesSaver;
-import ch.nolix.system.databaseAdapter.DatabaseAdapter;
-import ch.nolix.system.databaseAdapter.Entity;
 
 //interface
-public interface IDatabaseAdapter extends IChangesSaver<DatabaseAdapter>  {
+public interface IDatabaseAdapter extends IChangesSaver<IDatabaseAdapter>  {
 	
 	//abstract method
 	public <V> V createValueFromSpecification(Class<V> type, BaseNode specificaiton);
