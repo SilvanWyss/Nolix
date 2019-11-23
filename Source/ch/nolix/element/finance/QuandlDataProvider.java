47161616d1396a7d8213ea8a3de59e03403715b8
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-23 22:55:04 +0100

    Edited QuandlDataProvider.

--- Source/ch/nolix/element/finance/QuandlDataProvider.java
+++ Source/ch/nolix/element/finance/QuandlDataProvider.java
@@ -6,29 +6,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+//own imports
 import ch.nolix.common.containers.List;
 import ch.nolix.element.time.Time;
 
 //class
 /**
- * Provides methods to get finance data.
- * To get the data a web service from Quandl is used.
+ * A {@link QuandlDataProvider} provides methods to get finance data from the Quandl web service.
+ * A {@link QuandlDataProvider} is not mutable.
  * 
  * @author Silvan Wyss
  * @month 2017-08
  * @lines 70
  */
 public final class QuandlDataProvider {
-
+	
 	//method
 	/**
 	 * @param productSymbol
 	 * @param startDate
 	 * @param endDate
-	 * @return the candle sticks per day
-	 * of the product with the given product symbol
-	 * from the given start date
-	 * to the given end date.
+	 * @return the candle sticks per day of the product with the given productSymbol
+	 * from the given startDate to the given endDate.
 	 * @throws RuntimeException if an error occurs.
 	 */
 	public List<VolumeCandlestick> getCandleSticksPerDay(
@@ -36,43 +35,36 @@ public final class QuandlDataProvider {
 		final Time startDate,
 		final Time endDate
 	) {
-		try {
+		
+		final var dailyCandleSticks = new List<VolumeCandlestick>();
+		final var URLString = "https://www.quandl.com/api/v3/datasets/WIKI/" + productSymbol + "/data.csv";
+		
+		try (final var bufferedReader = new BufferedReader(new InputStreamReader(new URL(URLString).openStream()))) {
 			
-			final List<VolumeCandlestick> dailyCandleSticks = new List<>();
-			
-			final String URLString = "https://www.quandl.com/api/v3/datasets/WIKI/"
-				+ productSymbol
-				+ "/data.csv";
-			
-			final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(URLString).openStream()));
 			String line;
 			bufferedReader.readLine();
 			while ((line = bufferedReader.readLine()) != null) {
-
-				final String[] stringArray = line.split(",");
 				
-				final Time time = new Time(stringArray[0]);
+				final var stringArray = line.split(",");
+				final var time = new Time(stringArray[0]);
 				
-				if (time.isBefore(startDate)) {
-					break;
+				if (!time.isBefore(startDate) && !time.isAfter(endDate)) {
+					dailyCandleSticks.addAtBegin(
+						new VolumeCandlestick(
+							time,
+							Integer.valueOf(stringArray[5].substring(0, stringArray[5].length() - 3)),
+							Double.valueOf(stringArray[1]),
+							Double.valueOf(stringArray[4]),
+							Double.valueOf(stringArray[3]),
+							Double.valueOf(stringArray[2])
+						)	
+					);
 				}
-				
-				dailyCandleSticks.addAtBegin(
-					new VolumeCandlestick(
-						time,
-						Integer.valueOf(stringArray[5].substring(0, stringArray[5].length() - 3)),
-						Double.valueOf(stringArray[1]),
-						Double.valueOf(stringArray[4]),
-						Double.valueOf(stringArray[3]),
-						Double.valueOf(stringArray[2])
-					)	
-				);
 			}
 			
 			return dailyCandleSticks;
-			
-		} catch (final Exception e) {
-			throw new RuntimeException(e);
+		} catch (final Exception exception) {
+			throw new RuntimeException(exception);
 		}
 	}
 }
