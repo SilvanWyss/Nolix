b4a1d5961db47a75588737d309b21bdb536fba06
Silvan Wyss <silvan.wyss@gmail.com>
2019-11-23 12:09:49 +0100

    Edited IChangeSaver interface.

--- Source/ch/nolix/common/skillAPI/IChangesSaver.java
+++ Source/ch/nolix/common/skillAPI/IChangesSaver.java
@@ -10,15 +10,14 @@ package ch.nolix.common.skillAPI;
  * @lines 20
  * @param <CS> The type of a {@link IChangeSaver}.
  */
-public interface IChangesSaver<CS extends IChangesSaver<CS>>
-extends Resettable<CS> {
+public interface IChangesSaver<CS extends IChangesSaver<CS>> extends Resettable<CS> {
 	
 	//abstract method
 	/**
 	 * @return true if the current {@link IChangesSaver} has changes.
 	 */
 	public abstract boolean hasChanges();
-
+	
 	//abstract method
 	/**
 	 * Saves the changes of the current {@link IChangesSaver}.
