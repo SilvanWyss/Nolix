bac153c26b78ffab98982efabad6b016f2ccd1be
Silvan Wyss <silvan.wyss@gmail.com>
2019-12-30 17:21:25 +0100

    Created element.templates package.

---
+++ Source/ch/nolix/element/templates/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+* The {@link ch.nolix.element.templates} package defines templates for the {@link ch.nolix.element} package .
+* 
+* @author Silvan Wyss
+* @month 2019-12
+* @lines 10
+*/
+package ch.nolix.element.templates;
