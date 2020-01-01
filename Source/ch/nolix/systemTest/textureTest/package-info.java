5b053e0e8d63179e257fb4d5bf6f62c9bc960724
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-01 23:42:05 +0100

    Created textureTest package.

---
+++ Source/ch/nolix/systemTest/textureTest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemTest.textureTest} package defines tests for the {@link ch.nolix.system.texture} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.systemTest.textureTest;
