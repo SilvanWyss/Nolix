4ee161371ba8ffe0143419a28d66d5cd64572875
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-01 23:13:02 +0100

    Created texture package.

---
+++ Source/ch/nolix/system/texture/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.texture} package defines a texture creator.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.system.texture;
