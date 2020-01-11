371c1129f8b8c2d587b16cd6f9ae6d9e0ea18b00
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-11 18:24:17 +0100

    Created testableGUI_API package.

---
+++ Source/ch/nolix/element/testableGUI_API/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+* The {@link ch.nolix.element.testableGUI_API} package declares a testable GUI.
+* 
+* @author Silvan Wyss
+* @month 2020-01
+* @lines 10
+*/
+package ch.nolix.element.testableGUI_API;
