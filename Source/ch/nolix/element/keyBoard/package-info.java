892a621cd22ae9bc90f6ddc3fd0d0cd1bc6b685d
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-12 17:09:23 +0100

    Created keyBoard package.

---
+++ Source/ch/nolix/element/keyBoard/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.element.keyBoard} package defines types for keyboard inputs.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.element.keyBoard;
