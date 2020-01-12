4d555390912dcbdbc848b8892d613149810bca53
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-12 17:13:11 +0100

    Renamed testableGUI_API package to baseTestableGUI.

---
+++ Source/ch/nolix/element/baseTestableGUI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+* The {@link ch.nolix.element.baseTestableGUI} package declares a testable GUI.
+* 
+* @author Silvan Wyss
+* @month 2020-01
+* @lines 10
+*/
+package ch.nolix.element.baseTestableGUI;
