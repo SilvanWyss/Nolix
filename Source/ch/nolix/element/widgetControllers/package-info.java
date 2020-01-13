62c307d0bf13db62cc5a0bb58bae60972d608ac1
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-13 21:30:39 +0100

    Created widgetControllers package.

---
+++ Source/ch/nolix/element/widgetControllers/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.element.widgetControllers} package
+ * defines {@link ch.nolix.element.testableGUI.GUIElementController} for {@link Widget}s.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.element.widgetControllers;
