e36e48b25fd46bafacaf2f7183895534210ecfed
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-13 21:32:29 +0100

    Created TextLineWidgetController.

---
+++ Source/ch/nolix/element/widgetControllers/TextLineWidgetController.java
@@ -0,0 +1,44 @@
+//package declaration
+package ch.nolix.element.widgetControllers;
+
+//own import
+import ch.nolix.element.color.Color;
+
+//class
+public abstract class TextLineWidgetController extends BorderWidgetController {
+	
+	//method
+	public final Color getBaseTextColor() {
+		return Color.fromSpecification(getAttribute("BaseTextColor"));
+	}
+	
+	//method
+	public final Color getFocusTextColor() {
+		return Color.fromSpecification(getAttribute("FocusTextColor"));
+	}
+	
+	//method
+	public final Color getHoverTextColor() {
+		return Color.fromSpecification(getAttribute("HoverTextColor"));
+	}
+	
+	//method
+	public final String getText() {
+		return getAttribute("Text").getOneAttributeAsString();
+	}
+	
+	//method
+	public final int getTextLength() {
+		return getText().length();
+	}
+	
+	//method
+	public final boolean isBlank() {
+		return getText().isBlank();
+	}
+	
+	//method
+	public final boolean isEmpty() {
+		return getText().isEmpty();
+	}
+}
