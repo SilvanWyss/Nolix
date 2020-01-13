a622e2aea77798f3af8017339e5f6872234f8782
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-13 21:32:02 +0100

    Created BorderWidgetController.

---
+++ Source/ch/nolix/element/widgetControllers/BorderWidgetController.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.element.widgetControllers;
+
+//own imports
+import ch.nolix.element.color.Color;
+import ch.nolix.element.testableGUI.GUIElementController;
+
+//class
+public abstract class BorderWidgetController extends GUIElementController {
+	
+	//method
+	public final Color getBaseBackgroundColor() {
+		return Color.fromSpecification(getAttribute("BaseBackgroundColor"));
+	}
+	
+	//method
+	public final Color getFocusBackgroundColor() {
+		return Color.fromSpecification(getAttribute("FocusBackgroundColor"));
+	}
+	
+	//method
+	public final Color getHoverBackgroundColor() {
+		return Color.fromSpecification(getAttribute("HoverBackgroundColor"));
+	}
+}
