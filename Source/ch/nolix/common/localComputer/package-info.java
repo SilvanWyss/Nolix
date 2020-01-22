87e056f2cb928c0e910afe25398c4d01e57bc931
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-22 23:08:16 +0100

    Edited package-info file.

--- Source/ch/nolix/common/localComputer/package-info.java
+++ Source/ch/nolix/common/localComputer/package-info.java
@@ -1,6 +1,7 @@
 //package info
 /**
- * The {@link ch.nolix.common.localComputer} package defines providers to access functionalities of the local computer.
+ * The {@link ch.nolix.common.localComputer} package
+ * defines providers to access functionalities of the local computer.
  * 
  * @author Silvan Wyss
  * @month 2019-07
