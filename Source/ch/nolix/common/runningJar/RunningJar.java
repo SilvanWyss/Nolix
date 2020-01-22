f67d8b20f350d2a97a394ec6802907d7c953283c
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-22 23:07:23 +0100

    Created RunningJar.

---
+++ Source/ch/nolix/common/runningJar/RunningJar.java
@@ -0,0 +1,32 @@
+//package declaration
+package ch.nolix.common.runningJar;
+
+//Java imports
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//class
+public final class RunningJar {
+	
+	//static method
+	public static String getResource(final String pPackage) {
+		final var stringBuilder = new StringBuilder();
+		final var inputStream = RunningJar.class.getResourceAsStream("/" + pPackage);
+		final var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+		try {
+			var line = bufferedReader.readLine();
+			while (line != null) {
+				stringBuilder.append(line);
+				line = bufferedReader.readLine();
+			}
+		}
+		catch (final IOException IOException) {
+			throw new RuntimeException(IOException);
+		}
+		return stringBuilder.toString();
+	}
+	
+	//access-reducing constructor
+	private RunningJar() {}
+}
