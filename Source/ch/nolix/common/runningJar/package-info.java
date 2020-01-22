1ea4b75ce5554364eea35632968567aa01e7fc38
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-22 23:06:45 +0100

    Created runningJar package.

---
+++ Source/ch/nolix/common/runningJar/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.runningJar} package defines providers to access the currently running jar.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.common.runningJar;
