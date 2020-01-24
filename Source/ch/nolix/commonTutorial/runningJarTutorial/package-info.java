597a22fe0b6703c9a4fea75cd9426b804691d8a9
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-24 19:12:10 +0100

    Created runningJarTutorial package.

---
+++ Source/ch/nolix/commonTutorial/runningJarTutorial/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.commonTutorial.runningJarTutorial} package
+ * defines tutorials for the {@link ch.nolix.common.runningJar} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.commonTutorial.runningJarTutorial;
