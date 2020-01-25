27b10d9b417d93167b02daad784baba99eb59f6a
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-25 13:55:17 +0100

    Created ch.nolix.system.client.resources package.

---
+++ Source/ch/nolix/system/client/resources/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.client.resources} package
+ * contains the resources of the {@link ch.nolix.system.client} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.system.client.resources;
