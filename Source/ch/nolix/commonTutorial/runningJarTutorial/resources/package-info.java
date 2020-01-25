1992dfa405a23ebc74d98445efd596be18b0bb90
Silvan Wyss <silvan.wyss@gmail.com>
2020-01-25 13:08:31 +0100

    Created ch.nolix.commonTutorial.runningJarTutorial.resources package.

---
+++ Source/ch/nolix/commonTutorial/runningJarTutorial/resources/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.commonTutorial.runningJarTutorial.resources} package
+ * contains the resources of the {@link ch.nolix.commonTutorial.runningJarTutorial} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ * @lines 10
+ */
+package ch.nolix.commonTutorial.runningJarTutorial.resources;
