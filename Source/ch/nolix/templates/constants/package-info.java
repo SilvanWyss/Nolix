4e5703abe5f5f662f7cb508d1d606dfcc9304491
Heocro <silvan.wyss@gmail.com>
2020-02-01 21:19:48 +0100

    Created ch.nolix.templates.constants package.

---
+++ Source/ch/nolix/templates/constants/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.templates.constants} constants defines various constants.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-02
+ * @lines 10
+ */
+package ch.nolix.templates.constants;
