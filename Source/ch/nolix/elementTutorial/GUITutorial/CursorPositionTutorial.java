97a59a294055084e08b31bd5d35d02b72d0ddd2f
Heocro <silvan.wyss@gmail.com>
2020-02-01 17:02:08 +0100

    Edited the CursorPositionTutorial.

--- Source/ch/nolix/elementTutorial/GUITutorial/CursorPositionTutorial.java
+++ Source/ch/nolix/elementTutorial/GUITutorial/CursorPositionTutorial.java
@@ -3,29 +3,49 @@ package ch.nolix.elementTutorial.GUITutorial;
 import ch.nolix.common.sequencer.Sequencer;
 import ch.nolix.element.GUI.Frame;
 import ch.nolix.element.elementEnums.ExtendedContentPosition;
-import ch.nolix.element.widgets.HorizontalStack;
 import ch.nolix.element.widgets.Label;
 
+/**
+ * The {@link CursorPositionTutorial} is a tutorial for {@link Frame}s.
+ * Of the {@link CursorPositionTutorial} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-01
+ */
 public final class CursorPositionTutorial {
 	
+	/**
+	 * Creates a {@link Frame} that displays the cursor position.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
-		final var frame = new Frame("Mouse position Tutorial");
+		//Creates a Frame.
+		final var frame = new Frame("Cursor Position Tutorial");
 		
+		//Creates cursorPositionLabel.
 		final var cursorPositionLabel = new Label();
 		
-		frame.addLayerOnTop(
-			ExtendedContentPosition.LeftTop,
-			new HorizontalStack(new Label("Cursor position:"), cursorPositionLabel)
-		);
+		//Configures the look of the cursorPositionLabel.
+		cursorPositionLabel.applyOnBaseLook(bl -> bl.setPaddings(5));
 		
+		//Adds the cursorPositionLabel to the Frame at the left top.
+		frame.addLayerOnTop(ExtendedContentPosition.LeftTop, cursorPositionLabel);
+		
+		//Updates the cursorPositionLabel as long as the Frame is open.
 		Sequencer
 		.asLongAs(frame::isOpen)
 		.runInBackground(
 			() ->
-			cursorPositionLabel.setText(frame.getViewAreaCursorXPosition() + ", " + frame.getViewAreaCursorYPosition())
+			cursorPositionLabel.setText(
+				"Cursor position: " + frame.getViewAreaCursorXPosition() + ", " + frame.getViewAreaCursorYPosition()
+			)
 		);
 	}
 	
+	/**
+	 * Avoids that an instance of the {@link CursorPositionTutorial} can be created.
+	 */
 	private CursorPositionTutorial() {}
 }
