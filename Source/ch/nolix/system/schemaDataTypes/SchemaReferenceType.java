7e222708e542ad4d5c3a9b522e0ebaac89c0682c
Heocro <silvan.wyss@gmail.com>
2020-02-03 18:26:45 +0100

    Created SchemaReferenceType.

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaReferenceType.java
@@ -0,0 +1,59 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own imports
+import ch.nolix.common.validator.Validator;
+import ch.nolix.system.databaseSchemaAdapter.EntitySet;
+
+//class
+public abstract class SchemaReferenceType extends SchemaDataType<EntitySet> {
+	
+	//attribute
+	private final EntitySet referencedEntitySet;
+	
+	//constructor
+	public SchemaReferenceType(final EntitySet referencedEntitySet) {
+		
+		super(EntitySet.class);
+		
+		Validator.suppose(referencedEntitySet).thatIsNamed("referenced EntitySet").isNotNull();
+		
+		this.referencedEntitySet = referencedEntitySet;
+	}
+	
+	//method
+	@Override
+	public final boolean isAnyBackReferenceType() {
+		return false;
+	}
+	
+	//method
+	@Override
+	public final boolean isAnyControlType() {
+		return false;
+	}
+	
+	//method
+	@Override
+	public final boolean isAnyReferenceType() {
+		return true;
+	}
+	
+	//method
+	@Override
+	public final boolean isAnyValueType() {
+		return false;
+	}
+	
+	//method
+	@Override
+	public final boolean references(final EntitySet entitySet) {
+		return (referencedEntitySet == entitySet);
+	}
+	
+	//method
+	@Override
+	public final boolean referencesBack(final EntitySet entitySet) {
+		return false;
+	}
+}
