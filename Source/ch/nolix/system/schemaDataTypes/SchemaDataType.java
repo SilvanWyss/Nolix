db4888878203ea8fe2ba5237cf806e90894b6cd2
Heocro <silvan.wyss@gmail.com>
2020-02-03 18:25:01 +0100

    Created SchemaDataType.

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaDataType.java
@@ -0,0 +1,48 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own imports
+import ch.nolix.common.validator.Validator;
+import ch.nolix.system.databaseSchemaAdapter.EntitySet;
+import ch.nolix.system.entity.PropertyKind;
+
+//class
+public abstract class SchemaDataType<C> {
+	
+	//attribute
+	private final Class<C> contentClass;
+	
+	//constructor
+	public SchemaDataType(final Class<C> contentClass) {
+		
+		Validator.suppose(contentClass).thatIsNamed("content class").isNotNull();
+		
+		this.contentClass = contentClass;
+	}
+	
+	//method declaration
+	public abstract PropertyKind getPropertyKind();
+	
+	//method
+	public final Class<C> getRefContentClass() {
+		return contentClass;
+	}
+	
+	//method declaration
+	public abstract boolean isAnyBackReferenceType();
+	
+	//method declaration
+	public abstract boolean isAnyControlType();
+	
+	//method declaration
+	public abstract boolean isAnyReferenceType();
+	
+	//method declaration
+	public abstract boolean isAnyValueType();
+	
+	//method declaration
+	public abstract boolean references(EntitySet entitySet);
+	
+	//method declaration
+	public abstract boolean referencesBack(EntitySet entitySet);
+}
