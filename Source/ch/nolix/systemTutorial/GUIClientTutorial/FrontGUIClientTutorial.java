126eadc2295d0d7143b8f7829e5188ad3424be36
Heocro <silvan.wyss@gmail.com>
2020-02-06 18:53:10 +0100

    Edited FrontGUIClientTutorial.

--- Source/ch/nolix/systemTutorial/GUIClientTutorial/FrontGUIClientTutorial.java
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/FrontGUIClientTutorial.java
@@ -1,7 +1,6 @@
 package ch.nolix.systemTutorial.GUIClientTutorial;
 
 import ch.nolix.element.GUI.CursorIcon;
-import ch.nolix.element.GUI.ValueCatalogue;
 import ch.nolix.element.color.Color;
 import ch.nolix.element.elementEnums.ExtendedContentPosition;
 import ch.nolix.element.widgets.Button;
@@ -45,35 +44,35 @@ public final class FrontGUIClientTutorial {
 			//Sets the title of the GUI of the current MainSession.
 			getRefGUI().setTitle("Back GUI Client Tutorial");
 			
-			//Sets the background color of the GUI of the current MainSession.
-			getRefGUI().setBackgroundColor(Color.GREEN);
-			
 			//Creates a Button.
 			final var button =
 			new Button()
-			.setText("Change color")
-			.setLeftMouseButtonPressCommand(this::changeColor)
-			.setCustomCursorIcon(CursorIcon.Hand);
+			.setText("Change background color")
+			.setLeftMouseButtonPressCommand(this::changeColor);
 			
-			//Configures the base look of the Button.
+			//Configures the look of the Button.
 			button
-			.getRefBaseLook()
-			.setBackgroundColor(Color.LIGHT_GREY)
-			.setPaddings(10)
-			.setTextSize(ValueCatalogue.MEDIUM_TEXT_SIZE);
-			
-			//Configures the hover look of the Button.
-			button
-			.getRefHoverLook()
-			.setTextSize(ValueCatalogue.BIG_TEXT_SIZE);
+			.setCustomCursorIcon(CursorIcon.Hand)
+			.applyOnBaseLook(
+				bl ->
+				bl
+				.setBorderThicknesses(5)
+				.setBorderColors(Color.DARK_BLUE)
+				.setBackgroundColor(Color.WHITE_SMOKE)
+				.setPaddings(5)
+				.setTextSize(50)
+				.setTextColor(Color.DARK_BLUE)
+			)
+			.applyOnHoverLook(hl -> hl.setBackgroundColor(Color.LIGHT_GREY))
+			.applyOnFocusLook(fl -> fl.setBackgroundColor(Color.LIGHT_GREY));
 			
 			//Adds the Button to the GUI of the current MainSession.
-			getRefGUI().addLayerOnTop(ExtendedContentPosition.Center, button);
+			getRefGUI().addLayerOnTop(ExtendedContentPosition.Top, button);
 		}
 		
 		private void changeColor() {
 			
-			getRefGUI().setBackgroundColor(counter % 2 == 0 ? Color.GREEN : Color.RED);
+			getRefGUI().setBackgroundColor(counter % 2 == 0 ? Color.WHITE : Color.LIGHT_GREEN);
 			
 			counter++;
 		}
