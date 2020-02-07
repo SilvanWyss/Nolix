ab9561b289246fc3505375c08d3b9766814138f7
Heocro <silvan.wyss@gmail.com>
2020-02-07 19:13:15 +0100

    Created SchemaMultiBackReferenceType.

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaMultiBackReferenceType.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own imports
+import ch.nolix.system.databaseSchemaAdapter.EntitySet;
+import ch.nolix.system.entity.PropertyKind;
+
+//class
+public final class SchemaMultiBackReferenceType extends BaseSchemaBackReferenceType {
+	
+	//constructor
+	public SchemaMultiBackReferenceType(final EntitySet referencedEntitySet) {
+		super(referencedEntitySet);
+	}
+	
+	//method
+	@Override
+	public PropertyKind getPropertyKind() {
+		return PropertyKind.MULTI_BACK_REFERENCE;
+	}
+}
