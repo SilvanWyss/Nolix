55e91cc27739f44bf186093b351a994e7bbf4ebb
Heocro <silvan.wyss@gmail.com>
2020-02-07 18:35:06 +0100

    Created SchemaReferenceType.

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaReferenceType.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own imports
+import ch.nolix.system.databaseSchemaAdapter.EntitySet;
+import ch.nolix.system.entity.PropertyKind;
+
+//class
+public final class SchemaReferenceType extends BaseSchemaReferenceType {
+	
+	//constructor
+	public SchemaReferenceType(final EntitySet referencedEntitySet) {
+		super(referencedEntitySet);
+	}
+	
+	//method
+	@Override
+	public PropertyKind getPropertyKind() {
+		return PropertyKind.REFERENCE;
+	}
+}
