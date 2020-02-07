0ab978213af7c0cfc03d0cebce44828a00230b8b
Heocro <silvan.wyss@gmail.com>
2020-02-07 18:32:17 +0100

    Created SchemaMultiValueType.

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaMultiValueType.java
@@ -0,0 +1,20 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own import
+import ch.nolix.system.entity.PropertyKind;
+
+//class
+public final class SchemaMultiValueType<V> extends BaseSchemaValueType<V> {
+	
+	//constructor
+	public SchemaMultiValueType(final Class<V> valueClass) {
+		super(valueClass);
+	}
+	
+	//method
+	@Override
+	public PropertyKind getPropertyKind() {
+		return PropertyKind.MULTI_VALUE;
+	}
+}
