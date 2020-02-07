a3f1f4c68ac27391242337c95842034f86a7bcba
Heocro <silvan.wyss@gmail.com>
2020-02-07 18:54:23 +0100

    Created SchemaMultiReferenceType.

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaMultiReferenceType.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own imports
+import ch.nolix.system.databaseSchemaAdapter.EntitySet;
+import ch.nolix.system.entity.PropertyKind;
+
+//class
+public final class SchemaMultiReferenceType extends BaseSchemaReferenceType {
+	
+	//constructor
+	public SchemaMultiReferenceType(final EntitySet referencedEntitySet) {
+		super(referencedEntitySet);
+	}
+	
+	//method
+	@Override
+	public PropertyKind getPropertyKind() {
+		return PropertyKind.MULTI_REFERENCE;
+	}
+}
