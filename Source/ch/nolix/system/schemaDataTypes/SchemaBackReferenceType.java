d758c071da3a9eeea019d97b459cad3396a9958d
Heocro <silvan.wyss@gmail.com>
2020-02-07 19:05:12 +0100

    Created SchemaBackReferenceType.

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaBackReferenceType.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own imports
+import ch.nolix.system.databaseSchemaAdapter.EntitySet;
+import ch.nolix.system.entity.PropertyKind;
+
+//class
+public final class SchemaBackReferenceType extends BaseSchemaBackReferenceType {
+	
+	//constructor
+	public SchemaBackReferenceType(final EntitySet referencedEntitySet) {
+		super(referencedEntitySet);
+	}
+	
+	//method
+	@Override
+	public PropertyKind getPropertyKind() {
+		return PropertyKind.BACK_REFERENCE;
+	}
+}
