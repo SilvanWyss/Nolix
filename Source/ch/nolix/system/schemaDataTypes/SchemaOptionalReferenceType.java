78d3edb2248abb20c225cf138a95e6726f78e217
Heocro <silvan.wyss@gmail.com>
2020-02-07 18:36:00 +0100

    Created SchemaOptionalReferenceType

---
+++ Source/ch/nolix/system/schemaDataTypes/SchemaOptionalReferenceType.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.system.schemaDataTypes;
+
+//own imports
+import ch.nolix.system.databaseSchemaAdapter.EntitySet;
+import ch.nolix.system.entity.PropertyKind;
+
+//class
+public final class SchemaOptionalReferenceType extends BaseSchemaReferenceType {
+	
+	//constructor
+	public SchemaOptionalReferenceType(final EntitySet referencedEntitySet) {
+		super(referencedEntitySet);
+	}
+	
+	//method
+	@Override
+	public PropertyKind getPropertyKind() {
+		return PropertyKind.OPTIONAL_REFERENCE;
+	}
+}
