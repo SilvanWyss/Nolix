a1903d6ab00a617baacd26e447f96ce59ca0e064
Silvan Wyss <silvan.wyss@gmail.com>
2020-02-20 22:37:41 +0100

    Added toCamelCaseString method to IElementEnum interface.

--- Source/ch/nolix/element/baseAPI/IElementEnum.java
+++ Source/ch/nolix/element/baseAPI/IElementEnum.java
@@ -1,6 +1,7 @@
 //package declaration
 package ch.nolix.element.baseAPI;
 
+//own imports
 import ch.nolix.common.containers.LinkedList;
 import ch.nolix.common.node.Node;
 
@@ -8,7 +9,7 @@ import ch.nolix.common.node.Node;
 /**
  * @author Silvan Wyss
  * @month 2018-03
- * @lines 20
+ * @lines 50
  */
 public interface IElementEnum extends IElement {
 	
@@ -19,5 +20,34 @@ public interface IElementEnum extends IElement {
 	@Override
 	public default LinkedList<Node> getAttributes() {
 		return new Node(toString()).intoList();
+	}
+	
+	//method
+	/**
+	 * @return a {@link String} representation of the current {@link IElementEnum} in camel case.
+	 */
+	public default String toCamelCaseString() {
+		
+		final var stringBuilder = new StringBuilder();
+		
+		final var string = toString();
+		var nextCharacterIsUpperCase = true;
+		for (var i = 0; i < string.length(); i++) {
+			
+			final var character = string.charAt(i);
+			
+			if (character == '_') {
+				nextCharacterIsUpperCase = true;
+			}
+			else if (!nextCharacterIsUpperCase) {
+				stringBuilder.append(Character.toLowerCase(character));
+			}
+			else {
+				stringBuilder.append(Character.toUpperCase(character));
+				nextCharacterIsUpperCase = false;
+			}
+		}
+		
+		return stringBuilder.toString();
 	}
 }
