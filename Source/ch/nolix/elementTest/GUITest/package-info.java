13489e70d24662cc0c959e0c4b230a1158481418
Silvan Wyss <silvan.wyss@gmail.com>
2020-02-21 23:16:57 +0100

    Created GUITest package.

---
+++ Source/ch/nolix/elementTest/GUITest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.elementTest.GUITest} package defines tests for the {@link ch.nolix.element.GUI} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-02
+ * @lines 10
+ */
+package ch.nolix.elementTest.GUITest;
