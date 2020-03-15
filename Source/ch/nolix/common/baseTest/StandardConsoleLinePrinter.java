aa71ae15ae354b9f39e58378c3ceb3aabfde5747
Silvan Wyss <silvan.wyss@gmail.com>
2020-03-15 18:07:54 +0100

    Edited StandardConsoleLinePrinter.

--- Source/ch/nolix/common/baseTest/StandardConsoleLinePrinter.java
+++ Source/ch/nolix/common/baseTest/StandardConsoleLinePrinter.java
@@ -2,23 +2,45 @@
 package ch.nolix.common.baseTest;
 
 //class
+/**
+ * A {@link StandardConsoleLinePrinter} uses the standard output stream only and not the error output stream.
+ * The reason is that several output stream cannot be synchronized.
+ * Several output streams are flushed independent from each other.
+ * The order of the output of several output streams in the console is not guaranteed.
+ * 
+ * @month 2020-03
+ * @author Silvan Wyss
+ * @lines 40
+ */
 public final class StandardConsoleLinePrinter implements ILinePrinter {
 	
 	//method
+	/**
+	 * {@inheritDoc}}
+	 */
 	@Override
 	public void printEmptyLine() {
 		System.out.println();
+		System.out.flush();
 	}
 	
 	//method
+	/**
+	 * {@inheritDoc}}
+	 */
 	@Override
 	public void printErrorLine(final String errorLine) {
-		System.err.println(errorLine);
+		System.out.println(errorLine);
+		System.out.flush();
 	}
 	
 	//method
+	/**
+	 * {@inheritDoc}}
+	 */
 	@Override
 	public void printInfoLine(final String infoLine) {
 		System.out.println(infoLine);
+		System.out.flush();
 	}
 }
