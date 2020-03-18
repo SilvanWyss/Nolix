a5f3fa446fe82406423c6fd59acbcd9d9172daf0
Silvan Wyss <silvan.wyss@gmail.com>
2020-03-18 19:18:03 +0100

    Added getDefaultConstructor static method to ReflectionHelper.

--- Source/ch/nolix/common/baseTest/ReflectionHelper.java
+++ Source/ch/nolix/common/baseTest/ReflectionHelper.java
@@ -5,12 +5,29 @@ package ch.nolix.common.baseTest;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
+//own import
+import ch.nolix.common.invalidArgumentExceptions.InvalidArgumentException;
+
 //class
 final class ReflectionHelper {
 	
 	//static method
 	public static boolean firstIsSubClassOfSecond(final Class<?> class1, final Class<?> class2) {
 		return (class1 != null && class2 != null && class2.isAssignableFrom(class1) && class1 != class2);
+	}
+	
+	//static method
+	public static <T> Constructor<T> getDefaultConstructor(final Class<T> pClass) {
+		try {
+			
+			final var defaultConstructor = pClass.getConstructor();
+			defaultConstructor.setAccessible(true);
+			
+			return defaultConstructor;
+		}
+		catch (final NoSuchMethodException noSuchMethodException) {
+			throw new InvalidArgumentException(pClass, "does not have a default constructor");
+		}
 	}
 	
 	//static method
