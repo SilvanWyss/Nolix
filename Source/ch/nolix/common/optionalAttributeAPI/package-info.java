c494b853d8c2160d82c5f2125e4fd0d5d3ff3b2f
Silvan Wyss <silvan.wyss@gmail.com>
2020-03-28 23:52:12 +0100

    Created optionalAttributeAPI package.

---
+++ Source/ch/nolix/common/optionalAttributeAPI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.optionalAttributeAPI} package declares types with a certain optional attribute.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-03
+ * @lines 10
+ */
+package ch.nolix.common.optionalAttributeAPI;
