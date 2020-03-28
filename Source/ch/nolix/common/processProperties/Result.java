b175ba9e2baca250dc4c23a5c4b0aab9041ea47e
Silvan Wyss <silvan.wyss@gmail.com>
2020-03-28 17:13:57 +0100

    Created Result enum.

---
+++ Source/ch/nolix/common/processProperties/Result.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.common.processProperties;
+
+//enum
+/**
+ * A {@link Result} defines if a process run was a success or a failure.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-03
+ * @lines 10
+ */
+public enum Result {
+	SUCCESS,
+	FAILURE
+}
