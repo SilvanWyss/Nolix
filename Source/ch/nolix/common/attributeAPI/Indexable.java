5d927e9bd5ea2ca0b52a0d07b66dc7e0802f09a5
Silvan Wyss <silvan.wyss@gmail.com>
2020-03-29 18:06:29 +0200

    Created Indexable interface.

---
+++ Source/ch/nolix/common/attributeAPI/Indexable.java
@@ -0,0 +1,23 @@
+//package declaration
+package ch.nolix.common.attributeAPI;
+
+//interface
+/**
+ * A {@link Indexable} is a {@link Indexed} whose index can be set programmatically.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-03
+ * @lines 20
+ * @param <I> The type of a {@link Indexable}.
+ */
+public interface Indexable<I> extends Indexed {
+	
+	//method declaration
+	/**
+	 * Sets the index of the current {@link Indexable}.
+	 * 
+	 * @param index
+	 * @return the current {@link Indexable}.
+	 */
+	public abstract I setIndex(int index);
+}
