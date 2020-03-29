5be6e9399e493d3425c1c887b084e0ecd1f2f4f7
Silvan Wyss <silvan.wyss@gmail.com>
2020-03-29 18:06:02 +0200

    Fixed Identifiable interface and IdentifiableByString interface.

--- Source/ch/nolix/common/attributeAPI/Identifiable.java
+++ Source/ch/nolix/common/attributeAPI/Identifiable.java
@@ -3,22 +3,14 @@ package ch.nolix.common.attributeAPI;
 
 //interface
 /**
- * A {@link Identifiable} is a {@link Identified} whose id can be set and removed programmatically.
+ * A {@link Identifiable} is a {@link Identified} whose id can be set programmatically.
  * 
  * @author Silvan Wyss
  * @month 2020-03
- * @lines 30
+ * @lines 20
  * @param <I> The type of a {@link Identifiable}.
  */
 public interface Identifiable<I> extends Identified {
-	
-	//method declaration
-	/**
-	 * Removes the id of the current {@link Identifiable}.
-	 * 
-	 * @return the current {@link Identifiable}.
-	 */
-	public abstract I removeId();
 	
 	//method declaration
 	/**
--- Source/ch/nolix/common/attributeAPI/IdentifiableByString.java
+++ Source/ch/nolix/common/attributeAPI/IdentifiableByString.java
@@ -3,22 +3,14 @@ package ch.nolix.common.attributeAPI;
 
 //interface
 /**
- * A {@link IdentifiableByString} is a {@link IdentifiedByString} whose id can be set and removed programmatically.
+ * A {@link IdentifiableByString} is a {@link IdentifiedByString} whose id can be set programmatically.
  * 
  * @author Silvan Wyss
  * @month 2020-03
- * @lines 30
+ * @lines 20
  * @param <IBS> The type of a {@link IdentifiableByString}.
  */
 public interface IdentifiableByString<IBS> extends IdentifiedByString {
-	
-	//method declaration
-	/**
-	 * Removes the id of the current {@link IdentifiableByString}.
-	 * 
-	 * @return the current {@link IdentifiableByString}.
-	 */
-	public abstract IBS removeId();
 	
 	//method declaration
 	/**
