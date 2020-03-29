4cfb47b3b30d8c2bdff6b9102ac3c6afce915f40
Silvan Wyss <silvan.wyss@gmail.com>
2020-03-29 13:58:57 +0200

    Created Identifiable interface.

---
+++ Source/ch/nolix/common/attributeAPI/Identifiable.java
@@ -0,0 +1,31 @@
+//package declaration
+package ch.nolix.common.attributeAPI;
+
+//interface
+/**
+ * A {@link Identifiable} is a {@link Identified} whose id can be set and removed programmatically.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-03
+ * @lines 30
+ * @param <I> The type of a {@link Identifiable}.
+ */
+public interface Identifiable<I> extends Identified {
+	
+	//method declaration
+	/**
+	 * Removes the id of the current {@link Identifiable}.
+	 * 
+	 * @return the current {@link Identifiable}.
+	 */
+	public abstract I removeId();
+	
+	//method declaration
+	/**
+	 * Sets the id of the current {@link Identifiable}.
+	 * 
+	 * @param id
+	 * @return the current {@link Identifiable}.
+	 */
+	public abstract I setId(long id);
+}
