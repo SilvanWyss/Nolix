364137a07f47ba04cd46b650c7292234f1b57e08
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-04 13:23:19 +0200

    Edited IElementEnum enum.

--- Source/ch/nolix/element/baseAPI/IElementEnum.java
+++ Source/ch/nolix/element/baseAPI/IElementEnum.java
@@ -9,7 +9,7 @@ import ch.nolix.common.node.Node;
 /**
  * @author Silvan Wyss
  * @month 2018-03
- * @lines 50
+ * @lines 20
  */
 public interface IElementEnum extends IElement {
 	
@@ -19,35 +19,6 @@ public interface IElementEnum extends IElement {
 	 */
 	@Override
 	public default LinkedList<Node> getAttributes() {
-		return new Node(toCamelCaseString()).intoList();
-	}
-	
-	//method
-	/**
-	 * @return a {@link String} representation of the current {@link IElementEnum} in camel case.
-	 */
-	public default String toCamelCaseString() {
-		
-		final var stringBuilder = new StringBuilder();
-		
-		final var string = toString();
-		var nextCharacterIsUpperCase = true;
-		for (var i = 0; i < string.length(); i++) {
-			
-			final var character = string.charAt(i);
-			
-			if (character == '_') {
-				nextCharacterIsUpperCase = true;
-			}
-			else if (!nextCharacterIsUpperCase) {
-				stringBuilder.append(Character.toLowerCase(character));
-			}
-			else {
-				stringBuilder.append(Character.toUpperCase(character));
-				nextCharacterIsUpperCase = false;
-			}
-		}
-		
-		return stringBuilder.toString();
+		return new Node(toString()).intoList();
 	}
 }
--- Source/ch/nolix/elementTest/GUITest/WidgetStateTest.java
+++ Source/ch/nolix/elementTest/GUITest/WidgetStateTest.java
@@ -36,32 +36,4 @@ public final class WidgetStateTest extends Test {
 		//verification
 		expect(result).hasStringRepresentation("WidgetState(Normal)");
 	}
-	
-	//method
-	@TestCase
-	public void testCase_toCamelCaseString_whenHovered() {
-		
-		//setup
-		final var testUnit = WidgetState.HOVERED;
-		
-		//execution
-		final var result = testUnit.toCamelCaseString();
-		
-		//verification
-		expect(result).isEqualTo("Hovered");
-	}
-	
-	//method
-	@TestCase
-	public void testCase_toCamelCaseString_whenNormal() {
-		
-		//setup
-		final var testUnit = WidgetState.NORMAL;
-		
-		//execution
-		final var result = testUnit.toCamelCaseString();
-		
-		//verification
-		expect(result).isEqualTo("Normal");
-	}
 }
