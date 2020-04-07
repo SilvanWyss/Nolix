893af12db55d72dbc45933fd64719fd5d9f5b5be
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-07 21:18:13 +0200

    Created FieldHelper.

---
+++ Source/ch/nolix/common/reflectionHelpers/FieldHelper.java
@@ -0,0 +1,55 @@
+//package declaration
+package ch.nolix.common.reflectionHelpers;
+
+//Java imports
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+//own imports
+import ch.nolix.common.constants.VariableNameCatalogue;
+import ch.nolix.common.invalidArgumentExceptions.ArgumentIsNullException;
+import ch.nolix.common.invalidArgumentExceptions.InvalidArgumentException;
+import ch.nolix.common.wrapperException.WrapperException;
+
+//class
+public final class FieldHelper {
+	
+	//static method
+	@SuppressWarnings("unchecked")
+	public static <V> V getValueFromStaticField(final Field staticField) {
+		
+		if (!isStatic(staticField)) {
+			throw new InvalidArgumentException(staticField, "is not static");
+		}
+		
+		try {
+			return (V)staticField.get(null);
+		}
+		catch (final IllegalAccessException illegalAccessException) {
+			throw new WrapperException(illegalAccessException);
+		}
+	}
+	
+	//static method
+	public static final boolean isStatic(final Field field) {
+		
+		if (field == null) {
+			throw new ArgumentIsNullException(Field.class);
+		}
+		
+		return Modifier.isStatic(field.getModifiers());
+	}
+	
+	//method
+	public static boolean isStaticAndStoresValueOfGivenType(final Field field, final Class<?> type) {
+		
+		if (type == null) {
+			throw new ArgumentIsNullException(VariableNameCatalogue.TYPE);
+		}
+		
+		return (isStatic(field) && type.isAssignableFrom(getValueFromStaticField(field).getClass()));
+	}
+	
+	//access-reducing constructor
+	private FieldHelper() {}
+}
