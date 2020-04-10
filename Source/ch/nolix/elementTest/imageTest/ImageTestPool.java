93b116f3bb12ff508a2d7fec09f625b2da9b3195
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-10 16:10:34 +0200

    Created ImageTest.

---
+++ Source/ch/nolix/elementTest/imageTest/ImageTestPool.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.elementTest.imageTest;
+
+//own import
+import ch.nolix.common.baseTest.TestPool;
+
+//class
+public final class ImageTestPool extends TestPool {
+	
+	//constructor
+	public ImageTestPool() {
+		super(ImageTest.class);
+	}
+}
---
+++ Source/ch/nolix/elementTest/imageTest/Launcher.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.elementTest.imageTest;
+
+//class
+public final class Launcher {
+	
+	//main method
+	public static void main(String[] args) {
+		new ImageTestPool().run();
+	}
+	
+	//access-reducing constructor
+	private Launcher() {}
+}
