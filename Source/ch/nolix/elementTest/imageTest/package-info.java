b9ddde0b982f01314ff47cd7902b3b338152420d
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-10 16:10:15 +0200

    Created imageTest package.

---
+++ Source/ch/nolix/elementTest/imageTest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.elementTest.imageTest} package defines tests for the {@link ch.nolix.element.Image} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-04
+ * @lines 10
+ */
+package ch.nolix.elementTest.imageTest;
