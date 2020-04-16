967895cc8e4e3121941b3c4b782f9f7937e031d5
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-16 21:49:48 +0200

    Created WidgetIdCatalogue.

---
+++ Source/ch/nolix/element/GUI/WidgetIdCatalogue.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.element.GUI;
+
+//class
+public final class WidgetIdCatalogue {
+	
+	//constants
+	public static final String CONSOLE = "Console";
+	public static final String INFO_PANEL = "InfoPanel";
+	public static final String MAIN_MENU = "MainMenu";
+	public static final String MENU = "Menu";
+	
+	//visibility-reducing constructor
+	private WidgetIdCatalogue() {}
+}
