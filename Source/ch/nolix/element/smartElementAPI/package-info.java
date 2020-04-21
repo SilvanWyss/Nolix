8c025bc85abb51940152504a68c206b20091e93e
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-21 22:45:32 +0200

    Created smartElementAPI package.

---
+++ Source/ch/nolix/element/smartElementAPI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.element.smartElementAPI} package declares a smart element.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-04
+ * @lines 10
+ */
+package ch.nolix.element.smartElementAPI;
