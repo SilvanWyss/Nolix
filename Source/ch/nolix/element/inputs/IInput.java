4fea204c5436d4f775cb60d4e1905c894cb9708b
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-21 22:51:07 +0200

    Created IInput interface.

---
+++ Source/ch/nolix/element/inputs/IInput.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.element.inputs;
+
+//own import
+import ch.nolix.element.smartElementAPI.ISmartElement;
+
+//interface
+public interface IInput<I extends IInput<I>> extends ISmartElement<I> {}
