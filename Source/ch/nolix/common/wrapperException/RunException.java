b526ab5a85554c7f52816c95fe25de2ba626b701
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-21 21:57:31 +0200

    Created RunException.

---
+++ Source/ch/nolix/common/wrapperException/RunException.java
@@ -0,0 +1,26 @@
+//package declaration
+package ch.nolix.common.wrapperException;
+
+//class
+@SuppressWarnings("serial")
+public final class RunException extends RuntimeException {
+	
+	//static method
+	private static String createSafeErrorMessage(final String actionName) {
+		
+		if (actionName == null) {
+			throw new RuntimeException("The given action name is null.");
+		}
+		
+		if (actionName.isBlank()) {
+			throw new RuntimeException("The given action name is blank.");
+		}
+		
+		return ("An error occured by " + actionName + ".");
+	}
+	
+	//constructor
+	public RunException(final String actionName) {
+		super(createSafeErrorMessage(actionName));
+	}
+}
