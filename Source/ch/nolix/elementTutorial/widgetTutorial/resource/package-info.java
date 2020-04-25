a868cb1c5290d7f0f231791e41169907e2364056
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-25 20:22:12 +0200

    Created widgetTutorial.resource package.

---
+++ Source/ch/nolix/elementTutorial/widgetTutorial/resource/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.elementTutorial.widgetTutorial.resource} package
+ * contains the resources of the {@link ch.nolix.elementTutorial.widgetTutorial} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-04
+ * @lines 10
+ */
+package ch.nolix.elementTutorial.widgetTutorial.resource;
