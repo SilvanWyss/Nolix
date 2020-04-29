cec2888a50e55a0c19ab0c510f783b62b8131d96
Silvan Wyss <silvan.wyss@gmail.com>
2020-04-29 20:10:08 +0200

    Created worker package.

---
+++ Source/ch/nolix/common/worker/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.worker} package defines a worker, that can be started.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-04
+ * @lines 10
+ */
+package ch.nolix.common.worker;
