4d0ac1c788fe8bb68cde3763dd56ba7af80da50c
Silvan Wyss <silvan.wyss@gmail.com>
2020-05-01 10:51:47 +0200

    Created ConnectionOrigin enum.

---
+++ Source/ch/nolix/common/processProperty/ConnectionOrigin.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.common.processProperty;
+
+//enum
+/**
+ * A {@link ConnectionOrigin} defines if a connection was requested or accepted.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-05
+ * @lines 10
+ */
+public enum ConnectionOrigin {
+	REQUESTED_CONNECTION,
+	ACCEPTED_CONNECTION;
+}
