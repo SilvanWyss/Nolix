585b683cf0af9d084fc84eac8d05deb2c1df3ed8
Silvan Wyss <silvan.wyss@gmail.com>
2020-05-03 16:15:20 +0200

    Created TargetInfoState enum.

---
+++ Source/ch/nolix/common/processProperty/TargetInfoState.java
@@ -0,0 +1,16 @@
+//package declaration
+package ch.nolix.common.processProperty;
+
+//enum
+/**
+ * A {@link TargetInfoState} defines whether
+ * a {@link Object} either has received a target info or is waiting to a target info.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-05
+ * @lines 10
+ */
+public enum TargetInfoState {
+	RECEIVED_TARGET_INFO,
+	WAITS_TO_TARGET_INFO
+}
