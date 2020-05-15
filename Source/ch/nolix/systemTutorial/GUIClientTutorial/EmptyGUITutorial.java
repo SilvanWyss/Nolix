28a75dc1f0bc59ede24c052c69503e97e31682af
Silvan Wyss <silvan.wyss@gmail.com>
2020-05-15 17:01:32 +0200

    Created EmptyGUITutorial.

---
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/EmptyGUITutorial.java
@@ -0,0 +1,37 @@
+package ch.nolix.systemTutorial.GUIClientTutorial;
+
+import ch.nolix.common.localComputer.ShellProvider;
+import ch.nolix.element.color.Color;
+import ch.nolix.system.GUIClient.BackGUIClient;
+import ch.nolix.system.GUIClient.BackGUIClientSession;
+import ch.nolix.system.GUIClient.FrontGUIClient;
+import ch.nolix.system.client.Application;
+import ch.nolix.system.client.NetServer;
+
+public class EmptyGUITutorial {
+	
+	public static void main(String[] args) {
+		
+		//Defines port.
+		final var port = 50000;
+		
+		//Creates a NetServer with an Application for BackGUIClients.
+		new NetServer(port, new Application<>("Label Tutorial", BackGUIClient.class, MainSession.class));
+		
+		//Creates a FrontGUIClient that will connect to the NetServer.
+		new FrontGUIClient(port);
+		
+		//Starts a web browser that will connect to the NetServer.
+		ShellProvider.startFirefox(port);
+	}
+	
+	private static final class MainSession extends BackGUIClientSession {
+		
+		@Override
+		public void initialize() {
+			
+			//Sets the background color of the GUI of the current MainSession.
+			getRefGUI().setBackgroundColor(Color.LIGHT_GREY);
+		}
+	}
+}
