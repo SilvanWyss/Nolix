4b25363cdfbfe3b7903c8eca9e4617871803d9d6
Silvan Wyss <silvan.wyss@gmail.com>
2020-05-25 23:54:26 +0200

    Created caching package.

---
+++ Source/ch/nolix/common/caching/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.caching} package defines types that provide caching mechanisms.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-05
+ * @lines 10
+ */
+package ch.nolix.common.caching;
