58c10ec259aaf3ddf9567b901d10d5c33545f2d3
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-04 18:02:01 +0200

    Extended Pair.

--- Source/ch/nolix/common/pair/Pair.java
+++ Source/ch/nolix/common/pair/Pair.java
@@ -1,29 +1,29 @@
 //package declaration
 package ch.nolix.common.pair;
 
-//own imports
+//own import
 import ch.nolix.common.validator.Validator;
 
 //class
 /**
- * A {@link Pair} contains 2 elements.
- * A {@link Pair} is not mutable.
+ * A {@link {@link Pair}} contains 2 elements.
+ * A {@link {@link Pair}} is not mutable.
  * 
  * @author Silvan Wyss
  * @month 2015-12
- * @lines 70
+ * @lines 90
  * @param <E1> The type of the element 1 of a {@link Pair}.
  * @param <E2> The type of the element 2 of a {@link Pair}.
  */
-public class Pair<E1, E2> {
-
+public final class Pair<E1, E2> {
+	
 	//attributes
 	private final E1 element1;
 	private final E2 element2;
 	
 	//constructor
 	/**
-	 * Creates a new pair with the given elements.
+	 * Creates a new {@link Pair} with the given elements.
 	 * 
 	 * @param element1
 	 * @param element2
@@ -38,16 +38,16 @@ public class Pair<E1, E2> {
 		//Asserts that the given element 2 is not null.
 		Validator.assertThat(element2).thatIsNamed("element 2").isNotNull();
 		
-		//Sets the element 1 of the current Pair.
+		//Sets the element 1 of the current {@link Pair}.
 		this.element1 = element1;
 		
-		//Sets the element 2 of the current Pair.
+		//Sets the element 2 of the current {@link Pair}.
 		this.element2 = element2;
 	}
 	
 	//method
 	/**
-	 * @return the element 1 of this pair.
+	 * @return the element 1 of this {@link Pair}.
 	 */
 	public E1 getRefElement1() {
 		return element1;
@@ -55,7 +55,7 @@ public class Pair<E1, E2> {
 	
 	//method
 	/**
-	 * @return the element 2 of this pair.
+	 * @return the element 2 of this {@link Pair}.
 	 */
 	public E2 getRefElement2() {
 		return element2;
@@ -63,7 +63,25 @@ public class Pair<E1, E2> {
 	
 	//method
 	/**
-	 * @return a string representation of this pair.
+	 * @param object
+	 * @return true if the element 1 of the current {@link Pair} is the given object.
+	 */
+	public boolean hasElement1(final Object object) {
+		return (element1 == object);
+	}
+	
+	//method
+	/**
+	 * @param object
+	 * @return true if the element 2 of the current {@link Pair} is the given object.
+	 */
+	public boolean hasElement2(final Object object) {
+		return (element2 == object);
+	}
+	
+	//method
+	/**
+	 * @return a string representation of this {@link Pair}.
 	 */
 	@Override
 	public String toString() {
