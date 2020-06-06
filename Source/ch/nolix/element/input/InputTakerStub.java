bd2e92cd41cbb396178fdfbca34d069183fffa4a
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-06 23:09:22 +0200

    Created InputTakerStub.

---
+++ Source/ch/nolix/element/input/InputTakerStub.java
@@ -0,0 +1,65 @@
+//package declaration
+package ch.nolix.element.input;
+
+//own import
+import ch.nolix.element.elementEnum.DirectionOfRotation;
+
+//class
+public class InputTakerStub implements IInputTaker {
+	
+	//method
+	@Override
+	public void noteKeyPress(final Key key) {}
+	
+	//method
+	@Override
+	public void noteKeyRelease(final Key key) {}
+	
+	//method
+	@Override
+	public void noteKeyTyping(final Key key) {}
+	
+	//method
+	@Override
+	public void noteLeftMouseButtonClick() {}
+	
+	//method
+	@Override
+	public void noteLeftMouseButtonPress() {}
+	
+	//method
+	@Override
+	public void noteLeftMouseButtonRelease() {}
+	
+	//method
+	@Override
+	public void noteMouseMove(final int cursorXPositionOnViewArea, final int cursorYPositionOnViewArea) {}
+	
+	//method
+	@Override
+	public void noteMouseWheelClick() {}
+	
+	//method
+	@Override
+	public void noteMouseWheelPress() {}
+	
+	//method
+	@Override
+	public void noteMouseWheelRelease() {}
+	
+	//method
+	@Override
+	public void noteMouseWheelRotationStep(final DirectionOfRotation directionOfRotation) {}
+	
+	//method
+	@Override
+	public void noteRightMouseButtonClick() {}
+	
+	//method
+	@Override
+	public void noteRightMouseButtonPress() {}
+	
+	//method
+	@Override
+	public void noteRightMouseButtonRelease() {}
+}
