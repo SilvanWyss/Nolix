4831d58703cf102835e0c2c2852e6a2a511f01a7
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-11 17:09:03 +0200

    Created requestAPI package.

---
+++ Source/ch/nolix/common/requestAPI/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.common.requestAPI} package
+ * is an API that declares types that can be requested for certain information.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-06
+ * @lines 10
+ */
+package ch.nolix.common.requestAPI;
