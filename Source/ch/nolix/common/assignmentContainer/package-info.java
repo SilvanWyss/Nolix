68b1ff9863b8fb7f7afdb7aeba926515f90b73f2
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-13 15:02:41 +0200

    Created assignmentContainer package.

---
+++ Source/ch/nolix/common/assignmentContainer/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.assignmentContainer} package defines containers that assign elements.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-06
+ * @lines 10
+ */
+package ch.nolix.common.assignmentContainer;
