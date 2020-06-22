9191903cff573264dbd3c4bcf5daf993f14e1506
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-22 22:59:36 +0200

    Edited InvisibleCanvasGUI.

--- Source/ch/nolix/element/GUI/InvisibleCanvasGUI.java
+++ Source/ch/nolix/element/GUI/InvisibleCanvasGUI.java
@@ -3,18 +3,11 @@ package ch.nolix.element.GUI;
 
 //own imports
 import ch.nolix.common.state.Visibility;
-import ch.nolix.common.validator.Validator;
 import ch.nolix.element.input.IResizableInputTaker;
 
 //class
 public final class InvisibleCanvasGUI extends CanvasGUI<InvisibleCanvasGUI> {
-	
-	//attributes
-	private int viewAreaWidth;
-	private int viewAreaHeight;
-	private int viewAreaCursorXPosition;
-	private int viewAreaCursorYPosition;
-	
+		
 	//constructor
 	public InvisibleCanvasGUI(final IResizableInputTaker inputTaker) {
 		super(inputTaker, Visibility.INVISIBLE);
@@ -23,65 +16,18 @@ public final class InvisibleCanvasGUI extends CanvasGUI<InvisibleCanvasGUI> {
 	//method
 	@Override
 	public int getHeight() {
-		return viewAreaHeight;
+		return getViewAreaHeight();
 	}
 	
 	//method
 	@Override
 	public int getWidth() {
-		return viewAreaWidth;
-	}
-	
-	//method
-	@Override
-	public int getViewAreaCursorXPosition() {
-		return viewAreaCursorXPosition;
-	}
-
-	//method
-	@Override
-	public int getViewAreaCursorYPosition() {
-		return viewAreaCursorYPosition;
-	}
-	
-	//method
-	@Override
-	public int getViewAreaHeight() {
-		return viewAreaHeight;
-	}
-
-	//method
-	@Override
-	public int getViewAreaWidth() {
-		return viewAreaWidth;
+		return getViewAreaWidth();
 	}
 	
 	//method
 	@Override
 	public boolean isRootGUI() {
 		return true;
-	}
-	
-	//method
-	@Override
-	public void noteMouseMove(final int viewAreaCursorXPosition, final int viewAreaCursorYPosition) {
-		
-		super.noteMouseMove(viewAreaCursorXPosition, viewAreaCursorYPosition);
-		
-		this.viewAreaCursorXPosition = viewAreaCursorXPosition;
-		this.viewAreaCursorYPosition = viewAreaCursorYPosition;
-	}
-	
-	//method
-	@Override
-	public void noteResize(final int viewAreaWidth,final int viewAreaHeight) {
-		
-		super.noteResize(viewAreaWidth, viewAreaHeight);
-		
-		this.viewAreaWidth = viewAreaWidth;
-		
-		Validator.assertThat(viewAreaHeight).thatIsNamed("view arew height").isNotNegative();
-		
-		this.viewAreaHeight = viewAreaHeight;
 	}
 }
