b122124f715bb021129d0e1d5a8ae44881e8ac5b
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-28 11:14:53 +0200

    Edited ButtonTutorial.

--- Source/ch/nolix/systemTutorial/GUIClientTutorial/ButtonTutorial.java
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/ButtonTutorial.java
@@ -1,16 +1,17 @@
 package ch.nolix.systemTutorial.GUIClientTutorial;
 
+import ch.nolix.common.localComputer.ShellProvider;
 import ch.nolix.element.GUI.CursorIcon;
 import ch.nolix.element.color.Color;
-import ch.nolix.element.elementEnum.ExtendedContentPosition;
 import ch.nolix.element.widget.Button;
 import ch.nolix.system.GUIClient.BackGUIClient;
 import ch.nolix.system.GUIClient.BackGUIClientSession;
 import ch.nolix.system.GUIClient.FrontGUIClient;
 import ch.nolix.system.client.Application;
+import ch.nolix.system.client.NetServer;
 
 /**
- * The {@link ButtonTutorial} is a tutorial for {@link BackGUIClient}s.
+ * The {@link ButtonTutorial} is a tutorial for {@link Button}s.
  * Of the {@link ButtonTutorial} an instance cannot be created.
  * 
  * @author Silvan Wyss
@@ -21,17 +22,20 @@ public final class ButtonTutorial {
 	
 	/**
 	 * 1. Creates a {@link Application} for {@link BackGUIClient}s.
-	 * 2. Creates a front GUI client that will connect to the application.
+	 * 2. Creates a {@link FrontGUIClient} that will connect to the {@link Application}.
 	 * 
 	 * @param arguments
 	 */
 	public static void main(String[] args) {
 		
-		//Creates an Application for BackGUIClients.
-		final var application = new Application<BackGUIClient>("Application", BackGUIClient.class, MainSession.class);
+		//Creates a NetServer with an Application for BackGUIClients.
+		new NetServer("Button Tutorial", MainSession.class);
 		
-		//Creates a FrontGUIClient that will connect to the Application.
-		new FrontGUIClient(application);
+		//Creates a FrontGUIClient that will connect to the NetServer.
+		new FrontGUIClient();
+		
+		//Starts a web browser that will connect to the NetServer.
+		ShellProvider.startFirefoxToLoopBackAddress();
 	}
 	
 	private static final class MainSession extends BackGUIClientSession {
@@ -40,15 +44,12 @@ public final class ButtonTutorial {
 		
 		@Override
 		protected void initializeStage2() {
-			
-			//Sets the title of the GUI of the current MainSession.
-			getRefGUI().setTitle("Back GUI Client Tutorial");
-			
+						
 			//Creates a Button.
 			final var button =
 			new Button()
 			.setText("Change background color")
-			.setLeftMouseButtonPressCommand(this::changeColor);
+			.setLeftMouseButtonPressCommand(this::changeBackgroundColor);
 			
 			//Configures the look of the Button.
 			button
@@ -57,22 +58,20 @@ public final class ButtonTutorial {
 				bl ->
 				bl
 				.setBorderThicknesses(5)
-				.setBorderColors(Color.DARK_BLUE)
-				.setBackgroundColor(Color.WHITE_SMOKE)
+				.setBackgroundColor(Color.LAVENDER)
 				.setPaddings(5)
 				.setTextSize(50)
-				.setTextColor(Color.DARK_BLUE)
 			)
-			.applyOnHoverLook(hl -> hl.setBackgroundColor(Color.LIGHT_GREY))
-			.applyOnFocusLook(fl -> fl.setBackgroundColor(Color.LIGHT_GREY));
+			.applyOnHoverLook(hl -> hl.setBackgroundColor(Color.LAVENDER))
+			.applyOnFocusLook(fl -> fl.setBackgroundColor(Color.LAVENDER));
 			
 			//Adds the Button to the GUI of the current MainSession.
-			getRefGUI().addLayerOnTop(ExtendedContentPosition.Top, button);
+			getRefGUI().addLayerOnTop(button);
 		}
 		
-		private void changeColor() {
+		private void changeBackgroundColor() {
 			
-			getRefGUI().setBackgroundColor(counter % 2 == 0 ? Color.WHITE : Color.LIGHT_GREEN);
+			getRefGUI().setBackgroundColor(counter % 2 == 0 ? Color.WHITE : Color.BLUE);
 			
 			counter++;
 		}
