0e25bbb92673d7b1a7a6290e2f30013ac2276502
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-28 17:58:43 +0200

    Created IFrontEndWriter.

---
+++ Source/ch/nolix/element/baseGUI_API/IFrontEndWriter.java
@@ -0,0 +1,9 @@
+//package declaration
+package ch.nolix.element.baseGUI_API;
+
+//interface
+public interface IFrontEndWriter {
+	
+	//method declaration
+	public abstract void saveFile(byte[] bytes);
+}
