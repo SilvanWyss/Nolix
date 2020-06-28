f38ecf1c482fc5e28b818e6ef240e3023d0e3fbb
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-28 16:09:11 +0200

    Edited IOccupiableHoverableCanvas.

--- Source/ch/nolix/common/canvasAPI/IOccupiableHoverableCanvas.java
+++ Source/ch/nolix/common/canvasAPI/IOccupiableHoverableCanvas.java
@@ -4,11 +4,6 @@ package ch.nolix.common.canvasAPI;
 //interface
 public interface IOccupiableHoverableCanvas extends IHoverableCanvas {
 	
-	//method
-	public default boolean freeAreaIsUnderCursor() {
-		return !occupiedAreaIsUnderCursor();
-	}
-	
 	//method declaration
-	public abstract boolean occupiedAreaIsUnderCursor();
+	public abstract boolean freeContentAreaIsUnderCursor();
 }
