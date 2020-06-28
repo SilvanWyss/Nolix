39e824df5fa00d663b850d1dc99d3e747e41dfa8
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-28 11:03:39 +0200

    Edited HelloWorldGUITutorial.

--- Source/ch/nolix/systemTutorial/GUIClientTutorial/HelloWorldGUIOnCustomPortTutorial.java
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/HelloWorldGUIOnCustomPortTutorial.java
@@ -6,27 +6,38 @@ import ch.nolix.system.GUIClient.BackGUIClientSession;
 import ch.nolix.system.GUIClient.FrontGUIClient;
 import ch.nolix.system.client.NetServer;
 
-public final class HelloWorldGUIOnStandardPortTutorial {
+public final class HelloWorldGUIOnCustomPortTutorial {
 	
 	public static void main(String[] args) {
 		
+		//Defines port.
+		final var port = 50000;
+		
 		//Creates a NetServer with an Application for BackGUIClients.
-		new NetServer("Hello World GUI Tutorial", MainSession.class);
+		new NetServer(port, "Hello World GUI Tutorial", MainSession.class);
 		
 		//Creates a FrontGUIClient that will connect to the NetServer.
-		new FrontGUIClient();
+		new FrontGUIClient(port);
 		
 		//Starts a web browser that will connect to the NetServer.
-		ShellProvider.startFirefoxToLoopBackAddress();
+		ShellProvider.startFirefox(port);
 	}
 	
 	private static final class MainSession extends BackGUIClientSession {
 		
 		@Override
 		protected void initializeStage2() {
-			getRefGUI().addLayerOnTop(new Label("Hello World!").applyOnBaseLook(bl -> bl.setTextSize(50)));
+			
+			//Creates Label.
+			final var label = new Label("Hello World");
+			
+			//Configures the look of the Label.
+			label.applyOnBaseLook(bl -> bl.setTextSize(50));
+			
+			//Adds the Label to the GUI of the current MainSession.
+			getRefGUI().addLayerOnTop(label);
 		}
 	}
 	
-	private HelloWorldGUIOnStandardPortTutorial() {}
+	private HelloWorldGUIOnCustomPortTutorial() {}
 }
--- Source/ch/nolix/systemTutorial/GUIClientTutorial/HelloWorldGUITutorial.java
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/HelloWorldGUITutorial.java
@@ -2,34 +2,37 @@ package ch.nolix.systemTutorial.GUIClientTutorial;
 
 import ch.nolix.common.localComputer.ShellProvider;
 import ch.nolix.element.widget.Label;
-import ch.nolix.system.GUIClient.BackGUIClient;
 import ch.nolix.system.GUIClient.BackGUIClientSession;
 import ch.nolix.system.GUIClient.FrontGUIClient;
-import ch.nolix.system.client.Application;
 import ch.nolix.system.client.NetServer;
 
 public final class HelloWorldGUITutorial {
 	
 	public static void main(String[] args) {
 		
-		//Defines port.
-		final var port = 50000;
-		
 		//Creates a NetServer with an Application for BackGUIClients.
-		new NetServer(port, new Application<>("Hello World GUI Tutorial", BackGUIClient.class, MainSession.class));
+		new NetServer("Hello World GUI Tutorial", MainSession.class);
 		
 		//Creates a FrontGUIClient that will connect to the NetServer.
-		new FrontGUIClient(port);
+		new FrontGUIClient();
 		
 		//Starts a web browser that will connect to the NetServer.
-		ShellProvider.startFirefox(port);
+		ShellProvider.startFirefoxToLoopBackAddress();
 	}
 	
 	private static final class MainSession extends BackGUIClientSession {
 		
 		@Override
 		protected void initializeStage2() {
-			getRefGUI().addLayerOnTop(new Label("Hello World!").applyOnBaseLook(bl -> bl.setTextSize(50)));
+			
+			//Creates Label.
+			final var label = new Label("Hello World");
+			
+			//Configures the look of the Label.
+			label.applyOnBaseLook(bl -> bl.setTextSize(50));
+			
+			//Adds the Label to the GUI of the current MainSession.
+			getRefGUI().addLayerOnTop(label);
 		}
 	}
 	
