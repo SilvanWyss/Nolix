6d11efa60d7897a66fb716083bdfdb853c2a7e5d
Silvan Wyss <silvan.wyss@gmail.com>
2020-06-28 18:06:30 +0200

    Edited IFrontEndReader.

--- Source/ch/nolix/element/baseGUI_API/IFrontEndReader.java
+++ Source/ch/nolix/element/baseGUI_API/IFrontEndReader.java
@@ -1,9 +1,12 @@
 //package declaration
 package ch.nolix.element.baseGUI_API;
 
+//own import
+import ch.nolix.common.container.SingleContainer;
+
 //interface
 public interface IFrontEndReader {
 	
 	//method declaration
-	public abstract byte[] readFile();
+	public abstract SingleContainer<byte[]> readFileToBytes();
 }
