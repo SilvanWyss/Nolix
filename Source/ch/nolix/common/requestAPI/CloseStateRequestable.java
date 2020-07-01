674fdd603bd06ef8bce032551862b4171622622b
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-01 22:10:35 +0200

    Created CloseStateRequestable.

---
+++ Source/ch/nolix/common/requestAPI/CloseStateRequestable.java
@@ -0,0 +1,27 @@
+//package declaration
+package ch.nolix.common.requestAPI;
+
+//interface
+/**
+ * A {@link CloseStateRequestable} can be asked if it is closed or open.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-07
+ * @lines 20
+ */
+public interface CloseStateRequestable {
+	
+	//method declaration
+	/**
+	 * @return true if the current {@link CloseStateRequestable} is closed.
+	 */
+	public abstract boolean isClosed();
+	
+	//method
+	/**
+	 * @return true if the current {@link CloseStateRequestable} is open.
+	 */
+	public default boolean isOpen() {
+		return !isClosed();
+	}
+}
