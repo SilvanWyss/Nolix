b72c06f2d21655810642625005350103a24e3164
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-05 18:31:59 +0200

    Deleted ClosableElement.

--- Source/ch/nolix/common/closableElement/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.common.moduleManager} package defines a base element that is optionally closable.
- * 
- * @author Silvan Wyss
- * @month 2019-07
- * @lines 10
- */
-package ch.nolix.common.closableElement;
