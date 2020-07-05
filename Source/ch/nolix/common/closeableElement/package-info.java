4971e0b11b9754d5192b05c0555b35a9f5b8accf
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-05 18:20:22 +0200

    Created closeableElement package.

---
+++ Source/ch/nolix/common/closeableElement/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.closeableElement} package declares a closable element.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-07
+ * @lines 10
+ */
+package ch.nolix.common.closeableElement;
