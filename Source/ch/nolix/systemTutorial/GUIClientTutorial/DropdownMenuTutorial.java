e8d731d680a3244f81e13c745877d8a51e794b3e
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-17 11:18:42 +0200

    Created DropdownMenuTutorial.

---
+++ Source/ch/nolix/systemTutorial/GUIClientTutorial/DropdownMenuTutorial.java
@@ -0,0 +1,63 @@
+package ch.nolix.systemTutorial.GUIClientTutorial;
+
+import ch.nolix.common.localComputer.ShellProvider;
+import ch.nolix.common.sequencer.Sequencer;
+import ch.nolix.element.color.Color;
+import ch.nolix.element.widget.DropdownMenu;
+import ch.nolix.system.GUIClient.BackGUIClientSession;
+import ch.nolix.system.GUIClient.FrontGUIClient;
+import ch.nolix.system.client.NetServer;
+
+public final class DropdownMenuTutorial {
+	
+	public static void main(String[] args) {
+		
+		//Creates a NetServer with an Application for BackGUIClients.
+		final var netServer = new NetServer("DropdownMenu Tutorial", MainSession.class);
+		
+		//Creates a FrontGUIClient that will connect to the NetServer.
+		new FrontGUIClient();
+		
+		//Starts a web browser that will connect to the NetServer.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the NetServer as soon as it does not have a client connected any more.
+		Sequencer.asSoonAsNoMore(netServer::hasClientConnected).runInBackground(netServer::close);
+	}
+	
+	private static final class MainSession extends BackGUIClientSession {
+		
+		@Override
+		protected void initializeStage2() {
+			
+			//Creates a DropdownMenu.
+			final var dropdownMenu =
+			new DropdownMenu(
+				"Gottfried Wilhelm Leibniz",
+				"Immanuel Kant",
+				"Johann Gottlieb Fichte",
+				"Georg Wilehlm Friedrich Hegel",
+				"Arthur Schopenhauer",
+				"Johann Gottfried Herder",
+				"Karl Marx",
+				"Friedrich Nietzsche",
+				"Ludwig Wittgenstein",
+				"Theodor W. Adorno"
+			);
+			
+			//Configures the look of the DropdownMenu.
+			dropdownMenu.applyOnBaseLook(
+				bl ->
+				bl
+				.setBorderThicknesses(5)
+				.setBackgroundColor(Color.LAVENDER)
+				.setItemPadding(5)
+			);
+			
+			//Adds the DropdownMenu to the GUI of the current MainSession.
+			getRefGUI().addLayerOnTop(dropdownMenu);
+		}
+	}
+	
+	private DropdownMenuTutorial() {}
+}
