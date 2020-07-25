d652d31f1c824dd1c93ad9f974a44e6b227489e5
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-25 09:33:06 +0200

    Created dialog package.

---
+++ Source/ch/nolix/element/dialog/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.element.dialog} package defines dialog {@link Layer}s.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-07
+ * @lines 10
+ */
+package ch.nolix.element.dialog;
