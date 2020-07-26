b6724fd80bd05388abf99cec911ec16788b3912a
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-26 15:51:05 +0200

    Created mutableMultiAttributeAPI.

---
+++ Source/ch/nolix/common/mutableMultiAttributeAPI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.mutableMultiAttributeAPI} package declares types with a certain mutable multi-attribute.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-07
+ * @lines 10
+ */
+package ch.nolix.common.mutableMultiAttributeAPI;
