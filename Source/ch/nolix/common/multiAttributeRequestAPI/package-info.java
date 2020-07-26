410b6f5dca8af05b6785c5e74fb9ed6ebfc74648
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-26 15:41:35 +0200

    Created multiAttributeRequestAPI.

---
+++ Source/ch/nolix/common/multiAttributeRequestAPI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.multiAttributeRequestAPI} package declares types that have a multi-attribute.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-07
+ * @lines 10
+ */
+package ch.nolix.common.multiAttributeRequestAPI;
