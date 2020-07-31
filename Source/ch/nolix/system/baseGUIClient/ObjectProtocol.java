585da6b4319e23ba315963b735a06a00f83e4e81
Silvan Wyss <silvan.wyss@gmail.com>
2020-07-31 15:00:45 +0200

    Extended ObjectProtocol.

--- Source/ch/nolix/system/baseGUIClient/ObjectProtocol.java
+++ Source/ch/nolix/system/baseGUIClient/ObjectProtocol.java
@@ -7,6 +7,7 @@ final class ObjectProtocol {
 	//constants
 	public static final String GUI = "GUI";
 	public static final String GUI_TYPE = "GUIType";
+	public static final String VIEW_AREA_SIZE = "ViewAreaSize";
 	
 	//visibility-reducing constructor
 	private ObjectProtocol() {}
