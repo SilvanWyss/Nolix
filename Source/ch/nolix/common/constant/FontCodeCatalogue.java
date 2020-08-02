0c81c89c9e60003f5fdc9cbcadf38463787b52c4
Silvan Wyss <silvan.wyss@gmail.com>
2020-08-02 11:55:25 +0200

    Created FontCodeCatalogue.

---
+++ Source/ch/nolix/common/constant/FontCodeCatalogue.java
@@ -0,0 +1,29 @@
+//package declaration
+package ch.nolix.common.constant;
+
+//class
+/**
+ * Of the {@link FontCodeCatalogue} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-08
+ * @lines 20
+ */
+public final class FontCodeCatalogue {
+	
+	//constants
+    public static final String ARIAL = "Arial";
+    public static final String ARIAL_BLACK = "Arial Black";
+    public static final String COMIC_SANS_MS = "Comic Sans MS";
+    public static final String IMPACT = "Impact";
+    public static final String LUCIDA_CONSOLE = "Lucida Console";
+    public static final String PAPYRUS = "Papyrus";
+    public static final String TAHOMA = "Tahoma";
+    public static final String VERDANA = "Verdana";
+	
+	//visibility-reducing constructor
+	/**
+	 * Avoids that an instance of the {@link FontCodeCatalogue} can be created.
+	 */
+	private FontCodeCatalogue() {}
+}
