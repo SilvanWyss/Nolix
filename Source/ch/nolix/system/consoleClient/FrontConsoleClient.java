f3ff460343c0f86fe8b4ec4c9f57deaf56d63a02
Silvan Wyss <silvan.wyss@gmail.com>
2020-08-04 18:41:05 +0200

    Extended FrontConsoleClient.

--- Source/ch/nolix/system/consoleClient/FrontConsoleClient.java
+++ Source/ch/nolix/system/consoleClient/FrontConsoleClient.java
@@ -2,6 +2,7 @@
 package ch.nolix.system.consoleClient;
 
 //own imports
+import ch.nolix.common.endPoint2.NetServer;
 import ch.nolix.system.baseGUIClient.BaseFrontGUIClient;
 import ch.nolix.system.baseGUIClient.BaseFrontGUIClientGUIType;
 import ch.nolix.system.client.Application;
@@ -11,9 +12,20 @@ import ch.nolix.system.client.Server;
 /**
  * @author Silvan Wyss
  * @month 2017-03
- * @lines 70
+ * @lines 90
  */
 public final class FrontConsoleClient extends BaseFrontGUIClient<FrontConsoleClient> {
+	
+	//constructor
+	/**
+	 * Creates a new {@link FrontConsoleClient} that will connect to the default application on the default port.
+	 */
+	public FrontConsoleClient() {
+		
+		super(BaseFrontGUIClientGUIType.CanvasGUI);
+		
+		internalConnectTo(NetServer.DEFAULT_PORT);
+	}
 	
 	//constructor
 	/**
@@ -24,21 +36,21 @@ public final class FrontConsoleClient extends BaseFrontGUIClient<FrontConsoleCli
 	 */
 	public FrontConsoleClient(final Application<BackConsoleClient> application) {
 		
-		super(BaseFrontGUIClientGUIType.LayerGUI);
+		super(BaseFrontGUIClientGUIType.CanvasGUI);
 		
 		internalConnectTo(application);
 	}
 	
 	//constructor
 	/**
-	 * Creates a new {@link FrontConsoleClient} that will connect to the main application on the given port.
+	 * Creates a new {@link FrontConsoleClient} that will connect to the default application on the given port.
 	 * 
 	 * @param port
 	 * @throws ArgumentIsOutOfRangeException if the given port is not in [0,65535].
 	 */
 	public FrontConsoleClient(final int port) {
 		
-		super(BaseFrontGUIClientGUIType.LayerGUI);
+		super(BaseFrontGUIClientGUIType.CanvasGUI);
 		
 		internalConnectTo(port);
 	}
@@ -54,7 +66,7 @@ public final class FrontConsoleClient extends BaseFrontGUIClient<FrontConsoleCli
 	 */
 	public FrontConsoleClient(final Server server, final String application) {
 		
-		super(BaseFrontGUIClientGUIType.LayerGUI);
+		super(BaseFrontGUIClientGUIType.CanvasGUI);
 		
 		internalConnectTo(server, application);
 	}
@@ -72,7 +84,7 @@ public final class FrontConsoleClient extends BaseFrontGUIClient<FrontConsoleCli
 	 */
 	public FrontConsoleClient(final String ip, final int port, final String application) {
 		
-		super(BaseFrontGUIClientGUIType.LayerGUI);
+		super(BaseFrontGUIClientGUIType.CanvasGUI);
 		
 		internalConnectTo(ip, port, application);
 	}
