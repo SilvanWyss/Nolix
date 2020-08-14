d8848ff57b896ac4bce0c06ce671d32d26ca04c4
Silvan Wyss <silvan.wyss@gmail.com>
2020-08-14 11:43:30 +0200

    Created incrementAPI package.

---
+++ Source/ch/nolix/common/incrementAPI/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.incrementAPI} package declares types that can return the increment of a value.
+ * 
+ * @author Silvan Wyss
+ * @month 2020-08
+ * @lines 10
+ */
+package ch.nolix.common.incrementAPI;
