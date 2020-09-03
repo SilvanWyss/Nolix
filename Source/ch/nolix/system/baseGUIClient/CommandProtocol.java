25092192efb4a61b2b3080cbd9221cf462588ba1
Heocro <silvan.wyss@gmail.com>
2020-09-03 14:42:36 +0200

    Edited CommandProtocol.

--- Source/ch/nolix/system/baseGUIClient/CommandProtocol.java
+++ Source/ch/nolix/system/baseGUIClient/CommandProtocol.java
@@ -8,10 +8,10 @@ final class CommandProtocol {
 	public static final String GET_FILE = "GetFile";
 	public static final String GET_TEXT_FROM_CLIPBOARD = "GetTextFromClipboard";
 	public static final String NOTE_INPUT = "NoteInput";
-	public static final String RECEIVE_FILE = "ReceiveFile";
+	public static final String RECEIVE_FILE = "ReceiveFile"; //used for web Clients only
 	public static final String RESET = "Reset";
 	public static final String SAVE_FILE = "SaveFile";
-	public static final String SEND_FILE = "SendFile";
+	public static final String SEND_FILE = "SendFile"; //used for web Clients only
 	public static final String SET_CURSOR_ICON = "SetCursorIcon";
 	public static final String SET_PAINT_COMMANDS = "SetPaintCommands";
 	public static final String SET_TITLE = "SetTitle";
