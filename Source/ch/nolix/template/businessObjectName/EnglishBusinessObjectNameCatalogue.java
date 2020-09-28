2fdc8a1120168602fcaf5e5bc9f7708655b8a2de
Silvan Wyss <silvan.wyss@gmail.com>
2020-09-28 22:53:55 +0200

    Created EnglishBusinessObjectNameCatalogue.

---
+++ Source/ch/nolix/template/businessObjectName/EnglishBusinessObjectNameCatalogue.java
@@ -0,0 +1,48 @@
+//package info
+package ch.nolix.template.businessObjectName;
+
+//class
+public final class EnglishBusinessObjectNameCatalogue {
+	
+	//constants
+	public static final String ACCOUNT = "account";
+	public static final String ADDRESS = "address";
+	public static final String ADDRESSEE = "addressee";
+	public static final String BANK = "bank";
+	public static final String BANK_ACCOUNT = "bank account";
+	public static final String BUILDING_PROJECT = "building project";
+	public static final String BUSINESS_CASE = "business case";
+	public static final String CLAIM_CASE = "claim case";
+	public static final String CONTRACT = "contract";
+	public static final String CONTRIBUTOR = "contributor";
+	public static final String COVER = "cover";
+	public static final String CREDITOR = "creditor";
+	public static final String CUSTOMER = "customer";
+	public static final String DEBTOR = "debtor";
+	public static final String DEPARTMENT = "department";
+	public static final String EDITOR = "editor";
+	public static final String GROUP = "group";
+	public static final String MILESTONE = "milestone";
+	public static final String NAME = "Name";
+	public static final String OFFER = "offer";
+	public static final String ORDER = "order";
+	public static final String ORGANISATION = "organisation";
+	public static final String PARTICIPANT = "participant";
+	public static final String PARTNER = "partner";
+	public static final String PAYMENT = "payment";
+	public static final String PERSON = "person";
+	public static final String PHASE  ="phase";
+	public static final String PREPAYMENT = "prepayment";
+	public static final String PRODUCT = "product";
+	public static final String PROJECT = "project";
+	public static final String RECEPTOR = "receptor";
+	public static final String SENDER = "sender";
+	public static final String SITE = "site";
+	public static final String STORAGE_LOCATION = "storage location";
+	public static final String SUPPLIER = "supplier";
+	public static final String SURNAME = "surname";
+	public static final String WARE_HOUSE = "ware house";
+	
+	//visibility-reducing constructor
+	private EnglishBusinessObjectNameCatalogue() {}
+}
