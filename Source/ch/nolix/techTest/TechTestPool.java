b498108f53eac28900d5a08e70ec7aad1c1a5841
Silvan Wyss <silvan.wyss@gmail.com>
2020-10-01 22:21:09 +0200

    Edited TechTestPool.

--- Source/ch/nolix/techTest/TechTestPool.java
+++ Source/ch/nolix/techTest/TechTestPool.java
@@ -4,12 +4,13 @@ package ch.nolix.techTest;
 //own imports
 import ch.nolix.common.baseTest.TestPool;
 import ch.nolix.techTest.genericMathTest.GenericMathTestPool;
+import ch.nolix.techTest.projectTest.ProjectTestPool;
 
 //class
 public final class TechTestPool extends TestPool {
 	
 	//constructor
 	public TechTestPool() {
-		super(new GenericMathTestPool());
+		super(new GenericMathTestPool(), new ProjectTestPool());
 	}
 }
