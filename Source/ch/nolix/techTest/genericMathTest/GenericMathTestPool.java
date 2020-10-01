744e5b77ccc58e8347b3eaa53568f6813cb69a7f
Silvan Wyss <silvan.wyss@gmail.com>
2020-10-01 22:05:07 +0200

    Edited GenericMathTestPool.

--- Source/ch/nolix/techTest/genericMathTest/GenericMathTestPool.java
+++ Source/ch/nolix/techTest/genericMathTest/GenericMathTestPool.java
@@ -9,9 +9,6 @@ public final class GenericMathTestPool extends TestPool {
 	
 	//constructor
 	public GenericMathTestPool() {
-		super(
-			ClosedIntervalTest.class,
-			ComplexNumberTest.class	
-		);
+		super(ClosedIntervalTest.class,	ComplexNumberTest.class);
 	}
 }
