fc0f223a59d3d31cc1c0f1ce2dc2513d6bb2d104
Silvan Wyss <silvan.wyss@gmail.com>
2020-10-03 22:44:29 +0200

    Created language package.

---
+++ Source/ch/nolix/common/language/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.language} package defines helpers for languages.
+ * 
+ * @author Silvan Wyss
+ * @date 2020-10-03
+ * @lines 10
+ */
+package ch.nolix.common.language;
