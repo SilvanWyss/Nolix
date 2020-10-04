901493951f05cb6a8d9bde5a9cc28a2f7448164a
Silvan Wyss <silvan.wyss@gmail.com>
2020-10-04 17:46:20 +0200

    Edited description of package-info.

--- Source/ch/nolix/elementTutorial/widgetTutorial/resource/package-info.java
+++ Source/ch/nolix/elementTutorial/widgetTutorial/resource/package-info.java
@@ -1,7 +1,7 @@
 //package info
 /**
  * The {@link ch.nolix.elementTutorial.widgetTutorial.resource} package
- * contains the resources of the {@link ch.nolix.elementTutorial.widgetTutorial} package.
+ * contains the resources for the {@link ch.nolix.elementTutorial.widgetTutorial} package.
  * 
  * @author Silvan Wyss
  * @month 2020-04
