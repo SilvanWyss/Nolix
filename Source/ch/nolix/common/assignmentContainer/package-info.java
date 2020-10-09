547647bb819eac87c893623420724abd1b4c3b89
Silvan Wyss <silvan.wyss@gmail.com>
2020-10-09 15:50:38 +0200

    Deleted assignmentContainer package.

--- Source/ch/nolix/common/assignmentContainer/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.common.assignmentContainer} package defines containers that assign elements.
- * 
- * @author Silvan Wyss
- * @month 2020-06
- * @lines 10
- */
-package ch.nolix.common.assignmentContainer;
