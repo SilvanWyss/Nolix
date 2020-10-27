1eb197586c608561a52c82384b705d0314fa1e93
Silvan Wyss <silvan.wyss@gmail.com>
2020-10-27 22:17:16 +0100

    Refactored AnthrazitGUILook.

--- Source/ch/nolix/template/GUILook/AnthrazitGUILook.java
+++ Source/ch/nolix/template/GUILook/AnthrazitGUILook.java
@@ -3,12 +3,258 @@ package ch.nolix.template.GUILook;
 
 //own imports
 import ch.nolix.element.configuration.DeepConfiguration;
-import ch.nolix.element.configuration.Configuration;
 import ch.nolix.element.containerWidget.ContainerRole;
+import ch.nolix.element.containerWidget.Grid;
+import ch.nolix.element.GUI.LayerRole;
+import ch.nolix.element.GUI.Widget;
+import ch.nolix.element.color.Color;
+import ch.nolix.element.configuration.Configuration;
+import ch.nolix.element.widget.BorderWidget;
+import ch.nolix.element.widget.Button;
+import ch.nolix.element.widget.Checkbox;
+import ch.nolix.element.widget.Console;
+import ch.nolix.element.widget.Downloader;
 import ch.nolix.element.widget.LabelRole;
+import ch.nolix.element.widget.TextItemMenu;
 
 //class
 public final class AnthrazitGUILook extends Configuration {
+	
+	//nested class
+	private static final class BorderWidgetLook extends DeepConfiguration {
+		
+		//constructor
+		public BorderWidgetLook() {
+			
+			setSelectorType(BorderWidget.class);
+			
+			addAttachingAttribute(
+				"BaseBaseScrollbarLook(ScrollbarColor(0x101010),ScrollbarCursorColor(0x000000))",
+				"BaseHoverScrollbarLook(ScrollbarColor(0x101010),ScrollbarCursorColor(0xF0F0F0))",
+				"BaseSelectionScrollbarLook(ScrollbarColor(0x101010),ScrollbarCursorColor(0xF0F0F0))"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class ButtonLook extends DeepConfiguration {
+		
+		//constructor
+		public ButtonLook() {
+			
+			setSelectorType(Button.class);
+			
+			addAttachingAttribute(
+				"MinWidth(200)",
+				"CursorIcon(Hand)",
+				"ContentPosition(Center)",
+				"BaseLeftPadding(5)",
+				"BaseRightPadding(5)",
+				"BaseBackgroundColor(0x101010)",
+				"HoverBackgroundColor(0xF0F0F0)",
+				"HoverTextColor(0x000000)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class CheckboxLook extends DeepConfiguration {
+		
+		//constructor
+		public CheckboxLook() {
+			
+			setSelectorType(Checkbox.class);
+			
+			addAttachingAttribute("CursorIcon(Hand)", "BaseBackgroundColor(0x101010)");
+		}
+	}
+	
+	//nested class
+	private static final class ConsoleLook extends DeepConfiguration {
+		
+		//constructor
+		public ConsoleLook() {
+			
+			setSelectorType(Console.class);
+			
+			addAttachingAttribute("BaseBackgroundColor(0x101010)");
+		}
+	}
+	
+	//nested class
+	private static final class DialogLook extends DeepConfiguration {
+		
+		//constructor
+		public DialogLook() {
+			
+			addSelectorRole(LayerRole.DIALOG_LAYER);
+			
+			addAttachingAttribute(
+				"BackgroundColor(" + new Color("0x101010F0").getHexadecimalValue() + ")"
+			);
+			
+			addConfiguration(new DialogContainerLook());
+		}
+	}
+	
+	private static final class DialogContainerLook extends DeepConfiguration {
+		
+		//constructor
+		public DialogContainerLook() {
+			
+			addSelectorRole(ContainerRole.DialogContainer);
+			
+			addAttachingAttribute(
+				"BaseBackgroundColor(0x202020)",
+				"BasePadding(50)",
+				"ContentPosition(Top)",
+				"ElementMargin(100)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class DownoaderLook extends DeepConfiguration {
+		
+		//constructor
+		public DownoaderLook() {
+			
+			setSelectorType(Downloader.class);
+			
+			addAttachingAttribute(
+				"MinWidth(200)",
+				"CursorIcon(Hand)",
+				"ContentPosition(Center)",
+				"BaseLeftPadding(5)",
+				"BaseRightPadding(5)",
+				"BaseBackgroundColor(0x101010)",
+				"HoverBackgroundColor(0xF0F0F0)",
+				"HoverTextColor(0x000000)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class GridLook extends DeepConfiguration {
+		
+		//constructor
+		public GridLook() {
+			
+			setSelectorType(Grid.class);
+			
+			addAttachingAttribute("BaseElementMargin(10)");
+		}
+	}
+	
+	//nested class
+	private static final class Level1HeaderLook extends DeepConfiguration {
+		
+		//constructor
+		public Level1HeaderLook() {
+			
+			addSelectorRole(LabelRole.Level1Header);
+			
+			addAttachingAttribute(
+				"BaseTextSize(30)",
+				"BaseBottomPadding(10)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class Level2HeaderLook extends DeepConfiguration {
+		
+		//constructor
+		public Level2HeaderLook() {
+			
+			addSelectorRole(LabelRole.Level1Header);
+			
+			addAttachingAttribute(
+				"BaseTextSize(20)",
+				"BaseBottomPadding(10)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class OverallContainerLook extends DeepConfiguration {
+		
+		public OverallContainerLook() {
+			
+			addSelectorRole(ContainerRole.OverallContainer);
+			
+			addAttachingAttribute(
+				"ProposalWidth(1000)",
+				"ProposalHeight(600)",
+				"BaseBackgroundColor(0x202020)",
+				"BasePadding(10)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class TextBoxLook extends DeepConfiguration {
+		
+		//constructor
+		public TextBoxLook() {
+			
+			setSelectorType("TextBox");
+			
+			addAttachingAttribute(
+				"BaseBackgroundColor(0x101010)",
+				"BasePadding(5)"
+			);
+		}
+	}
+		
+	//nested class
+	private static final class TextItemMenuLook extends DeepConfiguration {
+		
+		//constructor
+		public TextItemMenuLook() {
+			
+			setSelectorType(TextItemMenu.class);
+			
+			addAttachingAttribute(
+				"MinWidth(100)",
+				"MaxHeight(200)",
+				"BaseBackgroundColor(0x101010)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class TitleLook extends DeepConfiguration {
+		
+		//constructor
+		public TitleLook() {
+			
+			addSelectorRole(LabelRole.Title);
+			
+			addAttachingAttribute(
+				"BaseTopPadding(20)",
+				"BaseBottomPadding(50)",
+				"BaseTextSize(40)",
+				"BaseTextColor(0xC0C0C0)"
+			);
+		}
+	}
+	
+	//nested class
+	private static final class WidgetLook extends DeepConfiguration {
+		
+		//constructor
+		public WidgetLook() {
+			
+			setSelectorType(Widget.class);
+			
+			addAttachingAttribute(
+				"BaseFont(Verdana)",
+				"BaseTextSize(20)",
+				"BaseTextColor(0xF0F0F0)"
+			);
+		}
+	}
 	
 	//constructor
 	public AnthrazitGUILook() {
@@ -16,70 +262,20 @@ public final class AnthrazitGUILook extends Configuration {
 		addAttachingAttribute("BackgroundColor(0x101010)");
 		
 		addConfiguration(
-			new AnthrazitDialogLook(),
-			new DeepConfiguration()
-			.setSelectorType("Widget")
-			.addAttachingAttribute(
-				"BaseFont(Arial)",
-				"BaseTextSize(20)",
-				"BaseTextColor(0x808080)"
-			),
-			new DeepConfiguration()
-			.addSelectorRole(ContainerRole.OverallContainer)
-			.addAttachingAttribute(
-				"MinWidth(800)",
-				"BaseBackgroundColor(0x202020)",
-				"BasePadding(10)"
-			),
-			new DeepConfiguration()
-			.addSelectorRole(ContainerRole.MainContainer)
-			.addAttachingAttribute("MinHeight(500)", "MaxHeight(600)")
-			.addConfiguration(
-				new DeepConfiguration()
-				.addAttachingAttribute("BaseTextSize(15)")
-			),
-			new DeepConfiguration()
-			.setSelectorType("Grid")
-			.addAttachingAttribute("BaseElementMargin(10)"),
-			new DeepConfiguration()
-			.addSelectorRole(LabelRole.Title)
-			.addAttachingAttribute(
-				"BaseTextSize(40)",
-				"BaseBottomPadding(10)"
-			),
-			new DeepConfiguration()
-			.addSelectorRole(LabelRole.Level1Header)
-			.addAttachingAttribute(
-				"BaseTextSize(30)",
-				"BaseBottomPadding(10)"
-			),
-			new DeepConfiguration()
-			.addSelectorRole(LabelRole.Level2Header)
-			.addAttachingAttribute(
-				"BaseTextSize(20)",
-				"BaseBottomPadding(10)"
-			),
-			new DeepConfiguration()
-			.setSelectorType("Button")
-			.addAttachingAttribute(
-				"MinWidth(50)",
-				"CursorIcon(Hand)",
-				"ContentPosition(Center)",
-				"BaseLeftPadding(5)",
-				"BaseRightPadding(5)",
-				"BaseTextColor(0x0000C0)",
-				"HoverTextColor(0x0040FF)"		
-			),
-			new DeepConfiguration()
-			.setSelectorType("TextBox")
-			.addAttachingAttribute("BaseBackgroundColor(0xE0E0E0)"),
-			new DeepConfiguration()
-			.setSelectorType("SelectionMenu")
-			.addAttachingAttribute(
-				"MinWidth(100)",
-				"MaxHeight(200)",
-				"BaseBackgroundColor(0xE0E0E0)"
-			)
+			new WidgetLook(),
+			new BorderWidgetLook(),
+			new OverallContainerLook(),
+			new DialogLook(),
+			new GridLook(),
+			new TitleLook(),
+			new Level1HeaderLook(),
+			new Level2HeaderLook(),
+			new CheckboxLook(),
+			new ButtonLook(),
+			new DownoaderLook(),
+			new TextBoxLook(),
+			new TextItemMenuLook(),	
+			new ConsoleLook()
 		);
 		
 		freeze();
