9d260a62e4deff865939c135ad05398487a4c6d4
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-07 14:04:54 +0100

    Created Copyable.

---
+++ Source/ch/nolix/common/generalSkillAPI/Copyable.java
@@ -0,0 +1,19 @@
+//package declaration
+package ch.nolix.common.generalSkillAPI;
+
+//interface
+/**
+* A {@link Copyable} can create a copy of itself.
+* 
+* @author Silvan Wyss
+* @date 2020-11-07
+* @lines 10
+*/
+public interface Copyable<C extends Copyable<C>> {
+	
+	//method declaration
+	/**
+	 * @return a new copy of the current {@link Copyable}.
+	 */
+	public abstract C getCopy();
+}
