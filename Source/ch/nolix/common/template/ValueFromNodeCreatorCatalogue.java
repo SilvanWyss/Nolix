ef8780d7711bad1d32cf4efbaed275dc8f1217bd
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-21 15:29:44 +0100

    Beautified ValueFromNodeCreatorCatalogue.

--- Source/ch/nolix/common/template/ValueFromNodeCreatorCatalogue.java
+++ Source/ch/nolix/common/template/ValueFromNodeCreatorCatalogue.java
@@ -22,15 +22,15 @@ public final class ValueFromNodeCreatorCatalogue {
 	
 	//constant
 	public static final SpecificValueCreator<BaseNode, Boolean> BOOLEAN_CREATOR =
-	new SpecificValueCreator<>(Boolean.class, s -> s.toBoolean());
+	new SpecificValueCreator<>(Boolean.class, BaseNode::toBoolean);
 	
 	//constant
 	public static final SpecificValueCreator<BaseNode, Integer> INTEGER_CREATOR =
-	new SpecificValueCreator<>(Integer.class, s -> s.toInt());
+	new SpecificValueCreator<>(Integer.class, BaseNode::toInt);
 	
 	//constant
 	public static final SpecificValueCreator<BaseNode, String> STRING_CREATOR =
-	new SpecificValueCreator<>(String.class, s -> s.getHeader());
+	new SpecificValueCreator<>(String.class, BaseNode::getHeader);
 	
 	//visibility-reducing constructor
 	private ValueFromNodeCreatorCatalogue() {}
