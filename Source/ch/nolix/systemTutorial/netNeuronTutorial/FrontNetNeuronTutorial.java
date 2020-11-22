934941b0e2a8a3deefe1e75a32507fb515a7a677
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-22 19:43:21 +0100

    Edited FrontNetNeuronTutorial.

--- Source/ch/nolix/systemTutorial/netNeuronTutorial/FrontNetNeuronTutorial.java
+++ Source/ch/nolix/systemTutorial/netNeuronTutorial/FrontNetNeuronTutorial.java
@@ -7,47 +7,46 @@ import ch.nolix.system.netNeuron.BackNetNeuron;
 
 //class
 /**
-* The {@link FrontNetNeuronTutorial} is a tutorial for {FrontNetNeuron}.
+* The {@link FrontNetNeuronTutorial} is a tutorial for {FrontNetNeuron}s.
 * 
 * @author Silvan Wyss
-* @month 2017-01
-* @lines 60
+* @date 2017-01-22
+* @lines 50
 */
 public final class FrontNetNeuronTutorial {
 	
 	/**
-	 * 1. Creates a back net neuron and adds an input neuron to it.
-	 * 3. Creates front net neurons that will connect to the back net neuron.
-	 * 4. Fires the back net neuron.
-	 * 5. Prints out the outputs of the front net neurons to the console.
-	 * 6. Closes the back net neuron and the front net neurons.
+	 * 1. Creates a {@link BackNetNeuron} and adds an {@link InputNeuron} to it.
+	 * 3. Creates some {@link FrontNetNeuron} that will connect to the back net neuron.
+	 * 4. Fires the {@link BackNetNeuron}.
+	 * 5. Prints out the outputs of the {@link FrontNetNeuron}s to the console.
+	 * 6. Closes the {@link BackNetNeuron} and the {@link FrontNetNeuron}s.
 	 * 
 	 * @param arguments
 	 */
 	public static final void main(String[] arguments) {
 		
-		//Defines the port for the back net neuron.
+		//Defines the port for the BackNetNeuron.
 		final var port = 20000;
 		
-		//Creates a back net neuron.
-		final var netBackNeuron =
-		new BackNetNeuron<String>(port, s -> Node.fromString(s));
+		//Creates a BackNeNeuron.
+		final var netBackNeuron = new BackNetNeuron<String>(port, Node::fromString);
 		
-		//Creates and adds an input neuron to the back net neuron.
+		//Creates and adds an InpuNeuron to the BackNetNeuron.
 		netBackNeuron.addInputNeuron(new SourceNeuron<String>("Hello_World!"));
 		
-		//Creates front net neurons that will connect to the back net neuron.
-		final var frontNetNeuron1 = new FrontNetNeuron<String>(port, s -> s.toString());
-		final var frontNetNeuron2 = new FrontNetNeuron<String>(port, s -> s.toString());
+		//Creates some FrontNetNeuron that will connect to the BackNetNeuron.
+		final var frontNetNeuron1 = new FrontNetNeuron<String>(port, Node::toString);
+		final var frontNetNeuron2 = new FrontNetNeuron<String>(port, Node::toString);
 		
-		//Fires the back net neuron.
+		//Fires the BackNetNeuron.
 		netBackNeuron.fire();
 		
-		//Prints out the output of the front net neurons to the console.
+		//Prints out the output of the FrontNetNeurons to the console.
 		System.out.println("front net neuron 1 output: " + frontNetNeuron1.getRefOutput());
 		System.out.println("front net neuron 2 output: " + frontNetNeuron2.getRefOutput());
 		
-		//Closes the back net neuron and the fron net neurons.
+		//Closes the BackNetNeuron and the FrontNetNeurons.
 		netBackNeuron.close();
 		frontNetNeuron1.close();
 		frontNetNeuron2.close();
