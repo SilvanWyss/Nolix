3c28882870f1833c30121a7ece34bb07e4ea7fcd
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-28 10:08:54 +0100

    Beautified AfterAllMediator.

--- Source/ch/nolix/common/sequencer/AfterAllMediator.java
+++ Source/ch/nolix/common/sequencer/AfterAllMediator.java
@@ -10,40 +10,34 @@ import ch.nolix.common.validator.Validator;
 
 //class
 /**
- * An after all mediator is not mutable.
+ * A {@link AfterAllMediator} is not mutable.
  * 
  * @author Silvan Wyss
- * @month 2017-05
- * @lines 200
+ * @month 2017-01-01
+ * @lines 220
  */
 public final class AfterAllMediator {
 
-	//attributes
-	final int timeIntervalInMilliseconds;
+	//attribute
+	private final int timeIntervalInMilliseconds;
 	
 	//optional attributes
 	private final Integer maxRunCount;
 	private final IBooleanGetter condition;
 	
-	//constructor
+	//visibility-reduced constructor
 	/**
-	 * Creates a new after all mediator with the given condition and time interval in milliseconds.
+	 * Creates a new {@link AfterAllMediator} with the given condition and time interval in milliseconds.
 	 * 
 	 * @param condition
 	 * @param timeIntervalInMilliseconds
 	 * @throws ArgumentIsNullException if the given condition is null.
-	 * @throws NegativeArgumentException if the given time interval in milliseconds is negative.
+	 * @throws NegativeArgumentException if the given timeIntervalInMilliseconds is negative.
 	 */
 	AfterAllMediator(final IBooleanGetter condition, final int timeIntervalInMilliseconds) {
 		
-		//Asserts that the given condition is not null.
-		Validator.assertThat(condition).thatIsNamed("condition").isNotNull();
-		
-		//Asserts that the given time interval in milliseconds is not negative.
-		Validator
-		.assertThat(timeIntervalInMilliseconds)
-		.thatIsNamed("time interval in milliseconds")
-		.isNotNegative();
+		Validator.assertThat(condition).thatIsNamed(VariableNameCatalogue.CONDITION).isNotNull();
+		Validator.assertThat(timeIntervalInMilliseconds).thatIsNamed("time interval in milliseconds").isNotNegative();
 		
 		maxRunCount = null;
 		this.condition = condition;
@@ -52,31 +46,24 @@ public final class AfterAllMediator {
 	
 	//constructor
 	/**
-	 * Creates a new after all mediator with the given max run count, condition and time interval in milliseconds.
+	 * Creates a new {@link AfterAllMediator} with the given maxRunCount, condition and time interval in milliseconds.
 	 * 
 	 * @param maxRunCount
 	 * @param condition
 	 * @param timeIntervalInMilliseconds
 	 * @throws NegativeArgumentException if the given max run count is negative.
 	 * @throws ArgumentIsNullException if the given condition is null.
-	 * @throws NegativeArgumentException if the given time interval in milliseconds is negative.
+	 * @throws NegativeArgumentException if the given timeIntervalInMillisecondss is negative.
 	 */
 	AfterAllMediator(
 		final int maxRunCount,
 		final IBooleanGetter condition,
 		final int timeIntervalInMilliseconds
 	) {
-		//Asserts that the given max run count is not negative.
+		
 		Validator.assertThat(maxRunCount).thatIsNamed("max run count").isNotNegative();
-		
-		//Asserts that the given condition is not null.
 		Validator.assertThat(condition).thatIsNamed("condition").isNotNull();
-		
-		//Asserts that the given time interval in milliseconds is not negative.
-		Validator
-		.assertThat(timeIntervalInMilliseconds)
-		.thatIsNamed("time interval in milliseconds")
-		.isNotNegative();
+		Validator.assertThat(timeIntervalInMilliseconds).thatIsNamed("time interval in milliseconds").isNotNegative();
 		
 		this.maxRunCount = maxRunCount;
 		this.condition = condition;
@@ -85,58 +72,55 @@ public final class AfterAllMediator {
 	
 	//method
 	/**
-	 * Lets this after all mediator run the given job.
+	 * Lets the current {@link AfterAllMediator} run the given job.
 	 * 
 	 * @param job
 	 */
 	public void run(final IAction job) {
 		
-		//Handles the case that this after all mediator does not have a max run count.
+		//Handles the case that the current AfterAllMediator does not have a max run count.
 		if (!hasMaxRunCount()) {
 			runWhenDoesNotHaveMaxRunCount(job);
 		}
 		
-		//Handles the case that this after all mediator has a max run count.
+		//Handles the case that the current AfterAllMediator has a max run count.
 		else {
 			runWhenHasMaxRunCount(job);
 		}
 	}
 	
+	//method
 	/**
-	 * Lets this after all mediator run the given job in background.
+	 * Lets the current {@link AfterAllMediator} run the given job in background.
 	 * 
 	 * @param job
-	 * @return a new future.
+	 * @return a new {@link Future}.
 	 * @throws ArgumentIsNullException if the given job is null.
 	 */
 	public Future runInBackground(final IAction job) {
 		
-		//Handles the case that this after all mediator does not have a max count.
+		//Handles the case that the current AfterAllMediator does not have a max count.
 		if (!hasMaxRunCount()) {
-			
-			//Handles the case that this after all mediator does not have a condition.
-			if (!hasCondition()) {
-				return new Future(new JobRunner(job, timeIntervalInMilliseconds, () -> true));
-			}
-			
-			//Handles the case that this after all mediator has a condition.
-			return new Future(new JobRunner(job, condition, timeIntervalInMilliseconds));
+			return runInBackgroundWhenDoesNotHaveMaxRunConunt(job);
 		}
 		
-		//Handles the case that this after all mediator has a max count.
-
-			//Handles the case that this after all mediator does not have a condition.
-			if (!hasCondition()) {
-				return new Future(new JobRunner(job, maxRunCount, timeIntervalInMilliseconds));
-			}
-			
-			//Handles the case that this after all mediator has a condition.
-			return new Future(new JobRunner(job, maxRunCount, condition, timeIntervalInMilliseconds));
+		//Handles the case that the current AfterAllMediator has a max count.
+		return runInBackgroundWhenHasMaxRunConunt(job);
 	}
 	
 	//method
 	/**
-	 * @return true if this after all mediator has a condition.
+	 * @throws ArgumentDoesNotHaveAttributeException if the current {@link AfterAllMediator} does not have condition.
+	 */
+	private void assertHasCondition() {
+		if (!hasCondition()) {
+			throw new ArgumentDoesNotHaveAttributeException(this, VariableNameCatalogue.CONDITION);
+		}
+	}
+	
+	//method
+	/**
+	 * @return true if the current {@link AfterAllMediator} has a condition.
 	 */
 	private boolean hasCondition() {
 		return (condition != null);
@@ -144,7 +128,7 @@ public final class AfterAllMediator {
 	
 	//method
 	/**
-	 * @return true if this after all mediator has a max run count.
+	 * @return true if the current {@link AfterAllMediator} has a max run count.
 	 */
 	private boolean hasMaxRunCount() {
 		return (maxRunCount != null);
@@ -152,16 +136,55 @@ public final class AfterAllMediator {
 	
 	//method
 	/**
-	 * Lets this after all mediator run the given job when this after all mediator does not have a run count.
+	 * Lets the current {@link AfterAllMediator} run the given job in background
+	 * for the case when the current {@link AfterAllMediator} does not have a max run count.
 	 * 
 	 * @param job
+	 * @return a new {@link Future}.
+	 * @throws ArgumentIsNullException if the given job is null.
+	 */
+	private Future runInBackgroundWhenDoesNotHaveMaxRunConunt(final IAction job) {
+		
+		//Handles the case that the current AfterAllMediator does not have a condition.
+		if (!hasCondition()) {
+			return new Future(new JobRunner(job, timeIntervalInMilliseconds, () -> true));
+		}
+		
+		//Handles the case that the current AfterAllMediator has a condition.
+		return new Future(new JobRunner(job, condition, timeIntervalInMilliseconds));
+	}
+	
+	//method
+	/**
+	 * Lets the current {@link AfterAllMediator} run the given job in background
+	 * for the case when the current {@link AfterAllMediator} has a max run count.
+	 * 
+	 * @param job
+	 * @return a new {@link Future}.
+	 * @throws ArgumentIsNullException if the given job is null.
+	 */
+	private Future runInBackgroundWhenHasMaxRunConunt(final IAction job) {
+		
+		//Handles the case that the current AfterAllMediator does not have a condition.
+		if (!hasCondition()) {
+			return new Future(new JobRunner(job, maxRunCount, timeIntervalInMilliseconds));
+		}
+		
+		//Handles the case that the current AfterAllMediator has a condition.
+		return new Future(new JobRunner(job, maxRunCount, condition, timeIntervalInMilliseconds));
+	}
+	
+	//method
+	/**
+	 * Lets the current {@link AfterAllMediator} run the given job
+	 * for the case when the current {@link AfterAllMediator} does not have a max run count.
+	 * 
+	 * @param job
+	 * @throws ArgumentDoesNotHaveAttributeException if the current {@link AfterAllMediator} does not have condition.
 	 */
 	private void runWhenDoesNotHaveMaxRunCount(IAction job) {
-			
-		//Asserts that the current AfterAllMediator has a condition.
-		if (!hasCondition()) {
-			throw new ArgumentDoesNotHaveAttributeException(this, VariableNameCatalogue.CONDITION);
-		}
+		
+		assertHasCondition();
 		
 		while (condition.getOutput()) {
 			job.run();
@@ -171,13 +194,14 @@ public final class AfterAllMediator {
 	
 	//method
 	/**
-	 * Lets this after all mediator run the given job when this after all mediator has a run count.
+	 * Lets the current {@link AfterAllMediator} run the given job
+	 * for the case when the current {@link AfterAllMediator} has a max run count.
 	 * 
 	 * @param job
 	 */
 	private void runWhenHasMaxRunCount(IAction job) {
 		
-		//Handles the case that this after all mediator does not have a condition.
+		//Handles the case that the current AfterAllMediator does not have a condition.
 		if (!hasCondition()) {
 			for (int i = 1; i <= maxRunCount; i++) {
 				job.run();
@@ -185,7 +209,7 @@ public final class AfterAllMediator {
 			}
 		}
 		
-		//Handles the case that this after all mediator has a condition.
+		//Handles the case that the current AfterAllMediator has a condition.
 		else {
 			for (int i = 1; i <= maxRunCount; i++) {
 				
