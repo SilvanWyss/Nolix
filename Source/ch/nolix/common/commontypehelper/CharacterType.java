275bf0968e3acb603989d20117c4eb6b522282fa
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-28 22:13:49 +0100

    Added fromCharacter static method to CharacterType.

--- Source/ch/nolix/common/commontypehelper/CharacterType.java
+++ Source/ch/nolix/common/commontypehelper/CharacterType.java
@@ -6,5 +6,74 @@ enum CharacterType {
 	UPPER_CASE_LETTER,
 	LOWER_CASE_LETTER,
 	UNDERSCORE,
-	OTHER
+	OTHER;
+	
+	//static method
+	public static CharacterType fromCharacter(final char character) {
+		switch (character) {
+			case 'A':
+			case 'B':
+			case 'C':
+			case 'D':
+			case 'E':
+			case 'F':
+			case 'G':
+			case 'H':
+			case 'I':
+			case 'J':
+			case 'K':
+			case 'L':
+			case 'M':
+			case 'N':
+			case 'O':
+			case 'P':
+			case 'Q':
+			case 'R':
+			case 'S':
+			case 'T':
+			case 'U':
+			case 'V':
+			case 'W':
+			case 'X':
+			case 'Z':
+			case 'Ä':
+			case 'Ö':
+			case 'Ü':
+				return CharacterType.UPPER_CASE_LETTER;
+			case 'a':
+			case 'b':
+			case 'c':
+			case 'd':
+			case 'e':
+			case 'f':
+			case 'g':
+			case 'h':
+			case 'i':
+			case 'j':
+			case 'k':
+			case 'l':
+			case 'm':
+			case 'n':
+			case 'o':
+			case 'p':
+			case 'q':
+			case 'r':
+			case 's':
+			case 't':
+			case 'u':
+			case 'v':
+			case 'w':
+			case 'x':
+			case 'y':
+			case 'z':
+			case 'ä':
+			case 'ö':
+			case 'ü':
+				return CharacterType.LOWER_CASE_LETTER;
+			case '_':
+				return CharacterType.UNDERSCORE;
+			default:
+				return CharacterType.OTHER;
+		}
+	}
 }
--- Source/ch/nolix/common/commontypehelper/PascalCaseCreator.java
+++ Source/ch/nolix/common/commontypehelper/PascalCaseCreator.java
@@ -18,7 +18,7 @@ final class PascalCaseCreator {
 		final var stringBuilder = new StringBuilder();
 		
 		final var firstCharacter = string.charAt(0);
-		final var firstCharacterType = getCharacterType(firstCharacter);
+		final var firstCharacterType = CharacterType.fromCharacter(firstCharacter);
 		switch (firstCharacterType) {
 			case LOWER_CASE_LETTER:
 				stringBuilder.append(Character.toUpperCase(firstCharacter));
@@ -31,7 +31,7 @@ final class PascalCaseCreator {
 		for (var i = 1; i < string.length(); i++) {
 			
 			final var character = string.charAt(i);
-			final var characterType = getCharacterType(character);
+			final var characterType = CharacterType.fromCharacter(character);
 			
 			switch (characterType) {
 				case UPPER_CASE_LETTER:
@@ -64,74 +64,5 @@ final class PascalCaseCreator {
 		}
 		
 		return stringBuilder.toString();
-	}
-	
-	//method
-	private static CharacterType getCharacterType(final char character) {
-		switch (character) {
-			case 'A':
-			case 'B':
-			case 'C':
-			case 'D':
-			case 'E':
-			case 'F':
-			case 'G':
-			case 'H':
-			case 'I':
-			case 'J':
-			case 'K':
-			case 'L':
-			case 'M':
-			case 'N':
-			case 'O':
-			case 'P':
-			case 'Q':
-			case 'R':
-			case 'S':
-			case 'T':
-			case 'U':
-			case 'V':
-			case 'W':
-			case 'X':
-			case 'Z':
-			case 'Ä':
-			case 'Ö':
-			case 'Ü':
-				return CharacterType.UPPER_CASE_LETTER;
-			case 'a':
-			case 'b':
-			case 'c':
-			case 'd':
-			case 'e':
-			case 'f':
-			case 'g':
-			case 'h':
-			case 'i':
-			case 'j':
-			case 'k':
-			case 'l':
-			case 'm':
-			case 'n':
-			case 'o':
-			case 'p':
-			case 'q':
-			case 'r':
-			case 's':
-			case 't':
-			case 'u':
-			case 'v':
-			case 'w':
-			case 'x':
-			case 'y':
-			case 'z':
-			case 'ä':
-			case 'ö':
-			case 'ü':
-				return CharacterType.LOWER_CASE_LETTER;
-			case '_':
-				return CharacterType.UNDERSCORE;
-			default:
-				return CharacterType.OTHER;
-		}			
 	}
 }
