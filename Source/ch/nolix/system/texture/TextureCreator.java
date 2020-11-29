3b65fc01d95ef05a292b789896c1f5b4e3a3ffd9
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-29 13:32:31 +0100

    Edited TextureCreator.

--- Source/ch/nolix/system/texture/TextureCreator.java
+++ Source/ch/nolix/system/texture/TextureCreator.java
@@ -8,7 +8,7 @@ import ch.nolix.element.graphic.Image;
 public abstract class TextureCreator {
 	
 	//constant
-	private static final int TEXTURE_16x16 = 16;
+	private static final int TEXTURE_16_16 = 16;
 	
 	//method
 	public final Image createTexture(final int width, final int height) {
@@ -18,7 +18,7 @@ public abstract class TextureCreator {
 	//method
 	public final Image createTexture16x16() {
 		
-		final var texture = new Image(TEXTURE_16x16, TEXTURE_16x16);
+		final var texture = new Image(TEXTURE_16_16, TEXTURE_16_16);
 		fillTexture16x16(texture);
 		
 		return texture;
