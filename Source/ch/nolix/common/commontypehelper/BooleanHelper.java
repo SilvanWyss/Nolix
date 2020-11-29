58ef9b2c8f523db35152d05e252bc9870b278d10
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-29 19:19:34 +0100

    Edited BooleanHelper.

--- Source/ch/nolix/common/commontypehelper/BooleanHelper.java
+++ Source/ch/nolix/common/commontypehelper/BooleanHelper.java
@@ -1,12 +1,14 @@
 //package declaration
 package ch.nolix.common.commontypehelper;
 
+//own import
 import ch.nolix.common.constant.StringCatalogue;
 
 //class
 public final class BooleanHelper {
 	
 	//static method
+	@SuppressWarnings("all")
 	public static String toString(final boolean pBoolean) {
 		return (pBoolean ? StringCatalogue.TRUE_HEADER : StringCatalogue.FALSE_HEADER);
 	}
