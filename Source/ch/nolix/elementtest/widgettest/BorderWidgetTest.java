8fa914a1a4915c0c4c8a5dfa33e62fe97c21528e
Silvan Wyss <silvan.wyss@gmail.com>
2020-11-30 22:00:01 +0100

    Extended BorderWidgetTest.

--- Source/ch/nolix/elementtest/widgettest/BorderWidgetTest.java
+++ Source/ch/nolix/elementtest/widgettest/BorderWidgetTest.java
@@ -1,6 +1,7 @@
 //package declaration
 package ch.nolix.elementtest.widgettest;
 
+//own imports
 import ch.nolix.common.basetest.TestCase;
 import ch.nolix.common.invalidargumentexception.ArgumentIsNullException;
 import ch.nolix.common.invalidargumentexception.NonPositiveArgumentException;
@@ -9,65 +10,106 @@ import ch.nolix.element.widget.BorderWidget;
 
 //class
 /**
- * A {@link BorderWidgetTest} is a test for {@link BorderWidget}.
+ * A {@link BorderWidgetTest} is a test for {@link BorderWidget}s.
  * 
  * @author Silvan Wyss
- * @month 2018-12
- * @lines 90
- * @param <BW> The type of {@link BorderWidget} of a {@link BorderWidgetTest}.
+ * @date 2018-12-11
+ * @lines 150
+ * @param <BW> The type of the {@link BorderWidget}s a {@link BorderWidgetTest} is for.
  */
 public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends WidgetTest<BW> {
+	
+	//method
+	@TestCase
+	public final void testCase_getWidth_whenHasMinWidthThatIsBiggerThanTheWidth() {
+		
+		//setup part 1
+		final var testUnit = createTestUnit();
+		testUnit.recalculate();
+				
+		//setu part 2
+		final var targetMinWidth = testUnit.getWidth() + 100;
+		testUnit.setMinWidth(targetMinWidth);
+		testUnit.recalculate();
+		
+		//execution
+		final var result = testUnit.getWidth();
+		
+		//verification
+		expect(result).isEqualTo(targetMinWidth);
+	}
 	
 	//method
 	@TestCase
 	public final void testCase_removeMaxWidth() {
 		
 		//setup
-		final var borderWidget = createTestUnit();
-		borderWidget.setMaxWidth(500);
+		final var testUnit = createTestUnit();
+		testUnit.setMaxWidth(500);
 		
 		//setup verification
-		expect(borderWidget.hasMaxWidth());
+		expect(testUnit.hasMaxWidth());
 		
 		//execution
-		borderWidget.removeMaxWidtht();
+		testUnit.removeMaxWidtht();
 		
 		//verification
-		expectNot(borderWidget.hasMaxWidth());
-	}
-	
-	public final void testCase_setBaseBorderColors() {
-		
-		//setup
-		final var borderWidget = createTestUnit();
-		
-		//execution
-		borderWidget.getRefBaseLook().setBorderColors(Color.RED);
-		
-		//verification
-			expect(borderWidget.getRefBaseLook()
-			.getRecursiveOrDefaultLeftBorderColor())
-			.isEqualTo(Color.RED);
-			
-			expect(borderWidget.getRefBaseLook()
-			.getRecursiveOrDefaultRightBorderColor())
-			.isEqualTo(Color.RED);
-			
-			expect(borderWidget.getRefBaseLook()
-			.getRecursiveOrDefaultTopBorderColor())
-			.isEqualTo(Color.RED);
-			
-			expect(borderWidget.getRefBaseLook()
-			.getRecursiveOrDefaultBottomBorderColor())
-			.isEqualTo(Color.RED);
+		expectNot(testUnit.hasMaxWidth());
 	}
 	
 	//method
 	@TestCase
-	public final void testCase_setBaseBorderColors_borderColorIsNull() {
+	public final void testCase_resetBorderWidget() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		testUnit.setMinWidth(200);
+		testUnit.setMinHeight(100);
+		testUnit.setMaxWidth(500);
+		testUnit.setMaxHeight(200);
+		
+		//setup verification
+		expect(testUnit.hasMinWidth());
+		expect(testUnit.hasMinHeight());
+		expect(testUnit.hasMaxWidth());
+		expect(testUnit.hasMaxHeight());
+		
+		//execution
+		testUnit.reset();
+		
+		//verification
+		expectNot(testUnit.hasMinWidth());
+		expectNot(testUnit.hasMinHeight());
+		expectNot(testUnit.hasMaxWidth());
+		expectNot(testUnit.hasMaxHeight());
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_setBorderColorsOfBaseLook() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+				
+		//execution
+		testUnit.getRefBaseLook().setBorderColors(Color.RED);
+		
+		//verification
+		expect(testUnit.getRefBaseLook().getRecursiveOrDefaultLeftBorderColor()).isEqualTo(Color.RED);
+		expect(testUnit.getRefBaseLook().getRecursiveOrDefaultRightBorderColor()).isEqualTo(Color.RED);
+		expect(testUnit.getRefBaseLook().getRecursiveOrDefaultTopBorderColor()).isEqualTo(Color.RED);
+		expect(testUnit.getRefBaseLook().getRecursiveOrDefaultBottomBorderColor()).isEqualTo(Color.RED);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_setBorderColorsOfBaseLook_whenTheGivenBorderColorIsNull() {
+		
+		//setup
+		final var testUnit = createTestUnit();
 		
 		//execution & verification
-		expect(() -> createTestUnit().getRefBaseLook().setBorderColors(null))
+		expect(() -> testUnit.getRefBaseLook().setBorderColors(null))
 		.throwsException()
 		.ofType(ArgumentIsNullException.class)
 		.withMessage("The given border color is null.");
@@ -78,23 +120,40 @@ public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends W
 	public final void testCase_setMaxWidth() {
 		
 		//setup
-		final var borderWidget = createTestUnit();
+		final var testUnit = createTestUnit();
 		
 		//execution
-		borderWidget.setMaxWidth(500);
+		testUnit.setMaxWidth(500);
 		
 		//verification
-		expect(borderWidget.getMaxWidth()).isEqualTo(500);
+		expect(testUnit.getMaxWidth()).isEqualTo(500);
 	}
 	
 	//method
 	@TestCase
-	public final void testCase_setMaxWidth_maxWidthIsNegative() {
+	public final void testCase_setMaxWidth_whenTheGivenMaxWidthIsNegative() {
+		
+		//setup
+		final var testUnit = createTestUnit();
 		
 		//execution & verification
-		expect(() -> createTestUnit().setMaxWidth(-100))
+		expect(() -> testUnit.setMaxWidth(-100))
 		.throwsException()
 		.ofType(NonPositiveArgumentException.class)
 		.withMessage("The given max width '-100' is not positive.");
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_setMinWidth() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		
+		//execution
+		testUnit.setMinWidth(500);
+		
+		//verification
+		expect(testUnit.getMinWidth()).isEqualTo(500);
 	}
 }
