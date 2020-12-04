e5789a450a5bc56a44912b014bd84a9ab9714750
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-04 14:58:39 +0100

    Deleted testablegui package.

--- Source/ch/nolix/element/testablegui/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
-* The {@link ch.nolix.element.testablegui} package declares a testable GUI.
-* 
-* @author Silvan Wyss
-* @month 2020-01
-* @lines 10
-*/
-package ch.nolix.element.testablegui;
