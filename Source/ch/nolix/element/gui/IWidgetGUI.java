4a519742b6d9e16d81058bf51605a9a1f4d55e7d
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-13 20:05:38 +0100

    Created IWidgetGUI.

---
+++ Source/ch/nolix/element/gui/IWidgetGUI.java
@@ -0,0 +1,33 @@
+//package declaration
+package ch.nolix.element.gui;
+
+//own imports
+import ch.nolix.common.chainednode.ChainedNode;
+import ch.nolix.common.container.IContainer;
+import ch.nolix.common.skillapi.Clearable;
+import ch.nolix.element.baseguiapi.IBaseGUI;
+import ch.nolix.element.color.Color;
+import ch.nolix.element.configuration.Configuration;
+import ch.nolix.element.elementapi.IConfigurableElement;
+
+//interface
+public interface IWidgetGUI<WG extends IWidgetGUI<WG>> extends Clearable<WG>,  IBaseGUI<WG>, IConfigurableElement<WG>{
+	
+	//method declaration
+	WG addLayerOnTop(Layer layer);
+	
+	//method declaration
+	WG addLayerOnTop(Widget<?, ?> rootWidget);
+	
+	//method declaration
+	IContainer<ChainedNode> getPaintCommands();
+	
+	//method declaration
+	<W extends Widget<?, ?>> W getRefWidgetById(String id);
+	
+	//method declaration
+	WG setBackgroundColor(Color backgroundColor);
+	
+	//method declaration
+	WG setConfiguration(Configuration configuration);
+}
