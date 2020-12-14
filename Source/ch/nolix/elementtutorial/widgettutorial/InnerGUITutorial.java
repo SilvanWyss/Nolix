73ce0015c466725648ae4801429b8660d27f249f
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-14 21:58:57 +0100

    Created InnerGUITutorial.

---
+++ Source/ch/nolix/elementtutorial/widgettutorial/InnerGUITutorial.java
@@ -0,0 +1,46 @@
+package ch.nolix.elementtutorial.widgettutorial;
+
+import ch.nolix.element.color.Color;
+import ch.nolix.element.gui.Frame;
+import ch.nolix.element.widget.HorizontalStack;
+import ch.nolix.element.widget.InnerGUI;
+import ch.nolix.element.widget.Label;
+
+/**
+ * The {@link InnerGUITutorial} is a tutorial for {@link InnerGUI}s.
+ * Of the {@link InnerGUITutorial} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @date 2020-12-14
+ * @lines 40
+ */
+public final class InnerGUITutorial {
+	
+	/**
+	 * Creates a {@link Frame} with 2 {@link InnerGUI}s.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Frame()
+		.setTitle("InnerGUI Tutorial")
+		.addLayerOnTop(
+			new HorizontalStack()
+			.addWidget(
+				new InnerGUI()
+				.applyOnBaseLook(bl ->	bl.setBorderThicknesses(5).setBackgroundColor(Color.LAVENDER))
+				.setTitle("Inner GUI 1")
+				.addLayerOnTop(new Label().setText("A").applyOnBaseLook(bl -> bl.setTextSize(100))),
+				new InnerGUI()
+				.applyOnBaseLook(bl ->	bl.setBorderThicknesses(5).setBackgroundColor(Color.LAVENDER))
+				.setTitle("Inner GUI 2")
+				.addLayerOnTop(new Label().setText("B").applyOnBaseLook(bl -> bl.setTextSize(100)))
+			)
+		);
+	}
+	
+	/**
+	 * Avoids that an instance of the {@link InnerGUITutorial} can be created.
+	 */
+	private InnerGUITutorial() {}
+}
