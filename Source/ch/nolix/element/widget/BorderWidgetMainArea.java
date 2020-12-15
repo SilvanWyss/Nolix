90c82ca40344c4ce34fab1160c90952b03f1974b
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-15 20:45:54 +0100

    Extended BorderWidgetMainArea.

--- Source/ch/nolix/element/widget/BorderWidgetMainArea.java
+++ Source/ch/nolix/element/widget/BorderWidgetMainArea.java
@@ -3,16 +3,16 @@ package ch.nolix.element.widget;
 
 //own imports
 import ch.nolix.common.invalidargumentexception.ArgumentIsNullException;
-import ch.nolix.common.rasterapi.Rectangular;
 import ch.nolix.common.validator.Validator;
+import ch.nolix.element.baseguiapi.HoverableByCursor;
 
 //class
 /**
  * @author Silvan Wyss
  * @date 2019-06-26
- * @lines 50
+ * @lines 90
  */
-public final class BorderWidgetMainArea implements Rectangular {
+public final class BorderWidgetMainArea implements HoverableByCursor {
 	
 	//attribute
 	/**
@@ -29,9 +29,29 @@ public final class BorderWidgetMainArea implements Rectangular {
 	 */
 	public BorderWidgetMainArea(final BorderWidget<?, ?> parentBorderWidget) {
 		
+		//Asserts that the given parentBorderWidget is not null.
 		Validator.assertThat(parentBorderWidget).thatIsNamed("parent BorderWidget").isNotNull();
 		
+		//Sets the parentBorderWidget of the current BorderWidgetMainArea.
 		this.parentBorderWidget = parentBorderWidget;
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getCursorXPosition() {
+		return parentBorderWidget.getCursorXPosition();
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getCursorYPosition() {
+		return parentBorderWidget.getCursorXPosition();
 	}
 	
 	//method
@@ -50,5 +70,23 @@ public final class BorderWidgetMainArea implements Rectangular {
 	@Override
 	public int getWidth() {
 		return parentBorderWidget.getWidth();
+	}
+	
+	//method
+	/**
+	 * @return the x-position of the current {@link BorderWidgetMainArea} on the {@link BorderWidget} it belongs to.
+	 */
+	@Override
+	public int getXPosition() {
+		return 0;
+	}
+	
+	//method
+	/**
+	 * @return the y-position of the current {@link BorderWidgetMainArea} on the {@link BorderWidget} it belongs to.
+	 */
+	@Override
+	public int getYPosition() {
+		return 0;
 	}
 }
