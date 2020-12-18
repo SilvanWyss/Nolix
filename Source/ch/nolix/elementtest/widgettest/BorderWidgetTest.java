88c27ae6dcc6bb1b30c05a72c33d4ab8a4d3561b
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-18 20:46:10 +0100

    Extended BorderWidgetTest.

--- Source/ch/nolix/elementtest/widgettest/BorderWidgetTest.java
+++ Source/ch/nolix/elementtest/widgettest/BorderWidgetTest.java
@@ -14,10 +14,88 @@ import ch.nolix.element.widget.BorderWidget;
  * 
  * @author Silvan Wyss
  * @date 2018-12-11
- * @lines 150
- * @param <BW> The type of the {@link BorderWidget}s a {@link BorderWidgetTest} is for.
+ * @lines 340
+ * @param <BW> The type of the {@link BorderWidget}s of a {@link BorderWidgetTest}.
  */
 public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends WidgetTest<BW> {
+	
+	//method
+	@TestCase
+	public final void testCase_getHeight_whenHasTopBorder() {
+		
+		//parameter definition
+		final var topBorderThickness = 5;
+		
+		//setup
+		final var testUnit = createTestUnit();
+		testUnit.getRefBaseLook().setTopBorderThickness(topBorderThickness);
+		testUnit.recalculate();
+		
+		//execution
+		final var result = testUnit.getHeight();
+		
+		//execution
+		expect(result).isEqualTo(testUnit.getBorderedArea().getHeight() + topBorderThickness);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_getHeight_whenHasMinHeightThatIsBiggerThanTheHeight() {
+		
+		//setup part 1
+		final var testUnit = createTestUnit();
+		testUnit.recalculate();
+		
+		//setup part 2
+		final var targetMinHeight = testUnit.getHeight() + 100;
+		testUnit.setMinHeight(targetMinHeight);
+		testUnit.recalculate();
+		
+		//execution
+		final var result = testUnit.getHeight();
+		
+		//verification
+		expect(result).isEqualTo(targetMinHeight);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_getHeight_whenHasProposalHeightThatIsBiggerThanTheHeight() {
+		
+		//setup part 1
+		final var testUnit = createTestUnit();
+		testUnit.recalculate();
+		
+		//setup part 2
+		final var targetProposalHeight = testUnit.getHeight() + 100;
+		testUnit.setProposalHeight(targetProposalHeight);
+		testUnit.recalculate();
+				
+		//execution
+		final var result = testUnit.getHeight();
+		
+		//verification
+		expect(result).isEqualTo(targetProposalHeight);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_getWidth_whenHasLeftBorder() {
+		
+		//parameter definition
+		final var leftBorderThickenss = 5;
+		
+		//setup
+		final var testUnit = createTestUnit();
+		testUnit.getRefBaseLook().setLeftBorderThickness(leftBorderThickenss);
+		testUnit.recalculate();
+		
+		//execution
+		final var result = testUnit.getWidth();
+		
+		//verification
+		expect(result).isEqualTo(testUnit.getBorderedArea().getWidth() + leftBorderThickenss);
+	}
 	
 	//method
 	@TestCase
@@ -27,7 +105,7 @@ public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends W
 		final var testUnit = createTestUnit();
 		testUnit.recalculate();
 				
-		//setu part 2
+		//setup part 2
 		final var targetMinWidth = testUnit.getWidth() + 100;
 		testUnit.setMinWidth(targetMinWidth);
 		testUnit.recalculate();
@@ -37,6 +115,26 @@ public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends W
 		
 		//verification
 		expect(result).isEqualTo(targetMinWidth);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_getWidth_whenHasProposalWidthThatIsBiggerThanTheWidth() {
+		
+		//setup part 1
+		final var testUnit = createTestUnit();
+		testUnit.recalculate();
+		
+		//setup part 2
+		final var targetProposalWidth = testUnit.getWidth() + 100;
+		testUnit.setProposalWidth(targetProposalWidth);
+		testUnit.recalculate();
+				
+		//execution
+		final var result = testUnit.getWidth();
+		
+		//verification
+		expect(result).isEqualTo(targetProposalWidth);
 	}
 	
 	//method
@@ -59,20 +157,60 @@ public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends W
 	
 	//method
 	@TestCase
+	public final void testCase_removeMinWidth() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		testUnit.setMinWidth(500);
+		
+		//setup verification
+		expect(testUnit.hasMinWidth());
+		
+		//execution
+		testUnit.removeMinWidth();
+		
+		//verification
+		expectNot(testUnit.hasMinWidth());
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_removeProposalWidth() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		testUnit.setProposalWidth(500);
+		
+		//setup verification
+		expect(testUnit.hasProposalWidth());
+		
+		//execution
+		testUnit.removeProposalWidth();
+		
+		//verification
+		expectNot(testUnit.hasProposalWidth());
+	}
+	
+	//method
+	@TestCase
 	public final void testCase_resetBorderWidget() {
 		
 		//setup
 		final var testUnit = createTestUnit();
+		testUnit.setProposalWidth(200);
+		testUnit.setProposalHeight(100);
 		testUnit.setMinWidth(200);
 		testUnit.setMinHeight(100);
-		testUnit.setMaxWidth(500);
-		testUnit.setMaxHeight(200);
+		testUnit.setMaxWidth(200);
+		testUnit.setMaxHeight(100);
 		
 		//setup verification
 		expect(testUnit.hasMinWidth());
 		expect(testUnit.hasMinHeight());
 		expect(testUnit.hasMaxWidth());
 		expect(testUnit.hasMaxHeight());
+		expect(testUnit.hasProposalWidth());
+		expect(testUnit.hasProposalHeight());
 		
 		//execution
 		testUnit.reset();
@@ -82,6 +220,8 @@ public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends W
 		expectNot(testUnit.hasMinHeight());
 		expectNot(testUnit.hasMaxWidth());
 		expectNot(testUnit.hasMaxHeight());
+		expectNot(testUnit.hasProposalWidth());
+		expectNot(testUnit.hasProposalHeight());
 	}
 	
 	//method
@@ -155,5 +295,47 @@ public abstract class BorderWidgetTest<BW extends BorderWidget<BW, ?>> extends W
 		
 		//verification
 		expect(testUnit.getMinWidth()).isEqualTo(500);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_setMinWidth_whenTheGivenMinWidthIsNegative() {
+
+		//setup
+		final var testUnit = createTestUnit();
+		
+		//execution
+		testUnit.setMinWidth(-100);
+		
+		//verification
+		expect(testUnit.getMinWidth()).isEqualTo(-100);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_setProposalWidth() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		
+		//execution
+		testUnit.setProposalWidth(500);
+		
+		//verification
+		expect(testUnit.getProposalWidth()).isEqualTo(500);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_setProposalWidth_whenTheGivenProposalWidthIsNegative() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		
+		//execution & verification
+		expect(() -> testUnit.setProposalWidth(-100))
+		.throwsException()
+		.ofType(NonPositiveArgumentException.class)
+		.withMessage("The given proposal width '-100' is not positive.");
 	}
 }
