f73ebabe3b6032e5e6fa41ef70abaf6acb7cc27b
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-20 14:02:55 +0100

    Created WidgetParentType.

---
+++ Source/ch/nolix/element/gui/WidgetParentType.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.element.gui;
+
+//enum
+/**
+ * A {@link WidgetParentType} specifies the type of the parent of a {@link Widget}.
+ * 
+ * @author Silvan Wyss
+ * @date 2020-12-20
+ * @lines 10
+ */
+public enum WidgetParentType {
+	GUI,
+	WIDGET
+}
