fe420e5ebc5b5a48977a9f4d56d029209966ae5a
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-25 23:57:10 +0100

    Created AligningContainerWithAutomaticSizeTutorial.

---
+++ Source/ch/nolix/elementtutorial/containerwidgettutorial/AligningContainerWithAutomaticSizeTutorial.java
@@ -0,0 +1,47 @@
+package ch.nolix.elementtutorial.containerwidgettutorial;
+
+import ch.nolix.common.math.Calculator;
+import ch.nolix.element.color.Color;
+import ch.nolix.element.containerwidget.AligningContainer;
+import ch.nolix.element.gui.Frame;
+import ch.nolix.element.widget.Button;
+import ch.nolix.element.widget.Label;
+
+public final class AligningContainerWithAutomaticSizeTutorial {
+	
+	public static void main(String[] args) {
+		
+		final var label = new Label().setText("X").applyOnBaseLook(bl -> bl.setTextSize(100));
+		
+		new Frame()
+		.setTitle("AligningContainer with automatic size Tutorial")
+		.addLayerOnTop(
+			new AligningContainer()
+			.setOnTop(label)
+			.setOnBottomLeft(
+				new Button()
+				.setText("Make smaller")
+				.setLeftMouseButtonPressAction(
+					() ->
+					label.getRefBaseLook().setTextSize(
+						Calculator.getMax(20, label.getRefBaseLook().getRecursiveOrDefaultTextSize() - 20)
+					)
+				)
+			)
+			.setOnBottomRight(
+				new Button()
+				.setText("Make bigger")
+				.setLeftMouseButtonPressAction(
+					() ->
+					label.getRefBaseLook().setTextSize(
+						Calculator.getMin(200, label.getRefBaseLook().getRecursiveOrDefaultTextSize() + 20)
+					)
+				)
+			)
+			.activateAutomaticSize()
+			.applyOnBaseLook(bl -> bl.setBackgroundColor(Color.LAVENDER))
+		);
+	}
+	
+	private AligningContainerWithAutomaticSizeTutorial() {}
+}
