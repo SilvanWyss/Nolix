d69f2fe8379524d64bb1b1ec2d0d226614286818
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-27 15:28:41 +0100

    Cleaned up ButtonLook.

--- Source/ch/nolix/element/widget/ButtonLook.java
+++ Source/ch/nolix/element/widget/ButtonLook.java
@@ -1,54 +1,10 @@
 //package declaration
 package ch.nolix.element.widget;
 
-//own imports
-import ch.nolix.common.node.BaseNode;
-import ch.nolix.common.node.Node;
-import ch.nolix.element.layerelement.LayerProperty;
-
 //class
 public final class ButtonLook extends TextLineWidgetLook<ButtonLook> {
 	
-	//constant
-	public static final boolean DEFAULT_SHORTENS_TEXT_WHEN_LIMITED_FLAG = false;
-	
-	//constant
-	private static final String SHORTENS_TEXT_WHEN_LIMIITED_HEADER = "ShortensTextWhenLimited";
-	
-	//attribute
-	private final LayerProperty<Boolean> shortensTextWhenLimited =
-	new LayerProperty<>(
-		SHORTENS_TEXT_WHEN_LIMIITED_HEADER,
-		DEFAULT_SHORTENS_TEXT_WHEN_LIMITED_FLAG,
-		BaseNode::getOneAttributeAsBoolean,
-		Node::withAttribute
-	);
-	
 	//method
-	public boolean getRecursiveOrDefaultShortensTextWhenLimitedFlag() {
-		return shortensTextWhenLimited.getRecursiveOrDefaultValue();
-	}
-	
-	//method
-	public ButtonLook remainTextWhenLimited() {
-		
-		shortensTextWhenLimited.setValue(false);
-		
-		return this;
-	}
-	
-	//method
-	public ButtonLook shortenTextWhenLimited() {
-		
-		shortensTextWhenLimited.setValue(true);
-		
-		return this;
-	}
-	
-	//method
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	protected void resetStage3() {}
 }
