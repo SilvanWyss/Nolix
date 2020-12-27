87437d463c391257c5300ecfdaaf8533e6294bd0
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-27 15:30:52 +0100

    Cleaned up DownloaderLook.

--- Source/ch/nolix/element/widget/DownloaderLook.java
+++ Source/ch/nolix/element/widget/DownloaderLook.java
@@ -1,47 +1,10 @@
 //package declaration
 package ch.nolix.element.widget;
 
-//own imports
-import ch.nolix.common.node.BaseNode;
-import ch.nolix.common.node.Node;
-import ch.nolix.element.layerelement.LayerProperty;
-
 //class
 public final class DownloaderLook extends TextLineWidgetLook<DownloaderLook> {
 	
-	//constant
-	public static final boolean DEFAULT_SHORTENS_TEXT_WHEN_LIMITED_FLAG = false;
-	
-	//constant
-	private static final String SHORTENS_TEXT_WHEN_LIMIITED_HEADER = "ShortensTextWhenLimited";
-	
-	//attribute
-	private final LayerProperty<Boolean> shortensTextWhenLimited =
-	new LayerProperty<>(
-		SHORTENS_TEXT_WHEN_LIMIITED_HEADER,
-		DEFAULT_SHORTENS_TEXT_WHEN_LIMITED_FLAG,
-		BaseNode::getOneAttributeAsBoolean,
-		Node::withAttribute
-	);
-	
 	//method
-	public boolean getRecursiveOrDefaultShortensTextWhenLimitedFlag() {
-		return shortensTextWhenLimited.getRecursiveOrDefaultValue();
-	}
-	
-	//method
-	public DownloaderLook remainTextWhenLimited() {
-		
-		shortensTextWhenLimited.setValue(false);
-		
-		return this;
-	}
-	
-	//method
-	public DownloaderLook shortenTextWhenLimited() {
-		
-		shortensTextWhenLimited.setValue(true);
-		
-		return this;
-	}
+	@Override
+	protected void resetStage3() {}
 }
