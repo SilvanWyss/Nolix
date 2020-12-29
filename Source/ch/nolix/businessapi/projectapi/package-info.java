c4614c12db00b3a9c3225af8748d8f14687455a4
Silvan Wyss <silvan.wyss@gmail.com>
2020-12-29 17:35:58 +0100

    fixed Javadoc comments of package info

--- Source/ch/nolix/businessapi/projectapi/package-info.java
+++ Source/ch/nolix/businessapi/projectapi/package-info.java
@@ -1,6 +1,6 @@
 //package info
 /**
- * The {@link ch.nolix.projectAPI} package is an API that defines a project, that manages tasks.
+ * The {@link ch.nolix.businessapi.projectapi} package is an API that defines a project, that manages tasks.
  * 
  * @author Silvan Wyss
  * @date 2019-07-22
