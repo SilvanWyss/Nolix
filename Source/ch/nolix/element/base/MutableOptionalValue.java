76f5926ce7962eb6a9a06533d167d463ee76c117
Silvan Wyss <silvan.wyss@gmail.com>
2021-01-12 22:42:23 +0100

    added forInt static method to MutableOptionalValue

--- Source/ch/nolix/element/base/MutableOptionalValue.java
+++ Source/ch/nolix/element/base/MutableOptionalValue.java
@@ -12,11 +12,25 @@ import ch.nolix.common.node.Node;
 //class
 /**
  * @author Silvan Wyss
- * @month 2017-10
- * @lines 80
- * @param <V> is the type of the value of an optional property.
+ * @date 2017-11-01
+ * @lines 100
+ * @param <V> is the type of the value of a {@link MutableOptionalValue}.
  */
 public final class MutableOptionalValue<V> extends SingleValue<V> {
+	
+	//static method
+	/**
+	 * @param name
+	 * @param setterMethod
+	 * @return
+	 * a new {@link MutableOptionalValue} that will store a {@link Integer} and have the given name and setterMethod.
+	 * @throws ArgumentIsNullException if the given name is null.
+	 * @throws InvalidArgumentException if the given name is blank.
+	 * @throws ArgumentIsNullException if the given setterMethod is null.
+	 */
+	public static MutableOptionalValue<Integer> forInt(final String name, final IElementTaker<Integer> setterMethod) {
+		return new MutableOptionalValue<>(name, setterMethod, BaseNode::getOneAttributeAsInt, Node::withAttribute);
+	}
 	
 	//TODO: Add such static creator methods for Strings and Integers to all concrete Property classes.
 	//static method
@@ -30,7 +44,7 @@ public final class MutableOptionalValue<V> extends SingleValue<V> {
 	 * @throws ArgumentIsNullException if the given setterMethod is null.
 	 */
 	public static MutableOptionalValue<String> forString(final String name,	final IElementTaker<String> setterMethod) {
-		return new MutableOptionalValue<>(name, setterMethod, BaseNode::getHeader, Node::withHeader);
+		return new MutableOptionalValue<>(name, setterMethod, BaseNode::getOneAttributeHeader, Node::withAttribute);
 	}
 	
 	//constructor
