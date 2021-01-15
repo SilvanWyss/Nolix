00556660e8c57926a060b3e21b265e7b0d728d97
Silvan Wyss <silvan.wyss@gmail.com>
2021-01-15 18:04:17 +0100

    created implprovider package

---
+++ Source/ch/nolix/common/implprovider/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.common.implprovider} package
+ * defines a registrator where implementations for interfaces can be registered at.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-01-15
+ * @lines 10
+ */
+package ch.nolix.common.implprovider;
