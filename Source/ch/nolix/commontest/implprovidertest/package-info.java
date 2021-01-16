74fdef22dc8a150e5d4b65f74d9e86653fba47fe
Silvan Wyss <silvan.wyss@gmail.com>
2021-01-16 23:53:19 +0100

    created implprovidertest package

---
+++ Source/ch/nolix/commontest/implprovidertest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.commontest.implprovidertest} package
+ * defines tests for the {@link ch.nolix.common.implprovider} package.
+ * 
+ * @author Silvan Wyss
+ * @month 2021-01-16
+ * @lines 10
+ */
+package ch.nolix.commontest.implprovidertest;
