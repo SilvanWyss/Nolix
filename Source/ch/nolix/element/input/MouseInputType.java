c04e3a37933d05b57d332d9a111abbb6dc2ab56b
Silvan Wyss <silvan.wyss@gmail.com>
2021-01-22 13:11:59 +0100

    added createMouseWheelRotationStepFrom static method to MouseInputType

--- Source/ch/nolix/element/input/MouseInputType.java
+++ Source/ch/nolix/element/input/MouseInputType.java
@@ -4,9 +4,12 @@ package ch.nolix.element.input;
 //own imports
 import ch.nolix.common.commontypehelper.StringHelper;
 import ch.nolix.common.container.LinkedList;
+import ch.nolix.common.invalidargumentexception.InvalidArgumentException;
 import ch.nolix.common.node.BaseNode;
 import ch.nolix.common.node.Node;
+import ch.nolix.common.validator.Validator;
 import ch.nolix.element.elementapi.IElement;
+import ch.nolix.element.elementenum.RotationDirection;
 
 //enum
 public enum MouseInputType implements IElement {
@@ -22,6 +25,18 @@ public enum MouseInputType implements IElement {
 	MOUSE_WHEEL_CLICK,
 	FORWARD_MOUSE_WHEEL_ROTATION_STEP,
 	BACKWARD_MOUSE_WHEEL_ROTATION_STEP;
+	
+	//static method
+	public static MouseInputType createMouseWheelRotationStepFrom(final RotationDirection rotationDirection) {
+		switch (rotationDirection) {
+			case FORWARD:
+				return FORWARD_MOUSE_WHEEL_ROTATION_STEP;
+			case BACKWARD:
+				return BACKWARD_MOUSE_WHEEL_ROTATION_STEP;
+			default:
+				throw new InvalidArgumentException("rotation direction", rotationDirection, "is not valid");
+		}
+	}
 	
 	//static method
 	public static MouseInputType fromSpecification(final BaseNode specification) {
