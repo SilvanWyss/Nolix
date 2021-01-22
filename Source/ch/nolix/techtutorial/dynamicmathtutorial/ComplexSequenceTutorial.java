116b7fcdc37bf5aea0955ac5de62386a291d69c1
Silvan Wyss <silvan.wyss@gmail.com>
2021-01-22 17:39:51 +0100

    beautified ComplexSequenceTutorial

--- Source/ch/nolix/techtutorial/dynamicmathtutorial/ComplexSequenceTutorial.java
+++ Source/ch/nolix/techtutorial/dynamicmathtutorial/ComplexSequenceTutorial.java
@@ -1,9 +1,8 @@
 package ch.nolix.techtutorial.dynamicmathtutorial;
 
-//own imports
 import ch.nolix.common.functionapi.IIntTaker;
-import ch.nolix.common.instanceprovider.CentralInstanceProvider;
-import ch.nolix.tech.dynamicmath.Registrator;
+import ch.nolix.common.implprovider.GlobalImplProvider;
+import ch.nolix.tech.dynamicmath.DynamicMathImplRegistrator;
 import ch.nolix.techapi.dynamicmathapi.IComplexNumber;
 import ch.nolix.techapi.dynamicmathapi.IComplexNumberFactory;
 import ch.nolix.techapi.dynamicmathapi.IImplicitComplexSequenceBuilder;
@@ -12,17 +11,18 @@ public final class ComplexSequenceTutorial {
 	
 	public static void main(String[] args) {
 		
-		//Registers an implementation for the GenericMathAPI at the ClassProvider.
-		Registrator.register();
+		//Registers an implementation of the dynamicmathapi at the GlobalImplProvider.
+		new DynamicMathImplRegistrator().registerImplementationTo(GlobalImplProvider.getRefInstance());
 		
-		final var complexNumberFactory = CentralInstanceProvider.create(IComplexNumberFactory.class);
+		final var complexNumberFactory = GlobalImplProvider.ofInterface(IComplexNumberFactory.class).createInstance();
 		final var startValues = new IComplexNumber[] { complexNumberFactory.create(0.0, 0.0) };
 		final var c = complexNumberFactory.create(0.0, 1.0);
 		
 		//Creates a Sequence.
 		final var sequence =
-		CentralInstanceProvider
-		.create(IImplicitComplexSequenceBuilder.class)
+		GlobalImplProvider
+		.ofInterface(IImplicitComplexSequenceBuilder.class)
+		.createInstance()
 		.setStartIndex(1)
 		.setStartValues(startValues)
 		.setNextValueFunction(z -> z[0].getPower2().getSum(c))
@@ -30,7 +30,7 @@ public final class ComplexSequenceTutorial {
 		
 		final IIntTaker function = (int i) -> System.out.println("a(" + i + ") = " + sequence.getValue(i));
 		
-		//Prints out to the console some values of the sequence.
+		//Prints out to the console some values of the Sequence.
 		function.run(1);
 		function.run(2);
 		function.run(3);
