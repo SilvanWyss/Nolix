7efa513fdd83e427afce3ce032e7bbf9b34e0362
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-14 22:23:34 +0100

    improved ValueOrPercentageHolder

--- Source/ch/nolix/element/valueholder/ValueOrPercentageHolder.java
+++ Source/ch/nolix/element/valueholder/ValueOrPercentageHolder.java
@@ -4,6 +4,7 @@ package ch.nolix.element.valueholder;
 //own imports
 import ch.nolix.common.commontypehelper.StringHelper;
 import ch.nolix.common.constant.LowerCaseCatalogue;
+import ch.nolix.common.constant.PascalCaseCatalogue;
 import ch.nolix.common.container.LinkedList;
 import ch.nolix.common.invalidargumentexception.ArgumentDoesNotHaveAttributeException;
 import ch.nolix.common.invalidargumentexception.InvalidArgumentException;
@@ -15,17 +16,13 @@ import ch.nolix.element.base.Element;
 /**
  * A {@link ValueOrPercentageHolder} stores either a value or a percentage.
  * A {@link ValueOrPercentageHolder} is not mutable.
- * 
- * For higher performance,
- * a {@link ValueOrPercentageHolder} accesses its private attributes directly
- * and not through the available methods.
- * 
+ *  
  * @author Silvan Wyss
  * @date 2018-03-25
- * @lines 150
+ * @lines 140
  */
 public final class ValueOrPercentageHolder extends Element<ValueOrPercentageHolder> {
-
+	
 	//attributes
 	private final boolean hasValue;
 	private final int value;
@@ -38,24 +35,16 @@ public final class ValueOrPercentageHolder extends Element<ValueOrPercentageHold
 	 * from the given specification.
 	 * @throws InvalidArgumentException if the given specification is not valid.
 	 */
-	public static ValueOrPercentageHolder fromSpecification(
-		final BaseNode specification
-	) {
+	public static ValueOrPercentageHolder fromSpecification(final BaseNode specification) {
 		
-		var attribute = specification.getOneAttributeHeader();
+		final var attribute = specification.getOneAttributeHeader();
 		
-		//Handles the case that the given specification
-		//specifies a value or percentage holder with a percentage.
+		//Handles the case that the given specification specifies a ValueOrPercentageHolder with a percentage.
 		if (attribute.endsWith("%")) {
-			return new ValueOrPercentageHolder(
-				StringHelper.toDouble(
-					attribute.substring(0, attribute.length() - 2)
-				)
-			);
+			return new ValueOrPercentageHolder(StringHelper.toDouble(attribute.substring(0, attribute.length() - 2)));
 		}
 		
-		//Handles the case that the given specification
-		//specifies a value or percentage holder with a value.
+		//Handles the case that the given specification specifies a ValueOrPercentageHolder with a value.
 		return new ValueOrPercentageHolder(StringHelper.toInt(attribute));
 	}
 	
@@ -67,7 +56,9 @@ public final class ValueOrPercentageHolder extends Element<ValueOrPercentageHold
 	 * @param value
 	 */
 	public ValueOrPercentageHolder(final int value) {
+		
 		hasValue = true;
+		
 		this.value = value;
 		this.percentage = 0.0;
 	}
@@ -79,7 +70,9 @@ public final class ValueOrPercentageHolder extends Element<ValueOrPercentageHold
 	 * @param percentage
 	 */
 	public ValueOrPercentageHolder(final double percentage) {
+		
 		hasValue = false;
+		
 		this.value = 0;
 		this.percentage = percentage;
 	}
@@ -93,22 +86,26 @@ public final class ValueOrPercentageHolder extends Element<ValueOrPercentageHold
 		
 		super.fillUpAttributesInto(list);
 		
-		//TODO: Implement.
+		if (hasValue()) {
+			list.addAtEnd(Node.withHeaderAndAttribute(PascalCaseCatalogue.VALUE, getValue()));
+		}
+		
+		if (hasPercentage()) {
+			list.addAtEnd(Node.withHeaderAndAttribute(PascalCaseCatalogue.PERCENTAGE, getPercentage()));
+		}
 	}
 	
 	//method
 	/**
 	 * @return the value of this percentage holder.
-	 * @throws ArgumentDoesNotHaveAttributeException if the current {@link ValueOrPercentageHolder} does not have a value.
+	 * @throws ArgumentDoesNotHaveAttributeException
+	 * if the current {@link ValueOrPercentageHolder} does not have a value.
 	 */
 	public int getValue() {
 		
-		//Asserts that this value or percentage holder has a value.
-		if (!hasValue) {
-			throw new ArgumentDoesNotHaveAttributeException(
-				this,
-				LowerCaseCatalogue.VALUE
-			);
+		//Asserts that the current ValueOrPercentageHolder has a value.
+		if (!hasValue()) {
+			throw new ArgumentDoesNotHaveAttributeException(this, LowerCaseCatalogue.VALUE);
 		}
 		
 		return value;
@@ -117,16 +114,14 @@ public final class ValueOrPercentageHolder extends Element<ValueOrPercentageHold
 	//method
 	/**
 	 * @return the percentage of this percentage holder.
-	 * @throws ArgumentDoesNotHaveAttributeException if the current {@link ValueOrPercentageHolder} does not have a percentage.
+	 * @throws ArgumentDoesNotHaveAttributeException
+	 * if the current {@link ValueOrPercentageHolder} does not have a percentage.
 	 */
 	public double getPercentage() {
 		
-		//Asserts that this value or percentage holder has a percentage.
-		if (hasValue) {
-			throw new ArgumentDoesNotHaveAttributeException(
-				this,
-				LowerCaseCatalogue.PERCENTAGE
-			);
+		//Asserts that the current ValueOrPercentageHolder has a percentage.
+		if (!hasPercentage()) {
+			throw new ArgumentDoesNotHaveAttributeException(this, LowerCaseCatalogue.PERCENTAGE);
 		}
 		
 		return percentage;
@@ -137,7 +132,7 @@ public final class ValueOrPercentageHolder extends Element<ValueOrPercentageHold
 	 * @return true if the current {@link ValueOrPercentageHolder} has a percentage.
 	 */
 	public boolean hasPercentage() {
-		return !hasValue;
+		return !hasValue();
 	}
 	
 	//method
