348c7e53b6fadff8903fb4b702595f8b9ead3dcc
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-14 19:44:52 +0100

    created constanttest package

---
+++ Source/ch/nolix/commontest/constanttest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.commontest.constanttest} package defines tests for the {@link ch.nolix.common.constant} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-02-14
+ * @lines 10
+ */
+package ch.nolix.commontest.constanttest;
