dcfc41eb841ea512116e70c5c1ac5b3a255bfa54
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-14 19:30:16 +0100

    created GlobalClassHelper

---
+++ Source/ch/nolix/common/reflectionhelper/GlobalClassHelper.java
@@ -0,0 +1,35 @@
+//package declaration
+package ch.nolix.common.reflectionhelper;
+
+//own imports
+import ch.nolix.common.container.IContainer;
+import ch.nolix.common.container.LinkedList;
+import ch.nolix.common.exception.WrapperException;
+
+//class
+public final class GlobalClassHelper {
+	
+	//static method
+	public static IContainer<Object> getPublicStaticFieldValuesOfClass(final Class<?> pClass) {
+		
+		final var publicStaticFields = new LinkedList<>();
+		
+		//Iterates the fields of the given Class.
+		for (final var f : pClass.getDeclaredFields()) {
+			
+			//Handles the case that the current field is static.
+			if (FieldHelper.isStatic(f) && FieldHelper.isPublic(f)) {
+				try {
+					publicStaticFields.addAtEnd(f.get(null));
+				} catch (final IllegalAccessException illegalAccessException) {
+					throw new WrapperException(illegalAccessException);
+				}
+			}
+		}
+		
+		return publicStaticFields;
+	}
+	
+	//visibility-reducing constructor
+	private GlobalClassHelper() {}
+}
