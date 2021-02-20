086b5016ab6336c2afc10abd1b3c5ee21f4b9aa7
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-20 21:12:09 +0100

    created PropertyBinding

---
+++ Source/ch/nolix/system/database/propertybinder/PropertyBinding.java
@@ -0,0 +1,104 @@
+//package declaration
+package ch.nolix.system.database.propertybinder;
+
+//own imports
+import ch.nolix.common.container.SingleContainer;
+import ch.nolix.common.validator.Validator;
+import ch.nolix.element.gui.Widget;
+import ch.nolix.element.widget.Label;
+import ch.nolix.element.widget.LabelRole;
+import ch.nolix.system.entity.Property;
+
+//class
+public final class PropertyBinding {
+	
+	//attributes
+	private final Property<?> property;
+	private final Widget<?, ?> widget;
+	private final Label errorLabel = new Label().setRole(LabelRole.ERROR_TEXT);
+	
+	//optional attribute
+	private Throwable currentError;
+	
+	//constructors
+	public PropertyBinding(final Property<?> property, final Widget<?, ?> widget) {
+		
+		Validator.assertThat(property).thatIsNamed(Property.class).isNotNull();
+		Validator.assertThat(widget).thatIsNamed(Widget.class).isNotNull();
+		
+		this.property = property;
+		this.widget = widget;
+		
+		errorLabel.setCollapsed();
+	}
+	
+	//method
+	public SingleContainer<Throwable> getCurrentErrorOptionally() {
+		
+		if (!hasCurrentError()) {
+			return new SingleContainer<>();
+		}
+		
+		return new SingleContainer<>(currentError);
+	}
+	
+	//method
+	public Label getRefErrorLabel() {
+		return errorLabel;
+	}
+	
+	//method
+	public Property<?> getRefProperty() {
+		return property;
+	}
+	
+	//method
+	public Widget<?, ?> getRefWidget() {
+		return widget;
+	}
+	
+	//method
+	public boolean hasCurrentError() {
+		return (currentError != null);
+	}
+	
+	//visibility-reduced method
+	void removeCurrentError() {
+		currentError = null;
+		errorLabel.emptyText();
+		errorLabel.setCollapsed();
+	}
+	
+	//visibility-reduced method
+	void setCurrentError(final Throwable currentError) {
+		
+		Validator.assertThat(currentError).thatIsNamed("current error").isNotNull();
+		
+		this.currentError = currentError;
+		updateErrorLabelFrom(currentError);
+	}
+	
+	//method
+	private String getDefaultErrorMessage() {
+		return ("The " + property.getHeader() + " is not valid.");
+	}
+	
+	//method
+	private String getErrorMessageFrom(final Throwable error) {
+		
+		final var errorMessage = error.getMessage();
+		
+		if (errorMessage == null || errorMessage.isBlank()) {
+			return getDefaultErrorMessage();
+		}
+		
+		return errorMessage;
+	}
+	
+	//method
+	private void updateErrorLabelFrom(final Throwable error) {
+		errorLabel
+		.setExpanded()
+		.setText(getErrorMessageFrom(error));
+	}
+}
