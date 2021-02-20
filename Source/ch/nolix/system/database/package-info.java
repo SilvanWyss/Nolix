bef787f3104488b386ca1920ca22ec1245040ef7
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-20 16:37:24 +0100

    created database package

---
+++ Source/ch/nolix/system/database/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.database} package contains types for interacting with databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-02-20
+ * @lines 10
+ */
+package ch.nolix.system.database;
