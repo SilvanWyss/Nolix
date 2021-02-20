7faa57fd5f77bf0f355406f0d2638efb9d6d364a
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-20 12:42:54 +0100

    created propertybinder package

---
+++ Source/ch/nolix/system/propertybinder/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.propertybinder} package defines binders
+ * that bind {@link ch.nolix.system.entity.Property}s to {@link ch.nolix.element.gui.Widget}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-02-20
+ * @lines 10
+ */
+package ch.nolix.system.propertybinder;
