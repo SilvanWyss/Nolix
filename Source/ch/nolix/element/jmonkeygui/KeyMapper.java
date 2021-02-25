09c21d43b3a8a64b4516a22efa0cc78c92ca43c6
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-25 20:20:54 +0100

    created KeyMapper

--- Source/ch/nolix/element/jmonkeygui/KeyMapper.java
+++ Source/ch/nolix/element/jmonkeygui/KeyMapper.java
@@ -4,7 +4,7 @@ import com.jme3.input.event.KeyInputEvent;
 
 import ch.nolix.element.input.Key;
 
-public final class JMonkeyKeyMapper {
+public final class KeyMapper {
 	
 	public Key getKeyFrom(final KeyInputEvent keyInputEvent) {
 		return Key.fromCharacter(keyInputEvent.getKeyChar());
