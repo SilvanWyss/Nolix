a02bcd1b011b5add1741cf74c300d9ac25195cb6
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-26 15:48:35 +0100

    beautified KeyMapper

--- Source/ch/nolix/element/jmonkeygui/KeyMapper.java
+++ Source/ch/nolix/element/jmonkeygui/KeyMapper.java
@@ -1,11 +1,16 @@
+//package declaration
 package ch.nolix.element.jmonkeygui;
 
+//JMonkey import
 import com.jme3.input.event.KeyInputEvent;
 
+//own import
 import ch.nolix.element.input.Key;
 
+//class
 public final class KeyMapper {
 	
+	//method
 	public Key getKeyFrom(final KeyInputEvent keyInputEvent) {
 		return Key.fromCharacter(keyInputEvent.getKeyChar());
 	}
