6b93a6575e19f229c26c2f3cdcda18ddf85a5d51
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-26 15:51:44 +0100

    created BaseDataType enum

---
+++ Source/ch/nolix/businessapi/databaseapi/datatypeapi/BaseDataType.java
@@ -0,0 +1,10 @@
+//package declaration
+package ch.nolix.businessapi.databaseapi.datatypeapi;
+
+//enum
+public enum BaseDataType {
+	BASE_VALUE,
+	BASE_REFERENCE,
+	BASE_BACK_REFERENCE,
+	BASE_TECNICAL_TYPE
+}
