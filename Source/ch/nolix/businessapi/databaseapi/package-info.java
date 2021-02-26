76465ac6a8bd814b6c4479764ee9bbdf835e02f0
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-26 15:50:00 +0100

    created databaseapi package

---
+++ Source/ch/nolix/businessapi/databaseapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.businessapi.databaseapi} package declares entities that are persistable in a database.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-02-26
+ * @lines 10
+ */
+package ch.nolix.businessapi.databaseapi;
