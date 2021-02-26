dd9cb7589ebb5a21ddcb7ff9fa758c5ab2c5d829
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-26 15:50:57 +0100

    created datatypeapi package

---
+++ Source/ch/nolix/businessapi/databaseapi/datatypeapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.businessapi.databaseapi.datatypeapi} package declares datatypes.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-02-26
+ * @lines 10
+ */
+package ch.nolix.businessapi.databaseapi.datatypeapi;
