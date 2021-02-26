d13a5dc68bf864752187a54a5c56222cbc18a9ce
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-26 15:47:23 +0100

    beautified HeaderLengthAndTaskAfterHeaderParameter

--- Source/ch/nolix/common/chainednode/HeaderLengthAndTaskAfterHeaderParameter.java
+++ Source/ch/nolix/common/chainednode/HeaderLengthAndTaskAfterHeaderParameter.java
@@ -1,12 +1,17 @@
+//package declaration
 package ch.nolix.common.chainednode;
 
+//own import
 import ch.nolix.common.invalidargumentexception.ArgumentIsNullException;
 
+//class
 final class HeaderLengthAndTaskAfterHeaderParameter {
 	
+	//attributes
 	private final int headerLength;
 	private final TaskAfterHeader taskAfterHeader;
 	
+	//constructor
 	public HeaderLengthAndTaskAfterHeaderParameter(final int headerLength, final TaskAfterHeader taskAfterHeader) {
 		
 		if (taskAfterHeader == null) {
@@ -17,10 +22,12 @@ final class HeaderLengthAndTaskAfterHeaderParameter {
 		this.taskAfterHeader = taskAfterHeader;
 	}
 	
+	//method
 	public int getHeaderLength() {
 		return headerLength;
 	}
 	
+	//method
 	public TaskAfterHeader getTaskAfterHeader() {
 		return taskAfterHeader;
 	}
