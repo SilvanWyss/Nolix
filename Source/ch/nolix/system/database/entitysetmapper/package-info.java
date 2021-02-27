9bedcd71db62fe8d57c0487029db8ca92bf89663
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-27 16:27:44 +0100

    created entitysetmapper package

---
+++ Source/ch/nolix/system/database/entitysetmapper/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.database.entitysetmapper} package defines
+ * a mapper for {@link ch.nolix.system.database.databaseschemaadapter.EntitySet}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-02-27
+ * @lines 10
+ */
+package ch.nolix.system.database.entitysetmapper;
