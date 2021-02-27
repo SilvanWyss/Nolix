e3d253408f19e0fe55e81afd04c896587fb3d214
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-27 18:14:50 +0100

    refactored CandleStickTest

--- Source/ch/nolix/elementtest/financetest/CandleStickTest.java
+++ Source/ch/nolix/elementtest/financetest/CandleStickTest.java
@@ -9,101 +9,95 @@ import ch.nolix.element.time.Time;
 
 //class
 /**
- * This class is a test class for the candle stick class.
+ * A {@link CandleStickTest} is a test for {@link CandleStick}s
  * 
  * @author Silvan Wyss
- * @month 2016-09
+ * @date 2016-10-01
  * @lines 100
  */
 public final class CandleStickTest extends Test {
 	
 	//method
 	@TestCase
-	public void testCase_isBearish_1() {
+	public void testCase_isBearish_whenIsBearish() {
 		
 		//setup
-		final CandleStick candleStick
-		= new CandleStick(
-		 new Time(2020, 1, 1),
-			100.0,
-			200.0,
-			50.0,
-			250.0
-		);
+		final var testUnit = new CandleStick(new Time(2020, 1, 1), 200.0, 100.0, 50.0, 250.0);
 		
-		//execution & verification
-		expectNot(candleStick.isBearish());
-	}
-	
-	//method
-	@TestCase
-	public void testCase_isBearish_2() {
+		//execution
+		final var result = testUnit.isBearish();
 		
-		//setup
-		final CandleStick candleStick
-		= new CandleStick(
-		 new Time(2020, 1, 1),
-			200.0,
-			100.0,
-			50.0,
-			250.0
-		);
-		
-		//execution & verification
-		expect(candleStick.isBearish());
+		//verification
+		expect(result);
 	}
 
 	//method
 	@TestCase
-	public void testCase_isBullish_1() {
+	public void testCase_isBearish_whenIsNotBearish() {
 		
 		//setup
-		final CandleStick candleStick
-		= new CandleStick(
-		 new Time(2020, 1, 1),
-			100.0,
-			200.0,
-			50.0,
-			250.0
-		);
+		final var testUnit = new CandleStick(new Time(2020, 1, 1), 100.0, 200.0, 50.0, 250.0);
 		
-		//execution & verification
-		expect(candleStick.isBullish());
+		//execution
+		final var result = testUnit.isBearish();
+		
+		//verification
+		expectNot(result);
 	}
 	
 	//method
 	@TestCase
-	public void testCase_isBullish_2() {
+	public void testCase_isBullish_whenIsBullish() {
 		
 		//setup
-		final CandleStick candleStick
-		= new CandleStick(
-		 new Time(2020, 1, 1),
-			200.0,
-			100.0,
-			50.0,
-			250.0
-		);
+		final var testUnit = new CandleStick(new Time(2020, 1, 1), 100.0, 200.0, 50.0, 250.0);
 		
-		//execution & verification
-		expectNot(candleStick.isBullish());
+		//execution
+		final var result = testUnit.isBullish();
+		
+		//verification
+		expect(result);
 	}
 	
 	//method
 	@TestCase
-	public void testCase_isHammer() {
+	public void testCase_isBullish_whenIsNotBullish() {
 		
 		//setup
-		final CandleStick candleStick
-		= new CandleStick(
-		 new Time(2020, 1, 1),
-			200.0,
-			210.0,
-			100.0,
-			210.0
-		);
+		final var testUnit = new CandleStick(new Time(2020, 1, 1), 200.0, 100.0, 50.0, 250.0);
 		
-		//execution & verification
-		expect(candleStick.isHammer());
+		//execution
+		final var result = testUnit.isBullish();
+		
+		//verification
+		expectNot(result);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_isHammer_whenIsHammer() {
+		
+		//setup
+		final var testUnit = new CandleStick(new Time(2020, 1, 1), 200.0, 210.0, 100.0,	210.0);
+		
+		//execution
+		final var result = testUnit.isHammer();
+		
+		//verification
+		expect(result);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_isHammer_whenIsNotHammer() {
+		
+		//setup
+		final var testUnit = new CandleStick(new Time(2020, 1, 1), 200.0, 210.0, 200.0,	210.0);
+		
+		//execution
+		final var result = testUnit.isHammer();
+		
+		//verification
+		expectNot(result);
 	}
 }
