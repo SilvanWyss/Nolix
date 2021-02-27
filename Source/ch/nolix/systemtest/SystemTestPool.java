af03663fb187505845a1cb0728766029a780f5bf
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-27 17:41:03 +0100

    fixed ElementTestPool

--- Source/ch/nolix/systemtest/SystemTestPool.java
+++ Source/ch/nolix/systemtest/SystemTestPool.java
@@ -4,6 +4,7 @@ package ch.nolix.systemtest;
 //own imports
 import ch.nolix.common.basetest.TestPool;
 import ch.nolix.systemtest.databasetest.DatabaseTestPool;
+import ch.nolix.systemtest.dynamicmathtest.DynamicMathTestPool;
 import ch.nolix.systemtest.neurontest.NeuronTestPool;
 import ch.nolix.systemtest.texturetest.TextureTestPool;
 
@@ -14,6 +15,7 @@ public final class SystemTestPool extends TestPool {
 	public SystemTestPool() {
 		super(
 			new DatabaseTestPool(),
+			new DynamicMathTestPool(),
 			new NeuronTestPool(),
 			new TextureTestPool()
 		);
--- Source/ch/nolix/systemtest/dynamicmathtest/DynamicMathTestPool.java
+++ Source/ch/nolix/systemtest/dynamicmathtest/DynamicMathTestPool.java
@@ -1,7 +1,7 @@
 //package declaration
 package ch.nolix.systemtest.dynamicmathtest;
 
-//own imports
+//own import
 import ch.nolix.common.basetest.TestPool;
 
 //class
