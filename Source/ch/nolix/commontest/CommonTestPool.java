b86f0102c4702432d9c86c0e650faf276fa86a5d
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-27 17:37:04 +0100

    fixed CommonTestPool

--- Source/ch/nolix/commontest/CommonTestPool.java
+++ Source/ch/nolix/commontest/CommonTestPool.java
@@ -9,12 +9,14 @@ import ch.nolix.commontest.commontypehelpertest.CommonTypeHelpersTestPool;
 import ch.nolix.commontest.commontypewrappertest.CommonTypeWrapperTestPool;
 import ch.nolix.commontest.constanttest.ConstantTestPool;
 import ch.nolix.commontest.containertest.ContainerTestPool;
+import ch.nolix.commontest.implprovidertest.ImplProviderTestPool;
 import ch.nolix.commontest.licensetest.LicenseTestPool;
 import ch.nolix.commontest.mathtest.MathematicsTestPool;
 import ch.nolix.commontest.nodetest.NodeTestPool;
 import ch.nolix.commontest.sequencertest.SequencerTestPool;
 import ch.nolix.commontest.validatortest.ValidatorTestPool;
 import ch.nolix.commontest.websockettest.WebSocketTestPool;
+import ch.nolix.commontest.xmltest.XMLTestPool;
 
 //class
 /**
@@ -31,17 +33,19 @@ public final class CommonTestPool extends TestPool {
 	public CommonTestPool() {
 		super(
 			new CachingTestPool(),
+			new ChainedNodeTestPool(),
 			new CommonTypeHelpersTestPool(),
 			new CommonTypeWrapperTestPool(),
 			new ConstantTestPool(),
 			new ContainerTestPool(),
-			new NodeTestPool(),
+			new ImplProviderTestPool(),
 			new LicenseTestPool(),
 			new MathematicsTestPool(),
+			new NodeTestPool(),
 			new SequencerTestPool(),
-			new ChainedNodeTestPool(),
 			new ValidatorTestPool(),
 			new WebSocketTestPool(),
+			new XMLTestPool(),
 			new ch.nolix.commontest.endpointtest.EndPointTestPool(),
 			new ch.nolix.commontest.endpoint2test.EndPointTestPool(),
 			new ch.nolix.commontest.endpoint3test.EndPointTestPool()
