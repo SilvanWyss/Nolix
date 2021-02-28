563a84e576b5ef2f45c9974ae868321e21630222
Silvan Wyss <silvan.wyss@gmail.com>
2021-02-28 20:40:37 +0100

    fixed isNotEmpty method of StringMediator

--- Source/ch/nolix/common/validator/StringMediator.java
+++ Source/ch/nolix/common/validator/StringMediator.java
@@ -125,7 +125,7 @@ public class StringMediator extends ArgumentMediator<String> {
 		
 		//Asserts that the argument of the current StringMediator is not empty.
 		if (getRefArgument().isEmpty()) {
-			throw new EmptyArgumentException(getRefArgument());
+			throw new EmptyArgumentException(getArgumentName(), getRefArgument());
 		}
 	}
 	
