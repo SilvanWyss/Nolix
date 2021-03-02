76155ec70d1e5c64630eaad458648b0e314af2d6
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-02 18:37:38 +0100

    created errorcontrol package

---
+++ Source/ch/nolix/common/errorcontrol/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.errorcontrol} package provides classes for error control.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-02
+ * @lines 10
+ */
+package ch.nolix.common.errorcontrol;
