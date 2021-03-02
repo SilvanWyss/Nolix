1def9661ccac55970fd29d12ae47ccfee7cb6dd3
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-02 19:01:24 +0100

    created net package

---
+++ Source/ch/nolix/common/net/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.net} package provides classes for net communication.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-02
+ * @lines 10
+ */
+package ch.nolix.common.net;
