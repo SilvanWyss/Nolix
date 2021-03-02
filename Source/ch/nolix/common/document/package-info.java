d88c31bedf4f1c411e8e0611807d67abbb65e13b
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-02 19:14:53 +0100

    created document package

---
+++ Source/ch/nolix/common/document/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.document} package defines documents.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-02
+ * @lines 10
+ */
+package ch.nolix.common.document;
