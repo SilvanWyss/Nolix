3bea1ac5011f2ff25fbb72a58c4f2c80998e74a4
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-02 20:59:29 +0100

    created nettest package

---
+++ Source/ch/nolix/commontest/nettest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.commontest.nettest} package defines tests for the {@link ch.nolix.common.net} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-02
+ * @lines 10
+ */
+package ch.nolix.commontest.nettest;
