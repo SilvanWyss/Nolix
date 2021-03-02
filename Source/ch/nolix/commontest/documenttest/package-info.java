b78cf443cbf315b3da95d5c11d3e9224f2f78df7
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-02 19:23:12 +0100

    created documenttest package

---
+++ Source/ch/nolix/commontest/documenttest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.commontest.documenttest} package defines tests for the {@link ch.nolix.common.document} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-02
+ * @lines 10
+ */
+package ch.nolix.commontest.documenttest;
