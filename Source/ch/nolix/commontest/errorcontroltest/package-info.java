94c4b2ad017da06826862908957a280f714a0461
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-03 17:39:53 +0100

    created errorcontroltest package

---
+++ Source/ch/nolix/commontest/errorcontroltest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.commontest.errorcontroltest} package defines
+ * tests for the {@link ch.nolix.common.errorcontrol} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-03
+ * @lines 10
+ */
+package ch.nolix.commontest.errorcontroltest;
