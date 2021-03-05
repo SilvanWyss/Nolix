ba2a95625024e9ab885bd82ab61c3d6256f39737
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-05 13:34:18 +0100

    created demotest package

---
+++ Source/ch/nolix/commontest/demotest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.commontest.demotest} package defines tests for the {@link ch.nolix.common.demo} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-05
+ * @lines 10
+ */
+package ch.nolix.commontest.demotest;
