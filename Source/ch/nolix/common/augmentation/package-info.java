b2c7a61deeef013beb5e0bdf132a5a80962fc75e
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-06 22:51:04 +0100

    deleted augmentation package

--- Source/ch/nolix/common/augmentation/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.common.augmentation} package declares an augmentable object.
- * 
- * @author Silvan Wyss
- * @date 2020-12-03
- * @lines 10
- */
-package ch.nolix.common.augmentation;
