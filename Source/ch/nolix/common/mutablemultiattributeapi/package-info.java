02a22604ce00b3a02408b752ddaff67b103afe3b
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-06 22:49:16 +0100

    deleted multiattributerequestapi and mutablemultiattributeapi

--- Source/ch/nolix/common/mutablemultiattributeapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.common.mutablemultiattributeapi} package declares types with a certain mutable multi-attribute.
- * 
- * @author Silvan Wyss
- * @month 2020-07
- * @lines 10
- */
-package ch.nolix.common.mutablemultiattributeapi;
