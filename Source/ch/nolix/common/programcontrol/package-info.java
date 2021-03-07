52b9715eb6b29a792bffa828a152c51ef7b4202b
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-07 16:50:58 +0100

    created programcontrol package

---
+++ Source/ch/nolix/common/programcontrol/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.programcontrol} package defines types for program control.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-07
+ * @lines 10
+ */
+package ch.nolix.common.programcontrol;
