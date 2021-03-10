09f897c35520ec1d5c36aa6bbc6d4780586bf8f3
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-10 18:13:40 +0100

    added getBaseDataType method to DataType

--- Source/ch/nolix/businessapi/databaseapi/datatypeapi/DataType.java
+++ Source/ch/nolix/businessapi/databaseapi/datatypeapi/DataType.java
@@ -1,6 +1,9 @@
 //package declaration
 package ch.nolix.businessapi.databaseapi.datatypeapi;
 
+//own import
+import ch.nolix.common.errorcontrol.invalidargumentexception.InvalidArgumentException;
+
 //enum
 public enum DataType {
 	ID,
@@ -12,5 +15,27 @@ public enum DataType {
 	MULTI_REFERENCE,
 	BACK_REFERENCE,
 	OPTIONAL_BACK_REFERENCE,
-	MULTI_BACK_REFERENCE
+	MULTI_BACK_REFERENCE;
+	
+	//method
+	public final BaseDataType getBaseDataType() {
+		switch (this) {
+			case ID:
+				return BaseDataType.BASE_TECNICAL_TYPE;
+			case VALUE:
+			case OPTIONAL_VALUE:
+			case MULTI_VALUE:
+				return BaseDataType.BASE_VALUE;
+			case REFERENCE:
+			case OPTIONAL_REFERENCE:
+			case MULTI_REFERENCE:
+				return BaseDataType.BASE_REFERENCE;
+			case BACK_REFERENCE:
+			case OPTIONAL_BACK_REFERENCE:
+			case MULTI_BACK_REFERENCE:
+				return BaseDataType.BASE_BACK_REFERENCE;
+			default:
+				throw new InvalidArgumentException(this);
+		}
+	}
 }
