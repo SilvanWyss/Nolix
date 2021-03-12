ec60841e2909b38311a7e8e8c6de114b34001eb9
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-12 14:27:54 +0100

    fixed FileNodeTest

--- Source/ch/nolix/commontest/documenttest/filenodetest/FileNodeTest.java
+++ Source/ch/nolix/commontest/documenttest/filenodetest/FileNodeTest.java
@@ -1,6 +1,7 @@
-//package delcaration
+//package declaration
 package ch.nolix.commontest.documenttest.filenodetest;
 
+//own imports
 import ch.nolix.common.document.filenode.FileNode;
 import ch.nolix.common.environment.filesystem.FileSystemAccessor;
 import ch.nolix.common.testing.basetest.Cleanup;
@@ -9,13 +10,15 @@ import ch.nolix.commontest.documenttest.nodetest.BaseNodeTest;
 //class
 public final class FileNodeTest extends BaseNodeTest {
 	
+	//method
+	@Cleanup
+	public void cleanup() {
+		FileSystemAccessor.getFolderOfRunningJarFile().deleteFileSystemItem("fileNode");
+	}
+	
+	//method
 	@Override
 	protected FileNode createTestUnit() {
 		return new FileNode(FileSystemAccessor.getFolderOfRunningJarFile().getPath() + "/fileNode");
-	}
-	
-	@Cleanup
-	private void afterTestCase() {
-		FileSystemAccessor.getFolderOfRunningJarFile().deleteFileSystemItem("fileNode");
 	}
 }
