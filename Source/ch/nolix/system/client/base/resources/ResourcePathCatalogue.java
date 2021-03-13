7235587d97d6f53a76c4d4d2ee7ed37ab78a765f
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-13 12:38:25 +0100

    added technical comments to ResourcePathCatalogue

--- Source/ch/nolix/system/client/base/resources/ResourcePathCatalogue.java
+++ Source/ch/nolix/system/client/base/resources/ResourcePathCatalogue.java
@@ -1,9 +1,13 @@
+//package declaration
 package ch.nolix.system.client.base.resources;
 
+//class
 public final class ResourcePathCatalogue {
 	
+	//constants
 	public static final String NOLIX_JS = "ch/nolix/system/client/base/resources/Nolix.js";
 	public static final String REQUIRE_JS = "ch/nolix/system/client/base/resources/require_js.js";
 	
+	//visibility-reduced constructor
 	private ResourcePathCatalogue() {}
 }
