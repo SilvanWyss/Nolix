0be5a8c8f406805100115cd0c1a6eebc0fc10bbd
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-26 22:21:03 +0100

    created formatelement package

---
+++ Source/ch/nolix/element/formatelement/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.element.formatelement} package defines
+ * a cascading formatable {@link ch.nolix.element.elementapi.IMutableElement}.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-26
+ * @lines 10
+ */
+package ch.nolix.element.formatelement;
