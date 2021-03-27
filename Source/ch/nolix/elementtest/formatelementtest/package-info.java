2742e858af4417538c64a2a02622c2695587f0db
Silvan Wyss <silvan.wyss@gmail.com>
2021-03-27 10:22:38 +0100

    created formatelementtest package

---
+++ Source/ch/nolix/elementtest/formatelementtest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.elementtest.formatelementtest} package defines
+ * tests for the {@link ch.nolix.element.formatelement} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-03-27
+ * @lines 10
+ */
+package ch.nolix.elementtest.formatelementtest;
