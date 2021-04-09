1e5f07ce689297f50d03b6eb252347d6e4261625
Silvan Wyss <silvan.wyss@gmail.com>
2021-04-09 20:30:41 +0200

    added lineThickness to CheckBoxLook

--- Source/ch/nolix/element/gui/widget/CheckBoxLook.java
+++ Source/ch/nolix/element/gui/widget/CheckBoxLook.java
@@ -1,8 +1,52 @@
 //package declaration
 package ch.nolix.element.gui.widget;
 
-//own import
+//own imports
+import ch.nolix.common.constant.LowerCaseCatalogue;
+import ch.nolix.common.document.node.BaseNode;
+import ch.nolix.common.document.node.Node;
+import ch.nolix.common.errorcontrol.validator.Validator;
+import ch.nolix.element.formatelement.NonCascadingProperty;
 import ch.nolix.element.gui.base.WidgetLook;
+import ch.nolix.element.gui.base.WidgetLookState;
 
 //class
-public final class CheckBoxLook extends WidgetLook<CheckBoxLook> {}
+public final class CheckBoxLook extends WidgetLook<CheckBoxLook> {
+	
+	//constant
+	public static final int DEFAULT_LINE_THICKNESS = 1;
+	
+	//constant
+	private static final String DEFAULT_LINE_THICKNESS_HEADER = "LineThickness";
+	
+	//attribute
+	private final NonCascadingProperty<WidgetLookState, Integer> lineThickness =
+	new NonCascadingProperty<>(
+		DEFAULT_LINE_THICKNESS_HEADER,
+		WidgetLookState.class,
+		BaseNode::getOneAttributeAsInt,
+		Node::withAttribute,
+		this::setLineThicknessForState,
+		DEFAULT_LINE_THICKNESS
+	);
+	
+	//method
+	public int getLineThickness() {
+		return lineThickness.getValue();
+	}
+	
+	//method
+	public void removeLineThicknesses() {
+		lineThickness.setUndefined();
+	}
+	
+	//method
+	public CheckBoxLook setLineThicknessForState(final WidgetLookState state, final int lineThickness) {
+		
+		Validator.assertThat(lineThickness).thatIsNamed(LowerCaseCatalogue.LINE_THICKNESS).isNotNegative();
+		
+		this.lineThickness.setValueForState(state, lineThickness);
+		
+		return asConcrete();
+	}
+}
