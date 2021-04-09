b5a34a69244f88eca50373692eef62722303839c
Silvan Wyss <silvan.wyss@gmail.com>
2021-04-09 18:41:52 +0200

    created SingleContainerLook

---
+++ Source/ch/nolix/element/gui/containerwidget/SingleContainerLook.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.element.gui.containerwidget;
+
+//own import
+import ch.nolix.element.gui.widget.BorderWidgetLook;
+
+//class
+public final class SingleContainerLook extends BorderWidgetLook<SingleContainerLook> {}
