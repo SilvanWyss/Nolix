8934649b90a635f4680658e066fd622cca063264
Silvan Wyss <silvan.wyss@gmail.com>
2021-04-09 20:14:45 +0200

    created TextLineWidgetLook

---
+++ Source/ch/nolix/element/gui/widget/TextLineWidgetLook.java
@@ -0,0 +1,5 @@
+//package declaration
+package ch.nolix.element.gui.widget;
+
+//class
+public abstract class TextLineWidgetLook<TLWL extends TextLineWidgetLook<TLWL>> extends BorderWidgetLook<TLWL> {}
