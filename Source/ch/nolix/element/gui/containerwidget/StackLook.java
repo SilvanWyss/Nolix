4f32e25338cc298447daee560234bab9d0a30bc4
Silvan Wyss <silvan.wyss@gmail.com>
2021-04-10 22:33:54 +0200

    added fromSpecification static method to StackLook

--- Source/ch/nolix/element/gui/containerwidget/StackLook.java
+++ Source/ch/nolix/element/gui/containerwidget/StackLook.java
@@ -1,8 +1,19 @@
 //package declaration
 package ch.nolix.element.gui.containerwidget;
 
-//own import
+//own imports
+import ch.nolix.common.document.node.BaseNode;
 import ch.nolix.element.gui.widget.BorderWidgetLook;
 
 //class
-public final class StackLook extends BorderWidgetLook<StackLook> {}
+public final class StackLook extends BorderWidgetLook<StackLook> {
+	
+	//static method
+	public static StackLook fromSpecification(final BaseNode specification) {
+		
+		final var stackLook = new StackLook();
+		stackLook.resetFrom(specification);
+		
+		return stackLook;
+	}
+}
