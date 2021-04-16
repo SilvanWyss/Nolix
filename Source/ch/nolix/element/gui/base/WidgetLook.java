5745d14e23d9fe8e6aaf2542e62e0157d2ac2ea3
Silvan Wyss <silvan.wyss@gmail.com>
2021-04-16 14:45:30 +0200

    added textColor to WidgetLook

--- Source/ch/nolix/element/gui/base/WidgetLook.java
+++ Source/ch/nolix/element/gui/base/WidgetLook.java
@@ -9,6 +9,7 @@ import ch.nolix.common.document.node.Node;
 import ch.nolix.common.errorcontrol.validator.Validator;
 import ch.nolix.element.formatelement.CascadingProperty;
 import ch.nolix.element.formatelement.FormatElement;
+import ch.nolix.element.gui.color.Color;
 import ch.nolix.element.gui.textformat.Font;
 
 //class
@@ -18,11 +19,13 @@ public abstract class WidgetLook<WL extends WidgetLook<WL>> extends FormatElemen
 	public static final Font DEFAULT_FONT = Font.ARIAL;
 	public static final boolean DEFAULT_BOLD_TEXT_FLAG = false;
 	public static final int DEAULT_TEXT_SIZE = 10;
+	public static final Color DEFAULT_TEXT_COLOR = Color.BLACK;
 	
 	//constants
 	private static final String FONT_HEADER = PascalCaseCatalogue.FONT;
 	private static final String BOLD_TEXT_FLAG_HEADER = "BoldTextag";
 	private static final String TEXT_SIZE_HEADER = PascalCaseCatalogue.TEXT_SIZE;
+	private static final String TEXT_COLOR_HEADER = "TextColor";
 	
 	//attribute
 	private final CascadingProperty<WidgetLookState, Font> font =
@@ -55,6 +58,16 @@ public abstract class WidgetLook<WL extends WidgetLook<WL>> extends FormatElemen
 		DEAULT_TEXT_SIZE
 	);
 	
+	//attribute
+	private final CascadingProperty<WidgetLookState, Color> textColor =
+	new CascadingProperty<WidgetLookState, Color>(
+		TEXT_COLOR_HEADER,
+		WidgetLookState.class,
+		Color::fromSpecification,
+		Color::getSpecification,
+		DEFAULT_TEXT_COLOR
+	);
+	
 	//constructor
 	public WidgetLook() {
 		super(WidgetLookState.NORMAL);
@@ -71,6 +84,11 @@ public abstract class WidgetLook<WL extends WidgetLook<WL>> extends FormatElemen
 	}
 	
 	//method
+	public final Color getTextColor() {
+		return textColor.getValue();
+	}
+	
+	//method
 	public final int getTextSize() {
 		return textSize.getValue();
 	}
@@ -83,6 +101,11 @@ public abstract class WidgetLook<WL extends WidgetLook<WL>> extends FormatElemen
 	//method
 	public final void removeFonts() {
 		font.setUndefined();
+	}
+	
+	//method
+	public final void removeTextColors() {
+		textColor.setUndefined();
 	}
 	
 	//method
@@ -102,6 +125,14 @@ public abstract class WidgetLook<WL extends WidgetLook<WL>> extends FormatElemen
 	public final WL setFontForState(final WidgetLookState state, final Font font) {
 		
 		this.font.setValueForState(state, font);
+		
+		return asConcrete();
+	}
+	
+	//method
+	public final WL setTextColorForState(final WidgetLookState state, final Color textColor) {
+		
+		this.textColor.setValueForState(state, textColor);
 		
 		return asConcrete();
 	}
