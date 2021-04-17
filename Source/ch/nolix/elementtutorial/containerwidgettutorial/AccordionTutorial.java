898920511b1cb7ca864c776462cfebed25cca47d
Silvan Wyss <silvan.wyss@gmail.com>
2021-04-17 18:18:06 +0200

    edited AccordionTutorial

--- Source/ch/nolix/elementtutorial/containerwidgettutorial/AccordionTutorial.java
+++ Source/ch/nolix/elementtutorial/containerwidgettutorial/AccordionTutorial.java
@@ -12,47 +12,35 @@ import ch.nolix.element.gui.widget.Area;
  * Of the {@link AccordionTutorial} an instance cannot be created.
  * 
  * @author Silvan Wyss
- * @month 2018-08
- * @lines 50
+ * @date 2018-08-13
+ * @lines 40
  */
 public final class AccordionTutorial {
 	
-	/**
-	 * Creates a {@link Frame} with a {@link Accordion}.
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
 				
 		//Creates a Frame.
-		final var frame =
-		new Frame()
-		.setTitle("Accordion Container Tutorial");
+		final var frame = new Frame().setTitle("Accordion Tutorial");
 		
 		//Creates an Accordion.
 		final var accordion =
 		new Accordion()
 		.addTab(
-			new AccordionTab("A", new Area().setSize(500,  200).setBackgroundColor(Color.BLUE)),
-			new AccordionTab("B", new Area().setSize(500,  200).setBackgroundColor(Color.GREEN)),
-			new AccordionTab("C", new Area().setSize(500,  200).setBackgroundColor(Color.BLUE)),
-			new AccordionTab("D", new Area().setSize(500,  200).setBackgroundColor(Color.GREEN))
+			new AccordionTab("A", new Area().setSize(500,  200).setBackgroundColor(Color.ORANGE)),
+			new AccordionTab("B", new Area().setSize(450,  200).setBackgroundColor(Color.VIOLET)),
+			new AccordionTab("C", new Area().setSize(400,  200).setBackgroundColor(Color.GREEN)),
+			new AccordionTab("D", new Area().setSize(350,  200).setBackgroundColor(Color.BROWN))
 		);
 		
 		//Configures the look of the Accordion.
-		accordion.onLook(
-			l ->
-			l
-			.setBorderThicknessForState(WidgetLookState.BASE, 5)
-			.setBackgroundColorForState(WidgetLookState.BASE, Color.LAVENDER)
-		);
+		accordion
+		.getRefLook()
+		.setBorderThicknessForState(WidgetLookState.BASE, 5)
+		.setBackgroundColorForState(WidgetLookState.BASE, Color.LAVENDER);
 		
 		//Adds the Accordion to the Frame.
 		frame.addLayerOnTop(accordion);
 	}
 	
-	/**
-	 * Avoids that an instance of the {@link AccordionTutorial} can be created.
-	 */
 	private AccordionTutorial() {}
 }
