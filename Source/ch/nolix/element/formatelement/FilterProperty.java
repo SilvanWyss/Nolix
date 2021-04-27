ce8348bd22774ce84316367fd36e6b6491396cb8
Silvan Wyss <silvan.wyss@gmail.com>
2021-04-27 22:47:17 +0200

    created FilterProperty

---
+++ Source/ch/nolix/element/formatelement/FilterProperty.java
@@ -0,0 +1,51 @@
+//package declaration
+package ch.nolix.element.formatelement;
+
+//own imports
+import ch.nolix.common.container.IContainer;
+import ch.nolix.common.container.LinkedList;
+import ch.nolix.common.document.node.BaseNode;
+import ch.nolix.common.document.node.Node;
+
+//class
+public final class FilterProperty<S extends Enum<S>, V> extends Property<S>{
+	
+	//multi-attribute
+	private final IContainer<MaterializedProperty<S, V>> materializedProperties;
+	
+	//constructor
+	@SuppressWarnings("unchecked")
+	public FilterProperty(final String name, final MaterializedProperty<S, V>... materializedProperties) {
+		
+		super(name);
+		
+		this.materializedProperties = LinkedList.withElements(materializedProperties);
+	}
+	
+	//constructor
+	public FilterProperty(final String name, final IContainer<MaterializedProperty<S, V>> materializedProperties) {
+		
+		super(name);
+		
+		this.materializedProperties = LinkedList.fromIterable(materializedProperties);
+	}
+	
+	//method
+	@Override
+	protected void fillUpValuesSpecificationInto(LinkedList<Node> list) {}
+	
+	//method
+	@Override
+	protected void setFrom(Property<S> property) {}
+	
+	//method
+	@Override
+	protected void setUndefined() {}
+	
+	@Override
+	protected void setValueFromSpecification(final BaseNode specification) {
+		for (final var mp : materializedProperties) {
+			mp.setValueFromSpecification(specification);
+		}
+	}
+}
