ef3bce4610865d6b29e22021256beabf348893a8
Silvan Wyss <silvan.wyss@gmail.com>
2021-05-21 17:57:39 +0200

    renamed LabelShorteningTextTutorial to ShorteningTextLabelTutorial

--- Source/ch/nolix/elementtutorial/guitutorial/widgettutorial/ShorteningTextLabelTutorial.java
+++ Source/ch/nolix/elementtutorial/guitutorial/widgettutorial/ShorteningTextLabelTutorial.java
@@ -5,14 +5,14 @@ import ch.nolix.element.gui.base.WidgetLookState;
 import ch.nolix.element.gui.widget.Label;
 
 /**
- * The {@link LabelShorteningTextTutorial} is a tutorial for {@link Label}s.
+ * The {@link ShorteningTextLabelTutorial} is a tutorial for {@link Label}s.
  * Of the {@link TimeLabelTutorial} an instance cannot be created.
  *
  * @author Silvan Wyss
  * @date 2019-05-19
  * @lines 40
  */
-public final class LabelShorteningTextTutorial {
+public final class ShorteningTextLabelTutorial {
 	
 	/**
 	 * Creates a {@link Frame} with a {@link Label}.
@@ -22,24 +22,22 @@ public final class LabelShorteningTextTutorial {
 	public static void main(String[] args) {
 		
 		//Creates a Frame.
-		final var frame =
-		new Frame()
-		.setTitle("Label Shortening Text Tutorial");
+		final var frame = new Frame().setTitle("Shortening text Label Tutorial");
 		
 		//Creates a Label with a long text.
 		final var label = new Label().setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr");
 		
-		//Configures the look of the label.
+		//Configures the look of the Label.
 		label
 		.setMaxWidth(500)
 		.onLook(l -> l.setTextSizeForState(WidgetLookState.BASE, 50));	
 			
-		//Adds the label to the frame.
+		//Adds the Label to the Frame.
 		frame.addLayerOnTop(label);
 	}
 	
 	/**
-	 * Prevents that an instance of the {@link LabelShorteningTextTutorial} can be created.
+	 * Prevents that an instance of the {@link ShorteningTextLabelTutorial} can be created.
 	 */
-	private LabelShorteningTextTutorial() {}
+	private ShorteningTextLabelTutorial() {}
 }
