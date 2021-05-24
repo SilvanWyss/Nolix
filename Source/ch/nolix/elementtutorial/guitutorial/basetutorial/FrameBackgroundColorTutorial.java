68beb334ce03308f57a15252a7779322731443c8
Silvan Wyss <silvan.wyss@gmail.com>
2021-05-24 19:25:45 +0200

    created FrameBackgroundColorTutorial

---
+++ Source/ch/nolix/elementtutorial/guitutorial/basetutorial/FrameBackgroundColorTutorial.java
@@ -0,0 +1,31 @@
+package ch.nolix.elementtutorial.guitutorial.basetutorial;
+
+import ch.nolix.element.gui.base.Frame;
+import ch.nolix.element.gui.color.Color;
+
+/**
+ * The {@link FrameBackgroundColorTutorial} is a tutorial for a {@link Frame}s.
+ * Of the {@link FrameBackgroundColorTutorial} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-05-24
+ * @lines 30
+ */
+public final class FrameBackgroundColorTutorial {
+	
+	/**
+	 * Creates a {@link Frame} and sets a background color to it.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Frame()
+		.setTitle("Frame background color Tutorial")
+		.setBackgroundColor(Color.LAVENDER);
+	}
+	
+	/**
+	 * Prevents that an instance of the {@link FrameBackgroundColorTutorial} can be created.
+	 */
+	private FrameBackgroundColorTutorial() {}
+}
