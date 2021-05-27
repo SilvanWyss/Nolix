2b001734740ad95f87ba7113086f140ed1c5964d
Silvan Wyss <silvan.wyss@gmail.com>
2021-05-27 23:03:45 +0200

    created resource package

---
+++ Source/ch/nolix/elementtutorial/guitutorial/basetutorial/resource/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.elementtutorial.guitutorial.basetutorial.resource} package contains
+ * the resources for the {@link ch.nolix.elementtutorial.guitutorial.basetutorial} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-05-27
+ * @lines 10
+ */
+package ch.nolix.elementtutorial.guitutorial.basetutorial.resource;
