c774968c38b464a4c68ccd417baeea36faeae079
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-04 21:57:41 +0200

    created ForwardingProperty

---
+++ Source/ch/nolix/element/base/ForwardingProperty.java
@@ -0,0 +1,59 @@
+//package declaration
+package ch.nolix.element.base;
+
+//own imports
+import ch.nolix.common.attributeapi.mandatoryattributeapi.Named;
+import ch.nolix.common.constant.LowerCaseCatalogue;
+import ch.nolix.common.container.LinkedList;
+import ch.nolix.common.document.node.BaseNode;
+import ch.nolix.common.document.node.Node;
+import ch.nolix.common.errorcontrol.validator.Validator;
+import ch.nolix.common.functionapi.IElementTaker;
+import ch.nolix.common.functionapi.IElementTakerElementGetter;
+
+//class
+public final class ForwardingProperty<V> extends Property implements Named {
+	
+	//attributes
+	private final String name;
+	private final IElementTaker<V> setter;
+	private final IElementTakerElementGetter<BaseNode, V> valueCreator;
+	
+	//constructor
+	public ForwardingProperty(
+		final String name,
+		final IElementTaker<V> setter,
+		final IElementTakerElementGetter<BaseNode, V> valueCreator
+	) {
+		
+		Validator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).isNotBlank();
+		Validator.assertThat(setter).thatIsNamed("setter").isNotNull();
+		Validator.assertThat(valueCreator).thatIsNamed("value creator").isNotNull();
+		
+		this.name = name;
+		this.setter = setter;
+		this.valueCreator = valueCreator;
+	}
+	
+	//method
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	//method
+	@Override
+	protected boolean addedOrChangedAttribute(BaseNode attribute) {
+		
+		if (hasName(attribute.getHeader())) {
+			setter.run(valueCreator.getOutput(attribute));
+			return true;
+		}
+		
+		return false;
+	}
+	
+	//method
+	@Override
+	protected void fillUpAttributesInto(final LinkedList<Node> list) {}
+}
