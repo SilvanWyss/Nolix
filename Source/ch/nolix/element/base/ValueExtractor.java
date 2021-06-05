e26c98a78fe1eb622809584d8bc3e30062717bc8
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-05 10:29:11 +0200

    created ValueExtractor

---
+++ Source/ch/nolix/element/base/ValueExtractor.java
@@ -0,0 +1,70 @@
+//package declaration
+package ch.nolix.element.base;
+
+//own imports
+import ch.nolix.common.constant.PascalCaseCatalogue;
+import ch.nolix.common.container.LinkedList;
+import ch.nolix.common.document.node.BaseNode;
+import ch.nolix.common.document.node.Node;
+import ch.nolix.common.errorcontrol.validator.Validator;
+import ch.nolix.common.functionapi.IElementGetter;
+import ch.nolix.common.functionapi.IElementTaker;
+import ch.nolix.common.functionapi.IElementTakerElementGetter;
+
+//class
+public final class ValueExtractor<V> extends Property {
+	
+	//attributes
+	private final String name;
+	private final IElementTaker<V> setter;
+	
+	//attributes
+	private final IElementGetter<V> getter;
+	private final IElementTakerElementGetter<BaseNode, V> valueCreator;
+	private final IElementTakerElementGetter<V, Node> specificationCreator;
+	
+	//constructor
+	public ValueExtractor(
+		final String name,
+		final IElementTaker<V> setter,
+		final IElementGetter<V> getter,
+		final IElementTakerElementGetter<BaseNode, V> valueCreator,
+		final IElementTakerElementGetter<V, Node> specificationCreator
+	) {
+		
+		Validator.assertThat(name).thatIsNamed(PascalCaseCatalogue.NAME).isNotBlank();
+		Validator.assertThat(setter).thatIsNamed("setter").isNotNull();
+		Validator.assertThat(getter).thatIsNamed("getter").isNotNull();
+		Validator.assertThat(valueCreator).thatIsNamed("value creator").isNotNull();
+		Validator.assertThat(specificationCreator).thatIsNamed("specification creator").isNotNull();
+		
+		this.name = name;
+		this.setter = setter;
+		this.getter = getter;
+		this.valueCreator = valueCreator;
+		this.specificationCreator = specificationCreator;
+	}
+	
+	//method
+	public String getName() {
+		return name;
+	}
+	
+	//method
+	@Override
+	protected boolean addedOrChangedAttribute(final BaseNode attribute) {
+		
+		if (attribute.hasHeader(getName())) {
+			setter.run(valueCreator.getOutput(attribute));
+			return true;
+		}
+		
+		return false;
+	}
+	
+	//method
+	@Override
+	protected void fillUpAttributesInto(final LinkedList<Node> list) {
+		list.addAtEnd(specificationCreator.getOutput(getter.getOutput()));
+	}
+}
