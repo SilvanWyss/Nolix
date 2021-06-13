53c1784418d0987bfd9627875ac565bd67c3e5d8
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-13 22:38:03 +0200

    created resource package

---
+++ Source/ch/nolix/element/gui/base/resource/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.element.gui.base.resource} package contains
+ * the resources for the {@link ch.nolix.element.gui.base} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-06-13
+ * @lines 10
+ */
+package ch.nolix.element.gui.base.resource;
