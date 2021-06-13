2ee7ad375d8e1d74a98c0da2d65b816422b459ee
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-13 18:14:26 +0200

    created IImage

---
+++ Source/ch/nolix/element/gui/image/IImage.java
@@ -0,0 +1,66 @@
+//package declaration
+package ch.nolix.element.gui.image;
+
+//Java imports
+import java.awt.image.BufferedImage;
+
+//own imports
+import ch.nolix.element.gui.color.Color;
+
+//interface
+public interface IImage {
+	
+	//method declaration
+	Color getBottomLeftPixel();
+	
+	//method declaration
+	Color getBottomRightPixel();
+	
+	//method declaration
+	int getHeight();
+	
+	//method declaration
+	Color getPixel(int xPosition, int yPosition);
+	
+	//method declaration
+	int getPixelCount();
+	
+	//method declaration
+	IImage getSection(int xPosition, int yPosition, int width, int height);
+	
+	//method declaration
+	Color getTopLeftPixel();
+	
+	//method declaration
+	Color getTopRightPixel();
+	
+	//method declaration
+	int getWidth();
+			
+	//method declaration
+	BufferedImage toBufferedImage();
+	
+	//method declaration
+	IImage toImmutableImage();
+	
+	//method declaration
+	byte[] toJPG();
+	
+	//method declaration
+	IImage toLeftRotatedImage();
+	
+	//method declaration
+	byte[] toPNG();
+	
+	//method declaration
+	IImage toRepeatedImage(int width, int height);
+	
+	//method declaration
+	IImage toRightRotatedImage();
+	
+	//method declaration
+	IImage toScaledImage(double factor);
+	
+	//method declaration
+	IImage toScaledImage(double widthFactor, double heightFactor);
+}
