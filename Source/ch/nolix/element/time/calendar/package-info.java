fda316ff5c50d7276938dde0670ae773e56474a2
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-15 23:24:19 +0200

    created calendar package

---
+++ Source/ch/nolix/element/time/calendar/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.element.time.calendar} package defines an appointment for calendars.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-06-15
+ * @lines 10
+ */
+package ch.nolix.element.time.calendar;
