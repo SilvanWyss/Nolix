8f276c7094d0c6207e89c8fb4a074e914c959ebe
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-16 21:42:00 +0200

    created calendarapi package

---
+++ Source/ch/nolix/element/time/calendarapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.element.time.calendar} package declares an appointment for calendars.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-06-16
+ * @lines 10
+ */
+package ch.nolix.element.time.calendarapi;
