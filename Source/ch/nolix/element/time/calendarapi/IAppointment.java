e95ba38c83a3beae6f29a9afd8645b5a96d544c1
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-16 21:42:58 +0200

    created IAppointment

---
+++ Source/ch/nolix/element/time/calendarapi/IAppointment.java
@@ -0,0 +1,42 @@
+//package declaration
+package ch.nolix.element.time.calendarapi;
+
+//own imports
+import ch.nolix.common.attributeapi.mutablemandatoryattributeapi.IMutableSubjectOwner;
+import ch.nolix.element.time.base.Time;
+
+//interface
+public interface IAppointment<A extends IAppointment<A>> extends IMutableSubjectOwner<A> {
+	
+	//method declaration
+	Time getEndTime();
+	
+	//method declaration
+	Time getStartTime();
+	
+	//method
+	default boolean isAfter(final IAppointment<?> appointment) {
+		return isAfter(appointment.getEndTime());
+	}
+	
+	//method
+	default boolean isAfter(final Time time) {
+		return getStartTime().isAfter(time);
+	}
+	
+	//method
+	default boolean isBefore(final Time time) {
+		return getEndTime().isBefore(time);
+	}
+	
+	//method
+	default boolean isBefore(final IAppointment<?> appointment) {
+		return isBefore(appointment.getStartTime());
+	}
+	
+	//method declaration
+	A setEndTime(Time endTime);
+	
+	//method declaration
+	A setStartTime(Time startTime);
+}
