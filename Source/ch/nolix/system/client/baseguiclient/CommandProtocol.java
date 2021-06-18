0b67ea011257a9339f18c2099dd80f4f6d11d476
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-18 15:44:18 +0200

    extended CommandProtocol

--- Source/ch/nolix/system/client/baseguiclient/CommandProtocol.java
+++ Source/ch/nolix/system/client/baseguiclient/CommandProtocol.java
@@ -13,6 +13,7 @@ final class CommandProtocol {
 	public static final String SAVE_FILE = "SaveFile";
 	public static final String SEND_FILE = "SendFile";
 	public static final String SET_CURSOR_ICON = "SetCursorIcon";
+	public static final String SET_ICON = "SetIcon";
 	public static final String SET_PAINT_COMMANDS = "SetPaintCommands";
 	public static final String SET_TITLE = "SetTitle";
 	public static final String SHOW_ERROR_MESSAGE = "ShowErrorMessage";
