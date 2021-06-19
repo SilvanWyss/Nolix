c69c0bad06b40541a8bb1dffd0c2a33c6dde4e04
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-19 23:23:05 +0200

    created ForwardingMutableValue

---
+++ Source/ch/nolix/element/base/ForwardingMutableValue.java
@@ -0,0 +1,70 @@
+//package declaration
+package ch.nolix.element.base;
+
+//own imports
+import ch.nolix.common.attributeapi.mandatoryattributeapi.Named;
+import ch.nolix.common.constant.LowerCaseCatalogue;
+import ch.nolix.common.container.LinkedList;
+import ch.nolix.common.document.node.BaseNode;
+import ch.nolix.common.document.node.Node;
+import ch.nolix.common.errorcontrol.validator.Validator;
+import ch.nolix.common.functionapi.IElementGetter;
+import ch.nolix.common.functionapi.IElementTaker;
+import ch.nolix.common.functionapi.IElementTakerElementGetter;
+
+//class
+public final class ForwardingMutableValue<V> extends Property implements Named {
+	
+	//attributes
+	private final String name;
+	private final IElementTaker<V> setter;
+	private final IElementGetter<V> getter;
+	private final IElementTakerElementGetter<BaseNode, V> valueCreator;
+	private final IElementTakerElementGetter<V, Node> specificationCreator;
+	
+	//constructor
+	public ForwardingMutableValue(
+		final String name,
+		final IElementTaker<V> setter,
+		final IElementGetter<V> getter,
+		final IElementTakerElementGetter<BaseNode, V> valueCreator,
+		final IElementTakerElementGetter<V, Node> specificationCreator
+	) {
+		
+		Validator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).isNotBlank();
+		Validator.assertThat(setter).thatIsNamed("setter").isNotNull();
+		Validator.assertThat(getter).thatIsNamed("getter").isNotNull();
+		Validator.assertThat(valueCreator).thatIsNamed("value creator").isNotNull();
+		Validator.assertThat(specificationCreator).thatIsNamed("specification creator").isNotNull();
+		
+		this.name = name;
+		this.setter = setter;
+		this.getter = getter;
+		this.valueCreator = valueCreator;
+		this.specificationCreator = specificationCreator;
+	}
+	
+	//method
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	//method
+	@Override
+	protected boolean addedOrChangedAttribute(BaseNode attribute) {
+		
+		if (hasName(attribute.getHeader())) {
+			setter.run(valueCreator.getOutput(attribute));
+			return true;
+		}
+		
+		return false;
+	}
+	
+	//method
+	@Override
+	protected void fillUpAttributesInto(final LinkedList<Node> list) {
+		list.addAtEnd(specificationCreator.getOutput(getValue()));
+	}
+}
