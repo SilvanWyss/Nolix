741d9173a72d15878f3eb7a1c8264a36da2bcd57
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-20 11:24:11 +0200

    created textbox package

---
+++ Source/ch/nolix/element/gui/textbox/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.element.gui.textbox} package defines a text box.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-06-19
+ * @lines 10
+ */
+package ch.nolix.element.gui.textbox;
