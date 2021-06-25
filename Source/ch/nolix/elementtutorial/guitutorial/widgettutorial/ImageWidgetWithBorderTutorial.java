700c43eed96472b088a036e35ec3868a0c279ef7
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-25 14:54:32 +0200

    edited ImageWidget tutorials

--- Source/ch/nolix/elementtutorial/guitutorial/widgettutorial/ImageWidgetWithBorderTutorial.java
+++ Source/ch/nolix/elementtutorial/guitutorial/widgettutorial/ImageWidgetWithBorderTutorial.java
@@ -1,18 +1,20 @@
 package ch.nolix.elementtutorial.guitutorial.widgettutorial;
 
 import ch.nolix.element.gui.base.Frame;
+import ch.nolix.element.gui.color.Color;
 import ch.nolix.element.gui.image.MutableImage;
 import ch.nolix.element.gui.widget.ImageWidget;
+import ch.nolix.element.gui.widget.WidgetLookState;
 
 /**
- * The {@link ImageWidgetTutorial} is a tutorial for {@link ImageWidget}s.
- * Of the {@link ImageWidgetTutorial} an instance cannot be created.
+ * The {@link ImageWidgetWithBorderTutorial} is a tutorial for {@link ImageWidget}s.
+ * Of the {@link ImageWidgetWithBorderTutorial} an instance cannot be created.
  * 
  * @author Silvan Wyss
- * @date 2018-08-23
- * @lines 40
+ * @date 2021-05-21
+ * @lines 50
  */
-public final class ImageWidgetTutorial {
+public final class ImageWidgetWithBorderTutorial {
 	
 	/**
 	 * Creates a {@link Frame} with a {@link ImageWidget}.
@@ -22,7 +24,7 @@ public final class ImageWidgetTutorial {
 	public static void main(String[] args) {
 		
 		//Creates a Frame.
-		final var frame = new Frame().setTitle("ImageWidget tutorial");
+		final var frame = new Frame().setTitle("ImageWidget with border tutorial");
 		
 		//Creates an Image from resource.
 		final var image =
@@ -31,12 +33,20 @@ public final class ImageWidgetTutorial {
 		//Creates an ImageWidget with the image.
 		final var imageWidget = new ImageWidget().setImage(image);
 		
+		//Configures the look of the ImageWidget.
+		imageWidget.onLook(
+			l ->
+			l
+			.setBorderThicknessForState(WidgetLookState.BASE, 2)
+			.setBorderColorForState(WidgetLookState.BASE, Color.BLUE)
+		);
+		
 		//Adds the ImageWidget to the Frame.
 		frame.addLayerOnTop(imageWidget);
 	}
 	
 	/**
-	 * Prevents that an instance of the {@link ImageWidgetTutorial} can be created.
+	 * Prevents that an instance of the {@link ImageWidgetWithBorderTutorial} can be created.
 	 */
-	private ImageWidgetTutorial() {}
+	private ImageWidgetWithBorderTutorial() {}
 }
