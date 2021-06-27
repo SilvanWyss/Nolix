5c5d2e6c0627de93b97c2f1f56bc95b6541f9bd4
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-27 17:17:05 +0200

    created LocalServer

---
+++ Source/ch/nolix/common/net/endpoint2/LocalServer.java
@@ -0,0 +1,10 @@
+//package declaration
+package ch.nolix.common.net.endpoint2;
+
+//class
+/**
+ * @author Silvan Wyss
+ * @date 2021-06-27
+ * @lines 10
+ */
+public final class LocalServer extends BaseServer {}
