84fec1074e9ef8cb4160e4bbde243af64fd0deec
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-28 21:28:44 +0200

    edited LocalServer

--- Source/ch/nolix/common/net/endpoint2/LocalServer.java
+++ Source/ch/nolix/common/net/endpoint2/LocalServer.java
@@ -5,15 +5,21 @@ package ch.nolix.common.net.endpoint2;
 /**
  * @author Silvan Wyss
  * @date 2021-06-27
- * @lines 10
+ * @lines 20
  */
 public final class LocalServer extends BaseServer {
 	
 	//method
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected void noteAddedDefaultEndPointTaker(final IEndPointTaker defaultEndPointTaker) {}
 	
 	//method
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected void noteAddedEndPointTaker(final IEndPointTaker endPointTaker) {}
 }
