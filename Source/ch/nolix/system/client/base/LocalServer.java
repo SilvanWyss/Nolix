bb1d6c0c8ced5d5ed1caede02d0aa8f124cc32d9
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-29 18:35:11 +0200

    created LocalServer

---
+++ Source/ch/nolix/system/client/base/LocalServer.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.system.client.base;
+
+//class
+/**
+ * @author Silvan Wyss
+ * @date 2021-06-29
+ * @lines 20
+ */
+public final class LocalServer extends BaseServer {
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void noteAddedApplication(Application<?> application) {}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void noteAddedDefaultApplication(Application<?> defaultApplication2) {}
+}
