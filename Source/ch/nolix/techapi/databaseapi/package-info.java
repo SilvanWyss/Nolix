b4e2adf1c9cf97d6bf4a2cf277a2f64cd458489b
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-30 23:10:21 +0200

    created databaseapi package

---
+++ Source/ch/nolix/techapi/databaseapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.databaseapi} package declares entity sets that can be applied as tables to a database.
+ * The {@link ch.nolix.techapi.databaseapi} package declares also entities that can be persisted in a database.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-06-30
+ */
+package ch.nolix.techapi.databaseapi;
