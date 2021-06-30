cf2ce79c226c3b74139fd0ae871fc9453eeb2f60
Silvan Wyss <silvan.wyss@gmail.com>
2021-06-30 19:29:48 +0200

    created techapi package

---
+++ Source/ch/nolix/techapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi} package defines APIs that declares types for technical use.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-06-30
+ */
+package ch.nolix.techapi;
