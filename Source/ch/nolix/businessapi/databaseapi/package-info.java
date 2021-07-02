fb3f7a92159579872774c0f0f3b5f70cc04cc155
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-02 09:10:59 +0200

    deleted old databaseapi package

--- Source/ch/nolix/businessapi/databaseapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.businessapi.databaseapi} package declares entities that are persistable in a database.
- * 
- * @author Silvan Wyss
- * @date 2021-02-26
- * @lines 10
- */
-package ch.nolix.businessapi.databaseapi;
