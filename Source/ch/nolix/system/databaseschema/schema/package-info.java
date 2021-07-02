29cecab6f97357d518ce9b7c715f2d7aa5060ce6
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-02 12:04:26 +0200

    created schema package

---
+++ Source/ch/nolix/system/databaseschema/schema/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.databaseschema.schema} package contains
+ * an implementation of the types of the {ch.nolix.techapi.databaseschemaapi.schemaapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-02
+ */
+package ch.nolix.system.databaseschema.schema;
