a71f5e2b27ee89df56884364acdfe564c6fe57db
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-02 11:16:35 +0200

    created schemaadapterapi package

---
+++ Source/ch/nolix/techapi/databaseschemaapi/schemaadapterapi/IDatabaseSchemaAdapter.java
@@ -0,0 +1,63 @@
+//package declaration
+package ch.nolix.techapi.databaseschemaapi.schemaadapterapi;
+
+//own imports
+import ch.nolix.common.skillapi.IChangesSaver;
+import ch.nolix.techapi.databaseschemaapi.schema.IColumn;
+import ch.nolix.techapi.databaseschemaapi.schema.IDatabase;
+import ch.nolix.techapi.databaseschemaapi.schema.IParametrizedPropertyType;
+import ch.nolix.techapi.databaseschemaapi.schema.ITable;
+
+//interface
+public interface IDatabaseSchemaAdapter<
+	D extends IDatabase<D, T, C, PPT>,
+	T extends ITable<T, C, PPT>,
+	C extends IColumn<C, PPT>,
+	PPT extends IParametrizedPropertyType<Object>
+>
+extends IChangesSaver {
+	
+	//method
+	default IDatabaseSchemaAdapter<D, T, C, PPT> addTable(T table) {
+		
+		getRefDatabase().addTable(table);
+		
+		return this;
+	}
+	
+	//method
+	default IDatabaseSchemaAdapter<D, T, C, PPT> addTableWithName(final String name) {
+		
+		getRefDatabase().addTableWithName(name);
+		
+		return this;
+	}
+	
+	//method
+	default boolean containsTableWithName(final String name) {
+		return getRefDatabase().containsTableWithName(name);
+	}
+	
+	//method
+	default void deleteTable(T table) {
+		getRefDatabase().deleteTable(table);
+	}
+	
+	//method
+	default void deleteTableWithName(final String name) {
+		deleteTable(getRefTableByName(name));
+	}
+	
+	//method declaration
+	D getRefDatabase();
+	
+	//method
+	default T getRefTableByName(final String name) {
+		return getRefDatabase().getRefTableByName(name);
+	}
+	
+	//method
+	default int getTableCount() {
+		return getRefDatabase().getTableCount();
+	}
+}
---
+++ Source/ch/nolix/techapi/databaseschemaapi/schemaadapterapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.databaseschemaapi.schemaadapterapi} package declares
+ * an adapter to access the schema of databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-02
+ */
+package ch.nolix.techapi.databaseschemaapi.schemaadapterapi;
