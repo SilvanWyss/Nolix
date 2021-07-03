c36402555c16c9f6d616ee8eb4439db07a676067
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-03 19:55:22 +0200

    created IDatabaseObject

---
+++ Source/ch/nolix/techapi/databasecommonapi/databaseobjectapi/IDatabaseObject.java
@@ -0,0 +1,55 @@
+//package
+package ch.nolix.techapi.databasecommonapi.databaseobjectapi;
+
+//own imports
+import ch.nolix.common.errorcontrol.invalidargumentexception.ExpiredArgumentException;
+import ch.nolix.common.errorcontrol.invalidargumentexception.InvalidArgumentException;
+import ch.nolix.common.errorcontrol.invalidargumentexception.NonNewArgumentException;
+
+//interface
+public interface IDatabaseObject {
+	
+	//method
+	default void assertIsLoaded() {
+		if (!isLoaded()) {
+			throw new InvalidArgumentException(this, "is not loaded");
+		}
+	}
+	
+	//method
+	default void assertIsNew() {
+		if (!isNew()) {
+			throw new NonNewArgumentException(this);
+		}
+	}
+	
+	//method
+	default void assertIsNotExpired() {
+		if (isExpired()) {
+			throw new ExpiredArgumentException(this);
+		}
+	}
+	
+	//method
+	DatabaseObjectState getState();
+		
+	//method
+	default boolean isEdited() {
+		return (getState() == DatabaseObjectState.EDITED);
+	}
+	
+	//method
+	default boolean isExpired() {
+		return (getState() == DatabaseObjectState.EXPIRED);
+	}
+	
+	//method
+	default boolean isLoaded() {
+		return (getState() == DatabaseObjectState.LOADED);
+	}
+	
+	//method
+	default boolean isNew() {
+		return (getState() == DatabaseObjectState.NEW);
+	}
+}
