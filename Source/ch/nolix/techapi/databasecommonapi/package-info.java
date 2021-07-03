0f633bbfccf974e2b7603163aa4734a5f789a95e
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-03 18:30:06 +0200

    created databasecommonapi package

---
+++ Source/ch/nolix/techapi/databasecommonapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.databasecommonapi} package defines common types to access databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-03
+ */
+package ch.nolix.techapi.databasecommonapi;
