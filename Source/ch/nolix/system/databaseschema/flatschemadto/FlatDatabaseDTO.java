9a2e4b187a585e108331c947d2501e848e0e5706
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-04 19:12:53 +0200

    created flatschemadto package

---
+++ Source/ch/nolix/system/databaseschema/flatschemadto/FlatDatabaseDTO.java
@@ -0,0 +1,31 @@
+//package declaration
+package ch.nolix.system.databaseschema.flatschemadto;
+
+//own imports
+import ch.nolix.common.constant.LowerCaseCatalogue;
+import ch.nolix.common.errorcontrol.invalidargumentexception.ArgumentIsNullException;
+import ch.nolix.techapi.databaseschemaapi.flatschemadtoapi.IFlatDatabaseDTO;
+
+//class
+public final class FlatDatabaseDTO implements IFlatDatabaseDTO {
+	
+	//attribute
+	private final String name;
+	
+	//constructor
+	//For a better performance, this implementation does not use all comforDatabase methods.
+	public FlatDatabaseDTO(final String name) {
+		
+		if (name == null) {
+			throw new ArgumentIsNullException(LowerCaseCatalogue.NAME);
+		}
+		
+		this.name = name;
+	}
+	
+	//method
+	@Override
+	public String getName() {
+		return name;
+	}
+}
---
+++ Source/ch/nolix/system/databaseschema/flatschemadto/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.databaseschema.flatschemadto} package defines
+ * the types that are declared in the {@link ch.nolix.techapi.databaseschemaapi.flatschemadtoapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-04
+ */
+package ch.nolix.system.databaseschema.flatschemadto;
