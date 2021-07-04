c6cd38ccdea5ab6fc93e576e5797b56e86a3265c
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-04 19:34:45 +0200

    added assertDoesNotContainColumnWithHeader method to IExtendedTable

--- Source/ch/nolix/techapi/databaseschemaapi/extendedschemaapi/IExtendedTable.java
+++ Source/ch/nolix/techapi/databaseschemaapi/extendedschemaapi/IExtendedTable.java
@@ -2,6 +2,7 @@
 package ch.nolix.techapi.databaseschemaapi.extendedschemaapi;
 
 //own imports
+import ch.nolix.common.errorcontrol.invalidargumentexception.InvalidArgumentException;
 import ch.nolix.techapi.databasecommonapi.databaseobjectapi.IDatabaseObject;
 import ch.nolix.techapi.databaseschemaapi.schemaapi.ITable;
 
@@ -11,4 +12,12 @@ public interface IExtendedTable<
 	EC extends IExtendedColumn<EC, EPPT>,
 	EPPT extends IExtendedParametrizedPropertyType<? super Object>
 >
-extends IDatabaseObject, ITable<ET, EC, EPPT> {}
+extends IDatabaseObject, ITable<ET, EC, EPPT> {
+	
+	//method
+	default void assertDoesNotContainColumnWithHeader(final String header) {
+		if (containsColumnWithHeader(header)) {
+			throw new InvalidArgumentException(this, "contains already a column with the header '" + header + "'");
+		}
+	}
+}
