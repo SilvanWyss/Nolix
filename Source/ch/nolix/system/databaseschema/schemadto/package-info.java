4428512886fbab482b9b6a33dee8382e8d367bc7
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-05 20:32:54 +0200

    adjusted package info of schemadto package

--- Source/ch/nolix/system/databaseschema/schemadto/package-info.java
+++ Source/ch/nolix/system/databaseschema/schemadto/package-info.java
@@ -1,7 +1,7 @@
 //package info
 /**
- * The {@link ch.nolix.system.databaseschema.schemadto} package defines the types that
- * are declared in the {@link ch.nolix.techapi.databaseschemaapi.schemadtoapi} package.
+ * The {@link ch.nolix.system.databaseschema.schemadto} package defines
+ * the types that are declared in the {@link ch.nolix.techapi.databaseschemaapi.schemadtoapi} package.
  * 
  * @author Silvan Wyss
  * @date 2021-07-04
