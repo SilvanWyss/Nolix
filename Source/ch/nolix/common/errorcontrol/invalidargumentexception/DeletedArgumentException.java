104743a8f16f29b60383cc68809448a6de91b280
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-12 22:10:02 +0200

    created DeletedArgumentException

---
+++ Source/ch/nolix/common/errorcontrol/invalidargumentexception/DeletedArgumentException.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.common.errorcontrol.invalidargumentexception;
+
+//class
+/**
+ * A {@link DeletedArgumentException} is a {@link InvalidArgumentException} that
+ * is supposed to be thrown when an argument is undesirably deleted.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-12
+ * @lines 30
+ */
+@SuppressWarnings("serial")
+public final class DeletedArgumentException extends InvalidArgumentException {
+	
+	//constant
+	private static final String ERROR_PREDICATE = "is deleted";
+	
+	//constructor
+	/**
+	 * Creates a new {@link DeletedArgumentException} for the given argument.
+	 * 
+	 * @param argument
+	 */
+	public DeletedArgumentException(final Object argument) {
+		
+		//Calls constructor of the base class.
+		super(argument, ERROR_PREDICATE);
+	}
+}
