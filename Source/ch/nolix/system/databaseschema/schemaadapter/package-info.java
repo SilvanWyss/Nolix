7b77a3f3d5618ef9e9c51dbe1f8564130e84eac8
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-13 20:39:32 +0200

    created schemaadapter package

---
+++ Source/ch/nolix/system/databaseschema/schemaadapter/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.databaseschema.schemaadapter} package defines
+ * a {ch.nolix.techapi.databaseschemaapi.schemaadapterapi.IDatabaseSchemaAdapter}.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-13
+ */
+package ch.nolix.system.databaseschema.schemaadapter;
