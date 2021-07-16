3cbbce2276b22c18413a2c65980e8a14496fbe8d
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-16 18:24:49 +0200

    created ReferencedArgumentException

---
+++ Source/ch/nolix/common/errorcontrol/invalidargumentexception/ReferencedArgumentException.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.common.errorcontrol.invalidargumentexception;
+
+//class
+/**
+ * A {@link ReferencedArgumentException} is a {@link InvalidArgumentException} that
+ * is supposed to be thrown when an argument is undesirably referenced.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-16
+ * @lines 30
+ */
+@SuppressWarnings("serial")
+public final class ReferencedArgumentException extends InvalidArgumentException {
+	
+	//constant
+	private static final String ERROR_PREDICATE = "is referenced";
+	
+	//constructor
+	/**
+	 * Creates a new {@link ReferencedArgumentException} for the given argument.
+	 * 
+	 * @param argument
+	 */
+	public ReferencedArgumentException(final Object argument) {
+		
+		//Calls constructor of the base class.
+		super(argument, ERROR_PREDICATE);
+	}
+}
