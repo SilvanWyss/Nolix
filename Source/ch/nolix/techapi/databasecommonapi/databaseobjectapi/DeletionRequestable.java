276a80cad617b7d8d09b3c7fb1a47488d13a4081
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-16 18:09:57 +0200

    created DeletionRequestable

---
+++ Source/ch/nolix/techapi/databasecommonapi/databaseobjectapi/DeletionRequestable.java
@@ -0,0 +1,9 @@
+//package declaration
+package ch.nolix.techapi.databasecommonapi.databaseobjectapi;
+
+//interface
+public interface DeletionRequestable {
+	
+	//method declaration
+	boolean isDeleted();
+}
