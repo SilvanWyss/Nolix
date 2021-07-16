b61f2170e37b5d167a76169d8cb53f777b2ec6a6
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-16 21:48:38 +0200

    created NewArgumentException

---
+++ Source/ch/nolix/common/errorcontrol/invalidargumentexception/NewArgumentException.java
@@ -0,0 +1,31 @@
+//package declaration
+package ch.nolix.common.errorcontrol.invalidargumentexception;
+
+//class
+/**
+ * A {@link NewArgumentException} is a {@link InvalidArgumentException} that
+ * is supposed to be thrown when a given argument is undesirably not new.
+ * A {@link NewArgumentException} is not mutable.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-16
+ * @lines 30
+ */
+@SuppressWarnings("serial")
+public final class NewArgumentException extends InvalidArgumentException {
+	
+	//constant
+	private static final String ERROR_PREDICATE = "is new";
+	
+	//constructor
+	/**
+	 * Creates a new {@link NewArgumentException} for the given argument.
+	 * 
+	 * @param argument
+	 */
+	public NewArgumentException(final Object argument) {
+		
+		//Calls constructor of the base class.
+		super(argument, ERROR_PREDICATE);
+	}
+}
