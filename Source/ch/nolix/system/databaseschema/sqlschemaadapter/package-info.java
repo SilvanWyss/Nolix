f08d2a5ed755d1e31a60637749bbf1b0f518551d
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-17 13:27:36 +0200

    created sqlschemaadapter package

---
+++ Source/ch/nolix/system/databaseschema/sqlschemaadapter/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.databaseschema.sqlschemaadapter} package defines
+ * a base {ch.nolix.system.databaseschema.schemaadapter.DatabaseSchemaAdapter} for SQL databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-17
+ */
+package ch.nolix.system.databaseschema.sqlschemaadapter;
