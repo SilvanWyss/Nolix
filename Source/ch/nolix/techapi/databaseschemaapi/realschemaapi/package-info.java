12d0fb8f47eda2e99fa4758c89b4d860e1bb7aa9
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-17 16:52:34 +0200

    created realschemaapi package

---
+++ Source/ch/nolix/techapi/databaseschemaapi/realschemaapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.databaseschemaapi.realschemaapi} package declares
+ * types to access the schema of real databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-07-17
+ */
+package ch.nolix.techapi.databaseschemaapi.realschemaapi;
