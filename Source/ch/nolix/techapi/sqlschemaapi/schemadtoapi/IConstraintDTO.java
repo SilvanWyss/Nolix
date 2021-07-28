778a680ac6d85d484147c9e84baeaabf0eaaec90
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-28 20:57:52 +0200

    edited IConstraintDTO

--- Source/ch/nolix/techapi/sqlschemaapi/schemadtoapi/IConstraintDTO.java
+++ Source/ch/nolix/techapi/sqlschemaapi/schemadtoapi/IConstraintDTO.java
@@ -1,15 +1,15 @@
 //package declaration
 package ch.nolix.techapi.sqlschemaapi.schemadtoapi;
 
-//own imports
-import ch.nolix.common.container.SingleContainer;
-
 //interface
 public interface IConstraintDTO {
 	
 	//method declaration
-	SingleContainer<String> getOptionalParameter();
+	String getParameter();
 	
 	//method declaration
 	ConstraintType getType();
+	
+	//method declaration
+	boolean hasParameter();
 }
