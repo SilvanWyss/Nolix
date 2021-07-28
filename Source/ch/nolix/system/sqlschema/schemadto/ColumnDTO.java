c28a881b92c6204f7c203b98245d73fc911a9b00
Silvan Wyss <silvan.wyss@gmail.com>
2021-07-28 21:43:26 +0200

    added additional constructor to ColumnDTO

--- Source/ch/nolix/system/sqlschema/schemadto/ColumnDTO.java
+++ Source/ch/nolix/system/sqlschema/schemadto/ColumnDTO.java
@@ -4,6 +4,7 @@ package ch.nolix.system.sqlschema.schemadto;
 //own imports
 import ch.nolix.common.constant.LowerCaseCatalogue;
 import ch.nolix.common.container.IContainer;
+import ch.nolix.common.container.LinkedList;
 import ch.nolix.common.errorcontrol.validator.Validator;
 import ch.nolix.techapi.sqlschemaapi.schemadtoapi.IColumnDTO;
 import ch.nolix.techapi.sqlschemaapi.schemadtoapi.IConstraintDTO;
@@ -11,12 +12,20 @@ import ch.nolix.techapi.sqlschemaapi.schemadtoapi.IConstraintDTO;
 //class
 public final class ColumnDTO implements IColumnDTO {
 	
+	//constant
+	private static final IContainer<IConstraintDTO> EMPTY_CONSTRAINT_LIST = new LinkedList<>();
+	
 	//attributes
 	private final String name;
 	private final String dataType;
 	
 	//multi-attribute
 	private final IContainer<IConstraintDTO> constraints;
+	
+	//constructor
+	public ColumnDTO(final String name, final String dataType) {
+		this(name, dataType, EMPTY_CONSTRAINT_LIST);
+	}
 	
 	//constructor
 	public ColumnDTO(final String name, final String dataType, final IContainer<IConstraintDTO> constraints) {
