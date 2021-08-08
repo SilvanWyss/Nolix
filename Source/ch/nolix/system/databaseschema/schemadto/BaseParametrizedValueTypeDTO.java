9dd69047e53a5f229cfff091962f36fcef708fc7
Silvan Wyss <silvan.wyss@gmail.com>
2021-08-08 11:17:42 +0200

    created IBaseParametrizedValueTypeDTO

--- Source/ch/nolix/system/databaseschema/schemadto/BaseParametrizedValueTypeDTO.java
+++ Source/ch/nolix/system/databaseschema/schemadto/BaseParametrizedValueTypeDTO.java
@@ -3,9 +3,11 @@ package ch.nolix.system.databaseschema.schemadto;
 
 //own imports
 import ch.nolix.techapi.databasecommonapi.propertytypeapi.PropertyType;
+import ch.nolix.techapi.databaseschemaapi.schemadtoapi.IBaseParametrizedValueTypeDTO;
 
 //class
-public final class BaseParametrizedValueTypeDTO extends ParametrizedPropertyTypeDTO {
+public final class BaseParametrizedValueTypeDTO extends ParametrizedPropertyTypeDTO
+implements IBaseParametrizedValueTypeDTO {
 	
 	//constructor
 	public BaseParametrizedValueTypeDTO(final PropertyType propertyType, final String dataTypeFullClassName) {
---
+++ Source/ch/nolix/techapi/databaseschemaapi/schemadtoapi/IBaseParametrizedValueTypeDTO.java
@@ -0,0 +1,5 @@
+//package declaration
+package ch.nolix.techapi.databaseschemaapi.schemadtoapi;
+
+//interface
+public interface IBaseParametrizedValueTypeDTO extends IParametrizedPropertyTypeDTO {}
