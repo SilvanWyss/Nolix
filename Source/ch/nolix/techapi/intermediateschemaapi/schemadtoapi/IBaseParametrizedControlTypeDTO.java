4651de155913bcedac15028cffcb1a56faeffc2d
Silvan Wyss <silvan.wyss@gmail.com>
2021-08-12 18:25:59 +0200

    created IBaseParametrizedControlTypeDTO

---
+++ Source/ch/nolix/techapi/intermediateschemaapi/schemadtoapi/IBaseParametrizedControlTypeDTO.java
@@ -0,0 +1,5 @@
+//package declaration
+package ch.nolix.techapi.intermediateschemaapi.schemadtoapi;
+
+//interface
+public interface IBaseParametrizedControlTypeDTO extends IParametrizedPropertyTypeDTO {}
