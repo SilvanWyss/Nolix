c09d576088e8d2d4a0e0dc0e2999d4de68415d1f
Silvan Wyss <silvan.wyss@gmail.com>
2021-08-12 18:33:13 +0200

    created SQLSyntaxCatalogue

---
+++ Source/ch/nolix/common/sql/SQLSyntaxCatalogue.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.common.sql;
+
+//class
+public final class SQLSyntaxCatalogue {
+	
+	//constant
+	public static final String NULL = "NULL";
+	
+	//constructor
+	private SQLSyntaxCatalogue() {}
+}
