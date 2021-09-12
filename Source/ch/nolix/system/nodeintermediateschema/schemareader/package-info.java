cdc4cb4799fd5827d0538eaf713d3bfc86d57811
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-12 12:32:11 +0200

    created schemareader package

---
+++ Source/ch/nolix/system/nodeintermediateschema/schemareader/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.nodeintermediateschema.schemareader} package defines
+ * a {@link ch.nolix.techapi.intermediateschemaapi.schemaadapterapi.ISchemaReader}.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-09-12
+ */
+package ch.nolix.system.nodeintermediateschema.schemareader;
