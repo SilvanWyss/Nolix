a2867c42e7598411b607af9c84b66ad4a2ac304a
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-12 12:27:57 +0200

    extended SubNodeHeaderCatalogue

--- Source/ch/nolix/system/nodeintermediateschema/structure/SubNodeHeaderCatalogue.java
+++ Source/ch/nolix/system/nodeintermediateschema/structure/SubNodeHeaderCatalogue.java
@@ -5,7 +5,7 @@ package ch.nolix.system.nodeintermediateschema.structure;
 import ch.nolix.common.constant.PascalCaseCatalogue;
 
 //class
-public final class SubNodeHeaderCatalogue {
+final class SubNodeHeaderCatalogue {
 	
 	//constants
 	public static final String DATABASE = PascalCaseCatalogue.DATABASE;
@@ -15,12 +15,13 @@ public final class SubNodeHeaderCatalogue {
 	public static final String NAME = PascalCaseCatalogue.NAME;
 	public static final String COLUMN = PascalCaseCatalogue.COLUMN;
 	public static final String HEADER = PascalCaseCatalogue.HEADER;
+	public static final String PARAMETRIZED_PROPERTY_TYPE = "ParametrizedPropertyType";
 	public static final String PROPERTY_TYPE = "PropertyType";
 	public static final String DATA_TYPE = PascalCaseCatalogue.DATA_TYPE;
 	public static final String REFERENCED_TABLE = "ReferencedTable";
 	public static final String BACK_REFERENCED_TABLE = "BackReferencedTable";
 	public static final String BACK_REFERENCED_COLUMN = "BackReferencedColumn";
-	public static final String Entity = PascalCaseCatalogue.ENTITY;
+	public static final String ENTITY = PascalCaseCatalogue.ENTITY;
 	
 	//constructor
 	private SubNodeHeaderCatalogue() {}
