20e1bcd9ba48da6922bb9883baac9b242df7f164
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-12 15:33:11 +0200

    created schemawriter package

---
+++ Source/ch/nolix/system/nodeintermediateschema/schemawriter/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.nodeintermediateschema.schemawriter} package defines
+ * a {@link ch.nolix.techapi.intermediateschemaapi.schemaadapterapi.ISchemaWriter}.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-09-12
+ */
+package ch.nolix.system.nodeintermediateschema.schemawriter;
