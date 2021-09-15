86556906684593336a53817519db7abfe324099d
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-15 21:27:33 +0200

    created schemaadaptertutorial package

---
+++ Source/ch/nolix/systemtutorial/databaseschematutorial/schemaadaptertutorial/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtutorial.databaseschematutorial.schemaadaptertutorial} package defines
+ * tutorials for the {@link ch.nolix.system.databaseschema.schemaadapter} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-09-15
+ * @lines 10
+ */
+package ch.nolix.systemtutorial.databaseschematutorial.schemaadaptertutorial;
