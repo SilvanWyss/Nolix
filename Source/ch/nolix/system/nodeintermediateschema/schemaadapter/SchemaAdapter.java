c318c4b5d16bc202d8215c67e0589c2cc8da126d
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-15 21:49:40 +0200

    fixed SchemaAdapter

--- Source/ch/nolix/system/nodeintermediateschema/schemaadapter/SchemaAdapter.java
+++ Source/ch/nolix/system/nodeintermediateschema/schemaadapter/SchemaAdapter.java
@@ -4,7 +4,6 @@ package ch.nolix.system.nodeintermediateschema.schemaadapter;
 //own imports
 import ch.nolix.common.document.filenode.FileNode;
 import ch.nolix.common.document.node.BaseNode;
-import ch.nolix.common.errorcontrol.validator.Validator;
 import ch.nolix.system.nodeintermediateschema.databaseinitializer.DatabaseInitializer;
 import ch.nolix.system.nodeintermediateschema.schemareader.SchemaReader;
 import ch.nolix.system.nodeintermediateschema.schemawriter.SchemaWriter;
@@ -28,28 +27,28 @@ public final class SchemaAdapter implements ISchemaAdapter {
 		return new SchemaAdapter(new FileNode(filePath));
 	}
 	
-	//attribute
-	private final BaseNode databaseNode;
+	//attributes
+	private final ISchemaReader schemaReader;
+	private final ISchemaWriter schemaWriter;
 	
 	//constructor
 	private SchemaAdapter(final BaseNode databaseNode) {
 		
-		Validator.assertThat(databaseNode).thatIsNamed("database Node").isNotNull();
-		
 		databaseInitializer.initializeDatabaseIfNotInitialized(databaseNode);
 		
-		this.databaseNode = databaseNode;
+		schemaReader = new SchemaReader(databaseNode);
+		schemaWriter = new SchemaWriter(databaseNode);
 	}
 	
 	//method
 	@Override
 	public ISchemaReader getRefSchemaReader() {
-		return new SchemaReader(databaseNode);
+		return schemaReader;
 	}
 	
 	//method
 	@Override
 	public ISchemaWriter getRefSchemaWriter() {
-		return new SchemaWriter(databaseNode);
+		return schemaWriter;
 	}
 }
