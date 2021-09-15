d7a71db837f9bb24f1de3cea4986896ba58bbdae
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-15 21:26:25 +0200

    created databaseschematutorial package

---
+++ Source/ch/nolix/systemtutorial/databaseschematutorial/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtutorial.databaseschematutorial} package defines
+ * tutorials for the {@link ch.nolix.system.databaseschema} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-09-15
+ * @lines 10
+ */
+package ch.nolix.systemtutorial.databaseschematutorial;
