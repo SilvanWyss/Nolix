892e5bd66135e2e79d5e81b149903cab30d3ca6e
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-20 22:20:10 +0200

    created ICellDTO

---
+++ Source/ch/nolix/techapi/intermediatedataapi/recorddtoapi/ICellDTO.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.techapi.intermediatedataapi.recorddtoapi;
+
+//interface
+public interface ICellDTO {
+	
+	//method declaration
+	String getColumnHeader();
+	
+	//method declaration
+	String getValue();
+}
