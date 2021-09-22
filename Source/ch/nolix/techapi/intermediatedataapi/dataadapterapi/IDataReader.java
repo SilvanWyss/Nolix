f529c39edc4218694d91002712d202290df2ad1e
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-22 22:03:54 +0200

    fixed IDataReader

--- Source/ch/nolix/techapi/intermediatedataapi/dataadapterapi/IDataReader.java
+++ Source/ch/nolix/techapi/intermediatedataapi/dataadapterapi/IDataReader.java
@@ -2,12 +2,12 @@
 package ch.nolix.techapi.intermediatedataapi.dataadapterapi;
 
 //own imports
-import ch.nolix.common.independent.independentcontainer.List;
+import ch.nolix.common.container.LinkedList;
 import ch.nolix.techapi.intermediatedataapi.recorddtoapi.IRecordDTO;
 
 //interface
 public interface IDataReader {
 	
 	//method declaration
-	List<IRecordDTO> loadAllRecordsFromTable(String tableName);
+	LinkedList<IRecordDTO> loadAllRecordsFromTable(String tableName);
 }
