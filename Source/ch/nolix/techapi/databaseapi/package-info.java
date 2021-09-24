b87820013775dd4378af0d1f0a1d32c1616be4e4
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-24 22:51:19 +0200

    deleted databaseapi package

--- Source/ch/nolix/techapi/databaseapi/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.techapi.databaseapi} package defines entities that can be persisted in a database.
- * 
- * @author Silvan Wyss
- * @date 2021-06-30
- */
-package ch.nolix.techapi.databaseapi;
