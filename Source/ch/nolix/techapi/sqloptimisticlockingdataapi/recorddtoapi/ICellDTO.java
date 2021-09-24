92db3c9004574c2cb9ee2b4f4cf44350f36e793d
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-24 20:02:58 +0200

    created CellDTO

---
+++ Source/ch/nolix/techapi/sqloptimisticlockingdataapi/recorddtoapi/ICellDTO.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.techapi.sqloptimisticlockingdataapi.recorddtoapi;
+
+//interface
+public interface ICellDTO {
+    
+    //method declaration
+    String getColumnHeader();
+    
+    //method declaration
+    String getValue();
+}
