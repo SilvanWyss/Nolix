31d7a4e40e6794a428bca8f082ac01542b28d955
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-26 13:48:45 +0200

    created objectdataapi package

---
+++ Source/ch/nolix/techapi/objectdataapi/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The {@link ch.nolix.techapi.objectdataapi} package declares types to access the data of databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-09-26
+ */
+package ch.nolix.techapi.objectdataapi;
