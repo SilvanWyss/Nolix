2607b379e6df16743f8f3fdb08ca59c46e6002c0
Silvan Wyss <silvan.wyss@gmail.com>
2021-09-26 23:07:59 +0200

    made IEntity a Deletable

--- Source/ch/nolix/techapi/objectdataapi/dataapi/IEntity.java
+++ Source/ch/nolix/techapi/objectdataapi/dataapi/IEntity.java
@@ -4,7 +4,9 @@ package ch.nolix.techapi.objectdataapi.dataapi;
 //own imports
 import ch.nolix.common.attributeapi.mandatoryattributeapi.IdentifiedByString;
 import ch.nolix.common.attributeapi.mandatoryattributeapi.ShortDescripted;
+import ch.nolix.techapi.databaseapi.databaseobjectapi.Deletable;
 import ch.nolix.techapi.databaseapi.databaseobjectapi.IDatabaseObject;
 
 //interface
-public interface IEntity<E extends IEntity<E>> extends IDatabaseObject, IdentifiedByString, ShortDescripted {}
+public interface IEntity<E extends IEntity<E>>
+extends Deletable, IDatabaseObject, IdentifiedByString, ShortDescripted {}
