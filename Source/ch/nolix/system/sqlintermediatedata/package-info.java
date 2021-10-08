24d4c50a6c9a6a785e26e294500d9ccc5a1239f6
Silvan Wyss <silvan.wyss@gmail.com>
2021-10-08 13:57:28 +0200

    created sqlintermediatedata package

---
+++ Source/ch/nolix/system/sqlintermediatedata/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.sqlintermediatedata} package defines
+ * the types that are declared in the {@link ch.nolix.techapi.intermediatedataapi} package for SQL databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-08-10
+ */
+package ch.nolix.system.sqlintermediatedata;
