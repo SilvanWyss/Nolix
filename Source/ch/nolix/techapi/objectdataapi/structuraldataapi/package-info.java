dd4d294604308f58a25ee4001474b64194225219
Silvan Wyss <silvan.wyss@gmail.com>
2021-10-13 22:29:31 +0200

    created structuraldataapi package

---
+++ Source/ch/nolix/techapi/objectdataapi/structuraldataapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.objectdataapi.structuraldataapi} package declares
+ * types for the data of databases with methods to access properties.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-10-13
+ */
+package ch.nolix.techapi.objectdataapi.structuraldataapi;
