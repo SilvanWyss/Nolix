10be71f657ec6f93f4d1b8ce2195b2e263573bdf
Silvan Wyss <silvan.wyss@gmail.com>
2021-10-17 11:26:11 +0200

    created nodeintermediatedata package

---
+++ Source/ch/nolix/system/nodeintermediatedata/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.nodeintermediatedata} package defines
+ * the types that are declared in the {@link ch.nolix.techapi.intermediatedataapi} package for
+ * databases that base on {@link ch.nolix.common.document.node.BaseNode}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-10-17
+ */
+package ch.nolix.system.nodeintermediatedata;
