d0b7a5a0bc34fef5a811742a179d4d202b818818
Silvan Wyss <silvan.wyss@gmail.com>
2021-10-22 18:15:35 +0200

    extended PortCatalogue

--- Source/ch/nolix/common/constant/PortCatalogue.java
+++ Source/ch/nolix/common/constant/PortCatalogue.java
@@ -17,6 +17,7 @@ public final class PortCatalogue {
 	public static final int SMTP_PORT = 25;
 	public static final int HTTP_PORT = 80;
 	public static final int HTTPS_PORT = 443;
+	public static final int MSSQL_PORT = 1433;
 	public static final int MAX_PORT = 65535;
 	
 	//constructor
