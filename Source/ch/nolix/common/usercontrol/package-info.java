b95f245942fe55a4f754ea7747f5f5c1935ddfa2
Silvan Wyss <silvan.wyss@gmail.com>
2021-10-22 17:51:17 +0200

    created usercontrol package

---
+++ Source/ch/nolix/common/usercontrol/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.usercontrol} package defines functionalities to control users.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-10-22
+ * @lines 10
+ */
+package ch.nolix.common.usercontrol;
