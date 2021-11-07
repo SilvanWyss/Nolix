5f93fd8bd4dc34fcb820796ccc5f7c99ed5a84ea
Silvan Wyss <silvan.wyss@gmail.com>
2021-11-07 14:58:11 +0100

    added parentEntity to Property

--- Source/ch/nolix/system/objectdata/data/Property.java
+++ Source/ch/nolix/system/objectdata/data/Property.java
@@ -2,7 +2,38 @@
 package ch.nolix.system.objectdata.data;
 
 //own imports
+import ch.nolix.common.errorcontrol.validator.Validator;
+import ch.nolix.techapi.objectdataapi.dataapi.IEntity;
 import ch.nolix.techapi.objectdataapi.extendeddataapi.IExtendedProperty;
 
 //class
-public abstract class Property implements IExtendedProperty<Property> {}
+public abstract class Property implements IExtendedProperty<Property> {
+	
+	//optional attribute
+	private Entity parentEntity;
+	
+	//method
+	@Override
+	public final boolean belongsToEntity() {
+		return (parentEntity != null);
+	}
+	
+	//method
+	@Override
+	public final IEntity<?, ?> getParentEntity() {
+		
+		assertBelongsToEntity();
+		
+		return parentEntity;
+	}
+	
+	//method
+	void setParentEntity(final Entity parentEntity) {
+		
+		Validator.assertThat(parentEntity).thatIsNamed("parent entity").isNotNull();
+		
+		assertDoesNotBelongToEntity();
+		
+		this.parentEntity = parentEntity;
+	}
+}
