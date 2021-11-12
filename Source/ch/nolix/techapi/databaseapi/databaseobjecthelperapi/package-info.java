25b269849d3fa75694673e084c714d0ec8a3d589
Silvan Wyss <silvan.wyss@gmail.com>
2021-11-12 18:33:33 +0100

    created databaseobjecthelperapi package

---
+++ Source/ch/nolix/techapi/databaseapi/databaseobjecthelperapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.databaseapi.databaseobjecthelperapi} package declares
+ * helpers for the types of {@link ch.nolix.techapi.databaseapi.databaseobjectapi}.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-11-12
+ */
+package ch.nolix.techapi.databaseapi.databaseobjecthelperapi;
