7f03fb93ebcccd20c10fecfa27f0b09ac1f986e2
Silvan Wyss <silvan.wyss@gmail.com>
2021-11-13 13:57:51 +0100

    created datahelper package

---
+++ Source/ch/nolix/system/objectdata/datahelper/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.objectdata.datahelper} packages implements
+ * the types that are declared in the {@link ch.nolix.techapi.objectdataapi.datahelperapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-11-13
+ */
+package ch.nolix.system.objectdata.datahelper;
