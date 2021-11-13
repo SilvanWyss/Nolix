5a42ab575161cb30c203aff24fc4060552413df8
Silvan Wyss <silvan.wyss@gmail.com>
2021-11-13 12:16:12 +0100

    created databaseobjecthelper package

---
+++ Source/ch/nolix/system/database/databaseobjecthelper/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.database.databaseobjecthelper} package defines
+ * the types that are declared in the {@link ch.nolix.techapi.databaseapi.databaseobjecthelperapi}.
+ * 
+ * @author Silvan Wyss
+ * @date 2021-11-13
+ */
+package ch.nolix.system.database.databaseobjecthelper;
