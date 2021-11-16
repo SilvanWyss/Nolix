379af5ff572f511f22c8ce005ec1e863c078d3af
Silvan Wyss <silvan.wyss@gmail.com>
2021-11-16 22:10:18 +0100

    fixed package info

--- Source/ch/nolix/system/noderawobjectschema/structure/package-info.java
+++ Source/ch/nolix/system/noderawobjectschema/structure/package-info.java
@@ -1,8 +1,6 @@
 //package info
 /**
- * The {@link ch.nolix.system.noderawobjectschema.structure} package defines types for the structure of
- * {ch.nolix.techapi.intermediateschemaapi.schemaadapterapi.IIntermediateSchemaAdapter} for databases that base on
- * {@link ch.nolix.common.document.node.BaseNode}s.
+ * The {@link ch.nolix.system.noderawobjectschema.structure} package defines types for the structure.
  * 
  * @author Silvan Wyss
  * @date 2021-09-11
