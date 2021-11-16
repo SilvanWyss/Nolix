d5df5fcb797374d81d363a942ba4a5428db88c56
Silvan Wyss <silvan.wyss@gmail.com>
2021-11-16 22:16:22 +0100

    fixed package info

--- Source/ch/nolix/system/sqlrawobjectschema/structure/package-info.java
+++ Source/ch/nolix/system/sqlrawobjectschema/structure/package-info.java
@@ -1,8 +1,6 @@
 //package info
 /**
- * The {@link ch.nolix.system.sqlrawobjectschema.structure} package defines
- * types for the structure of
- * a SQL {ch.nolix.techapi.intermediateschemaapi.schemaadapterapi.IIntermediateSchemaAdapter}.
+ * The {@link ch.nolix.system.sqlrawobjectschema.structure} package defines types for the structure.
  * 
  * @author Silvan Wyss
  * @date 2021-08-29
