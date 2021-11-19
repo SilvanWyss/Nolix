8387d4567e01d425687f9f2eac54a5242a80df22
Silvan Wyss <silvan.wyss@gmail.com>
2021-11-19 22:46:25 +0100

    created ITableHelper

---
+++ Source/ch/nolix/system/objectdata/datahelper/TableHelper.java
@@ -0,0 +1,36 @@
+//package declaration
+package ch.nolix.system.objectdata.datahelper;
+
+//own imports
+import ch.nolix.system.database.databaseobjecthelper.DatabaseObjectHelper;
+import ch.nolix.techapi.objectdataapi.dataapi.IEntity;
+import ch.nolix.techapi.objectdataapi.dataapi.ITable;
+import ch.nolix.techapi.objectdataapi.datahelperapi.ITableHelper;
+
+//class
+public final class TableHelper extends DatabaseObjectHelper implements ITableHelper {
+	
+	//static attribute
+	private static final EntityHelper entityHelper = new EntityHelper();
+	
+	//method
+	@Override
+	public boolean canInsertEntity(final ITable<?, ?, ?> table) {
+		return table.isOpen();
+	}
+	
+	//method
+	@Override
+	public boolean canInsertGivenEntity(ITable<?, ?, ?> table, IEntity<?, ?> entity) {
+		return
+		canInsertEntity(table)
+		&& entityHelper.canBeInsertedIntoTable(entity)
+		&& hasInsertedGivenEntity(table, entity);
+	}
+	
+	//method
+	@Override
+	public boolean hasInsertedGivenEntity(final ITable<?, ?, ?> table, final IEntity<?, ?> entity) {
+		return table.hasInsertedEntityWithId(entity.getId());
+	}
+}
---
+++ Source/ch/nolix/techapi/objectdataapi/datahelperapi/ITableHelper.java
@@ -0,0 +1,20 @@
+//package declaration
+package ch.nolix.techapi.objectdataapi.datahelperapi;
+
+//own imports
+import ch.nolix.techapi.databaseapi.databaseobjecthelperapi.IDatabaseObjectHelper;
+import ch.nolix.techapi.objectdataapi.dataapi.IEntity;
+import ch.nolix.techapi.objectdataapi.dataapi.ITable;
+
+//interface
+public interface ITableHelper extends IDatabaseObjectHelper {
+	
+	//method declaration
+	boolean canInsertEntity(ITable<?, ?, ?> table);
+	
+	//method declaration
+	boolean canInsertGivenEntity(ITable<?, ?, ?> table, IEntity<?, ?> entity);
+	
+	//method declaration
+	boolean hasInsertedGivenEntity(ITable<?, ?, ?> table, IEntity<?, ?> entity);
+}
