878eca56605a95805b4e05c91a86d4be8c291e09
Silvan Wyss <silvan.wyss@gmail.com>
2021-12-10 14:33:22 +0100

    extended IMultiValue

--- Source/ch/nolix/techapi/objectdataapi/dataapi/IMultiValue.java
+++ Source/ch/nolix/techapi/objectdataapi/dataapi/IMultiValue.java
@@ -3,9 +3,26 @@ package ch.nolix.techapi.objectdataapi.dataapi;
 
 //own imports
 import ch.nolix.common.container.IContainer;
+import ch.nolix.common.skillapi.Clearable;
 
 //interface
 public interface IMultiValue<
 	IMPL,
 	V
-> extends IContainer<V>, IBaseValue<IMPL, V> {}
+> extends Clearable, IBaseValue<IMPL, V>, IContainer<V> {
+	
+	//method declaration
+	void addValue(V value);
+	
+	//method
+	@Override
+	default boolean containsAny() {
+		return IContainer.super.containsAny();
+	}
+	
+	//method
+	@Override
+	default boolean isEmpty() {
+		return IContainer.super.isEmpty();
+	}
+}
