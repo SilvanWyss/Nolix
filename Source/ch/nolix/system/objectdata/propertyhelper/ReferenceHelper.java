3c394251555624e1b228292567ad5860c75f277b
Silvan Wyss <silvan.wyss@gmail.com>
2021-12-21 21:52:54 +0100

    extended IReferenceHelper

--- Source/ch/nolix/system/objectdata/propertyhelper/ReferenceHelper.java
+++ Source/ch/nolix/system/objectdata/propertyhelper/ReferenceHelper.java
@@ -3,6 +3,7 @@ package ch.nolix.system.objectdata.propertyhelper;
 
 //own imports
 import ch.nolix.common.errorcontrol.invalidargumentexception.InvalidArgumentException;
+import ch.nolix.techapi.objectdataapi.dataapi.IEntity;
 import ch.nolix.techapi.objectdataapi.dataapi.IReference;
 import ch.nolix.techapi.objectdataapi.propertyhelperapi.IReferenceHelper;
 
@@ -15,5 +16,31 @@ public final class ReferenceHelper extends PropertyHelper implements IReferenceH
 		if (!reference.referencesEntity()) {
 			throw new InvalidArgumentException(reference, "does not reference an entity");
 		}
+	}
+	
+	//method
+	@Override
+	public void assertCanSetGivenEntity(final IReference<?, ?> reference, final IEntity<?> entity) {
+		if (!canSetGivenEntity(reference, entity)) {
+			throw new InvalidArgumentException(reference, "cannot reference the given entity");
+		}
+	}
+	
+	//method
+	@Override
+	public boolean canSetGivenEntity(final IReference<?, ?> reference, final IEntity<?> entity) {
+		return
+		canSetEntity(reference)
+		&& entity != null
+		&& entity.isOpen()
+		&& reference.getReferencedTableName().equals(entity.getTableName());
+	}
+	
+	//method
+	private boolean canSetEntity(final IReference<?, ?> reference) {
+		return
+		reference != null
+		&& reference.belongsToEntity()
+		&& reference.getParentEntity().isOpen();
 	}
 }
--- Source/ch/nolix/techapi/objectdataapi/propertyhelperapi/IReferenceHelper.java
+++ Source/ch/nolix/techapi/objectdataapi/propertyhelperapi/IReferenceHelper.java
@@ -2,11 +2,18 @@
 package ch.nolix.techapi.objectdataapi.propertyhelperapi;
 
 //own imports
+import ch.nolix.techapi.objectdataapi.dataapi.IEntity;
 import ch.nolix.techapi.objectdataapi.dataapi.IReference;
 
 //interface
 public interface IReferenceHelper extends IPropertyHelper {
 	
 	//method declaration
+	void assertCanSetGivenEntity(final IReference<?, ?> reference, final IEntity<?> entity);
+	
+	//method declaration
 	void assertReferencesEntity(IReference<?, ?> referenc);
+	
+	//method
+	boolean canSetGivenEntity(final IReference<?, ?> reference, final IEntity<?> entity);
 }
