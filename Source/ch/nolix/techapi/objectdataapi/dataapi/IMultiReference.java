ce54961168ada5e404f6bf71ce3180c8ce9a4d44
Silvan Wyss <silvan.wyss@gmail.com>
2021-12-21 12:22:35 +0100

    extended IMultiReference

--- Source/ch/nolix/techapi/objectdataapi/dataapi/IMultiReference.java
+++ Source/ch/nolix/techapi/objectdataapi/dataapi/IMultiReference.java
@@ -3,9 +3,26 @@ package ch.nolix.techapi.objectdataapi.dataapi;
 
 //own imports
 import ch.nolix.common.container.IContainer;
+import ch.nolix.common.skillapi.Clearable;
 
 //interface
 public interface IMultiReference<
 	IMPL,
 	E extends IEntity<IMPL>
-> extends IContainer<E>, IBaseReference<IMPL, E> {}
+> extends Clearable, IContainer<E>, IBaseReference<IMPL, E> {
+	
+	//method declaration
+	void addEntity(E entity);
+	
+	//method
+	@Override
+	default boolean containsAny() {
+		return IContainer.super.containsAny();
+	}
+	
+	//method
+	@Override
+	default boolean isEmpty() {
+		return IContainer.super.isEmpty();
+	}
+}
