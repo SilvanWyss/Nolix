134c52c5d2ac76fe0b63628ee84744b4465d8181
Silvan Wyss <silvan.wyss@gmail.com>
2022-01-01 18:18:00 +0100

    created DatabasePropertyTableSQLDTOCatalogue

--- Source/ch/nolix/system/sqlrawobjectschema/databaseinitializer/InternalDatabaseInitializer.java
+++ Source/ch/nolix/system/sqlrawobjectschema/databaseinitializer/InternalDatabaseInitializer.java
@@ -3,7 +3,7 @@ package ch.nolix.system.sqlrawobjectschema.databaseinitializer;
 
 //own imports
 import ch.nolix.system.sqlrawobjectschema.columntable.ColumnTableSQLDTOCatalogue;
-import ch.nolix.system.sqlrawobjectschema.structure.SystemDataTableSQLDTOCatalogue;
+import ch.nolix.system.sqlrawobjectschema.databasepropertytable.DatabasePropertyTableSQLDTOCatalogue;
 import ch.nolix.system.sqlrawobjectschema.tabletable.TableTableSQLDTOCatalogue;
 import ch.nolix.techapi.sqlschemaapi.schemaadapterapi.ISchemaAdapter;
 
@@ -13,7 +13,7 @@ final class InternalDatabaseInitializer {
 	//method
 	public void initializeDatabase(final ISchemaAdapter schemaAdapter) {
 		
-		schemaAdapter.addTable(SystemDataTableSQLDTOCatalogue.DATABASE_PROPERTY_SYSTEM_TABLE_SQL_DTO);
+		schemaAdapter.addTable(DatabasePropertyTableSQLDTOCatalogue.DATABASE_PROPERTY_TABLE_SQL_DTO);
 		schemaAdapter.addTable(TableTableSQLDTOCatalogue.TABLE_TABLE_SQL_DTO);
 		schemaAdapter.addTable(ColumnTableSQLDTOCatalogue.COLUMN_TABLE_SQL_DTO);
 		
