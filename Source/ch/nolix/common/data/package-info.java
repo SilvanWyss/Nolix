4527889b20f1426c77ea5e9f131480930b2562cf
Silvan Wyss <silvan.wyss@gmail.com>
2022-01-20 22:25:07 +0100

    created data package

---
+++ Source/ch/nolix/common/data/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.common.data} package defines functionalities for data and persistence.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-01-20
+ * @lines 10
+ */
+package ch.nolix.common.data;
