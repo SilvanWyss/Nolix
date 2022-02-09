2520a7dda5d3a649871377d19f087c27a0d8b9e8
Heocro <silvan.wyss@gmail.com>
2022-02-09 22:03:23 +0100

    edited SchemaWriter

--- Source/ch/nolix/system/sqlschema/schemaadapter/SchemaWriter.java
+++ Source/ch/nolix/system/sqlschema/schemaadapter/SchemaWriter.java
@@ -1,10 +1,11 @@
 //package declaration
 package ch.nolix.system.sqlschema.schemaadapter;
 
+//own imports
 import ch.nolix.core.container.IContainer;
 import ch.nolix.core.errorcontrol.validator.Validator;
+import ch.nolix.core.sql.SQLCollector;
 import ch.nolix.core.sql.SQLConnection;
-import ch.nolix.core.sql.SQLExecutor;
 import ch.nolix.systemapi.sqlschemaapi.schemaadapterapi.ISchemaWriter;
 import ch.nolix.systemapi.sqlschemaapi.schemadtoapi.IColumnDTO;
 import ch.nolix.systemapi.sqlschemaapi.schemadtoapi.ITableDTO;
@@ -13,59 +14,67 @@ import ch.nolix.systemapi.sqlschemaapi.schemalanguageapi.ISchemaStatementCreator
 //class
 public class SchemaWriter implements ISchemaWriter {
 	
-	//attributes
-	private final SQLExecutor mSQLExecutor;
+	//attribute
+	private final SQLCollector mSQLCollector = new SQLCollector();
+	
+	//attribute
+	private final SQLConnection mSQLConnection;
+	
+	//attribute
 	private final ISchemaStatementCreator schemaStatementCreator;
 	
 	//constructor
-	public SchemaWriter(final SQLConnection pSQLConnection, final ISchemaStatementCreator schemaStatementCreator) {
+	public SchemaWriter(
+		final SQLConnection pSQLConnection,
+		final ISchemaStatementCreator schemaStatementCreator
+	) {
 		
 		Validator.assertThat(schemaStatementCreator).thatIsNamed(ISchemaStatementCreator.class).isNotNull();
 		
-		mSQLExecutor = new SQLExecutor(pSQLConnection);
+		mSQLConnection = pSQLConnection;
 		this.schemaStatementCreator = schemaStatementCreator;
 	}
 	
 	//method
 	@Override
 	public void addColumn(final String tableName, final IColumnDTO column) {
-		mSQLExecutor.addSQLStatement(schemaStatementCreator.createStatementToAddColumn(tableName, column));
+		mSQLCollector.addSQLStatement(schemaStatementCreator.createStatementToAddColumn(tableName, column));
 	}
 	
 	//method
 	@Override
 	public void addTable(final ITableDTO table) {
-		mSQLExecutor.addSQLStatement(schemaStatementCreator.createStatementToAddTable(table));
+		mSQLCollector.addSQLStatement(schemaStatementCreator.createStatementToAddTable(table));
 	}
 	
 	//method
 	@Override
 	public void deleteColumn(final String tableName, final String columnName) {
-		mSQLExecutor.addSQLStatement(schemaStatementCreator.createStatementToDeleteColumn(tableName, columnName));
+		mSQLCollector.addSQLStatement(schemaStatementCreator.createStatementToDeleteColumn(tableName, columnName));
 	}
 	
 	//method
 	@Override
 	public void deleteTable(final String tableName) {
-		mSQLExecutor.addSQLStatement(schemaStatementCreator.createStatementToDeleteTable(tableName));
+		mSQLCollector.addSQLStatement(schemaStatementCreator.createStatementToDeleteTable(tableName));
 	}
 	
 	//method
 	@Override
 	public IContainer<String> getSQLStatements() {
-		return mSQLExecutor.getSQLStatements();
+		return mSQLCollector.getSQLStatements();
 	}
 	
 	//method
 	@Override
 	public boolean hasChanges() {
-		return mSQLExecutor.getSQLStatements().containsAny();
+		return mSQLCollector.containsAny();
 	}
 	
 	//method
 	@Override
 	public void renameColumn(final String tableName, final String columnName, final String newColumnName) {
-		mSQLExecutor.addSQLStatement(
+		mSQLCollector.addSQLStatement(
 			schemaStatementCreator.createStatementToRenameColumn(tableName, columnName, newColumnName)
 		);
 	}
@@ -73,12 +82,12 @@ public class SchemaWriter implements ISchemaWriter {
 	//method
 	@Override
 	public void renameTable(final String tableName, final String newTableName) {
-		mSQLExecutor.addSQLStatement(schemaStatementCreator.createStatementToRenameTable(tableName, newTableName));
+		mSQLCollector.addSQLStatement(schemaStatementCreator.createStatementToRenameTable(tableName, newTableName));
 	}
 	
 	//method
 	@Override
 	public void saveChanges() {
-		mSQLExecutor.execute();
+		mSQLCollector.executeUsingConnection(mSQLConnection);
 	}
 }
