7f75882c2c72a5b54a8dd4c72e6df288b3bccda9
Heocro <silvan.wyss@gmail.com>
2022-02-10 21:33:29 +0100

    created IMultiTimeChangeSaver

---
+++ Source/ch/nolix/core/skillapi/IMultiTimeChangeSaver.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.core.skillapi;
+
+//own imports
+import ch.nolix.core.requestapi.ChangeRequestable;
+
+//interface
+public interface IMultiTimeChangeSaver extends ChangeRequestable, Resettable {
+	
+	//method declaration
+	void saveChangesAndReset();
+}
