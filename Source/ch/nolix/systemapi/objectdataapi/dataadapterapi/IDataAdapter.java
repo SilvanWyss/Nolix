ae3cc512e01e6cf9ab11c4e9e2e7b5a8660baf73
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-16 22:45:31 +0100

    made IDataAdapter a GroupCloseable

--- Source/ch/nolix/systemapi/objectdataapi/dataadapterapi/IDataAdapter.java
+++ Source/ch/nolix/systemapi/objectdataapi/dataadapterapi/IDataAdapter.java
@@ -2,11 +2,12 @@
 package ch.nolix.systemapi.objectdataapi.dataadapterapi;
 
 //own imports
+import ch.nolix.core.programcontrol.groupcloseable.GroupCloseable;
 import ch.nolix.core.skillapi.IMultiTimeChangeSaver;
 import ch.nolix.systemapi.objectdataapi.dataapi.IDatabase;
 
 //interface
-public interface IDataAdapter<IMPL> extends IMultiTimeChangeSaver {
+public interface IDataAdapter<IMPL> extends GroupCloseable, IMultiTimeChangeSaver {
 	
 	//method declaration
 	IDatabase<IMPL> getRefDatabase();
