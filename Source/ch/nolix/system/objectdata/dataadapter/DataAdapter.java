2a8a0713837bebdbb0bd6c36a1f29fd920a46412
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-18 18:20:38 +0100

    edited DataAdapter

--- Source/ch/nolix/system/objectdata/dataadapter/DataAdapter.java
+++ Source/ch/nolix/system/objectdata/dataadapter/DataAdapter.java
@@ -2,8 +2,19 @@
 package ch.nolix.system.objectdata.dataadapter;
 
 //own imports
+import ch.nolix.core.programcontrol.groupcloseable.CloseController;
 import ch.nolix.system.objectdata.data.DataImplementation;
 import ch.nolix.systemapi.objectdataapi.dataadapterapi.IDataAdapter;
 
 //class
-public abstract class DataAdapter implements IDataAdapter<DataImplementation> {}
+public abstract class DataAdapter implements IDataAdapter<DataImplementation> {
+	
+	//attribute
+	private final CloseController closeController = new CloseController(this);
+	
+	//method
+	@Override
+	public final CloseController getRefCloseController() {
+		return closeController;
+	}
+}
