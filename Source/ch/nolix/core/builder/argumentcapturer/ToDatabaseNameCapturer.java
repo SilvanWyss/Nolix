df29ddb60dce97e53ece1ce7392e38df795f9867
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-20 20:01:41 +0100

    created ToDatabaseNameCapturer

---
+++ Source/ch/nolix/core/builder/argumentcapturer/ToDatabaseNameCapturer.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.core.builder.argumentcapturer;
+
+//own imports
+import ch.nolix.core.builder.base.ArgumentCapturer;
+import ch.nolix.core.builder.base.BaseArgumentCapturer;
+import ch.nolix.core.constant.LowerCaseCatalogue;
+import ch.nolix.core.errorcontrol.validator.Validator;
+
+//class
+public final class ToDatabaseNameCapturer<NAC extends BaseArgumentCapturer<?>> extends ArgumentCapturer<String, NAC> {
+	
+	//method
+	public ToDatabaseNameCapturer(final NAC nextArgumentCapturer) {
+		super(nextArgumentCapturer);
+	}
+	
+	//method
+	public NAC toDatabase(final String databaseName) {
+		
+		Validator.assertThat(databaseName).thatIsNamed(LowerCaseCatalogue.DATABASE_NAME).isNotBlank();
+		
+		return setArgumentAndGetRefNextArgumentCapturer(databaseName);
+	}
+}
