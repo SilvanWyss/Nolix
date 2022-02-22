03d890433cc49e4f0ab3f94c17ea3059cab69598
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-22 22:26:10 +0100

    edited MSSQLSchemaAdapter

--- Source/ch/nolix/system/sqlrawobjectschema/schemaadapter/MSSQLSchemaAdapter.java
+++ Source/ch/nolix/system/sqlrawobjectschema/schemaadapter/MSSQLSchemaAdapter.java
@@ -2,6 +2,7 @@
 package ch.nolix.system.sqlrawobjectschema.schemaadapter;
 
 //own imports
+import ch.nolix.core.constant.PascalCaseCatalogue;
 import ch.nolix.core.sql.SQLConnection;
 import ch.nolix.system.sqlschema.schemadto.ColumnDTO;
 import ch.nolix.system.sqlschema.schemadto.DataTypeDTO;
@@ -11,7 +12,8 @@ import ch.nolix.systemapi.sqlschemaapi.schemadtoapi.IColumnDTO;
 public final class MSSQLSchemaAdapter extends SchemaAdapter {
 	
 	//constant
-	private static final IColumnDTO SAVE_STAMP_COLUMN_DTO = new ColumnDTO("RowVersion",	new DataTypeDTO("rowversion"));
+	private static final IColumnDTO SAVE_STAMP_COLUMN_DTO =
+	new ColumnDTO(PascalCaseCatalogue.SAVE_STAMP, new DataTypeDTO("BigInt"));
 	
 	//constructor
 	public MSSQLSchemaAdapter(final String databaseName, final SQLConnection pSQLConnection) {
