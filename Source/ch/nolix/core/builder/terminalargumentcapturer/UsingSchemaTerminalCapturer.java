071429777bf14de1301f6a2026fe2be7460ba508
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-23 22:16:40 +0100

    created UsingSchemaTerminalCapturer

---
+++ Source/ch/nolix/core/builder/terminalargumentcapturer/UsingSchemaTerminalCapturer.java
@@ -0,0 +1,22 @@
+//package declaration
+package ch.nolix.core.builder.terminalargumentcapturer;
+
+//own imports
+import ch.nolix.core.builder.base.TerminalArgumentCapturer;
+
+//class
+public final class UsingSchemaTerminalCapturer<
+	S,
+	O
+> extends TerminalArgumentCapturer<S, O> {
+	
+	//method
+	public S getRefSchema() {
+		return getRefArgument();
+	}
+	
+	//method
+	public O usingSchema(final S schema) {
+		return setArgumentAndBuild(schema);
+	}
+}
