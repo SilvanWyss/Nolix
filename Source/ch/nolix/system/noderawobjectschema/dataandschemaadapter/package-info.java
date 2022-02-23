4f88828a8cf93008accd6ef8de4f6960126a703a
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-23 21:37:55 +0100

    created dataandschemaadapter package

---
+++ Source/ch/nolix/system/noderawobjectschema/dataandschemaadapter/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.noderawobjectschema.dataandschemaadapter} package defines
+ * the types that are declared in the {@link ch.nolix.systemapi.rawobjectschemaapi.dataandschemaadapter} package for
+ * databases that base on {@link ch.nolix.core.document.node.BaseNode}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-02-23
+ */
+package ch.nolix.system.noderawobjectschema.dataandschemaadapter;
