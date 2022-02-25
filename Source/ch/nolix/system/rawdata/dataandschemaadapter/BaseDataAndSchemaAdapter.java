6147b1125501648cb955182f8c88099e82b0e57b
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-25 15:41:35 +0100

    created BaseDataAndSchemaAdapter

---
+++ Source/ch/nolix/system/rawdata/dataandschemaadapter/BaseDataAndSchemaAdapter.java
@@ -0,0 +1,271 @@
+//package declaration
+package ch.nolix.system.rawdata.dataandschemaadapter;
+
+//own imports
+import ch.nolix.core.container.LinkedList;
+import ch.nolix.core.errorcontrol.validator.Validator;
+import ch.nolix.core.programcontrol.groupcloseable.CloseController;
+import ch.nolix.element.time.base.Time;
+import ch.nolix.systemapi.rawdataapi.dataadapterapi.IDataAdapter;
+import ch.nolix.systemapi.rawdataapi.dataandschemaadapterapi.IDataAndSchemaAdapter;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.IEntityHeadDTO;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.ILoadedRecordDTO;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.IRecordDTO;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.IRecordUpdateDTO;
+import ch.nolix.systemapi.rawschemaapi.flatschemadtoapi.IFlatTableDTO;
+import ch.nolix.systemapi.rawschemaapi.schemaadapterapi.ISchemaReader;
+import ch.nolix.systemapi.rawschemaapi.schemadtoapi.IColumnDTO;
+import ch.nolix.systemapi.rawschemaapi.schemadtoapi.ITableDTO;
+
+//class
+public abstract class BaseDataAndSchemaAdapter implements IDataAndSchemaAdapter {
+	
+	//attribute
+	private final CloseController closeController = new CloseController(this);
+	
+	//attribute
+	private final IDataAdapter dataAdapter;
+	
+	//attribute
+	private final ISchemaReader schemaReader;
+	
+	//constructor
+	public BaseDataAndSchemaAdapter(
+		final IDataAdapter dataAdapter,
+		final ISchemaReader schemaReader
+	) {
+		
+		Validator.assertThat(dataAdapter).thatIsNamed(IDataAdapter.class).isNotNull();
+		Validator.assertThat(schemaReader).thatIsNamed(ISchemaReader.class).isNotNull();
+		
+		this.dataAdapter = dataAdapter;
+		this.schemaReader = schemaReader;
+		
+		createCloseDependencyTo(dataAdapter);
+		createCloseDependencyTo(schemaReader);
+	}
+	
+	//method
+	@Override
+	public final boolean columnIsEmpty(final String tableName, final String columnName) {
+		return schemaReader.columnIsEmpty(tableName, columnName);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntriesFromMultiReference(
+		final String tableName,
+		final String entityId,
+		final String multiReferenceColumnName
+	) {
+		dataAdapter.deleteEntriesFromMultiReference(tableName, entityId, multiReferenceColumnName);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntriesFromMultiValue(
+		final String tableName,
+		final String entityId,
+		final String multiValueColumnName
+	) {
+		dataAdapter.deleteEntriesFromMultiValue(tableName, entityId, multiValueColumnName);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntryFromMultiReference(
+		final String tableName,
+		final String entityId,
+		final String multiRefereceColumnName,
+		final String referencedEntityId
+	) {
+		dataAdapter.deleteEntryFromMultiReference(tableName, entityId, multiRefereceColumnName, referencedEntityId);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntryFromMultiValue(
+		final String tableName,
+		final String entityId,
+		final String multiValueColumnName,
+		final String entry
+	) {
+		dataAdapter.deleteEntryFromMultiValue(tableName, entityId, multiValueColumnName, entry);
+	}
+	
+	//method
+	@Override
+	public final void deleteRecordFromTable(final String tableName, final IEntityHeadDTO entity) {
+		dataAdapter.deleteRecordFromTable(tableName, entity);
+	}
+	
+	//method
+	@Override
+	public final CloseController getRefCloseController() {
+		return closeController;
+	}
+	
+	//method
+	@Override
+	public final int getSaveCount() {
+		return dataAdapter.getSaveCount();
+	}
+	
+	//method
+	@Override
+	public final boolean hasChanges() {
+		return dataAdapter.hasChanges();
+	}
+	
+	//method
+	@Override
+	public final void insertEntryIntoMultiReference(
+		final String tableName,
+		final String entityId,
+		final String multiReferenceColumnName,
+		final String referencedEntityId
+	) {
+		dataAdapter.insertEntryIntoMultiReference(tableName, entityId, multiReferenceColumnName, referencedEntityId);
+	}
+	
+	//method
+	@Override
+	public final void insertEntryIntoMultiValue(
+		final String tableName,
+		final String entityId,
+		final String multiValueColumnName,
+		final String entry
+	) {
+		dataAdapter.insertEntryIntoMultiValue(tableName, entityId, multiValueColumnName, entry);
+	}
+	
+	//method
+	@Override
+	public final void insertRecordIntoTable(final String tableName, final IRecordDTO record) {
+		dataAdapter.insertRecordIntoTable(tableName, record);
+	}
+	
+	//method
+	@Override
+	public final LinkedList<String> loadAllMultiReferenceEntriesForRecord(
+		final String tableName,
+		final String entityId,
+		final String multiReferenceColumnName
+	) {
+		return dataAdapter.loadAllMultiReferenceEntriesForRecord(tableName, entityId, multiReferenceColumnName);
+	}
+	
+	//method
+	@Override
+	public final LinkedList<Object> loadAllMultiValueEntriesFromRecord(
+		final String tableName,
+		final String entityId,
+		final String multiFieldColumnName
+	) {
+		return dataAdapter.loadAllMultiValueEntriesFromRecord(tableName, entityId, multiFieldColumnName);
+	}
+	
+	//method	
+	@Override
+	public final LinkedList<ILoadedRecordDTO> loadAllRecordsFromTable(final String tableName) {
+		return dataAdapter.loadAllRecordsFromTable(tableName);
+	}
+	
+	//method
+	@Override
+	public final LinkedList<IColumnDTO> loadColumnsByTableId(final String tableId) {
+		return schemaReader.loadColumnsByTableId(tableId);
+	}
+	
+	//method
+	@Override
+	public final LinkedList<IColumnDTO> loadColumnsByTableName(final String tableName) {
+		return schemaReader.loadColumnsByTableName(tableName);
+	}
+	
+	//method
+	@Override
+	public IFlatTableDTO loadFlatTableById(String id) {
+		return schemaReader.loadFlatTableById(id);
+	}
+	
+	//method
+	@Override
+	public final IFlatTableDTO loadFlatTableByName(final String name) {
+		return schemaReader.loadFlatTableByName(name);
+	}
+	
+	//method
+	@Override
+	public final LinkedList<IFlatTableDTO> loadFlatTables() {
+		return schemaReader.loadFlatTables();
+	}
+	
+	//method
+	@Override
+	public final ILoadedRecordDTO loadRecordFromTableById(final String tableName, final String id) {
+		return dataAdapter.loadRecordFromTableById(tableName, id);
+	}
+	
+	//method
+	@Override
+	public final Time loadSchemaTimestamp() {
+		return schemaReader.loadSchemaTimestamp();
+	}
+	
+	//method
+	@Override
+	public final ITableDTO loadTableById(final String id) {
+		return schemaReader.loadTableById(id);
+	}
+	
+	//method
+	@Override
+	public final ITableDTO loadTableByName(final String name) {
+		return schemaReader.loadTableByName(name);
+	}
+	
+	//method
+	@Override
+	public final LinkedList<ITableDTO> loadTables() {
+		return schemaReader.loadTables();
+	}
+	
+	//method
+	@Override
+	public final void noteClose() {}
+	
+	//method
+	@Override
+	public final void reset() {
+		dataAdapter.reset();
+	}
+	
+	//method
+	@Override
+	public final void saveChangesAndReset() {
+		dataAdapter.saveChangesAndReset();
+	}
+	
+	//method
+	@Override
+	public final void setEntityAsUpdated(final String tableName, final IEntityHeadDTO entity) {
+		dataAdapter.setEntityAsUpdated(tableName, entity);
+	}
+	
+	//method
+	@Override
+	public final boolean tableContainsRecordWithGivenValueAtColumn(
+		final String tableName,
+		final String columnName,
+		final String value
+	) {
+		return dataAdapter.tableContainsRecordWithGivenValueAtColumn(tableName, columnName, value);
+	}
+	
+	//method
+	@Override
+	public final void updateRecordOnTable(final String tableName, final IRecordUpdateDTO recordUpdate) {
+		dataAdapter.updateRecordOnTable(tableName, recordUpdate);
+	}
+}
