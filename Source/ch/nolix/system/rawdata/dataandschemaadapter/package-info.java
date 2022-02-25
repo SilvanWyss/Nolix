6faf6c23d5875e1345cf93d05ece97b0f716df3e
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-25 15:28:20 +0100

    created dataandschemaadapter package

---
+++ Source/ch/nolix/system/rawdata/dataandschemaadapter/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.rawdata.dataandschemaadapter} package defines
+ * some base types for the types that are declared in the
+ * {@link ch.nolix.systemapi.rawdataapi.dataandschemaadapterapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-02-25
+ */
+package ch.nolix.system.rawdata.dataandschemaadapter;
