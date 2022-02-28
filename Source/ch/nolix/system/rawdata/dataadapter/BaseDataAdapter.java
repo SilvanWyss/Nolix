edbff0cae651021d844fcab4a6c3fea6a249e7d3
Silvan Wyss <silvan.wyss@gmail.com>
2022-02-28 20:15:30 +0100

    created BaseDataAdapter

---
+++ Source/ch/nolix/system/rawdata/dataadapter/BaseDataAdapter.java
@@ -0,0 +1,209 @@
+//package declaration
+package ch.nolix.system.rawdata.dataadapter;
+
+//own imports
+import ch.nolix.core.container.LinkedList;
+import ch.nolix.core.errorcontrol.validator.Validator;
+import ch.nolix.core.programcontrol.groupcloseable.CloseController;
+import ch.nolix.systemapi.rawdataapi.dataadapterapi.IDataAdapter;
+import ch.nolix.systemapi.rawdataapi.dataadapterapi.IDataReader;
+import ch.nolix.systemapi.rawdataapi.dataadapterapi.IDataWriter;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.IEntityHeadDTO;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.ILoadedRecordDTO;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.IRecordDTO;
+import ch.nolix.systemapi.rawdataapi.datadtoapi.IRecordUpdateDTO;
+
+//class
+public abstract class BaseDataAdapter implements IDataAdapter {
+	
+	//attribute
+	private final CloseController closeController = new CloseController(this);
+	
+	//attribute
+	private final IDataReader dataReader;
+	
+	//attribute
+	private final IDataWriter dataWriter;
+	
+	//constructor
+	public BaseDataAdapter(final IDataReader dataReader, final IDataWriter dataWriter) {
+		
+		Validator.assertThat(dataReader).thatIsNamed(IDataReader.class).isNotNull();
+		Validator.assertThat(dataWriter).thatIsNamed(IDataWriter.class).isNotNull();
+		
+		this.dataReader = dataReader;
+		this.dataWriter = dataWriter;
+		
+		getRefCloseController().createCloseDependencyTo(dataReader);
+		getRefCloseController().createCloseDependencyTo(dataWriter);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntriesFromMultiReference(
+		final String tableName,
+		final String entityId,
+		final String multiReferenceColumnName
+	) {
+		dataWriter.deleteEntriesFromMultiReference(tableName, entityId, multiReferenceColumnName);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntriesFromMultiValue(
+		final String tableName,
+		final String entityId,
+		final String multiFieldColumn
+	) {
+		dataWriter.deleteEntriesFromMultiValue(tableName, entityId, multiFieldColumn);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntryFromMultiReference(
+		final String tableName,
+		final String entityId,
+		final String multiRefereceColumnName,
+		final String referencedEntityId
+	) {
+		dataWriter.deleteEntryFromMultiReference(tableName, entityId, multiRefereceColumnName, referencedEntityId);
+	}
+	
+	//method
+	@Override
+	public final void deleteEntryFromMultiValue(
+		final String tableName,
+		final String entityId,
+		final String multiFieldColumn,
+		final String entry
+	) {
+		dataWriter.deleteEntryFromMultiValue(tableName, entityId, multiFieldColumn, entry);
+	}
+	
+	//method
+	@Override
+	public final void deleteRecordFromTable(final String tableName, final IEntityHeadDTO entity) {
+		dataWriter.deleteRecordFromTable(tableName, entity);
+	}
+	
+	//method
+	@Override
+	public final CloseController getRefCloseController() {
+		return closeController;
+	}
+	
+	//method
+	@Override
+	public final int getSaveCount() {
+		return dataWriter.getSaveCount();
+	}
+	
+	//method
+	@Override
+	public final String getSchemaTimestamp() {
+		return dataReader.getSchemaTimestamp();
+	}
+	
+	//method
+	@Override
+	public final boolean hasChanges() {
+		return dataWriter.hasChanges();
+	}
+	
+	//method
+	@Override
+	public final void insertEntryIntoMultiReference(
+		final String tableName,
+		final String entityId,
+		final String multiReferenceColumnName,
+		final String referencedEntityId
+	) {
+		dataWriter.insertEntryIntoMultiReference(tableName, entityId, multiReferenceColumnName, referencedEntityId);
+	}
+	
+	//method
+	@Override
+	public final void insertEntryIntoMultiValue(
+		final String tableName,
+		final String entityId,
+		final String multiFieldColumn,
+		final String entry
+	) {
+		dataWriter.insertEntryIntoMultiValue(tableName, entityId, multiFieldColumn, entry);
+	}
+	
+	//method
+	@Override
+	public final void insertRecordIntoTable(final String tableName, final IRecordDTO record) {
+		dataWriter.insertRecordIntoTable(tableName, record);
+	}
+	
+	@Override
+	public final LinkedList<String> loadAllMultiReferenceEntriesForRecord(
+		final String tableName,
+		final String entityId,
+		final String multiReferenceColumnName
+	) {
+		return dataReader.loadAllMultiReferenceEntriesForRecord(tableName, entityId, multiReferenceColumnName);
+	}
+	
+	//method
+	@Override
+	public final LinkedList<Object> loadAllMultiValueEntriesFromRecord(
+		final String tableName,
+		final String entityId,
+		final String multiFieldColumnName
+	) {
+		return dataReader.loadAllMultiValueEntriesFromRecord(tableName, entityId, multiFieldColumnName);
+	}
+	
+	//method	
+	@Override
+	public final LinkedList<ILoadedRecordDTO> loadAllRecordsFromTable(final String tableName) {
+		return dataReader.loadAllRecordsFromTable(tableName);
+	}
+	
+	//method
+	@Override
+	public final ILoadedRecordDTO loadRecordFromTableById(final String tableName, final String id) {
+		return dataReader.loadRecordFromTableById(tableName, id);
+	}
+	
+	//method
+	@Override
+	public void noteClose() {}
+	
+	//method
+	@Override
+	public final void reset() {
+		dataWriter.reset();
+	}
+	
+	//method
+	@Override
+	public final void saveChangesAndReset() {
+		dataWriter.saveChangesAndReset();
+	}
+	
+	//method
+	@Override
+	public final void setEntityAsUpdated(final String tableName, final IEntityHeadDTO entity) {
+		dataWriter.setEntityAsUpdated(tableName, entity);
+	}
+	
+	//method
+	@Override
+	public final boolean tableContainsRecordWithGivenValueAtColumn(
+		final String tableName,
+		final String columnName,
+		final String value
+	) {
+		return dataReader.tableContainsRecordWithGivenValueAtColumn(tableName, columnName, value);
+	}
+	
+	//method
+	@Override
+	public final void updateRecordOnTable(final String tableName, final IRecordUpdateDTO recordUpdate) {
+		dataWriter.updateRecordOnTable(tableName, recordUpdate);
+	}
+}
