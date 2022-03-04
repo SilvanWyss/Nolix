91bcfb9540d906120db19d9a4184a96ec1ee0689
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-04 19:59:57 +0100

    created IParametrizedPropertyTypeMapper

---
+++ Source/ch/nolix/systemapi/objectdataapi/schemamapperapi/IParametrizedPropertyTypeMapper.java
@@ -0,0 +1,18 @@
+//package declaration
+package ch.nolix.systemapi.objectdataapi.schemamapperapi;
+
+//own imports
+import ch.nolix.core.container.IContainer;
+import ch.nolix.systemapi.objectdataapi.dataapi.IProperty;
+import ch.nolix.systemapi.objectschemaapi.schemaapi.IParametrizedPropertyType;
+import ch.nolix.systemapi.objectschemaapi.schemaapi.ITable;
+
+//interface
+public interface IParametrizedPropertyTypeMapper<IMPL> {
+	
+	//method declaration
+	IParametrizedPropertyType<IMPL, ?> createParametrizedPropertyTypeFromGivenPropertyUsingGivenReferencableTables(
+		IProperty<?> property,
+		IContainer<ITable<IMPL>> referencableTables
+	);
+}
