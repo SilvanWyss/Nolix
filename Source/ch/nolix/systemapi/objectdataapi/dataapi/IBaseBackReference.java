80e29bd99909de87c9b70316791c0fcf4c7c97af
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-04 17:41:08 +0100

    extended IBaseBackReference

--- Source/ch/nolix/systemapi/objectdataapi/dataapi/IBaseBackReference.java
+++ Source/ch/nolix/systemapi/objectdataapi/dataapi/IBaseBackReference.java
@@ -5,4 +5,14 @@ package ch.nolix.systemapi.objectdataapi.dataapi;
 public interface IBaseBackReference<
 	IMPL,
 	E extends IEntity<IMPL>
-> extends IProperty<IMPL> {}
+> extends IProperty<IMPL> {
+	
+	//method declaration
+	String getBackReferencedPropertyName();
+	
+	//method declaration
+	String getBackReferencedTableName();
+	
+	//method declaration
+	ITable<IMPL, E> getRefBackReferencedTable();
+}
