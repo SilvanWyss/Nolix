b6f910abe19505ded1b13414eac452dfae8f98e6
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-05 11:18:31 +0100

    added fromSQLConnectionPool static method to MSSQLSchemaAdapter

--- Source/ch/nolix/system/sqlbasicschema/schemaadapter/MSSQLSchemaAdapter.java
+++ Source/ch/nolix/system/sqlbasicschema/schemaadapter/MSSQLSchemaAdapter.java
@@ -3,11 +3,17 @@ package ch.nolix.system.sqlbasicschema.schemaadapter;
 
 //own imports
 import ch.nolix.core.sql.SQLConnection;
+import ch.nolix.core.sql.SQLConnectionPool;
 import ch.nolix.system.sqlbasicschema.mssqllanguage.MSSQLSchemaQueryCreator;
 import ch.nolix.system.sqlbasicschema.mssqllanguage.MSSQLSchemaStatementCreator;
 
 //class
 public final class MSSQLSchemaAdapter extends SchemaAdapter {
+	
+	//static method
+	public static MSSQLSchemaAdapter fromSQLConnectionPool(final SQLConnectionPool pSQLConnectionPool) {
+		return new MSSQLSchemaAdapter(pSQLConnectionPool.borrowSQLConnection());
+	}
 	
 	//constructor
 	public MSSQLSchemaAdapter(final SQLConnection pSQLConnection) {
