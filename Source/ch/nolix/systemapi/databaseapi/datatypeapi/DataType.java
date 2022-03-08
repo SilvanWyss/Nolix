c4b62dffa17804d44c0a9f13273c53dba25dca18
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-08 21:16:42 +0100

    added forType static method to DataType enum

--- Source/ch/nolix/systemapi/databaseapi/datatypeapi/DataType.java
+++ Source/ch/nolix/systemapi/databaseapi/datatypeapi/DataType.java
@@ -6,7 +6,9 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 //own imports
+import ch.nolix.core.constant.LowerCaseCatalogue;
 import ch.nolix.core.document.node.Node;
+import ch.nolix.core.errorcontrol.invalidargumentexception.UnrepresentingArgumentException;
 import ch.nolix.core.errorcontrol.validator.Validator;
 import ch.nolix.element.time.base.Time;
 
@@ -24,6 +26,38 @@ public enum DataType {
 	STRING(String.class),
 	TIME(Time.class),
 	NODE(Node.class);
+	
+	//static method
+	public static DataType forType(final Class<?> type) {
+		switch (type.getSimpleName()) {
+			case "Byte":
+				return INTEGER_1;
+			case "Short":
+				return INTEGER_2;
+			case "Integer":
+				return INTEGER_4;
+			case "Long":
+				return INTEGER_8;
+			case "Float":
+				return FLOATING_POINT_NUMBER_4;
+			case "Double":
+				return FLOATING_POINT_NUMBER_8;
+			case "BigInteger":
+				return DYNAMIC_INTEGER;
+			case "BigDecimal":
+				return DYNAMIC_DECIMAL;
+			case "Boolean":
+				return BOOLEAN;
+			case "String":
+				return STRING;
+			case "Time":
+				return TIME;
+			case "Node":
+				return NODE;
+			default:
+				throw new UnrepresentingArgumentException(LowerCaseCatalogue.TYPE, type, DataType.class);
+		}
+	}
 	
 	//attribute
 	private final Class<?> dataTypeClass;
