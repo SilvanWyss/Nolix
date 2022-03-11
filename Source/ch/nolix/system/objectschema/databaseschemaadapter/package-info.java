7b5f5e143fa6f0cb459e3227f78fc27a58ed4d74
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-11 20:01:37 +0100

    deleted EntitySet

--- Source/ch/nolix/system/objectschema/databaseschemaadapter/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.objectschema.databaseschemaadapter} package defines a base database schema adapter.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.system.objectschema.databaseschemaadapter;
