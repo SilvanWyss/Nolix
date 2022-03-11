79bc931c82f01a84cfbfe1dfa5be642714e58b84
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-11 20:12:21 +0100

    deleted SimpleValidator

--- Source/ch/nolix/core/errorcontrol/simplevalidator/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.errorcontrol.simplevalidator} package defines a simple validator.
- * 
- * @author Silvan Wyss
- * @date 2019-07-01
- */
-package ch.nolix.core.errorcontrol.simplevalidator;
