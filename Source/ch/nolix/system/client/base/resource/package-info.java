52da74bcf672d30f102c2b0bb2ed8a669472949a
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-18 16:56:01 +0100

    edited package info

--- Source/ch/nolix/system/client/base/resource/package-info.java
+++ Source/ch/nolix/system/client/base/resource/package-info.java
@@ -1,7 +1,7 @@
 //package info
 /**
- * The {@link ch.nolix.system.client.base.resource} package
- * contains the resources of the {@link ch.nolix.system.client.base} package.
+ * The {@link ch.nolix.system.client.base.resource} package contains
+ * the resources for the {@link ch.nolix.system.client.base} package.
  * 
  * @author Silvan Wyss
  * @date 2020-02-01
