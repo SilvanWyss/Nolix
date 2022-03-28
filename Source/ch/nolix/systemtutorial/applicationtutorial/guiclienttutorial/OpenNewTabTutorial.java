b9cf5ad32c7de726db73ee1daf45b4eb4384dae2
Silvan Wyss <silvan.wyss@gmail.com>
2022-03-28 20:54:28 +0200

    created OpenNewTabTutorial

---
+++ Source/ch/nolix/systemtutorial/applicationtutorial/guiclienttutorial/OpenNewTabTutorial.java
@@ -0,0 +1,60 @@
+package ch.nolix.systemtutorial.applicationtutorial.guiclienttutorial;
+
+//own imports
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.sequencer.Sequencer;
+import ch.nolix.element.gui.color.Color;
+import ch.nolix.element.gui.widget.Button;
+import ch.nolix.element.gui.widget.WidgetLookState;
+import ch.nolix.system.application.guiclient.BackendGUIClientSession;
+import ch.nolix.system.application.guiclient.FrontendGUIClient;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+
+public final class OpenNewTabTutorial {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		//Creates a Server.
+		final var server = Server.forDefaultPort();
+		
+		//Adds a default Application to the Server.
+		server.addDefaultApplication("Open new tab tutorial", MainSession.class, VoidApplicationContext.INSTANCE);
+		
+		//Creates a FrontGUIClient that will connect to the Server.
+		new FrontendGUIClient();
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		Sequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	private static final class MainSession extends BackendGUIClientSession<VoidApplicationContext> {
+		
+		@Override
+		protected void initializeBaseBackGUIClientSession() {
+			
+			//Creates a Button.
+			final var button =
+			new Button()
+			.setText("www.nolix.ch")
+			.setLeftMouseButtonPressAction(this::openWebsite);
+			
+			//Configures the look of the Button.
+			button.getRefLook().setTextColorForState(WidgetLookState.BASE, Color.BLUE);
+			
+			//Adds the Button to the GUI of the current MainSession.
+			getRefGUI().addLayerOnTop(button);
+		}
+		
+		//method
+		private void openWebsite() {
+			getRefGUI().onFrontEnd().openNewTabWithURL("www.nolix.ch");
+		}
+	}
+	
+	private OpenNewTabTutorial() {}
+}
