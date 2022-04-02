ad5d458e19169012b2852ff8a43d6f894aba5b22
Silvan Wyss <silvan.wyss@gmail.com>
2022-04-02 21:38:37 +0200

    created elementapi package

---
+++ Source/ch/nolix/elementapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.elementapi} package declares elements.
+ * An element is reproducible. That means that an element can be represented as a {@link java.lang.String}.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-04-02
+ */
+package ch.nolix.elementapi;
