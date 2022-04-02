c56d41c0df5d6e17144598a381c262e6004c5558
Silvan Wyss <silvan.wyss@gmail.com>
2022-04-02 21:36:43 +0200

    deleted projectapi package

--- Source/ch/nolix/businessapi/projectapi/TaskSize.java
+++
@@ -1,9 +0,0 @@
-//package declaration
-package ch.nolix.businessapi.projectapi;
-
-//enum
-public enum TaskSize {
-	SMALL,
-	MEDIUM,
-	BIG;
-}
--- Source/ch/nolix/businessapi/projectapi/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.businessapi.projectapi} package is an API that defines a project, that manages tasks.
- * 
- * @author Silvan Wyss
- * @date 2019-07-22
- */
-package ch.nolix.businessapi.projectapi;
