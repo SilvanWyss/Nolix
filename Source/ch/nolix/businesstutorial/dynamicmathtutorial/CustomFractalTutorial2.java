a53d9f4c3422f2365cb49ebda258363bf0d2e092
Silvan Wyss <silvan.wyss@gmail.com>
2022-04-18 19:53:46 +0200

    edited CustomFractalTutorial2

--- Source/ch/nolix/businesstutorial/dynamicmathtutorial/CustomFractalTutorial2.java
+++ Source/ch/nolix/businesstutorial/dynamicmathtutorial/CustomFractalTutorial2.java
@@ -1,9 +1,10 @@
 package ch.nolix.businesstutorial.dynamicmathtutorial;
 
-import ch.nolix.business.dynamicmath.DynamicMathImplRegistrator;
-import ch.nolix.businessapi.dynamicmathapi.IFractalBuilder;
+import ch.nolix.business.dynamicmath.ComplexNumber;
+import ch.nolix.business.dynamicmath.FractalBuilder;
+import ch.nolix.business.dynamicmath.SequenceDefinedBy1Predecessor;
+import ch.nolix.businessapi.dynamicmathapi.IComplexNumber;
 import ch.nolix.core.programcontrol.sequencer.Sequencer;
-import ch.nolix.core.provider.implprovider.GlobalImplProvider;
 import ch.nolix.element.gui.base.Frame;
 import ch.nolix.element.gui.color.Color;
 import ch.nolix.element.gui.widget.ImageWidget;
@@ -11,13 +12,6 @@ import ch.nolix.element.gui.widget.ImageWidget;
 public final class CustomFractalTutorial2 {
 	
 	public static void main(String[] args) {
-		
-		//Registers an implementation of the dynamicmathapi at the GlobalImplProvider.
-		new DynamicMathImplRegistrator().registerImplementationTo(GlobalImplProvider.getRefInstance());
-		
-		final var maxIterationCount = 500;
-		
-		//TODO: final var j = GlobalImplProvider.ofInterface(IComplexNumberFactory.class).createInstance().createComplexNumber(-0.8, 0.15);
 		
 		//Creates a Frame that shows a realtime-generated image of a Fractal.
 		@SuppressWarnings("resource")
@@ -27,29 +21,24 @@ public final class CustomFractalTutorial2 {
 		.addLayerOnTop(
 			new ImageWidget()
 			.setImage(
-				GlobalImplProvider.ofInterface(IFractalBuilder.class).createInstance()
+				new FractalBuilder()
 				.setRealComponentInterval(-1.0, 1.0)
 				.setImaginaryComponentInterval(-1.0, 1.0)
-				.setWidthInPixel(800)
-				//TODO: .setStartValuesFunction(c -> new IComplexNumber[]{c})
-				//TODO: .setNextValueFunctionFor1Predecessor((p, c) -> p.getPower(6).getSum(j))
-				.setMinMagnitudeForDivergence(2.5)
-				.setMaxIterationCount(maxIterationCount)
-				.setColorFunction(
-					i -> {
-						
-						if (i < maxIterationCount) {
-							
-							final var component = i % 256;
-							
-							return
-							Color.withRedValueAndGreenValueAndBlueValue(component,	component,	component);
-						}
-						
-						return Color.BLACK;
-					}
+				.setWidthInPixel(500)
+				.setHeightInPixel(500)
+				.setSequenceCreator(
+					z -> new SequenceDefinedBy1Predecessor<>(
+						z,
+						p -> p.getPower(6).getSum(new ComplexNumber(-0.8, 0.15)),
+						IComplexNumber::getSquaredMagnitude
+					)
 				)
-				.setBigDecimalScale(20)
+				.setMinMagnitudeForDivergence(10.0)
+				.setMaxIterationCount(50)
+				.setColorFunction(
+					i -> Color.withRedValueAndGreenValueAndBlueValue((2 * i) % 256, (3 * i) % 256, (4 * i) % 256)
+				)
+				.setBigDecimalScale(10)
 				.build()
 				.startImageBuild()
 				.getRefImage()
