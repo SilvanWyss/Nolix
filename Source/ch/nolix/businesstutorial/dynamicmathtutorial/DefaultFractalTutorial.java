eab11ebccbc7be4f77479bb738ef381a01e1e2ae
Silvan Wyss <silvan.wyss@gmail.com>
2022-04-18 20:06:45 +0200

    edited DefaultFractalTutorial

--- Source/ch/nolix/businesstutorial/dynamicmathtutorial/DefaultFractalTutorial.java
+++ Source/ch/nolix/businesstutorial/dynamicmathtutorial/DefaultFractalTutorial.java
@@ -1,30 +1,19 @@
 package ch.nolix.businesstutorial.dynamicmathtutorial;
 
-import ch.nolix.business.dynamicmath.DynamicMathImplRegistrator;
-import ch.nolix.businessapi.dynamicmathapi.IFractalBuilder;
-import ch.nolix.core.provider.implprovider.GlobalImplProvider;
+import ch.nolix.business.dynamicmath.FractalBuilder;
 import ch.nolix.element.gui.base.Frame;
 import ch.nolix.element.gui.widget.ImageWidget;
-import ch.nolix.element.gui.widget.Label;
 
 public final class DefaultFractalTutorial {
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
-		//Registers an implementation of the dynamicmathapi at the GlobalImplProvider.
-		new DynamicMathImplRegistrator().registerImplementationTo(GlobalImplProvider.getRefInstance());
-		
-		//Creates a Frame.
-		@SuppressWarnings("resource")
-		final var frame =
+		//Creates a Frame that shows a realtime-generated image of a Fractal.
 		new Frame()
 		.setTitle("Default Fractal tutorial")
-		.addLayerOnTop(new Label().setText("Please wait..."));
-		
-		//Sets the Image of a Fractal to the Frame. The creation of the Image may last a few seconds.
-		frame.addLayerOnTop(
-			new ImageWidget()
-			.setImage(GlobalImplProvider.ofInterface(IFractalBuilder.class).createInstance().build().toImage())
+		.addLayerOnTop(
+			new ImageWidget().setImage(new FractalBuilder().build().toImage())
 		);
 	}
 	
