b522df97e44674df4d772d10583654365773a26e
Silvan Wyss <silvan.wyss@gmail.com>
2022-04-24 14:10:39 +0200

    adjusted GUIIconCatalogue

--- Source/ch/nolix/system/gui/base/GUIIconCatalogue.java
+++ Source/ch/nolix/system/gui/base/GUIIconCatalogue.java
@@ -1,13 +1,14 @@
 //package declaration
 package ch.nolix.system.gui.base;
 
+//own imports
 import ch.nolix.system.gui.image.Image;
 
 //class
 public final class GUIIconCatalogue {
 	
 	//constant
-	private static final String NOLIX_ICON_RESOURCE_PATH = "ch/nolix/element/gui/base/resource/Nolix_Icon.png";
+	private static final String NOLIX_ICON_RESOURCE_PATH = "ch/nolix/system/gui/base/resource/Nolix_Icon.png";
 	
 	//constant
 	public static final Image NOLIX_ICON = Image.fromResource(NOLIX_ICON_RESOURCE_PATH);
