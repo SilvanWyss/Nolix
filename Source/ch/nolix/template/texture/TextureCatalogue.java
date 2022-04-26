be0348414ccea4ada830243519ff1f5e6aebd957
Silvan Wyss <silvan.wyss@gmail.com>
2022-04-26 22:26:57 +0200

    created TextureCatalogue

---
+++ Source/ch/nolix/template/texture/TextureCatalogue.java
@@ -0,0 +1,18 @@
+//package declaration
+package ch.nolix.template.texture;
+
+//own imports
+import ch.nolix.systemapi.guiapi.imageapi.IImage;
+
+//class
+public final class TextureCatalogue {
+	
+	//constant
+	public static final IImage<?> CONCRETE_TEXTURE = TextureCreator.INSTANCE.createConcreteTexture();
+	
+	//constant
+	public static final IImage<?> JUTE_TEXTURE = TextureCreator.INSTANCE.createJuteTexture();
+	
+	//constructor
+	private TextureCatalogue() {}
+}
---
+++ Source/ch/nolix/template/texture/TextureCreator.java
@@ -0,0 +1,64 @@
+//package declaration
+package ch.nolix.template.texture;
+
+//own imports
+import ch.nolix.system.gui.color.Color;
+import ch.nolix.system.gui.image.MutableImage;
+import ch.nolix.systemapi.guiapi.imageapi.IImage;
+
+//class
+final class TextureCreator {
+	
+	//static attribute
+	public static final TextureCreator INSTANCE = new TextureCreator();
+	
+	//constructor
+	private TextureCreator() {}
+	
+	//method
+	public IImage<?> createConcreteTexture() {
+		
+		final var texture = MutableImage.withWidthAndHeight(16, 16);
+		
+		final var color1 = Color.fromValue(0x808080);
+		final var color2 = Color.fromValue(0xC0C0C0);
+		final var color3 = Color.fromValue(0xA0A0A0);
+		for (var x = 1; x <= texture.getWidth(); x++) {
+			for (var y = 1; y <= texture.getHeight(); y++) {
+				
+				if ((y + 1) * (x * y - y % 3) % 7 == (x * y - x % 5 + y % 2) % 11) {
+					texture.setPixel(x, y, color1);
+				} else if ((x * x + y * y) % 11 == (x * y) % 13) {
+					texture.setPixel(x, y, color2);
+				} else {
+					texture.setPixel(x, y, color3);
+				}
+			}
+		}
+		
+		return texture.toImmutableImage();
+	}
+	
+	//method
+	public IImage<?> createJuteTexture() {
+		
+		final var texture = MutableImage.withWidthAndHeight(16, 16);
+		
+		final var color1 = Color.fromValue(0xC0C0A0);
+		final var color2 = Color.fromValue(0x808060);
+		final var color3 = Color.fromValue(0xA0A080);
+		for (var x = 1; x <= texture.getWidth(); x++) {
+			for (var y = 1; y <= texture.getHeight(); y++) {							
+				if ((x * y) % 3 == (x + y) % 2) {
+					texture.setPixel(x, y, color1);
+				} else if ((x * x + y * y) % 5 == (x * y) % 7) {
+					texture.setPixel(x, y, color2);
+				} else {
+					texture.setPixel(x, y, color3);
+				}
+			}
+		}
+		
+		return texture.toImmutableImage();
+	}
+}
