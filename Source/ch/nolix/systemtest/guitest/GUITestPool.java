68b5fe569333b24cb1e0bce50f944d76891d212e
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-06 16:33:37 +0200

    fixed GUITestPool

--- Source/ch/nolix/systemtest/guitest/GUITestPool.java
+++ Source/ch/nolix/systemtest/guitest/GUITestPool.java
@@ -3,10 +3,21 @@ package ch.nolix.systemtest.guitest;
 
 //own imports
 import ch.nolix.core.testing.basetest.TestPool;
+import ch.nolix.systemtest.guitest.colortest.ColorTestPool;
+import ch.nolix.systemtest.guitest.containerwidgettest.ContainerWidgetTestPool;
+import ch.nolix.systemtest.guitest.imagetest.ImageTestPool;
+import ch.nolix.systemtest.guitest.widgettest.WidgetTestPool;
 
 //class
 public final class GUITestPool extends TestPool {
 	
 	//constructor
-	public GUITestPool() {}
+	public GUITestPool() {
+		super(
+			new ColorTestPool(),
+			new ContainerWidgetTestPool(),
+			new ImageTestPool(),
+			new WidgetTestPool()
+		);
+	}
 }
