496c14958d04ea52b3fec422ddb3cc39ff130b59
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-13 16:38:29 +0200

    created serverdashboard package

---
+++ Source/ch/nolix/business/serverdashboard/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.business.serverdashboard} package provides
+ * an implementation of the {@link ch.nolix.businessapi.serverdashboardapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-05-13
+ */
+package ch.nolix.business.serverdashboard;
