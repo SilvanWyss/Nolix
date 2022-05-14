a6cac4b9259c3ea690cb33e1379f0a7e466686c5
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-14 22:35:30 +0200

    created applicationapi package

---
+++ Source/ch/nolix/systemapi/applicationapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.applicationapi} package declares types for server applications.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-05-14
+ */
+package ch.nolix.systemapi.applicationapi;
