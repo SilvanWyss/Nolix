869e4582e03f0c63e02576f4396cdcc450101a4a
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-14 22:57:11 +0200

    created guiapplicationapi package

---
+++ Source/ch/nolix/systemapi/applicationapi/guiapplicationapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.applicationapi.guiapplicationapi} package declares
+ * types for server applications that have a GUI.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-05-14
+ */
+package ch.nolix.systemapi.applicationapi.guiapplicationapi;
