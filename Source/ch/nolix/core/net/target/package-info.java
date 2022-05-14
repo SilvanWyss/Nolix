5988f8e63ad314925111c1f536299b4c4b3c4110
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-14 17:58:04 +0200

    created target package

---
+++ Source/ch/nolix/core/net/target/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.net.target} package defines
+ * some types that are declared {@link ch.nolix.core.net.targetapi}.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-05-14
+ */
+package ch.nolix.core.net.target;
