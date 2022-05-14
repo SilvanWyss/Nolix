fbbb243e33ae9b16d19506ec8a7622a0edec0a99
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-14 18:04:41 +0200

    created ServerTarget

---
+++ Source/ch/nolix/core/net/target/ServerTarget.java
@@ -0,0 +1,48 @@
+//package declaration
+package ch.nolix.core.net.target;
+
+//own imports
+import ch.nolix.core.constant.LowerCaseCatalogue;
+import ch.nolix.core.constant.PortCatalogue;
+import ch.nolix.core.errorcontrol.validator.Validator;
+import ch.nolix.core.net.targetapi.IServerTarget;
+
+//class
+public final class ServerTarget implements IServerTarget {
+	
+	//static method
+	public ServerTarget forIpOrAddressNameAndPort(final String ipOrAddressName, final int port) {
+		return new ServerTarget(ipOrAddressName, port);
+	}
+	
+	//attribute
+	private final String ipOrAddressName;
+	
+	//attribute
+	private final int port;
+	
+	//constructor
+	private ServerTarget(final String ipOrAddressName, final int port) {
+		
+		Validator.assertThat(ipOrAddressName).thatIsNamed("ip or address name").isNotBlank();
+		
+		Validator.assertThat(port)
+		.thatIsNamed(LowerCaseCatalogue.PORT)
+		.isBetween(PortCatalogue.MIN_PORT, PortCatalogue.MAX_PORT);
+		
+		this.ipOrAddressName = ipOrAddressName;
+		this.port = port;
+	}
+	
+	//method
+	@Override
+	public String getIpOrAddressName() {
+		return ipOrAddressName;
+	}
+	
+	//method
+	@Override
+	public int getPort() {
+		return port;
+	}
+}
