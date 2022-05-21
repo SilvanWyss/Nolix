7f9ea21b72160a8fc48bba94ba8f5eb071db2ace
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-21 19:50:25 +0200

    deleted SimpleTest

--- Source/ch/nolix/core/testing/simpletest/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.testing.simpletest} package defines a simple base test.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.core.testing.simpletest;
