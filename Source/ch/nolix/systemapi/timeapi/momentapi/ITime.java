2c94b60a91ed6ff83c6415985b18ae871bd030fb
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-28 12:55:43 +0200

    created ITime

---
+++ Source/ch/nolix/systemapi/timeapi/momentapi/ITime.java
@@ -0,0 +1,29 @@
+//package declaration
+package ch.nolix.systemapi.timeapi.momentapi;
+
+//own imports
+import ch.nolix.systemapi.elementapi.IElement;
+
+//interface
+public interface ITime extends IElement<ITime> {
+	
+	//method declaration
+	/**
+	 * @return the milliseconds of the current {@link ITime}.
+	 */
+	long getMilliseconds();
+	
+	//method declaration
+	/**
+	 * @param time
+	 * @return true if the current {@link ITime} is after the given time.
+	 */
+	boolean isAfter(ITime time);
+	
+	//method
+	/**
+	 * @param time
+	 * @return true if the current {@link ITime} is before the given time.
+	 */
+	boolean isBefore(ITime time);
+}
