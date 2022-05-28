0617bfc67fef4dff0bc3f2a6af1e9eef093457e1
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-28 14:25:52 +0200

    created colorapi package

---
+++ Source/ch/nolix/systemapi/guiapi/baseapi/colorapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.guiapi.baseapi.colorapi} package declares a color.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-05-28
+ */
+package ch.nolix.systemapi.guiapi.baseapi.colorapi;
