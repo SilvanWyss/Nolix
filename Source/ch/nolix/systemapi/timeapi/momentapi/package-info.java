b828fff9613b39b059d1016ac00550ff422fc5cf
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-28 12:36:36 +0200

    created momentapi package

---
+++ Source/ch/nolix/systemapi/timeapi/momentapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.timeapi.momentapi} package declares types that represent a time moment.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-05-28
+ */
+package ch.nolix.systemapi.timeapi.momentapi;
