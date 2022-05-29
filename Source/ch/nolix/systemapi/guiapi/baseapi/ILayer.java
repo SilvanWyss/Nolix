b66f40a5f61b5d0819caec99c68ee4ef74e33fed
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-29 15:31:42 +0200

    created ILayer

---
+++ Source/ch/nolix/systemapi/guiapi/baseapi/ILayer.java
@@ -0,0 +1,48 @@
+//package declaration
+package ch.nolix.systemapi.guiapi.baseapi;
+
+//own imports
+import ch.nolix.core.container.IContainer;
+import ch.nolix.core.skilluniversalapi.Clearable;
+import ch.nolix.core.skilluniversalapi.Recalculable;
+import ch.nolix.system.gui.widget.Widget;
+import ch.nolix.systemapi.elementapi.configurationapi.IConfigurableElement;
+import ch.nolix.systemapi.guiapi.inputapi.IResizableInputTaker;
+import ch.nolix.systemapi.guiapi.painterapi.IPainter;
+
+//interface
+public interface ILayer<L extends ILayer<L>>
+extends
+Clearable,
+IConfigurableElement<L>,
+IOccupiableCanvasInputActionManager<L>,
+IResizableInputTaker,
+Recalculable {
+	
+	//method declaration
+	boolean allowesConfiguration();
+	
+	//method declaration
+	CursorIcon getCursorIcon();
+	
+	//method declaration
+	int getCursorXPosition();
+	
+	//method declaration
+	int getCursorYPosition();
+	
+	//method declaration
+	Widget<?, ?> getRefRootWidget();
+	
+	//method declaration
+	IContainer<Widget<?, ?>> getRefWidgets();
+	
+	//method declaration
+	IContainer<Widget<?, ?>> getRefWidgetsForPainting();
+	
+	//method declaration
+	void paint(IPainter painter);
+	
+	//method declaration
+	void setParentGUI(IWidgetGUI<?> parentGUI);
+}
