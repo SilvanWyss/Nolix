971b1c8f79f0d3f9606d9732e0872778caf22ba4
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-29 15:53:28 +0200

    created widgetapi package

---
+++ Source/ch/nolix/systemapi/guiapi/widgetapi/package-info.java
@@ -0,0 +1,9 @@
+//package declaration
+/**
+ * The {@link ch.nolix.systemapi.guiapi.widgetapi} package declares widgets.
+ * A widget is an element of a GUI.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-05-29
+ */
+package ch.nolix.systemapi.guiapi.widgetapi;
