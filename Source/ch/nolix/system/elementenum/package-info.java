56a7cfc4408303686ea18526703464518be347a3
Silvan Wyss <silvan.wyss@gmail.com>
2022-05-29 13:21:54 +0200

    deleted elementenum package

--- Source/ch/nolix/system/elementenum/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.elementenum} package
- * defines enums that are {@link ch.nolix.systemapi.elementuniversalapi.Specified}s.
- * 
- * @author Silvan Wyss
- * @date 2019-05-01
- */
-package ch.nolix.system.elementenum;
