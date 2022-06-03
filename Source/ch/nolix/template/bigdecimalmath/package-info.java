d3d640de888aa8df37106fd7b26c94e72614b29b
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-03 17:51:58 +0200

    created bigdecimalmath package

---
+++ Source/ch/nolix/template/bigdecimalmath/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.template.bigdecimalmath} package defines mathematical objects with {@link BigDecimal}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-06-03
+ */
+package ch.nolix.template.bigdecimalmath;
