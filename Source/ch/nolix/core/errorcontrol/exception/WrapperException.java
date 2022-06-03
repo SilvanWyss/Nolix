45d64bd5d9db6cda43888277f4a46962124084fa
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-03 18:49:43 +0200

    extended WrapperException

--- Source/ch/nolix/core/errorcontrol/exception/WrapperException.java
+++ Source/ch/nolix/core/errorcontrol/exception/WrapperException.java
@@ -5,8 +5,16 @@ package ch.nolix.core.errorcontrol.exception;
 @SuppressWarnings("serial")
 public final class WrapperException extends RuntimeException {
 	
+	//constant
+	public static final String DEFAULT_ERROR_MESSAGE = "An error occured.";
+	
 	//constructor
-	public WrapperException(final Exception exception) {
-		super("An error occured.", exception);
+	public WrapperException(final Throwable error) {
+		this(DEFAULT_ERROR_MESSAGE, error);
+	}
+	
+	//constructor
+	public WrapperException(final String errorMessage, final Throwable error) {
+		super(errorMessage, error);
 	}
 }
