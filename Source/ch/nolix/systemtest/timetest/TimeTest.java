dc25d318c2fe30c99f527a5f2c735472145f24a3
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-08 19:35:16 +0200

    refactored TimeTest

--- Source/ch/nolix/systemtest/timetest/TimeTest.java
+++ Source/ch/nolix/systemtest/timetest/TimeTest.java
@@ -15,12 +15,40 @@ import ch.nolix.system.time.base.Time;
  */
 public final class TimeTest extends Test {
 	
-	//loop test case
-	public void loopTestCase_creation_1A() {
+	//method
+	@TestCase
+	public void testCase_getDay() {
 		
-		//main loop
-		for (int y = 1600; y <= 1600; y++) {
-			for (int m = 1; m <= 12; m++) {
+		//setup
+		final var time =
+		Time.withYearAndMonthOfYearAndDayOfMonthAndHourOfDayAndMinuteOfHourAndSecondOfMinuteAndMillisecondOfSecond(
+			2020,
+			1,
+			2,
+			10,
+			15,
+			30,
+			500
+		);
+		
+		//execution
+		final Time result = time.getDay();
+		
+		//verification
+		expect(result.getYearAsInt()).isEqualTo(2020);
+		expect(result.getMonthOfYear()).isEqualTo(1);
+		expect(result.getDayOfMonth()).isEqualTo(2);
+		expect(result.getHourOfDay()).isEqualTo(0);
+		expect(result.getMinuteOfHour()).isEqualTo(0);
+		expect(result.getSecondOfMinute()).isEqualTo(0);
+		expect(result.getMillisecondOfSecond()).isEqualTo(0);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_withYearAndMonthOfYearAndDayOfMonth() {
+		for (var y = 1600; y <= 3000; y++) {
+			for (var m = 1; m <= 12; m++) {
 				
 				final int dayCount;
 				if (m == 2) {
@@ -32,66 +60,38 @@ public final class TimeTest extends Test {
 				for (int d = 1; d <= dayCount; d++) {
 					
 					//execution
-					final Time time = Time.withYearAndMonthOfYearAndDayOfMonth(y, m, d);
+					final var result = Time.withYearAndMonthOfYearAndDayOfMonth(y, m, d);
 					
 					//verification
-					expect(time.getYearAsInt()).isEqualTo(y);
-					expect(time.getMonthOfYear()).isEqualTo(m);
-					expect(time.getDayOfMonth()).isEqualTo(d);
+					expect(result.getYearAsInt()).isEqualTo(y);
+					expect(result.getMonthOfYear()).isEqualTo(m);
+					expect(result.getDayOfMonth()).isEqualTo(d);
 				}
-			}
-		}
-	}
-	
-	//loop test case
-	public void loopTestCase_creation_1B() {
-				
-		//main loop
-		for (int h = 0; h <= 23; h++) {
-			for (int m = 0; m <= 59; m++) {
-					
-				//execution
-				final Time time	=
-				Time.withYearAndMonthOfYearAndDayOfMonthAndHourOfDayAndMinuteOfHour(2000, 1, 1, h, m);
-				
-				//verification
-				expect(time.getYearAsInt()).isEqualTo(2000);
-				expect(time.getMonthOfYear()).isEqualTo(1);
-				expect(time.getDayOfMonth()).isEqualTo(1);
-				expect(time.getHourOfDay()).isEqualTo(h);
-				expect(time.getMinuteOfHour()).isEqualTo(m);
-				expect(time.getSecondOfMinute()).isEqualTo(0);
-				expect(time.getMillisecondOfSecond()).isEqualTo(0);
 			}
 		}
 	}
 	
 	//method
 	@TestCase
-	public void testCase_getDay() {
-		
-		//setup
-		final Time time =
-		Time.withYearAndMonthOfYearAndDayOfMonthAndHourOfDayAndMinuteOfHourAndSecondOfMinuteAndMillisecondOfSecond(
-			2010,
-			10,
-			10,
-			10,
-			10,
-			10,
-			100
-		);
-		
-		//execution
-		final Time day = time.getDay();
-		
-		//verification
-		expect(day.getYearAsInt()).isEqualTo(2010);
-		expect(day.getMonthOfYear()).isEqualTo(10);
-		expect(day.getDayOfMonth()).isEqualTo(10);
-		expect(day.getHourOfDay()).isEqualTo(0);
-		expect(day.getMinuteOfHour()).isEqualTo(0);
-		expect(day.getSecondOfMinute()).isEqualTo(0);
-		expect(day.getMillisecondOfSecond()).isEqualTo(0);
+	public void testCase_withYearAndMonthOfYearAndDayOfMonthAndHourOfDayAndMinuteOfHour() {
+				
+		//main loop
+		for (var h = 0; h <= 23; h++) {
+			for (var m = 0; m <= 59; m++) {
+					
+				//execution
+				final var result =
+				Time.withYearAndMonthOfYearAndDayOfMonthAndHourOfDayAndMinuteOfHour(2000, 1, 1, h, m);
+				
+				//verification
+				expect(result.getYearAsInt()).isEqualTo(2000);
+				expect(result.getMonthOfYear()).isEqualTo(1);
+				expect(result.getDayOfMonth()).isEqualTo(1);
+				expect(result.getHourOfDay()).isEqualTo(h);
+				expect(result.getMinuteOfHour()).isEqualTo(m);
+				expect(result.getSecondOfMinute()).isEqualTo(0);
+				expect(result.getMillisecondOfSecond()).isEqualTo(0);
+			}
+		}
 	}
 }
