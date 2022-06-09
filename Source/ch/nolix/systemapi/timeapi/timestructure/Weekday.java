2465d28bcc106a803362e361400d6d49c4a5d832
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-09 22:53:43 +0200

    edited Weekday enum

--- Source/ch/nolix/systemapi/timeapi/timestructure/Weekday.java
+++ Source/ch/nolix/systemapi/timeapi/timestructure/Weekday.java
@@ -5,7 +5,6 @@ package ch.nolix.systemapi.timeapi.timestructure;
 import java.time.DayOfWeek;
 
 //own imports
-import ch.nolix.core.commontype.commontypehelper.GlobalStringHelper;
 import ch.nolix.core.container.LinkedList;
 import ch.nolix.core.document.node.BaseNode;
 import ch.nolix.core.document.node.Node;
@@ -53,11 +52,5 @@ public enum Weekday implements Specified {
 	@Override
 	public void fillUpAttributesInto(final LinkedList<Node> list) {
 		list.addAtEnd(Node.withHeader(name()));
-	}
-	
-	//method
-	@Override
-	public String toString() {
-		return GlobalStringHelper.toPascalCase(name());
 	}
 }
