d4c371867079d340999cb3fb4111290760de5324
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-10 17:49:03 +0200

    added fromJavaMonth static method to Month enum

--- Source/ch/nolix/systemapi/timeapi/timestructure/Month.java
+++ Source/ch/nolix/systemapi/timeapi/timestructure/Month.java
@@ -5,6 +5,7 @@ package ch.nolix.systemapi.timeapi.timestructure;
 import ch.nolix.core.container.LinkedList;
 import ch.nolix.core.document.node.BaseNode;
 import ch.nolix.core.document.node.Node;
+import ch.nolix.core.errorcontrol.invalidargumentexception.InvalidArgumentException;
 import ch.nolix.systemapi.elementapi.mainuniversalapi.Specified;
 
 //enum
@@ -21,6 +22,38 @@ public enum Month implements Specified {
 	OCTOBER,
 	NOVEMBER,
 	DECEMBER;
+	
+	//static method
+	public static Month fromJavaMonth(final java.time.Month month) {
+		switch (month) {
+			case JANUARY:
+				return JANUARY;
+			case FEBRUARY:
+				return FEBRUARY;
+			case MARCH:
+				return MARCH;
+			case APRIL:
+				return APRIL;
+			case MAY:
+				return MAY;
+			case JUNE:
+				return JUNE;
+			case JULY:
+				return JULY;
+			case AUGUST:
+				return AUGUST;
+			case SEPTEMBER:
+				return SEPTEMBER;
+			case OCTOBER:
+				return OCTOBER;
+			case NOVEMBER:
+				return NOVEMBER;
+			case DECEMBER:
+				return DECEMBER;
+			default:
+				throw new InvalidArgumentException(month);
+		}
+	}
 	
 	//static method
 	public Month fromSpecification(final BaseNode specification) {
