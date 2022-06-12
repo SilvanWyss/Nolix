9f74e90605f5da3162036cc3c239cc480cf26c6b
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-12 14:51:17 +0200

    beautified package info

--- Source/ch/nolix/systemtest/guitest/imagetest/package-info.java
+++ Source/ch/nolix/systemtest/guitest/imagetest/package-info.java
@@ -1,6 +1,7 @@
 //package info
 /**
- * The {@link ch.nolix.systemtest.guitest.imagetest} package defines tests for the {@link ch.nolix.system.gui.image} package.
+ * The {@link ch.nolix.systemtest.guitest.imagetest} package defines
+ * tests for the {@link ch.nolix.system.gui.image} package.
  * 
  * @author Silvan Wyss
  * @date 2020-06-01
