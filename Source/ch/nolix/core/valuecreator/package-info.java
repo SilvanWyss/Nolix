fe6ef1cd11865f8a03f975b97c1624ff687b2f61
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-14 23:08:32 +0200

    deleted valuecreator package

--- Source/ch/nolix/core/valuecreator/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.valuecreator} package
- * contains a creator that creates values, that are of given types and from given inputs.
- * 
- * @author Silvan Wyss
- * @date 2019-09-30
- */
-package ch.nolix.core.valuecreator;
