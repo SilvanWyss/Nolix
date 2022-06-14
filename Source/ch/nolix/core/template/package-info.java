75282d20a8ca7100a92748983f88412d570d9bf8
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-14 22:45:28 +0200

    deleted template package

--- Source/ch/nolix/core/template/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.template} package defines templates for the {@link ch.nolix.core} package .
- * 
- * @author Silvan Wyss
- * @date 2020-01-01
- */
-package ch.nolix.core.template;
