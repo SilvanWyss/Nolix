4e1efa98e6a8a6eedcd09241b2ef218f447b2696
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-15 23:44:03 +0200

    extended ChainedNodeTest

--- Source/ch/nolix/coretest/documenttest/chainednodetest/ChainedNodeTest.java
+++ Source/ch/nolix/coretest/documenttest/chainednodetest/ChainedNodeTest.java
@@ -3,12 +3,43 @@ package ch.nolix.coretest.documenttest.chainednodetest;
 
 //own imports
 import ch.nolix.core.document.chainednode.ChainedNode;
+import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentIsNullException;
+import ch.nolix.core.errorcontrol.invalidargumentexception.EmptyArgumentException;
+import ch.nolix.core.errorcontrol.invalidargumentexception.InvalidArgumentException;
 import ch.nolix.core.errorcontrol.invalidargumentexception.UnrepresentingArgumentException;
 import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.core.testing.test.Test;
 
 //class
 public final class ChainedNodeTest extends Test {
+	
+	//method
+	@TestCase
+	public void testCase_equals_whenIsBlankAndAnUnequalChainedNodeIsGiven() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("");
+		
+		//execution
+		final var result = testUnit.equals(ChainedNode.fromString("a"));
+		
+		//verification
+		expectNot(result);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_equals_whenIsBlankAndAnEqualChainedNodeIsGiven() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("");
+		
+		//execution
+		final var result = testUnit.equals(ChainedNode.fromString(""));
+		
+		//verification
+		expect(result);
+	}
 	
 	//method
 	@TestCase
@@ -222,6 +253,99 @@ public final class ChainedNodeTest extends Test {
 	
 	//method
 	@TestCase
+	public void testCase_getOneAttributeAsInt_whenDoesNotContainAttributes() {
+		
+		//setup
+		final var testUnit = ChainedNode.withHeader("a");
+		
+		//setup verification
+		expectNot(testUnit.containsAttributes());
+		
+		//execution & verification
+		expectRunning(testUnit::getOneAttributeAsInt)
+		.throwsException()
+		.ofType(EmptyArgumentException.class);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_getOneAttributeAsInt_whenContainsOneAttributeThatDoesNotRepresentAnInt() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("a(b)");
+		
+		//setup verification
+		expect(testUnit.getAttributeCount()).isEqualTo(1);
+		
+		//execution & verification
+		expectRunning(testUnit::getOneAttributeAsInt)
+		.throwsException()
+		.ofType(UnrepresentingArgumentException.class);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_getOneAttributeAsInt_whenContainsOneAttributeThatRepresentsAnInt() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("a(10)");
+		
+		//setup verification
+		expect(testUnit.getAttributeCount()).isEqualTo(1);
+		
+		//execution
+		final var result = testUnit.getOneAttributeAsInt();
+		
+		//verification
+		expect(result).isEqualTo(10);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_getOneAttributeAsInt_whenContainsSeveralAttributes() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("a(10, 20)");
+		
+		//setup verification
+		expect(testUnit.getAttributeCount()).isEqualTo(2);
+		
+		//execution & verification
+		expectRunning(testUnit::getOneAttributeAsInt)
+		.throwsException()
+		.ofType(InvalidArgumentException.class);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_isBlank_whenIsBlank() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("");
+		
+		//execution
+		final var result = testUnit.isBlank();
+		
+		//verification
+		expect(result);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_isBlank_whenHasHeaderOnly() {
+		
+		//setup
+		final var testUnit = ChainedNode.withHeader("a");
+		
+		//execution
+		final var result = testUnit.isBlank();
+		
+		//verification
+		expectNot(result);
+	}
+	
+	//method
+	@TestCase
 	public void testCase_toInt_whenDoesNotRepresentInt_A1() {
 		
 		//setup
@@ -288,5 +412,69 @@ public final class ChainedNodeTest extends Test {
 		
 		//verification
 		expect(result).isEqualTo(0);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_toNode_whenIsBlank() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("");
+		
+		//execution
+		final var result = testUnit.toNode();
+		
+		//verification
+		expect(result).hasStringRepresentation("");
+	}
+	
+	//method
+	@TestCase
+	public void testCase_toNode_whenHasHeaderOnly() {
+		
+		//setup
+		final var testUnit = ChainedNode.withHeader("a");
+		
+		//execution
+		final var result = testUnit.toNode();
+		
+		//verification
+		expect(result).hasStringRepresentation("a");
+	}
+	
+	//method
+	@TestCase
+	public void testCase_toNode_whenHasNextNode() {
+		
+		//setup
+		final var testUnit = ChainedNode.fromString("a.b");
+		
+		//execution & verification
+		expectRunning(testUnit::toNode)
+		.throwsException()
+		.ofType(UnrepresentingArgumentException.class)
+		.withMessage("The given ChainedNode 'a.b' does not represent a Node.");
+	}
+	
+	//method
+	@TestCase
+	public void testCase_withHeader_whenNullHeaderIsGiven() {
+		
+		//execution & verification
+		expectRunning(() -> ChainedNode.withHeader(null))
+		.throwsException()
+		.ofType(ArgumentIsNullException.class)
+		.withMessage("The given header is null.");
+	}
+	
+	//method
+	@TestCase
+	public void testCase_withHeader_whenHeaderIsGiven() {
+		
+		//execution
+		final var result = ChainedNode.withHeader("a");
+		
+		//verification
+		expect(result).hasStringRepresentation("a");
 	}
 }
