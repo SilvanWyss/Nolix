2db152a546cc8b6072198d34514b2a5f05a51531
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-16 17:54:06 +0200

    created Launcher

---
+++ Source/ch/nolix/coretest/documenttest/Launcher.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.coretest.documenttest;
+
+//class
+public final class Launcher {
+	
+	//main method
+	public static void main(String[] args) {
+		new DocumentTestPool().run();
+	}
+	
+	//constructor
+	private Launcher() {}
+}
