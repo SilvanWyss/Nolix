12bfa1efcbdeb21c090a4f97cc07cc6c90240f51
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-16 17:13:18 +0200

    created Launcher

---
+++ Source/ch/nolix/coretest/nettest/Launcher.java
@@ -0,0 +1,27 @@
+//package declaration
+package ch.nolix.coretest.nettest;
+
+//class
+/**
+* @author Silvan Wyss
+* @date 2022-06-16
+*/
+public final class Launcher {
+
+	//main method
+	/**
+	 * Creates a new {@link NetTestPool} and runs it.
+	 * 
+	 * @param arguments
+	 */
+	public static void main(final String[] arguments) {
+		new NetTestPool().run();
+	}
+	
+	//constructor
+	/**
+	 * Prevents that an instance of the {@link Launcher} can be created.
+	 */
+	private Launcher() {}
+}
+
