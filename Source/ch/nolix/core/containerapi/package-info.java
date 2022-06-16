d4b6b79b3fd2a31e3f6c6d447425f89da61f352c
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-16 20:40:29 +0200

    created containerapi package

---
+++ Source/ch/nolix/core/containerapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.containerapi} package declares types containers.
+ * Containers are data structures.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-06-16
+ */
+package ch.nolix.core.containerapi;
