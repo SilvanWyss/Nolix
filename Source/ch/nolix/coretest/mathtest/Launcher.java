1f1cca3d2aad2d841b220a27b176ed14930f2e5a
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-16 18:03:21 +0200

    edited Launcher

--- Source/ch/nolix/coretest/mathtest/Launcher.java
+++ Source/ch/nolix/coretest/mathtest/Launcher.java
@@ -7,10 +7,10 @@ package ch.nolix.coretest.mathtest;
  * @date 2016-09-01
  */
 public final class Launcher {
-
+	
 	//main method
 	/**
-	 * Creates a new mathematics test pool and executes it.
+	 * Creates a new {@link MathTestPool} and executes it.
 	 * 
 	 * @param arguments
 	 */
