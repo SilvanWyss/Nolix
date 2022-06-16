1c9c82b549aeec1d499289ea7bcdcce0d3823be5
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-16 17:30:38 +0200

    created Launcher

---
+++ Source/ch/nolix/coretest/errorcontroltest/Launcher.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.coretest.errorcontroltest;
+
+//class
+public final class Launcher {
+	
+	//main method
+	public static void main(String[] args) {
+		new ErrorControlTestPool().run();
+	}
+	
+	//constructor
+	private Launcher() {}
+}
