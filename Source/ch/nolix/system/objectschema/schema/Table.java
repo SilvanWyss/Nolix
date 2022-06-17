3b2ee9ccc1b6021fe2ebfe46e03e70a2aeb29445
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-17 22:19:52 +0200

    beautified Table

--- Source/ch/nolix/system/objectschema/schema/Table.java
+++ Source/ch/nolix/system/objectschema/schema/Table.java
@@ -224,7 +224,10 @@ public final class Table extends SchemaObject implements ITable<SchemaImplementa
 		
 		final var tables = getParentDatabase().getRefTables();
 		
-		columns = LinkedList.fromIterable(internalgetRefRawSchemaAdapter().loadColumnsOfTable(this).to(c -> Column.fromDTO(c, tables)));
+		columns =
+		LinkedList.fromIterable(
+			internalgetRefRawSchemaAdapter().loadColumnsOfTable(this).to(c -> Column.fromDTO(c, tables))
+		);
 		
 		for (final var c : columns) {
 			final var column = (Column)c;
