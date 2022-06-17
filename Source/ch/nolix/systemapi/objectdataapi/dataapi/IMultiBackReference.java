081cdbe65419bd229a7c924424f9066076971824
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-17 17:50:24 +0200

    made IMultiBackReference not a IContainer

--- Source/ch/nolix/systemapi/objectdataapi/dataapi/IMultiBackReference.java
+++ Source/ch/nolix/systemapi/objectdataapi/dataapi/IMultiBackReference.java
@@ -1,24 +1,8 @@
 //package declaration
 package ch.nolix.systemapi.objectdataapi.dataapi;
 
-//own imports
-import ch.nolix.core.container.IContainer;
-
 //interface
 public interface IMultiBackReference<
 	IMPL,
 	E extends IEntity<IMPL>
-> extends IContainer<E>, IBaseBackReference<IMPL, E> {
-	
-	//method
-	@Override
-	default boolean containsAny() {
-		return IContainer.super.containsAny();
-	}
-	
-	//method
-	@Override
-	default boolean isEmpty() {
-		return IContainer.super.isEmpty();
-	}
-}
+> extends IBaseBackReference<IMPL, E> {}
