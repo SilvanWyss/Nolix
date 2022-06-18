031b984c56e41149dbc49cc0746aaf4baf11a9a3
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-18 14:01:19 +0200

    created marker package

---
+++ Source/ch/nolix/core/programatom/marker/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.programatom.marker} package defines markers.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-06-18
+ */
+package ch.nolix.core.programatom.marker;
