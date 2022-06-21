29d6900e618bee78ebcee81a9b74854ab6cc3260
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-21 20:35:14 +0200

    refactored BiggerArgumentException

--- Source/ch/nolix/core/errorcontrol/invalidargumentexception/BiggerArgumentException.java
+++ Source/ch/nolix/core/errorcontrol/invalidargumentexception/BiggerArgumentException.java
@@ -4,77 +4,51 @@ package ch.nolix.core.errorcontrol.invalidargumentexception;
 //class
 /**
  * A {@link BiggerArgumentException} is a {@link InvalidArgumentException}
- * that is supposed to be thrown when an argument is undesirably bigger than a given limit.
+ * that is supposed to be thrown when a given argument is undesirably bigger than a certain max.
  * 
  * @author Silvan Wyss
  * @date 2016-03-01
  */
 @SuppressWarnings("serial")
 public final class BiggerArgumentException extends InvalidArgumentException {
-
-	//constructor
-	/**
-	 * Creates a new bigger argument exception for the given argument and limit.
-	 * 
-	 * @param argument
-	 * @param limit
-	 */
-	public BiggerArgumentException(final double argument, final double limit) {
-		
-		//Calls constructor of the base class.
-		super(argument, "is bigger than " + limit);
-	}
 	
 	//constructor
 	/**
-	 * Creates a new bigger argument exception for the given argument and limit.
-	 * 
-	 * @param argument
-	 * @param limit
-	 */
-	public BiggerArgumentException(final long argument, final long limit) {
-		
-		//Calls constructor of the base class.
-		super(argument, "is bigger than " + limit);
-	}
-	
-	//constructor
-	/**
-	 * Creates a new bigger argument exception for the given argument,
-	 * that has the given argument name, and the given limit.
+	 * Creates a new {@link BiggerArgumentException} for the given argumentName, argument and max.
 	 * 
 	 * @param argumentName
 	 * @param argument
-	 * @param limit
-	 * @throws IllegalArgumentException if the given argument name is null.
-	 * @throws IllegalArgumentException if the given argument name is empty.
+	 * @param max
+	 * @throws IllegalArgumentException if the given argumentName is null.
+	 * @throws IllegalArgumentException if the given argumentName is blank.
 	 */
 	public BiggerArgumentException(
 		final String argumentName,
 		final double argument,
-		final double limit) {
+		final double max
+	) {
 		
 		//Calls constructor of the base class.
-		super(argumentName,	argument, "is bigger than " + limit);
+		super(argumentName,	argument, "is bigger than " + max);
 	}
 	
 	//constructor
 	/**
-	 * Creates a new bigger argument exception for the given argument,
-	 * that has the given argument name, and the given limit.
+	 * Creates a new {@link BiggerArgumentException} for the given argumentName, argument and max.
 	 * 
 	 * @param argumentName
 	 * @param argument
-	 * @param limit
-	 * @throws IllegalArgumentException if the given argument name is null.
-	 * @throws IllegalArgumentException if the given argument name is empty.
+	 * @param max
+	 * @throws IllegalArgumentException if the given argumentName is null.
+	 * @throws IllegalArgumentException if the given argumentName is blank.
 	 */
 	public BiggerArgumentException(
 		final String argumentName,
 		final long argument,
-		final long limit) {
+		final long max
+	) {
 		
 		//Calls constructor of the base class.
-		super(argumentName, argument, "is bigger than " + limit);
+		super(argumentName,	argument, "is bigger than " + max);
 	}
 }
