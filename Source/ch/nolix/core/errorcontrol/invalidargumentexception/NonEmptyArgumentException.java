268606c04cf0da0e5b9d6e34428a17dd73db14b4
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-23 22:27:47 +0200

    refactored NonEmptyArgumentException

--- Source/ch/nolix/core/errorcontrol/invalidargumentexception/NonEmptyArgumentException.java
+++ Source/ch/nolix/core/errorcontrol/invalidargumentexception/NonEmptyArgumentException.java
@@ -3,38 +3,38 @@ package ch.nolix.core.errorcontrol.invalidargumentexception;
 
 //class
 /**
- * A non empty argument exception is an argument exception
- * that is supposed to be thrown when an argument is undesired not empty.
+ * A {@link NonEmptyArgumentException} is a {@link InvalidArgumentException} that
+ * is supposed to be thrown when a given argument is undesirably not (!) empty.
  * 
  * @author Silvan Wyss
  * @date 2017-01-01
  */
 @SuppressWarnings("serial")
 public final class NonEmptyArgumentException extends InvalidArgumentException {
-
+	
 	//constant
 	private static final String ERROR_PREDICATE = "is not empty";
 	
 	//constructor
 	/**
-	 * Creates a new non empty argument exception for the given argument.
+	 * Creates a new {@link NonEmptyArgumentException} for the given argument.
 	 * 
 	 * @param argument
-	 * @throws IllegalArgumentException if the given argument is null.
 	 */
 	public NonEmptyArgumentException(final Object argument) {
-
+		
 		//Calls constructor of the base class.
 		super(argument, ERROR_PREDICATE);
 	}
 	
 	//constructor
 	/**
-	 * Creates a new non empty argument exception for the given argument with the given argumet name.
+	 * Creates a new {@link NonEmptyArgumentException} for the given argumentName and argument.
+	 * 
 	 * @param argumentName
 	 * @param argument
-	 * @throws IllegalArgumentException if the given argument name is null.
-	 * @throws IllegalArgumentException if the given argument name is empty.
+	 * @throws IllegalArgumentException if the given argumentName is null.
+	 * @throws IllegalArgumentException if the given argumentName is empty.
 	 */
 	public NonEmptyArgumentException(final String argumentName, final Object argument) {
 		
