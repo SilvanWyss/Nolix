8653699408ed20bca9c74aad001ad5f92d151b19
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-24 13:39:23 +0200

    created nodeapi package

---
+++ Source/ch/nolix/core/document/nodeapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.document.nodeapi} package declares a document node.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-06-24
+ */
+package ch.nolix.core.document.nodeapi;
