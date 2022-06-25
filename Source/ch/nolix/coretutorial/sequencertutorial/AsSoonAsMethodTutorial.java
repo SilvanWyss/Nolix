f7f06b167a96e52630a48c42f4a1fba841ba72d8
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-25 21:08:40 +0200

    edited AsSoonAsMethodTutorial

--- Source/ch/nolix/coretutorial/sequencertutorial/AsSoonAsMethodTutorial.java
+++ Source/ch/nolix/coretutorial/sequencertutorial/AsSoonAsMethodTutorial.java
@@ -7,6 +7,8 @@ import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
 
 public final class AsSoonAsMethodTutorial {
 	
+	private static final Random random = new Random();
+	
 	public static void main(String[] args) {
 		GlobalSequencer
 		.asSoonAs(() -> getRandomNumberBetween1And100() == 100)
@@ -14,7 +16,7 @@ public final class AsSoonAsMethodTutorial {
 	}
 	
 	private static int getRandomNumberBetween1And100() {
-		return (new Random().nextInt(100) + 1);
+		return (random.nextInt(100) + 1);
 	}
 	
 	private AsSoonAsMethodTutorial() {}
