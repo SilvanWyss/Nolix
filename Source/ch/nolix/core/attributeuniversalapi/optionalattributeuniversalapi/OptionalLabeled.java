2dd6d1bd6197fcd6a69beb370c18501c0945672b
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-25 11:19:54 +0200

    turned default methods of OptionalLabeled to normal abstract methods

--- Source/ch/nolix/core/attributeuniversalapi/optionalattributeuniversalapi/OptionalLabeled.java
+++ Source/ch/nolix/core/attributeuniversalapi/optionalattributeuniversalapi/OptionalLabeled.java
@@ -1,8 +1,6 @@
 //package declaration
 package ch.nolix.core.attributeuniversalapi.optionalattributeuniversalapi;
 
-import ch.nolix.coreapi.markerapi.AllowDefaultMethodsAsDesignPattern;
-
 //interface
 /**
  * A {@link OptionalLabeled} can have an info string.
@@ -10,7 +8,6 @@ import ch.nolix.coreapi.markerapi.AllowDefaultMethodsAsDesignPattern;
  * @author Silvan Wyss
  * @date 2020-03-29
  */
-@AllowDefaultMethodsAsDesignPattern
 public interface OptionalLabeled {
 	
 	//method declaration
@@ -19,33 +16,15 @@ public interface OptionalLabeled {
 	 */
 	String getInfoString();
 	
-	//method
+	//method declaration
 	/**
 	 * @return the info string of the current {@link OptionalLabeled} in quotes.
 	 */
-	default String getInfoStringInQuotes() {
-		return ("'" + getInfoString() + "'");
-	}
+	String getInfoStringInQuotes();
 	
 	//method declaration
 	/**
 	 * @return true if the current {@link OptionalLabeled} has an info string.
 	 */
 	boolean hasInfoString();
-	
-	//method
-	/**
-	 * @param infoString
-	 * @return true if the current {@link OptionalLabeled} has the given info string.
-	 */
-	default boolean hasInfoString(final String infoString) {
-		
-		//Handles the case that the current OptionalLabeled does not have an info string.
-		if (!hasInfoString()) {
-			return false;
-		}
-		
-		//Handles the case that the current OptionalLabeled has an info string.
-		return getInfoString().equals(infoString);
-	}
 }
