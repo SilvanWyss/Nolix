84b28f6387cc453d3ea13685c4e1e58c49498bb5
Silvan Wyss <silvan.wyss@gmail.com>
2022-06-25 18:48:24 +0200

    deleted unused packages

--- Source/ch/nolix/system/databaseapplication/main/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.databaseapplication.main} package defines a general database application.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.system.databaseapplication.main;
--- Source/ch/nolix/systemtutorial/databaseapplicationtutorial/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemtutorial.databaseapplicationtutorial} package defines
- * tutorials for the {@link ch.nolix.system.databaseapplication} package.
- * 
- * @author Silvan Wyss
- * @date 2020-09-01
- */
-package ch.nolix.systemtutorial.databaseapplicationtutorial;
