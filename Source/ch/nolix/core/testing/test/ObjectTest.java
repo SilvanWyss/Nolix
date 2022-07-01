984f37f269c30192ddc25afda0a0d90bcecc31f7
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-01 20:53:29 +0200

    extended ObjectTest

--- Source/ch/nolix/core/testing/test/ObjectTest.java
+++ Source/ch/nolix/core/testing/test/ObjectTest.java
@@ -1,6 +1,9 @@
 //package declaration
 package ch.nolix.core.testing.test;
 
+//own imports
+import ch.nolix.core.testing.basetest.TestCase;
+
 //class
 /**
  * A {@link ObjectTest} is a test to check test units of a certain type.
@@ -10,6 +13,67 @@ package ch.nolix.core.testing.test;
  * @param <OT> is the type of the test units of a {@link ObjectTest}.
  */
 public abstract class ObjectTest<OT> extends Test {
+	
+	//method
+	@TestCase
+	public void testCase_equals_whenEqualInstanceIsGiven() throws Exception {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		final var object = createTestUnit();
+		
+		//execution
+		final var result = testUnit.equals(object);
+		
+		//verification
+		expect(result);
+		
+		//cleanup
+		if (testUnit instanceof AutoCloseable) {
+			((AutoCloseable)testUnit).close();
+		}
+		if (object instanceof AutoCloseable) {
+			((AutoCloseable)object).close();
+		}
+	}
+	
+	//method
+	@TestCase
+	public void testCase_equals_whenNullIsGiven() throws Exception {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		
+		//execution
+		final var result = testUnit.equals(null);
+		
+		//verification
+		expectNot(result);
+		
+		//cleanup
+		if (testUnit instanceof AutoCloseable) {
+			((AutoCloseable)testUnit).close();
+		}
+	}
+	
+	//method
+	@TestCase
+	public void testCase_equals_whenSeflIsGiven() throws Exception {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		
+		//execution
+		final var result = testUnit.equals(testUnit);
+		
+		//verification
+		expect(result);
+		
+		//cleanup
+		if (testUnit instanceof AutoCloseable) {
+			((AutoCloseable)testUnit).close();
+		}
+	}
 	
 	//method declaration
 	/**
