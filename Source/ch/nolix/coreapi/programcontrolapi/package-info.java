2693ec624526e71f3e03f0c810af5801508b2cae
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-02 13:55:03 +0200

    created programcontrolapi package

---
+++ Source/ch/nolix/coreapi/programcontrolapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.programcontrolapi} package declares types for program control.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-02
+ */
+package ch.nolix.coreapi.programcontrolapi;
