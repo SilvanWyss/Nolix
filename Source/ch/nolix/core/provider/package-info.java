08c3a2698f7652e9e97b00a73d24f3037bb0569d
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-02 14:17:26 +0200

    created providerapi package

--- Source/ch/nolix/core/provider/package-info.java
+++ Source/ch/nolix/core/provider/package-info.java
@@ -1,6 +1,6 @@
 //package info
 /**
- * The {@link ch.nolix.core.provider} package defines provider for services and implementations.
+ * The {@link ch.nolix.core.provider} package defines providers for services and implementations.
  * 
  * @author Silvan Wyss
  * @date 2021-03-07
---
+++ Source/ch/nolix/coreapi/providerapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.providerapi} package declares providers for services and implementations.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-02
+ */
+package ch.nolix.coreapi.providerapi;
