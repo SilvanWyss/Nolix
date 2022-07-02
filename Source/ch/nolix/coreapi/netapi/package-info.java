5aa099f9d877a759ef5e4ad007d3048361b71952
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-02 17:28:52 +0200

    created netapi package

---
+++ Source/ch/nolix/coreapi/netapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.netapi} package declares types for net communication.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-02
+ */
+package ch.nolix.coreapi.netapi;
