f9ae4c30f3eb8a1c2356e87aba04ef732d513e57
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-03 17:41:25 +0200

    created geometryapi package

---
+++ Source/ch/nolix/coreapi/geometryapi/package-info.java
@@ -0,0 +1,8 @@
+//package declaration
+/**
+ * The {@link ch.nolix.coreapi.geometryapi} package declares types for geometry.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-03
+ */
+package ch.nolix.coreapi.geometryapi;
