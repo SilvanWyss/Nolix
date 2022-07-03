6fecc88ceedefdce46f487aa8ed33040994316f8
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-03 16:59:47 +0200

    created pairapi package

---
+++ Source/ch/nolix/coreapi/containerapi/pairapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.containerapi.pairapi} package declares pairs.
+ * Pairs are containers that store 2 elements.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-03
+ */
+package ch.nolix.coreapi.containerapi.pairapi;
