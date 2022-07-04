57a09932028a08467d7b75b1e6624b22a3277f5b
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-04 22:00:11 +0200

    deleted attributeuniversalapi package

--- Source/ch/nolix/core/attributeuniversalapi/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.attributeuniversalapi} package declares types with a certain attribute.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.core.attributeuniversalapi;
