2273e293996226e44981d6cea1740e5ce39144b7
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-05 22:58:07 +0200

    created matrixapi package

---
+++ Source/ch/nolix/coreapi/containerapi/matrixapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.containerapi.matrixapi} package declares matrixes.
+ * A matrix is a 2-dimensional {@link IContainer}.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-05
+ */
+package ch.nolix.coreapi.containerapi.matrixapi;
