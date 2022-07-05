79079e277a286a09be5947f8f941dd3f76383b42
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-05 20:05:26 +0200

    deleted skilluniversalapi package

--- Source/ch/nolix/core/skilluniversalapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.skilluniversalapi} package is an API that declares 
- * types that can do certain actions or reply certain requests.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.core.skilluniversalapi;
