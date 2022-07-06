983b02a4559b104901701f2ac90a0e79bc61b5aa
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-06 20:36:32 +0200

    edited ImageApplication enum

--- Source/ch/nolix/systemapi/guiapi/imageapi/ImageApplication.java
+++ Source/ch/nolix/systemapi/guiapi/imageapi/ImageApplication.java
@@ -2,25 +2,15 @@
 package ch.nolix.systemapi.guiapi.imageapi;
 
 //own imports
-import ch.nolix.core.commontype.commontypehelper.GlobalStringHelper;
-import ch.nolix.core.document.node.Node;
-import ch.nolix.coreapi.containerapi.mainapi.IMutableList;
 import ch.nolix.coreapi.documentapi.nodeapi.INode;
-import ch.nolix.systemapi.elementapi.mainuniversalapi.Specified;
 
 //enum
-public enum ImageApplication implements Specified {
+public enum ImageApplication {
 	SCALE_TO_FRAME,
 	REPEATE;
 	
 	//static method
 	public static ImageApplication fromSpecification(final INode<?> specification) {
-		return valueOf(GlobalStringHelper.toUpperSnakeCase(specification.getSingleChildNodeHeader()));
-	}
-	
-	//method
-	@Override
-	public void fillUpAttributesInto(final IMutableList<INode<?>> list) {
-		list.addAtEnd(Node.withHeader(GlobalStringHelper.toPascalCase(toString())));
+		return valueOf(specification.getSingleChildNodeHeader());
 	}
 }
