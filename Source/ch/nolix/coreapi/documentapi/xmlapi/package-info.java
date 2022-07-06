df556fc85aabf91d81f4212c1f9b8f7ca949df36
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-06 22:14:59 +0200

    created xmlapi package

---
+++ Source/ch/nolix/coreapi/documentapi/xmlapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.documentapi.xmlapi} package declares a XML node.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-06
+ */
+package ch.nolix.coreapi.documentapi.xmlapi;
