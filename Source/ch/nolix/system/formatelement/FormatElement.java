f932cc24f6b47df55f2682d3e43b2b8af3683d14
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-08 11:50:21 +0200

    made FormatElement to a Element

--- Source/ch/nolix/system/formatelement/FormatElement.java
+++ Source/ch/nolix/system/formatelement/FormatElement.java
@@ -13,10 +13,12 @@ import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.core.programatom.name.LowerCaseCatalogue;
 import ch.nolix.coreapi.containerapi.mainapi.IContainer;
 import ch.nolix.coreapi.documentapi.nodeapi.INode;
+import ch.nolix.system.element.main.Element;
 import ch.nolix.systemapi.elementapi.mainuniversalapi.IRespondingMutableElement;
 
 //class
 public abstract class FormatElement<FE extends FormatElement<FE, S>, S extends Enum<S>>
+extends Element
 implements IRespondingMutableElement<FE> {
 	
 	//static method
