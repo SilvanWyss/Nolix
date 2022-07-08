452711c6a6f9d359aca00e680380e60c221edbf5
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-08 12:33:22 +0200

    edited IImageGenerator

--- Source/ch/nolix/businessapi/bigdecimalmathapi/IImageGenerator.java
+++ Source/ch/nolix/businessapi/bigdecimalmathapi/IImageGenerator.java
@@ -1,12 +1,13 @@
 //package declaration
 package ch.nolix.businessapi.bigdecimalmathapi;
 
+//own imports
 import ch.nolix.coreapi.programcontrolapi.futureuniversalapi.IFuture;
-import ch.nolix.system.gui.image.MutableImage;
+import ch.nolix.systemapi.guiapi.imageapi.IMutableImage;
 
 //interface
 public interface IImageGenerator extends IFuture {
 	
 	//method declaration
-	MutableImage getRefImage();
+	IMutableImage<?> getRefImage();
 }
