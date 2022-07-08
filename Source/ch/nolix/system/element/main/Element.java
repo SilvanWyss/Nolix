b74530960a7a7626f4fe402e0cd6aeb6db841023
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-08 11:38:27 +0200

    created Element

---
+++ Source/ch/nolix/system/element/main/Element.java
@@ -0,0 +1,62 @@
+//package declaration
+package ch.nolix.system.element.main;
+
+//own imports
+import ch.nolix.core.document.node.Node;
+import ch.nolix.systemapi.elementapi.mainuniversalapi.Specified;
+
+//class
+/**
+ * @author Silvan Wyss
+ * @date 2022-07-08
+ */
+public abstract class Element implements Specified {
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final boolean equals(final Object object) {
+		return
+		object != null
+		&& getClass() == object.getClass()
+		&& hasSameSpecificationAs((Element)object);
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final Node getSpecification() {
+		return Node.withHeaderAndChildNodes(getSpecificationHeader(), getAttributes());
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final int hashCode() {
+		return getSpecification().hashCode();
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final String toString() {
+		return getSpecification().toString();
+	}
+	
+	//method
+	/**
+	 * @param element
+	 * @return true if the current {@link Element} has the same specification as the given element.
+	 */
+	private boolean hasSameSpecificationAs(final Element element) {
+		return getSpecification().equals(element.getSpecification());
+	}
+}
---
+++ Source/ch/nolix/system/element/main/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.system.element} package defines a base element.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-08-07
+ */
+package ch.nolix.system.element.main;
