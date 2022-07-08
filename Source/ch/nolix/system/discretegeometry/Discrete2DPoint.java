52f39e4b4a6e745c10288dd6f00159f67724f7be
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-08 11:59:36 +0200

    made Discrete2DPoint to a Element

--- Source/ch/nolix/system/discretegeometry/Discrete2DPoint.java
+++ Source/ch/nolix/system/discretegeometry/Discrete2DPoint.java
@@ -1,10 +1,12 @@
 //package declaration
 package ch.nolix.system.discretegeometry;
 
+//own imports
 import ch.nolix.core.container.main.LinkedList;
 import ch.nolix.core.document.node.Node;
 import ch.nolix.coreapi.containerapi.mainapi.IContainer;
 import ch.nolix.coreapi.documentapi.nodeapi.INode;
+import ch.nolix.system.element.main.Element;
 import ch.nolix.systemapi.elementapi.mainuniversalapi.Specified;
 
 //class
@@ -15,7 +17,7 @@ import ch.nolix.systemapi.elementapi.mainuniversalapi.Specified;
  * @author Silvan Wyss
  * @date 2019-05-19
  */
-public final class Discrete2DPoint implements Specified {
+public final class Discrete2DPoint extends Element implements Specified {
 	
 	//static method
 	/**
