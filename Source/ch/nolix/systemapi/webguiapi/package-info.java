a95b49b237004fe2bf5d838bea34ea046b75781c
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-12 20:01:18 +0200

    created webguiapi package

---
+++ Source/ch/nolix/systemapi/webguiapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.webguiapi} package declares a web GUI.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-12
+ */
+package ch.nolix.systemapi.webguiapi;
