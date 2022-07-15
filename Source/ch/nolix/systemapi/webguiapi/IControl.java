2cc89166a7b5e32444214c28757f5fe7e4458e20
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-15 20:41:17 +0200

    extended IControl

--- Source/ch/nolix/systemapi/webguiapi/IControl.java
+++ Source/ch/nolix/systemapi/webguiapi/IControl.java
@@ -3,13 +3,21 @@ package ch.nolix.systemapi.webguiapi;
 
 //own imports
 import ch.nolix.systemapi.elementapi.configurationapi.IConfigurableElement;
+import ch.nolix.systemapi.guiapi.inputapi.IInputTaker;
+import ch.nolix.systemapi.guiapi.mainapi.CursorIcon;
 
 //interface
-public interface IControl<C extends IControl<C>> extends IConfigurableElement<C> {
+public interface IControl<C extends IControl<C>> extends IConfigurableElement<C>, IInputTaker {
 	
 	//method declaration
 	boolean belongsToGUI();
 	
 	//method declaration
+	CursorIcon getCursorIcon();
+	
+	//method declaration
 	IWebGUI<?> getParentGUI();
+	
+	//method declaration
+	C setCursorIcon(CursorIcon cursorIcon);
 }
