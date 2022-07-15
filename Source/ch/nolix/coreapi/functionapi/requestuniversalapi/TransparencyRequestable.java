daa5086106dd12b1ca14a95ce7e7f6b70cbdf368
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-15 21:02:10 +0200

    created TransparencyRequestable

---
+++ Source/ch/nolix/coreapi/functionapi/requestuniversalapi/TransparencyRequestable.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.coreapi.functionapi.requestuniversalapi;
+
+//own imports
+import ch.nolix.coreapi.markerapi.AllowDefaultMethodsAsDesignPattern;
+
+//interface
+/**
+* A {@link TransparencyRequestable} can be asked if it is transparent.
+* 
+* @author Silvan Wyss
+* @date 2022-07-15
+*/
+@AllowDefaultMethodsAsDesignPattern
+public interface TransparencyRequestable {
+	
+	//method
+	/**
+	 * @return true if the current {@link TransparencyRequestable} is opaque.
+	 */
+	default boolean isOpaque() {
+		return !isTransparent();
+	}
+	
+	//method
+	/**
+	 * @return true if the current {@link TransparencyRequestable} is transparent.
+	 */
+	boolean isTransparent();
+}
