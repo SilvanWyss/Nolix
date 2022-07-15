4515edcd8beeff93ff1e5acf1cb751cda14a5809
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-15 23:03:50 +0200

    created controlapi package

---
+++ Source/ch/nolix/systemapi/webguiapi/controlapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.webguiapi.controlapi} package declares
+ * {@link ch.nolix.systemapi.webguiapi.mainapi.IControl}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-15
+ */
+package ch.nolix.systemapi.webguiapi.controlapi;
