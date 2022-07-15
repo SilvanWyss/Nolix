3da7df300674235308857a676c0deda03e009b58
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-15 22:53:37 +0200

    created BackgroundType enum

---
+++ Source/ch/nolix/systemapi/guiapi/structureproperty/BackgroundType.java
@@ -0,0 +1,27 @@
+//package declaration
+package ch.nolix.systemapi.guiapi.structureproperty;
+
+//own imports
+import ch.nolix.coreapi.documentapi.nodeapi.INode;
+
+//enum
+/**
+ * @author Silvan Wyss
+ * @date 2022-07-15
+ */
+public enum BackgroundType {
+	TRANSPARENT,
+	COLOR,
+	COLOR_GRADIENT,
+	IMAGE;
+	
+	//static method
+	/**
+	 * @param specification
+	 * @return a new {@link BackgroundType} from the given specification.
+	 * @throws RuntimeException if the given specification does not represent a {@link BackgroundType}.
+	 */
+	public static BackgroundType fromSpecification(final INode<?> specification) {
+		return valueOf(specification.getSingleChildNodeHeader());
+	}
+}
