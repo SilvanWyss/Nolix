6569b5de03674c18afdda97953d25a481f4592fb
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-17 15:42:41 +0200

    created itemmenuapi package

---
+++ Source/ch/nolix/systemapi/webguiapi/itemmenuapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.webguiapi.itemmenuapi} package declares {@link IConrols} that are item menus.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-17
+ */
+package ch.nolix.systemapi.webguiapi.itemmenuapi;
