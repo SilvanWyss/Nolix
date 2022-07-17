da5c2984ece296ad8cc83b0ad1f1cb20ab0dc70c
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-17 20:35:24 +0200

    created IKeyValuePair

---
+++ Source/ch/nolix/coreapi/documentapi/keyvalueapi/IKeyValuePair.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.coreapi.documentapi.keyvalueapi;
+
+//interface
+public interface IKeyValuePair {
+	
+	//method declaration
+	String getKey();
+	
+	//method declaration
+	String getValue();
+}
