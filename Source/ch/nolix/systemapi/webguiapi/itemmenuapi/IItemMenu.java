67e000bf15ca770e0a0ed4ac94d7d7d8e9a97976
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-17 19:44:51 +0200

    extended IItemMenu

--- Source/ch/nolix/systemapi/webguiapi/itemmenuapi/IItemMenu.java
+++ Source/ch/nolix/systemapi/webguiapi/itemmenuapi/IItemMenu.java
@@ -3,6 +3,8 @@ package ch.nolix.systemapi.webguiapi.itemmenuapi;
 
 //own imports
 import ch.nolix.coreapi.containerapi.mainapi.IContainer;
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IAction;
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IElementTaker;
 import ch.nolix.coreapi.functionapi.mutationuniversalapi.Clearable;
 import ch.nolix.systemapi.webguiapi.mainapi.IControl;
 
@@ -16,6 +18,45 @@ extends Clearable, IControl<IM, IML> {
 	
 	//method declaration
 	IM addItem(@SuppressWarnings("unchecked")IMI... items);
+	
+	//method declaration
+	IM addBlankItem();
+	
+	//method declaration
+	IM addItemWithIdAndText(String it, String text);
+	
+	//method declaration
+	IM addItemWithIdAndTextAndSelectAction(String id, String text, IAction selectAction);
+	
+	//method declaration
+	IM addItemWithIdAndTextAndSelectAction(String id, String text, IElementTaker<IItemMenuItem> selectAction);
+	
+	//method declaration
+	IM addItemWithText(String... texts);
+	
+	//method declaration
+	IM addItemWithTextAndSelectAction(String text, IAction selectAction);
+	
+	//method declaration
+	IM addItemWithTextAndSelectAction(String text, IElementTaker<IItemMenuItem> selectAction);
+	
+	//method declaration
+	boolean containsBlankItem();
+	
+	//method declaration
+	boolean containsItemWithId(String id);
+	
+	//method declaration
+	boolean containsItemWithText(String text);
+	
+	//method declaration
+	String getIdByItemText(String itemTxt);
+	
+	//method declaration
+	String getTextByItemId(String itemId);
+	
+	//method declaration
+	IM selectFirstItem();
 	
 	//method declaration
 	IContainer<IMI> getRefItems();
