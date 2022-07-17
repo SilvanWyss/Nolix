accea7804bd7fb23265be8a4e72edcf0f542d7f4
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-17 20:25:13 +0200

    created htmlapi package

---
+++ Source/ch/nolix/coreapi/documentapi/htmlapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.documentapi.htmlapi} package declares a HTML element.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-17
+ */
+package ch.nolix.coreapi.documentapi.htmlapi;
