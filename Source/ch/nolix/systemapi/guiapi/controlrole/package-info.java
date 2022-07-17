570eed425ca9839e9998d134ffd7b78576bbcfb2
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-17 14:34:46 +0200

    created controlrole package

---
+++ Source/ch/nolix/systemapi/guiapi/controlrole/package-info.java
@@ -0,0 +1,8 @@
+//package declaration
+/**
+ * The {@link ch.nolix.systemapi.guiapi.controlrole} package declares roles for GUI controls.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-17
+ */
+package ch.nolix.systemapi.guiapi.controlrole;
