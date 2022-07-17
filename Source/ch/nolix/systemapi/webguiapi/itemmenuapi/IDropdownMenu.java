711a3fee12b1c347a3ec649f740503ecbcf79b42
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-17 16:29:51 +0200

    created IDropdownMenu

---
+++ Source/ch/nolix/systemapi/webguiapi/itemmenuapi/IDropdownMenu.java
@@ -0,0 +1,18 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.itemmenuapi;
+
+//interface
+public interface IDropdownMenu<IMI extends IItemMenuItem> extends IItemMenu<IDropdownMenu<IMI>, IMI, IDropdownMenuLook> {
+	
+	//method declaration
+	void collapsMenu();
+	
+	//method declaration
+	void expandMenu();
+	
+	//method declaration
+	boolean hasCollapsedMenu();
+	
+	//method declaration
+	boolean hasExpandedMenu();
+}
---
+++ Source/ch/nolix/systemapi/webguiapi/itemmenuapi/IDropdownMenuLook.java
@@ -0,0 +1,5 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.itemmenuapi;
+
+//interface
+public interface IDropdownMenuLook extends IItemMenuLook<IDropdownMenuLook> {}
