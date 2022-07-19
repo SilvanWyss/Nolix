0993b9414c66e6430085621626d78002f19621e0
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-19 22:46:35 +0200

    created containerapi package

---
+++ Source/ch/nolix/systemapi/webguiapi/containerapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.webguiapi.containerapi} package declares
+ * container {@link ch.nolix.systemapi.webguiapi.mainapi.IControl}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-19
+ */
+package ch.nolix.systemapi.webguiapi.containerapi;
