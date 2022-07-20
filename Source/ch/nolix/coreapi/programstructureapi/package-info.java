9509fe44d00049fad1cfa2e6dc2c425e4d730a61
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-20 19:29:27 +0200

    created programstructureapi package

---
+++ Source/ch/nolix/coreapi/programstructureapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.programstructureapi} package declares types for program structure.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-20
+ */
+package ch.nolix.coreapi.programstructureapi;
