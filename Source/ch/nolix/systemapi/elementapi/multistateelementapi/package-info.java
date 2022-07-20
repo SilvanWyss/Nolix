4d4d4c26e5dc45f4e6ee5e9758c84fd34e7b878f
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-20 21:16:05 +0200

    created multistateelementapi package

---
+++ Source/ch/nolix/systemapi/elementapi/multistateelementapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.elementapi.multistateelementapi} package declares a multi-state element.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-20
+ */
+package ch.nolix.systemapi.elementapi.multistateelementapi;
