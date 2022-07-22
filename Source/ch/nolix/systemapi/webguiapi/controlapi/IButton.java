e8c3b6794ba9399615dccda0e2f913c052d45533
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-22 22:41:39 +0200

    created IButton

---
+++ Source/ch/nolix/systemapi/webguiapi/controlapi/IButton.java
@@ -0,0 +1,35 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.controlapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IAction;
+import ch.nolix.systemapi.guiapi.controlrole.ButtonRole;
+import ch.nolix.systemapi.webguiapi.mainapi.IControl;
+
+//interface
+public interface IButton extends IControl<IButton, IButtonLook> {
+	
+	//method declaration
+	ButtonRole getRole();
+	
+	//method declaration
+	String getText();
+	
+	//method declaration
+	boolean hasRole();
+	
+	//method declaration
+	void removeRole();
+	
+	//method declaration
+	IButton setLeftMouseButtonPressAction(IAction leftMouseButtonPressAction);
+	
+	//method declaration
+	IButton setLeftMouseButtonRelaseAction(IAction leftMouseButtonReleaseAction);
+	
+	//method declaration
+	IButton setRole(ButtonRole buttonRole);
+	
+	//method declaration
+	IButton setText(String text);
+}
---
+++ Source/ch/nolix/systemapi/webguiapi/controlapi/IButtonLook.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.controlapi;
+
+//own imports
+import ch.nolix.systemapi.guiapi.controllookapi.IControlLook;
+
+//interface
+public interface IButtonLook extends IControlLook<IButtonLook> {}
