21ecbf9d94c8ebcabe763b6bf931984a8c19ffb6
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-22 21:18:34 +0200

    created HTMLElementTransformable

---
+++ Source/ch/nolix/coreapi/documentapi/htmlapi/HTMLElementTransformable.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.coreapi.documentapi.htmlapi;
+
+//interface
+public interface HTMLElementTransformable {
+	
+	//method declaration
+	IHTMLElement<?, ?> toHTMLElement();
+	
+	//method declaration
+	String toHTMLString();
+}
