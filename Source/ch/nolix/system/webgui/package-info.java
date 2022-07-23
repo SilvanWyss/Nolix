4f1a9c79d346019bd7b095c6c12244f44ad1bbc3
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-23 20:57:33 +0200

    created webgui package

---
+++ Source/ch/nolix/system/webgui/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.webgui} package defines
+ * the types that are declared in the {@link ch.nolix.systemapi.webguiapi}.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-23
+ */
+package ch.nolix.system.webgui;
