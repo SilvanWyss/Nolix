da92a3de9d4f0e9a98e9336580eb824b2fa76a8c
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-23 18:12:06 +0200

    created KeyValuePair

---
+++ Source/ch/nolix/core/document/keyvalue/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.document.keyvalue} package defines
+ * the types that are declared in the {ch.nolix.coreapi.documentapi.keyvalueapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-23
+ */
+package ch.nolix.core.document.keyvalue;
