dfcfa8f93b9c965acbad4d6ca41a94a0b1017c31
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-23 17:55:34 +0200

    created html package

---
+++ Source/ch/nolix/core/document/html/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.document.html} package defines
+ * the types that are declared in the {ch.nolix.coreapi.documentapi.htmlapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-23
+ */
+package ch.nolix.core.document.html;
