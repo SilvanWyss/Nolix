c80fd3464ad763d984ac24a7c2ae39e1e55b57f3
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-23 17:22:25 +0200

    edited ITabContainerTab

--- Source/ch/nolix/systemapi/webguiapi/containerapi/ITabContainerTab.java
+++ Source/ch/nolix/systemapi/webguiapi/containerapi/ITabContainerTab.java
@@ -4,17 +4,12 @@ package ch.nolix.systemapi.webguiapi.containerapi;
 //own imports
 import ch.nolix.coreapi.attributeapi.mandatoryattributeuniversalapi.Headered;
 import ch.nolix.coreapi.attributeapi.mutablemandatoryattributeuniversalapi.Headerable;
+import ch.nolix.systemapi.guiapi.processapi.Selectable;
 import ch.nolix.systemapi.webguiapi.mainapi.IRootControlOwner;
 
 //interface
-public interface ITabContainerTab extends Headerable<Headered>, IRootControlOwner<ITabContainerTab> {
+public interface ITabContainerTab extends Headerable<Headered>, IRootControlOwner<ITabContainerTab>, Selectable {
 	
 	//method declaration
 	boolean belongsToTabContainer();
-	
-	//method declaration
-	boolean isSelected();
-	
-	//method declaration
-	void select();
 }
