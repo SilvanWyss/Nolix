a6f028a25a218c7e52e71f23ddd890bfb1a39605
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-25 21:04:16 +0200

    extended IBackgroundControlLook

--- Source/ch/nolix/systemapi/webguiapi/controllookapi/IBackgroundControlLook.java
+++ Source/ch/nolix/systemapi/webguiapi/controllookapi/IBackgroundControlLook.java
@@ -3,6 +3,10 @@ package ch.nolix.systemapi.webguiapi.controllookapi;
 
 //own imports
 import ch.nolix.systemapi.guiapi.canvasuniversalapi.IBackground;
+import ch.nolix.systemapi.guiapi.colorapi.IColor;
+import ch.nolix.systemapi.guiapi.colorapi.IColorGradient;
+import ch.nolix.systemapi.guiapi.imageapi.IImage;
+import ch.nolix.systemapi.guiapi.imageapi.ImageApplication;
 import ch.nolix.systemapi.guiapi.widgetguiapi.ControlState;
 
 //interface
@@ -10,6 +14,15 @@ public interface IBackgroundControlLook<BCL extends IBackgroundControlLook<BCL>>
 	
 	//method declaration
 	void removeCustomBackgrounds();
+	
+	//method declaration
+	BCL setBackgroundColorForState(ControlState state, IColor backgroundColor);
+	
+	//method declaration
+	BCL setBackgroundColorGradientForState(ControlState state, IColorGradient backgroundColorGradient);
+	
+	//method declaration
+	BCL setBackgroundImageForState(ControlState state, IImage backgroundImage, ImageApplication imageApplication);
 	
 	//method declaration
 	BCL setBackgroundForState(ControlState state, IBackground background);
