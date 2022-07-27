ddf9dc390bfc99cd945accdc3bc540e1cb75f5c6
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-27 23:14:36 +0200

    created HTMLElementTypeCatalogue

---
+++ Source/ch/nolix/core/document/html/HTMLElementTypeCatalogue.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.core.document.html;
+
+//class
+public final class HTMLElementTypeCatalogue {
+	
+	//constant
+	public static final String DIV = "div";
+	
+	//constructor
+	private HTMLElementTypeCatalogue() {}
+}
