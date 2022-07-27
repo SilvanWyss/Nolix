017dcdf2a7a02fa3707ea71ce6ada4afd3510234
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-27 23:05:26 +0200

    created HTMLAttributeNameCatalogue

---
+++ Source/ch/nolix/core/document/html/HTMLAttributeNameCatalogue.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.core.document.html;
+
+//class
+public final class HTMLAttributeNameCatalogue {
+	
+	//constant
+	public static final String Z_INDEX = "z-index";
+	
+	//constructor
+	private HTMLAttributeNameCatalogue() {}
+}
