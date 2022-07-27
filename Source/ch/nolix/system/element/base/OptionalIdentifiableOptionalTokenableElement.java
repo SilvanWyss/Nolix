3f79940bd19b3094ca70330a2499bc48773541c5
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-27 20:42:16 +0200

    created OptionalIdentifiableOptionalTokenableElement

---
+++ Source/ch/nolix/system/element/base/OptionalIdentifiableOptionalTokenableElement.java
@@ -0,0 +1,121 @@
+//package declaration
+package ch.nolix.system.element.base;
+
+//own imports
+import ch.nolix.core.commontype.commontypehelper.GlobalStringHelper;
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.core.programatom.name.LowerCaseCatalogue;
+import ch.nolix.core.programatom.name.PascalCaseCatalogue;
+import ch.nolix.coreapi.attributeapi.mutableoptionalattributeuniversalapi.OptionalIdentifiableByString;
+import ch.nolix.coreapi.attributeapi.mutableoptionalattributeuniversalapi.OptionalTokenable;
+import ch.nolix.system.element.mutableelement.MutableElement;
+import ch.nolix.system.element.mutableelement.MutableOptionalValue;
+
+//class
+public abstract class OptionalIdentifiableOptionalTokenableElement<
+	E extends OptionalIdentifiableOptionalTokenableElement<E>
+>
+extends MutableElement<E>
+implements OptionalIdentifiableByString<E>, OptionalTokenable<E> {
+	
+	//constant
+	private static final String ID_HEADER = PascalCaseCatalogue.ID;
+	
+	//constant
+	private static final String TOKEN_HEADER = PascalCaseCatalogue.TOKEN;
+	
+	//attribute
+	private final MutableOptionalValue<String> id = MutableOptionalValue.forString(ID_HEADER, this::setId);
+	
+	//attribute
+	private final MutableOptionalValue<String> token = MutableOptionalValue.forString(TOKEN_HEADER, this::setToken);
+	
+	//method
+	@Override
+	public final String getId() {
+		return id.getValue();
+	}
+	
+	//method
+	@Override
+	public final String getIdInQuotes() {
+		return GlobalStringHelper.getInQuotes(getId());
+	}
+	
+	//method
+	@Override
+	public final String getToken() {
+		return token.getValue();
+	}
+	
+	//method
+	@Override
+	public final boolean hasId() {
+		return id.hasValue();
+	}
+	
+	//method
+	@Override
+	public final boolean hasId(final String id) {
+		return (hasId() && getId().equals(id));
+	}
+	
+	//method
+	@Override
+	public final boolean hasToken() {
+		return token.hasValue();
+	}
+	
+	//method
+	@Override
+	public final boolean hasToken(final String token) {
+		return (hasToken() && getToken().equals(token));
+	}
+	
+	//method
+	@Override
+	public final void removeId() {
+		id.clear();
+	}
+	
+	//method
+	@Override
+	public final void removeToken() {
+		token.clear();
+	}
+	
+	//method
+	@Override
+	public final void reset() {
+		
+		removeId();
+		removeToken();
+		
+		resetElement();
+	}
+	
+	//method
+	@Override
+	public final E setId(final String id) {
+		
+		GlobalValidator.assertThat(id).thatIsNamed(LowerCaseCatalogue.ID).isNotBlank();
+		
+		this.id.setValue(id);
+		
+		return asConcrete();
+	}
+	
+	//method
+	@Override
+	public final E setToken(final String token) {
+		
+		GlobalValidator.assertThat(token).thatIsNamed(LowerCaseCatalogue.TOKEN).isNotBlank();
+		
+		this.token.setValue(token);
+		
+		return asConcrete();
+	}
+	
+	//method declaration
+	protected abstract void resetElement();
+}
