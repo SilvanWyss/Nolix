3846f11d6c256d08905a549512f88f315cad0eda
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-27 20:24:19 +0200

    created base package

---
+++ Source/ch/nolix/system/element/base/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.system.element.base} package defines some base {@link ch.nolix.system.element.main.Element}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-27
+ */
+package ch.nolix.system.element.base;
