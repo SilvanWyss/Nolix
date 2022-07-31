e9b76638ccc23c67c69ed52ecdffcf913745d4d0
Silvan Wyss <silvan.wyss@gmail.com>
2022-07-31 16:36:14 +0200

    created container package

---
+++ Source/ch/nolix/system/webgui/container/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.webgui.container} package defines
+ * the types that are declared in the {@link ch.nolix.systemapi.webguiapi.containerapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-07-31
+ */
+package ch.nolix.system.webgui.container;
