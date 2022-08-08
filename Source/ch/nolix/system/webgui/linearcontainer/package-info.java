909f9b2bcb28709a542aeea7c463d42b361d6324
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-08 22:12:30 +0200

    created linearcontainer package

---
+++ Source/ch/nolix/system/webgui/linearcontainer/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.webgui.linearcontainer} package defines
+ * the types that are declared in the {@link ch.nolix.systemapi.webguiapi.linearcontainerapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-08-08
+ */
+package ch.nolix.system.webgui.linearcontainer;
