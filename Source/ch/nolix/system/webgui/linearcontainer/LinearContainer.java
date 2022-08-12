8319a3d88ba757333be473cfce9a4575219c9767
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-12 22:34:23 +0200

    created LinearContainer

---
+++ Source/ch/nolix/system/webgui/linearcontainer/LinearContainer.java
@@ -0,0 +1,80 @@
+//package declaration
+package ch.nolix.system.webgui.linearcontainer;
+
+//own imports
+import ch.nolix.coreapi.containerapi.mainapi.IContainer;
+import ch.nolix.system.element.mutableelement.MultiValue;
+import ch.nolix.system.webgui.container.Container;
+import ch.nolix.system.webgui.main.GlobalControlFactory;
+import ch.nolix.systemapi.elementapi.styleapi.IStylableElement;
+import ch.nolix.systemapi.webguiapi.linearcontainerapi.ILinearContainer;
+import ch.nolix.systemapi.webguiapi.mainapi.IControl;
+
+//class
+public abstract class LinearContainer
+extends Container<LinearContainer, LinearContainerStyle>
+implements ILinearContainer<LinearContainer, LinearContainerStyle> {
+	
+	//constant
+	private static final String CHILD_CONTROL_HEADER = "ChildControl";
+	
+	//attribute
+	private final MultiValue<IControl<?, ?>> childControls =
+	new MultiValue<>(
+		CHILD_CONTROL_HEADER,
+		this::addControl,
+		GlobalControlFactory::createControlFromSpecification,
+		IControl::getSpecification
+	);
+	
+	//method
+	@Override
+	public final LinearContainer addControl(final IControl<?, ?>... controls) {
+		
+		for (final var c : controls) {
+			c.technicalSetParentControl(this);
+			this.childControls.add(c);
+		}
+		
+		return this;
+	}
+	
+	//method
+	@Override
+	public final LinearContainer addControls(final IContainer<? extends IControl<?, ?>> controls) {
+		
+		controls.forEach(this::addControl);
+		
+		return this;
+	}
+	
+	//method
+	@Override
+	public final void clear() {
+		childControls.clear();
+	}
+	
+	//method
+	@Override
+	public final IContainer<? extends IStylableElement<?>> getRefChildStylableElements() {
+		return childControls.getRefValues();
+	}
+	
+	//method
+	@Override
+	public final boolean isEmpty() {
+		return childControls.isEmpty();
+	}
+	
+	//method
+	@Override
+	public final void removeControl(final IControl<?, ?> control) {
+		childControls.remove(control);
+	}
+	
+	//method
+	@Override
+	protected final LinearContainerStyle createStyle() {
+		return new LinearContainerStyle();
+	}
+}
---
+++ Source/ch/nolix/system/webgui/linearcontainer/LinearContainerStyle.java
@@ -0,0 +1,57 @@
+//package declaration
+package ch.nolix.system.webgui.linearcontainer;
+
+//own imports
+import ch.nolix.core.document.node.Node;
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.coreapi.documentapi.nodeapi.INode;
+import ch.nolix.system.element.multistateelement.NonCascadingProperty;
+import ch.nolix.system.webgui.controlstyle.ExtendedControlStyle;
+import ch.nolix.systemapi.guiapi.widgetguiapi.ControlState;
+import ch.nolix.systemapi.webguiapi.linearcontainerapi.ILinearContainerStyle;
+
+//class
+public final class LinearContainerStyle
+extends ExtendedControlStyle<LinearContainerStyle>
+implements ILinearContainerStyle<LinearContainerStyle> {
+	
+	//constant
+	public static final int DEFAULT_CHILD_CONTROL_MARGIN = 0;
+	
+	//constant
+	private static final String CHILD_CONTROL_MARGIN_HEADER = "ChildControlMargin";
+	
+	//attribute
+	private final NonCascadingProperty<ControlState, Integer> childControlMargin =
+	new NonCascadingProperty<>(
+		CHILD_CONTROL_MARGIN_HEADER,
+		ControlState.class,
+		INode::getSingleChildNodeAsInt,
+		Node::withChildNode,
+		this::setChildControlMarginForState,
+		DEFAULT_CHILD_CONTROL_MARGIN
+	);
+	
+	//method
+	@Override
+	public int getChildControlMargin() {
+		return childControlMargin.getValue();
+	}
+	
+	//method
+	@Override
+	public void removeCustomChildControlMargins() {
+		childControlMargin.setUndefined();
+	}
+	
+	//method
+	@Override
+	public LinearContainerStyle setChildControlMarginForState(final ControlState state, final int childControlMargin) {
+		
+		GlobalValidator.assertThat(childControlMargin).thatIsNamed("child contorl margin").isNotNegative();
+		
+		this.childControlMargin.setValueForState(state, childControlMargin);
+		
+		return this;
+	}
+}
