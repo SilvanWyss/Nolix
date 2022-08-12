e7f3b6f7b7149b17d519827f478bfceb73f5c544
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-12 23:06:33 +0200

    created webapplication package

---
+++ Source/ch/nolix/system/application/webapplication/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.application.webapplication} package defines
+ * a {@link ch.nolix.system.application.main.Application} for web.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-08-12
+ */
+package ch.nolix.system.application.webapplication;
