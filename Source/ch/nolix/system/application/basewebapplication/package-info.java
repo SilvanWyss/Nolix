8adfba2f43a39f9a1a98af46495ecf53e775e804
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-14 14:33:24 +0200

    created basewebapplication package

---
+++ Source/ch/nolix/system/application/basewebapplication/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.application.basewebapplication} package defines
+ * a base {@link ch.nolix.system.application.main.Application} for web.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-08-14
+ */
+package ch.nolix.system.application.basewebapplication;
