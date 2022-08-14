c403817a6b7eba6a4a5a21f9052f9bfb8619461e
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-14 20:09:51 +0200

    created basewebapplicationprotocol package

---
+++ Source/ch/nolix/system/application/basewebapplicationprotocol/CommandProtocol.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.system.application.basewebapplicationprotocol;
+
+//class
+public final class CommandProtocol {
+	
+	//constant
+	public static final String OPEN_NEW_TAB = "OpenNewTab";
+	
+	//constant
+	public static final String REDIRECT = "Redirect";
+	
+	//constant
+	public static final String SAVE_FILE = "SaveFile";
+	
+	//constant
+	public static final String SEND_OPTIONAL_FILE = "SendOptionalFile";
+	
+	//constructor
+	private CommandProtocol() {}
+}
---
+++ Source/ch/nolix/system/application/basewebapplicationprotocol/ObjectProtocol.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.system.application.basewebapplicationprotocol;
+
+//class
+public final class ObjectProtocol {
+	
+	//constant
+	public static final String URL = "URL";
+	
+	//constructor
+	private ObjectProtocol() {}
+}
