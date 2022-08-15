0dc573627e78dd16ec2b57bf44241de68123662e
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-15 19:24:02 +0200

    created webapplicationprotocol package

---
+++ Source/ch/nolix/system/application/webapplicationprotocol/CommandProtocol.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.system.application.webapplicationprotocol;
+
+//class
+public final class CommandProtocol {
+	
+	//constant
+	public static final String SET_TITLE = "SetTitle";
+	
+	//constructor
+	private CommandProtocol() {}
+}
---
+++ Source/ch/nolix/system/application/webapplicationprotocol/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.application.webapplicationprotocol} package defines
+ * protocols for {@link ch.nolix.system.application.main.Application} for web.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-08-15
+ */
+package ch.nolix.system.application.webapplicationprotocol;
