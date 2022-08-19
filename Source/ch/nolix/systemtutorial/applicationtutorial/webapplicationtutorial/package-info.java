65e065961b143a24dfa9ebfaed5aba58969b7225
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-19 18:48:08 +0200

    created webapplicationtutorial package

---
+++ Source/ch/nolix/systemtutorial/applicationtutorial/webapplicationtutorial/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtutorial.applicationtutorial.webapplicationtutorial} package defines
+ * tutorials for the {@link ch.nolix.system.application.webapplication} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-08-19
+ */
+package ch.nolix.systemtutorial.applicationtutorial.webapplicationtutorial;
