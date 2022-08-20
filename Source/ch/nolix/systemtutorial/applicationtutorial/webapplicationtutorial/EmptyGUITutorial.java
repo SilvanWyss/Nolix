911aae016c3e4814aa54f27cdb7b7d5f7bfc73ed
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-20 18:05:20 +0200

    created EmptyGUITutorial

---
+++ Source/ch/nolix/systemtutorial/applicationtutorial/webapplicationtutorial/EmptyGUITutorial.java
@@ -0,0 +1,37 @@
+package ch.nolix.systemtutorial.applicationtutorial.webapplicationtutorial;
+
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+
+public final class EmptyGUITutorial {
+	
+	public static void main(String[] args) {
+		
+		//Creates a Server.
+		final var server = Server.forDefaultPort(true);
+		
+		//Adds a default Application to the Server.
+		server.addDefaultApplication("Empty GUI tutorial", MainSession.class, VoidApplicationContext.INSTANCE);
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		GlobalSequencer.waitForSeconds(2);
+		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	private static final class MainSession extends BackendWebClientSession<VoidApplicationContext> {
+		
+		@Override
+		protected void initialize() {
+			//Does nothing.
+		}
+	}
+	
+	private EmptyGUITutorial() {}
+}
+
