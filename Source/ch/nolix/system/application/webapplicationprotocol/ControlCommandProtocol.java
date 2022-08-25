3689d272d8e50b534b726a587985fab6361493d9
Silvan Wyss <silvan.wyss@gmail.com>
2022-08-25 22:50:29 +0200

    created ControlCommandProtocol

---
+++ Source/ch/nolix/system/application/webapplicationprotocol/ControlCommandProtocol.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.system.application.webapplicationprotocol;
+
+//class
+public final class ControlCommandProtocol {
+	
+	//constant
+	public static final String NOTE_KEY_TYPING = "NoteKeyTyping";
+	
+	//constant
+	public static final String NOTE_LEFT_MOUSE_BUTTON_PRESS = "NoteLeftMouseButtonPress";
+	
+	//constant
+	public static final String NOTE_LEFT_MOUSE_BUTTON_RELEASE = "NoteLeftMouseButtonRelease";
+	
+	//constant
+	public static final String NOTE_MOUSE_WHEEL_PRESS = "NoteMouseWheelPress";
+	
+	//constant
+	public static final String NOTE_MOUSE_WHEEL_RELEASE = "NoteMouseWheelRelease";
+	
+	//constant
+	public static final String NOTE_RIGHT_MOUSE_BUTTON_PRESS = "NoteRightMouseButtonPress";
+	
+	//constant
+	public static final String NOTE_RIGHT_MOUSE_BUTTON_RELEASE = "NoteRightMouseButtonRelease";
+	
+	//constructor
+	private ControlCommandProtocol() {}
+}
