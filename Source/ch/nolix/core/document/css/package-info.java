cc9fe406f45dcbddc9dd1798943d1eaebe8f6b05
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-16 20:56:30 +0200

    created css package

---
+++ Source/ch/nolix/core/document/css/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.document.css} package defines
+ * the types that are declared in the {ch.nolix.coreapi.documentapi.cssapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-09-16
+ */
+package ch.nolix.core.document.css;
