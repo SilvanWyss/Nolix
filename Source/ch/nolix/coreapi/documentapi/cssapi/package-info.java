3f05a2f5fedd17f7dae0acd2f1c42702b6184cca
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-16 17:18:10 +0200

    created cssapi package

---
+++ Source/ch/nolix/coreapi/documentapi/cssapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.documentapi.cssapi} package declares a CSS.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-09-16
+ */
+package ch.nolix.coreapi.documentapi.cssapi;
