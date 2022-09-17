7df048bae615b08e6c4e0f7bc87e52a2de91e7d9
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-17 18:15:27 +0200

    created CSSPropertyNameCatalogue

---
+++ Source/ch/nolix/core/document/css/CSSPropertyNameCatalogue.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.core.document.css;
+
+//class
+public final class CSSPropertyNameCatalogue {
+	
+	//constant
+	public static final String CURSOR = "cursor";
+	
+	//constructor
+	private CSSPropertyNameCatalogue() {}
+}
