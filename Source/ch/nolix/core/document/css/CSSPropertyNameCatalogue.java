54ff6d254878ad04c8ece0ab6ece35ea80cffb1f
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-19 17:12:24 +0200

    extended CSSPropertyNameCatalogue

--- Source/ch/nolix/core/document/css/CSSPropertyNameCatalogue.java
+++ Source/ch/nolix/core/document/css/CSSPropertyNameCatalogue.java
@@ -5,7 +5,13 @@ package ch.nolix.core.document.css;
 public final class CSSPropertyNameCatalogue {
 	
 	//constant
+	public static final String COLOR = "color";
+	
+	//constant
 	public static final String CURSOR = "cursor";
+	
+	//constant
+	public static final String FONT_SIZE = "font-size";
 	
 	//constructor
 	private CSSPropertyNameCatalogue() {}
