088406ff839efa602cb18ab7a0cf248b1be3c6dd
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-19 19:27:19 +0200

    extended CSSPropertyNameCatalogue

--- Source/ch/nolix/coreapi/webapi/cssapi/CSSPropertyNameCatalogue.java
+++ Source/ch/nolix/coreapi/webapi/cssapi/CSSPropertyNameCatalogue.java
@@ -5,6 +5,30 @@ package ch.nolix.coreapi.webapi.cssapi;
 public final class CSSPropertyNameCatalogue {
 	
 	//constant
+	public static final String BORDER_BOTTOM_COLOR = "border-bottom-color";
+	
+	//constant
+	public static final String BORDER_BOTTOM_WIDTH = "border-bottom-width";
+	
+	//constant
+	public static final String BORDER_LEFT_COLOR = "border-left-color";
+	
+	//constant
+	public static final String BORDER_LEFT_WIDTH = "border-left-width";
+	
+	//constant
+	public static final String BORDER_RIGHT_COLOR = "border-right-color";
+	
+	//constant
+	public static final String BORDER_RIGHT_WIDTH = "border-right-width";
+	
+	//constant
+	public static final String BORDER_TOP_COLOR = "border-top-color";
+	
+	//constant
+	public static final String BORDER_TOP_WIDTH = "border-top-width";
+	
+	//constant
 	public static final String COLOR = "color";
 	
 	//constant
@@ -12,6 +36,18 @@ public final class CSSPropertyNameCatalogue {
 	
 	//constant
 	public static final String FONT_SIZE = "font-size";
+	
+	//constant
+	public static final String PADDING_BOTTOM = "padding-bottom";
+	
+	//constant
+	public static final String PADDING_LEFT = "padding-left";
+	
+	//constant
+	public static final String PADDING_RIGHT = "padding-right";
+	
+	//constant
+	public static final String PADDING_TOP = "padding-top";
 	
 	//constructor
 	private CSSPropertyNameCatalogue() {}
