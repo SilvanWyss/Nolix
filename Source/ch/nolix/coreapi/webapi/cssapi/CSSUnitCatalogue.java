92d4e70809278ff184500fbcf7fe6a5c7cd0c4c0
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-23 12:26:37 +0200

    created CSSUnitCatalogue

---
+++ Source/ch/nolix/coreapi/webapi/cssapi/CSSUnitCatalogue.java
@@ -0,0 +1,18 @@
+//package declaration
+package ch.nolix.coreapi.webapi.cssapi;
+
+//class
+public final class CSSUnitCatalogue {
+	
+	//constant
+	public static final String PX = "px";
+	
+	//constant
+	public static final String VH = "vh";
+	
+	//constant
+	public static final String VW = "vw";
+	
+	//constructor
+	private CSSUnitCatalogue() {}
+}
