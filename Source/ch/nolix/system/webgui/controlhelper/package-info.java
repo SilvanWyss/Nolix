8479c562c564655aa3ab1f8d76060804a48ee0e1
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-25 12:21:06 +0200

    created controlhelper package

---
+++ Source/ch/nolix/system/webgui/controlhelper/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.webgui.controlhelper} package defines
+ * helpers for {@link ch.nolix.systemapi.webguiapi.mainapi.IControl}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-09-25
+ */
+package ch.nolix.system.webgui.controlhelper;
