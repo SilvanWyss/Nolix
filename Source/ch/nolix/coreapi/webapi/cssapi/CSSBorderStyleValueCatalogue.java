9eb0665bda48379ac6c7d58d54263fe31823ca2e
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-27 21:08:17 +0200

    created CSSBorderStyleValueCatalogue

---
+++ Source/ch/nolix/coreapi/webapi/cssapi/CSSBorderStyleValueCatalogue.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.coreapi.webapi.cssapi;
+
+//class
+public final class CSSBorderStyleValueCatalogue {
+	
+	//constant
+	public static final String SOLID = "solid";
+	
+	//constructor
+	private CSSBorderStyleValueCatalogue() {}
+}
