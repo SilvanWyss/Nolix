db097c164d69fdef6ad10b815135f8ff10cea6e2
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-27 16:47:50 +0200

    created mediamanagerapi package

---
+++ Source/ch/nolix/systemapi/webguiapi/mediamanagerapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.webguiapi.mediamanagerapi} package manages for media.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-09-27
+ */
+package ch.nolix.systemapi.webguiapi.mediamanagerapi;
