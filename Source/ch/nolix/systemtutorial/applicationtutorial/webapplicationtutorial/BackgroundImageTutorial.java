85af263baf76b16ee5a364cbcf5f288f88ed89fe
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-29 12:14:26 +0200

    created BackgroundImageTutorial

---
+++ Source/ch/nolix/systemtutorial/applicationtutorial/webapplicationtutorial/BackgroundImageTutorial.java
@@ -0,0 +1,44 @@
+package ch.nolix.systemtutorial.applicationtutorial.webapplicationtutorial;
+
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+import ch.nolix.system.gui.image.Image;
+import ch.nolix.systemapi.guiapi.imageapi.ImageApplication;
+
+public final class BackgroundImageTutorial {
+	
+	public static void main(String[] args) {
+		
+		//Creates a Server.
+		final var server = Server.forDefaultPort(true);
+		
+		//Adds a default Application to the Server.
+		server.addDefaultApplication("Background image tutorial", MainSession.class, VoidApplicationContext.INSTANCE);
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		GlobalSequencer.waitForSeconds(2);
+		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	private static final class MainSession extends BackendWebClientSession<VoidApplicationContext> {
+		
+		@Override
+		protected void initialize() {
+			
+			//Loads an Image.
+			final var image =
+			Image.fromResource("ch/nolix/systemTutorial/guiTutorial/maintutorial/resource/Pilatus.jpg");
+			
+			//Sets the Image as background image to the GUI of the current MainSession.
+			getRefGUI().setBackgroundImage(image, ImageApplication.SCALE_TO_FRAME);
+		}
+	}
+	
+	private BackgroundImageTutorial() {}
+}
