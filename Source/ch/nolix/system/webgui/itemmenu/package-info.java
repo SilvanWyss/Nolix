6c2cf7e9d90a845b89b146ade8e3d578774a5552
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-29 16:35:15 +0200

    created itemmenu package

---
+++ Source/ch/nolix/system/webgui/itemmenu/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.webgui.itemmenu} package defines
+ * the types that are declared in the {@link ch.nolix.systemapi.webguiapi.itemmenuapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-09-29
+ */
+package ch.nolix.system.webgui.itemmenu;
