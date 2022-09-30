810f41219fdcc1bbd4c41662e01af5ad1c881346
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-30 12:13:22 +0200

    created DropdownMenuStyle

---
+++ Source/ch/nolix/system/webgui/itemmenu/DropdownMenuStyle.java
@@ -0,0 +1,10 @@
+//package declaration
+package ch.nolix.system.webgui.itemmenu;
+
+//own imports
+import ch.nolix.systemapi.webguiapi.itemmenuapi.IDropdownMenuStyle;
+
+//class
+public final class DropdownMenuStyle
+extends ItemMenuStyle<DropdownMenuStyle>
+implements IDropdownMenuStyle<DropdownMenuStyle> {}
