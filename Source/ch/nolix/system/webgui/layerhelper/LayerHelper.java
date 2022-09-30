687b46ad2922661136e6a170da41d08620483584
Silvan Wyss <silvan.wyss@gmail.com>
2022-09-30 18:10:34 +0200

    created LayerHelper

---
+++ Source/ch/nolix/system/webgui/layerhelper/LayerHelper.java
@@ -0,0 +1,22 @@
+//package declaration
+package ch.nolix.system.webgui.layerhelper;
+
+//own imports
+import ch.nolix.core.web.html.HTMLAttribute;
+import ch.nolix.core.web.html.HTMLAttributeNameCatalogue;
+import ch.nolix.systemapi.webguiapi.mainapi.ILayer;
+
+//class
+public final class LayerHelper {
+	
+	//static attribute
+	public static final LayerHelper INSTANCE = new LayerHelper();
+	
+	//constructor
+	private LayerHelper() {}
+	
+	//method
+	public HTMLAttribute createIdHTMLAttributeForLayer(final ILayer<?> layer) {
+		return HTMLAttribute.withNameAndValue(HTMLAttributeNameCatalogue.ID, layer.getFixedId());
+	}
+}
