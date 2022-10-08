706ec4d07200792727c17bb7143716da8804c7ab
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-08 10:48:12 +0200

    created IUserInputCell

---
+++ Source/ch/nolix/systemapi/webguiapi/mainapi/IUserInputCell.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.mainapi;
+
+//interface
+public interface IUserInputCell<UIC extends IUserInputCell<UIC>> {
+	
+	//method declaration
+	String getUserInput();
+	
+	//method declaration
+	UIC setUserInput(String userInput);
+}
