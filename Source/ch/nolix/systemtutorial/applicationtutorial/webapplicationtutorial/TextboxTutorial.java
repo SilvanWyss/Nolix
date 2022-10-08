e8194eae8234458fb7a3987d9b2cd6016318c6eb
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-08 19:46:16 +0200

    created TextboxTutorial

---
+++ Source/ch/nolix/systemtutorial/applicationtutorial/webapplicationtutorial/TextboxTutorial.java
@@ -0,0 +1,48 @@
+package ch.nolix.systemtutorial.applicationtutorial.webapplicationtutorial;
+
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+import ch.nolix.system.webgui.control.Textbox;
+import ch.nolix.systemapi.guiapi.widgetguiapi.ControlState;
+
+public final class TextboxTutorial {
+	
+	public static void main(String[] args) {
+		
+		//Creates a Server.
+		final var server = Server.forDefaultPort(true);
+		
+		//Adds a default Application to the Server.
+		server.addDefaultApplication("Textbox tutorial", MainSession.class, VoidApplicationContext.INSTANCE);
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		GlobalSequencer.waitForSeconds(2);
+		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	private static final class MainSession extends BackendWebClientSession<VoidApplicationContext> {
+		
+		@Override
+		protected void initialize() {
+			
+			//Creates a Textbox.
+			final var textBox = new Textbox();
+						
+			//Configures the style of the Textbox.
+			textBox
+			.getRefStyle()
+			.setBorderThicknessForState(ControlState.BASE, 1);
+			
+			//Adds the Textbox to the GUI of the current MainSession.
+			getRefGUI().pushLayerWithRootControl(textBox);
+		}
+	}
+	
+	private TextboxTutorial() {}
+}
