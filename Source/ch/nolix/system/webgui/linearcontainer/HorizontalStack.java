ed0a8c082e22272924124739c805a7f9360263b4
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-09 14:37:23 +0200

    created HorizontalStackHTMLCreator

--- Source/ch/nolix/system/webgui/linearcontainer/HorizontalStack.java
+++ Source/ch/nolix/system/webgui/linearcontainer/HorizontalStack.java
@@ -2,12 +2,7 @@
 package ch.nolix.system.webgui.linearcontainer;
 
 //own imports
-import ch.nolix.core.container.immutablelist.ImmutableList;
-import ch.nolix.core.web.html.HTMLElement;
-import ch.nolix.core.web.html.HTMLElementTypeCatalogue;
 import ch.nolix.coreapi.webapi.htmlapi.IHTMLElement;
-import ch.nolix.system.webgui.controlhelper.ControlHelper;
-import ch.nolix.systemapi.webguiapi.mainapi.IControl;
 import ch.nolix.systemapi.webguiapi.mainapi.IControlCSSRuleCreator;
 
 //class
@@ -16,12 +11,7 @@ public final class HorizontalStack extends LinearContainer<HorizontalStack, Hori
 	//method
 	@Override
 	public IHTMLElement<?, ?> toHTMLElement() {
-		return
-		HTMLElement.withTypeAndAttributesAndChildElements(
-			HTMLElementTypeCatalogue.DIV,
-			ImmutableList.withElements(ControlHelper.INSTANCE.createIdHTMLAttributeForControl(this)),
-			getRefChildControls().to(IControl::toHTMLElement)
-		);
+		return HorizontalStackHTMLCreator.INSTANCE.createHTMLElementForHorizontalStack(this);
 	}
 	
 	//method
