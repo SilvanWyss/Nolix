cb5e4081874462e988d639877c9e251c89cf2647
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-15 20:11:32 +0200

    edited ImageControlCSSRuleCreator

--- Source/ch/nolix/system/webgui/control/ImageControlCSSRuleCreator.java
+++ Source/ch/nolix/system/webgui/control/ImageControlCSSRuleCreator.java
@@ -4,6 +4,7 @@ package ch.nolix.system.webgui.control;
 //own imports
 import ch.nolix.core.container.main.LinkedList;
 import ch.nolix.core.web.css.CSSProperty;
+import ch.nolix.coreapi.webapi.cssapi.CSSPropertyNameCatalogue;
 import ch.nolix.coreapi.webapi.cssapi.ICSSRule;
 import ch.nolix.system.webgui.controlstyle.ExtendedControlCSSRuleCreator;
 import ch.nolix.systemapi.guiapi.widgetguiapi.ControlState;
@@ -42,6 +43,6 @@ public final class ImageControlCSSRuleCreator extends ExtendedControlCSSRuleCrea
 		final ControlState state,
 		final LinkedList<CSSProperty> list
 	) {
-		//Does nothing.
+		list.addAtEnd(CSSProperty.withNameAndValue(CSSPropertyNameCatalogue.DISPLAY, "block"));
 	}
 }
