15c47d10ba9a54a5b4a7ed432e011dc86cb019fc
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-15 11:58:04 +0200

    created structureapi package

---
+++ Source/ch/nolix/systemapi/structureapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.structureapi} package declares types for structure.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-10-15
+ */
+package ch.nolix.systemapi.structureapi;
