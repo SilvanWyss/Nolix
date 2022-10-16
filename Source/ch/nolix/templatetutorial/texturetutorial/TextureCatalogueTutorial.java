5e402bf8e27e4b7d7452ddd2045850b7d7a56836
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-16 16:28:47 +0200

    refactored TextureCatalogueTutorial

--- Source/ch/nolix/templatetutorial/texturetutorial/TextureCatalogueTutorial.java
+++ Source/ch/nolix/templatetutorial/texturetutorial/TextureCatalogueTutorial.java
@@ -1,64 +1,86 @@
 package ch.nolix.templatetutorial.texturetutorial;
 
-//own imports
-import ch.nolix.system.gui.containerwidget.FloatContainer;
-import ch.nolix.system.gui.containerwidget.VerticalStack;
-import ch.nolix.system.gui.widget.ImageWidget;
-import ch.nolix.system.gui.widget.Label;
-import ch.nolix.system.gui.widgetgui.Frame;
-import ch.nolix.systemapi.guiapi.widgetguiapi.ControlState;
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+import ch.nolix.system.webgui.control.ImageControl;
+import ch.nolix.system.webgui.control.Text;
+import ch.nolix.system.webgui.linearcontainer.FloatContainer;
+import ch.nolix.system.webgui.linearcontainer.VerticalStack;
+import ch.nolix.systemapi.webguiapi.mainapi.ControlState;
 import ch.nolix.template.texture.TextureCatalogue;
 
 public final class TextureCatalogueTutorial {
 	
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
-		//Creates textures.
-		final var concreteTexture = TextureCatalogue.CONCRETE_TEXTURE.toScaledImage(10);
-		final var fireWoodTexture = TextureCatalogue.FIR_WOOD_TEXTURE.toScaledImage(10);
-		final var juteTexture = TextureCatalogue.JUTE_TEXTURE.toScaledImage(10);
-		final var whiteMarbleTexture = TextureCatalogue.WHITE_MARBLE_TEXTURE.toScaledImage(10);
+		//Creates a Server.
+		final var server = Server.forDefaultPort(true);
 		
-		//Creates a Frame that will show the textures.
-		new Frame()
-		.setTitle("TextureCatalogue Tutorial")
-		.pushLayerWithRootWidget(
-			new FloatContainer()
-			.setProposalWidth(1000)
-			.onStyle(
-				l -> l.setPaddingForState(ControlState.BASE, 20).setElementMarginForState(ControlState.BASE, 20)
-			)
-			.addWidget(
-				new VerticalStack()
-				.addWidget(
-					new Label()
-					.setText("Concrete"),
-					new ImageWidget()
-					.setImage(concreteTexture)
-				),
-				new VerticalStack()
-				.addWidget(
-					new Label()
-					.setText("Fir Wood"),
-					new ImageWidget()
-					.setImage(fireWoodTexture)
-				),
-				new VerticalStack()
-				.addWidget(
-					new Label()
-					.setText("Jute"),
-					new ImageWidget()
-					.setImage(juteTexture)
-				),
-				new VerticalStack()
-				.addWidget(
-					new Label()
-					.setText("White Marble"),
-					new ImageWidget()
-					.setImage(whiteMarbleTexture)
+		//Adds a default Application to the Server.
+		server.addDefaultApplication("Hello World GUI tutorial", MainSession.class, VoidApplicationContext.INSTANCE);
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		GlobalSequencer.waitForSeconds(2);
+		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	public static final class MainSession extends BackendWebClientSession<VoidApplicationContext> {
+		
+		@Override
+		protected void initialize() {
+		
+			//Creates textures.
+			final var concreteTexture = TextureCatalogue.CONCRETE_TEXTURE.toScaledImage(10);
+			final var fireWoodTexture = TextureCatalogue.FIR_WOOD_TEXTURE.toScaledImage(10);
+			final var juteTexture = TextureCatalogue.JUTE_TEXTURE.toScaledImage(10);
+			final var whiteMarbleTexture = TextureCatalogue.WHITE_MARBLE_TEXTURE.toScaledImage(10);
+			
+			//Adds the textures to the GUI of the current MainSession.
+			getRefGUI()
+			.setTitle("TextureCatalogue Tutorial")
+			.pushLayerWithRootControl(
+				new FloatContainer()
+				.setMaxWidth(1000)
+				.editStyle(
+					s -> s.setPaddingForState(ControlState.BASE, 20).setChildControlMarginForState(ControlState.BASE, 20)
 				)
-			)
-		);
+				.addControl(
+					new VerticalStack()
+					.addControl(
+						new Text()
+						.setText("Concrete"),
+						new ImageControl()
+						.setImage(concreteTexture)
+					),
+					new VerticalStack()
+					.addControl(
+						new Text()
+						.setText("Fir Wood"),
+						new ImageControl()
+						.setImage(fireWoodTexture)
+					),
+					new VerticalStack()
+					.addControl(
+						new Text()
+						.setText("Jute"),
+						new ImageControl()
+						.setImage(juteTexture)
+					),
+					new VerticalStack()
+					.addControl(
+						new Text()
+						.setText("White Marble"),
+						new ImageControl()
+						.setImage(whiteMarbleTexture)
+					)
+				)
+			);
+		}
 	}
 }
