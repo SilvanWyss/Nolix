fda9a92bd548414cca656ad6737f7a909cdfd1b0
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-17 21:28:10 +0200

    created WaitDialogFactory

---
+++ Source/ch/nolix/system/webgui/dialog/WaitDialogFactory.java
@@ -0,0 +1,40 @@
+//package declaration
+package ch.nolix.system.webgui.dialog;
+
+//own imports
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IAction;
+import ch.nolix.system.webgui.control.Text;
+import ch.nolix.system.webgui.linearcontainer.VerticalStack;
+import ch.nolix.system.webgui.main.Layer;
+import ch.nolix.systemapi.guiapi.containercontrolproperty.ContainerRole;
+import ch.nolix.systemapi.webguiapi.mainapi.LayerRole;
+
+//class
+public final class WaitDialogFactory {
+	
+	//static attribute
+	public static final WaitDialogFactory INSTANCE = new WaitDialogFactory();
+	
+	//constructor
+	private WaitDialogFactory() {}
+	
+	//method
+	public Layer createWaitDialogForJobAndTerminalAction(final IAction job, final IAction terminalAction) {
+		
+		final var waitDialog =
+		new Layer()
+		.setRole(LayerRole.DIALOG_LAYER)
+		.setRootControl(
+			new VerticalStack()
+			.setRole(ContainerRole.DIALOG_CONTAINER)
+			.addControl(
+				new Text().setText("Please wait...")
+			)
+		);
+		
+		GlobalSequencer.runInBackgroundAndOrder(job, waitDialog::removeSelfFromGUI, terminalAction);
+		
+		return waitDialog;
+	}	
+}
