416402a885eda2287cba1e69d3896244987f07a8
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-17 20:59:36 +0200

    created dialog package

---
+++ Source/ch/nolix/system/webgui/dialog/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.webgui.dialog} package defines
+ * dialog {@link ch.nolix.systemapi.webguiapi.mainapi.ILayer}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-10-17
+ */
+package ch.nolix.system.webgui.dialog;
