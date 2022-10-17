305169bdc1795899b71c13d0c20d344d77e20391
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-17 22:03:08 +0200

    created dialogtutorial package

---
+++ Source/ch/nolix/systemtutorial/webguitutorial/dialogtutorial/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtutorial.webguitutorial.dialogtutorial} package defines
+ * tutorials for the {@link ch.nolix.system.webgui.dialog} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-10-17
+ */
+package ch.nolix.systemtutorial.webguitutorial.dialogtutorial;
