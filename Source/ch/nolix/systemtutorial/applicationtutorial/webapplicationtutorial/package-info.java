51ef34db6948081e31819efc37c0c36635505fc1
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-17 22:02:23 +0200

    deleted webapplicationtutorial package

--- Source/ch/nolix/systemtutorial/applicationtutorial/webapplicationtutorial/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemtutorial.applicationtutorial.webapplicationtutorial} package defines
- * tutorials for the {@link ch.nolix.system.application.webapplication} package.
- * 
- * @author Silvan Wyss
- * @date 2022-08-19
- */
-package ch.nolix.systemtutorial.applicationtutorial.webapplicationtutorial;
