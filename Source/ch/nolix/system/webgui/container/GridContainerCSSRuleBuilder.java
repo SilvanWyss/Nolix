65693cb76742706775fef2adc9ca360df736a918
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-22 22:21:05 +0200

    completed ExtendedControlCSSRuleBuilder

--- Source/ch/nolix/system/webgui/container/GridContainerCSSRuleBuilder.java
+++ Source/ch/nolix/system/webgui/container/GridContainerCSSRuleBuilder.java
@@ -2,8 +2,12 @@
 package ch.nolix.system.webgui.container;
 
 //own imports
+import ch.nolix.core.container.immutablelist.ImmutableList;
 import ch.nolix.core.container.main.LinkedList;
 import ch.nolix.core.web.css.CSSProperty;
+import ch.nolix.core.web.css.CSSRule;
+import ch.nolix.core.web.html.HTMLElementTypeCatalogue;
+import ch.nolix.coreapi.webapi.cssapi.CSSUnitCatalogue;
 import ch.nolix.coreapi.webapi.cssapi.ICSSRule;
 import ch.nolix.system.webgui.controlstyle.ExtendedControlCSSRuleBuilder;
 import ch.nolix.systemapi.webguiapi.mainapi.ControlState;
@@ -34,7 +38,7 @@ extends ExtendedControlCSSRuleBuilder<GridContainer, GridContainerStyle> {
 		final ControlState state,
 		final LinkedList<CSSProperty> list
 	) {
-		//TODO: Implement.
+		//Does nothing.
 	}
 	
 	//method
@@ -44,7 +48,35 @@ extends ExtendedControlCSSRuleBuilder<GridContainer, GridContainerStyle> {
 		final ControlState state,
 		final LinkedList<? super ICSSRule<?>> list
 	) {
-		//TODO: Implement.
+		
+		final var style = control.getRefStyle();
+		final var gridThickness = style.getGridThicknessWhenHasState(state);
+		final var childControlMargin = style.getChildControlMarginWhenHasState(state);
+		
+		list.addAtEnd(
+			CSSRule.withSelectorAndProperties(
+				getCSSSelectorForControlAndAllStates(control)
+				+ " "
+				+ HTMLElementTypeCatalogue.TABLE,
+				ImmutableList.withElements(
+					CSSProperty.withNameAndValue("border-style", "solid"),
+					CSSProperty.withNameAndValue("border-thickness", gridThickness + CSSUnitCatalogue.PX)
+				)
+			)
+		);
+		
+		list.addAtEnd(
+			CSSRule.withSelectorAndProperties(
+				getCSSSelectorForControlAndAllStates(control)
+				+ " "
+				+ HTMLElementTypeCatalogue.TD,
+				ImmutableList.withElements(
+					CSSProperty.withNameAndValue("border-style", "solid"),
+					CSSProperty.withNameAndValue("border-thickness", gridThickness + CSSUnitCatalogue.PX),
+					CSSProperty.withNameAndValue("padding", childControlMargin + CSSUnitCatalogue.PX)
+				)
+			)
+		);
 	}
 	
 	//method
@@ -53,6 +85,13 @@ extends ExtendedControlCSSRuleBuilder<GridContainer, GridContainerStyle> {
 		final GridContainer control,
 		final LinkedList<? super ICSSRule<?>> list
 	) {
-		//Does nothing.
+		list.addAtEnd(
+			CSSRule.withSelectorAndProperties(
+				getCSSSelectorForControlAndAllStates(control)
+				+ " "
+				+ HTMLElementTypeCatalogue.TABLE,
+				ImmutableList.withElements(CSSProperty.withNameAndValue("border-collapse", "collapse"))
+			)
+		);
 	}
 }
