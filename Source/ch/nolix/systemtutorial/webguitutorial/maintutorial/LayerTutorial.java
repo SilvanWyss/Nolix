6f882d3dc24ce4a646b9068381475b8b7ee34df5
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-28 16:11:42 +0200

    created LayerTutorial

---
+++ Source/ch/nolix/systemtutorial/webguitutorial/maintutorial/LayerTutorial.java
@@ -0,0 +1,63 @@
+package ch.nolix.systemtutorial.webguitutorial.maintutorial;
+
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+import ch.nolix.system.gui.color.Color;
+import ch.nolix.system.webgui.control.Text;
+import ch.nolix.systemapi.webguiapi.mainapi.ControlState;
+
+public final class LayerTutorial {
+	
+	public static void main(String[] args) {
+		
+		//Creates a Server.
+		final var server = Server.forDefaultPort(true);
+		
+		//Adds a default Application to the Server.
+		server.addDefaultApplication("Layer tutorial", MainSession.class, VoidApplicationContext.INSTANCE);
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		GlobalSequencer.waitForSeconds(2);
+		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	private static final class MainSession extends BackendWebClientSession<VoidApplicationContext> {
+		
+		@Override
+		protected void initialize() {
+			
+			getRefGUI().setTitle("Layer tutorial");
+			
+			//Creates labelA.
+			final var labelA = new Text().setText("A");
+			
+			//Creates labelB.
+			final var labelB = new Text().setText("B");
+			
+			//Configures the style of labelA.
+			labelA.getRefStyle()
+			.setTextSizeForState(ControlState.BASE, 200)
+			.setTextColorForState(ControlState.BASE, Color.GREY);
+			
+			//Configures the look of labelB.
+			labelB.getRefStyle()
+			.setTextSizeForState(ControlState.BASE, 180)
+			.setTextColorForState(ControlState.BASE, Color.BLACK);
+			
+			//Adds a new layer with labelA to the Frame.
+			getRefGUI().pushLayerWithRootControl(labelA);
+			
+			//Adds a new layer with labelB to the Frame.
+			getRefGUI().pushLayerWithRootControl(labelB);
+		}
+	}
+	
+	private LayerTutorial() {}
+}
+
