334691975e4c77eb1370618040706ec474d5fa26
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-29 21:40:32 +0200

    created VisibilitySettable

---
+++ Source/ch/nolix/systemapi/guiapi/structureapi/VisibilitySettable.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.systemapi.guiapi.structureapi;
+
+//interface
+public interface VisibilitySettable extends VisibilityRequestable {
+	
+	//method declaration
+	void setInvisible();
+	
+	//method declaration
+	void setVisible();
+}
