a80a241ed5b9dcc5d296a93555f0d39bc9ebd84d
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-29 18:16:15 +0200

    created structureapi package

---
+++ Source/ch/nolix/systemapi/guiapi/structureapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.guiapi.structureapi} package declares types for the structure of GUIs.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-10-29
+ */
+package ch.nolix.systemapi.guiapi.structureapi;
