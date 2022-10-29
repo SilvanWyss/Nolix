573898cc4852276661f4fbbbab48a23bacab2038
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-29 18:17:39 +0200

    created VisibilityRequestable

---
+++ Source/ch/nolix/systemapi/guiapi/structureapi/VisibilityRequestable.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.systemapi.guiapi.structureapi;
+
+//interface
+public interface VisibilityRequestable {
+	
+	//method declaration
+	boolean isInvisible();
+	
+	//method declaration
+	boolean isVisible();
+}
