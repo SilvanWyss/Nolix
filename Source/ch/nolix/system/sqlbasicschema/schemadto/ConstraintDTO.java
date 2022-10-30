b5c769b3a482070805036999fb4b346f42ff46f2
Silvan Wyss <silvan.wyss@gmail.com>
2022-10-30 16:55:07 +0100

    made ConstraintDTO to a record

--- Source/ch/nolix/system/sqlbasicschema/schemadto/ConstraintDTO.java
+++ Source/ch/nolix/system/sqlbasicschema/schemadto/ConstraintDTO.java
@@ -2,28 +2,28 @@
 package ch.nolix.system.sqlbasicschema.schemadto;
 
 //own imports
-import ch.nolix.core.container.readcontainer.ReadContainer;
+import ch.nolix.core.container.immutablelist.ImmutableList;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.coreapi.containerapi.mainapi.IContainer;
 import ch.nolix.systemapi.sqlbasicschemaapi.schemadtoapi.ConstraintType;
 import ch.nolix.systemapi.sqlbasicschemaapi.schemadtoapi.IConstraintDTO;
 
 //class
-public final class ConstraintDTO implements IConstraintDTO {
-	
-	//attribute
-	private final ConstraintType type;
-	
-	//multi-attribute
-	private final IContainer<String> parameters;
+public record ConstraintDTO(ConstraintType type, ImmutableList<String> parameters) implements IConstraintDTO {
 	
 	//constructor
 	public ConstraintDTO(final ConstraintType type, final String... parameters) {
+		this(type, ImmutableList.withElements(parameters));
+		
+	}
+	
+	//constructor
+	public ConstraintDTO(final ConstraintType type, ImmutableList<String> parameters) { //NOSONAR
 		
 		GlobalValidator.assertThat(type).thatIsNamed(ConstraintType.class).isNotNull();
 		
 		this.type = type;
-		this.parameters = ReadContainer.forArray(parameters);
+		this.parameters = parameters;
 	}
 	
 	//method
