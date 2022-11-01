067805b7f5e585177671dae603cb4fb9d3c4ddbe
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-01 11:57:30 +0100

    edited package info

--- Source/ch/nolix/system/gui/package-info.java
+++ Source/ch/nolix/system/gui/package-info.java
@@ -1,7 +1,6 @@
 //package info
 /**
- * The {@link ch.nolix.system.gui} package defines a 2D GUI and a base Widget.
- * A widget is a control element of a GUI.
+ * The {@link ch.nolix.system.gui} package defines types for GUIs.
  * 
  * @author Silvan Wyss
  * @date 2019-08-01
