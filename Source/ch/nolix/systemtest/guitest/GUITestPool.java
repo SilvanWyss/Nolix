fa4c3a047fe62665b912cddccc687e4767c353bd
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 21:12:35 +0100

    deleted structurepropertytest package

--- Source/ch/nolix/systemtest/guitest/GUITestPool.java
+++ Source/ch/nolix/systemtest/guitest/GUITestPool.java
@@ -3,13 +3,12 @@ package ch.nolix.systemtest.guitest;
 
 //own imports
 import ch.nolix.core.testing.basetest.TestPool;
-import ch.nolix.systemtest.guitest.structurepropertytest.StructurePropertyTestPool;
 
 //class
 public final class GUITestPool extends TestPool {
 	
 	//constructor
 	public GUITestPool() {
-		super(new StructurePropertyTestPool());
+		super();
 	}
 }
--- Source/ch/nolix/systemtest/guitest/structurepropertytest/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemtest.guitest.structurepropertytest} package defines
- * tests for the {@link ch.nolix.systemapi.guiapi.structureproperty} package.
- * 
- * @author Silvan Wyss
- * @date 2020-06-12
- */
-package ch.nolix.systemtest.guitest.structurepropertytest;
