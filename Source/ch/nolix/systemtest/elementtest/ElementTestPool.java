8f1948fb6441ba6ae16026475ba2715709e12e5d
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 21:15:56 +0100

    created elementtest package

---
+++ Source/ch/nolix/systemtest/elementtest/ElementTestPool.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.systemtest.elementtest;
+
+//own imports
+import ch.nolix.core.testing.basetest.TestPool;
+
+//class
+public final class ElementTestPool extends TestPool {
+	
+	//constructor
+	public ElementTestPool() {
+		super();
+	}
+}
---
+++ Source/ch/nolix/systemtest/elementtest/Launcher.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.systemtest.elementtest;
+
+//class
+public final class Launcher {
+	
+	//main method
+	public static void main(String[] args) {
+		new ElementTestPool().run();
+	}
+	
+	//constructor
+	private Launcher() {}
+}
