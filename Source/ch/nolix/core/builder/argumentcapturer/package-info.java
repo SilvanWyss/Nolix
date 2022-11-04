18511e8bc338e0aa48f894e337a712177fbcb72d
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 15:48:10 +0100

    deleted argumentcapturer package

--- Source/ch/nolix/core/builder/argumentcapturer/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.builder} package defines
- * some {@link ch.nolix.core.builder.main.ArgumentCapturer}s.
- * 
- * @author Silvan Wyss
- * @date 2022-02-15
- */
-package ch.nolix.core.builder.argumentcapturer;
