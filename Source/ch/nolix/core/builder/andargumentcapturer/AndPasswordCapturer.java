7975b83be019061b05227815bd60aee6606c234e
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 16:33:13 +0100

    created AndPasswordCapturer

---
+++ Source/ch/nolix/core/builder/andargumentcapturer/AndPasswordCapturer.java
@@ -0,0 +1,24 @@
+//package declaration
+package ch.nolix.core.builder.andargumentcapturer;
+
+//own imports
+import ch.nolix.core.builder.main.ArgumentCapturer;
+
+//class
+public class AndPasswordCapturer<N> extends ArgumentCapturer<String, N> {
+	
+	//constructor
+	public AndPasswordCapturer(final N nextArgumentCapturer) {
+		super(nextArgumentCapturer);
+	}
+	
+	//method
+	public final N andPassword(final String password) {
+		return setArgumentAndGetNext(password);
+	}
+	
+	//method
+	public final String getPassword() {
+		return getRefArgument();
+	}
+}
