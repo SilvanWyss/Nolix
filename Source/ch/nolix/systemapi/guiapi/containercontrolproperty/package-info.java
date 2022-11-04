9a2c67d81ec81e251b26006478d969920ec31f2b
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 20:57:31 +0100

    deleted containercontrolproperty package

--- Source/ch/nolix/systemapi/guiapi/containercontrolproperty/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemapi.guiapi.containercontrolproperty} package defines properties of container GUI controls.
- * 
- * @author Silvan Wyss
- * @date 2022-07-23
- */
-package ch.nolix.systemapi.guiapi.containercontrolproperty;
