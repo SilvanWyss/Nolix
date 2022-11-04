24ed1d67901f1788195e51dd7bd0da5d633ae7b4
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 20:55:29 +0100

    deleted controlrole package

--- Source/ch/nolix/systemapi/guiapi/controlrole/package-info.java
+++
@@ -1,8 +0,0 @@
-//package declaration
-/**
- * The {@link ch.nolix.systemapi.guiapi.controlrole} package declares roles for GUI controls.
- * 
- * @author Silvan Wyss
- * @date 2022-07-17
- */
-package ch.nolix.systemapi.guiapi.controlrole;
