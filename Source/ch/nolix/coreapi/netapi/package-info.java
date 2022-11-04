9c241367ab004d4ba9477dcc82d1a5434fa4ebf8
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 15:34:03 +0100

    deleted netapi package

--- Source/ch/nolix/coreapi/netapi/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.netapi} package declares types for net communication.
- * 
- * @author Silvan Wyss
- * @date 2022-07-02
- */
-package ch.nolix.coreapi.netapi;
