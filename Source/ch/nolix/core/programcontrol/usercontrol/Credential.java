3076f7822f31bdd2cfc6c08ffefe8ae62de08e5a
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 16:43:15 +0100

    deleted LoginNameMediator

--- Source/ch/nolix/core/programcontrol/usercontrol/Credential.java
+++ Source/ch/nolix/core/programcontrol/usercontrol/Credential.java
@@ -2,19 +2,32 @@
 package ch.nolix.core.programcontrol.usercontrol;
 
 //own imports
+import ch.nolix.core.builder.andargumentcapturer.AndPasswordCapturer;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.core.programatom.name.LowerCaseCatalogue;
 
 //class
 public final class Credential {
 	
-	//attributes
+	//attribute
 	private final String loginName;
+	
+	//attribute
 	private final String password;
 	
+	//static class
+	public static final class CredentialBuilder extends AndPasswordCapturer<Credential> {
+		
+		//constructor
+		private CredentialBuilder(final String loginName) {
+			super(null);
+			this.setBuilder(() -> new Credential(loginName, super.getPassword()));
+		}
+	}
+	
 	//static method	
-	public static LoginNameMediator withLoginName(final String loginName) {
-		return new LoginNameMediator(loginName);
+	public static CredentialBuilder withLoginName(final String loginName) {
+		return new CredentialBuilder(loginName);
 	}
 	
 	//constructor
