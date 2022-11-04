0286ddd48163d764f71e6539451b5caa3799ae95
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-04 15:38:46 +0100

    created programstructure package

---
+++ Source/ch/nolix/core/programstructure/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.programstructure} package defines types for program structure.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-11-04
+ */
+package ch.nolix.core.programstructure;
