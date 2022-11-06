c350c00b232f1c4109b685b03f602bf4d74e6778
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-06 10:51:11 +0100

    created ContainerTest

---
+++ Source/ch/nolix/coretest/containertest/ContainerTest.java
@@ -0,0 +1,39 @@
+//package declaration
+package ch.nolix.coretest.containertest;
+
+//own imports
+import ch.nolix.core.testing.basetest.TestCase;
+import ch.nolix.core.testing.test.Test;
+import ch.nolix.coreapi.containerapi.mainapi.IContainer;
+
+//class
+public abstract class ContainerTest extends Test {
+	
+	//method
+	@TestCase
+	public void testCase_toArrayOfType() {
+		
+		//setup
+		final var string1 = "x";
+		final var string2 = "xx";
+		final var string3 = "xxx";
+		final var string4 = "xxxx";
+		final var testUnit = createContainerWithElements(string1, string2, string3, string4);
+		
+		//execution
+		final var result = testUnit.toArrayOfType(String.class);
+		
+		//verification
+		expect(result.length).isEqualTo(4);
+		expect(result[0]).isSameAs(string1);
+		expect(result[1]).isSameAs(string2);
+		expect(result[2]).isSameAs(string3);
+		expect(result[3]).isSameAs(string4);
+	}
+	
+	//method declaration
+	protected abstract <E> IContainer<E> createContainerWithElements(@SuppressWarnings("unchecked")E... elements);
+	
+	//method declaration
+	protected abstract <E> IContainer<E> createEmptyContainer();
+}
