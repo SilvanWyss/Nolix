b8839ae2bd30a384ba655065506c2c806ab54856
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-06 21:33:05 +0100

    created structuretest package

---
+++ Source/ch/nolix/systemtest/structuretest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.structuretest} package defines
+ * tests for the {@link ch.nolix.system.structure} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-11-06
+ */
+package ch.nolix.systemtest.structuretest;
