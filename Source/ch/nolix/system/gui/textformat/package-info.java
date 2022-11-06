0e190d76db0d2d2f6bc97f1bd04d8a49233d8538
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-06 14:42:58 +0100

    deleted TextFormat

--- Source/ch/nolix/system/gui/textformat/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.gui.textformat} package defines a text format.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.system.gui.textformat;
