2d057ebc971cf054eb5c88dca10e44c42926c8bf
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-06 20:43:16 +0100

    edited Launcher

--- Source/ch/nolix/coretest/programcontroltest/sequencertest/Launcher.java
+++ Source/ch/nolix/coretest/programcontroltest/sequencertest/Launcher.java
@@ -2,28 +2,13 @@
 package ch.nolix.coretest.programcontroltest.sequencertest;
 
 //class
-/**
- * The Launcher provides a main method to run a sequencer test pool.
- * Of this class an instance cannot be created.
- * 
- * @author Silvan Wyss
- * @date 2017-11-18
- */
 public final class Launcher {
 	
 	//main method
-	/**
-	 * Creates a new sequencer test pool and runs it.
-	 * li
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		new SequencerTestPool().run();
 	}
 
 	//constructor
-	/**
-	 * Prevents that an instance of the {@link Launcher} can be created.
-	 */
 	private Launcher() {}
 }
