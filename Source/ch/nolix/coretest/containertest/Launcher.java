f65a9c0817388809aa740e7716cf138ad5f1dc32
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-06 20:43:36 +0100

    edited Launcher

--- Source/ch/nolix/coretest/containertest/Launcher.java
+++ Source/ch/nolix/coretest/containertest/Launcher.java
@@ -2,27 +2,13 @@
 package ch.nolix.coretest.containertest;
 
 //class
-/**
- * Of this class an instance cannot be created.
- * 
- * @author Silvan Wyss
- * @date 2016-09-01
- */
 public final class Launcher {
 	
 	//main method
-	/**
-	 * Creates a new container test pool and runs it.
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		new ContainerTestPool().run();
 	}
 	
 	//constructor
-	/**
-	 * Prevents that an instance of the {@link Launcher} can be created.
-	 */
 	private Launcher() {}
 }
