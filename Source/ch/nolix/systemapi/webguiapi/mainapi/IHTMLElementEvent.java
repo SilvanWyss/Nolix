f2a87def2f62a875d50817afef5e1f0dca6ffc1a
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-11 21:12:38 +0100

    created IHTMLElementEvent

---
+++ Source/ch/nolix/systemapi/webguiapi/mainapi/IHTMLElementEvent.java
@@ -0,0 +1,12 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.mainapi;
+
+//interface
+public interface IHTMLElementEvent {
+	
+	//method declaration
+	String getHTMLElementId();
+	
+	//method declaration
+	String getHTMLEvent();
+}
