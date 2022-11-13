95791520449e391f1476a2bc26f7edb4ef7e58a9
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-13 13:13:23 +0100

    created algebra package

---
+++ Source/ch/nolix/core/math/algebra/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.math.algebra} package defines types for algebra.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-11-13
+ */
+package ch.nolix.core.math.algebra;
