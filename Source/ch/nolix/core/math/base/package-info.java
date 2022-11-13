b142fe173520ea09e5fd1c644c1aebb17eade075
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-13 13:18:08 +0100

    created base package

---
+++ Source/ch/nolix/core/math/base/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.math.base} package defines types for basic mathematics.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-11-13
+ */
+package ch.nolix.core.math.base;
