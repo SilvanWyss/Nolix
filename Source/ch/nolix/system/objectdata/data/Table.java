3400f6351e1c2507953d7a3cee6cafa803468ae8
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-15 22:19:59 +0100

    edited Table

--- Source/ch/nolix/system/objectdata/data/Table.java
+++ Source/ch/nolix/system/objectdata/data/Table.java
@@ -1,6 +1,7 @@
 //package declaration
 package ch.nolix.system.objectdata.data;
 
+//own imports
 import ch.nolix.core.container.main.LinkedList;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.core.programatom.name.LowerCaseCatalogue;
