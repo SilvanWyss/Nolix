099edf81b2762c4f777adb1480760195a87163cf
Silvan Wyss <silvan.wyss@gmail.com>
2022-11-19 09:56:16 +0100

    created BackReferenceHelper

---
+++ Source/ch/nolix/system/objectdatabase/propertyhelper/BackReferenceHelper.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.system.objectdatabase.propertyhelper;
+
+//own imports
+import ch.nolix.systemapi.objectdatabaseapi.propertyhelperapi.IBackReferenceHelper;
+
+//class
+public final class BackReferenceHelper extends PropertyHelper implements IBackReferenceHelper {}
