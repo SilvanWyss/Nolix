2dc757dc17bc36b4e801ccbb3a288672deaf7654
Silvan Wyss <silvan.wyss@gmail.com>
2022-12-18 11:28:38 +0100

    edited NodeDatabaseAdapterTest

--- Source/ch/nolix/systemtest/objectdatabasetest/databaseadaptertest/NodeDatabaseAdapterTest.java
+++ Source/ch/nolix/systemtest/objectdatabasetest/databaseadaptertest/NodeDatabaseAdapterTest.java
@@ -3,34 +3,21 @@ package ch.nolix.systemtest.objectdatabasetest.databaseadaptertest;
 
 //own imports
 import ch.nolix.core.document.node.MutableNode;
-import ch.nolix.core.errorcontrol.invalidargumentexception.InvalidArgumentException;
 import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.core.testing.test.Test;
 import ch.nolix.system.objectdatabase.database.Entity;
 import ch.nolix.system.objectdatabase.database.Schema;
-import ch.nolix.system.objectdatabase.database.Value;
 import ch.nolix.system.objectdatabase.databaseadapter.NodeDatabaseAdapter;
-import ch.nolix.system.objectdatabase.databasehelper.EntityHelper;
 
 //class
 public final class NodeDatabaseAdapterTest extends Test {
 	
 	//static class
-	private static final class EmptyThing extends Entity {}
-	
-	//static class
-	private static final class Person extends Entity {
+	private static final class Pet extends Entity {
 		
-		//attribute
-		private final Value<String> firstName = new Value<>();
-		
-		//attribute
-		private final Value<String> lastName = new Value<>();
-		
-		//method
-		public void setFirstNameAndLastName(final String firstName, final String lastName) {
-			this.firstName.setValue(firstName);
-			this.lastName.setValue(lastName);
+		//constructor
+		public Pet() {
+			initialize();
 		}
 	}
 	
@@ -47,30 +34,8 @@ public final class NodeDatabaseAdapterTest extends Test {
 		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("MyDatabase").usingSchema(schema);
 		
 		//verification
+		expect(result.getSaveCount()).isEqualTo(0);
 		expectNot(result.hasChanges());
-	}
-	
-	//method
-	@TestCase
-	public void testCase_insertEntity_whenGivenEntityContainsEmptyButMandatoryProperties() {
-		
-		//setup part 1
-		final var nodeDatabase = new MutableNode();
-		final var schema = Schema.withEntityType(Person.class);
-		final var testUnit =
-		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("MyDatabase").usingSchema(schema);
-		
-		//setup part 2
-		final var person = new Person();
-		
-		//setup verification
-		expect(new EntityHelper().containsMandatoryAndEmptyBaseValuesOrBaseReferences(person));
-		
-		//execution & verification
-		expectRunning(() -> testUnit.insert(person)).throwsException().ofType(InvalidArgumentException.class);
-		
-		//verification
-		expectNot(testUnit.hasChanges());
 	}
 	
 	//method
@@ -79,7 +44,7 @@ public final class NodeDatabaseAdapterTest extends Test {
 		
 		//setup
 		final var nodeDatabase = new MutableNode();
-		final var schema = Schema.withEntityType(EmptyThing.class);
+		final var schema = Schema.withEntityType(Pet.class);
 		final var testUnit =
 		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("MyDatabase").usingSchema(schema);
 		
@@ -87,46 +52,26 @@ public final class NodeDatabaseAdapterTest extends Test {
 		testUnit.saveChangesAndReset();
 		
 		//verification
+		expect(testUnit.getSaveCount()).isEqualTo(1);
 		expectNot(testUnit.hasChanges());
 	}
 	
 	//method
 	@TestCase
-	public void testCase_saveChangesAndReset_whenHasInsertedEmptyEntity() {
+	public void testCase_saveChangesAndReset_whenHasChanges() {
 		
 		//setup
 		final var nodeDatabase = new MutableNode();
-		final var schema = Schema.withEntityType(EmptyThing.class);
+		final var schema = Schema.withEntityType(Pet.class);
 		final var testUnit =
 		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("MyDatabase").usingSchema(schema);
-		testUnit.insert(new EmptyThing());
+		testUnit.insert(new Pet());
 		
 		//execution
 		testUnit.saveChangesAndReset();
 		
 		//verification
-		expectNot(testUnit.hasChanges());
-	}
-	
-	//method
-	@TestCase
-	public void testCase_saveChangesAndReset_whenHasInsertedEntity() {
-		
-		//setup part 1
-		final var nodeDatabase = new MutableNode();
-		final var schema = Schema.withEntityType(Person.class);
-		final var testUnit =
-		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("MyDatabase").usingSchema(schema);
-		
-		//setup part 2
-		final var person = new Person();
-		person.setFirstNameAndLastName("Donald", "Duck");
-		testUnit.insert(person);
-		
-		//execution
-		testUnit.saveChangesAndReset();
-		
-		//verification
+		expect(testUnit.getSaveCount()).isEqualTo(1);
 		expectNot(testUnit.hasChanges());
 	}
 }
