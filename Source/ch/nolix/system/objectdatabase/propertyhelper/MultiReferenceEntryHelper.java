f1c42b9cee1b498fa463d376b83b381cfc508854
Silvan Wyss <silvan.wyss@gmail.com>
2022-12-25 21:12:14 +0100

    created MultiReferenceEntryHelper

---
+++ Source/ch/nolix/system/objectdatabase/propertyhelper/MultiReferenceEntryHelper.java
@@ -0,0 +1,9 @@
+//package declaration
+package ch.nolix.system.objectdatabase.propertyhelper;
+
+//own imports
+import ch.nolix.system.database.databaseobjecthelper.DatabaseObjectHelper;
+import ch.nolix.systemapi.objectdatabaseapi.propertyhelperapi.IMultiReferenceEntryHelper;
+
+//class
+public final class MultiReferenceEntryHelper extends DatabaseObjectHelper implements IMultiReferenceEntryHelper {}
---
+++ Source/ch/nolix/systemapi/objectdatabaseapi/propertyhelperapi/IMultiReferenceEntryHelper.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.systemapi.objectdatabaseapi.propertyhelperapi;
+
+//own imports
+import ch.nolix.systemapi.databaseapi.databaseobjecthelperapi.IDatabaseObjectHelper;
+
+//interface
+public interface IMultiReferenceEntryHelper extends IDatabaseObjectHelper {}
