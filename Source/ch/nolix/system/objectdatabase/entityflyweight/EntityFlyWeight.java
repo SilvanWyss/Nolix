70154a3c31827085a494f875629fc7ee1e278585
Silvan Wyss <silvan.wyss@gmail.com>
2022-12-27 22:24:00 +0100

    created EntityFlyWeight

---
+++ Source/ch/nolix/system/objectdatabase/entityflyweight/EntityFlyWeight.java
@@ -0,0 +1,39 @@
+//package declaration
+package ch.nolix.system.objectdatabase.entityflyweight;
+
+//own imports
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IAction;
+import ch.nolix.systemapi.objectdatabaseapi.databaseflyweightapi.IEntityFlyWeight;
+
+//class
+public final class EntityFlyWeight implements IEntityFlyWeight {
+	
+	//optional attribute
+	private IAction insertAction;
+	
+	@Override
+	public boolean isVoid() {
+		return false;
+	}
+	
+	@Override
+	public void noteInsert() {
+		if (hasInsertAction()) {
+			insertAction.run();
+		}
+	}
+	
+	@Override
+	public void setInsertAction(final IAction insertAction) {
+		
+		GlobalValidator.assertThat(insertAction).thatIsNamed("insert action").isNotNull();
+		
+		this.insertAction = insertAction;
+	}
+	
+	//method
+	private boolean hasInsertAction() {
+		return (insertAction != null);
+	}
+}
