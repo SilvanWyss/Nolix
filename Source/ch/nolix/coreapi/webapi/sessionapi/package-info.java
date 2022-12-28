345dd770a90788ed076f318a57b5024a24c703e4
Silvan Wyss <silvan.wyss@gmail.com>
2022-12-28 22:11:33 +0100

    created sessionapi package

---
+++ Source/ch/nolix/coreapi/webapi/sessionapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.webapi.sessionapi} package declares types for web sessions.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-12-28
+ */
+package ch.nolix.coreapi.webapi.sessionapi;
