f854a81a47f1c6945f1445a71c6148b4c058db3c
Silvan Wyss <silvan.wyss@gmail.com>
2022-12-29 09:08:37 +0100

    extended RequestProtocol

--- Source/ch/nolix/system/application/basewebapplicationprotocol/RequestProtocol.java
+++ Source/ch/nolix/system/application/basewebapplicationprotocol/RequestProtocol.java
@@ -5,6 +5,9 @@ package ch.nolix.system.application.basewebapplicationprotocol;
 public final class RequestProtocol {
 	
 	//constant
+	public static final String GET_COOKIE_VALUE_BY_COOKIE_NAME = "GetCookieValueByCookieName";
+	
+	//constant
 	public static final String GET_TEXT_FROM_CLIPBOARD = "GetTextFromClipboard";
 	
 	//constructor
