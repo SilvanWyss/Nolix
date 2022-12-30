c36f33e2a21abe27184a012a489740e738781f32
Silvan Wyss <silvan.wyss@gmail.com>
2022-12-30 12:18:39 +0100

    created propertyvalidatorapi package

---
+++ Source/ch/nolix/systemapi/objectdatabaseapi/propertyvalidatorapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.objectdatabaseapi.propertyvalidatorapi} package declares
+ * validators for the types of the {@link ch.nolix.systemapi.objectdatabaseapi.propertyapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-12-30
+ */
+package ch.nolix.systemapi.objectdatabaseapi.propertyvalidatorapi;
