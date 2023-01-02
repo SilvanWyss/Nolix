8eded2ad506e20d1aeb13ce5296bbe2ecfad3f13
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-02 11:41:25 +0100

    edited MultiReferenceEntryTableSQLDTOCatalogue

--- Source/ch/nolix/system/sqlrawschema/multireferenceentrytable/MultiReferenceEntryTableSQLDTOCatalogue.java
+++ Source/ch/nolix/system/sqlrawschema/multireferenceentrytable/MultiReferenceEntryTableSQLDTOCatalogue.java
@@ -24,7 +24,7 @@ public final class MultiReferenceEntryTableSQLDTOCatalogue {
 	new ColumnDTO(MultiReferenceEntryTableColumn.ENTITY_ID.getName(), SQLDatatypeCatalogue.TEXT);
 	
 	//constant
-	private static final IColumnDTO REFERENCED_RECORD_COLUMN_SQL_DTO =
+	private static final IColumnDTO REFERENCED_ENTITY_COLUMN_SQL_DTO =
 	new ColumnDTO(
 		MultiReferenceEntryTableColumn.REFERENCED_ENTITY_ID.getName(),
 		SQLDatatypeCatalogue.TEXT
@@ -36,7 +36,7 @@ public final class MultiReferenceEntryTableSQLDTOCatalogue {
 		MultiContentTable.MULTI_REFERENCE_ENTRY.getFullName(),
 		MULTI_REFERENCE_COLUMN_COLUMN_SQL_DTO,
 		ENTITY_COLUMN_SQL_DTO,
-		REFERENCED_RECORD_COLUMN_SQL_DTO
+		REFERENCED_ENTITY_COLUMN_SQL_DTO
 	);
 	
 	//constructor
