a05ff82d146081ff06ef942851393525fb8fadcf
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-02 16:15:00 +0100

    created arraycontrol package

---
+++ Source/ch/nolix/core/container/arraycontrol/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.container.arraycontrol} package defines types to control arrays.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-01-02
+ */
+package ch.nolix.core.container.arraycontrol;
