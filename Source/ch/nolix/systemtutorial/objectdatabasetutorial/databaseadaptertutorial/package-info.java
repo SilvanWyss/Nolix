189bd076666f3432834e5c2b2264b6b18b6c7c08
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-08 12:34:36 +0100

    created databaseadaptertutorial package

---
+++ Source/ch/nolix/systemtutorial/objectdatabasetutorial/databaseadaptertutorial/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtutorial.objectdatabasetutorial.databaseadaptertutorial} package defines
+ * tutorials for the {@link ch.nolix.system.objectdatabase.databaseadapter} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-01-08
+ */
+package ch.nolix.systemtutorial.objectdatabasetutorial.databaseadaptertutorial;
