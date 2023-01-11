bd37bd4a97c7d55a49e1995e2d72b74889b59be9
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-11 21:30:46 +0100

    created triggeruniversalapi package

---
+++ Source/ch/nolix/coreapi/programcontrolapi/triggeruniversalapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.programcontrolapi.triggeruniversalapi} package declares
+ * types to subscribe and trigger.
+ *  
+ * @author Silvan Wyss
+ * @date 2023-01-11
+ */
+package ch.nolix.coreapi.programcontrolapi.triggeruniversalapi;
