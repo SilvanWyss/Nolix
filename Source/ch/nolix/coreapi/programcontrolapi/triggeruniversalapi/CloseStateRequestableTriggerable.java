8dede2618a80c9ec8b9784122dbf6ed9f71ef6c1
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-11 21:32:39 +0100

    created CloseStateRequestableTriggerable

---
+++ Source/ch/nolix/coreapi/programcontrolapi/triggeruniversalapi/CloseStateRequestableTriggerable.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.coreapi.programcontrolapi.triggeruniversalapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.requestuniversalapi.CloseStateRequestable;
+
+//interface
+public interface CloseStateRequestableTriggerable extends Triggerable, CloseStateRequestable {}
