7150e21b585644540c6df9b6c06e425b80c10cd6
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-13 16:59:19 +0100

    edited BlackWhiteJuliaFractalTutorial

--- Source/ch/nolix/businesstutorial/dynamicmathtutorial/BlackWhiteJuliaFractalTutorial.java
+++ Source/ch/nolix/businesstutorial/dynamicmathtutorial/BlackWhiteJuliaFractalTutorial.java
@@ -33,7 +33,7 @@ public final class BlackWhiteJuliaFractalTutorial {
 		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
 	}
 	
-	private static final class MainSession extends BackendWebClientSession<VoidApplicationContext> {
+	private static final class MainSession extends BackendWebClientSession<Object> {
 		
 		@Override
 		protected void initialize() {
