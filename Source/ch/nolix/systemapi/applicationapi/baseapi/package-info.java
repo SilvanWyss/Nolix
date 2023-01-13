2310fd0f09196eaa73736ecc1ee89e8f50ecbdf3
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-13 15:49:20 +0100

    created baseapi package

---
+++ Source/ch/nolix/systemapi/applicationapi/baseapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.applicationapi.baseapi} package declares applications and back-end clients.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-01-13
+ */
+package ch.nolix.systemapi.applicationapi.baseapi;
