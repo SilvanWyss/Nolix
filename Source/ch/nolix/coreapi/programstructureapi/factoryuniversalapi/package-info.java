bf53c8f765957f3c22071bd3a4019130ab9e248e
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-14 23:15:07 +0100

    created factoryuniversalapi package

---
+++ Source/ch/nolix/coreapi/programstructureapi/factoryuniversalapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.programstructureapi.factoryuniversalapi} package declares factory methods.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-01-14
+ */
+package ch.nolix.coreapi.programstructureapi.factoryuniversalapi;
