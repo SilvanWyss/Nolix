92e3bcc68a6c5762b96ca5c1db201de882017586
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-18 21:38:21 +0100

    created NestedHorizontalStackTutorial

---
+++ Source/ch/nolix/systemtutorial/webguitutorial/linearcontainertutorial/NestedHorizontalStackTutorial.java
@@ -0,0 +1,69 @@
+package ch.nolix.systemtutorial.webguitutorial.linearcontainertutorial;
+
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+import ch.nolix.system.webgui.control.Text;
+import ch.nolix.system.webgui.linearcontainer.HorizontalStack;
+import ch.nolix.system.webgui.linearcontainer.VerticalStack;
+import ch.nolix.systemapi.webguiapi.mainapi.ControlState;
+
+public final class NestedHorizontalStackTutorial {
+	
+	public static void main(String[] args) {
+		
+		//Creates a Server.
+		final var server = Server.forDefaultPort();
+		
+		//Adds a default Application to the Server.
+		server.addDefaultApplication(
+			"Nested HorizontalStack tutorial",
+			MainSession.class,
+			VoidApplicationContext.INSTANCE
+		);
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		GlobalSequencer.waitForSeconds(2);
+		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	private static final class MainSession extends BackendWebClientSession<Object> {
+		
+		@Override
+		protected void initialize() {
+			
+			//Creates a VerticalStack.
+			final var verticalStack = new VerticalStack();
+			
+			//Creates 3 HorizontalStacks with 2 Texts each.
+			final var horizontalStack1 =
+			new HorizontalStack().addControl(new Text().setText("A1"), new Text().setText("A2"));
+			final var horizontalStack2 =
+			new HorizontalStack().addControl(new Text().setText("B1"), new Text().setText("B2"));
+			final var horizontalStack3 =
+			new HorizontalStack().addControl(new Text().setText("C1"), new Text().setText("C2"));
+			
+			//Adds the HorizontalStacks to the VerticalStack.
+			verticalStack.addControl(horizontalStack1, horizontalStack2, horizontalStack3);
+			
+			//Configures the style of the VerticalStack.
+			verticalStack.editStyle(s -> s.setTextSizeForState(ControlState.BASE, 50));
+			verticalStack.editStyle(s -> s.setChildControlMarginForState(ControlState.BASE, 20));
+			
+			//Configures the style of the HorizontalStacks.
+			horizontalStack1.editStyle(s -> s.setChildControlMarginForState(ControlState.BASE, 20));
+			horizontalStack2.editStyle(s -> s.setChildControlMarginForState(ControlState.BASE, 20));
+			horizontalStack3.editStyle(s -> s.setChildControlMarginForState(ControlState.BASE, 20));
+			
+			//Adds the VerticalStack to the GUI of the current MainSession.
+			getRefGUI().pushLayerWithRootControl(verticalStack);
+		}
+	}
+	
+	private NestedHorizontalStackTutorial() {}
+}
