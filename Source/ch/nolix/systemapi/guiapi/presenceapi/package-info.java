c9582db21a2bce2420996063c31a5b617156f49c
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-20 11:28:08 +0100

    created presenceapi package

---
+++ Source/ch/nolix/systemapi/guiapi/presenceapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.guiapi.presenceapi} package declares types for presence.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-01-20
+ */
+package ch.nolix.systemapi.guiapi.presenceapi;
