0099cec31cc8a0f74b524c27fa5cea23a8094c2c
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-20 13:30:56 +0100

    made IChangeSaver a GroupCloseable

--- Source/ch/nolix/coreapi/functionapi/mutationuniversalapi/IChangeSaver.java
+++ Source/ch/nolix/coreapi/functionapi/mutationuniversalapi/IChangeSaver.java
@@ -3,6 +3,7 @@ package ch.nolix.coreapi.functionapi.mutationuniversalapi;
 
 //own imports
 import ch.nolix.coreapi.functionapi.requestuniversalapi.ChangeRequestable;
+import ch.nolix.coreapi.programcontrolapi.resourcecontrolapi.GroupCloseable;
 
 //interface
 /**
@@ -11,7 +12,7 @@ import ch.nolix.coreapi.functionapi.requestuniversalapi.ChangeRequestable;
  * @author Silvan Wyss
  * @date 2018-04-09
  */
-public interface IChangeSaver extends ChangeRequestable {
+public interface IChangeSaver extends ChangeRequestable, GroupCloseable {
 	
 	//method declaration
 	/**
