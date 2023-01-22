1c2c39b7bffa605dd09586d34b6b35994a8fe019
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-22 22:56:45 +0100

    created IValidationLabel

---
+++ Source/ch/nolix/systemapi/webguiapi/controlapi/IValidationLabel.java
@@ -0,0 +1,17 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.controlapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.mutationuniversalapi.Clearable;
+import ch.nolix.systemapi.webguiapi.mainapi.IControl;
+
+//interface
+public interface IValidationLabel<
+	VL extends IValidationLabel<VL, VLS>,
+	VLS extends IValidationLabelStyle<VLS>
+>
+extends Clearable, IControl<VL, VLS> {
+	
+	//method declaration
+	void showError(Throwable error);
+}
---
+++ Source/ch/nolix/systemapi/webguiapi/controlapi/IValidationLabelStyle.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.controlapi;
+
+//own imports
+import ch.nolix.systemapi.webguiapi.controlstyleapi.IExtendedControlStyle;
+
+//interface
+public interface IValidationLabelStyle<VLS extends IValidationLabelStyle<VLS>> extends IExtendedControlStyle<VLS> {}
