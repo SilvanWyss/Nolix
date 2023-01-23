1e797daae595ff4414a7d415682f5d041bd3ffb8
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-23 21:58:07 +0100

    created controlhelperapi package

---
+++ Source/ch/nolix/systemapi/webguiapi/controlhelperapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.webguiapi.controlhelperapi} package declares
+ * helpers for {@link ch.nolix.systemapi.webguiapi.mainapi.IControl}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-01-23
+ */
+package ch.nolix.systemapi.webguiapi.controlhelperapi;
