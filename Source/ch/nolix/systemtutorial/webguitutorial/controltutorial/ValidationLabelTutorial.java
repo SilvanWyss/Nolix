e818b2845810e6bb67082fa1f0d70a32b0638449
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-24 22:50:55 +0100

    created ValidationLabelTutorial

---
+++ Source/ch/nolix/systemtutorial/webguitutorial/controltutorial/ValidationLabelTutorial.java
@@ -0,0 +1,58 @@
+package ch.nolix.systemtutorial.webguitutorial.controltutorial;
+
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.core.programatom.name.LowerCaseCatalogue;
+import ch.nolix.core.programcontrol.sequencer.GlobalSequencer;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.main.VoidApplicationContext;
+import ch.nolix.system.application.webapplication.BackendWebClientSession;
+import ch.nolix.system.webgui.control.Button;
+import ch.nolix.system.webgui.control.Text;
+import ch.nolix.system.webgui.control.Textbox;
+import ch.nolix.system.webgui.control.ValidationLabel;
+import ch.nolix.system.webgui.linearcontainer.VerticalStack;
+
+public final class ValidationLabelTutorial {
+	
+	public static void main(String[] args) {
+		
+		//Creates a Server.
+		final var server = Server.forDefaultPort();
+		
+		//Adds a default Application to the Server.
+		server.addDefaultApplication("ValidationLabel tutorial", MainSession.class, VoidApplicationContext.INSTANCE);
+		
+		//Starts a web browser that will connect to the Server.
+		ShellProvider.startFirefoxOpeningLoopBackAddress();
+		
+		//Closes the Server as soon as it does not have a client connected any more.
+		GlobalSequencer.waitForSeconds(2);
+		GlobalSequencer.asSoonAsNoMore(server::hasClientConnected).runInBackground(server::close);
+	}
+	
+	private static final class MainSession extends BackendWebClientSession<Object> {
+		
+		private final Textbox positiveNumberTextbox = new Textbox();
+		
+		@Override
+		protected void initialize() {
+			getRefGUI().pushLayerWithRootControl(
+				new VerticalStack()
+				.addControl(
+					new Text().setText("Enter a positive number"),
+					new ValidationLabel(), 
+					positiveNumberTextbox,
+					new Button().setText("Ok").setLeftMouseButtonPressAction(this::enterPositiveNumber)
+				)
+			);
+		}
+		
+		private void enterPositiveNumber() {
+			final var number = Integer.parseInt(positiveNumberTextbox.getText());
+			GlobalValidator.assertThat(number).thatIsNamed(LowerCaseCatalogue.NUMBER).isPositive();
+		}
+	}
+	
+	private ValidationLabelTutorial() {}
+}
