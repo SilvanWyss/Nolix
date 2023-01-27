1f575c38126a97f816aa6d5901193069a14a54ff
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-27 15:04:34 +0100

    edited QualifiedNamed

--- Source/ch/nolix/coreapi/attributeapi/mandatoryattributeuniversalapi/QualifiedNamed.java
+++ Source/ch/nolix/coreapi/attributeapi/mandatoryattributeuniversalapi/QualifiedNamed.java
@@ -15,15 +15,26 @@ import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesig
 public interface QualifiedNamed extends Named {
 	
 	//method
+	/**
+	 * @return the qualified name of the current {@link QualifiedNamed}.
+	 */
 	default String getQualifiedName() {
 		return (getNamePrefix() + getName());
 	}
 	
 	//method
+	/**
+	 * @return the qualified name of the current {@link QualifiedNamed} in quotes.
+	 */
 	default String getQualifiedNameInQuotes() {
 		return ("'" + getQualifiedName() + "'");
 	}
 	
 	//method declaration
+	/**
+	 * @return the name prefix of the current {@link QualifiedNamed}.
+	 * The name prefix of a {@link QualifiedNamed} completes
+	 * the name of the {@link QualifiedNamed} to a qualified name.
+	 */
 	String getNamePrefix();
 }
