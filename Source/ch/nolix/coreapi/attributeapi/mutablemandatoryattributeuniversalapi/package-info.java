2023dd6ce8f34cc4d0c6cf4e8ff326c4974fe205
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-27 11:35:31 +0100

    created mutablemandatoryattributeuniversalapi package

---
+++ Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeuniversalapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.attributeapi.mutablemandatoryattributeuniversalapi} package declares
+ * types with a certain mutable attribute.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-01-27
+ */
+package ch.nolix.coreapi.attributeapi.mutablemandatoryattributeuniversalapi;
