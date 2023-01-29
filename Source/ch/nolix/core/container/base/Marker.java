a3d8126de0a6ccb4d2bc2757d54dc1754d04b00f
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-29 16:37:14 +0100

    suppressed Sonar warning

--- Source/ch/nolix/core/container/base/Marker.java
+++ Source/ch/nolix/core/container/base/Marker.java
@@ -1,3 +1,3 @@
 package ch.nolix.core.container.base;
 
-public final class Marker<T> {}
+public final class Marker<T> {} //NOSONAR: Marker cannot be an interface because Marker needs to be instantiable.
