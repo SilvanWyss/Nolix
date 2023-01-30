0684c27925797ac5f3624a4ba6727612e5eabed3
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-30 22:10:27 +0100

    created IXMLAttribute

---
+++ Source/ch/nolix/coreapi/documentapi/xmlapi/IXMLAttribute.java
@@ -0,0 +1,10 @@
+//package declaration
+package ch.nolix.coreapi.documentapi.xmlapi;
+
+//own imports
+import ch.nolix.core.document.xml.XMLAttribute;
+import ch.nolix.coreapi.attributeapi.mandatoryattributeuniversalapi.Named;
+import ch.nolix.coreapi.attributeapi.mandatoryattributeuniversalapi.Valued;
+
+//interface
+public interface IXMLAttribute extends Named, Valued<XMLAttribute, String> {}
