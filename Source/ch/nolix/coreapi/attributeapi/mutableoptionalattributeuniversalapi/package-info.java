acc4cb56d65b290d50a040366f80d128d8bcd876
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-03 18:42:03 +0100

    created mutableoptionalattributeuniversalapi package

---
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeuniversalapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.attributeapi.mutableoptionalattributeuniversalapi} package declares
+ * types with a certain mutable and optional attribute.
+ * 
+ * @author Silvan Wyss
+ * @date 2022-02-03
+ */
+package ch.nolix.coreapi.attributeapi.mutableoptionalattributeuniversalapi;
