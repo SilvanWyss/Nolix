a347ff65d57eda5432709b543cb60b28fd0d8876
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-05 16:39:28 +0100

    created IToggleButton

---
+++ Source/ch/nolix/systemapi/webguiapi/controlapi/IToggleButton.java
@@ -0,0 +1,28 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.controlapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IAction;
+import ch.nolix.systemapi.webguiapi.mainapi.IControl;
+
+//interface
+public interface IToggleButton extends IControl<IToggleButton, IToggleButtonStyle> {
+	
+	//method declaration
+	void removeOffAction();
+	
+	//method declaration
+	void removeOnAction();
+	
+	//method declaration
+	IToggleButton setOff();
+	
+	//method declaration
+	IToggleButton setOffAction(IAction offAcction);
+	
+	//method declaration
+	IToggleButton setOn();
+	
+	//method declaration
+	IToggleButton setOnAction(IAction onAcction);
+}
