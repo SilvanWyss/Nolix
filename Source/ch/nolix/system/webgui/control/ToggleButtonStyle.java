dbcece2c871d02e76701ffaeb5a65848c967e8f4
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-05 17:10:48 +0100

    created ToggleButtonStyle

---
+++ Source/ch/nolix/system/webgui/control/ToggleButtonStyle.java
@@ -0,0 +1,9 @@
+//package declaration
+package ch.nolix.system.webgui.control;
+
+//own imports
+import ch.nolix.system.webgui.controlstyle.ExtendedControlStyle;
+import ch.nolix.systemapi.webguiapi.controlapi.IToggleButtonStyle;
+
+//class
+public final class ToggleButtonStyle extends ExtendedControlStyle<IToggleButtonStyle> implements IToggleButtonStyle {}
