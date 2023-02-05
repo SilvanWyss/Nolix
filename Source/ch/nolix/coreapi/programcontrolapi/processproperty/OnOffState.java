d3f1e36ef8d3a650965cf9207b31246ad02991a8
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-05 20:02:38 +0100

    created OnOffState enum

---
+++ Source/ch/nolix/coreapi/programcontrolapi/processproperty/OnOffState.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.coreapi.programcontrolapi.processproperty;
+
+//enum
+/**
+ * A {@link OnOffState} defines if something is on or off.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-05
+ */
+public enum OnOffState {
+	ON,
+	OFF
+}
