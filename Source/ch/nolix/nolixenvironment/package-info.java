3f60e85f2dae659f69052a1c761089d2ab5f53f6
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-06 18:03:08 +0100

    deleted nolixenvironment package

--- Source/ch/nolix/nolixenvironment/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.nolixenvironment} package
- * provides information about the Nolix environment on the local computer.
- * 
- * @author Silvan Wyss
- * @date 2019-12-01
- */
-package ch.nolix.nolixenvironment;
