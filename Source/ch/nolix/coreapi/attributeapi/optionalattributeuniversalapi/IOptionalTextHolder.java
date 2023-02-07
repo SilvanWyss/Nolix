7d9506a01c7af568a6dac4b5f013c482e41c2d46
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-07 17:00:03 +0100

    created IOptionalTextHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/optionalattributeuniversalapi/IOptionalTextHolder.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.optionalattributeuniversalapi;
+
+//interface
+/**
+ * A {@link IOptionalTextHolder} can have a text.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-03
+ */
+public interface IOptionalTextHolder {
+	
+	//method declaration
+	/**
+	 * @return the text of the current {@link IOptionalTextHolder}.
+	 * @throws RuntimeException if the current {@link IOptionalTextHolder} does not have a text.
+	 */
+	String getText();
+	
+	//method declaration
+	/**
+	 * @return true if the current {@link IOptionalTextHolder} has a text.
+	 */
+	boolean hasText();
+}
