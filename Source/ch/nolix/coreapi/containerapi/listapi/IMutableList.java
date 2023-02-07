74172f76d5cff44e12f158d64c0e50a9ef8c7317
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-07 17:48:43 +0100

    edited IMutableList

--- Source/ch/nolix/coreapi/containerapi/listapi/IMutableList.java
+++ Source/ch/nolix/coreapi/containerapi/listapi/IMutableList.java
@@ -1,12 +1,14 @@
 //package declaration
 package ch.nolix.coreapi.containerapi.listapi;
 
-import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 //own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.coreapi.functionapi.mutationuniversalapi.Clearable;
 
 //interface
 /**
+ * A {@link IMutableList} is a {@link IContainer} where elements can be added or removed.
+ * 
  * @author Silvan Wyss
  * @date 2022-07-04
  * @param <E> is the type of the elements of a {@link IMutableList}.
@@ -25,16 +27,19 @@ public interface IMutableList<E> extends Clearable, IContainer<E> {
 	//method declaration
 	/**
 	 * Adds the given firstElement and the given elements at the end of the current {@link IMutableList}.
+	 * The elements will be added in the given order.
 	 * 
 	 * @param firstElement
 	 * @param elements
-	 * @throws RuntimeException if the given firstElement or one of the given elements is null.
+	 * @throws RuntimeException if the given firstElement is null.
+	 * @throws RuntimeException if one of the given elements is null.
 	 */
 	void addAtEnd(E firstElement, @SuppressWarnings("unchecked")E... elements);
 	
 	//method declaration
 	/**
 	 * Adds the given elements at the end of the current {@link IMutableList}.
+	 * The elements will be added in the given order.
 	 * 
 	 * @param elements
 	 * @throws RuntimeException if one of the given elements is null.
@@ -44,7 +49,7 @@ public interface IMutableList<E> extends Clearable, IContainer<E> {
 	//method declaration
 	/**
 	 * Adds the given elements at the end of the current {@link IMutableList}.
-	 * The elements will be added in the same order as they are given.
+	 * The elements will be added in the given order.
 	 * 
 	 * @param elements
 	 * @throws RuntimeException if one of the given elements is null.
