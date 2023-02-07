99d23145c7418a87da7d88bbf3843690034f64b2
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-07 12:07:35 +0100

    edited ITextHolder

--- Source/ch/nolix/coreapi/attributeapi/mandatoryattributeuniversalapi/ITextHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mandatoryattributeuniversalapi/ITextHolder.java
@@ -1,8 +1,6 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.mandatoryattributeuniversalapi;
 
-import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
-
 //interface
 /**
  * A {@link ITextHolder} has a text.
@@ -10,7 +8,6 @@ import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesig
  * @author Silvan Wyss
  * @date 2021-06-19
  */
-@AllowDefaultMethodsAsDesignPattern
 public interface ITextHolder {
 	
 	//method declaration
@@ -18,37 +15,4 @@ public interface ITextHolder {
 	 * @return the text of the current {@link ITextHolder}.
 	 */
 	String getText();
-	
-	//method
-	/**
-	 * @return the text of the current {@link ITextHolder} in quotes.
-	 */
-	default String getTextInQuotes() {
-		return ("'" + getText() + "'");
-	}
-	
-	//method
-	/**
-	 * @param object
-	 * @return true if the current {@link ITextHolder} has the same text as the given object.
-	 */
-	default boolean hasSameTextAs(final ITextHolder object) {
-		
-		//Handles the case that the given object is null.
-		if (object == null) {
-			return false;
-		}
-		
-		//Handles the case that the given object is not null.
-		return getText().equals(object.getText());
-	}
-	
-	//method
-	/**
-	 * @param text
-	 * @return true if the current {@link ITextHolder} has the given text.
-	 */
-	default boolean hasText(final String text) {
-		return getText().equals(text);
-	}
 }
