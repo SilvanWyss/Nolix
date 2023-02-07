f893d26c5efac1b29b1570165161619f43bde222
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-07 12:03:51 +0100

    created OptionalNameable

---
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeuniversalapi/OptionalNameable.java
@@ -0,0 +1,31 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mutableoptionalattributeuniversalapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.optionalattributeuniversalapi.OptionalNamed;
+
+//interface
+/**
+* A {@link OptionalNameable} is a {@link OptionalNamed} whose name can be set and removed programmatically.
+* 
+* @author Silvan Wyss
+* @date 2023-02-07
+*/
+public interface OptionalNameable extends OptionalNamed {
+	
+	//method declaration
+	/**
+	 * Removes the name of the current {@link OptionalNameable}.
+	 */
+	void removeName();
+	
+	//method declaration
+	/**
+	 * Sets the name of the current {@link OptionalNameable}.
+	 * 
+	 * @param name
+	 * @throws RuntimeException if the given name is null.
+	 * @throws RuntimeException if the given name is blank.
+	 */
+	void setName(String name);
+}
