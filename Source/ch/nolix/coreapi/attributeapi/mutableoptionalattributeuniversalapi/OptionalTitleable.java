212a3fd718aa895cfebc4a3f9fd17bfe47289b4c
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-09 15:57:47 +0100

    created OptionalTitleable

---
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeuniversalapi/OptionalTitleable.java
@@ -0,0 +1,31 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mutableoptionalattributeuniversalapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.optionalattributeuniversalapi.OptionalTitled;
+
+//interface
+/**
+ * A {@link OptionalTitleable} is a {@link OptionalTitled} whose title can be set and removed programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-09
+ */
+public interface OptionalTitleable extends OptionalTitled {
+	
+	//method declaration
+	/**
+	 * Removes the title of the current {@link OptionalTitleable}.
+	 */
+	void removeTitle();
+	
+	//method declaration
+	/**
+	 * Sets the title of the current {@link OptionalTitleable}.
+	 * 
+	 * @param title
+	 * @throws RuntimeException if the given title is null.
+	 * @throws RuntimeException if the given title is blank.
+	 */
+	void setTitle(String title);
+}
