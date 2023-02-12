05822734e54a3b70254acec6ccea8a0bf0dc8048
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-12 21:39:01 +0100

    created IterableWithCopyableIterator

---
+++ Source/ch/nolix/coreapi/containerapi/baseapi/IterableWithCopyableIterator.java
@@ -0,0 +1,20 @@
+//package declaration
+package ch.nolix.coreapi.containerapi.baseapi;
+
+//interface
+/**
+* A {@link IterableWithCopyableIterator} is a {@link Iterable} that provide {@link CopyableIterator}s.
+* 
+* @author Silvan Wyss
+* @date 2023-02-12
+* @param <E> is the type of the elements a {@link IterableWithCopyableIterator}.
+*/
+public interface IterableWithCopyableIterator<E> extends Iterable<E> {
+	
+	//method declaration
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	CopyableIterator<E> iterator();
+}
