9a8986f469065f7efedba039432ba2b3a630bfb8
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-12 21:36:43 +0100

    created CopyableIterator

---
+++ Source/ch/nolix/coreapi/containerapi/baseapi/CopyableIterator.java
@@ -0,0 +1,18 @@
+//package declaration
+package ch.nolix.coreapi.containerapi.baseapi;
+
+//Java imports
+import java.util.Iterator;
+
+//own imports
+import ch.nolix.coreapi.functionapi.mutationuniversalapi.Copyable;
+
+//interface
+/**
+ * A {@link CopyableIterator} is a {@link Iterator} that can be copied.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-12
+ * @param <E> is the type of the elements a {@link CopyableIterator}.
+ */
+public interface CopyableIterator<E> extends Copyable<CopyableIterator<E>>, Iterator<E> {}
