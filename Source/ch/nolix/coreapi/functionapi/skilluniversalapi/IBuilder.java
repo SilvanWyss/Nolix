d9329f3ed7486c0b4a83194d1d0c011f10948a3e
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-13 21:56:07 +0100

    created IBuilder

---
+++ Source/ch/nolix/coreapi/functionapi/skilluniversalapi/IBuilder.java
@@ -0,0 +1,19 @@
+//package declaration
+package ch.nolix.coreapi.functionapi.skilluniversalapi;
+
+//interface
+/**
+* A {@link IBuilder} can build {@link Object}s.
+* 
+* @author Silvan Wyss
+* @date 2023-02-13
+* @param <O> is the type of the {@link Object}s a {@link IBuilder} can build.
+*/
+public interface IBuilder<O> {
+	
+	//method declaration
+	/**
+	 * @return a new {@link Object} from the current {@link IBuilder}.
+	 */
+	O build();
+}
