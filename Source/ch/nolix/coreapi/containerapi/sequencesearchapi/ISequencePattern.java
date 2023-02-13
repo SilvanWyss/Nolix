f38ade6db8376fa578df166e2d67c1a1512ea8a0
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-13 22:28:26 +0100

    created ISequencePattern

---
+++ Source/ch/nolix/coreapi/containerapi/sequencesearchapi/ISequencePattern.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.coreapi.containerapi.sequencesearchapi;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+
+//interface
+/**
+ * @author Silvan Wyss
+ * @date 2023-02-13
+ * @param <E> is the type of the elements of the sequences a {@link ISequencePattern} selects.
+ */
+public interface ISequencePattern<E> {
+	
+	//method declaration
+	/**
+	 * @param container
+	 * @return all sequences from the given container that match the current {@link ISequencePattern}.
+	 */
+	IContainer<IContainer<E>> getMatchingSequencesFrom(IContainer<E> container);
+}
