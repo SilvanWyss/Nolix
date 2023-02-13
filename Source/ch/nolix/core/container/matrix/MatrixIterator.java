228ea8aed1955b65ce40a9611ac9ab5fcaa892c9
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-13 22:56:25 +0100

    made MatrixIterator a CopyableIterator

--- Source/ch/nolix/core/container/matrix/MatrixIterator.java
+++ Source/ch/nolix/core/container/matrix/MatrixIterator.java
@@ -2,7 +2,6 @@
 package ch.nolix.core.container.matrix;
 
 //Java imports
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 //own imports
@@ -10,6 +9,7 @@ import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentDoesNotHaveAt
 import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentIsNullException;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.core.programatom.name.LowerCaseCatalogue;
+import ch.nolix.coreapi.containerapi.baseapi.CopyableIterator;
 
 //class
 /**
@@ -17,18 +17,26 @@ import ch.nolix.core.programatom.name.LowerCaseCatalogue;
  * @date 2016-08-01
  * @param <E> is the type of the elements of a {@link MatrixIterator}.
  */
-final class MatrixIterator<E> implements Iterator<E> {
+final class MatrixIterator<E> implements CopyableIterator<E> {
 	
 	//static method
 	public static <E2> MatrixIterator<E2> forMatrix(final Matrix<E2> matrix) {
 		return new MatrixIterator<>(matrix);
 	}
 	
+	//static mehtod
+	public static <E2> MatrixIterator<E2> forMatrixAnd1BasedStartIndex(
+		final Matrix<E2> matrix,
+		final int p1BasedStartIndex
+	) {
+		return new MatrixIterator<>(matrix, p1BasedStartIndex);
+	} 
+	
 	//attribute
 	private final Matrix<E> parentMatrix;
 	
 	//attribute
-	private int nextElement1BasedIndex = 1;
+	private int nextElement1BasedIndex;
 	
 	//constructor
 	/**
@@ -42,6 +50,39 @@ final class MatrixIterator<E> implements Iterator<E> {
 		GlobalValidator.assertThat(parentMatrix).thatIsNamed("parent Matrix").isNotNull();
 		
 		this.parentMatrix = parentMatrix;
+		nextElement1BasedIndex = 1;
+	}
+	
+	//constructor
+	/**
+	 * Creates a new {@link MatrixIterator} with the given parentMatrix and p1BasedStartIndex.
+	 * 
+	 * @param parentMatrix
+	 * @param p1BasedStartIndex
+	 * @throws ArgumentIsNullException if the given parentMatrix is null.
+	 * @throws BiggerArgumentException if
+	 * the given p1BasedStartIndex is bigger than the element count of the given parentMatrix.
+	 */
+	private MatrixIterator(final Matrix<E> parentMatrix, final int p1BasedStartIndex) {
+		
+		GlobalValidator.assertThat(parentMatrix).thatIsNamed("parent Matrix").isNotNull();
+		
+		GlobalValidator
+		.assertThat(p1BasedStartIndex)
+		.thatIsNamed("start index")
+		.isNotBiggerThan(parentMatrix.getElementCount());
+		
+		this.parentMatrix = parentMatrix;
+		nextElement1BasedIndex = p1BasedStartIndex;
+	}
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public CopyableIterator<E> getCopy() {
+		return forMatrixAnd1BasedStartIndex(parentMatrix, nextElement1BasedIndex);
 	}
 	
 	//method
