3b78c2d9257b3f69e0b179bcd1ab2b993f3e66c3
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-13 22:28:41 +0100

    created ISequencePatternBuilder

---
+++ Source/ch/nolix/coreapi/containerapi/sequencesearchapi/ISequencePatternBuilder.java
@@ -0,0 +1,44 @@
+//package declaration
+package ch.nolix.coreapi.containerapi.sequencesearchapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IElementTakerBooleanGetter;
+import ch.nolix.coreapi.functionapi.requestuniversalapi.EmptinessRequestable;
+import ch.nolix.coreapi.functionapi.skilluniversalapi.IBuilder;
+
+//interface
+/**
+ * @author Silvan Wyss
+ * @date 2023-02-13
+ * @param <E> is the type of the elements of the searched-for sequences of a {@link ISequencePatternBuilder}.
+ */
+public interface ISequencePatternBuilder<E> extends EmptinessRequestable, IBuilder<ISequencePattern<E>> {
+	
+	//method declaration
+	/**
+	 * Adds a blank condition for the next element of the searched-for sequences to
+	 * the current {@link ISequencePatternBuilder}.
+	 * 
+	 * @return the current {@link ISequencePatternBuilder}.
+	 */
+	ISequencePatternBuilder<E> addBlankForNext();
+	
+	//method declaration
+	/**
+	 * Adds the given condition for the next element of the searched-for sequences to
+	 * the current {@link ISequencePatternBuilder}.
+	 * 
+	 * @param condition
+	 * @return the current {@link ISequencePatternBuilder}.
+	 * @throws RuntimeException if the given condition is null.
+	 */
+	ISequencePatternBuilder<E> addConditionForNext(IElementTakerBooleanGetter<E> condition);
+	
+	//method declaration
+	/**
+	 * @param count
+	 * @return a new {@link INextMediator} for the current {@link ISequencePatternBuilder} with the given count.
+	 * @throws RuntimeException if the given count is not positive
+	 */
+	INextMediator<E> forNext(int count);
+}
