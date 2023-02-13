eec63d421c73c2798a752d584cb0d37d350b312f
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-13 21:41:03 +0100

    created sequencesearchapi package

---
+++ Source/ch/nolix/coreapi/containerapi/sequencesearchapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.containerapi.sequencesearchapi} package declares
+ * types to search for specific sequences in {@link IContainer}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-13
+ */
+package ch.nolix.coreapi.containerapi.sequencesearchapi;
