63a01220270f7d2aee0315ae4fd70f93b8b2d8d7
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-15 22:14:14 +0100

    made ContainerViewIterator a CopyableIterator

--- Source/ch/nolix/core/container/base/ContainerViewIterator.java
+++ Source/ch/nolix/core/container/base/ContainerViewIterator.java
@@ -10,6 +10,8 @@ import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentIsNullExcepti
 import ch.nolix.core.errorcontrol.invalidargumentexception.NonPositiveArgumentException;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.core.programatom.name.LowerCaseCatalogue;
+import ch.nolix.coreapi.containerapi.baseapi.CopyableIterator;
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 
 //class
 /**
@@ -17,41 +19,59 @@ import ch.nolix.core.programatom.name.LowerCaseCatalogue;
  * @date 2017-08-27
  * @param <E> is the type of the elements of a {@link ContainerViewIterator}.
  */
-final class ContainerViewIterator<E> implements Iterator<E> {
+final class ContainerViewIterator<E> implements CopyableIterator<E> {
 	
-	//attributes
+	//attribute
+	private final IContainer<E> parentContainer;
+	
+	//attribute
 	private final int endIndex;
+	
+	//attribute
 	private final Iterator<E> iterator;
+	
+	//attribute
 	private int currentIndex;
 
 	//constructor
 	/**
-	 * Creates a new {@link ContainerViewIterator} with the given container, startIndex and endIndex.
+	 * Creates a new {@link ContainerViewIterator} for the given parentContainer, startIndex and endIndex.
 	 * 
-	 * @param container
+	 * @param parentContainer
 	 * @param startIndex
 	 * @param endIndex
-	 * @throws ArgumentIsNullException if the given container is null.
+	 * @throws ArgumentIsNullException if the given parentContainer is null.
 	 * @throws NonPositiveArgumentException if the given startIndex is not positive.
 	 * @throws NonPositiveArgumentException if the given endIndex is not positive.
 	 */
-	public ContainerViewIterator(final Iterable<E> container, final int startIndex, final int endIndex) {
+	public ContainerViewIterator(final IContainer<E> parentContainer, final int startIndex, final int endIndex) {
 		
-		GlobalValidator.assertThat(container).thatIsNamed(LowerCaseCatalogue.CONTAINER).isNotNull();
+		GlobalValidator.assertThat(parentContainer).thatIsNamed("parent container").isNotNull();
 		GlobalValidator.assertThat(startIndex).thatIsNamed(LowerCaseCatalogue.START_INDEX).isPositive();
 		GlobalValidator.assertThat(endIndex).thatIsNamed(LowerCaseCatalogue.END_INDEX).isPositive();
 		GlobalValidator.assertThat(endIndex).thatIsNamed(LowerCaseCatalogue.END_INDEX).isBiggerThanOrEquals(startIndex);
+		
+		this.parentContainer = parentContainer;
 		
 		this.endIndex = endIndex;
 		
 		currentIndex = startIndex;
 		
-		iterator = container.iterator();
+		iterator = parentContainer.iterator();
 		for (var i = 1; i < startIndex; i++) {
 			iterator.next();
 		}
 	}
-
+	
+	//method
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public CopyableIterator<E> getCopy() {
+		return new ContainerViewIterator<>(parentContainer, currentIndex, endIndex);
+	}
+	
 	//method
 	/**
 	 * @return true if the current {@link ContainerViewIterator} has a next element.
