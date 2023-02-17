ed128370d35dd4c7bb05f5930e9a5d2c2e9360ef
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-17 13:23:33 +0100

    created containervalidator package

---
+++ Source/ch/nolix/core/independent/containervalidator/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.independent.containervalidator} package defines independent validators for containers.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-17
+ */
+package ch.nolix.core.independent.containervalidator;
