5497c5a2663bc0d4b37dc3f7331bcd24c10457b3
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-17 23:21:09 +0100

    created CompletenessRequestable

---
+++ Source/ch/nolix/coreapi/functionapi/requestuniversalapi/CompletenessRequestable.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.coreapi.functionapi.requestuniversalapi;
+
+//own imports
+import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
+
+//interface
+/**
+ * A {@link CompletenessRequestable} can be asked if it is complete.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-17
+ */
+@AllowDefaultMethodsAsDesignPattern
+public interface CompletenessRequestable {
+	
+	//method declaration
+	/**
+	 * @return true if the current {@link CompletenessRequestable} is complete, false otherwise.
+	 */
+	boolean isComplete();
+	
+	//method
+	/**
+	 * @return true if the current {@link CompletenessRequestable} is not complete, false otherwise.
+	 */
+	default boolean isIncomplete() {
+		return !isComplete();
+	}
+}
