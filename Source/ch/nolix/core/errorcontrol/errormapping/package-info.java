5387b16bc31d1a9ae0a2a6913e8bca91a939d4b1
Silvan Wyss <silvan.wyss@gmail.com>
2023-02-24 12:07:14 +0100

    created errormapping package

---
+++ Source/ch/nolix/core/errorcontrol/errormapping/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.errorcontrol.errormapping} package defines mappers for errors.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-24
+ */
+package ch.nolix.core.errorcontrol.errormapping;
