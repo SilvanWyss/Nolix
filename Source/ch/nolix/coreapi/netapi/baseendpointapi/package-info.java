d055abb009a63b50fec3ff511ff07b8bea48cfe2
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-08 21:55:03 +0100

    created baseendpointapi package

---
+++ Source/ch/nolix/coreapi/netapi/baseendpointapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.netapi.baseendpointapi} package declares a base end point.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-03-08
+ */
+package ch.nolix.coreapi.netapi.baseendpointapi;
