ade18b38d184ca2bda954960c73087056f67527c
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-10 14:07:28 +0100

    created netproperty package

---
+++ Source/ch/nolix/coreapi/netapi/netproperty/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.netapi.netproperty} package defines properties for net.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-03-10
+ */
+package ch.nolix.coreapi.netapi.netproperty;
