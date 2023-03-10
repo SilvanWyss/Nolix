83e85e9a5d212dd9cb85ca361102281944128cf1
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-10 14:42:00 +0100

    created endpoint2api package

---
+++ Source/ch/nolix/coreapi/netapi/endpoint2api/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.netapi.endpoint2api} package declares an end point that can send requests.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-03-10
+ */
+package ch.nolix.coreapi.netapi.endpoint2api;
