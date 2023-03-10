8342f6ac8c7133381ea59c5a9fb7c9c371b62990
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-10 13:42:07 +0100

    created endpointapi package

---
+++ Source/ch/nolix/coreapi/netapi/endpointapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.netapi.endpointapi} package defines
+ * an end point that can send and receive {@link java.lang.String} messages.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-03-10
+ */
+package ch.nolix.coreapi.netapi.endpointapi;
