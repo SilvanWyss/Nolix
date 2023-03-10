0f80a8c34d9546267aac0eb33f49a305eff83c89
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-10 14:13:04 +0100

    created IEndPoint

---
+++ Source/ch/nolix/coreapi/netapi/endpointapi/IEndPoint.java
@@ -0,0 +1,19 @@
+//package declaration
+package ch.nolix.coreapi.netapi.endpointapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IElementTaker;
+import ch.nolix.coreapi.netapi.baseendpointapi.IBaseEndPoint;
+
+//interface
+public interface IEndPoint extends IBaseEndPoint {
+	
+	//method declaration
+	boolean hasReceiver();
+	
+	//method declaration
+	void sendMessage(String message);
+	
+	//method declaration
+	void setReceiver(final IElementTaker<String> receiver);
+}
