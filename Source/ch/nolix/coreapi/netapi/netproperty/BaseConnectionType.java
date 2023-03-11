be99d721241375a2e50f42e6b58334c81a844663
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-11 16:06:50 +0100

    created BaseConnectionType enum

---
+++ Source/ch/nolix/coreapi/netapi/netproperty/BaseConnectionType.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.coreapi.netapi.netproperty;
+
+//enum
+public enum BaseConnectionType {
+	LOCAL,
+	NET
+}
--- Source/ch/nolix/coreapi/netapi/netproperty/ConnectionType.java
+++ Source/ch/nolix/coreapi/netapi/netproperty/ConnectionType.java
@@ -3,7 +3,20 @@ package ch.nolix.coreapi.netapi.netproperty;
 
 //enum
 public enum ConnectionType {
-	LOCAL,
-	SOCKET,
-	WEB_SOCKET
+	LOCAL(BaseConnectionType.LOCAL),
+	SOCKET(BaseConnectionType.NET),
+	WEB_SOCKET(BaseConnectionType.NET);
+	
+	//attribute
+	private final BaseConnectionType baseType;
+	
+	//constructor
+	ConnectionType(final BaseConnectionType baseType) {
+		this.baseType = baseType;
+	}
+	
+	//method
+	public final BaseConnectionType getBaseType() {
+		return baseType;
+	}
 }
