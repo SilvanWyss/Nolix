14067220042684ce2aa509bf86d94f1bb05e284c
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-13 22:15:07 +0100

    made EndPoint a IEndPoint

--- Source/ch/nolix/core/net/endpoint2/EndPoint.java
+++ Source/ch/nolix/core/net/endpoint2/EndPoint.java
@@ -3,18 +3,18 @@ package ch.nolix.core.net.endpoint2;
 
 //own imports
 import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentDoesNotHaveAttributeException;
-import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentIsNullException;
 import ch.nolix.core.errorcontrol.invalidargumentexception.ClosedArgumentException;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.core.net.baseendpoint.BaseEndPoint;
 import ch.nolix.coreapi.functionapi.genericfunctionapi.IElementTakerElementGetter;
+import ch.nolix.coreapi.netapi.endpoint2api.IEndPoint;
 
 //class
 /**
  * @author Silvan Wyss
  * @date 2017-05-21
  */
-public abstract class EndPoint extends BaseEndPoint {
+public abstract class EndPoint extends BaseEndPoint implements IEndPoint {
 	
 	//constant
 	private static final long REPLIER_GETTING_DELAY_IN_MILLISECONDS = 5000;
@@ -22,13 +22,11 @@ public abstract class EndPoint extends BaseEndPoint {
 	//optional attribute
 	private IElementTakerElementGetter<String, String> replier;
 	
-	//method declaration
-	public abstract String getReplyForRequest(final String request);
-	
 	//method
 	/**
-	 * @return true if this end point has a replier.
+	 * {@inheritDoc}
 	 */
+	@Override
 	public boolean hasReplier() {
 		return (replier != null);
 	}
@@ -42,11 +40,9 @@ public abstract class EndPoint extends BaseEndPoint {
 	
 	//method
 	/**
-	 * Sets the replier of this end point.
-	 * 
-	 * @param replier
-	 * @throws ArgumentIsNullException if the given replier is null.
+	 * {@inheritDoc}
 	 */
+	@Override
 	public void setReplier(final IElementTakerElementGetter<String, String> replier) {
 		
 		//Asserts that the given replier is not null.
