40e2392a7096b5eb2a3b4d07bb870fad2ca57e8d
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-17 21:29:13 +0100

    deleted controlleruniversalapi package

--- Source/ch/nolix/core/net/controlleruniversalapi/IDataProviderController.java
+++
@@ -1,9 +0,0 @@
-//package declaration
-package ch.nolix.core.net.controlleruniversalapi;
-
-//interface
-/**
- * @author Silvan Wyss
- * @date 2017-10-14
- */
-public interface IDataProviderController extends IController, IDataProvider {}
