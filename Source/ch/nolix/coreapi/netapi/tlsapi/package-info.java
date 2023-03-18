c8851677774f6c85be909b58f95723a1c5317bb8
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-18 21:06:24 +0100

    created tlsapi package

---
+++ Source/ch/nolix/coreapi/netapi/tlsapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.netapi.tlsapi} package declares types for TLS.
+ * TLS (transport layer security) is a protocol.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-03-18
+ */
+package ch.nolix.coreapi.netapi.tlsapi;
