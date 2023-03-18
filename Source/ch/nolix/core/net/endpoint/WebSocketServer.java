b044fa5e83b85a21e16321ebf7580d5b69cec46c
Silvan Wyss <silvan.wyss@gmail.com>
2023-03-18 21:33:14 +0100

    created WebSocketServer

---
+++ Source/ch/nolix/core/net/endpoint/WebSocketServer.java
@@ -0,0 +1,20 @@
+//package declaration
+package ch.nolix.core.net.endpoint;
+
+//own imports
+import ch.nolix.coreapi.netapi.tlsapi.ISSLCertificate;
+
+//class
+public final class WebSocketServer extends BaseServer {
+	
+	//constructor
+	public WebSocketServer(final int port, final ISSLCertificate paramSSLCertificate) {
+		//TODO: Implement.
+	}
+	
+	//method
+	@Override
+	public void noteClose() {
+		//TODO: Implement.
+	}
+}
