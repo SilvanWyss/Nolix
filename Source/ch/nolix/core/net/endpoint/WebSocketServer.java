d13dc32cd2e021fa8ec0965089212cfc1c6b1ddf
Silvan Wyss <silvan.wyss@gmail.com>
2023-05-06 22:28:05 +0200

    deleted WebSocketServerInternalServer

--- Source/ch/nolix/core/net/endpoint/WebSocketServer.java
+++ Source/ch/nolix/core/net/endpoint/WebSocketServer.java
@@ -2,28 +2,19 @@
 package ch.nolix.core.net.endpoint;
 
 //own imports
-import ch.nolix.core.errorcontrol.exception.WrapperException;
 import ch.nolix.coreapi.netapi.tlsapi.ISSLCertificate;
 
 //class
 public final class WebSocketServer extends BaseServer {
 	
-	//attribute
-	private final WebSocketServerInternalServer internalWebSocketServer;
-	
 	//constructor
 	public WebSocketServer(final int port, final ISSLCertificate paramSSLCertificate) {
-		internalWebSocketServer = new WebSocketServerInternalServer(this, port, paramSSLCertificate);
+		//TODO: Implement.
 	}
 	
 	//method
 	@Override
 	public void noteClose() {
-		try {
-			internalWebSocketServer.stop();
-		}
-		catch (final InterruptedException interruptedException) { //NOSONAR: An Exception will be re-thrown.
-			throw WrapperException.forError(interruptedException);
-		}
+		//TODO: Implement.
 	}
 }
