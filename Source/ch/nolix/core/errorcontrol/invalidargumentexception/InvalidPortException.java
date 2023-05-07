9067c01c64672b56bbae6f1ae9e7999b28df485a
Silvan Wyss <silvan.wyss@gmail.com>
2023-05-07 11:47:43 +0200

    created InvalidPortException

---
+++ Source/ch/nolix/core/errorcontrol/invalidargumentexception/InvalidPortException.java
@@ -0,0 +1,35 @@
+//package declaration
+package ch.nolix.core.errorcontrol.invalidargumentexception;
+
+//own imports
+import ch.nolix.core.programatom.name.LowerCaseCatalogue;
+
+//class
+/**
+* A {@link InvalidPortException} is a {@link InvalidArgumentException} that
+* is supposed to be thrown when a given port is not valid.
+* 
+* @author Silvan Wyss
+* @date 2021-07-16
+*/
+@SuppressWarnings("serial")
+public final class InvalidPortException extends InvalidArgumentException {
+	
+	//constant
+	private static final String ARGUMENT_NAME = LowerCaseCatalogue.PORT;
+	
+	//constant
+	private static final String ERROR_PREDICATE = "is not valid";
+	
+	//static method
+	public static InvalidPortException forPort(final long port) {
+		return new InvalidPortException(port);
+	}
+	
+	//constructor
+	private InvalidPortException(final long port) {
+		
+		//Calls constructor of the base class.
+		super(ARGUMENT_NAME, port, ERROR_PREDICATE);
+	}
+}
