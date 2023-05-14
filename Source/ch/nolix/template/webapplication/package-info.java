86a3d712e4ec87696054839d45615efc68bca2d1
Nimeon <silvan.wyss@gmail.com>
2023-05-14 13:48:21 +0200

    created webapplication package

---
+++ Source/ch/nolix/template/webapplication/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.template.webapplication} package defines templates for web applications.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-05-14
+ */
+package ch.nolix.template.webapplication;
