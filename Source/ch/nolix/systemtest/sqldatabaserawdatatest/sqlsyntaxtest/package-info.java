6a7e376f616f00591138c38ffbc57c65d126d49c
Nimeon <silvan.wyss@gmail.com>
2023-05-18 14:38:05 +0200

    created sqlsyntaxtest package

---
+++ Source/ch/nolix/systemtest/sqldatabaserawdatatest/sqlsyntaxtest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.sqldatabaserawdatatest.sqlsyntaxtest} package defines
+ * tests for the {@link ch.nolix.system.sqldatabaserawdata.sqlsyntax} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-05-18
+ */
+package ch.nolix.systemtest.sqldatabaserawdatatest.sqlsyntaxtest;
