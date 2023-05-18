49ddba923300df268f32dae2404f53f9f6ff98bd
Nimeon <silvan.wyss@gmail.com>
2023-05-18 14:36:37 +0200

    created sqldatabaserawdatatest package

---
+++ Source/ch/nolix/systemtest/sqldatabaserawdatatest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.sqldatabaserawdatatest} package defines
+ * tests for the {@link ch.nolix.system.sqldatabaserawdata} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-05-18
+ */
+package ch.nolix.systemtest.sqldatabaserawdatatest;
