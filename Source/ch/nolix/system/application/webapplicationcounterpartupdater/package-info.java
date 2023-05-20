fa6f14a81cb175faac8a037542258bc7d26c0b74
Nimeon <silvan.wyss@gmail.com>
2023-05-20 10:49:18 +0200

    created webapplicationcounterpartupdater package

---
+++ Source/ch/nolix/system/application/webapplicationcounterpartupdater/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.application.webapplicationcounterpartupdater} package defines
+ * counterpart updater for web {@link ch.nolix.system.application.main.Application}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-05-20
+ */
+package ch.nolix.system.application.webapplicationcounterpartupdater;
