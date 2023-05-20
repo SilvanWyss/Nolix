271db26fcdc5972574dc6f5090eca0829894e4a9
Nimeon <silvan.wyss@gmail.com>
2023-05-20 10:29:26 +0200

    renamed nolix.js

--- Source/ch/nolix/system/application/mainresource/ResourcePathCatalogue.java
+++ Source/ch/nolix/system/application/mainresource/ResourcePathCatalogue.java
@@ -5,7 +5,7 @@ package ch.nolix.system.application.mainresource;
 public final class ResourcePathCatalogue {
 	
 	//constants
-	public static final String NOLIX_JS = "ch/nolix/system/application/mainresource/Nolix.js";
+	public static final String NOLIX_JS = "ch/nolix/system/application/mainresource/nolix.js";
 	public static final String REQUIRE_JS = "ch/nolix/system/application/mainresource/require_js.js";
 	
 	//constructor
