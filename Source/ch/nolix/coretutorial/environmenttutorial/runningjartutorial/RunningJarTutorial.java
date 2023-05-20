8afa554a3785ba08ba6e382a81f1295664fd2d97
Nimeon <silvan.wyss@gmail.com>
2023-05-20 10:27:58 +0200

    created runningjartutorialresource package

--- Source/ch/nolix/coretutorial/environmenttutorial/runningjartutorial/RunningJarTutorial.java
+++ Source/ch/nolix/coretutorial/environmenttutorial/runningjartutorial/RunningJarTutorial.java
@@ -1,14 +1,15 @@
 package ch.nolix.coretutorial.environmenttutorial.runningjartutorial;
 
-//own imports
 import ch.nolix.core.environment.runningjar.RunningJar;
 
 public final class RunningJarTutorial {
 	
 	public static void main(String[] args) {
-		System.out.print(
-			RunningJar.getResource("ch/nolix/commonTutorial/RunningJarTutorial/resource/Willkommen_und_Abschied.txt")
-		);
+		
+		final var resourcePath =
+		"ch/nolix/coretutorial/environmenttutorial/runningjartutorialresource/willkommen_und_abschied.txt";
+		
+		System.out.print(RunningJar.getResource(resourcePath));
 	}
 	
 	private RunningJarTutorial() {}
