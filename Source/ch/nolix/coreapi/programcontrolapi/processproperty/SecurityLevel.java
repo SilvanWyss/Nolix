a11a5b4cd98ca51c8bbfe2e5c043ff88fd004eb2
Nimeon <silvan.wyss@gmail.com>
2023-05-26 11:41:00 +0200

    created SecurityLevel enum

---
+++ Source/ch/nolix/coreapi/programcontrolapi/processproperty/SecurityLevel.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.coreapi.programcontrolapi.processproperty;
+
+//enum
+/**
+ * A {@link SecurityLevel} defines if a process is unsecure or secure.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-05-26
+ */
+public enum SecurityLevel {
+	UNSECURE,
+	SECURE
+}
