1114933289e95e8e986c9a09f6bbf1672f237c21
Nimeon <silvan.wyss@gmail.com>
2023-05-27 20:33:23 +0200

    created webgui package

---
+++ Source/ch/nolix/template/webgui/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.template.webgui} package defines
+ * templates for {@link ch.nolix.systemapi.webguiapi.mainapi.IWebGUI}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-05-27
+ */
+package ch.nolix.template.webgui;
