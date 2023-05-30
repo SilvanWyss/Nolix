4b895adc754731bc479f30eb8c911d2bf2cd5930
Nimeon <silvan.wyss@gmail.com>
2023-05-30 18:09:59 +0200

    created voidobject package

---
+++ Source/ch/nolix/core/programatom/voidobject/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.programatom.voidobject} package defines a void object.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-05-30
+ */
+package ch.nolix.core.programatom.voidobject;
