970a749db8d6491c269cd7e2e0dcc63f66f2fa06
Nimeon <silvan.wyss@gmail.com>
2023-06-11 15:20:17 +0200

    adjusted StringCatalogue

--- Source/ch/nolix/core/commontype/commontypeconstant/StringCatalogue.java
+++ Source/ch/nolix/core/commontype/commontypeconstant/StringCatalogue.java
@@ -10,21 +10,49 @@ package ch.nolix.core.commontype.commontypeconstant;
  */
 public final class StringCatalogue {
 	
-	//constants
+	//constant
 	public static final String DEFAULT_STRING = "Default";
+	
+	//constant
 	public static final String DOT = ".";
+	
+	//constant
 	public static final String ELLIPSIS = "...";
+	
+	//constant
 	public static final String EMPTY_STRING = "";
+	
+	//constant
 	public static final String FALSE_HEADER = "false";
+	
+	//constant
 	public static final String HEXADECIMAL_PREFIX = "0x";
+	
+	//constant
 	public static final String INFINITY = "\u221E";
+	
+	//constant
 	public static final String MINUS = "-";
+	
+	//constant
 	public static final String NULL_HEADER = "null";
+	
+	//constant
 	public static final String QUESTION_MARK = "?";
+	
+	//constant
 	public static final String SPACE = " ";
+	
+	//constant
 	public static final String TABULATOR = "\t";
+	
+	//constant
 	public static final String THIN_CROSS = "\u2715";
+	
+	//constant
 	public static final String TRUE_HEADER = "true";
+	
+	//constant
 	public static final String UNDERSCORE = "_";
 	
 	//constructor
