4d9418f638102ef4216c708f265a161e162eff81
Nimeon <silvan.wyss@gmail.com>
2023-06-16 15:01:06 +0200

    created IHorizontalStack

---
+++ Source/ch/nolix/systemapi/webguiapi/linearcontainerapi/IHorizontalStack.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.linearcontainerapi;
+
+//own imports
+import ch.nolix.systemapi.guiapi.structureproperty.VerticalContentAlignment;
+
+//interface
+public interface IHorizontalStack extends ILinearContainer<IHorizontalStack, IHorizontalStackStyle>{
+	
+	//method declaration
+	VerticalContentAlignment getContentAlignment();
+	
+	//method declaration
+	IHorizontalStack setContentAlignment(VerticalContentAlignment contentAlignment);
+}
---
+++ Source/ch/nolix/systemapi/webguiapi/linearcontainerapi/IHorizontalStackStyle.java
@@ -0,0 +1,5 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.linearcontainerapi;
+
+//interface
+public interface IHorizontalStackStyle extends ILinearContainerStyle<IHorizontalStackStyle> {}
