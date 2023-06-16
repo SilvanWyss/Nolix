6a468b42fc7ef75c2e16897e0a50b76e8302903c
Nimeon <silvan.wyss@gmail.com>
2023-06-16 23:45:47 +0200

    fixed EnterValueDialogFactory

--- Source/ch/nolix/system/webgui/dialog/EnterValueDialogFactory.java
+++ Source/ch/nolix/system/webgui/dialog/EnterValueDialogFactory.java
@@ -7,15 +7,15 @@ import ch.nolix.coreapi.functionapi.genericfunctionapi.IElementTaker;
 import ch.nolix.system.webgui.control.Button;
 import ch.nolix.system.webgui.control.Label;
 import ch.nolix.system.webgui.control.Textbox;
+import ch.nolix.system.webgui.control.ValidationLabel;
 import ch.nolix.system.webgui.linearcontainer.HorizontalStack;
 import ch.nolix.system.webgui.linearcontainer.VerticalStack;
 import ch.nolix.system.webgui.main.Layer;
 import ch.nolix.systemapi.webguiapi.containerapi.ContainerRole;
 import ch.nolix.systemapi.webguiapi.controlapi.ButtonRole;
 import ch.nolix.systemapi.webguiapi.controlapi.IButton;
-import ch.nolix.systemapi.webguiapi.controlapi.ILabel;
 import ch.nolix.systemapi.webguiapi.controlapi.ITextbox;
-import ch.nolix.systemapi.webguiapi.controlapi.LabelRole;
+import ch.nolix.systemapi.webguiapi.mainapi.ILayer;
 import ch.nolix.systemapi.webguiapi.mainapi.LayerRole;
 
 //class
@@ -31,7 +31,6 @@ public final class EnterValueDialogFactory {
 		GlobalValidator.assertThat(valueTaker).thatIsNamed("value taker").isNotNull();
 		
 		final var valueTextbox = new Textbox().setText(originalValue);
-		final var errorLabel = new Label().setRole(LabelRole.ERROR_LABEL);
 		
 		return
 		new Layer()
@@ -42,47 +41,39 @@ public final class EnterValueDialogFactory {
 			.addControl(
 				new Label()
 				.setText(text),
-				errorLabel,
+				new ValidationLabel(),
 				valueTextbox,
 				new HorizontalStack()
 				.addControl(
 					new Button()
 					.setRole(ButtonRole.CANCEL_BUTTON)
 					.setText("Cancel")
-					.setLeftMouseButtonPressAction(b -> b.getOriParentLayer().removeSelfFromGUI()),
+					.setLeftMouseButtonPressAction(b -> cancel(b.getOriParentLayer())),
 					new Button()
 					.setRole(ButtonRole.CONFIRM_BUTTON)
 					.setText("Ok")
-					.setLeftMouseButtonPressAction(b -> confirmNewValue(b, valueTextbox, errorLabel, valueTaker))
+					.setLeftMouseButtonPressAction(b -> confirmNewValue(b, valueTextbox, valueTaker))
 				)
 			)
 		);
 	}
 	
 	//method
+	private void cancel(final ILayer<?> dialogLayer) {
+		dialogLayer.removeSelfFromGUI();
+	}
+	
+	//method
 	private void confirmNewValue(
 		final IButton<?, ?> confirmButton,
 		final ITextbox valueTextbox,
-		final ILabel<?, ?> errorLabel,
 		final IElementTaker<String> valueTaker
 	) {
 		
 		final var newValue = valueTextbox.getText();
-		
-		try {
+
+		valueTaker.run(newValue);
 			
-			valueTaker.run(newValue);
-			
-			confirmButton.getOriParentLayer().removeSelfFromGUI();
-		} catch (final Exception exception) {
-			
-			final var message = exception.getMessage();
-			
-			if (message != null) {
-				errorLabel.setText(message);
-			} else {
-				errorLabel.setText("An error occured.");
-			}
-		}
+		confirmButton.getOriParentLayer().removeSelfFromGUI();
 	}
 }
