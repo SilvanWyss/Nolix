73a8c724594f470bf5cfdcedde3f4e75b05c8268
Nimeon <silvan.wyss@gmail.com>
2023-06-25 23:08:05 +0200

    created HorizontalContentAlignment enum

---
+++ Source/ch/nolix/systemapi/guiapi/structureproperty/HorizontalContentAlignment.java
@@ -0,0 +1,20 @@
+//package decalration
+package ch.nolix.systemapi.guiapi.structureproperty;
+
+//own imports
+import ch.nolix.coreapi.documentapi.nodeapi.INode;
+
+//enum
+public enum HorizontalContentAlignment {
+	LEFT,
+	CENTER,
+	RIGHT;
+	
+	//static method
+	public static HorizontalContentAlignment fromSpecification(final INode<?> specification) {
+		
+		final var horizontalContentAlignmentString = specification.getSingleChildNodeHeader();
+		
+		return valueOf(horizontalContentAlignmentString);
+	}
+}
