7312d9fcddfec253bfe3d36156f5cf5bf9b71615
Nimeon <silvan.wyss@gmail.com>
2023-06-26 10:31:32 +0200

    edited ExtendedControlCSSRuleBuilder

--- Source/ch/nolix/system/webgui/linearcontainer/VerticalStackCSSRuleBuilder.java
+++ Source/ch/nolix/system/webgui/linearcontainer/VerticalStackCSSRuleBuilder.java
@@ -1,14 +1,17 @@
 //package declaration
 package ch.nolix.system.webgui.linearcontainer;
 
+//own imports
 import ch.nolix.core.container.linkedlist.LinkedList;
 import ch.nolix.core.web.css.CSSProperty;
 import ch.nolix.core.web.css.CSSRule;
+import ch.nolix.coreapi.webapi.cssapi.CSSAlignItemsCatalogue;
 import ch.nolix.coreapi.webapi.cssapi.CSSPropertyNameCatalogue;
 import ch.nolix.coreapi.webapi.cssapi.CSSUnitCatalogue;
 import ch.nolix.coreapi.webapi.cssapi.ICSSProperty;
 import ch.nolix.coreapi.webapi.cssapi.ICSSRule;
 import ch.nolix.system.webgui.controlstyle.ExtendedControlCSSRuleBuilder;
+import ch.nolix.systemapi.guiapi.structureproperty.HorizontalContentAlignment;
 import ch.nolix.systemapi.webguiapi.linearcontainerapi.IVerticalStack;
 import ch.nolix.systemapi.webguiapi.linearcontainerapi.IVerticalStackStyle;
 import ch.nolix.systemapi.webguiapi.mainapi.ControlState;
@@ -59,7 +62,11 @@ extends ExtendedControlCSSRuleBuilder<IVerticalStack, IVerticalStackStyle> {
 		final IVerticalStack control,
 		final LinkedList<CSSProperty> list
 	) {
-		list.addAtEnd(CSSProperty.withNameAndValue("overflow", "auto"));
+		list.addAtEnd(
+			CSSProperty.withNameAndValue("display", "flex"),
+			CSSProperty.withNameAndValue("flex-direction", "column"),
+			createCSSPropertyForContentAlignmentOfControl(control)
+		);
 	}
 	
 	//method
@@ -70,5 +77,26 @@ extends ExtendedControlCSSRuleBuilder<IVerticalStack, IVerticalStackStyle> {
 		final LinkedList<ICSSProperty> list
 	) {
 		//Does nothing.
+	}
+	
+	//method
+	private CSSProperty createCSSPropertyForContentAlignment(final HorizontalContentAlignment contentAlignment) {
+		return
+		switch (contentAlignment) {
+			case LEFT ->
+				CSSProperty.withNameAndValue(CSSPropertyNameCatalogue.ALIGN_ITEMS, CSSAlignItemsCatalogue.START);
+			case CENTER ->
+				CSSProperty.withNameAndValue(CSSPropertyNameCatalogue.ALIGN_ITEMS, CSSAlignItemsCatalogue.CENTER);
+			case RIGHT ->
+				CSSProperty.withNameAndValue(CSSPropertyNameCatalogue.ALIGN_ITEMS, CSSAlignItemsCatalogue.END);
+		};
+	}
+	
+	//method
+	private CSSProperty createCSSPropertyForContentAlignmentOfControl(final IVerticalStack control) {
+		
+		final var contentAlignment = control.getContentAlignment();
+		
+		return createCSSPropertyForContentAlignment(contentAlignment);
 	}
 }
