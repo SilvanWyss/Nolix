73a69b6aaef124f29326c54bbdd9053456bd9032
Nimeon <silvan.wyss@gmail.com>
2023-06-26 22:31:20 +0200

    created webproperty package

---
+++ Source/ch/nolix/coreapi/webapi/webproperty/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.webapi.webproperty} package defines properties for web.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-06-26
+ */
+package ch.nolix.coreapi.webapi.webproperty;
