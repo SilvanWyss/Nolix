02b298a6efe743ffa5bcdce6217993baadecf1e5
Nimeon <silvan.wyss@gmail.com>
2023-06-26 12:24:01 +0200

    created resource package

---
+++ Source/ch/nolix/business/serverdashboardapplication/resource/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.business.serverdashboardapplication.resource} package contains
+ * the resources for the server dashboard application.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-06-26
+ */
+package ch.nolix.business.serverdashboardapplication.resource;
