1d78d0aac0df84555884499a4791c83fdb3f16e8
Nimeon <silvan.wyss@gmail.com>
2023-07-05 23:37:17 +0200

    extended ItemMenuTest

--- Source/ch/nolix/systemtest/webguitest/itemmenutest/ItemMenuTest.java
+++ Source/ch/nolix/systemtest/webguitest/itemmenutest/ItemMenuTest.java
@@ -2,6 +2,8 @@
 package ch.nolix.systemtest.webguitest.itemmenutest;
 
 //own imports
+import ch.nolix.core.testing.basetest.TestCase;
+import ch.nolix.system.webgui.itemmenu.ItemMenuItem;
 import ch.nolix.systemapi.webguiapi.itemmenuapi.IItemMenu;
 import ch.nolix.systemapi.webguiapi.itemmenuapi.IItemMenuStyle;
 import ch.nolix.systemtest.webguitest.controltest.ControlTest;
@@ -11,4 +13,53 @@ public abstract class ItemMenuTest<
 	IM extends IItemMenu<IM, IMS>,
 	IMS extends IItemMenuStyle<IMS>
 >
-extends ControlTest<IM> {}
+extends ControlTest<IM> {
+	
+	//method
+	@TestCase
+	public final void testCase_addItemWithIdAndText() {
+		
+		//setup
+		final var testUnit = createTestUnit();
+		
+		//setup verification
+		expect(testUnit.isEmpty());
+		
+		//execution
+		testUnit
+		.addItemWithIdAndText("my_id_1", "my_text_1")
+		.addItemWithIdAndText("my_id_2", "my_text_2")
+		.addItemWithIdAndText("my_id_3", "my_text_3");
+		
+		//verification
+		expect(testUnit.getOriItems()).containsExactlyEqualing(
+			ItemMenuItem.withIdAndText("my_id_1", "my_text_1"),
+			ItemMenuItem.withIdAndText("my_id_2", "my_text_2"),
+			ItemMenuItem.withIdAndText("my_id_3", "my_text_3")
+		);
+	}
+	
+	//method
+	@TestCase
+	public final void testCase_selectItemById_whenContainsItemWithGivenId() {
+		
+		//setup
+		final var item = ItemMenuItem.withIdAndText("my_id_2", "my_text_2");
+		final var testUnit =
+		createTestUnit()
+		.addItem(
+			ItemMenuItem.withIdAndText("my_id_1", "my_text_1"),
+			item,
+			ItemMenuItem.withIdAndText("my_id_3", "my_text_3")
+		);
+		
+		//setup verification
+		expectNot(item.isSelected());
+		
+		//execution
+		testUnit.selectItemById("my_id_2");
+		
+		//verification
+		expect(item.isSelected());
+	}
+}
