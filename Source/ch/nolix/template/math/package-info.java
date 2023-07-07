b07de1f32a0a2ae6277878e530059701316b049c
Nimeon <silvan.wyss@gmail.com>
2023-07-07 19:05:35 +0200

    created math package

---
+++ Source/ch/nolix/template/math/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.template.math} package defines mathematical objects.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-07
+ */
+package ch.nolix.template.math;
