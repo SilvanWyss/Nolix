c099001a93c2ed0b6ae2640a4a02314ac55b728a
Nimeon <silvan.wyss@gmail.com>
2023-07-07 21:20:13 +0200

    beautified NetEndPointProtocol

--- Source/ch/nolix/core/net/endpoint/NetEndPointProtocol.java
+++ Source/ch/nolix/core/net/endpoint/NetEndPointProtocol.java
@@ -3,17 +3,28 @@ package ch.nolix.core.net.endpoint;
 
 //class
 /**
+ * Of the {@link NetEndPointProtocol} an instance cannot be created.
+ * 
  * @author Silvan Wyss
  * @date 2019-05-01
  */
 final class NetEndPointProtocol {
 	
-	//constants
+	//constant
 	public static final char MESSAGE_PREFIX = 'M';
+	
+	//constant
 	public static final char TARGET_PREFIX = 'T';
+	
+	//constant
 	public static final char MAIN_TARGET_PREFIX = 'A';
+	
+	//constant
 	public static final char CLOSE_PREFIX = 'C';
 	
 	//constructor
+	/**
+	 * Prevents that an instance of the {@link NetEndPointProtocol} can be created.
+	 */
 	private NetEndPointProtocol() {}
 }
