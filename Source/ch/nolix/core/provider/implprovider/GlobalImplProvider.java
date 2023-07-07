da13de141d227842ff6dfe87c2f480be71d2a7ea
Nimeon <silvan.wyss@gmail.com>
2023-07-08 01:13:59 +0200

    edited GlobalImplProvider

--- Source/ch/nolix/core/provider/implprovider/GlobalImplProvider.java
+++ Source/ch/nolix/core/provider/implprovider/GlobalImplProvider.java
@@ -8,22 +8,22 @@ import ch.nolix.coreapi.providerapi.implproviderapi.IImplProviderMediator;
 //class
 public final class GlobalImplProvider {
 	
-	//static attribute
-	private static final ImplProvider implProvider = new ImplProvider();
+	//constant
+	private static final ImplProvider IMPL_PROVIDER = new ImplProvider();
 	
 	//static method
 	public static <O> ExtendedImplRegistratorMediator<O> forInterface(final Class<O> pInterface) {
-		return implProvider.forInterface(pInterface);
+		return IMPL_PROVIDER.forInterface(pInterface);
 	}
 	
 	//static method
 	public static IImplProvider getOriInstance() {
-		return implProvider;
+		return IMPL_PROVIDER;
 	}
 	
 	//static method
 	public static <IN> IImplProviderMediator<IN> ofInterface(final Class<IN> pInterface) {
-		return implProvider.ofInterface(pInterface);
+		return IMPL_PROVIDER.ofInterface(pInterface);
 	}
 	
 	//constructor
