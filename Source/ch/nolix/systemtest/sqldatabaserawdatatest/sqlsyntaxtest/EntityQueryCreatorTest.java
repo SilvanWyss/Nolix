8fc38d15cfcae0bff2759a7fc88e55382ed49115
Nimeon <silvan.wyss@gmail.com>
2023-07-07 12:21:30 +0200

    extended EntityQueryCreatorTest

--- Source/ch/nolix/systemtest/sqldatabaserawdatatest/sqlsyntaxtest/EntityQueryCreatorTest.java
+++ Source/ch/nolix/systemtest/sqldatabaserawdatatest/sqlsyntaxtest/EntityQueryCreatorTest.java
@@ -2,9 +2,14 @@
 package ch.nolix.systemtest.sqldatabaserawdatatest.sqlsyntaxtest;
 
 //own imports
+import ch.nolix.core.container.immutablelist.ImmutableList;
 import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.core.testing.test.Test;
+import ch.nolix.system.sqldatabaserawdata.schemainfo.ColumnInfo;
+import ch.nolix.system.sqldatabaserawdata.schemainfo.TableInfo;
 import ch.nolix.system.sqldatabaserawdata.sqlsyntax.EntityQueryCreator;
+import ch.nolix.systemapi.databaseapi.datatypeapi.DataType;
+import ch.nolix.systemapi.databaseapi.propertytypeapi.PropertyType;
 
 //class
 public final class EntityQueryCreatorTest extends Test {
@@ -22,6 +27,54 @@ public final class EntityQueryCreatorTest extends Test {
 		
 		//verification
 		final var expectedResult = "SELECT COUNT(MyColumn) FROM EMyTable WHERE MyColumn = 'my_value';";
+		expect(result).isEqualTo(expectedResult);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_createQueryToLoadEntitiesOfTable() {
+		
+		//setup
+		final var testUnit = new EntityQueryCreator();
+		final var tableInfo =
+		new TableInfo(
+			"ttttttt1",
+			"Cat",
+			ImmutableList.withElements(
+				new ColumnInfo("ccccccc1", "name", PropertyType.VALUE, DataType.STRING, 0),
+				new ColumnInfo("ccccccc2", "year_of_birth", PropertyType.VALUE, DataType.INTEGER_4BYTE, 0)
+			)
+		);
+		
+		//execution
+		final var result = testUnit.createQueryToLoadEntitiesOfTable(tableInfo);
+		
+		//verification
+		final var expectedResult = "SELECT Id, SaveStamp, name, year_of_birth FROM ECat;";
+		expect(result).isEqualTo(expectedResult);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_createQueryToLoadEntity() {
+		
+		//setup
+		final var testUnit = new EntityQueryCreator();
+		final var tableInfo =
+		new TableInfo(
+			"ttttttt1",
+			"Cat",
+			ImmutableList.withElements(
+				new ColumnInfo("ccccccc1", "name", PropertyType.VALUE, DataType.STRING, 0),
+				new ColumnInfo("ccccccc2", "year_of_birth", PropertyType.VALUE, DataType.INTEGER_4BYTE, 0)
+			)
+		);
+		
+		//execution
+		final var result = testUnit.createQueryToLoadEntity("eeeeeee1", tableInfo);
+		
+		//verification
+		final var expectedResult = "SELECT Id, SaveStamp, name, year_of_birth FROM ECat WHERE Id = 'eeeeeee1';";
 		expect(result).isEqualTo(expectedResult);
 	}
 	
