194989f5f7c45f7ba7aa427f79140399621435d1
Nimeon <silvan.wyss@gmail.com>
2023-07-07 11:25:54 +0200

    edited Key enum

--- Source/ch/nolix/systemapi/guiapi/inputapi/Key.java
+++ Source/ch/nolix/systemapi/guiapi/inputapi/Key.java
@@ -107,262 +107,264 @@ public enum Key {
 	
 	//static method
 	public static Key fromAWTKeyEvent(final KeyEvent keyEvent) {
+		return
 		switch (keyEvent.getKeyCode()) {
-			case KeyEvent.VK_A:
-				return A;
-			case KeyEvent.VK_B:
-				return B;
-			case KeyEvent.VK_C:
-				return C;	
-			case KeyEvent.VK_D:
-				return D;
-			case KeyEvent.VK_E:
-				return E;
-			case KeyEvent.VK_F:
-				return F;	
-			case KeyEvent.VK_G:
-				return G;
-			case KeyEvent.VK_H:
-				return H;
-			case KeyEvent.VK_I:
-				return I;	
-			case KeyEvent.VK_J:
-				return J;
-			case KeyEvent.VK_K:
-				return K;
-			case KeyEvent.VK_L:
-				return L;	
-			case KeyEvent.VK_M:
-				return M;
-			case KeyEvent.VK_N:
-				return N;
-			case KeyEvent.VK_O:
-				return O;	
-			case KeyEvent.VK_P:
-				return P;
-			case KeyEvent.VK_Q:
-				return Q;
-			case KeyEvent.VK_R:
-				return R;
-			case KeyEvent.VK_S:
-				return S;
-			case KeyEvent.VK_T:
-				return T;
-			case KeyEvent.VK_U:
-				return U;
-			case KeyEvent.VK_V:
-				return V;
-			case KeyEvent.VK_W:
-				return W;
-			case KeyEvent.VK_X:
-				return X;
-			case KeyEvent.VK_Y:
-				return Y;
-			case KeyEvent.VK_Z:
-				return Z;
-			case KeyEvent.VK_0:
-				return NUMBER_0;
-			case KeyEvent.VK_1:
-				return NUMBER_1;
-			case KeyEvent.VK_2:
-				return NUMBER_2;
-			case KeyEvent.VK_3:
-				return NUMBER_3;
-			case KeyEvent.VK_4:
-				return NUMBER_4;
-			case KeyEvent.VK_5:
-				return NUMBER_5;
-			case KeyEvent.VK_6:
-				return NUMBER_6;
-			case KeyEvent.VK_7:
-				return NUMBER_7;
-			case KeyEvent.VK_8:
-				return NUMBER_8;
-			case KeyEvent.VK_9:
-				return NUMBER_9;
-			case KeyEvent.VK_NUMPAD0:
-				return NUMBERPAD_0;
-			case KeyEvent.VK_NUMPAD1:
-				return NUMBERPAD_1;
-			case KeyEvent.VK_NUMPAD2:
-				return NUMBERPAD_2;
-			case KeyEvent.VK_NUMPAD3:
-				return NUMBERPAD_3;
-			case KeyEvent.VK_NUMPAD4:
-				return NUMBERPAD_4;
-			case KeyEvent.VK_NUMPAD5:
-				return NUMBERPAD_5;
-			case KeyEvent.VK_NUMPAD6:
-				return NUMBERPAD_6;
-			case KeyEvent.VK_NUMPAD7:
-				return NUMBERPAD_7;
-			case KeyEvent.VK_NUMPAD8:
-				return NUMBERPAD_8;
-			case KeyEvent.VK_NUMPAD9:
-				return NUMBERPAD_9;
-			case KeyEvent.VK_LEFT:
-				return ARROW_LEFT;
-			case KeyEvent.VK_RIGHT:
-				return ARROW_RIGHT;
-			case KeyEvent.VK_UP:
-				return ARROW_UP;
-			case KeyEvent.VK_DOWN:
-				return ARROW_DOWN;
-			case KeyEvent.VK_BACK_SPACE:
-				return BACKSPACE;
-			case KeyEvent.VK_COMMA:
-				return COMMA;
-			case KeyEvent.VK_CONTROL:
-				return CONTROL;
-			case KeyEvent.VK_CAPS_LOCK:
-				return Key.CAPS_LOCK;
-			case KeyEvent.VK_DELETE:
-				return Key.DELETE;
-			case KeyEvent.VK_DOLLAR:
-				return DOLLAR_SYMBOL;
-			case KeyEvent.VK_ENTER:
-				return Key.ENTER;
-			case KeyEvent.VK_ESCAPE:
-				return ESCAPE;
-			case KeyEvent.VK_SHIFT:
-				return SHIFT;
-			case KeyEvent.VK_SPACE:
-				return Key.SPACE;
-			default:
+			case KeyEvent.VK_A ->
+				A;
+			case KeyEvent.VK_B ->
+				B;
+			case KeyEvent.VK_C ->
+				C;	
+			case KeyEvent.VK_D ->
+				D;
+			case KeyEvent.VK_E ->
+				E;
+			case KeyEvent.VK_F ->
+				F;	
+			case KeyEvent.VK_G ->
+				G;
+			case KeyEvent.VK_H ->
+				H;
+			case KeyEvent.VK_I ->
+				I;	
+			case KeyEvent.VK_J ->
+				J;
+			case KeyEvent.VK_K ->
+				K;
+			case KeyEvent.VK_L ->
+				L;	
+			case KeyEvent.VK_M ->
+				M;
+			case KeyEvent.VK_N ->
+				N;
+			case KeyEvent.VK_O ->
+				O;	
+			case KeyEvent.VK_P ->
+				P;
+			case KeyEvent.VK_Q ->
+				Q;
+			case KeyEvent.VK_R ->
+				R;
+			case KeyEvent.VK_S ->
+				S;
+			case KeyEvent.VK_T ->
+				T;
+			case KeyEvent.VK_U ->
+				U;
+			case KeyEvent.VK_V ->
+				V;
+			case KeyEvent.VK_W ->
+				W;
+			case KeyEvent.VK_X ->
+				X;
+			case KeyEvent.VK_Y ->
+				Y;
+			case KeyEvent.VK_Z ->
+				Z;
+			case KeyEvent.VK_0 ->
+				NUMBER_0;
+			case KeyEvent.VK_1 ->
+				NUMBER_1;
+			case KeyEvent.VK_2 ->
+				NUMBER_2;
+			case KeyEvent.VK_3 ->
+				NUMBER_3;
+			case KeyEvent.VK_4 ->
+				NUMBER_4;
+			case KeyEvent.VK_5 ->
+				NUMBER_5;
+			case KeyEvent.VK_6 ->
+				NUMBER_6;
+			case KeyEvent.VK_7 ->
+				NUMBER_7;
+			case KeyEvent.VK_8 ->
+				NUMBER_8;
+			case KeyEvent.VK_9 ->
+				NUMBER_9;
+			case KeyEvent.VK_NUMPAD0 ->
+				NUMBERPAD_0;
+			case KeyEvent.VK_NUMPAD1 ->
+				NUMBERPAD_1;
+			case KeyEvent.VK_NUMPAD2 ->
+				NUMBERPAD_2;
+			case KeyEvent.VK_NUMPAD3 ->
+				NUMBERPAD_3;
+			case KeyEvent.VK_NUMPAD4 ->
+				NUMBERPAD_4;
+			case KeyEvent.VK_NUMPAD5 ->
+				NUMBERPAD_5;
+			case KeyEvent.VK_NUMPAD6 ->
+				NUMBERPAD_6;
+			case KeyEvent.VK_NUMPAD7 ->
+				NUMBERPAD_7;
+			case KeyEvent.VK_NUMPAD8 ->
+				NUMBERPAD_8;
+			case KeyEvent.VK_NUMPAD9 ->
+				NUMBERPAD_9;
+			case KeyEvent.VK_LEFT ->
+				ARROW_LEFT;
+			case KeyEvent.VK_RIGHT ->
+				ARROW_RIGHT;
+			case KeyEvent.VK_UP ->
+				ARROW_UP;
+			case KeyEvent.VK_DOWN ->
+				ARROW_DOWN;
+			case KeyEvent.VK_BACK_SPACE ->
+				BACKSPACE;
+			case KeyEvent.VK_COMMA ->
+				COMMA;
+			case KeyEvent.VK_CONTROL ->
+				CONTROL;
+			case KeyEvent.VK_CAPS_LOCK ->
+				Key.CAPS_LOCK;
+			case KeyEvent.VK_DELETE ->
+				Key.DELETE;
+			case KeyEvent.VK_DOLLAR ->
+				DOLLAR_SYMBOL;
+			case KeyEvent.VK_ENTER ->
+				Key.ENTER;
+			case KeyEvent.VK_ESCAPE ->
+				ESCAPE;
+			case KeyEvent.VK_SHIFT ->
+				SHIFT;
+			case KeyEvent.VK_SPACE ->
+				Key.SPACE;
+			default ->
 				throw new IllegalArgumentException("The given key event does not represent a Key.");
-		}
+		};
 	}
 	
 	//static method
 	public static Key fromCharacter(final char character) {
+		return
 		switch (character) {
-			case 'A':
-				return Key.A;
-			case 'B':
-				return Key.B;
-			case 'C':
-				return Key.C;
-			case 'D':
-				return Key.D;
-			case 'E':
-				return Key.E;
-			case 'F':
-				return Key.F;
-			case 'G':
-				return Key.G;
-			case 'H':
-				return Key.H;
-			case 'I':
-				return Key.I;
-			case 'J':
-				return Key.J;
-			case 'K':
-				return Key.K;
-			case 'L':
-				return Key.L;
-			case 'M':
-				return Key.M;
-			case 'N':
-				return Key.N;
-			case 'O':
-				return Key.O;
-			case 'P':
-				return Key.P;
-			case 'Q':
-				return Key.Q;
-			case 'R':
-				return Key.R;
-			case 'S':
-				return Key.S;
-			case 'T':
-				return Key.T;
-			case 'U':
-				return Key.U;
-			case 'V':
-				return Key.V;
-			case 'W':
-				return Key.W;
-			case 'X':
-				return Key.X;
-			case 'Y':
-				return Key.Y;
-			case 'Z':
-				return Key.Z;
-			case 'a':
-				return Key.A;
-			case 'b':
-				return Key.B;
-			case 'c':
-				return Key.C;
-			case 'd':
-				return Key.D;
-			case 'e':
-				return Key.E;
-			case 'f':
-				return Key.F;
-			case 'g':
-				return Key.G;
-			case 'h':
-				return Key.H;
-			case 'i':
-				return Key.I;
-			case 'j':
-				return Key.J;
-			case 'k':
-				return Key.K;
-			case 'l':
-				return Key.L;
-			case 'm':
-				return Key.M;
-			case 'n':
-				return Key.N;
-			case 'o':
-				return Key.O;
-			case 'p':
-				return Key.P;
-			case 'q':
-				return Key.Q;
-			case 'r':
-				return Key.R;
-			case 's':
-				return Key.S;
-			case 't':
-				return Key.T;
-			case 'u':
-				return Key.U;
-			case 'v':
-				return Key.V;
-			case 'w':
-				return Key.W;
-			case 'x':
-				return Key.X;
-			case 'y':
-				return Key.Y;
-			case 'z':
-				return Key.Z;
-			case '0':
-				return Key.NUMBER_0;
-			case '1':
-				return Key.NUMBER_1;
-			case '2':
-				return Key.NUMBER_2;
-			case '3':
-				return Key.NUMBER_3;
-			case '4':
-				return Key.NUMBER_4;
-			case '5':
-				return Key.NUMBER_5;
-			case '6':
-				return Key.NUMBER_6;
-			case '7':
-				return Key.NUMBER_7;
-			case '8':
-				return Key.NUMBER_8;
-			case '9':
-				return Key.NUMBER_9;
-			default:
+			case 'A' ->
+				Key.A;
+			case 'B' ->
+				Key.B;
+			case 'C' ->
+				Key.C;
+			case 'D' ->
+				Key.D;
+			case 'E' ->
+				Key.E;
+			case 'F' ->
+				Key.F;
+			case 'G' ->
+				Key.G;
+			case 'H' ->
+				Key.H;
+			case 'I' ->
+				Key.I;
+			case 'J' ->
+				Key.J;
+			case 'K' ->
+				Key.K;
+			case 'L' ->
+				Key.L;
+			case 'M' ->
+				Key.M;
+			case 'N' ->
+				Key.N;
+			case 'O' ->
+				Key.O;
+			case 'P' ->
+				Key.P;
+			case 'Q' ->
+				Key.Q;
+			case 'R' ->
+				Key.R;
+			case 'S' ->
+				Key.S;
+			case 'T' ->
+				Key.T;
+			case 'U' ->
+				Key.U;
+			case 'V' ->
+				Key.V;
+			case 'W' ->
+				Key.W;
+			case 'X' ->
+				Key.X;
+			case 'Y' ->
+				Key.Y;
+			case 'Z' ->
+				Key.Z;
+			case 'a' ->
+				Key.A;
+			case 'b' ->
+				Key.B;
+			case 'c' ->
+				Key.C;
+			case 'd' ->
+				Key.D;
+			case 'e' ->
+				Key.E;
+			case 'f' ->
+				Key.F;
+			case 'g' ->
+				Key.G;
+			case 'h' ->
+				Key.H;
+			case 'i' ->
+				Key.I;
+			case 'j' ->
+				Key.J;
+			case 'k' ->
+				Key.K;
+			case 'l' ->
+				Key.L;
+			case 'm' ->
+				Key.M;
+			case 'n' ->
+				Key.N;
+			case 'o' ->
+				Key.O;
+			case 'p' ->
+				Key.P;
+			case 'q' ->
+				Key.Q;
+			case 'r' ->
+				Key.R;
+			case 's' ->
+				Key.S;
+			case 't' ->
+				Key.T;
+			case 'u' ->
+				Key.U;
+			case 'v' ->
+				Key.V;
+			case 'w' ->
+				Key.W;
+			case 'x' ->
+				Key.X;
+			case 'y' ->
+				Key.Y;
+			case 'z' ->
+				Key.Z;
+			case '0' ->
+				Key.NUMBER_0;
+			case '1' ->
+				Key.NUMBER_1;
+			case '2' ->
+				Key.NUMBER_2;
+			case '3' ->
+				Key.NUMBER_3;
+			case '4' ->
+				Key.NUMBER_4;
+			case '5' ->
+				Key.NUMBER_5;
+			case '6' ->
+				Key.NUMBER_6;
+			case '7' ->
+				Key.NUMBER_7;
+			case '8' ->
+				Key.NUMBER_8;
+			case '9' ->
+				Key.NUMBER_9;
+			default ->
 				throw new IllegalArgumentException("The given char is not a character.");
-		}
+		};
 	}
 	
 	//static method
@@ -377,6 +379,7 @@ public enum Key {
 			return true;
 		}
 		
+		return
 		switch (this) {
 			case
 			COMMA,	
@@ -388,15 +391,16 @@ public enum Key {
 			HYPHEN,
 			QUESTION_MARK,
 			RELATIONS,
-			SPACE:
-				return true;
-			default:
-				return false;
-		}
+			SPACE ->
+				true;
+			default ->
+				false;
+		};
 	}
 	
 	//method
 	public boolean isLetter() {
+		return
 		switch(this) {
 			case
 			A,
@@ -411,7 +415,7 @@ public enum Key {
 			J,	
 			K,
 			L,
-			 M,
+			M,
 			N,
 			O,
 			P,
@@ -424,15 +428,16 @@ public enum Key {
 			W,
 			X,
 			Y,
-			Z:
-				return true;
-			default:
-				return false;
-		}
+			Z ->
+				true;
+			default ->
+				false;
+		};
 	}
 	
 	//method
 	public boolean isNumber() {
+		return
 		switch(this) {
 			case
 			NUMBER_0,
@@ -454,15 +459,16 @@ public enum Key {
 			NUMBERPAD_6,
 			NUMBERPAD_7,
 			NUMBERPAD_8,
-			NUMBERPAD_9:
-				return true;
-			default:
-				return false;
-		}
+			NUMBERPAD_9 ->
+				true;
+			default ->
+				false;
+		};
 	}
 	
 	//method
 	public boolean isOnNumberPad() {
+		return
 		switch(this) {
 			case
 			NUMBERPAD_0,
@@ -474,198 +480,201 @@ public enum Key {
 			NUMBERPAD_6,
 			NUMBERPAD_7,
 			NUMBERPAD_8,
-			NUMBERPAD_9:
-				return true;
-			default:
-				return false;
-		}
+			NUMBERPAD_9 ->
+				true;
+			default ->
+				false;
+		};
 	}
 	
 	//method
 	public char toLowerCaseChar() {
+		return
 		switch(this) {
-			case A:
-				return 'a';
-			case B:
-				return 'b';
-			case C:
-				return 'c';
-			case D:
-				return 'd';
-			case E:
-				return 'e';
-			case F:
-				return 'f';
-			case G:
-				return 'g';
-			case H:
-				return 'h';
-			case I:
-				return 'i';
-			case J:
-				return 'j';
-			case K:
-				return 'k';
-			case L:
-				return 'l';
-			case M:
-				return 'm';
-			case N:
-				return 'n';
-			case O:
-				return 'o';
-			case P:
-				return 'p';
-			case Q:
-				return 'q';
-			case R:
-				return 'r';
-			case S:
-				return 's';
-			case T:
-				return 't';
-			case U:
-				return 'u';
-			case V:
-				return 'v';
-			case W:
-				return 'w';
-			case X:
-				return 'x';
-			case Y:
-				return 'y';
-			case Z:
-				return 'z';
-			default:
-				return toNonNumberChar();
-		}
+			case A ->
+				'a';
+			case B ->
+				'b';
+			case C ->
+				'c';
+			case D ->
+				'd';
+			case E ->
+				'e';
+			case F ->
+				'f';
+			case G ->
+				'g';
+			case H ->
+				'h';
+			case I ->
+				'i';
+			case J ->
+				'j';
+			case K ->
+				'k';
+			case L ->
+				'l';
+			case M ->
+				'm';
+			case N ->
+				'n';
+			case O ->
+				'o';
+			case P ->
+				'p';
+			case Q ->
+				'q';
+			case R ->
+				'r';
+			case S ->
+				's';
+			case T ->
+				't';
+			case U ->
+				'u';
+			case V ->
+				'v';
+			case W ->
+				'w';
+			case X ->
+				'x';
+			case Y ->
+				'y';
+			case Z ->
+				'z';
+			default ->
+				toNonNumberChar();
+		};
 	}
 	
 	//method
 	public char toUpperCaseChar() {
+		return
 		switch(this) {
-			case A:
-				return 'A';
-			case B:
-				return 'B';
-			case C:
-				return 'C';
-			case D:
-				return 'D';
-			case E:
-				return 'E';
-			case F:
-				return 'F';
-			case G:
-				return 'G';
-			case H:
-				return 'H';
-			case I:
-				return 'I';
-			case J:
-				return 'J';
-			case K:
-				return 'K';
-			case L:
-				return 'L';
-			case M:
-				return 'M';
-			case N:
-				return 'N';
-			case O:
-				return 'O';
-			case P:
-				return 'P';
-			case Q:
-				return 'Q';
-			case R:
-				return 'R';
-			case S:
-				return 'S';
-			case T:
-				return 'T';
-			case U:
-				return 'U';
-			case V:
-				return 'V';
-			case W:
-				return 'W';
-			case X:
-				return 'X';
-			case Y:
-				return 'Y';
-			case Z:
-				return 'Z';
-			default:
-				return toNonNumberChar();
-		}
+			case A ->
+				'A';
+			case B ->
+				'B';
+			case C ->
+				'C';
+			case D ->
+				'D';
+			case E ->
+				'E';
+			case F ->
+				'F';
+			case G ->
+				'G';
+			case H ->
+				'H';
+			case I ->
+				'I';
+			case J ->
+				'J';
+			case K ->
+				'K';
+			case L ->
+				'L';
+			case M ->
+				'M';
+			case N ->
+				'N';
+			case O ->
+				'O';
+			case P ->
+				'P';
+			case Q ->
+				'Q';
+			case R ->
+				'R';
+			case S ->
+				'S';
+			case T ->
+				'T';
+			case U ->
+				'U';
+			case V ->
+				'V';
+			case W ->
+				'W';
+			case X ->
+				'X';
+			case Y ->
+				'Y';
+			case Z ->
+				'Z';
+			default ->
+				toNonNumberChar();
+		};
 	}
 	
 	//method
 	private char toNonNumberChar() {
+		return
 		switch (this) {
-			case NUMBER_0:
-				return '0';
-			case NUMBER_1:
-				return '1';
-			case NUMBER_2:
-				return '2';
-			case NUMBER_3:
-				return '3';
-			case NUMBER_4:
-				return '4';
-			case NUMBER_5:
-				return '5';
-			case NUMBER_6:
-				return '6';
-			case NUMBER_7:
-				return '7';
-			case NUMBER_8:
-				return '8';
-			case NUMBER_9:
-				return '9';
-			case NUMBERPAD_0:
-				return '0';
-			case NUMBERPAD_1:
-				return '1';
-			case NUMBERPAD_2:
-				return '2';
-			case NUMBERPAD_3:
-				return '3';
-			case NUMBERPAD_4:
-				return '4';
-			case NUMBERPAD_5:
-				return '5';
-			case NUMBERPAD_6:
-				return '6';
-			case NUMBERPAD_7:
-				return '7';
-			case NUMBERPAD_8:
-				return '8';
-			case NUMBERPAD_9:
-				return '9';
-			case COMMA:
-				return ',';
-			case DOLLAR_SYMBOL:
-				return '$';
-			case DOT:
-				return '.';
-			case EXCLAMATION_MARK:
-				return '!';
-			case GRADE_SYMBOL:
-				return '°';
-			case GRAVIS:
-				return '`';
-			case HYPHEN:
-				return '-';
-			case QUESTION_MARK:
-				return '?';
-			case RELATIONS:
-				return '<';
-			case SPACE:
-				return ' ';
-		default:
+			case NUMBER_0 ->
+				'0';
+			case NUMBER_1 ->
+				'1';
+			case NUMBER_2 ->
+				'2';
+			case NUMBER_3 ->
+				'3';
+			case NUMBER_4 ->
+				'4';
+			case NUMBER_5 ->
+				'5';
+			case NUMBER_6 ->
+				'6';
+			case NUMBER_7 ->
+				'7';
+			case NUMBER_8 ->
+				'8';
+			case NUMBER_9 ->
+				'9';
+			case NUMBERPAD_0 ->
+				'0';
+			case NUMBERPAD_1 ->
+				'1';
+			case NUMBERPAD_2 ->
+				'2';
+			case NUMBERPAD_3 ->
+				'3';
+			case NUMBERPAD_4 ->
+				'4';
+			case NUMBERPAD_5 ->
+				'5';
+			case NUMBERPAD_6 ->
+				'6';
+			case NUMBERPAD_7 ->
+				'7';
+			case NUMBERPAD_8 ->
+				'8';
+			case NUMBERPAD_9 ->
+				'9';
+			case COMMA ->
+				',';
+			case DOLLAR_SYMBOL ->
+				'$';
+			case DOT ->
+				'.';
+			case EXCLAMATION_MARK ->
+				'!';
+			case GRADE_SYMBOL ->
+				'°';
+			case GRAVIS ->
+				'`';
+			case HYPHEN ->
+				'-';
+			case QUESTION_MARK ->
+				'?';
+			case RELATIONS ->
+				'<';
+			case SPACE ->
+				' ';
+		default ->
 			throw new IllegalArgumentException("The current Key is not a non-number character.");			
-		}
+		};
 	}
 }
