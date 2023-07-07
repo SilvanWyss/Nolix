628ac95d4f815a02bc73de5729fb94549890fc90
Nimeon <silvan.wyss@gmail.com>
2023-07-07 22:26:37 +0200

    beautified FieldIndexCatalogue

--- Source/ch/nolix/system/nodedatabaserawdata/tabledefinition/FieldIndexCatalogue.java
+++ Source/ch/nolix/system/nodedatabaserawdata/tabledefinition/FieldIndexCatalogue.java
@@ -4,8 +4,10 @@ package ch.nolix.system.nodedatabaserawdata.tabledefinition;
 //class
 public final class FieldIndexCatalogue {
 	
-	//constants
+	//constant
 	public static final int ID_INDEX = 1;
+	
+	//constant
 	public static final int SAVE_STAMP_INDEX = 2;
 	
 	//constructor
