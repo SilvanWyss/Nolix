2835ff36188a32cb0a0c6531b4d906f861722617
Nimeon <silvan.wyss@gmail.com>
2023-07-08 17:39:01 +0200

    created ActivenessRequestable

---
+++ Source/ch/nolix/coreapi/functionapi/requestapi/ActivenessRequestable.java
@@ -0,0 +1,28 @@
+//package declaration
+package ch.nolix.coreapi.functionapi.requestapi;
+
+//own imports
+import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
+
+//interface
+/**
+ * A {@link ActivenessRequestable} can be asked if it is active.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-08
+ */
+@AllowDefaultMethodsAsDesignPattern
+public interface ActivenessRequestable {
+	
+	//method declaration
+	/**
+	 * @return true if the current {@link ActivenessRequestable} is active.
+	 */
+	boolean isActive();
+	
+	//method declaration
+	/**
+	 * @return true if the current {@link ActivenessRequestable} is not active.
+	 */
+	boolean isInactive();
+}
