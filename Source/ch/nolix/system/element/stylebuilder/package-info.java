f79d6d6c32fda4f46cca6fb90a6b1d0e72f48895
Nimeon <silvan.wyss@gmail.com>
2023-07-10 22:05:49 +0200

    created stylebuilder package

---
+++ Source/ch/nolix/system/element/stylebuilder/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.element.style} package defines
+ * a builder for {@link ch.nolix.system.element.style.Style}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-10
+ */
+package ch.nolix.system.element.stylebuilder;
