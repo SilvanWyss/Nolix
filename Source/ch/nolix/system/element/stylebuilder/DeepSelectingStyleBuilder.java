309e81cfbfc0b12ee4cebc24295aca1558babfe0
Nimeon <silvan.wyss@gmail.com>
2023-07-10 23:32:54 +0200

    created StyleBuilder

---
+++ Source/ch/nolix/system/element/stylebuilder/DeepSelectingStyleBuilder.java
@@ -0,0 +1,28 @@
+//package declaration
+package ch.nolix.system.element.stylebuilder;
+
+//own imports
+import ch.nolix.system.element.style.DeepSelectingStyle;
+
+//class
+public final class DeepSelectingStyleBuilder extends BaseSelectingStyleBuilder<DeepSelectingStyleBuilder> {
+	
+	//method
+	public DeepSelectingStyle build() {
+		return
+		new DeepSelectingStyle(
+			getSelectorIdContainer(),
+			getSelectorTypeContainer(),
+			getSelectorRoles(),
+			getSelectorTokens(),
+			getAttachingAttributes(),
+			getSubStyles()
+		);
+	}
+	
+	//method
+	@Override
+	protected DeepSelectingStyleBuilder asConcrete() {
+		return this;
+	}
+}
---
+++ Source/ch/nolix/system/element/stylebuilder/SelectingStyleBuilder.java
@@ -0,0 +1,28 @@
+//package declaration
+package ch.nolix.system.element.stylebuilder;
+
+//own imports
+import ch.nolix.system.element.style.SelectingStyle;
+
+//class
+public final class SelectingStyleBuilder extends BaseSelectingStyleBuilder<SelectingStyleBuilder> {
+	
+	//method
+	public SelectingStyle build() {
+		return
+		new SelectingStyle(
+			getSelectorIdContainer(),
+			getSelectorTypeContainer(),
+			getSelectorRoles(),
+			getSelectorTokens(),
+			getAttachingAttributes(),
+			getSubStyles()
+		);
+	}
+	
+	//method
+	@Override
+	protected SelectingStyleBuilder asConcrete() {
+		return this;
+	}
+}
