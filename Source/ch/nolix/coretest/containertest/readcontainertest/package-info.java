da6b1a0d9cdd131ee4337a66162bba07f45a0bde
Nimeon <silvan.wyss@gmail.com>
2023-07-11 21:10:33 +0200

    created readcontainertest package

---
+++ Source/ch/nolix/coretest/containertest/readcontainertest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coretest.containertest.readcontainertest} package defines
+ * tests for the {@link ch.nolix.core.container.readcontainer} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-11
+ */
+package ch.nolix.coretest.containertest.readcontainertest;
