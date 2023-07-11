35f25514b09d680fef12d7782257ec5afb8266cc
Nimeon <silvan.wyss@gmail.com>
2023-07-11 22:09:57 +0200

    deleted input types

--- Source/ch/nolix/systemapi/guiapi/inputapi/IInput.java
+++
@@ -1,7 +0,0 @@
-//package declaration
-package ch.nolix.systemapi.guiapi.inputapi;
-
-import ch.nolix.systemapi.elementapi.mainapi.Specified;
-
-//interface
-public interface IInput<I extends IInput<I>> extends Specified {}
--- Source/ch/nolix/systemapi/guiapi/inputapi/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemapi.guiapi.inputapi} package declares inputs.
- * 
- * @author Silvan Wyss
- * @date 2022.04.02
- */
-package ch.nolix.systemapi.guiapi.inputapi;
