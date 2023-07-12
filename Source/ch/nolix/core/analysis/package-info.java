cbffd6204de76c7e0874462c523de256832368d8
Nimeon <silvan.wyss@gmail.com>
2023-07-12 19:40:40 +0200

    created analysis package

---
+++ Source/ch/nolix/core/analysis/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.core.analysis} package defines types for analyses.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-12
+ */
+package ch.nolix.core.analysis;
