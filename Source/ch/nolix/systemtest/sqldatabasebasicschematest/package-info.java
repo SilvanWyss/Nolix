739d424ebd06f1e60171bf0c769d1ba40667c416
Nimeon <silvan.wyss@gmail.com>
2023-07-14 12:03:08 +0200

    created sqldatabasebasicschematest package

---
+++ Source/ch/nolix/systemtest/sqldatabasebasicschematest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link h.nolix.systemtest.sqldatabasebasicschematest} package defines
+ * tests for the {@link ch.nolix.system.sqldatabasebasicschema} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-14
+ */
+package ch.nolix.systemtest.sqldatabasebasicschematest;
