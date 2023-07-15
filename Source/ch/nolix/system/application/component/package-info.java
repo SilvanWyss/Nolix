517ab85649a18890009658f6564ffc55f963f98d
Nimeon <silvan.wyss@gmail.com>
2023-07-15 18:18:23 +0200

    created component package

---
+++ Source/ch/nolix/system/application/component/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.system.application.component} package defines a base type of components for web applications.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-15
+ */
+package ch.nolix.system.application.component;
