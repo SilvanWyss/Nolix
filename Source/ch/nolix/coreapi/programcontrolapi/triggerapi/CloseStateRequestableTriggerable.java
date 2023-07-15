22af0a6780239739a3314e4b422367657ff1d7f2
Nimeon <silvan.wyss@gmail.com>
2023-07-15 19:38:50 +0200

    deleted CloseStateRequestableTriggerable

--- Source/ch/nolix/coreapi/programcontrolapi/triggerapi/CloseStateRequestableTriggerable.java
+++
@@ -1,7 +0,0 @@
-//package declaration
-package ch.nolix.coreapi.programcontrolapi.triggerapi;
-
-import ch.nolix.coreapi.functionapi.requestapi.CloseStateRequestable;
-
-//interface
-public interface CloseStateRequestableTriggerable extends Triggerable, CloseStateRequestable {}
