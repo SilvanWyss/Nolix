221d8943b4608a7d9412b621976cb0ccb861ca7f
Nimeon <silvan.wyss@gmail.com>
2023-07-16 20:20:08 +0200

    extended ArrayReadContainerTest

--- Source/ch/nolix/coretest/containertest/readcontainertest/ArrayReadContainerTest.java
+++ Source/ch/nolix/coretest/containertest/readcontainertest/ArrayReadContainerTest.java
@@ -4,11 +4,24 @@ package ch.nolix.coretest.containertest.readcontainertest;
 //own imports
 import ch.nolix.core.commontype.commontypehelper.GlobalArrayHelper;
 import ch.nolix.core.container.readcontainer.ArrayReadContainer;
+import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentIsNullException;
+import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.coretest.containertest.basetest.ContainerTest;
 
 //class
 public final class ArrayReadContainerTest extends ContainerTest {
+	
+	//method
+	@TestCase
+	public void testCase_forArray_whenTheGivenArrayIsNull() {
+		
+		//execution & verification
+		expectRunning(() -> ArrayReadContainer
+		.forArray(null)).throwsException()
+		.ofType(ArgumentIsNullException.class)
+		.withMessage("The given array is null.");
+	}
 	
 	//method
 	@Override
