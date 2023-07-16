2a62a4a32a85843b3de0c1358844906ad56cbb5f
Nimeon <silvan.wyss@gmail.com>
2023-07-16 15:09:04 +0200

    created mutablemultiattributeapi package

---
+++ Source/ch/nolix/coreapi/attributeapi/mutablemultiattributeapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.attributeapi.mutablemultiattributeapi} package declares
+ * types with a certain mutable multi-attribute.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-16
+ */
+package ch.nolix.coreapi.attributeapi.mutablemultiattributeapi;
