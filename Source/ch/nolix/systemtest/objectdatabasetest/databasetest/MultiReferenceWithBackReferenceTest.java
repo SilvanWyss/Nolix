34f24d13737ae49aeb22509fe2ba5e6c2ab7d47f
Nimeon <silvan.wyss@gmail.com>
2023-07-19 22:10:22 +0200

    extended MultiReferenceWithBackReferenceTest

--- Source/ch/nolix/systemtest/objectdatabasetest/databasetest/MultiReferenceWithBackReferenceTest.java
+++ Source/ch/nolix/systemtest/objectdatabasetest/databasetest/MultiReferenceWithBackReferenceTest.java
@@ -13,7 +13,7 @@ import ch.nolix.system.objectdatabase.schema.Schema;
 
 //class
 public final class MultiReferenceWithBackReferenceTest extends Test {
-
+	
 	//static class
 	private static final class Person extends Entity {
 		
@@ -47,7 +47,7 @@ public final class MultiReferenceWithBackReferenceTest extends Test {
 		final var nodeDatabase = new MutableNode();
 		final var schema = Schema.withEntityType(Person.class, Pet.class);
 		final var nodeDatabaseAdapter =
-		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("MyDatabase").usingSchema(schema);
+		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("my_database").usingSchema(schema);
 		final var garfield = new Pet();
 		nodeDatabaseAdapter.insert(garfield);
 		final var simba = new Pet();
@@ -72,12 +72,55 @@ public final class MultiReferenceWithBackReferenceTest extends Test {
 		nodeDatabaseAdapter.getStoredTableByEntityType(Pet.class).getStoredEntityById(simba.getId());
 		final var loadedOdie =
 		nodeDatabaseAdapter.getStoredTableByEntityType(Pet.class).getStoredEntityById(odie.getId());
-		expect(loadedJohn.pets.getReferencedEntities().getElementCount()).isEqualTo(3);
-		expect(loadedJohn.pets.getReferencedEntities().contains(loadedGarfield));
-		expect(loadedJohn.pets.getReferencedEntities().contains(loadedSimba));
-		expect(loadedJohn.pets.getReferencedEntities().contains(loadedOdie));
+		expect(loadedJohn.pets.getReferencedEntities()).containsExactly(loadedGarfield, loadedSimba, loadedOdie);
 		expect(loadedGarfield.owner.getBackReferencedEntity()).is(loadedJohn);
 		expect(loadedSimba.owner.getBackReferencedEntity()).is(loadedJohn);
 		expect(loadedOdie.owner.getBackReferencedEntity()).is(loadedJohn);
+	}
+	
+	//method
+	//TODO: @TestCase
+	public void testCase_removeEntity_whenContainsEntity() {
+		
+		//setup part 1
+		final var nodeDatabase = new MutableNode();
+		final var schema = Schema.withEntityType(Person.class, Pet.class);
+		final var nodeDatabaseAdapter =
+		NodeDatabaseAdapter.forNodeDatabase(nodeDatabase).withName("my_database").usingSchema(schema);
+		final var garfield = new Pet();
+		nodeDatabaseAdapter.insert(garfield);
+		final var simba = new Pet();
+		nodeDatabaseAdapter.insert(simba);
+		final var odie = new Pet();
+		nodeDatabaseAdapter.insert(odie);
+		final var john = new Person();
+		john.pets.addEntity(garfield);
+		john.pets.addEntity(simba);
+		john.pets.addEntity(odie);
+		nodeDatabaseAdapter.insert(john);
+		nodeDatabaseAdapter.saveChanges();
+		
+		//setup part 2
+		final var loadedJohn1 =
+		nodeDatabaseAdapter.getStoredTableByEntityType(Person.class).getStoredEntityById(john.getId());
+		final var loadedOdie1 =
+		nodeDatabaseAdapter.getStoredTableByEntityType(Pet.class).getStoredEntityById(odie.getId());
+		
+		//execution
+		loadedJohn1.pets.removeEntity(loadedOdie1);
+		loadedOdie1.delete();
+		nodeDatabaseAdapter.saveChanges();
+		
+		//verification part 1
+		expectNot(loadedJohn1.pets.referencesEntity(loadedOdie1));
+		
+		//verification part 2
+		final var loadedJohn2 =
+		nodeDatabaseAdapter.getStoredTableByEntityType(Person.class).getStoredEntityById(john.getId());
+		final var loadedGarfield2 =
+		nodeDatabaseAdapter.getStoredTableByEntityType(Pet.class).getStoredEntityById(garfield.getId());
+		final var loadedSimba2 =
+		nodeDatabaseAdapter.getStoredTableByEntityType(Pet.class).getStoredEntityById(simba.getId());
+		expect(loadedJohn2.pets.getReferencedEntities()).containsExactly(loadedGarfield2, loadedSimba2);
 	}
 }
