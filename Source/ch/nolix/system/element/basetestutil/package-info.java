519f85c816ee309f5d3b651f038ba747ff5fbac7
Nimeon <silvan.wyss@gmail.com>
2023-07-21 12:09:32 +0200

    created basetestutil package

---
+++ Source/ch/nolix/system/element/basetestutil/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.element.basetestutil} package defines
+ * test util types for the {@link ch.nolix.system.element.base} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2019-07-21
+ */
+package ch.nolix.system.element.basetestutil;
