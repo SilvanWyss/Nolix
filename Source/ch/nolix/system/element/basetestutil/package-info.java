3de037f5548b881d7c2e3a82a19afb61a448ce44
Nimeon <silvan.wyss@gmail.com>
2023-07-22 17:26:57 +0200

    fixed package info

--- Source/ch/nolix/system/element/basetestutil/package-info.java
+++ Source/ch/nolix/system/element/basetestutil/package-info.java
@@ -4,6 +4,6 @@
  * test util types for the {@link ch.nolix.system.element.base} package.
  * 
  * @author Silvan Wyss
- * @date 2019-07-21
+ * @date 2023-07-21
  */
 package ch.nolix.system.element.basetestutil;
