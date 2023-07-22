8976394ba33f197c1ab619a38c1808f67974da4a
Nimeon <silvan.wyss@gmail.com>
2023-07-22 17:42:34 +0200

    created TestSession

---
+++ Source/ch/nolix/system/application/maintestutil/TestSession.java
@@ -0,0 +1,51 @@
+//package declaration
+package ch.nolix.system.application.maintestutil;
+
+//own imports
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.system.application.main.BackendClient;
+import ch.nolix.system.application.main.Session;
+
+//class
+public final class TestSession<
+	BC extends BackendClient<BC, AC>,
+	AC
+>
+extends Session<BC, AC> {
+	
+	//static method
+	public static <BC2 extends BackendClient<BC2, AC2>, AC2> TestSession<BC2, AC2> withClientClass(
+		final Class<?> clientClass
+	) {
+		return new TestSession<>(clientClass);
+	}
+	
+	//attribute
+	private final Class<?> clientClass;
+	
+	//constructor
+	private TestSession(final Class<?> clientClass) {
+		
+		GlobalValidator.assertThat(clientClass).thatIsNamed("client class").isNotNull();
+		
+		this.clientClass = clientClass;
+	}
+	
+	//method
+	@Override
+	protected void fullInitialize() {
+		//Does nothing.
+	}
+	
+	//method
+	@Override
+	protected Class<?> getClientClass() {
+		return clientClass;
+	}
+	
+	//method
+	@Override
+	protected void updateCounterpartActually() {
+		//Does nothing.
+	}
+}
