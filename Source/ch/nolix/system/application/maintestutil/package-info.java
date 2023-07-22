d42d724f55baffe2408dea4d27c1fda6936c5f75
Nimeon <silvan.wyss@gmail.com>
2023-07-22 17:27:44 +0200

    created maintestutil package

---
+++ Source/ch/nolix/system/application/maintestutil/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.application.maintestutil} package defines
+ * test util types for the {@link ch.nolix.system.application.main} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-21
+ */
+package ch.nolix.system.application.maintestutil;
