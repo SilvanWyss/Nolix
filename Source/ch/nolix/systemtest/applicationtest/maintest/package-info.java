fbe3a914650e0c79ef7c17ad29cbadde40f610ef
Nimeon <silvan.wyss@gmail.com>
2023-07-22 17:54:11 +0200

    created maintest package

---
+++ Source/ch/nolix/systemtest/applicationtest/maintest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.applicationtest.maintest} package defines
+ * tests for the {@link ch.nolix.system.application.main} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-22
+ */
+package ch.nolix.systemtest.applicationtest.maintest;
