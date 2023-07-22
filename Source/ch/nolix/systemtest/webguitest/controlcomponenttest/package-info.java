a81f570961d427714906cb8f426df0ffcc9791d1
Nimeon <silvan.wyss@gmail.com>
2023-07-22 23:52:08 +0200

    created controlcomponenttest package

---
+++ Source/ch/nolix/systemtest/webguitest/controlcomponenttest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.webguitest.controlcomponenttest} package defines
+ * base tests for the {@link ch.nolix.system.webgui.controlcomponent} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-22
+ */
+package ch.nolix.systemtest.webguitest.controlcomponenttest;
