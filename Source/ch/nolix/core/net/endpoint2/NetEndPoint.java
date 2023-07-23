fcb7faf4278f357b87b4f90c5890fd7d23021307
Nimeon <silvan.wyss@gmail.com>
2023-07-23 15:00:28 +0200

    simplified NetEndPoint

--- Source/ch/nolix/core/net/endpoint2/NetEndPoint.java
+++ Source/ch/nolix/core/net/endpoint2/NetEndPoint.java
@@ -15,7 +15,6 @@ import ch.nolix.coreapi.netapi.endpointapi.IEndPoint;
 import ch.nolix.coreapi.netapi.netproperty.ConnectionType;
 import ch.nolix.coreapi.netapi.netproperty.PeerType;
 import ch.nolix.coreapi.programcontrolapi.processproperty.SecurityLevel;
-import ch.nolix.coreapi.programcontrolapi.resourcecontrolapi.GroupCloseable;
 
 //class
 /**
@@ -125,17 +124,6 @@ public final class NetEndPoint extends EndPoint {
 		this.internalEndPoint = internalEndPoint;
 		createCloseDependencyTo(internalEndPoint);
 		internalEndPoint.setReceiver(this::receive);		
-	}
-	
-	//method
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void createCloseDependencyTo(final GroupCloseable element) {
-		
-		//This implementation just ensures that it cannot be overwritten.
-		super.createCloseDependencyTo(element);
 	}
 	
 	//method
