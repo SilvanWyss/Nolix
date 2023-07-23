15be1d51460bddac20674d9ec6c1f464ab81e8be
Nimeon <silvan.wyss@gmail.com>
2023-07-23 19:56:33 +0200

    suppressed some Sonar warnings

--- Source/ch/nolix/core/nolixlicense/Nolix2023Pro.java
+++ Source/ch/nolix/core/nolixlicense/Nolix2023Pro.java
@@ -14,7 +14,7 @@ public final class Nolix2023Pro extends License {
 		try {
 			final var keyAsInt = Integer.valueOf(filteredKey);
 			return (-3 * keyAsInt + keyAsInt / 20 - keyAsInt % 250 + 134_756_237 == 0);
-		} catch (final Throwable error) {
+		} catch (final Throwable error) { //NOSONAR: This method just discovers if the given filteredKey would work.
 			return false;
 		}
 	}
--- Source/ch/nolix/core/nolixlicense/Nolix2023Ultimate.java
+++ Source/ch/nolix/core/nolixlicense/Nolix2023Ultimate.java
@@ -14,7 +14,7 @@ public final class Nolix2023Ultimate extends License {
 		try {
 			final var keyAsInt = Integer.valueOf(filteredKey);
 			return (keyAsInt / 20 - keyAsInt % 260 - 9 * keyAsInt + 438_159_801 == 0);
-		} catch (final Throwable error) {
+		} catch (final Throwable error) { //NOSONAR: This method just discovers if the given filteredKey would work.
 			return false;
 		}
 	}
