1d3153d46e2e1559f37d4eb85a3a7237a905274b
Nimeon <silvan.wyss@gmail.com>
2023-07-23 18:03:21 +0200

    created RegularExpressionStringPatternCatalogue

---
+++ Source/ch/nolix/core/commontype/commontypeconstant/RegularExpressionStringPatternCatalogue.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.core.commontype.commontypeconstant;
+
+//class
+/**
+ * Of the {@link RegularExpressionStringPatternCatalogue} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-07-23
+ */
+public final class RegularExpressionStringPatternCatalogue {
+	
+	//constant
+	public static final String DOUBLE_PATTERN = "\\d+.\\d+";
+	
+	//constructor
+	/**
+	 * Prevents that an instance of the {@link RegularExpressionStringPatternCatalogue} can be created.
+	 */
+	private RegularExpressionStringPatternCatalogue() {}
+}
