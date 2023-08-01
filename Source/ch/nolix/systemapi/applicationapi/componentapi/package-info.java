4a62e49ea7a81624a376760299330214870d71da
Nimeon <silvan.wyss@gmail.com>
2023-08-01 13:42:09 +0200

    created componentapi package

---
+++ Source/ch/nolix/systemapi/applicationapi/componentapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.applicationapi.componentapi} package declares
+ * components for web {@link ch.nolix.systemapi.applicationapi.mainapi.IApplication}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-01
+ */
+package ch.nolix.systemapi.applicationapi.componentapi;
