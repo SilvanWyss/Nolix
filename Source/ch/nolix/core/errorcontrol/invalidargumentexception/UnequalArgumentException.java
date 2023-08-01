84c3cb5771a9e0143d8f1d6019669fad6f56030f
Nimeon <silvan.wyss@gmail.com>
2023-08-01 17:40:57 +0200

    refactored UnequalArgumentException

--- Source/ch/nolix/core/errorcontrol/invalidargumentexception/UnequalArgumentException.java
+++ Source/ch/nolix/core/errorcontrol/invalidargumentexception/UnequalArgumentException.java
@@ -4,7 +4,7 @@ package ch.nolix.core.errorcontrol.invalidargumentexception;
 //class
 /**
  * A {@link UnequalArgumentException} is a {@link InvalidArgumentException} that
- * is supposed to be thrown when a given argument does undesirably not equal a certain .
+ * is supposed to be thrown when a given argument does undesirably not equal a given value.
  * 
  * @author Silvan Wyss
  * @date 2017-01-01
@@ -16,10 +16,9 @@ public final class UnequalArgumentException extends InvalidArgumentException {
 	/**
 	 * @param argument
 	 * @param value
-	 * @param <A> is the type of the given argument.
 	 * @return a new {@link UnequalArgumentException} for the given argument and value.
 	 */
-	public static <A> UnequalArgumentException forArgumentAndValue(final A argument, final A value) {
+	public static UnequalArgumentException forArgumentAndValue(final Object argument, final Object value) {
 		return new UnequalArgumentException(argument, value);
 	}
 	
@@ -34,44 +33,27 @@ public final class UnequalArgumentException extends InvalidArgumentException {
 	 */
 	public static UnequalArgumentException forArgumentNameAndArgumentAndValue(
 		final String argumentName,
-		final double argument,
-		final double value
+		final Object argument,
+		final Object value
 	) {
 		return new UnequalArgumentException(argumentName, argument, value);
 	}
 	
 	//static method
 	/**
-	 * @param argumentName
-	 * @param argument
 	 * @param value
-	 * @return a new {@link UnequalArgumentException} for the given argumentName, argument and value.
-	 * @throws IllegalArgumentException if the given argumentName is null.
-	 * @throws IllegalArgumentException if the given argumentName is blank.
+	 * @return a name for the given value.
+	 * @throws IllegalArgumentException if the given value is null.
 	 */
-	public static UnequalArgumentException forArgumentNameAndArgumentAndValue(
-		final String argumentName,
-		final long argument,
-		final long value
-	) {
-		return new UnequalArgumentException(argumentName, argument, value);
-	}
-	
-	//static method
-	/**
-	 * @param argumentName
-	 * @param argument
-	 * @param value
-	 * @return a new {@link UnequalArgumentException} for the given argumentName, argument and value.
-	 * @throws IllegalArgumentException if the given argumentName is null.
-	 * @throws IllegalArgumentException if the given argumentName is blank.
-	 */
-	public static UnequalArgumentException forArgumentNameAndArgumentAndValue(
-		final String argumentName,
-		final String argument,
-		final String value
-	) {
-		return new UnequalArgumentException(argumentName, argument, value);
+	private static String getNameForValue(final Object value) {
+		
+		//Asserts that the given argument is not null.
+		if (value == null) {
+			throw new IllegalArgumentException("The given value is null");
+		}
+		
+		//Returns a name for the given value.
+		return value.getClass().getSimpleName();
 	}
 	
 	//constructor
@@ -80,17 +62,16 @@ public final class UnequalArgumentException extends InvalidArgumentException {
 	 * 
 	 * @param argument
 	 * @param value
-	 * @param <A> is the type of the given argument.
 	 */
-	private <A> UnequalArgumentException(final A argument, final A value) {
+	private UnequalArgumentException(final Object argument, final Object value) {
 		
 		//Calls constructor of the base class.
-		super(argument, "does not equal the " + value.getClass().getSimpleName() + " '" + value + "'");
+		super(argument, "does not equal the " + getNameForValue(value) + " '" + value + "'");
 	}
 	
 	//constructor
 	/**
-	 * Creates a new {@link UnequalArgumentException} for the given argumentName, argument and value.
+	 * Creates a new {@link UnequalArgumentException} for the given argument and value.
 	 * 
 	 * @param argumentName
 	 * @param argument
@@ -98,41 +79,9 @@ public final class UnequalArgumentException extends InvalidArgumentException {
 	 * @throws IllegalArgumentException if the given argumentName is null.
 	 * @throws IllegalArgumentException if the given argumentName is blank.
 	 */
-	private UnequalArgumentException(final String argumentName, final double argument, final double value) {
+	private UnequalArgumentException(final String argumentName, final Object argument, final Object value) {
 		
 		//Calls constructor of the base class.
-		super(argumentName,	argument, "does not equal " + value);
-	}
-	
-	//constructor
-	/**
-	 * Creates a new {@link UnequalArgumentException} for the given argumentName, argument and value.
-	 * 
-	 * @param argumentName
-	 * @param argument
-	 * @param value
-	 * @throws IllegalArgumentException if the given argumentName is null.
-	 * @throws IllegalArgumentException if the given argumentName is blank.
-	 */
-	private UnequalArgumentException(final String argumentName, final long argument, final long value) {
-		
-		//Calls constructor of the base class.
-		super(argumentName,	argument, "does not equal " + value);
-	}
-	
-	//constructor
-	/**
-	 * Creates a new {@link UnequalArgumentException} for the given argumentName, argument and value.
-	 * 
-	 * @param argumentName
-	 * @param argument
-	 * @param value
-	 * @throws IllegalArgumentException if the given argumentName is null.
-	 * @throws IllegalArgumentException if the given argumentName is blank.
-	 */
-	private UnequalArgumentException(final String argumentName, final String argument, final String value) {
-		
-		//Calls constructor of the base class.
-		super(argumentName,	argument, "does not equal " + value);
+		super(argumentName, argument, "does not equal the " + getNameForValue(value) + " '" + value + "'");
 	}
 }
