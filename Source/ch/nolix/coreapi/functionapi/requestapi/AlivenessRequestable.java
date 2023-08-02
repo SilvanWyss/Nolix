e8d0eb323ef83c7db557cfee26f89d759f961d41
Nimeon <silvan.wyss@gmail.com>
2023-08-02 20:20:28 +0200

    created AlivenessRequestable

---
+++ Source/ch/nolix/coreapi/functionapi/requestapi/AlivenessRequestable.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.coreapi.functionapi.requestapi;
+
+//interface
+import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
+
+//interface
+/**
+* A {@link AlivenessRequestable} can be asked if it is alive or outdated.
+* 
+* @author Silvan Wyss
+* @date 2023-08-02
+*/
+@AllowDefaultMethodsAsDesignPattern
+public interface AlivenessRequestable {
+	
+	//method declaration
+	/**
+	 * @return true if the current {@link AlivenessRequestable} is alive.
+	 */
+	boolean isAlive();
+	
+	//method
+	/**
+	 * @return true if the current {@link AlivenessRequestable} is outdated.
+	 */
+	default boolean isOutdated() {
+		return !isAlive();
+	}
+}
