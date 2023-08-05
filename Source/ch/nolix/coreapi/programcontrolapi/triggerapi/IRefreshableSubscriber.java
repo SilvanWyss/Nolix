be56990009e94b1bd3b5e80a8fa8414e47037bd7
Nimeon <silvan.wyss@gmail.com>
2023-08-05 17:35:30 +0200

    created IRefreshableSubscriber

---
+++ Source/ch/nolix/coreapi/programcontrolapi/triggerapi/IRefreshableSubscriber.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.coreapi.programcontrolapi.triggerapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.requestapi.AlivenessRequestable;
+
+//interface
+public interface IRefreshableSubscriber extends AlivenessRequestable, Refreshable {}
