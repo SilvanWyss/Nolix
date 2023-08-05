55b0556d7ae1aa3138daf7c8365bdc798c569241
Nimeon <silvan.wyss@gmail.com>
2023-08-05 17:34:23 +0200

    created ITriggerableSubscriber

---
+++ Source/ch/nolix/coreapi/programcontrolapi/triggerapi/ITriggerableSubscriber.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.coreapi.programcontrolapi.triggerapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.requestapi.AlivenessRequestable;
+
+//interface
+public interface ITriggerableSubscriber extends AlivenessRequestable, Triggerable {}
