08f7b574e0f7afb4ce1596e52bfa0921f6a95aa1
Nimeon <silvan.wyss@gmail.com>
2023-08-06 12:33:06 +0200

    created applicationcontextapi package

---
+++ Source/ch/nolix/systemapi/applicationapi/applicationcontextapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.applicationapi.applicationcontextapi} package declares types for application contexts.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-06
+ */
+package ch.nolix.systemapi.applicationapi.applicationcontextapi;
