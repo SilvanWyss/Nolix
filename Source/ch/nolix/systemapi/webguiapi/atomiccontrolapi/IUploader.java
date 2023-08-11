0b7de692b251af84be9daf73f5aaab99d4315b17
Nimeon <silvan.wyss@gmail.com>
2023-08-11 13:12:37 +0200

    created IUploader

---
+++ Source/ch/nolix/systemapi/webguiapi/atomiccontrolapi/IUploader.java
@@ -0,0 +1,13 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.atomiccontrolapi;
+
+//own imports
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IElementTaker;
+import ch.nolix.systemapi.webguiapi.mainapi.IControl;
+
+//interface
+public interface IUploader extends IControl<IUploader, IUploaderStyle> {
+	
+	//method declaration
+	IUploader setFileTaker(IElementTaker<Byte[]> file);
+}
---
+++ Source/ch/nolix/systemapi/webguiapi/atomiccontrolapi/IUploaderStyle.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.atomiccontrolapi;
+
+//own imports
+import ch.nolix.systemapi.webguiapi.controlstyleapi.IControlStyle;
+
+//interface
+public interface IUploaderStyle extends IControlStyle<IUploaderStyle> {}
