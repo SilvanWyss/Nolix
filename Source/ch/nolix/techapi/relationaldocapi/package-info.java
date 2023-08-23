4effd3bdad3a7349ae6664a472bf54e079605213
Nimeon <silvan.wyss@gmail.com>
2023-08-23 23:28:37 +0200

    created relationaldocapi package

---
+++ Source/ch/nolix/techapi/relationaldocapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.relationaldocapi} package declares a relational document model.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-23
+ */
+package ch.nolix.techapi.relationaldocapi;
