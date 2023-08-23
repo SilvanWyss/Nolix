2a91e41f1a426ea8fc27906fadc4da7559562814
Nimeon <silvan.wyss@gmail.com>
2023-08-23 23:03:29 +0200

    created fieldproperty package

---
+++ Source/ch/nolix/coreapi/datamodelapi/fieldproperty/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.datamodelapi.fieldproperty} package defines properties for fields.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-23
+ */
+package ch.nolix.coreapi.datamodelapi.fieldproperty;
