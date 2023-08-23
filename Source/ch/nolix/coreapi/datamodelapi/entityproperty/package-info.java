b2ce3d47220c6cf508735a0e0e3a4c023d7c3fe1
Nimeon <silvan.wyss@gmail.com>
2023-08-23 23:05:43 +0200

    created entityproperty package

---
+++ Source/ch/nolix/coreapi/datamodelapi/entityproperty/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.datamodelapi.entityproperty} package defines properties for entities.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-23
+ */
+package ch.nolix.coreapi.datamodelapi.entityproperty;
