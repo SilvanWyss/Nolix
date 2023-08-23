568b49bf87763852e4c37ef1e9134daace256f25
Nimeon <silvan.wyss@gmail.com>
2023-08-23 22:57:53 +0200

    created datamodelapi package

---
+++ Source/ch/nolix/coreapi/datamodelapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.datamodelapi} package declares types for data models.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-23
+ */
+package ch.nolix.coreapi.datamodelapi;
