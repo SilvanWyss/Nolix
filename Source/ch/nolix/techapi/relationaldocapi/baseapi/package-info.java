357ae917cd8c9bf1f5eb38d064f663d90e0213f4
Nimeon <silvan.wyss@gmail.com>
2023-08-25 20:35:34 +0200

    created baseapi package

---
+++ Source/ch/nolix/techapi/relationaldocapi/baseapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.relationaldocapi.baseapi} package declares
+ * the base types of a relational document model.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-25
+ */
+package ch.nolix.techapi.relationaldocapi.baseapi;
