7bd99fa5b8bdf9d904fa9be2205a577c30a6042f
Nimeon <silvan.wyss@gmail.com>
2023-08-25 22:10:22 +0200

    made IAbstractableField a IFluentMutableCardinalityHolder

--- Source/ch/nolix/techapi/relationaldocapi/mainapi/IAbstractableField.java
+++ Source/ch/nolix/techapi/relationaldocapi/mainapi/IAbstractableField.java
@@ -3,15 +3,12 @@ package ch.nolix.techapi.relationaldocapi.mainapi;
 
 //own imports
 import ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi.FluentNameable;
-import ch.nolix.coreapi.datamodelapi.cardinalityapi.Cardinality;
+import ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi.IFluentMutableCardinalityHolder;
 import ch.nolix.techapi.relationaldocapi.baseapi.Abstractable;
 
 //interface
-public interface IAbstractableField extends Abstractable<IAbstractableField>, FluentNameable<IAbstractableField> {
-	
-	//method declaration
-	Cardinality getCardinality();
-	
-	//method declaration
-	IAbstractableField setCardinality(Cardinality cardinality);
-}
+public interface IAbstractableField
+extends
+Abstractable<IAbstractableField>,
+FluentNameable<IAbstractableField>,
+IFluentMutableCardinalityHolder<IAbstractableField> {}
