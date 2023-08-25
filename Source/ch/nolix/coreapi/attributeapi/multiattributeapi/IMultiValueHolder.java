386ceabe5abd18d507b2a4dcc1839c94fc414a07
Nimeon <silvan.wyss@gmail.com>
2023-08-25 22:55:37 +0200

    created IMultiValueHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/multiattributeapi/IMultiValueHolder.java
@@ -0,0 +1,22 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.multiattributeapi;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+
+//interface
+/**
+* A {@link IMultiValueHolder} can contain several values.
+* 
+* @author Silvan Wyss
+* @date 2023-08-25
+* @param <V> is the type of the values of a {@link IMultiValueHolder}.
+*/
+public interface IMultiValueHolder<V> {
+	
+	//method declaration
+	/**
+	 * @return the values of the current {@link MultiValueed}.
+	 */
+	IContainer<V> getStoredValues();
+}
