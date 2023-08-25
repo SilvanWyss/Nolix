f50920769f0a07de603a3d0a7c8682ac487e92d1
Nimeon <silvan.wyss@gmail.com>
2023-08-25 20:20:07 +0200

    created IAbstractableObject

---
+++ Source/ch/nolix/techapi/relationaldocapi/mainapi/IAbstractableObject.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.techapi.relationaldocapi.mainapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi.FluentNameable;
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.datamodelapi.entityrequestapi.AbstractnessRequestable;
+
+//interface
+public interface IAbstractableObject extends AbstractnessRequestable, FluentNameable<IAbstractableObject> {
+	
+	//method declaration
+	IContainer<IAbstractableObject> getTypes();
+}
