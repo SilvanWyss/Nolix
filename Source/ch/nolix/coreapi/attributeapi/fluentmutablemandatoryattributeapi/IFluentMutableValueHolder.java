5f136ab50d52e925cac064ceed8f0b9563c22343
Nimeon <silvan.wyss@gmail.com>
2023-08-25 22:11:27 +0200

    named IFluentMutableValueHolder correctly

---
+++ Source/ch/nolix/coreapi/attributeapi/fluentmutablemandatoryattributeapi/IFluentMutableValueHolder.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi;
+
+import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IValueHolder;
+
+//interface
+/**
+ * A {@link IFluentMutableValueHolder} is a {@link IValueHolder} whose value can be set programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2018-09-06
+ * @param <FVH> is the type of a {@link IFluentMutableValueHolder}.
+ * @param <V> is the type of the value of a {@link IFluentMutableValueHolder}.
+ */
+public interface IFluentMutableValueHolder<
+	FVH extends IFluentMutableValueHolder<FVH, V>,
+	V
+>
+extends IValueHolder<V> {
+	
+	//method declaration
+	/**
+	 * Sets the value of the current {@link IFluentMutableValueHolder}.
+	 * 
+	 * @param value
+	 * @return the current {@link IFluentMutableValueHolder}.
+	 * @throws RuntimeException if the given value is null.
+	 */
+	FVH setValue(V value);
+}
