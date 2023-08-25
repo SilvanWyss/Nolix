4dbade29f07c5c96afa6030f3cff31b9e4cff405
Nimeon <silvan.wyss@gmail.com>
2023-08-25 19:56:05 +0200

    created entityrequestapi package

---
+++ Source/ch/nolix/coreapi/datamodelapi/entityrequestapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.datamodelapi.entityrequestapi} package declares types for entities
+ * that can be asked for certain information.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-25
+ */
+package ch.nolix.coreapi.datamodelapi.entityrequestapi;
