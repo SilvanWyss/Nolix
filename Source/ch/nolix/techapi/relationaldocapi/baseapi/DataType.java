a244bab0def6a2adde6f845963dd5a7fd3c16a50
Nimeon <silvan.wyss@gmail.com>
2023-08-25 21:37:36 +0200

    created DataType enum

---
+++ Source/ch/nolix/techapi/relationaldocapi/baseapi/DataType.java
@@ -0,0 +1,11 @@
+//package declaration
+package ch.nolix.techapi.relationaldocapi.baseapi;
+
+//enum
+public enum DataType {
+	INTEGER_8BYTE,
+	FLOATING_POINT_NUMBER_8BYTE,
+	BOOLEAN,
+	TEXT,
+	IMAGE
+}
