1a44f61c952c387986f6d327771e9d2fb1e5a6e0
Nimeon <silvan.wyss@gmail.com>
2023-08-25 22:03:25 +0200

    created ICardinalityHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/mandatoryattributeapi/ICardinalityHolder.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mandatoryattributeapi;
+
+//own imports
+import ch.nolix.coreapi.datamodelapi.cardinalityapi.Cardinality;
+
+//interface
+/**
+ * A {@link ICardinalityHolder} has a {@link Cardinality}.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-25
+ */
+public interface ICardinalityHolder {
+	
+	//method declaration
+	/**
+	 * @return the {@link Cardinality} of the current {@link ICardinalityHolder}.
+	 */
+	Cardinality getCardinality();
+}
