96a758153c22269f140269133e2a13956a27efca
Nimeon <silvan.wyss@gmail.com>
2023-08-25 20:04:18 +0200

    created mainapi package

---
+++ Source/ch/nolix/techapi/relationaldocapi/mainapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.relationaldocapi.mainapi} package declares
+ * the main types of a relational document model.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-25
+ */
+package ch.nolix.techapi.relationaldocapi.mainapi;
