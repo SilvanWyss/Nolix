50b3d2c3239d32057ef078b1dc75f829f799afc0
Nimeon <silvan.wyss@gmail.com>
2023-08-26 16:42:24 +0200

    edited DataType

--- Source/ch/nolix/techapi/relationaldocapi/baseapi/DataType.java
+++ Source/ch/nolix/techapi/relationaldocapi/baseapi/DataType.java
@@ -1,11 +1,27 @@
 //package declaration
 package ch.nolix.techapi.relationaldocapi.baseapi;
 
+//own imports
+import ch.nolix.systemapi.graphicapi.imageapi.IImage;
+
 //enum
 public enum DataType {
-	INTEGER_8BYTE,
-	FLOATING_POINT_NUMBER_8BYTE,
-	BOOLEAN,
-	TEXT,
-	IMAGE
+	INTEGER_8BYTE(Long.class),
+	FLOATING_POINT_NUMBER_8BYTE(Double.class),
+	BOOLEAN(Boolean.class),
+	TEXT(String.class),
+	IMAGE(IImage.class);
+	
+	//attribute
+	private final Class<?> dataTypeClass;
+	
+	//constructor
+	<V> DataType(final Class<V> dataTypeClass) {
+		this.dataTypeClass = dataTypeClass;
+	}
+	
+	//method
+	public Class<?> getDataTypeClass() {
+		return dataTypeClass;
+	}
 }
