ebd4916720ce00d7d3f7a28d44719e227f2ccdcb
Nimeon <silvan.wyss@gmail.com>
2023-08-27 20:11:44 +0200

    created datavalidator package

---
+++ Source/ch/nolix/tech/relationaldoc/datavalidator/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.tech.relationaldoc.datavalidator} package defines data validators.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-27
+ */
+package ch.nolix.tech.relationaldoc.datavalidator;
