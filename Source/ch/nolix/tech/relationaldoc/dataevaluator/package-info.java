42dfab231254f41c5c552a915e8714d3e1b39593
Nimeon <silvan.wyss@gmail.com>
2023-08-27 22:41:16 +0200

    created dataevaluator package

---
+++ Source/ch/nolix/tech/relationaldoc/dataevaluator/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.tech.relationaldoc.dataevaluator} package defines data evaluators.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-08-27
+ */
+package ch.nolix.tech.relationaldoc.dataevaluator;
