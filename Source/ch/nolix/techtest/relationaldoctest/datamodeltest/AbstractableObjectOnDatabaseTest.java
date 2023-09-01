f3efa770d2aab8ec6f16237010750c063b1bb140
Nimeon <silvan.wyss@gmail.com>
2023-09-01 17:14:53 +0200

    extended AbstractableObjectOnDatabaseTest

--- Source/ch/nolix/techtest/relationaldoctest/datamodeltest/AbstractableObjectOnDatabaseTest.java
+++ Source/ch/nolix/techtest/relationaldoctest/datamodeltest/AbstractableObjectOnDatabaseTest.java
@@ -16,17 +16,17 @@ public final class AbstractableObjectOnDatabaseTest extends Test {
 	@TestCase
 	public void testCase_getStoredBaseTypes_whenDoesNotHaveBaseTypes() {
 		
-		//setup database
+		//setup part 1: Create database.
 		final var database = new MutableNode();
 		
-		//setup databaseAdapter
+		//setup part 2: Create databaseAdapter.
 		final var databaseAdapter =
 		NodeDataAdapter
 		.forNodeDatabase(database)
 		.withName("test_database")
 		.usingSchema(SchemaCatalogue.RELATIONAL_DOC_SCHEMA);
 		
-		//setup testUnit
+		//setup part 3: Create testUnit.
 		final var testUnit = new AbstractableObject();
 		databaseAdapter.insert(testUnit);
 		
@@ -35,5 +35,45 @@ public final class AbstractableObjectOnDatabaseTest extends Test {
 		
 		//verification
 		expect(result).isEmpty();
+	}
+	
+	//method
+	@TestCase
+	public void testCase_getStoredBaseTypes_whenHasBaseTypeWithBaseType() {
+		
+		//setup part 1: Create database.
+		final var database = new MutableNode();
+		
+		//setup part 2: Create databaseAdapter.
+		final var databaseAdapter =
+		NodeDataAdapter
+		.forNodeDatabase(database)
+		.withName("test_database")
+		.usingSchema(SchemaCatalogue.RELATIONAL_DOC_SCHEMA);
+		
+		//setup part 3: Create level1Type.
+		final var level1Oject = new AbstractableObject();
+		databaseAdapter.insert(level1Oject);
+		level1Oject.setName("level_1_object");
+		level1Oject.setAsAbstract();
+		
+		//setup part 4: Create level2Type.
+		final var level2Oject = new AbstractableObject();
+		databaseAdapter.insert(level2Oject);
+		level2Oject.setName("level_2_object");
+		level2Oject.setAsAbstract();
+		level2Oject.addBaseType(level1Oject);
+		
+		//setup part 3: Create testUnit.
+		final var testUnit = new AbstractableObject();
+		databaseAdapter.insert(testUnit);
+		testUnit.setName("test_unit");
+		testUnit.addBaseType(level2Oject);
+		
+		//execution
+		final var result = testUnit.getStoredBaseTypes();
+		
+		//verification
+		expect(result).containsExactly(level1Oject, level2Oject);
 	}
 }
