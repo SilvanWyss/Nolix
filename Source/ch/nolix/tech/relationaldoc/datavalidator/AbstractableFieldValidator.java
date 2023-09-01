6e061ec6ff2ed3f173795d6742bb210ac0631877
Nimeon <silvan.wyss@gmail.com>
2023-09-01 12:45:23 +0200

    edited AbstractableFieldValidator

--- Source/ch/nolix/tech/relationaldoc/datavalidator/AbstractableFieldValidator.java
+++ Source/ch/nolix/tech/relationaldoc/datavalidator/AbstractableFieldValidator.java
@@ -2,18 +2,29 @@
 package ch.nolix.tech.relationaldoc.datavalidator;
 
 //own imports
+import ch.nolix.core.errorcontrol.invalidargumentexception.InvalidArgumentException;
+import ch.nolix.core.programatom.name.LowerCaseCatalogue;
+import ch.nolix.tech.relationaldoc.dataevaluator.AbstractableFieldEvaluator;
 import ch.nolix.techapi.relationaldocapi.datamodelapi.IAbstractableField;
 
 //class
 public final class AbstractableFieldValidator {
 	
+	//constant
+	private static final AbstractableFieldEvaluator ABSTRACTABLE_FIELD_EVALUATOR = new AbstractableFieldEvaluator();
+	
 	//method
 	public void assertCanSetAsAbstract(final IAbstractableField abstractableField) {
-		//TODO: Implement.
+		if (!ABSTRACTABLE_FIELD_EVALUATOR.canBeSetAsAbstract(abstractableField)) {
+			throw
+			InvalidArgumentException.forArgumentAndErrorPredicate(abstractableField, "cannot be set as abstract");
+		}
 	}
 	
 	//method
 	public void assertCanSetName(final IAbstractableField abstractableField, final String name) {
-		//TODO: Implement.
+		if (!ABSTRACTABLE_FIELD_EVALUATOR.canSetName(abstractableField, name)) {
+			throw InvalidArgumentException.forArgumentNameAndArgument(LowerCaseCatalogue.NAME, name);
+		}
 	}
 }
