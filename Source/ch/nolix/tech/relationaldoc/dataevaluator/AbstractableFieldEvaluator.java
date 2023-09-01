6e4af2ee98e792a7bc6f40f8f50e3abb68832a89
Nimeon <silvan.wyss@gmail.com>
2023-09-01 12:37:05 +0200

    created AbstractableFieldEvaluator

---
+++ Source/ch/nolix/tech/relationaldoc/dataevaluator/AbstractableFieldEvaluator.java
@@ -0,0 +1,51 @@
+//package declaration
+package ch.nolix.tech.relationaldoc.dataevaluator;
+
+//own imports
+import ch.nolix.techapi.relationaldocapi.datamodelapi.IAbstractableField;
+
+//class
+public final class AbstractableFieldEvaluator {
+	
+	//method
+	public boolean canBeSetAsAbstract(final IAbstractableField abstractableField) {
+		return
+		abstractableField != null
+		&& !abstractableField.inheritsFromBaseField()
+		&& canBeSetAsAbstractBecauseOfParentObject(abstractableField);
+	}
+	
+	//method
+	public boolean canSetName(final IAbstractableField abstractableField, final String name) {
+		return
+		canSetName(name)
+		&& abstractableField != null
+		&& abstractableField.getStoredParentObject().getStoredFields().containsNone(f -> f.hasName(name))
+		&& canSetNameBecauseOfSubTypesOfParentObject(abstractableField, name);
+	}
+	
+	//method
+	private boolean canBeSetAsAbstractBecauseOfParentObject(final IAbstractableField abstractableField) {
+		return
+		abstractableField != null
+		&& abstractableField.getStoredParentObject().isAbstract();
+	}
+	
+	//method
+	private boolean canSetName(final String name) {
+		return
+		name != null
+		&& !name.isBlank();
+	}
+	
+	//method
+	private boolean canSetNameBecauseOfSubTypesOfParentObject(
+		final IAbstractableField abstractableField,
+		final String name
+	) {
+		return
+		abstractableField != null
+		&&
+		abstractableField.getStoredParentObject().getStoredConcreteSubTypes().containsNone(cst -> cst.hasName(name));
+	}
+}
