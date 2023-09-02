20010bb4da845fd90deee90547d668a08f1a2fac
Nimeon <silvan.wyss@gmail.com>
2023-09-02 23:40:00 +0200

    created ReferenceContent

---
+++ Source/ch/nolix/tech/relationaldoc/datamodel/ReferenceContent.java
@@ -0,0 +1,8 @@
+//package declaration
+package ch.nolix.tech.relationaldoc.datamodel;
+
+//own imports
+import ch.nolix.techapi.relationaldocapi.datamodelapi.IReferenceContent;
+
+//class
+public abstract class ReferenceContent extends Content implements IReferenceContent {}
