ee6349ce0a4f7f7c1f46d95ec123fd16a7c4af77
Nimeon <silvan.wyss@gmail.com>
2023-09-03 11:13:29 +0200

    added getStoredRealisingFields method to AbstractableFieldEvaluator

--- Source/ch/nolix/tech/relationaldoc/dataevaluator/AbstractableFieldEvaluator.java
+++ Source/ch/nolix/tech/relationaldoc/dataevaluator/AbstractableFieldEvaluator.java
@@ -2,6 +2,8 @@
 package ch.nolix.tech.relationaldoc.dataevaluator;
 
 //own imports
+import ch.nolix.core.container.immutablelist.ImmutableList;
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.techapi.relationaldocapi.datamodelapi.IAbstractableField;
 
 //class
@@ -22,6 +24,16 @@ public final class AbstractableFieldEvaluator {
 		&& abstractableField != null
 		&& abstractableField.getStoredParentObject().getStoredFields().containsNone(f -> f.hasName(name))
 		&& canSetNameBecauseOfSubTypesOfParentObject(abstractableField, name);
+	}
+	
+	//method
+	public IContainer<? extends IAbstractableField> getStoredRealisingFields(final IAbstractableField field) {
+		
+		if (field == null || field.isConcrete()) {
+			return new ImmutableList<>();
+		}
+		
+		return getStoredRealisingFieldsWhenIsAbstract(field);
 	}
 	
 	//method
@@ -47,5 +59,14 @@ public final class AbstractableFieldEvaluator {
 		abstractableField != null
 		&&
 		abstractableField.getStoredParentObject().getStoredSubTypes().containsNone(cst -> cst.hasName(name));
+	}
+	
+	//method
+	private IContainer<? extends IAbstractableField> getStoredRealisingFieldsWhenIsAbstract(final IAbstractableField field) {
+		return
+		field
+		.getStoredParentObject()
+		.getStoredSubTypes()
+		.toFromGroups(st -> st.getStoredDeclaredFields().getStoredSelected(df -> df.hasSameNameAs(field)));
 	}
 }
