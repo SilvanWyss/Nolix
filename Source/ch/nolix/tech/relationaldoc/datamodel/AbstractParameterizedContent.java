8c502c861a784f9ca8620c3a34b9625455733fec
Nimeon <silvan.wyss@gmail.com>
2023-09-08 22:22:32 +0200

    deleted IParameterizedValueContent

--- Source/ch/nolix/tech/relationaldoc/datamodel/AbstractParameterizedContent.java
+++
@@ -1,9 +0,0 @@
-//package declaration
-package ch.nolix.tech.relationaldoc.datamodel;
-
-//own imports
-import ch.nolix.system.objectdatabase.database.Entity;
-import ch.nolix.techapi.relationaldocapi.datamodelapi.IParameterizedValueContent;
-
-//class
-public abstract class AbstractParameterizedContent<V> extends Entity implements IParameterizedValueContent<V> {}
