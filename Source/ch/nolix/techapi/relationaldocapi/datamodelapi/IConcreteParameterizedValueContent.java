060caf9d44a7d98f7d92026eb533f8128e3d900b
Nimeon <silvan.wyss@gmail.com>
2023-09-08 20:21:14 +0200

    made IConcreteParameterizedValueContent a EmptinessRequestable

--- Source/ch/nolix/techapi/relationaldocapi/datamodelapi/IConcreteParameterizedValueContent.java
+++ Source/ch/nolix/techapi/relationaldocapi/datamodelapi/IConcreteParameterizedValueContent.java
@@ -3,10 +3,12 @@ package ch.nolix.techapi.relationaldocapi.datamodelapi;
 
 //own imports
 import ch.nolix.coreapi.attributeapi.fluentmutablemultiattributeapi.IFluentMutableMultiValueHolder;
+import ch.nolix.coreapi.functionapi.requestapi.EmptinessRequestable;
 
 //interface
 public interface IConcreteParameterizedValueContent<V>
 extends
+EmptinessRequestable,
 IFluentMutableMultiValueHolder<IConcreteParameterizedValueContent<V>, V>,
 IParameterizedValueContent<V> {
 	
