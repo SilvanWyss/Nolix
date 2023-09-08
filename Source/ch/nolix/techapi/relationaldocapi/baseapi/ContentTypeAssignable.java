ea2afb044c929bb5bddfddb7d35004dbd6c65fa7
Nimeon <silvan.wyss@gmail.com>
2023-09-08 10:45:23 +0200

    created ContentTypeAssignable

---
+++ Source/ch/nolix/techapi/relationaldocapi/baseapi/ContentTypeAssignable.java
@@ -0,0 +1,15 @@
+//package declaration
+package ch.nolix.techapi.relationaldocapi.baseapi;
+
+//own imports
+import ch.nolix.coreapi.datamodelapi.entityrequestapi.ContentTypeRequestable;
+
+//interface
+public interface ContentTypeAssignable<CTA extends ContentTypeAssignable<CTA>> extends ContentTypeRequestable {
+	
+	//method declaration
+	CTA setForReferences();
+	
+	//method declaration
+	CTA setForValues();
+}
