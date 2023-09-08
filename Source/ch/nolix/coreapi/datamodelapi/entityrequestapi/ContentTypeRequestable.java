9708ae38e397b0368565b7b85ff3428cdcc90f65
Nimeon <silvan.wyss@gmail.com>
2023-09-08 10:34:38 +0200

    created ContentTypeRequestable

---
+++ Source/ch/nolix/coreapi/datamodelapi/entityrequestapi/ContentTypeRequestable.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.coreapi.datamodelapi.entityrequestapi;
+
+//own imports
+import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
+
+//interface
+/**
+ * A {@link ContentTypeRequestable} can be asked if it either is for holding values or references.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-09-08
+ */
+@AllowDefaultMethodsAsDesignPattern
+public interface ContentTypeRequestable {
+	
+	//method
+	/**
+	 * @return true if the current {@link ContentTypeRequestable} is for references, false otherwise.
+	 */
+	default boolean isForReferences() {
+		return !isForValues();
+	}
+	
+	//method declaration
+	/**
+	 * @return true if the current {@link ContentTypeRequestable} is for values, false otherwiese.
+	 */
+	boolean isForValues();
+}
