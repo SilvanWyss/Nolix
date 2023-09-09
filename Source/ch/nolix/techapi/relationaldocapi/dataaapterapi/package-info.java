0a4f78f86bebb293331b1f61a38838610cd6a6fe
Nimeon <silvan.wyss@gmail.com>
2023-09-09 18:11:52 +0200

    created dataaapterapi package

---
+++ Source/ch/nolix/techapi/relationaldocapi/dataaapterapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.relationaldocapi.dataaapterapi} package declares
+ * the data adapter of a relational document model.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-09-09
+ */
+package ch.nolix.techapi.relationaldocapi.dataaapterapi;
