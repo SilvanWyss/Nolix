c2a9f30eb5a57033b3c094640fa7c50e1818a96a
Nimeon <silvan.wyss@gmail.com>
2023-09-09 18:12:15 +0200

    created IDataAdapter

---
+++ Source/ch/nolix/techapi/relationaldocapi/dataaapterapi/IDataAdapter.java
@@ -0,0 +1,20 @@
+//package declaration
+package ch.nolix.techapi.relationaldocapi.dataaapterapi;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.programcontrolapi.savecontrolapi.IChangeSaver;
+import ch.nolix.techapi.relationaldocapi.datamodelapi.IAbstractableObject;
+
+//interface
+public interface IDataAdapter extends IChangeSaver {
+	
+	//method declaration
+	IAbstractableObject createObject();
+	
+	//method declaration
+	void deleteObject(IAbstractableObject object);
+	
+	//method declaration
+	IContainer<? extends IAbstractableObject> getStoredTopLevelObjects();
+}
