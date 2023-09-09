bf6efdc9f3702c0b04ece92db85d3ebfd7983be0
Nimeon <silvan.wyss@gmail.com>
2023-09-09 18:12:45 +0200

    created dataadapter package

---
+++ Source/ch/nolix/tech/relationaldoc/dataadapter/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.tech.relationaldoc.dataadapter} package provides
+ * an implementation of the {@link ch.nolix.techapi.relationaldocapi.dataaapterapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-09-09
+ */
+package ch.nolix.tech.relationaldoc.dataadapter;
