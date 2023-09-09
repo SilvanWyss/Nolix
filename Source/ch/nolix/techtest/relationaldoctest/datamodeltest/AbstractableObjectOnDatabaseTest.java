646000f4c7762c4a82fcf5059e677043110d3552
Nimeon <silvan.wyss@gmail.com>
2023-09-09 11:34:28 +0200

    extended AbstractableObjectOnDatabaseTest

--- Source/ch/nolix/techtest/relationaldoctest/datamodeltest/AbstractableObjectOnDatabaseTest.java
+++ Source/ch/nolix/techtest/relationaldoctest/datamodeltest/AbstractableObjectOnDatabaseTest.java
@@ -6,6 +6,7 @@ import ch.nolix.core.document.node.MutableNode;
 import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.core.testing.test.Test;
 import ch.nolix.system.objectdatabase.dataadapter.NodeDataAdapter;
+import ch.nolix.tech.relationaldoc.datamodel.AbstractableField;
 import ch.nolix.tech.relationaldoc.datamodel.AbstractableObject;
 import ch.nolix.tech.relationaldoc.datamodel.SchemaCatalogue;
 
@@ -179,5 +180,30 @@ public final class AbstractableObjectOnDatabaseTest extends Test {
 		
 		//verification
 		expect(result).containsExactly(subType1, subType2);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_isSaved_whenNewFieldWasAdded() {
+		
+		//setup part 1: Create database.
+		final var database = new MutableNode();
+		
+		//setup part 2: Create databaseAdapter.
+		final var databaseAdapter =
+		NodeDataAdapter
+		.forNodeDatabase(database)
+		.withName("test_database")
+		.andSchema(SchemaCatalogue.RELATIONAL_DOC_SCHEMA);
+		
+		//setup part 3: Create testUnit.
+		final var testUnit = new AbstractableObject();
+		databaseAdapter.insert(testUnit);
+		final var field = new AbstractableField();
+		databaseAdapter.insert(field);
+		testUnit.addField(field);
+		
+		//execution & verification
+		expectRunning(databaseAdapter::saveChanges).doesNotThrowException();
 	}
 }
