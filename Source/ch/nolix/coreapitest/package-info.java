7947f4f97d3376ca2c1e4630e48d641834b68b90
Nimeon <silvan.wyss@gmail.com>
2023-09-29 14:56:01 +0200

    created coreapitest package

---
+++ Source/ch/nolix/coreapitest/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapitest} package defines tests for the {@link ch.nolix.coreapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-09-29
+ */
+package ch.nolix.coreapitest;
