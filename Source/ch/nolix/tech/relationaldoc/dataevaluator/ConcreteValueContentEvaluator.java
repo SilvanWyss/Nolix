931630bfaa8e88dcaaaaeed9fae39dd781692ef3
Nimeon <silvan.wyss@gmail.com>
2023-09-29 10:58:33 +0200

    edited ConcreteValueContentEvaluator

--- Source/ch/nolix/tech/relationaldoc/dataevaluator/ConcreteValueContentEvaluator.java
+++ Source/ch/nolix/tech/relationaldoc/dataevaluator/ConcreteValueContentEvaluator.java
@@ -11,9 +11,9 @@ public final class ConcreteValueContentEvaluator {
 	
 	//method
 	public boolean canAddValue(final IConcreteValueContent concreteValueContent, final String value) {
-		
-		//TODO: Implement.
-		return true;
+		return
+		canAddValueBecauseOfCardinality(concreteValueContent)
+		&& canAddValueBecauseOfConstraints(concreteValueContent, value);
 	}
 	
 	//method
@@ -39,5 +39,23 @@ public final class ConcreteValueContentEvaluator {
 		concreteValueContent != null
 		&& !concreteValueContent.getStoredParentField().inheritsFromBaseField()
 		&& dataType != null;
+	}
+	
+	//method
+	private boolean canAddValueBecauseOfCardinality(final IConcreteValueContent concreteValueContent) {
+		return
+		concreteValueContent != null
+		&& (
+			concreteValueContent.isEmpty()
+			|| concreteValueContent.getStoredParentField().getCardinality() == Cardinality.TO_MANY
+		);
+	}
+	
+	//method
+	private boolean canAddValueBecauseOfConstraints(
+		final IConcreteValueContent concreteValueContent,
+		final String value
+	) {
+		return concreteValueContent.getConstraints().containsOnly(c -> c.acceptsEntry(value));
 	}
 }
