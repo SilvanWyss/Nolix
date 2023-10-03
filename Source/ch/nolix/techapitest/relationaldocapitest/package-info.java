42b183f4cef8f25a8c88eff26add3948d80258b1
Nimeon <silvan.wyss@gmail.com>
2023-10-03 22:21:49 +0200

    created relationaldocapitest package

---
+++ Source/ch/nolix/techapitest/relationaldocapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techapitest.relationaldocapitest} package defines
+ * tests for the {@link ch.nolix.techapi.relationaldocapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-03
+ */
+package ch.nolix.techapitest.relationaldocapitest;
