76d99f593ee7b1e66fe02112ff09aa40cb252b26
Nimeon <silvan.wyss@gmail.com>
2023-10-03 22:17:45 +0200

    created techapitest package

---
+++ Source/ch/nolix/techapitest/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.techapitest} package defines tests for the {@link ch.nolix.techapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-03
+ */
+package ch.nolix.techapitest;
