963a96965a8ad914e2c4c476bfd709ae5235144f
Nimeon <silvan.wyss@gmail.com>
2023-10-07 22:45:34 +0200

    created IProperty

--- Source/ch/nolix/system/element/property/Property.java
+++ Source/ch/nolix/system/element/property/Property.java
@@ -1,18 +1,8 @@
 //package declaration
 package ch.nolix.system.element.property;
 
-import ch.nolix.coreapi.containerapi.listapi.ILinkedList;
-import ch.nolix.coreapi.documentapi.nodeapi.INode;
+//own imports
+import ch.nolix.systemapi.elementapi.propertyapi.IProperty;
 
 //class
-public abstract class Property {
-	
-	//constructor
-	protected Property() {}
-	
-	//method declaration
-	public abstract boolean addedOrChangedAttribute(final INode<?> attribute);
-	
-	//method declaration
-	public abstract void fillUpAttributesInto(ILinkedList<INode<?>> list);
-}
+public abstract class Property implements IProperty {}
