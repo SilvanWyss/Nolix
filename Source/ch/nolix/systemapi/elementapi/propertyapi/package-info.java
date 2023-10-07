3c1c39985efc7e69f66ef0889f2dc4e111d5d0dc
Nimeon <silvan.wyss@gmail.com>
2023-10-07 22:37:08 +0200

    created propertyapi package

---
+++ Source/ch/nolix/systemapi/elementapi/propertyapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.elementapi.propertyapi} package declares properties.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-07
+ */
+package ch.nolix.systemapi.elementapi.propertyapi;
