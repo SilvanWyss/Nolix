5ec173a4d27ed1a651999c1a4da7c955793605ff
Nimeon <silvan.wyss@gmail.com>
2023-10-08 17:46:52 +0200

    extended WebGuiTest

--- Source/ch/nolix/systemtest/webguitest/maintest/WebGuiTest.java
+++ Source/ch/nolix/systemtest/webguitest/maintest/WebGuiTest.java
@@ -2,6 +2,7 @@
 package ch.nolix.systemtest.webguitest.maintest;
 
 //own imports
+import ch.nolix.core.errorcontrol.invalidargumentexception.InvalidArgumentException;
 import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.core.testing.test.Test;
 import ch.nolix.system.webgui.atomiccontrol.Label;
@@ -24,6 +25,53 @@ public final class WebGuiTest extends Test {
 		expect(result.getBackgroundColor()).isEqualTo(WebGui.DEFAULT_BACKGROUND_COLOR);
 		expect(result.isEmpty());
 		expect(result.getTokens()).isEmpty();
+	}
+	
+	//method
+	@TestCase
+	public void testCase_getStoredControlByInternalId_whenDoesNotContainSuchAControl() {
+		
+		//setup
+		final var control = new Label();
+		final var testUnit = new WebGui();
+		
+		//execution & verification
+		expectRunning(() -> testUnit.getStoredControlByInternalId(control.getInternalId()))
+		.throwsException()
+		.ofType(InvalidArgumentException.class);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_getStoredControlByInternalId_A1() {
+		
+		//setup
+		final var control = new Label();
+		final var testUnit = new WebGui();
+		testUnit.pushLayerWithRootControl(control);
+		
+		//execution
+		final var result = testUnit.getStoredControlByInternalId(control.getInternalId());
+		
+		//verification
+		expect(result).is(control);
+	}
+	
+	//method
+	@TestCase
+	public void testCase_getStoredControlByInternalId_A2() {
+		
+		//setup
+		final var control = new Label();
+		final var testUnit = new WebGui();
+		testUnit.pushLayerWithRootControl(new VerticalStack().addControl(new Label(), new Label(), new Label()));
+		testUnit.pushLayerWithRootControl(new VerticalStack().addControl(new Label(), new Label(), control));
+		
+		//execution
+		final var result = testUnit.getStoredControlByInternalId(control.getInternalId());
+		
+		//verification
+		expect(result).is(control);
 	}
 	
 	//method
