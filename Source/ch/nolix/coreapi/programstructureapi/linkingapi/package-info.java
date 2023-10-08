cd24818047f8cafffb6038d19b4054a4dc6836de
Nimeon <silvan.wyss@gmail.com>
2023-10-08 23:04:22 +0200

    created linkingapi package

---
+++ Source/ch/nolix/coreapi/programstructureapi/linkingapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.programstructureapi.linkingapi} package declares types that provide a linking mechanism.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-08
+ */
+package ch.nolix.coreapi.programstructureapi.linkingapi;
