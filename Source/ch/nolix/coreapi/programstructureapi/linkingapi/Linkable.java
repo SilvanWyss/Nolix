4859b03065a127cfd66d6f203a24659a3cef2200
Nimeon <silvan.wyss@gmail.com>
2023-10-08 23:07:05 +0200

    created Linkable

---
+++ Source/ch/nolix/coreapi/programstructureapi/linkingapi/Linkable.java
@@ -0,0 +1,9 @@
+//package declaration
+package ch.nolix.coreapi.programstructureapi.linkingapi;
+
+//interface
+public interface Linkable extends LinkedRequestable {
+	
+	//method declaration
+	void linkTo(Object object);
+}
