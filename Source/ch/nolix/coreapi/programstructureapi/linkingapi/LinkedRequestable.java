6246d273b57c4344dfcb906fd163f842bbbc6bc7
Nimeon <silvan.wyss@gmail.com>
2023-10-08 23:05:03 +0200

    created LinkedRequestable

---
+++ Source/ch/nolix/coreapi/programstructureapi/linkingapi/LinkedRequestable.java
@@ -0,0 +1,18 @@
+//package declaration
+package ch.nolix.coreapi.programstructureapi.linkingapi;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+
+//interface
+public interface LinkedRequestable {
+	
+	//method declaration
+	IContainer<Object> getStoredLinkedObjects();
+	
+	//method declaration
+	boolean isLinkedTo(Object object);
+	
+	//method declaration
+	boolean isLinkedToAnObject();
+}
