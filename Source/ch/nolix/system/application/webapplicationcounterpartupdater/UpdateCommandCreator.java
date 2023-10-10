62d01328638f7f2e7d4288501ea91c7ec7d24b93
Nimeon <silvan.wyss@gmail.com>
2023-10-10 23:05:21 +0200

    created UpdateCommandCreator

---
+++ Source/ch/nolix/system/application/webapplicationcounterpartupdater/UpdateCommandCreator.java
@@ -0,0 +1,121 @@
+//package declaration
+package ch.nolix.system.application.webapplicationcounterpartupdater;
+
+//own imports
+import ch.nolix.core.container.linkedlist.LinkedList;
+import ch.nolix.core.document.chainednode.ChainedNode;
+import ch.nolix.core.document.node.Node;
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.webapi.cssapi.ICss;
+import ch.nolix.coreapi.webapi.htmlapi.IHtmlElement;
+import ch.nolix.system.application.webapplicationprotocol.CommandProtocol;
+import ch.nolix.system.application.webapplicationprotocol.ObjectProtocol;
+import ch.nolix.systemapi.webguiapi.mainapi.IControl;
+import ch.nolix.systemapi.webguiapi.mainapi.IHtmlElementEvent;
+import ch.nolix.systemapi.webguiapi.mainapi.IWebGui;
+
+//class
+public final class UpdateCommandCreator {
+	
+	//method
+	public ChainedNode createSetCssCommandFromWebGui(final IWebGui<?> webGui) {
+		return createSetCssCommandFromCss(webGui.getCss());
+	}
+	
+	//method
+	public ChainedNode createSetEventFunctionsCommandFromWebGui(final IWebGui<?> webGui) {
+		return createSetEventFunctionsCommandFromHtmlElementEventRegistrations(webGui.getHtmlElementEventRegistrations());
+	}
+	
+	//method
+	public ChainedNode createSetRootHtmlElementCommandFromControl(final IControl<?, ?> control) {
+		return createSetHtmlElementCommandFromHtmlElement(control.getInternalId(), control.getHtml());
+	}
+	
+	//method
+	public ChainedNode createSetUserInputFunctionsCommandFromWebGui(final IWebGui<?> webGui) {
+		return createSetUserInputFunctionsCommandForControls(webGui.getStoredControls());
+	}
+	
+	//method
+	private ChainedNode createSetCssCommandFromCss(final ICss css) {
+		return createSetCssCommandFromCss(css.toStringWithoutEnclosingBrackets());
+	}
+	
+	//method
+	private ChainedNode createSetCssCommandFromCss(final String css) {
+		return
+		ChainedNode.withHeaderAndNextNode(
+			ObjectProtocol.GUI,
+			ChainedNode.withHeaderAndChildNode(
+				CommandProtocol.SET_CSS,
+				ChainedNode.withHeader(css)
+			)
+		);
+	}
+	
+	//method
+	private ChainedNode createSetHtmlElementCommandFromHtmlElement(
+		final String htmlElementId,
+		final IHtmlElement htmlElement
+	) {
+		return
+		ChainedNode.withHeaderAndNextNode(
+			ObjectProtocol.GUI,
+			ChainedNode.withHeaderAndChildNodes(
+				CommandProtocol.SET_HTML_ELEMENT,
+				ChainedNode.withHeader(htmlElementId),
+				ChainedNode.withHeader(htmlElement.toString())
+			)
+		);
+	}
+
+	//method
+	private ChainedNode createSetEventFunctionsCommandFromHtmlElementEventRegistrations(
+		final IContainer<IHtmlElementEvent> htmlElementEventRegistrations
+	) {
+		
+		final var eventFunctions =
+		htmlElementEventRegistrations.to(
+			e -> Node.withChildNode(Node.withHeader(e.getHtmlElementId()), Node.withHeader(e.getHtmlEvent()))
+		);
+		
+		return
+		ChainedNode.withHeaderAndNextNode(
+			ObjectProtocol.GUI,
+			ChainedNode.withHeaderAndChildNodesFromNodes(
+				CommandProtocol.SET_EVENT_FUNCTIONS,
+				eventFunctions
+			)
+		);
+	}
+	
+	//method
+	private ChainedNode createSetUserInputFunctionsCommandForControls(final IContainer<IControl<?, ?>> controls) {
+		
+		final var userInputFunctions = new LinkedList<ChainedNode>();
+		
+		for (final var c : controls) {
+			final var userInputFunction = c.getOptionalJavaScriptUserInputFunction();
+			if (userInputFunction.containsAny()) {
+				userInputFunctions.addAtEnd(
+					createUserInputFunctionFromControlAndString(c, userInputFunction.getStoredElement())
+				);
+			}
+		}
+		
+		return
+		ChainedNode.withHeaderAndNextNode(
+			ObjectProtocol.GUI,
+			ChainedNode.withHeaderAndChildNodes(
+				CommandProtocol.SET_USER_INPUT_FUNCTIONS,
+				userInputFunctions
+			)
+		);
+	}
+	
+	//method
+	private ChainedNode createUserInputFunctionFromControlAndString(final IControl<?, ?> control, final String string) {
+		return ChainedNode.withChildNodesFromNodes(Node.withHeader(control.getInternalId()), Node.withHeader(string));
+	}
+}
--- Source/ch/nolix/system/application/webapplicationcounterpartupdater/WebClientPartialCounterpartUpdater.java
+++ Source/ch/nolix/system/application/webapplicationcounterpartupdater/WebClientPartialCounterpartUpdater.java
@@ -3,23 +3,18 @@ package ch.nolix.system.application.webapplicationcounterpartupdater;
 
 //own imports
 import ch.nolix.core.container.immutablelist.ImmutableList;
-import ch.nolix.core.container.linkedlist.LinkedList;
 import ch.nolix.core.document.chainednode.ChainedNode;
-import ch.nolix.core.document.node.Node;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.coreapi.documentapi.chainednodeapi.IChainedNode;
 import ch.nolix.coreapi.functionapi.genericfunctionapi.IElementTaker;
-import ch.nolix.coreapi.webapi.cssapi.ICss;
-import ch.nolix.coreapi.webapi.htmlapi.IHtmlElement;
-import ch.nolix.system.application.webapplicationprotocol.CommandProtocol;
-import ch.nolix.system.application.webapplicationprotocol.ObjectProtocol;
 import ch.nolix.systemapi.webguiapi.mainapi.IControl;
-import ch.nolix.systemapi.webguiapi.mainapi.IHtmlElementEvent;
-import ch.nolix.systemapi.webguiapi.mainapi.IWebGui;
 
 //class
 public final class WebClientPartialCounterpartUpdater {
+	
+	//constant
+	private static final UpdateCommandCreator UPDATE_COMMAND_CREATOR = new UpdateCommandCreator(); 
 	
 	//static method
 	public static WebClientPartialCounterpartUpdater forCounterpartRunner(
@@ -62,112 +57,10 @@ public final class WebClientPartialCounterpartUpdater {
 		
 		return
 		ImmutableList.withElement(
-			createSetRootHtmlElementCommandFromControl(control),
-			createSetCssCommandFromWebGui(gui),
-			createSetEventFunctionsCommandFromWebGui(gui),
-			createSetUserInputFunctionsCommandFromWebGui(gui)
+			UPDATE_COMMAND_CREATOR.createSetRootHtmlElementCommandFromControl(control),
+			UPDATE_COMMAND_CREATOR.createSetCssCommandFromWebGui(gui),
+			UPDATE_COMMAND_CREATOR.createSetEventFunctionsCommandFromWebGui(gui),
+			UPDATE_COMMAND_CREATOR.createSetUserInputFunctionsCommandFromWebGui(gui)
 		);
-	}
-	
-	//method
-	private ChainedNode createSetRootHtmlElementCommandFromControl(final IControl<?, ?> control) {
-		return createSetHtmlElementCommandFromHtmlElement(control.getInternalId(), control.getHtml());
-	}
-	
-	//method
-	private ChainedNode createSetHtmlElementCommandFromHtmlElement(
-		final String htmlElementId,
-		final IHtmlElement htmlElement
-	) {
-		return
-		ChainedNode.withHeaderAndNextNode(
-			ObjectProtocol.GUI,
-			ChainedNode.withHeaderAndChildNodes(
-				CommandProtocol.SET_HTML_ELEMENT,
-				ChainedNode.withHeader(htmlElementId),
-				ChainedNode.withHeader(htmlElement.toString())
-			)
-		);
-	}
-	
-	//method
-	private ChainedNode createSetCssCommandFromWebGui(final IWebGui<?> webGui) {
-		return createSetCssCommandFromCss(webGui.getCss());
-	}
-	
-	//method
-	private ChainedNode createSetCssCommandFromCss(final ICss css) {
-		return createSetCssCommandFromCss(css.toStringWithoutEnclosingBrackets());
-	}
-	
-	//method
-	private ChainedNode createSetCssCommandFromCss(final String css) {
-		return
-		ChainedNode.withHeaderAndNextNode(
-			ObjectProtocol.GUI,
-			ChainedNode.withHeaderAndChildNode(
-				CommandProtocol.SET_CSS,
-				ChainedNode.withHeader(css)
-			)
-		);
-	}
-	
-	//method
-	private ChainedNode createSetEventFunctionsCommandFromWebGui(final IWebGui<?> webGui) {
-		return createSetEventFunctionsCommandFromHtmlElementEventRegistrations(webGui.getHtmlElementEventRegistrations());
-	}
-	
-	//method
-	private ChainedNode createSetEventFunctionsCommandFromHtmlElementEventRegistrations(
-		final IContainer<IHtmlElementEvent> htmlElementEventRegistrations
-	) {
-		
-		final var eventFunctions =
-		htmlElementEventRegistrations.to(
-			e -> Node.withChildNode(Node.withHeader(e.getHtmlElementId()), Node.withHeader(e.getHtmlEvent()))
-		);
-		
-		return
-		ChainedNode.withHeaderAndNextNode(
-			ObjectProtocol.GUI,
-			ChainedNode.withHeaderAndChildNodesFromNodes(
-				CommandProtocol.SET_EVENT_FUNCTIONS,
-				eventFunctions
-			)
-		);
-	}
-	
-	//method
-	private ChainedNode createSetUserInputFunctionsCommandFromWebGui(final IWebGui<?> webGui) {
-		return createSetUserInputFunctionsCommandForControls(webGui.getStoredControls());
-	}
-	
-	//method
-	private ChainedNode createSetUserInputFunctionsCommandForControls(final IContainer<IControl<?, ?>> controls) {
-		
-		final var userInputFunctions = new LinkedList<ChainedNode>();
-		
-		for (final var c : controls) {
-			final var userInputFunction = c.getOptionalJavaScriptUserInputFunction();
-			if (userInputFunction.containsAny()) {
-				userInputFunctions.addAtEnd(
-					createUserInputFunctionFromControlAndString(c, userInputFunction.getStoredElement())
-				);
-			}
-		}
-		
-		return
-		ChainedNode.withHeaderAndNextNode(
-			ObjectProtocol.GUI,
-			ChainedNode.withHeaderAndChildNodes(
-				CommandProtocol.SET_USER_INPUT_FUNCTIONS,
-				userInputFunctions
-			)
-		);
-	}
-	
-	//method
-	private ChainedNode createUserInputFunctionFromControlAndString(final IControl<?, ?> control, final String string) {
-		return ChainedNode.withChildNodesFromNodes(Node.withHeader(control.getInternalId()), Node.withHeader(string));
 	}
 }
