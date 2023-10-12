c11bb2b1f5a42e21180ebf66a7cf4ebfd644df86
Nimeon <silvan.wyss@gmail.com>
2023-10-12 22:59:48 +0200

    created webapplicationcounterpartupdatertest package

---
+++ Source/ch/nolix/systemtest/applicationtest/webapplicationcounterpartupdatertest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.applicationtest.webapplicationcounterpartupdatertest} package defines
+ * tests for the {@link ch.nolix.system.application.webapplicationcounterpartupdater} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-12
+ */
+package ch.nolix.systemtest.applicationtest.webapplicationcounterpartupdatertest;
