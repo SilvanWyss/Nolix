8292b0f5438b788d4736559c75af261d911dd052
Nimeon <silvan.wyss@gmail.com>
2023-10-14 00:00:03 +0200

    extended WebGuiTest

--- Source/ch/nolix/systemtest/webguitest/maintest/WebGuiTest.java
+++ Source/ch/nolix/systemtest/webguitest/maintest/WebGuiTest.java
@@ -4,6 +4,9 @@ package ch.nolix.systemtest.webguitest.maintest;
 //own imports
 import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.core.testing.test.Test;
+import ch.nolix.system.graphic.color.Color;
+import ch.nolix.system.graphic.image.MutableImage;
+import ch.nolix.system.webgui.main.Layer;
 import ch.nolix.system.webgui.main.WebGui;
 
 //class
@@ -22,5 +25,26 @@ public final class WebGuiTest extends Test {
 		expect(result.getBackgroundColor()).isEqualTo(WebGui.DEFAULT_BACKGROUND_COLOR);
 		expect(result.isEmpty());
 		expect(result.getTokens()).isEmpty();
+	}
+	
+	//method
+	@TestCase
+	public void testCase_reset() {
+		
+		//setup
+		final var testUnit = new WebGui();
+		testUnit.setTitle("my_title");
+		testUnit.setIcon(MutableImage.withWidthAndHeightAndColor(100, 5, Color.BLUE));
+		testUnit.pushLayer(new Layer());
+		testUnit.addToken("my_token");
+		
+		//execution
+		testUnit.reset();
+		
+		//verification
+		expect(testUnit.getTitle()).isEqualTo(WebGui.DEFAULT_TITLE);
+		expect(testUnit.getIcon()).isEqualTo(WebGui.DEFAULT_ICON);
+		expect(testUnit.isEmpty());
+		expect(testUnit.getTokens()).isEmpty();
 	}
 }
