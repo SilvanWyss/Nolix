73c2851ea9d877321e6ff3c8c4e3e531154a36d5
Nimeon <silvan.wyss@gmail.com>
2023-10-13 23:23:01 +0200

    created ILayerStack

---
+++ Source/ch/nolix/systemapi/webguiapi/mainapi/ILayerStack.java
@@ -0,0 +1,38 @@
+//package declaration
+package ch.nolix.systemapi.webguiapi.mainapi;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.functionapi.genericfunctionapi.IAction;
+import ch.nolix.coreapi.functionapi.mutationapi.Clearable;
+
+//interface
+public interface ILayerStack extends Clearable {
+	
+	//method declaration
+	IControl<?, ?> getStoredControlOrNullByInternalId(String internalId);
+	
+	//method declaration
+	IContainer<IControl<?, ?>> getStoredControls();
+	
+	//method declaration
+	IContainer<ILayer<?>> getStoredLayers();
+	
+	//method declaration
+	ILayer<?> getStoredTopLayer();
+	
+	//method declaration
+	boolean hasRemoveLayerAction();
+	
+	//method declaration
+	ILayerStack pushLayer(ILayer<?> layer);
+	
+	//method declaration
+	ILayerStack pushLayerWithRootControl(IControl<?, ?> rootControl);
+	
+	//method declaration
+	void removeLayer(ILayer<?> layer);
+	
+	//method declaration
+	ILayerStack setRemoveLayerAction(IAction removeLayerAction);
+}
