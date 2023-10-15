fcdac8ff15ce20c7e4ac57c3a6340ec96bf2cc04
Nimeon <silvan.wyss@gmail.com>
2023-10-15 17:29:55 +0200

    deleted genericfunctionapi package

--- Source/ch/nolix/coreapi/functionapi/genericfunctionapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.functionapi.genericfunctionapi} is an API that
- * defines functional interfaces.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.coreapi.functionapi.genericfunctionapi;
