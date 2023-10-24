f5c654b60ae5765d4c51a9b5ce5abe29423aa250
Nimeon <silvan.wyss@gmail.com>
2023-10-24 22:36:56 +0200

    created IOptionalCardinalityHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/optionalattributeapi/IOptionalCardinalityHolder.java
@@ -0,0 +1,27 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.optionalattributeapi;
+
+//interface
+/**
+ * A {@link IOptionalCardinalityHolder} can have a cardinality.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-24
+ */
+public interface IOptionalCardinalityHolder {
+
+  //method declaration
+  /**
+   * @return the cardinality of the current {@link IOptionalCardinalityHolder}.
+   * @throws RuntimeException if the current {@link IOptionalCardinalityHolder}
+   *                          does not have a cardinality.
+   */
+  String getCardinality();
+
+  //method declaration
+  /**
+   * @return true if the current {@link IOptionalCardinalityHolder} has a
+   *         cardinality.
+   */
+  boolean hasCardinality();
+}
