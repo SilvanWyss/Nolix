355ef7bc3c9751be66715f5520efba62ea7a8a52
Nimeon <silvan.wyss@gmail.com>
2023-10-24 22:30:00 +0200

    created IMutableCardinalityHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableCardinalityHolder.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mutablemandatoryattributeapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.ICardinalityHolder;
+
+//interface
+/**
+ * A {@link IMutableCardinalityHolder} is a {@link ICardinalityHolder} whose
+ * cardinality can be set programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-24
+ */
+public interface IMutableCardinalityHolder extends ICardinalityHolder {
+
+  //method declaration
+  /**
+   * Sets the cardinality of the current {@link IMutableCardinalityHolder}.
+   * 
+   * @param cardinality
+   * @throws RuntimeException if the given cardinality is null.
+   */
+  void setCardinality(String cardinality);
+}
