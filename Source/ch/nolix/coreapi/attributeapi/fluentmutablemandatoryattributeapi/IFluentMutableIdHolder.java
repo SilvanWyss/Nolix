1b142c19031d3a0452ce58d291ffe362d0d31640
Nimeon <silvan.wyss@gmail.com>
2023-10-24 22:47:19 +0200

    renamed IFluentMutableIdHolder

--- Source/ch/nolix/coreapi/attributeapi/fluentmutablemandatoryattributeapi/IFluentMutableIdHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/fluentmutablemandatoryattributeapi/IFluentMutableIdHolder.java
@@ -1,27 +1,28 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.Identified;
 
 //interface
 /**
- * A {@link FluentIdentifiable} is a {@link Identified} whose id can be set
+ * A {@link IFluentMutableIdHolder} is a {@link Identified} whose id can be set
  * programmatically.
  * 
  * @author Silvan Wyss
  * @date 2020-03-29
- * @param <FI> is the type of a {@link FluentIdentifiable}.
+ * @param <FMIH> is the type of a {@link IFluentMutableIdHolder}.
  */
-public interface FluentIdentifiable<FI> extends Identified {
+public interface IFluentMutableIdHolder<FMIH> extends Identified {
 
   //method declaration
   /**
-   * Sets the id of the current {@link FluentIdentifiable}.
+   * Sets the id of the current {@link IFluentMutableIdHolder}.
    * 
    * @param id
-   * @return the current {@link FluentIdentifiable}.
+   * @return the current {@link IFluentMutableIdHolder}.
    * @throws RuntimeException if the given id is null.
    * @throws RuntimeException if the given id is blank.
    */
-  FI setId(String id);
+  FMIH setId(String id);
 }
