37010ac3cff7496930f9ad0f3aa8c3568392b74c
Nimeon <silvan.wyss@gmail.com>
2023-10-25 22:42:34 +0200

    edited IOptionalLabelHolder

--- Source/ch/nolix/coreapi/attributeapi/optionalattributeapi/IOptionalLabelHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/optionalattributeapi/IOptionalLabelHolder.java
@@ -1,6 +1,9 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.optionalattributeapi;
 
+//own imports
+import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
+
 //interface
 /**
  * A {@link IOptionalLabelHolder} can have a label.
@@ -8,15 +11,26 @@ package ch.nolix.coreapi.attributeapi.optionalattributeapi;
  * @author Silvan Wyss
  * @date 2023-02-07
  */
+@AllowDefaultMethodsAsDesignPattern
 public interface IOptionalLabelHolder {
 
   //method declaration
   /**
    * @return the label of the current {@link IOptionalLabelHolder}.
-   * @throws RuntimeException if the current {@link IOptionalLabelHolder} does not have
-   *                          a label.
+   * @throws RuntimeException if the current {@link IOptionalLabelHolder} does not
+   *                          have a label.
    */
   String getLabel();
+
+  //method
+  /**
+   * @return the label of the current {@link IOptionalLabelHolder} in quotes.
+   * @throws RuntimeException if the current {@link IOptionalLabelHolder} does not
+   *                          have a label.
+   */
+  default String getLabelInQuotes() {
+    return ("'" + getLabel() + "'");
+  }
 
   //method declaration
   /**
