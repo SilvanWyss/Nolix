69dc217e998a0f4ca9711f1084078d64c6da1c3a
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:10:59 +0200

    created IMultiTextHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/multiattributeapi/IMultiTextHolder.java
@@ -0,0 +1,21 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.multiattributeapi;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+
+//interface
+/**
+ * A {@link IMultiTextHolder} can contain several texts.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-25
+ */
+public interface IMultiTextHolder {
+
+  //method declaration
+  /**
+   * @return the texts of the current {@link IMultiTextHolder}.
+   */
+  IContainer<String> getTexts();
+}
