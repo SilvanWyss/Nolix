c0bf1a4192c86effa1707f40e4a38d9acef7924b
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:21:56 +0200

    renamed IFluentOptionalTitleHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/fluentmutableoptionalattributeapi/IFluentOptionalTitleHolder.java
@@ -0,0 +1,36 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.fluentmutableoptionalattributeapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.optionalattributeapi.OptionalTitled;
+
+//interface
+/**
+ * A {@link IFluentOptionalTitleHolder} is a {@link OptionalTitled} whose title
+ * can be set and removed programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-02-06
+ * @param <FOTH> is the type of a {@link IFluentOptionalTitleHolder}.
+ */
+public interface IFluentOptionalTitleHolder<FOTH extends IFluentOptionalTitleHolder<FOTH>> extends OptionalTitled {
+
+  //method declaration
+  /**
+   * Removes the title of the current {@link IFluentOptionalTitleHolder}.
+   * 
+   * @return the current {@link IFluentOptionalTitleHolder}.
+   */
+  FOTH removeTitle();
+
+  //method declaration
+  /**
+   * Sets the title of the current {@link IFluentOptionalTitleHolder}.
+   * 
+   * @param title
+   * @return the current {@link IFluentOptionalTitleHolder}.
+   * @throws RuntimeException if the given title is null.
+   * @throws RuntimeException if the given title is blank.
+   */
+  FOTH setTitle(String title);
+}
