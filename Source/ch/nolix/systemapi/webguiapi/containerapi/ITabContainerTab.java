b6af5e028ba481873f0b42b654a63ee77dd5fa1e
Nimeon <silvan.wyss@gmail.com>
2023-10-25 22:51:48 +0200

    beautified ITabContainerTab

--- Source/ch/nolix/systemapi/webguiapi/containerapi/ITabContainerTab.java
+++ Source/ch/nolix/systemapi/webguiapi/containerapi/ITabContainerTab.java
@@ -1,13 +1,15 @@
 //package declaration
 package ch.nolix.systemapi.webguiapi.containerapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi.IFluentMutableHeaderHolder;
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IHeaderHolder;
 import ch.nolix.systemapi.guiapi.processapi.Selectable;
 import ch.nolix.systemapi.webguiapi.mainapi.IRootControlOwner;
 
 //interface
-public interface ITabContainerTab extends IFluentMutableHeaderHolder<IHeaderHolder>, IRootControlOwner<ITabContainerTab>, Selectable {
+public interface ITabContainerTab
+    extends IFluentMutableHeaderHolder<IHeaderHolder>, IRootControlOwner<ITabContainerTab>, Selectable {
 
   //method declaration
   boolean belongsToTabContainer();
