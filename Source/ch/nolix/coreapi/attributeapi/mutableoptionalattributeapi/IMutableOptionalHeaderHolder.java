7b4755cf514d6b7ccd85f843b1811e4c5327ca6a
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:50:13 +0200

    renamed IMutableOptionalHeaderHolder

--- Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalHeaderHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalHeaderHolder.java
@@ -1,27 +1,28 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.mutableoptionalattributeapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.optionalattributeapi.IOptionalHeaderHolder;
 
 //interface
 /**
- * A {@link OptionalHeaderable} is a {@link IOptionalHeaderHolder} whose header can
- * be set and removed programmatically.
+ * A {@link IMutableOptionalHeaderHolder} is a {@link IOptionalHeaderHolder}
+ * whose header can be set and removed programmatically.
  * 
  * @author Silvan Wyss
  * @date 2023-02-07
  */
-public interface OptionalHeaderable extends IOptionalHeaderHolder {
+public interface IMutableOptionalHeaderHolder extends IOptionalHeaderHolder {
 
   //method declaration
   /**
-   * Removes the header of the current {@link OptionalHeaderable}.
+   * Removes the header of the current {@link IMutableOptionalHeaderHolder}.
    */
   void removeHeader();
 
   //method declaration
   /**
-   * Sets the header of the current {@link OptionalHeaderable}.
+   * Sets the header of the current {@link IMutableOptionalHeaderHolder}.
    * 
    * @param header
    * @throws RuntimeException if the given header is null.
