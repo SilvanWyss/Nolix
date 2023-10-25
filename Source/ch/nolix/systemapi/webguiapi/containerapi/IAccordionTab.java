93156434a4c8a5bb9e8f335e5d55343129a0b787
Nimeon <silvan.wyss@gmail.com>
2023-10-25 22:51:21 +0200

    beautified IAccordionTab

--- Source/ch/nolix/systemapi/webguiapi/containerapi/IAccordionTab.java
+++ Source/ch/nolix/systemapi/webguiapi/containerapi/IAccordionTab.java
@@ -1,12 +1,14 @@
 //package declaration
 package ch.nolix.systemapi.webguiapi.containerapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi.IFluentMutableHeaderHolder;
 import ch.nolix.systemapi.guiapi.processapi.Selectable;
 import ch.nolix.systemapi.webguiapi.mainapi.IRootControlOwner;
 
 //interface
-public interface IAccordionTab extends IFluentMutableHeaderHolder<IAccordionTab>, IRootControlOwner<IAccordionTab>, Selectable {
+public interface IAccordionTab
+    extends IFluentMutableHeaderHolder<IAccordionTab>, IRootControlOwner<IAccordionTab>, Selectable {
 
   //method declaration
   boolean belongsToAccordion();
