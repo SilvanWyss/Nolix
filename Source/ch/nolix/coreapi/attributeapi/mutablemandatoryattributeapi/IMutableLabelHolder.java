b2e7b50e1d74d0cc0d9b2a78ea50654a9067ff49
Nimeon <silvan.wyss@gmail.com>
2023-10-25 22:24:45 +0200

    created IMutableLabelHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableLabelHolder.java
@@ -0,0 +1,26 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mutablemandatoryattributeapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.ILabelHolder;
+
+//interface
+/**
+ * A {@link IMutableLabelHolder} is a {@link ILabelHolder} whose label can be
+ * set programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-25
+ */
+public interface IMutableLabelHolder extends ILabelHolder {
+
+  //method declaration
+  /**
+   * Sets the label of the current {@link IMutableLabelHolder}.
+   * 
+   * @param label
+   * @throws RuntimeException if the given label is null.
+   * @throws RuntimeException if the given label is blank.
+   */
+  void setLabel(String label);
+}
