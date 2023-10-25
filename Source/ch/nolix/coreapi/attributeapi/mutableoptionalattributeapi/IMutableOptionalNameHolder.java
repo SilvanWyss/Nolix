eaf4de24960258eef87aacd0966b81d9f079efcf
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:51:09 +0200

    renamed IMutableOptionalNameHolder

--- Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalNameHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalNameHolder.java
@@ -1,27 +1,28 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.mutableoptionalattributeapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.optionalattributeapi.IOptionalNameHolder;
 
 //interface
 /**
- * A {@link OptionalNameable} is a {@link IOptionalNameHolder} whose name can be set
- * and removed programmatically.
+ * A {@link IMutableOptionalNameHolder} is a {@link IOptionalNameHolder} whose
+ * name can be set and removed programmatically.
  * 
  * @author Silvan Wyss
  * @date 2023-02-07
  */
-public interface OptionalNameable extends IOptionalNameHolder {
+public interface IMutableOptionalNameHolder extends IOptionalNameHolder {
 
   //method declaration
   /**
-   * Removes the name of the current {@link OptionalNameable}.
+   * Removes the name of the current {@link IMutableOptionalNameHolder}.
    */
   void removeName();
 
   //method declaration
   /**
-   * Sets the name of the current {@link OptionalNameable}.
+   * Sets the name of the current {@link IMutableOptionalNameHolder}.
    * 
    * @param name
    * @throws RuntimeException if the given name is null.
