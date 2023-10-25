34047b9d24b18008d69899e5ff605b76116d8e30
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:38:46 +0200

    renamed IMutableNameHolder

--- Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableNameHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableNameHolder.java
@@ -1,23 +1,26 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.mutablemandatoryattributeapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.INameHolder;
 
 //interface
 /**
- * A {@link Nameable} is a {@link INameHolder} whose name can be set programmatically.
+ * A {@link IMutableNameHolder} is a {@link INameHolder} whose name can be set
+ * programmatically.
  * 
  * @author Silvan Wyss
  * @date 2023-01-27
  */
-public interface Nameable extends INameHolder {
+public interface IMutableNameHolder extends INameHolder {
 
   //method declaration
   /**
-   * Sets the name of the current {@link Nameable}.
+   * Sets the name of the current {@link IMutableNameHolder}.
    * 
    * @param name
    * @throws RuntimeException if the given name is null.
+   * @throws RuntimeException if the given name is blank.
    */
   void setName(String name);
 }
