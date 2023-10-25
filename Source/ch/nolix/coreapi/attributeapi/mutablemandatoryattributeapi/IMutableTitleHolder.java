8d9da695e1c58d3d1a4f0350374badf721ed5cb4
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:39:31 +0200

    renamed IMutableTitleHolder

--- Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableTitleHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableTitleHolder.java
@@ -1,21 +1,22 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.mutablemandatoryattributeapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.ITitleHolder;
 
 //interface
 /**
- * A {@link Titleable} is a {@link ITitleHolder} whose title can be set
- * programmatically.
+ * A {@link IMutableTitleHolder} is a {@link ITitleHolder} whose title can be
+ * set programmatically.
  * 
  * @author Silvan Wyss
  * @date 2023-02-09
  */
-public interface Titleable extends ITitleHolder {
+public interface IMutableTitleHolder extends ITitleHolder {
 
   //method declaration
   /**
-   * Sets the title of the current {@link Titleable}.
+   * Sets the title of the current {@link IMutableTitleHolder}.
    * 
    * @param title
    * @throws RuntimeException if the given title is null.
