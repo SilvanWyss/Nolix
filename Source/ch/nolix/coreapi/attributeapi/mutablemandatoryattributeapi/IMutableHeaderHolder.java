bf26fd78d0af7d68f4a0f414003174d2a4a891ec
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:36:21 +0200

    renamed IMutableHeaderHolder

--- Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableHeaderHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableHeaderHolder.java
@@ -1,21 +1,22 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.mutablemandatoryattributeapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IHeaderHolder;
 
 //interface
 /**
- * A {@link Headerable} is a {@link IHeaderHolder} whose header can be set
- * programmatically.
+ * A {@link IMutableHeaderHolder} is a {@link IHeaderHolder} whose header can be
+ * set programmatically.
  * 
  * @author Silvan Wyss
  * @date 2023-01-29
  */
-public interface Headerable extends IHeaderHolder {
+public interface IMutableHeaderHolder extends IHeaderHolder {
 
   //method declaration
   /**
-   * Sets the header of the current {@link Headerable}.
+   * Sets the header of the current {@link IMutableHeaderHolder}.
    * 
    * @param header
    * @throws RuntimeException if the given header is null.
