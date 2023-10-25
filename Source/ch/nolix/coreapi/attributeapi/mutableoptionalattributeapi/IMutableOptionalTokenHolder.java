55d1d8cabd49fd92420c7218a9291412f1b36633
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:52:28 +0200

    renamed IMutableOptionalTokenHolder

--- Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalTokenHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalTokenHolder.java
@@ -1,27 +1,28 @@
 //package declaration
 package ch.nolix.coreapi.attributeapi.mutableoptionalattributeapi;
 
+//own imports
 import ch.nolix.coreapi.attributeapi.optionalattributeapi.IOptionalTokenHolder;
 
 //interface
 /**
- * A {@link OptionalTokenable} is a {@link IOptionalTokenHolder} whose token can be
- * set and removed programmatically.
+ * A {@link IMutableOptionalTokenHolder} is a {@link IOptionalTokenHolder} whose
+ * token can be set and removed programmatically.
  * 
  * @author Silvan Wyss
  * @date 2023-02-09
  */
-public interface OptionalTokenable extends IOptionalTokenHolder {
+public interface IMutableOptionalTokenHolder extends IOptionalTokenHolder {
 
   //method declaration
   /**
-   * Removes the token of the current {@link OptionalTokenable}.
+   * Removes the token of the current {@link IMutableOptionalTokenHolder}.
    */
   void removeToken();
 
   //method declaration
   /**
-   * Sets the token of the current {@link OptionalTokenable}.
+   * Sets the token of the current {@link IMutableOptionalTokenHolder}.
    * 
    * @param token
    * @throws RuntimeException if the given token is null.
