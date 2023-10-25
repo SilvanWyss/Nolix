c0890678d8e2a0d533d398c87922e04e5e768e79
Nimeon <silvan.wyss@gmail.com>
2023-10-25 21:12:25 +0200

    created IFluentMutableMultiTextHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/fluentmutablemultiattributeapi/IFluentMutableMultiTextHolder.java
@@ -0,0 +1,47 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.fluentmutablemultiattributeapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.multiattributeapi.IMultiTextHolder;
+
+//interface
+/**
+ * A {@link IFluentMutableMultiTextHolder} is a {@link MultiTexted} whose texts
+ * can be added and removed programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-25
+ * @param <FMMTH> is the type of a {@link IFluentMutableMultiTextHolder}.
+ */
+public interface IFluentMutableMultiTextHolder<FMMTH extends IFluentMutableMultiTextHolder<FMMTH>>
+    extends IMultiTextHolder {
+
+  //method declaration
+  /**
+   * Adds the given text to the current {@link IFluentMutableMultiTextHolder}.
+   * 
+   * @param text
+   * @return the current {@link IFluentMutableMultiTextHolder}.
+   * @throws RuntimeException if the given text is null or blank.
+   */
+  FMMTH addText(String text);
+
+  //method declaration
+  /**
+   * Removes the text that equals the given text from the current
+   * {@link IFluentMutableMultiTextHolder} if the current
+   * {@link IFluentMutableMultiTextHolder} contains such a text.
+   * 
+   * @param text
+   * @return the current {@link IFluentMutableMultiTextHolder}.
+   */
+  FMMTH removeText(String text);
+
+  //method declaration
+  /**
+   * Removes all texts from the current {@link IFluentMutableMultiTextHolder}.
+   * 
+   * @return the current {@link IFluentMutableMultiTextHolder}.
+   */
+  FMMTH removeTexts();
+}
