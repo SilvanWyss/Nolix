721fa069855abb68199d444f484c099cdd544202
Nimeon <silvan.wyss@gmail.com>
2023-10-29 22:38:00 +0100

    created mainvalidator package

---
+++ Source/ch/nolix/system/webgui/mainvalidator/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.webgui.mainvalidator} package defines validators
+ * for the types in the {@link ch.nolix.systemapi.webguiapi.mainapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-29
+ */
+package ch.nolix.system.webgui.mainvalidator;
