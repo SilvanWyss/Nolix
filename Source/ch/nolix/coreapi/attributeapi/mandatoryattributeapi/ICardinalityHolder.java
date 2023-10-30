5395d6fbdd668578dcabbc820649900dc40153c3
Nimeon <silvan.wyss@gmail.com>
2023-10-30 21:54:02 +0100

    added getBaseCardinality method to ICardinalityHolder

--- Source/ch/nolix/coreapi/attributeapi/mandatoryattributeapi/ICardinalityHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/mandatoryattributeapi/ICardinalityHolder.java
@@ -2,7 +2,9 @@
 package ch.nolix.coreapi.attributeapi.mandatoryattributeapi;
 
 //own imports
+import ch.nolix.coreapi.datamodelapi.cardinalityapi.BaseCardinality;
 import ch.nolix.coreapi.datamodelapi.cardinalityapi.Cardinality;
+import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
 
 //interface
 /**
@@ -11,6 +13,7 @@ import ch.nolix.coreapi.datamodelapi.cardinalityapi.Cardinality;
  * @author Silvan Wyss
  * @date 2023-08-25
  */
+@AllowDefaultMethodsAsDesignPattern
 public interface ICardinalityHolder {
 
   //method declaration
@@ -18,4 +21,13 @@ public interface ICardinalityHolder {
    * @return the {@link Cardinality} of the current {@link ICardinalityHolder}.
    */
   Cardinality getCardinality();
+
+  //method
+  /**
+   * @return the {@link BaseCardinality} of the current
+   *         {@link ICardinalityHolder}.
+   */
+  default BaseCardinality getBaseCardinality() {
+    return getCardinality().getBaseCardinality();
+  }
 }
