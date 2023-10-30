fa041d778ce6cd5b08710d34ae6e1a29c1cbbf47
Nimeon <silvan.wyss@gmail.com>
2023-10-30 22:02:59 +0100

    created attributeapitest package

---
+++ Source/ch/nolix/coreapitest/attributeapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapitest.attributeapitest} package defines tests for
+ * the {@link ch.nolix.coreapi.attributeapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-10-30
+ */
+package ch.nolix.coreapitest.attributeapitest;
