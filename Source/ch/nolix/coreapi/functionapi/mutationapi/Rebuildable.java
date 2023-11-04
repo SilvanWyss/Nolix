527e043861ae482aa64c2b9281fc4303d8fd9b79
Nimeon <silvan.wyss@gmail.com>
2023-11-04 11:25:57 +0100

    created Rebuildable

---
+++ Source/ch/nolix/coreapi/functionapi/mutationapi/Rebuildable.java
@@ -0,0 +1,18 @@
+//package declaration
+package ch.nolix.coreapi.functionapi.mutationapi;
+
+//interface
+/**
+ * A {@link Rebuildable} can rebuild itself.
+ * 
+ * @author Silvan Wyss
+ * @date 2023-11-04
+ */
+public interface Rebuildable {
+
+  //method declaration
+  /**
+   * Rebuilds the current {@link Rebuildable}.
+   */
+  void rebuild();
+}
