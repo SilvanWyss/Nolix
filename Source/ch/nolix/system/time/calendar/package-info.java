1fd7995f2aaa3ed735d88d70d6be5daff6f625fc
Nimeon <silvan.wyss@gmail.com>
2023-11-18 00:15:30 +0100

    deleted calendarapi package

--- Source/ch/nolix/system/time/calendar/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.time.calendar} package defines an appointment for
- * calendars.
- * 
- * @author Silvan Wyss
- * @date 2021-06-15
- */
-package ch.nolix.system.time.calendar;
--- Source/ch/nolix/systemapi/timeapi/calendarapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.time.calendar} package declares an appointment for
- * calendars.
- * 
- * @author Silvan Wyss
- * @date 2021-06-16
- */
-package ch.nolix.systemapi.timeapi.calendarapi;
