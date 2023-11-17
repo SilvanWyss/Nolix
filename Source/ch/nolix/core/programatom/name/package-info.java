98b4203b2bb3fe4cd8338c92d07eb7a0f193d0ad
Nimeon <silvan.wyss@gmail.com>
2023-11-17 17:48:29 +0100

    deleted name package

--- Source/ch/nolix/core/programatom/name/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.programatom.name} package defines names.
- * 
- * @author Silvan Wyss
- * @date 2022-06-16
- */
-package ch.nolix.core.programatom.name;
