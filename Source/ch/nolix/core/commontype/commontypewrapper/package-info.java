a58fb570f175888790aaa6db2ce3e0255ac99902
Nimeon <silvan.wyss@gmail.com>
2023-11-18 15:57:48 +0100

    deleted commontypewrapper package

--- Source/ch/nolix/core/commontype/commontypewrapper/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.commontype.commontypewrapper} package defines
- * wrappers for common types.
- * 
- * @author Silvan Wyss
- * @date 2019-10-01
- */
-package ch.nolix.core.commontype.commontypewrapper;
