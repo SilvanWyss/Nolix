35d27b44a8e122b7c3a31f8ed9f48549e400fef8
Nimeon <silvan.wyss@gmail.com>
2023-11-19 13:30:26 +0100

    deleted processproperty package

--- Source/ch/nolix/systemapi/guiapi/processproperty/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemapi.guiapi.processproperty} package defines
- * properties for processes for GUIs and their elements.
- * 
- * @author Silvan Wyss
- * @date 2022-05-29
- */
-package ch.nolix.systemapi.guiapi.processproperty;
