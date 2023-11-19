1ff56fa27d86303b8a3bd6ec746ac1ad9e6e1af4
Nimeon <silvan.wyss@gmail.com>
2023-11-19 13:50:45 +0100

    deleted geometry package

--- Source/ch/nolix/system/geometry/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.geometry} package defines types that represent
- * geometric objects.
- * 
- * @author Silvan Wyss
- * @date 2019-07-21
- */
-package ch.nolix.system.geometry;
