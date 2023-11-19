72b7bb93717fdee8fb5da5f05a85a73ba8afffbf
Nimeon <silvan.wyss@gmail.com>
2023-11-19 13:39:47 +0100

    deleted structureapi package

--- Source/ch/nolix/systemapi/structureapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemapi.structureapi} package declares types for
- * structure.
- * 
- * @author Silvan Wyss
- * @date 2022-10-15
- */
-package ch.nolix.systemapi.structureapi;
