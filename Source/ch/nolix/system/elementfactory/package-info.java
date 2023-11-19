628c4273fc15fecad40f2ec6eb56a20f5f0a188a
Nimeon <silvan.wyss@gmail.com>
2023-11-19 13:52:21 +0100

    deleted elementfactory package

--- Source/ch/nolix/system/elementfactory/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.elementfactory} package defines an element
- * factory.
- * 
- * @author Silvan Wyss
- * @date 2020-06-01
- */
-package ch.nolix.system.elementfactory;
