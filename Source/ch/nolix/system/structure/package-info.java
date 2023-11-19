f38285a26bb88b0bb303bbfcc6c3ec5e7f70a7bc
Nimeon <silvan.wyss@gmail.com>
2023-11-19 13:41:38 +0100

    deleted structure package

--- Source/ch/nolix/system/structure/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.structure} package defines types for structure.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.system.structure;
