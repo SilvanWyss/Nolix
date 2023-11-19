e7e28c7396603eb68a83856f7c14c6206d956d33
Nimeon <silvan.wyss@gmail.com>
2023-11-19 12:20:26 +0100

    deleted applicationcontextapi package

--- Source/ch/nolix/systemapi/applicationapi/applicationcontextapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemapi.applicationapi.applicationcontextapi} package
- * declares types for application contexts.
- * 
- * @author Silvan Wyss
- * @date 2023-08-06
- */
-package ch.nolix.systemapi.applicationapi.applicationcontextapi;
