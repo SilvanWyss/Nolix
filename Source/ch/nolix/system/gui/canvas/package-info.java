e558b1d36780a8cbbc5906571b6cb7aa0a0b94b0
Nimeon <silvan.wyss@gmail.com>
2023-11-19 12:35:29 +0100

    deleted canvas package

--- Source/ch/nolix/system/gui/canvas/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.gui.canvas} package defines some types that are
- * declared in the {@link ch.nolix.systemapi.guiapi.canvasapi} package.
- * 
- * @author Silvan Wyss
- * @date 2022-07-20
- */
-package ch.nolix.system.gui.canvas;
