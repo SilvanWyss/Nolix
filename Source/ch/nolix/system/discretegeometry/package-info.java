86eb3697576429dbd260da4997ddbb51a0e74864
Nimeon <silvan.wyss@gmail.com>
2023-11-19 13:50:15 +0100

    deleted discretegeometry package

--- Source/ch/nolix/system/discretegeometry/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.discretegeometry} package defines types that
- * represent geometric objects with discrete lengths.
- * 
- * @author Silvan Wyss
- * @date 2019-07-21
- */
-package ch.nolix.system.discretegeometry;
