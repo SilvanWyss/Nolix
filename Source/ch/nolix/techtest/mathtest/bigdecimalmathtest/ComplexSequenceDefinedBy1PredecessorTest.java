4ccb2916e1349bf2a95eaf5c922b096f433fb0f4
Nimeon <silvan.wyss@gmail.com>
2023-12-21 17:24:33 +0100

    extended ComplexSequenceDefinedBy1PredecessorTest

--- Source/ch/nolix/techtest/mathtest/bigdecimalmathtest/ComplexSequenceDefinedBy1PredecessorTest.java
+++ Source/ch/nolix/techtest/mathtest/bigdecimalmathtest/ComplexSequenceDefinedBy1PredecessorTest.java
@@ -1,6 +1,9 @@
 //package declaration
 package ch.nolix.techtest.mathtest.bigdecimalmathtest;
 
+//Java imports
+import java.math.BigDecimal;
+
 //own imports
 import ch.nolix.core.testing.basetest.TestCase;
 import ch.nolix.core.testing.test.Test;
@@ -9,6 +12,62 @@ import ch.nolix.tech.math.bigdecimalmath.ComplexSequenceDefinedBy1Predecessor;
 
 //class
 public final class ComplexSequenceDefinedBy1PredecessorTest extends Test {
+
+  //method
+  @TestCase
+  public void test_getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne_1() {
+
+    //setup
+    final var testUnit = new ComplexSequenceDefinedBy1Predecessor(
+      new ComplexNumber(0.0, 0.0, 20),
+      p -> p.getPower2().getSum(new ComplexNumber(1.0, 0.0)));
+
+    //execution & verification
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(2.5), 4))
+      .isEqualTo(3);
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(12.5), 4))
+      .isEqualTo(4);
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(112.5), 4))
+      .isEqualTo(-1);
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(1112.5), 4))
+      .isEqualTo(-1);
+  }
+
+  //method
+  @TestCase
+  public void test_getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne_2() {
+
+    //setup
+    final var testUnit = new ComplexSequenceDefinedBy1Predecessor(
+      new ComplexNumber(0.0, 0.0, 20),
+      p -> p.getPower2().getSum(new ComplexNumber(1.0, 0.0)));
+
+    //execution & verification
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(2.5), 1))
+      .isEqualTo(-1);
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(2.5), 2))
+      .isEqualTo(-1);
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(2.5), 3))
+      .isEqualTo(3);
+    expect(testUnit.getIterationCountWhereSquaredMagnitudeOfValueExceedsLimitOrMinusOne(BigDecimal.valueOf(2.5), 4))
+      .isEqualTo(3);
+  }
+
+  //method
+  @TestCase
+  public void test_getSquaredMagnitudeOfValueAt1BasedIndex() {
+
+    //setup
+    final var testUnit = new ComplexSequenceDefinedBy1Predecessor(
+      new ComplexNumber(0.0, 0.0, 20),
+      p -> p.getPower2().getSum(new ComplexNumber(1.0, 0.0)));
+
+    //execution & verification
+    expect(testUnit.getSquaredMagnitudeOfValueAt1BasedIndex(1)).isEqualTo(BigDecimal.valueOf(0.0).setScale(20));
+    expect(testUnit.getSquaredMagnitudeOfValueAt1BasedIndex(2)).isEqualTo(BigDecimal.valueOf(1.0).setScale(20));
+    expect(testUnit.getSquaredMagnitudeOfValueAt1BasedIndex(3)).isEqualTo(BigDecimal.valueOf(4.0).setScale(20));
+    expect(testUnit.getSquaredMagnitudeOfValueAt1BasedIndex(4)).isEqualTo(BigDecimal.valueOf(25.0).setScale(20));
+  }
 
   //method
   @TestCase
