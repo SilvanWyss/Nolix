b303ce7897cc928560f51ff4c0a3a27d5cd5dad3
Nimeon <silvan.wyss@gmail.com>
2023-12-28 14:28:09 +0100

    edited IParameterizedPropertyTypeMapper

--- Source/ch/nolix/system/objectdatabase/parameterizedpropertytypemapper/ParameterizedValueTypeMapper.java
+++ Source/ch/nolix/system/objectdatabase/parameterizedpropertytypemapper/ParameterizedValueTypeMapper.java
@@ -2,8 +2,11 @@
 package ch.nolix.system.objectdatabase.parameterizedpropertytypemapper;
 
 //own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.system.objectdatabase.parameterizedpropertytype.ParameterizedValueType;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IEntity;
 import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IParameterizedPropertyType;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.ITable;
 import ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapperapi.IParameterizedPropertyTypeMapper;
 import ch.nolix.systemapi.rawschemaapi.schemadtoapi.IBaseParameterizedValueTypeDto;
 
@@ -14,7 +17,8 @@ implements IParameterizedPropertyTypeMapper<IBaseParameterizedValueTypeDto> {
   //method
   @Override
   public IParameterizedPropertyType createParameterizedPropertyTypeFromDto(
-    final IBaseParameterizedValueTypeDto parameterizedPropertyTypeDto) {
+    final IBaseParameterizedValueTypeDto parameterizedPropertyTypeDto,
+    final IContainer<? extends ITable<IEntity>> referencableTables) {
 
     final var valueType = parameterizedPropertyTypeDto.getDataType().getDataTypeClass();
 
--- Source/ch/nolix/systemapi/objectdatabaseapi/parameterizedpropertytypemapperapi/IParameterizedPropertyTypeMapper.java
+++ Source/ch/nolix/systemapi/objectdatabaseapi/parameterizedpropertytypemapperapi/IParameterizedPropertyTypeMapper.java
@@ -2,12 +2,17 @@
 package ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapperapi;
 
 //own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IEntity;
 import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IParameterizedPropertyType;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.ITable;
 import ch.nolix.systemapi.rawschemaapi.schemadtoapi.IParameterizedPropertyTypeDto;
 
 //interface
 public interface IParameterizedPropertyTypeMapper<PPTDTO extends IParameterizedPropertyTypeDto> {
 
   //method declaration
-  IParameterizedPropertyType createParameterizedPropertyTypeFromDto(PPTDTO parameterizedPropertyTypeDto);
+  IParameterizedPropertyType createParameterizedPropertyTypeFromDto(
+    PPTDTO parameterizedPropertyTypeDto,
+    IContainer<? extends ITable<IEntity>> referencableTables);
 }
