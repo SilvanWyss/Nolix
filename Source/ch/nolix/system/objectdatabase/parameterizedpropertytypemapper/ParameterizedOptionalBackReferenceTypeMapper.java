d015403558eae940dfbdc82c90032035186e8378
Nimeon <silvan.wyss@gmail.com>
2023-12-28 16:34:38 +0100

    created ParameterizedOptionalBackReferenceTypeMapper

---
+++ Source/ch/nolix/system/objectdatabase/parameterizedpropertytypemapper/ParameterizedOptionalBackReferenceTypeMapper.java
@@ -0,0 +1,29 @@
+//package declaration
+package ch.nolix.system.objectdatabase.parameterizedpropertytypemapper;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.system.objectdatabase.parameterizedpropertytype.ParameterizedOptionalBackReferenceType;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IEntity;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IParameterizedPropertyType;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.ITable;
+import ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapperapi.IParameterizedPropertyTypeMapper;
+import ch.nolix.systemapi.rawschemaapi.schemadtoapi.IBaseParameterizedBackReferenceTypeDto;
+
+//class
+public final class ParameterizedOptionalBackReferenceTypeMapper
+implements IParameterizedPropertyTypeMapper<IBaseParameterizedBackReferenceTypeDto> {
+
+  //method
+  @Override
+  public IParameterizedPropertyType createParameterizedPropertyTypeFromDto(
+    final IBaseParameterizedBackReferenceTypeDto parameterizedPropertyTypeDto,
+    final IContainer<? extends ITable<IEntity>> referencableTables) {
+
+    final var backReferencedColumnId = parameterizedPropertyTypeDto.getBackReferencedColumnId();
+    final var referencableColumns = referencableTables.toFromGroups(ITable::getStoredColumns);
+    final var backReferencedColumn = referencableColumns.getStoredFirst(c -> c.hasId(backReferencedColumnId));
+
+    return ParameterizedOptionalBackReferenceType.forBackReferencedColumn(backReferencedColumn);
+  }
+}
