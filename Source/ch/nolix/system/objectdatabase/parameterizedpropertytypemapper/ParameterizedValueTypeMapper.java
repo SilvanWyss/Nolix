ba679d03773ff97f9d5bcaddf06da7082cfbbbec
Nimeon <silvan.wyss@gmail.com>
2023-12-28 13:57:50 +0100

    created ParameterizedValueTypeMapper

---
+++ Source/ch/nolix/system/objectdatabase/parameterizedpropertytypemapper/ParameterizedValueTypeMapper.java
@@ -0,0 +1,23 @@
+//package declaration
+package ch.nolix.system.objectdatabase.parameterizedpropertytypemapper;
+
+//own imports
+import ch.nolix.system.objectdatabase.parameterizedpropertytype.ParameterizedValueType;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IParameterizedPropertyType;
+import ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapperapi.IParameterizedPropertyTypeMapper;
+import ch.nolix.systemapi.rawschemaapi.schemadtoapi.IBaseParameterizedValueTypeDto;
+
+//class
+public final class ParameterizedValueTypeMapper
+implements IParameterizedPropertyTypeMapper<IBaseParameterizedValueTypeDto> {
+
+  //method
+  @Override
+  public IParameterizedPropertyType createParameterizedPropertyTypeFromDto(
+    final IBaseParameterizedValueTypeDto parameterizedPropertyTypeDto) {
+
+    final var valueType = parameterizedPropertyTypeDto.getDataType().getDataTypeClass();
+
+    return ParameterizedValueType.forValueType(valueType);
+  }
+}
