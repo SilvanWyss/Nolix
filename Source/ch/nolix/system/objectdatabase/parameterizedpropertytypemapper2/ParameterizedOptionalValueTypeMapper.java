d4810a5d4aa70d03a2f7b879e88185b93a9bd6a1
Nimeon <silvan.wyss@gmail.com>
2024-01-02 16:21:28 +0100

    created ParameterizedOptionalValueTypeMapper

---
+++ Source/ch/nolix/system/objectdatabase/parameterizedpropertytypemapper2/ParameterizedOptionalValueTypeMapper.java
@@ -0,0 +1,33 @@
+//package declaration
+package ch.nolix.system.objectdatabase.parameterizedpropertytypemapper2;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.system.objectdatabase.propertyhelper.PropertyHelper;
+import ch.nolix.system.objectschema.parameterizedpropertytype.ParameterizedOptionalValueType;
+import ch.nolix.systemapi.entitypropertyapi.datatypeapi.DataType;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IEntity;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IOptionalValue;
+import ch.nolix.systemapi.objectdatabaseapi.databaseapi.ITable;
+import ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapper2api.IParameterizedPropertyTypeMapper;
+import ch.nolix.systemapi.objectdatabaseapi.propertyhelperapi.IPropertyHelper;
+import ch.nolix.systemapi.objectschemaapi.schemaapi.IParameterizedPropertyType;
+
+//class
+public final class ParameterizedOptionalValueTypeMapper
+implements IParameterizedPropertyTypeMapper<IOptionalValue<?>> {
+
+  //constant
+  private static final IPropertyHelper PROPERTY_HELPER = new PropertyHelper();
+
+  //method
+  @Override
+  public IParameterizedPropertyType createParameterizedPropertyTypeFromProperty(
+    final IOptionalValue<?> property,
+    final IContainer<? extends ITable<IEntity>> referencedTables) {
+
+    final var dataType = DataType.forType(PROPERTY_HELPER.getDataType(property));
+
+    return ParameterizedOptionalValueType.forDataType(dataType);
+  }
+}
