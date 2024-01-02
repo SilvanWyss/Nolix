984de2c5bcafbdd9293e6f6e29f015e4d559e933
Nimeon <silvan.wyss@gmail.com>
2024-01-02 13:15:06 +0100

    created parameterizedpropertytypemapper2api package

---
+++ Source/ch/nolix/systemapi/objectdatabaseapi/parameterizedpropertytypemapper2api/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The
+ * {@link ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapper2api}
+ * package declares mappers for
+ * {@link ch.nolix.systemapi.objectschemaapi.schemaapi.IParameterizedPropertyType}.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-02
+ */
+package ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapper2api;
