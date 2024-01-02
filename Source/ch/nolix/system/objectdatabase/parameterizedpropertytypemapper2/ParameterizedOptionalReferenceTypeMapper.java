c253d621f445cc668ff3acb63f34631f12d6f3a6
Nimeon <silvan.wyss@gmail.com>
2024-01-02 16:56:54 +0100

    fixed ParameterizedOptionalReferenceTypeMapper

--- Source/ch/nolix/system/objectdatabase/parameterizedpropertytypemapper2/ParameterizedOptionalReferenceTypeMapper.java
+++ Source/ch/nolix/system/objectdatabase/parameterizedpropertytypemapper2/ParameterizedOptionalReferenceTypeMapper.java
@@ -3,7 +3,7 @@ package ch.nolix.system.objectdatabase.parameterizedpropertytypemapper2;
 
 //own imports
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
-import ch.nolix.system.objectschema.parameterizedpropertytype.ParameterizedReferenceType;
+import ch.nolix.system.objectschema.parameterizedpropertytype.ParameterizedOptionalReferenceType;
 import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IEntity;
 import ch.nolix.systemapi.objectdatabaseapi.databaseapi.IOptionalReference;
 import ch.nolix.systemapi.objectdatabaseapi.parameterizedpropertytypemapper2api.IParameterizedPropertyTypeMapper;
@@ -23,6 +23,6 @@ implements IParameterizedPropertyTypeMapper<IOptionalReference<IEntity>> {
     final var referencedTableName = property.getReferencedTableName();
     final var referencedTable = referencedTables.getStoredFirst(t -> t.hasName(referencedTableName));
 
-    return ParameterizedReferenceType.forReferencedTable(referencedTable);
+    return ParameterizedOptionalReferenceType.forReferencedTable(referencedTable);
   }
 }
