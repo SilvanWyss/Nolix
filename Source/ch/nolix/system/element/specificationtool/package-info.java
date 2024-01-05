5cba14eebca4dde1e321168f7e680e5a8ebc018a
Nimeon <silvan.wyss@gmail.com>
2024-01-05 15:12:21 +0100

    created specificationtool package

---
+++ Source/ch/nolix/system/element/specificationtool/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.element.specificationtool} package defines tools for
+ * {@link ch.nolix.systemapi.elementapi.specificationapi.Specified}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-05
+ */
+package ch.nolix.system.element.specificationtool;
