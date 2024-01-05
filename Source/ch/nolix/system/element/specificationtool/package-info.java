4e798ad2082ead19dd81860f6b3e9f904117f771
Nimeon <silvan.wyss@gmail.com>
2024-01-05 16:21:06 +0100

    deleted specificationtool package

--- Source/ch/nolix/system/element/specificationtool/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.element.specificationtool} package defines tools for
- * {@link ch.nolix.systemapi.elementapi.baseapi.IElement}s.
- * 
- * @author Silvan Wyss
- * @date 2024-01-05
- */
-package ch.nolix.system.element.specificationtool;
