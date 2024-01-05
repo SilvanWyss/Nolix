1da2cfe05c28b4fb26d9a482ca6f724fc7cfdd98
Nimeon <silvan.wyss@gmail.com>
2024-01-05 14:32:13 +0100

    deleted basetestutil package

--- Source/ch/nolix/system/element/basetestutil/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.element.basetestutil} package defines test util
- * types for the {@link ch.nolix.system.element.base} package.
- * 
- * @author Silvan Wyss
- * @date 2023-07-21
- */
-package ch.nolix.system.element.basetestutil;
