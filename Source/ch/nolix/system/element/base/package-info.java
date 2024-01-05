4181b6aae9c348a0ba896dd2770b545e98c48e2c
Nimeon <silvan.wyss@gmail.com>
2024-01-05 14:34:28 +0100

    deleted base package

--- Source/ch/nolix/system/element/base/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.element.base} package defines some base
- * {@link ch.nolix.system.element.main.Element}s.
- * 
- * @author Silvan Wyss
- * @date 2022-07-27
- */
-package ch.nolix.system.element.base;
