46745d346bfeebd0b1e4788cd19ba5c460b6f055
Nimeon <silvan.wyss@gmail.com>
2024-01-05 18:40:16 +0100

    created IStructureElement

---
+++ Source/ch/nolix/systemapi/elementapi/baseapi/IStructureElement.java
@@ -0,0 +1,30 @@
+//package declaration
+package ch.nolix.systemapi.elementapi.baseapi;
+
+//own imports
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.documentapi.nodeapi.INode;
+
+//interface
+/**
+ * @author Silvan Wyss
+ * @date 2024-01-05
+ */
+public interface IStructureElement extends IElement {
+
+  //method declaration
+  /**
+   * @return the child {@link IStructureElement}s of the current
+   *         {@link IStructureElement}.
+   */
+  IContainer<IStructureElement> getStoredChildStructureElements();
+
+  //method declaration
+  /**
+   * @return the structure specification of the current {@link IStructureElement}.
+   *         A structure specification of a {@link IStructureElement} contains
+   *         only the attributes of the {@link IStructureElement} that are
+   *         {@link IStructureElement}s themselves.
+   */
+  INode<?> getStructureSpecification();
+}
