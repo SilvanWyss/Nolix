1941a9e85f48d844960808431798b7dbd08fdda3
Nimeon <silvan.wyss@gmail.com>
2024-01-06 22:39:42 +0100

    created databaseconnectionapi package

--- Source/ch/nolix/coreapi/sqlapi/databaseconnectionapi/ISqlDatabaseTarget.java
+++ Source/ch/nolix/coreapi/sqlapi/databaseconnectionapi/ISqlDatabaseTarget.java
@@ -1,5 +1,5 @@
 //package declaration
-package ch.nolix.core.sql;
+package ch.nolix.coreapi.sqlapi.databaseconnectionapi;
 
 //own imports
 import ch.nolix.coreapi.programcontrolapi.targetapi.IDatabaseTarget;
