eb86146d4392b290ef641a6191d5b1cf5bc35ecc
Nimeon <silvan.wyss@gmail.com>
2024-01-06 19:58:39 +0100

    deleted constraintapi package

--- Source/ch/nolix/coreapi/datamodelapi/constraintapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.datamodelapi.constraintapi} package declares a
- * constraint for database entries.
- * 
- * @author Silvan Wyss
- * @date 2023-08-26
- */
-package ch.nolix.coreapi.datamodelapi.constraintapi;
