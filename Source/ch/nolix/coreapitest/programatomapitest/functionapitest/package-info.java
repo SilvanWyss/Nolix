715e82945e17d8995e5d552f23d4074a16947470
Nimeon <silvan.wyss@gmail.com>
2024-01-07 17:15:09 +0100

    fixed package info

--- Source/ch/nolix/coreapitest/programatomapitest/functionapitest/package-info.java
+++ Source/ch/nolix/coreapitest/programatomapitest/functionapitest/package-info.java
@@ -1,1 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapitest.programatomapitest.functionapitest} package
+ * defines tests for the {@link ch.nolix.coreapi.programatomapi.functionapi}
+ * package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-07
+ */
 package ch.nolix.coreapitest.programatomapitest.functionapitest;
