51cf0995fff6b88da633c2bf09bd6cb2d281d11a
Nimeon <silvan.wyss@gmail.com>
2024-01-07 12:16:06 +0100

    created basicapi package

---
+++ Source/ch/nolix/coreapi/mathapi/basicapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.mathapi.basicapi} package defines types for the
+ * basics of mathematics.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-14
+ */
+package ch.nolix.coreapi.mathapi.basicapi;
