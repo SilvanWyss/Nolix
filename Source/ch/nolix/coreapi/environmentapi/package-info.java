0ff422154c67f38646ddfcfef3081ca4205a48c2
Nimeon <silvan.wyss@gmail.com>
2024-01-07 13:42:58 +0100

    created environmentapi package

---
+++ Source/ch/nolix/coreapi/environmentapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.environmentapi} package declares types for the
+ * environment.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-07
+ */
+package ch.nolix.coreapi.environmentapi;
