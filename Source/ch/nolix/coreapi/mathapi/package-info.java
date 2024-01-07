ee92a9fe7b68ee46c905aee609a9e2e43c8b16fa
Nimeon <silvan.wyss@gmail.com>
2024-01-07 12:08:21 +0100

    created mathapi package

---
+++ Source/ch/nolix/coreapi/mathapi/package-info.java
@@ -0,0 +1,8 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.mathapi} package declares types for mathematics.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-07
+ */
+package ch.nolix.coreapi.mathapi;
