20ba1f6849e7fa7d53b981973010e5bef5c96887
Nimeon <silvan.wyss@gmail.com>
2024-01-16 22:24:24 +0100

    Delete functionapi package

--- Source/ch/nolix/coreapi/programatomapi/functionapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.programatomapi.functionapi} package defines
- * functions.
- * 
- * @author Silvan Wyss
- * @date 2024-01-07
- */
-package ch.nolix.coreapi.programatomapi.functionapi;
