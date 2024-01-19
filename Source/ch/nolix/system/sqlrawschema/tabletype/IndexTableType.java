988db8ca746a9dbf751b3eec031d86ce666b7454
Nimeon <silvan.wyss@gmail.com>
2024-01-19 16:23:43 +0100

    Create IndexTableType enum

---
+++ Source/ch/nolix/system/sqlrawschema/tabletype/IndexTableType.java
@@ -0,0 +1,38 @@
+//package declaration
+package ch.nolix.system.sqlrawschema.tabletype;
+
+//own imports
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IQualifiedNameHolder;
+import ch.nolix.coreapi.programatomapi.variablenameapi.LowerCaseCatalogue;
+
+//enum
+public enum IndexTableType implements IQualifiedNameHolder {
+  ENTITY_HEAD("EntityHead");
+
+  //constant
+  private static final String QUALIFYING_PREFIX = TableType.INDEX_TABLE.getQualifyingPrefix();
+
+  //attribute
+  private final String name;
+
+  //constructor
+  IndexTableType(final String name) {
+
+    GlobalValidator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).isNotBlank();
+
+    this.name = name;
+  }
+
+  //method
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  //method
+  @Override
+  public String getQualifyingPrefix() {
+    return QUALIFYING_PREFIX;
+  }
+}
