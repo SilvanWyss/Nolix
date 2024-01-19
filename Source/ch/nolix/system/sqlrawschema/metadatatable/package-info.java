8d2ed0f3f3a1554626424f91330d4dff7d5ed3b8
Nimeon <silvan.wyss@gmail.com>
2024-01-19 12:58:04 +0100

    Create metadatatable package

---
+++ Source/ch/nolix/system/sqlrawschema/metadatatable/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.sqlrawschema.metadatatable} package defines types
+ * for meta data tables.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-19
+ */
+package ch.nolix.system.sqlrawschema.metadatatable;
