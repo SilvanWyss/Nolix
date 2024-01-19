0fcd7803ddf2605d4345831b98f5966895799a99
Nimeon <silvan.wyss@gmail.com>
2024-01-19 13:33:42 +0100

    Create IQualifiedNameHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/mandatoryattributeapi/IQualifiedNameHolder.java
@@ -0,0 +1,32 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mandatoryattributeapi;
+
+//own imports
+import ch.nolix.coreapi.programstructureapi.markerapi.AllowDefaultMethodsAsDesignPattern;
+
+//interface
+/**
+ * A {@link IQualifiedNameHolder} has a qualified name.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-19
+ */
+@AllowDefaultMethodsAsDesignPattern
+public interface IQualifiedNameHolder extends INameHolder {
+
+  //method
+  /**
+   * @return the qualified name of the current {@link IQualifiedNameHolder}. The
+   *         qualified name of a {@link IQualifiedNameHolder} is its name headed
+   *         by its qualifying prefix.
+   */
+  default String getQualifiedName() {
+    return (getQualifyingPrefix() + getName());
+  }
+
+  //method declaration
+  /**
+   * @return the qualifying prefix of the current {@link IQualifiedNameHolder}.
+   */
+  String getQualifyingPrefix();
+}
