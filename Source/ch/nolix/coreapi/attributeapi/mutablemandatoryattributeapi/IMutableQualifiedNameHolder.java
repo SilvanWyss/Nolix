1004d853f8fac14cc8467bb374de64389f4124fa
Nimeon <silvan.wyss@gmail.com>
2024-01-19 13:36:40 +0100

    Create IMutableQualifiedNameHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/mutablemandatoryattributeapi/IMutableQualifiedNameHolder.java
@@ -0,0 +1,16 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mutablemandatoryattributeapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IQualifiedNameHolder;
+
+//interface
+/**
+ * A {@link IMutableQualifiedNameHolder} is a {@link IQualifiedNameHolder} whose
+ * name can be set programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-19
+ */
+public interface IMutableQualifiedNameHolder extends IQualifiedNameHolder, IMutableNameHolder {
+}
