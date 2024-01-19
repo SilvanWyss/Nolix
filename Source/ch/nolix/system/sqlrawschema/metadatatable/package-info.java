2bd05172c09c4cf551614cdac0bd9fb1960c69d8
Nimeon <silvan.wyss@gmail.com>
2024-01-19 16:25:17 +0100

    Delete metadatatable package

--- Source/ch/nolix/system/sqlrawschema/metadatatable/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.sqlrawschema.metadatatable} package defines types
- * for meta data tables.
- * 
- * @author Silvan Wyss
- * @date 2024-01-19
- */
-package ch.nolix.system.sqlrawschema.metadatatable;
