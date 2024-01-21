0816396fde1d8b40c0d9d87bd53fb7aecf3468f6
Nimeon <silvan.wyss@gmail.com>
2024-01-21 21:50:43 +0100

    Delete functionapitest package

--- Source/ch/nolix/coreapitest/programatomapitest/ProgramAtomApiTestPool.java
+++ Source/ch/nolix/coreapitest/programatomapitest/ProgramAtomApiTestPool.java
@@ -3,13 +3,12 @@ package ch.nolix.coreapitest.programatomapitest;
 
 //own imports
 import ch.nolix.core.testing.basetest.TestPool;
-import ch.nolix.coreapitest.programatomapitest.functionapitest.FunctionApiTestPool;
 
 //class
 public final class ProgramAtomApiTestPool extends TestPool {
 
   //constructor
   public ProgramAtomApiTestPool() {
-    super(new FunctionApiTestPool());
+    super();
   }
 }
