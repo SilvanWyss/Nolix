a01e450d9bba2b5776fb5716d116b4d3ae28bc71
Nimeon <silvan.wyss@gmail.com>
2024-01-21 11:36:44 +0100

    Delete commontypetoolapi package

--- Source/ch/nolix/coreapi/commontypetoolapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.commontypetoolapi} package declares helpers for
- * common types.
- * 
- * @author Silvan Wyss
- * @date 2023-11-17
- */
-package ch.nolix.coreapi.commontypetoolapi;
