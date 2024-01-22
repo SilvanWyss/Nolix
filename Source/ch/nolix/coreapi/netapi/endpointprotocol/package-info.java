f114a8837e659ddb95fb38d6874a9d09c88a915c
Nimeon <silvan.wyss@gmail.com>
2024-01-22 21:59:59 +0100

    Create endpointprotocol package

---
+++ Source/ch/nolix/coreapi/netapi/endpointprotocol/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.netapi.endpointprotocol} package defines the
+ * protocol for {@link ch.nolix.coreapi.netapi.endpointapi.IEndPoint}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-22
+ */
+package ch.nolix.coreapi.netapi.endpointprotocol;
