32d78a03aa44e76ea4be383f8b8d6603ac8f42de
Nimeon <silvan.wyss@gmail.com>
2024-01-22 18:58:20 +0100

    Create errorcontrolapi package

---
+++ Source/ch/nolix/coreapi/errorcontrolapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.errorcontrolapi} package declares types for error
+ * control.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-22
+ */
+package ch.nolix.coreapi.errorcontrolapi;
