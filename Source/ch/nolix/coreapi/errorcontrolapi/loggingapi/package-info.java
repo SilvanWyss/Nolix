7d666aeb3f2f52d3db7aa37f0a2f19a4b919b70f
Nimeon <silvan.wyss@gmail.com>
2024-01-22 19:00:41 +0100

    Create loggingapi package

---
+++ Source/ch/nolix/coreapi/errorcontrolapi/loggingapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.errorcontrolapi.loggingapi} package declares
+ * types for logging.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-22
+ */
+package ch.nolix.coreapi.errorcontrolapi.loggingapi;
