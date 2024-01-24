1afd3fe657bae9d8c068d8b4484dc623e425e3d7
Nimeon <silvan.wyss@gmail.com>
2024-01-24 22:10:43 +0100

    Create resourcecontrol package

---
+++ Source/ch/nolix/core/resourcecontrol/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.resourcecontrol} package defines types that are
+ * declared in the {@link ch.nolix.coreapi.resourcecontrolapi} package
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-24
+ */
+package ch.nolix.core.resourcecontrol;
