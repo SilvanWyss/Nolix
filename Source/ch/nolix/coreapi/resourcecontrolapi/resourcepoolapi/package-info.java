87e702ab7023ac941eefe8bafd2fb9523ffa5ad6
Nimeon <silvan.wyss@gmail.com>
2024-01-24 21:53:43 +0100

    Create resourcepoolapi package

---
+++ Source/ch/nolix/coreapi/resourcecontrolapi/resourcepoolapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.resourcecontrolapi.resourcepoolapi} package
+ * declares a resource pool.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-24
+ */
+package ch.nolix.coreapi.resourcecontrolapi.resourcepoolapi;
