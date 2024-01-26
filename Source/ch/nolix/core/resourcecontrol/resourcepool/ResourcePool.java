7e3c782e09070ca28c637b5380fdc6c044ec9217
Nimeon <silvan.wyss@gmail.com>
2024-01-26 17:29:20 +0100

    Add closeController to ResourcePool

--- Source/ch/nolix/core/resourcecontrol/resourcepool/ResourcePool.java
+++ Source/ch/nolix/core/resourcecontrol/resourcepool/ResourcePool.java
@@ -3,13 +3,18 @@ package ch.nolix.core.resourcecontrol.resourcepool;
 
 //own imports
 import ch.nolix.core.container.linkedlist.LinkedList;
+import ch.nolix.core.programcontrol.closepool.CloseController;
 import ch.nolix.coreapi.functionapi.mutationapi.Closeable;
 import ch.nolix.coreapi.resourcecontrolapi.resourceclosingapi.GroupCloseable;
+import ch.nolix.coreapi.resourcecontrolapi.resourceclosingapi.ICloseController;
 import ch.nolix.coreapi.resourcecontrolapi.resourcepoolapi.IResourcePool;
 
 //class
 public abstract class ResourcePool<WR extends WrapperResource<WR, R>, R extends GroupCloseable>
 implements IResourcePool<WR> {
+
+  //attribute
+  private final ICloseController closeController = CloseController.forElement(this);
 
   //multi-attribute
   private final LinkedList<R> resources = new LinkedList<>();
@@ -24,6 +29,12 @@ implements IResourcePool<WR> {
     final var resource = provideResource();
 
     return createWrapperResourceWithCurrentResourcePoolAndResource(resource);
+  }
+
+  //method
+  @Override
+  public final ICloseController getStoredCloseController() {
+    return closeController;
   }
 
   //method
