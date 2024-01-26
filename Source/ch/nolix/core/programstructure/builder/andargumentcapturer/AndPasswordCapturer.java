179020858563dec5a58d7939e3af33a3459a88ef
Nimeon <silvan.wyss@gmail.com>
2024-01-26 16:11:25 +0100

    Adjust AndPasswordCapturer

--- Source/ch/nolix/core/programstructure/builder/andargumentcapturer/AndPasswordCapturer.java
+++ Source/ch/nolix/core/programstructure/builder/andargumentcapturer/AndPasswordCapturer.java
@@ -2,7 +2,9 @@
 package ch.nolix.core.programstructure.builder.andargumentcapturer;
 
 //own imports
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 import ch.nolix.core.programstructure.builder.main.ArgumentCapturer;
+import ch.nolix.coreapi.programatomapi.variableapi.LowerCaseVariableCatalogue;
 
 //class
 public class AndPasswordCapturer<N> extends ArgumentCapturer<String, N> {
@@ -18,6 +20,9 @@ public class AndPasswordCapturer<N> extends ArgumentCapturer<String, N> {
 
   //method
   public final N andPassword(final String password) {
+
+    GlobalValidator.assertThat(password).thatIsNamed(LowerCaseVariableCatalogue.PASSWORD).isNotNull();
+
     return setArgumentAndGetNext(password);
   }
 
