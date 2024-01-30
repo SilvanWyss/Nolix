4d1ef7f58c4a72c9ed2839fbe86da8e8607133a1
Nimeon <silvan.wyss@gmail.com>
2024-01-30 20:45:29 +0100

    Create arraylist package

---
+++ Source/ch/nolix/core/container/arraylist/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.container.arraylist} package defines a
+ * {@link ch.nolix.coreapi.containerapi.listapi.IArrayList}.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-01-30
+ */
+package ch.nolix.core.container.arraylist;
