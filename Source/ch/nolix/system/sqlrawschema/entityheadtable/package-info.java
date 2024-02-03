4be91976d1ebde4c6733f4e3760064730cc3aa19
Nimeon <silvan.wyss@gmail.com>
2024-02-03 16:24:04 +0100

    Create entityheadtable package

---
+++ Source/ch/nolix/system/sqlrawschema/entityheadtable/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.sqlrawschema.entityheadtable} package defines
+ * types for the entity head table.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-03
+ */
+package ch.nolix.system.sqlrawschema.entityheadtable;
