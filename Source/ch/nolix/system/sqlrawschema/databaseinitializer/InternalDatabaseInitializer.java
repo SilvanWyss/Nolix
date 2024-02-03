28e7a20f73e665fafc6876b5e2ffe9eed042d9ab
Nimeon <silvan.wyss@gmail.com>
2024-02-03 17:27:49 +0100

    Edit InternalDatabaseInitializer

--- Source/ch/nolix/system/sqlrawschema/databaseinitializer/InternalDatabaseInitializer.java
+++ Source/ch/nolix/system/sqlrawschema/databaseinitializer/InternalDatabaseInitializer.java
@@ -1,9 +1,11 @@
 //package declaration
 package ch.nolix.system.sqlrawschema.databaseinitializer;
 
+//own imports
 import ch.nolix.core.sql.connectionpool.SqlConnectionPool;
 import ch.nolix.system.sqlrawschema.columntable.ColumnTableSqlDtoCatalogue;
 import ch.nolix.system.sqlrawschema.databasepropertytable.DatabasePropertyTableSqlDtoCatalogue;
+import ch.nolix.system.sqlrawschema.entityheadtable.EntityHeadTableDtoCatalogue;
 import ch.nolix.system.sqlrawschema.multireferenceentrytable.MultiReferenceEntryTableSqlDtoCatalogue;
 import ch.nolix.system.sqlrawschema.multivalueentrytable.MultiValueEntryTableSqlDtoCatalogue;
 import ch.nolix.system.sqlrawschema.tabletable.TableTableSqlDtoCatalogue;
@@ -23,16 +25,30 @@ final class InternalDatabaseInitializer {
     final ISchemaAdapter schemaAdapter,
     final SqlConnectionPool sqlConnectionPool) {
 
+    createFixTables(schemaAdapter);
+
+    createSchemaTimestampEntry(databaseName, sqlConnectionPool);
+  }
+
+  //method
+  private void createFixTables(final ISchemaAdapter schemaAdapter) {
+
+    //Adds metadata tables.
     schemaAdapter.addTable(DatabasePropertyTableSqlDtoCatalogue.DATABASE_PROPERTY_TABLE_SQL_DTO);
+
+    //Adds schema tables.
     schemaAdapter.addTable(TableTableSqlDtoCatalogue.TABLE_TABLE_SQL_DTO);
     schemaAdapter.addTable(ColumnTableSqlDtoCatalogue.COLUMN_TABLE_SQL_DTO);
 
+    //Adds index tables.
+    schemaAdapter.addTable(EntityHeadTableDtoCatalogue.ENTITY_HEAD_TABLE_DTO);
+
+    //Adds multi-entry tables.
     schemaAdapter.addTable(MultiReferenceEntryTableSqlDtoCatalogue.MULTI_REFERENCE_ENTRY_TABLE_SQL_DTO);
     schemaAdapter.addTable(MultiValueEntryTableSqlDtoCatalogue.MULTI_VALUE_ENTRY_TABLE_SQL_DTO);
 
+    //Save changes to database.
     schemaAdapter.saveChanges();
-
-    createSchemaTimestampEntry(databaseName, sqlConnectionPool);
   }
 
   //method
