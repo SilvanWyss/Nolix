dd9e2668c269a7dbdba129a93fa3db7ad8bbf88c
Nimeon <silvan.wyss@gmail.com>
2024-02-07 22:19:18 +0100

    Create datatool package

---
+++ Source/ch/nolix/tech/relationaldoc/datatool/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.tech.relationaldoc.datatool} package provides an
+ * implementation of the {@link ch.nolix.techapi.relationaldocapi.datatoolapi}
+ * package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-07
+ */
+package ch.nolix.tech.relationaldoc.datatool;
