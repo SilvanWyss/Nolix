d804fcc1965fef7b7049e20d805d808bf99ce565
Nimeon <silvan.wyss@gmail.com>
2024-02-07 22:15:31 +0100

    Create datatoolapi package

---
+++ Source/ch/nolix/techapi/relationaldocapi/datatoolapi/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.techapi.relationaldocapi.datatoolapi} package declares tools
+ * for
+ * {@link ch.nolix.techapi.relationaldocapi.datamodelapi.IAbstractableObject}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-7
+ */
+package ch.nolix.techapi.relationaldocapi.datatoolapi;
