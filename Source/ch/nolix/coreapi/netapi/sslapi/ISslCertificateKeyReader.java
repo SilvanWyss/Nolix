40db87c872dd232d05a1adf3a3f3be5b8b311c57
Nimeon <silvan.wyss@gmail.com>
2024-02-09 13:32:36 +0100

    Create ISslCertificateKeyReader

---
+++ Source/ch/nolix/coreapi/netapi/sslapi/ISslCertificateKeyReader.java
@@ -0,0 +1,9 @@
+//package decalration
+package ch.nolix.coreapi.netapi.sslapi;
+
+//interface
+public interface ISslCertificateKeyReader {
+
+  //method declaration
+  String readKeyFromPemFile(String pemFilePath);
+}
