733fa8fa8e62828aab3f18acdd964b4f84583bac
Nimeon <silvan.wyss@gmail.com>
2024-02-11 20:09:24 +0100

    Refactor GlobalObjectTool

--- Source/ch/nolix/core/reflection/GlobalObjectTool.java
+++ Source/ch/nolix/core/reflection/GlobalObjectTool.java
@@ -7,6 +7,8 @@ import java.lang.reflect.Field;
 //own imports
 import ch.nolix.core.errorcontrol.exception.WrapperException;
 import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentDoesNotHaveAttributeException;
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.coreapi.programatomapi.variableapi.LowerCaseVariableCatalogue;
 
 //class
 public final class GlobalObjectTool {
@@ -16,31 +18,43 @@ public final class GlobalObjectTool {
   }
 
   //static method
-  public static String getNameOfFirstFieldOfObjectThatStoresValue(final Object object, final Object attribute) {
-    return getFirstFieldOfObjectThatStoresValue(object, attribute).getName();
-  }
+  public static Field getFirstFieldOfObjectThatStoresValue(final Object object, final Object value) {
 
-  //static method
-  public static Field getFirstFieldOfObjectThatStoresValue(final Object object, final Object attribute) {
+    GlobalValidator.assertThat(value).thatIsNamed(LowerCaseVariableCatalogue.VALUE).isNotNull();
 
-    var lClass = object.getClass();
-    while (lClass != null) {
-      for (final var f : lClass.getDeclaredFields()) {
+    var localClass = object.getClass();
+    while (localClass != null) {
 
-        f.setAccessible(true);
-
-        try {
-          if (f.get(object) == attribute) {
-            return f;
-          }
-        } catch (final IllegalArgumentException | IllegalAccessException exception) {
-          throw WrapperException.forError(exception);
+      for (final var f : localClass.getDeclaredFields()) {
+        final var fieldValue = getValueOfFieldOfObject(object, f);
+        if (fieldValue == value) {
+          return f;
         }
       }
 
-      lClass = lClass.getSuperclass();
+      localClass = localClass.getSuperclass();
     }
 
-    throw ArgumentDoesNotHaveAttributeException.forArgumentAndAttributeType(object, attribute.getClass());
+    throw ArgumentDoesNotHaveAttributeException.forArgumentAndAttributeType(object, value.getClass());
+  }
+
+  //static method
+  public static String getNameOfFirstFieldOfObjectThatStoresValue(final Object object, final Object value) {
+
+    final var field = getFirstFieldOfObjectThatStoresValue(object, value);
+
+    return field.getName();
+  }
+
+  //static method
+  public static Object getValueOfFieldOfObject(final Object object, final Field field) {
+
+    field.setAccessible(true);
+
+    try {
+      return field.get(object);
+    } catch (final IllegalAccessException exception) {
+      throw WrapperException.forError(exception);
+    }
   }
 }
