c4a3e9cc62bad30cacd12e784eadb16d21b46858
Nimeon <silvan.wyss@gmail.com>
2024-02-11 15:27:37 +0100

    Create IMutableOptionalSaveStampHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalSaveStampHolder.java
@@ -0,0 +1,34 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mutableoptionalattributeapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.optionalattributeapi.IOptionalSaveStampHolder;
+
+//interface
+/**
+ * A {@link IMutableOptionalSaveStampHolder} is a
+ * {@link IOptionalSaveStampHolder} whose save stamp can be set and removed
+ * programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-11
+ */
+public interface IMutableOptionalSaveStampHolder extends IOptionalSaveStampHolder {
+
+  //method declaration
+  /**
+   * Removes the save stamp of the current
+   * {@link IMutableOptionalSaveStampHolder}.
+   */
+  void removeSaveStamp();
+
+  //method declaration
+  /**
+   * Sets the save stamp of the current {@link IMutableOptionalSaveStampHolder}.
+   * 
+   * @param saveStamp
+   * @throws RuntimeException if the given saveStamp is null.
+   * @throws RuntimeException if the given saveStamp is blank.
+   */
+  void setSaveStamp(String saveStamp);
+}
