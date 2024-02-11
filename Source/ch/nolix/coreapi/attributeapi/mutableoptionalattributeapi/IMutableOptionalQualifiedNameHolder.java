4f280d7ab8706397b943d9081737e89d3bfac6ac
Nimeon <silvan.wyss@gmail.com>
2024-02-11 15:39:04 +0100

    Create IMutableOptionalQualifiedNameHolder

---
+++ Source/ch/nolix/coreapi/attributeapi/mutableoptionalattributeapi/IMutableOptionalQualifiedNameHolder.java
@@ -0,0 +1,17 @@
+//package declaration
+package ch.nolix.coreapi.attributeapi.mutableoptionalattributeapi;
+
+//own imports
+import ch.nolix.coreapi.attributeapi.optionalattributeapi.IOptionalQualifiedNameHolder;
+
+//interface
+/**
+ * A {@link IMutableOptionalQualifiedNameHolder} is a
+ * {@link IOptionalQualifiedNameHolder} whose qualified name can be set and
+ * removed programmatically.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-11
+ */
+public interface IMutableOptionalQualifiedNameHolder extends IMutableOptionalNameHolder, IOptionalQualifiedNameHolder {
+}
