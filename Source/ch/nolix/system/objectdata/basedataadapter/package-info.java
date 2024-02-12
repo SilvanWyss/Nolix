254d9644b4fb2d465d25ce3067ff6a4f938e1690
Nimeon <silvan.wyss@gmail.com>
2024-02-12 21:16:30 +0100

    Create basedataadapter package

---
+++ Source/ch/nolix/system/objectdata/basedataadapter/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.objectdata.basedataadapter} packages defines a
+ * base {@link ch.nolix.systemapi.objectdataapi.dataadapterapi.IDataAdapter}.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-12
+ */
+package ch.nolix.system.objectdata.basedataadapter;
