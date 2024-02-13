a2a3a0cfaa0552c618040305a46bc373614dc811
Nimeon <silvan.wyss@gmail.com>
2024-02-13 21:44:45 +0100

    Create multibackreferenceentrytable package

---
+++ Source/ch/nolix/system/sqlrawschema/multibackreferenceentrytable/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.system.sqlrawschema.multibackreferenceentrytable} package
+ * defines types for multi-back-reference entry tables.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-13
+ */
+package ch.nolix.system.sqlrawschema.multibackreferenceentrytable;
