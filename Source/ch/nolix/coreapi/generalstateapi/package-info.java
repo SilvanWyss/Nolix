eeea264e504640f047b8220dc7ce188b7a4dad5a
Nimeon <silvan.wyss@gmail.com>
2024-02-16 17:40:31 +0100

    Create generalstateapi package

---
+++ Source/ch/nolix/coreapi/generalstateapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.generalstateapi} package declares types with
+ * general states.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-16
+ */
+package ch.nolix.coreapi.generalstateapi;
