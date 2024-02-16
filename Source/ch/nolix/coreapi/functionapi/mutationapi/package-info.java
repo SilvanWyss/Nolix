15f3d09b1deb16e80df30a6cee24d61840900478
Nimeon <silvan.wyss@gmail.com>
2024-02-16 19:40:24 +0100

    Delete functionapi package

--- Source/ch/nolix/coreapi/functionapi/mutationapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.functionapi.mutationapi} declares types that can
- * do certain actions.
- * 
- * @author Silvan Wyss
- * @date 2022-07-05
- */
-package ch.nolix.coreapi.functionapi.mutationapi;
--- Source/ch/nolix/coreapi/functionapi/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.functionapi} package declares functions.
- * 
- * @author Silvan Wyss
- * @date 2022-07-05
- */
-package ch.nolix.coreapi.functionapi;
