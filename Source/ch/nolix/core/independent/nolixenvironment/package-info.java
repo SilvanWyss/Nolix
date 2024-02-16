a9208cf93a361d54265c57ee248d67ec80fe21ee
Nimeon <silvan.wyss@gmail.com>
2024-02-16 22:38:41 +0100

    Delete old nolixenvironment package

--- Source/ch/nolix/core/independent/nolixenvironment/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.independent.nolixenvironment} package defines types
- * for Nolix environments that do not have other Nolix dependencies.
- * 
- * @author Silvan Wyss
- * @date 2023-02-06
- */
-package ch.nolix.core.independent.nolixenvironment;
