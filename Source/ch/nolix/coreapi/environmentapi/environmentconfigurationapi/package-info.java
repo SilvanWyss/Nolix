f77cb0c87e1b0878450248a796f3d32563874d39
Nimeon <silvan.wyss@gmail.com>
2024-02-16 22:35:52 +0100

    Delete environmentconfigurationapi package

--- Source/ch/nolix/coreapi/environmentapi/environmentconfigurationapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.environmentapi.environmentconfigurationapi}
- * package declares types for the environment configuration.
- * 
- * @author Silvan Wyss
- * @date 2024-01-07
- */
-package ch.nolix.coreapi.environmentapi.environmentconfigurationapi;
