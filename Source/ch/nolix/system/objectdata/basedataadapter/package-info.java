b3a17dc1dc46afc709b5fb51998f984f3f182dfc
Nimeon <silvan.wyss@gmail.com>
2024-02-16 22:45:22 +0100

    Delete basedataadapter package

--- Source/ch/nolix/system/objectdata/basedataadapter/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.objectdata.basedataadapter} packages defines a
- * base {@link ch.nolix.systemapi.objectdataapi.dataadapterapi.IDataAdapter}.
- * 
- * @author Silvan Wyss
- * @date 2024-02-12
- */
-package ch.nolix.system.objectdata.basedataadapter;
