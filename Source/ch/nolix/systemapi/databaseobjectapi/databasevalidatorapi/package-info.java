970abb1701af84f18df0fb5cb3ae854908416a97
Nimeon <silvan.wyss@gmail.com>
2024-02-20 20:32:54 +0100

    Create databasevalidatorapi package

---
+++ Source/ch/nolix/systemapi/databaseobjectapi/databasevalidatorapi/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.databaseobjectapi.databasevalidatorapi} package
+ * declares validators for
+ * {@link ch.nolix.systemapi.databaseobjectapi.databaseobjectapi.IDatabaseObject}s
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-20
+ */
+package ch.nolix.systemapi.databaseobjectapi.databasevalidatorapi;
