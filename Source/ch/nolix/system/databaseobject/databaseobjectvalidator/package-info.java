2276d03b0e3b1c72b3e9c1fabaef4f99cf4db27a
Nimeon <silvan.wyss@gmail.com>
2024-02-20 20:33:15 +0100

    Create databaseobjectvalidator package

---
+++ Source/ch/nolix/system/databaseobject/databaseobjectvalidator/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.databaseobject.databaseobjectvalidator} package
+ * defines the types that are declared in the
+ * {@link ch.nolix.systemapi.databaseobjectapi.databaseobjectvalidatorapi}.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-20
+ */
+package ch.nolix.system.databaseobject.databaseobjectvalidator;
