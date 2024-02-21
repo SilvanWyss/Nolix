56a805b0f784f496280097a718289391e1460ebf
Nimeon <silvan.wyss@gmail.com>
2024-02-21 21:43:59 +0100

    Create noderawschemaapi package

---
+++ Source/ch/nolix/systemapi/noderawschemaapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.noderawschemaapi} package declares types to
+ * access the schema of node databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-21
+ */
+package ch.nolix.systemapi.noderawschemaapi;
