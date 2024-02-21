5331f0edbc222051e7445502fef3228be8412ad9
Nimeon <silvan.wyss@gmail.com>
2024-02-21 21:45:55 +0100

    Create nodesearcherapi package

---
+++ Source/ch/nolix/systemapi/noderawschemaapi/nodesearcherapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.noderawschemaapi.nodesearcherapi} package
+ * declares searches for the schema of node databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-21
+ */
+package ch.nolix.systemapi.noderawschemaapi.nodesearcherapi;
