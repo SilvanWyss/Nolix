c71bd4331fb28906b55cc42406c94f1e77e6dc54
Nimeon <silvan.wyss@gmail.com>
2024-02-23 16:43:23 +0100

    Create noderawdataapi package

---
+++ Source/ch/nolix/systemapi/noderawdataapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.noderawdataapi} package declares types to
+ * access the raw data of node databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-23
+ */
+package ch.nolix.systemapi.noderawdataapi;
