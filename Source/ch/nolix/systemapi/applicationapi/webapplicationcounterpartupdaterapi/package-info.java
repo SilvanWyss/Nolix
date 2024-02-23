bcf2ab1bac098ab7184521adb34ec37639c805e0
Nimeon <silvan.wyss@gmail.com>
2024-02-23 17:18:06 +0100

    Create webapplicationcounterpartupdaterapi package

---
+++ Source/ch/nolix/systemapi/applicationapi/webapplicationcounterpartupdaterapi/package-info.java
@@ -0,0 +1,11 @@
+//package info
+/**
+ * The
+ * {@link ch.nolix.systemapi.applicationapi.webapplicationcounterpartupdaterapi}
+ * package declares types to update the counterpart of web
+ * {@link ch.nolix.system.application.main.Application}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-23
+ */
+package ch.nolix.systemapi.applicationapi.webapplicationcounterpartupdaterapi;
