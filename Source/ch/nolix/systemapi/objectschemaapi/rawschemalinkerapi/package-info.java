800766ee8ea1a41df3aa3b8c90a250f117d00093
Nimeon <silvan.wyss@gmail.com>
2024-02-23 16:01:38 +0100

    Create rawschemalinkerapi package

---
+++ Source/ch/nolix/systemapi/objectschemaapi/rawschemalinkerapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.objectschemaapi.rawschemalinkerapi} package
+ * declares types to access raw schemas for object schemas .
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-23
+ */
+package ch.nolix.systemapi.objectschemaapi.rawschemalinkerapi;
