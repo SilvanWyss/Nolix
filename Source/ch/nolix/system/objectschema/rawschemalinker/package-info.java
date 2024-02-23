d6910a83a84b6b341c5e48fddbdc5f5cea9235d4
Nimeon <silvan.wyss@gmail.com>
2024-02-23 16:01:58 +0100

    Create rawschemalinker package

---
+++ Source/ch/nolix/system/objectschema/rawschemalinker/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.system.objectschema.rawschemalinker} packages defines the
+ * types that are declared in the
+ * {@link ch.nolix.systemapi.objectschemaapi.rawschemalinkerapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-23
+ */
+package ch.nolix.system.objectschema.rawschemalinker;
