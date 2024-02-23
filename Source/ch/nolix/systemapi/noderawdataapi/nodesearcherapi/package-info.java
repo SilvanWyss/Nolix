2cb8d027f7acc8d85de562b3c531575e030ce798
Nimeon <silvan.wyss@gmail.com>
2024-02-23 16:44:38 +0100

    Create nodesearcherapi package

---
+++ Source/ch/nolix/systemapi/noderawdataapi/nodesearcherapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.noderawdataapi.nodesearcherapi} package
+ * declares searches for the raw data of node databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-02-23
+ */
+package ch.nolix.systemapi.noderawdataapi.nodesearcherapi;
