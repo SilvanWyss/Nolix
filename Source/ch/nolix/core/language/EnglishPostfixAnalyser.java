17b84d929793cd50fa83ab6cab16bfdb33f9d770
Nimeon <silvan.wyss@gmail.com>
2024-03-03 18:05:39 +0100

    Suppress Sonar warning

--- Source/ch/nolix/core/language/EnglishPostfixAnalyser.java
+++ Source/ch/nolix/core/language/EnglishPostfixAnalyser.java
@@ -7,7 +7,7 @@ public final class EnglishPostfixAnalyser {
   //method
   public boolean endsWithVocalAndY(final String noun) {
     return //
-    noun.endsWith("ay")
+    noun.endsWith("ay") //NOSONAR: This method is uniform.
     || noun.endsWith("ey")
     || noun.endsWith("iy")
     || noun.endsWith("oy")
