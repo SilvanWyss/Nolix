7af4e949b8ad214659afe045ad74b9052555d46c
Nimeon <silvan.wyss@gmail.com>
2024-03-03 19:22:25 +0100

    Create databaseinitializingapi package

---
+++ Source/ch/nolix/systemapi/noderawschemaapi/databaseinitializingapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.noderawschemaapi.databaseinitializingapi}
+ * package declares types to initialize node databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-03-03
+ */
+package ch.nolix.systemapi.noderawschemaapi.databaseinitializingapi;
