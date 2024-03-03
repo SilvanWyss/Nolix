e98d233a4c3735d95269eb32d7daf9dcc2bc4068
Nimeon <silvan.wyss@gmail.com>
2024-03-03 19:28:48 +0100

    Edit TableDefinitionLoader

--- Source/ch/nolix/system/noderawdata/datareader/TableDefinitionLoader.java
+++ Source/ch/nolix/system/noderawdata/datareader/TableDefinitionLoader.java
@@ -4,11 +4,14 @@ package ch.nolix.system.noderawdata.datareader;
 //own imports
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
-import ch.nolix.system.noderawschema.structure.StructureHeaderCatalogue;
+import ch.nolix.system.noderawschema.nodesearcher.DatabaseNodeSearcher;
 import ch.nolix.systemapi.rawdataapi.schemainfoapi.ITableInfo;
 
 //class
 public final class TableDefinitionLoader {
+
+  //constant
+  private static final DatabaseNodeSearcher DATABASE_NODE_SEARCHER = new DatabaseNodeSearcher();
 
   //constant
   private static final TableDefinitionMapper TABLE_DEFINITION_MAPPER = new TableDefinitionMapper();
@@ -16,7 +19,7 @@ public final class TableDefinitionLoader {
   //method
   public IContainer<ITableInfo> loadTableDefinitionsFromDatabaseNode(final IMutableNode<?> databaseNode) {
 
-    final var tableNodes = databaseNode.getStoredChildNodesWithHeader(StructureHeaderCatalogue.TABLE);
+    final var tableNodes = DATABASE_NODE_SEARCHER.getStoredTableNodesFromDatabaseNode(databaseNode);
 
     return tableNodes.to(TABLE_DEFINITION_MAPPER::createTableDefinitionFromTableNode);
   }
