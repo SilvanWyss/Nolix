f206cc5c1690f195a1854179fb922e1f88cfd8f1
Nimeon <silvan.wyss@gmail.com>
2024-03-15 17:10:26 +0100

    Create entityrequestapitest package

---
+++ Source/ch/nolix/coreapitest/datamodelapitest/entityrequestapitest/EntityRequestApiTestPool.java
@@ -0,0 +1,14 @@
+//package declaration
+package ch.nolix.coreapitest.datamodelapitest.entityrequestapitest;
+
+//own imports
+import ch.nolix.core.testing.basetest.TestPool;
+
+//class
+public final class EntityRequestApiTestPool extends TestPool {
+
+  //constructor
+  public EntityRequestApiTestPool() {
+    super();
+  }
+}
