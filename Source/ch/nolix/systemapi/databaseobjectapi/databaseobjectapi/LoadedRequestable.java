549cd0d5eb6d570414438b725920eb76e09a35b7
Nimeon <silvan.wyss@gmail.com>
2024-03-15 11:20:45 +0100

    Create LoadedRequestable

---
+++ Source/ch/nolix/systemapi/databaseobjectapi/databaseobjectapi/LoadedRequestable.java
@@ -0,0 +1,9 @@
+//package declaration
+package ch.nolix.systemapi.databaseobjectapi.databaseobjectapi;
+
+//interface
+public interface LoadedRequestable {
+
+  //method declaration
+  boolean isLoaded();
+}
