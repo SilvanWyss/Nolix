f951a34abfe5089156ec8c85127f44f6e3770291
Nimeon <silvan.wyss@gmail.com>
2024-03-15 11:49:18 +0100

    Create EditedRequestable

---
+++ Source/ch/nolix/systemapi/databaseobjectapi/databaseobjectapi/EditedRequestable.java
@@ -0,0 +1,9 @@
+//package declaration
+package ch.nolix.systemapi.databaseobjectapi.databaseobjectapi;
+
+//interface
+public interface EditedRequestable {
+
+  //method declaration
+  boolean isEdited();
+}
