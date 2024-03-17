fc75c18aeaca09ebe28083603633f7749c51fbc1
Nimeon <silvan.wyss@gmail.com>
2024-03-17 19:38:20 +0100

    Delete nolixtest package

--- Source/ch/nolix/nolixtest/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.nolixtest} package defines a test pool that bundles all
- * tests of the {@link ch.nolix} package.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.nolixtest;
