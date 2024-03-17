7d520a4281daffff9927c9175799c5d526a501a8
Nimeon <silvan.wyss@gmail.com>
2024-03-17 21:55:21 +0100

    Delete testingapi package

--- Source/ch/nolix/coreapi/testingapi/package-info.java
+++
@@ -1,8 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.testingapi} package declares types for testing.
- * 
- * @author Silvan Wyss
- * @date 2024-01-06
- */
-package ch.nolix.coreapi.testingapi;
--- Source/ch/nolix/coreapi/testingapi/testapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.coreapi.testingapi.testapi} package declares types for
- * tests.
- * 
- * @author Silvan Wyss
- * @date 2024-01-06
- */
-package ch.nolix.coreapi.testingapi.testapi;
