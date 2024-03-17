8091d92ca979843f50a9e0f612cd7cbd9766a6ca
Nimeon <silvan.wyss@gmail.com>
2024-03-17 21:53:07 +0100

    Delete basetest package

--- Source/ch/nolix/core/testing/basetest/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.testing.basetest} package defines a base test and a
- * test pool.
- * 
- * @author Silvan Wyss
- * @date 2019-08-01
- */
-package ch.nolix.core.testing.basetest;
