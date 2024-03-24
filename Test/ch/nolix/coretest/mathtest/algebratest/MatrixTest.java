365fdabfec2e1e67f5e6793f321b89fc73f9c495
Nimeon <silvan.wyss@gmail.com>
2024-03-24 21:57:21 +0100

    Edit MatrixTest

--- Test/ch/nolix/coretest/mathtest/algebratest/MatrixTest.java
+++ Test/ch/nolix/coretest/mathtest/algebratest/MatrixTest.java
@@ -3,6 +3,8 @@ package ch.nolix.coretest.mathtest.algebratest;
 
 //JUnit imports
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 //own imports
 import ch.nolix.core.errorcontrol.invalidargumentexception.UnrepresentingArgumentException;
@@ -235,59 +237,23 @@ final class MatrixTest extends StandardTest {
   }
 
   //method
-  @Test
-  void testCase_toString_whenMatrixIs1x1IdentityMatrix() {
+  @ParameterizedTest
+  @CsvSource({
+  "1, '[1]'",
+  "2, '[1,0;0,1]'",
+  "3, '[1,0,0;0,1,0;0,0,1]'",
+  "4, '[1,0,0,0;0,1,0,0;0,0,1,0;0,0,0,1]'"
+  })
+  void testCase_toString_whenIsIdentityMatrix(final int size, final String expectedResult) {
 
     //setup
-    final var testUnit = Matrix.createIdendityMatrix(1);
+    final var testUnit = Matrix.createIdendityMatrix(size);
 
     //execution
     final var result = testUnit.toString();
 
     //execution
-    expect(result).isEqualTo("[1]");
-  }
-
-  //method
-  @Test
-  void testCase_toString_whenMatrixIs2x2IdentityMatrix() {
-
-    //setup
-    final var testUnit = Matrix.createIdendityMatrix(2);
-
-    //execution
-    final var result = testUnit.toString();
-
-    //execution
-    expect(result).isEqualTo("[1,0;0,1]");
-  }
-
-  //method
-  @Test
-  void testCase_toString_whenMatrixIs3x3IdentityMatrix() {
-
-    //setup
-    final var testUnit = Matrix.createIdendityMatrix(3);
-
-    //execution
-    final var result = testUnit.toString();
-
-    //execution
-    expect(result).isEqualTo("[1,0,0;0,1,0;0,0,1]");
-  }
-
-  //method
-  @Test
-  void testCase_toString_whenMatrixIs4x4IdentityMatrix() {
-
-    //setup
-    final var testUnit = Matrix.createIdendityMatrix(4);
-
-    //execution
-    final var result = testUnit.toString();
-
-    //execution
-    expect(result).isEqualTo("[1,0,0,0;0,1,0,0;0,0,1,0;0,0,0,1]");
+    expect(result).isEqualTo(expectedResult);
   }
 
   //method
