3cd2b7382c89d0a41df236a773f6a8171abb247c
Nimeon <silvan.wyss@gmail.com>
2024-03-31 12:00:59 +0200

    Create nolixenvironment package

---
+++ Source/ch/nolix/core/environment/nolixenvironment/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.environment.nolixenvironment} package defines types
+ * for the Nolix environments.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-03-31
+ */
+package ch.nolix.core.environment.nolixenvironment;
