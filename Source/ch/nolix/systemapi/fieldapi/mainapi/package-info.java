f1d09b6c70fbd40cf17f0a4d7b18c41f71e7bf47
Nimeon <silvan.wyss@gmail.com>
2024-04-07 16:02:56 +0200

    Update package info

--- Source/ch/nolix/systemapi/fieldapi/mainapi/package-info.java
+++ Source/ch/nolix/systemapi/fieldapi/mainapi/package-info.java
@@ -1,7 +1,6 @@
 //package info
 /**
- * The {@link ch.nolix.systemapi.fieldapi.mainapi} package defines
- * property types.
+ * The {@link ch.nolix.systemapi.fieldapi.mainapi} package defines field types.
  * 
  * @author Silvan Wyss
  * @date 2021-06-30
