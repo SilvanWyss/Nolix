ca9085d96fd9c6291b76b39dbcf9715db6cf0a5e
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-07 22:43:14 +0200

    Delete stylebuilder package

--- Source/ch/nolix/system/element/stylebuilder/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.system.element.style} package defines a builder for
- * {@link ch.nolix.system.element.style.Style}s.
- * 
- * @author Silvan Wyss
- * @date 2023-07-10
- */
-package ch.nolix.system.element.stylebuilder;
