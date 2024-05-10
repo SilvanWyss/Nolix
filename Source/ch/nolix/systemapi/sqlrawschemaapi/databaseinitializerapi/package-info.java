e98e1e37539a17d2511f348fd664596fc0246983
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-10 14:27:19 +0200

    Create databaseinitializerapi package

---
+++ Source/ch/nolix/systemapi/sqlrawschemaapi/databaseinitializerapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.sqlrawschemaapi.databaseinitializerapi} package
+ * declares types to initialize SQL databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-10
+ */
+package ch.nolix.systemapi.sqlrawschemaapi.databaseinitializerapi;
