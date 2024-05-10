27e645fb76c671984e46d307b4072bd01095cc72
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-10 14:14:31 +0200

    Create tabletableapi package

---
+++ Source/ch/nolix/systemapi/sqlrawschemaapi/tabletableapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.sqlrawschemaapi.tabletableapi} package declares
+ * types for the schema of the meta table for tables.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-10
+ */
+package ch.nolix.systemapi.sqlrawschemaapi.tabletableapi;
