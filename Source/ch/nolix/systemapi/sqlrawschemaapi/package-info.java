57db9c024a769ef422ecb4031d63288a0257949c
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-10 14:12:50 +0200

    Create sqlrawschemaapi package

---
+++ Source/ch/nolix/systemapi/sqlrawschemaapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.sqlrawschemaapi} package declares types to
+ * access the schema of SQL databases.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-10
+ */
+package ch.nolix.systemapi.sqlrawschemaapi;
