1f72b1814239bd2909b0c22adba44a885122bee0
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-11 22:33:24 +0200

    Create ControlAnalyserTest

---
+++ Test/ch/nolix/systemtest/webguitest/controltooltest/ControlAnalyserTest.java
@@ -0,0 +1,89 @@
+//package declaration
+package ch.nolix.systemtest.webguitest.controltooltest;
+
+//JUnit imports
+import org.junit.jupiter.api.Test;
+
+//own imports
+import ch.nolix.core.testing.test.StandardTest;
+import ch.nolix.system.webgui.atomiccontrol.Button;
+import ch.nolix.system.webgui.atomiccontrol.ImageControl;
+import ch.nolix.system.webgui.atomiccontrol.Label;
+import ch.nolix.system.webgui.atomiccontrol.Link;
+import ch.nolix.system.webgui.atomiccontrol.Textbox;
+import ch.nolix.system.webgui.controltool.ControlAnalyser;
+import ch.nolix.system.webgui.linearcontainer.FloatContainer;
+import ch.nolix.system.webgui.linearcontainer.VerticalStack;
+
+//class
+final class ControlAnalyserTest extends StandardTest {
+
+  //method
+  @Test
+  void testCase_firstControlContainsSecondControl_whenTheGivenFirstControlIsNull() {
+
+    //setup
+    final var testUnit = new ControlAnalyser();
+
+    //execution
+    final var result = testUnit.firstControlContainsSecondControl(null, new Label());
+
+    //verification
+    expectNot(result);
+  }
+
+  //method
+  @Test
+  void testCase_firstControlContainsSecondControl_whenTheGivenSecondControlIsNull() {
+
+    //setup
+    final var testUnit = new ControlAnalyser();
+
+    //execution
+    final var result = testUnit.firstControlContainsSecondControl(new Label(), null);
+
+    //verification
+    expectNot(result);
+  }
+
+  //method
+  @Test
+  void testCase_firstControlContainsSecondControl_whenTheGivenFirstControlContainsTheGivenSecondControlDirectly() {
+
+    //setup
+    final var label = new Label();
+    final var verticalStack = new VerticalStack().addControl(new Button(), new Link(), label);
+    final var testUnit = new ControlAnalyser();
+
+    //execution
+    final var result = testUnit.firstControlContainsSecondControl(verticalStack, label);
+
+    //verification
+    expect(result);
+  }
+
+  //method
+  @Test
+  void testCase_firstControlContainsSecondControl_whenTheGivenFirstControlContainsTheGivenSecondControlRecursively() {
+
+    //setup
+    final var label = new Label();
+    final var verticalStack = //
+    new VerticalStack()
+      .addControl(
+        new Button(),
+        new Link(),
+        new FloatContainer()
+          .addControl(
+            new Textbox(),
+            new ImageControl(),
+            label));
+    final var testUnit = new ControlAnalyser();
+
+    //execution
+    final var result = testUnit.firstControlContainsSecondControl(verticalStack, label);
+
+    //verification
+    expect(result);
+  }
+}
