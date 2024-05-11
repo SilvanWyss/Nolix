21bd486f2fc183e74363d14448aea49676065f8d
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-11 22:16:09 +0200

    Create controltooltest package

---
+++ Test/ch/nolix/systemtest/webguitest/controltooltest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.webguitest.controltooltest} package defines
+ * tests for the {@link ch.nolix.system.webgui.controltool} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-11
+ */
+package ch.nolix.systemtest.webguitest.controltooltest;
