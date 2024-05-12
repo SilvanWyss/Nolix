2648055d08677ad8f981c4d607262da7ffacac78
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-12 15:46:57 +0200

    Create fieldapitest package

---
+++ Test/ch/nolix/systemapitest/fieldapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapitest.fieldapitest} package defines tests for the
+ * {@link ch.nolix.systemapi.fieldapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-12
+ */
+package ch.nolix.systemapitest.fieldapitest;
