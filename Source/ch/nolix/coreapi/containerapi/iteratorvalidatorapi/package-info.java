b21c60ff442ed1a19aa3281a3f5e2173eeb39db7
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-12 16:11:15 +0200

    Create iteratorvalidatorapi package

---
+++ Source/ch/nolix/coreapi/containerapi/iteratorvalidatorapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.containerapi.iteratorvalidatorapi} declares a
+ * validator for {@link java.util.Iterator}s.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-12
+ */
+package ch.nolix.coreapi.containerapi.iteratorvalidatorapi;
