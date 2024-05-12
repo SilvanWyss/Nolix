49bd1cf17411c9871f123b98f6771b53b4572a00
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-12 16:15:51 +0200

    Create iteratorvalidator package

---
+++ Source/ch/nolix/core/container/iteratorvalidator/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.core.container.iteratorvalidator} package provides an
+ * implementation of the
+ * {@link ch.nolix.coreapi.containerapi.iteratorvalidatorapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-12
+ */
+package ch.nolix.core.container.iteratorvalidator;
