332c9c1350c79ab9c9cfe00f331f88d6579098eb
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-13 22:10:46 +0200

    Create fieldproperty package

---
+++ Source/ch/nolix/systemapi/objectdataapi/fieldproperty/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapi.objectdataapi.fieldproperty} package defines
+ * properties for fields.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-05-13
+ */
+package ch.nolix.systemapi.objectdataapi.fieldproperty;
