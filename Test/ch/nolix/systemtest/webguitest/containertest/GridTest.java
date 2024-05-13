61981bf616119de4a2aa2ecb65e27420c6189b05
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-13 22:40:43 +0200

    Extend GridTest

--- Test/ch/nolix/systemtest/webguitest/containertest/GridTest.java
+++ Test/ch/nolix/systemtest/webguitest/containertest/GridTest.java
@@ -1,7 +1,11 @@
 //package declaration
 package ch.nolix.systemtest.webguitest.containertest;
 
+//JUnit imports
+import org.junit.jupiter.api.Test;
+
 //own imports
+import ch.nolix.system.webgui.atomiccontrol.Label;
 import ch.nolix.system.webgui.container.Grid;
 import ch.nolix.systemapi.webguiapi.containerapi.IGrid;
 
@@ -12,5 +16,38 @@ final class GridTest extends ContainerTest<IGrid> {
   @Override
   protected IGrid createTestUnit() {
     return new Grid();
+  }
+
+  //method
+  @Test
+  void testCase_clear() {
+
+    //setup
+    final var control = new Label();
+    final var testUnit = new Grid();
+    testUnit.insertControlAtRowAndColumn(2, 3, control);
+
+    //execution
+    testUnit.clear();
+
+    //verification
+    expect(testUnit.isEmpty());
+  }
+
+  //method
+  @Test
+  void testCase_insertControlAtRowAndColumn_whenIsEmpty() {
+
+    //setup
+    final var control = new Label();
+    final var testUnit = new Grid();
+
+    //execution
+    testUnit.insertControlAtRowAndColumn(2, 3, control);
+
+    //verification
+    expect(testUnit.getRowCount()).isEqualTo(2);
+    expect(testUnit.getColumnCount()).isEqualTo(3);
+    expect(testUnit.getStoredChildControls()).containsExactly(control);
   }
 }
