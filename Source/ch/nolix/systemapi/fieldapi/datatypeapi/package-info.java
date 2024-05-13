b25fc26028bceaa4d589dc0748762f5a2904d954
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-13 22:07:39 +0200

    Delete datatypeapi package

--- Source/ch/nolix/systemapi/fieldapi/datatypeapi/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.systemapi.fieldapi.datatypeapi} package defines data
- * types.
- * 
- * @author Silvan Wyss
- * @date 2021-11-01
- */
-package ch.nolix.systemapi.fieldapi.datatypeapi;
