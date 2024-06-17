84e32abe373420a11d570304a40bf72e5b02e1e4
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-17 23:17:50 +0200

    Create webguiapitest package

---
+++ Test/ch/nolix/systemapitest/webguiapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapitest.webguiapitest} package defines tests for
+ * the {@link ch.nolix.systemapi.webguiapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-17
+ */
+package ch.nolix.systemapitest.webguiapitest;
