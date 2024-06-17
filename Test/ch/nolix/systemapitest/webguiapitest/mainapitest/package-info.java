6a4496d110970330334160c98bac34102923f02f
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-17 23:18:38 +0200

    Create mainapitest package

---
+++ Test/ch/nolix/systemapitest/webguiapitest/mainapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapitest.webguiapitest.mainapitest} package defines
+ * tests for the {@link ch.nolix.systemapi.webguiapi.mainapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-17
+ */
+package ch.nolix.systemapitest.webguiapitest.mainapitest;
