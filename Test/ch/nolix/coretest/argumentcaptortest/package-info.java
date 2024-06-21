0e8af0f9ee534cdd2d3c720164d3de8afe7f92d0
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-21 12:11:21 +0200

    Create argumentcaptortest package

---
+++ Test/ch/nolix/coretest/argumentcaptortest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coretest.argumentcaptortest} package defines tests for
+ * the {@link ch.nolix.core.argumentcaptor} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-21
+ */
+package ch.nolix.coretest.argumentcaptortest;
