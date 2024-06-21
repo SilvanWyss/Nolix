b9c2d072a6eafc20265ee89119fc87a03278711d
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-21 12:45:09 +0200

    Create performancetestapi package

---
+++ Source/ch/nolix/coreapi/testingapi/performancetestapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.testingapi.performancetestapi} package declares
+ * validations for performance tests.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-21
+ */
+package ch.nolix.coreapi.testingapi.performancetestapi;
