6df795b0565bc75d25a553df7dda3586571de517
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-21 14:07:56 +0200

    Create performancetest package

---
+++ Source/ch/nolix/core/testing/performancetest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.core.testing.performancetest} package defines the types
+ * that are declared in the
+ * {@link ch.nolix.coreapi.testingapi.performancetestapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-21
+ */
+package ch.nolix.core.testing.performancetest;
