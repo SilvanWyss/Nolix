05c2f4f2759c8db746724cd417dfcb90025b622d
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-21 14:30:44 +0200

    Create function package

---
+++ Source/ch/nolix/coreapi/mathapi/function/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.mathapi.function} package defines mathematical
+ * functions.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-21
+ */
+package ch.nolix.coreapi.mathapi.function;
