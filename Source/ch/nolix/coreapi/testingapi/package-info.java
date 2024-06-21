b4638c58204c64e4806815dc74572420de9d8400
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-21 12:44:20 +0200

    Create testingapi package

---
+++ Source/ch/nolix/coreapi/testingapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.testingapi} package declares validations for
+ * tests.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-21
+ */
+package ch.nolix.coreapi.testingapi;
