28a3f096c70a294d0fc195aa5322fcc260e5e035
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-21 14:31:37 +0200

    Create IntToDoubleFunctionCatalogue

---
+++ Source/ch/nolix/coreapi/mathapi/function/IntToDoubleFunctionCatalogue.java
@@ -0,0 +1,16 @@
+//package declaration
+package ch.nolix.coreapi.mathapi.function;
+
+//Java imports
+import java.util.function.IntToDoubleFunction;
+
+//class
+public final class IntToDoubleFunctionCatalogue {
+
+  //constant
+  public static final IntToDoubleFunction LINEAR_FUNCTION = v -> v;
+
+  //constructor
+  private IntToDoubleFunctionCatalogue() {
+  }
+}
