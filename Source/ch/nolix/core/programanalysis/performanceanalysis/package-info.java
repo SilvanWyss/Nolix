97d6b25f93fdeb52106f92d4ff355505533f4539
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-23 12:40:21 +0200

    Create performanceanalysis package

---
+++ Source/ch/nolix/core/programanalysis/performanceanalysis/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.core.programanalysis.performanceanalysis} package defines
+ * the types that are declared in the
+ * {@link ch.nolix.coreapi.programanalysisapi.performanceanalysisapi} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-23
+ */
+package ch.nolix.core.programanalysis.performanceanalysis;
