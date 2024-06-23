06de4bebbf03e4d695d0e14a25339e793def677f
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-23 12:30:52 +0200

    Create programanalysisapi package

---
+++ Source/ch/nolix/coreapi/programanalysisapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.programanalysisapi} package declares types for
+ * program analysis.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-06-23
+ */
+package ch.nolix.coreapi.programanalysisapi;
