0511b640fbfe25237fc73dd7b3c76da8b882f6ab
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-30 12:31:23 +0200

    Create AndNameCaptorTest

---
+++ Test/ch/nolix/coretest/argumentcaptortest/andargumentcaptortest/AndNameCaptorTest.java
@@ -0,0 +1,32 @@
+//package declaration
+package ch.nolix.coretest.argumentcaptortest.andargumentcaptortest;
+
+//JUnit imports
+import org.junit.jupiter.api.Test;
+
+//own imports
+import ch.nolix.core.argumentcaptor.andargumentcaptor.AndNameCaptor;
+import ch.nolix.core.testing.standardtest.StandardTest;
+
+//class
+final class AndNameCaptorTest extends StandardTest {
+
+  //method
+  @Test
+  void testCase_andName() {
+
+    //parameter definition
+    final var name = "my_name";
+
+    //setup
+    final var andNameCaptor = new AndNameCaptor<>();
+    final var testUnit = new AndNameCaptor<>(andNameCaptor);
+
+    //execution
+    final var result = testUnit.andName(name);
+
+    //verification
+    expect(testUnit.getName()).isEqualTo(name);
+    expect(result).is(andNameCaptor);
+  }
+}
