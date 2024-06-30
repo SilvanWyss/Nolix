a5f7b3907f525137ad48bed77a0f9bd1c29eb9b1
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-30 17:57:53 +0200

    Create functiontest package

---
+++ Test/ch/nolix/coreapitest/mathapitest/functiontest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapitest.mathapitest.functiontest} package defines
+ * tests for the {@link ch.nolix.coreapi.mathapi.function} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-06-30
+ */
+package ch.nolix.coreapitest.mathapitest.functiontest;
