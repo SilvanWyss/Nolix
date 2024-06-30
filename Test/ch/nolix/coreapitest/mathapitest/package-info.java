6f398eacd7a69cb01ba99f488df8535a9b81bb3d
Silvan Wyss <silvan.wyss@gmail.com>
2024-06-30 17:56:42 +0200

    Create mathapitest package

---
+++ Test/ch/nolix/coreapitest/mathapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapitest.mathapitest} package defines tests for the
+ * {@link ch.nolix.coreapi.mathapi} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-06-30
+ */
+package ch.nolix.coreapitest.mathapitest;
