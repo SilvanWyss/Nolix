7eb7e173ecb6a8200701878f5fd15576eb550196
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-06 22:08:27 +0200

    Extend BackgroundTest

--- Test/ch/nolix/systemtest/guitest/backgroundtest/BackgroundTest.java
+++ Test/ch/nolix/systemtest/guitest/backgroundtest/BackgroundTest.java
@@ -3,7 +3,11 @@ package ch.nolix.systemtest.guitest.backgroundtest;
 
 //JUnit imports
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+//own imports
+import ch.nolix.core.document.node.Node;
 import ch.nolix.core.testing.standardtest.StandardTest;
 import ch.nolix.system.graphic.color.Color;
 import ch.nolix.system.gui.background.Background;
@@ -11,6 +15,25 @@ import ch.nolix.systemapi.guiapi.backgroundapi.BackgroundType;
 
 //class
 final class BackgroundTest extends StandardTest {
+
+  //method
+  @ParameterizedTest
+  @ValueSource(strings = {
+  "Background(Color(0x010203))",
+  "Background(ColorGradient(HORIZONTAL,0x010000,0x020000))",
+  "Background(Transparency)"
+  })
+  void testCase_fromSpecification(final String backgroundSpecificationAsString) {
+
+    //setup
+    final var backgroundSpecification = Node.fromString(backgroundSpecificationAsString);
+
+    //execute
+    final var result = Background.fromSpecification(backgroundSpecification);
+
+    //verification
+    expect(result.getSpecification()).isEqualTo(backgroundSpecification);
+  }
 
   //method
   @Test
