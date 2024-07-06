5f557125228bf284960c080eb16cce9f51c711a3
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-06 18:22:43 +0200

    Create arraycontroltest package

---
+++ Test/ch/nolix/coretest/containertest/arraycontroltest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coretest.containertest.arraycontroltest} package defines
+ * tests for the {@link ch.nolix.core.container.arraycontrol} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-06
+ */
+package ch.nolix.coretest.containertest.arraycontroltest;
