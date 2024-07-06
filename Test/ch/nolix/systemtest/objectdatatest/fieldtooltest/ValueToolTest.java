0be533b0376c1e1405818eac68392e391ce2fe77
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-06 23:28:22 +0200

    Create ValueToolTest

---
+++ Test/ch/nolix/systemtest/objectdatatest/fieldtooltest/ValueToolTest.java
@@ -0,0 +1,85 @@
+//package declaration
+package ch.nolix.systemtest.objectdatatest.fieldtooltest;
+
+//JUnit imports
+import org.junit.jupiter.api.Test;
+
+//own imports
+import ch.nolix.core.testing.standardtest.StandardTest;
+import ch.nolix.system.objectdata.data.Entity;
+import ch.nolix.system.objectdata.data.Value;
+import ch.nolix.system.objectdata.dataadapter.NodeDataAdapter;
+import ch.nolix.system.objectdata.fieldtool.ValueTool;
+import ch.nolix.system.objectdata.schema.Schema;
+import ch.nolix.systemapi.objectdataapi.dataapi.IValue;
+
+//class
+final class ValueToolTest extends StandardTest {
+
+  //constant
+  private static class Pet extends Entity {
+
+    //attribute
+    private final IValue<String> name = Value.withValueType(String.class);
+
+    //constructor
+    public Pet() {
+      initialize();
+    }
+  }
+
+  //method
+  @Test
+  void testCase_canSetValue() {
+
+    //setup
+    final var pet = new Pet();
+    final String valueToSet = "Garfield";
+    final var testUnit = new ValueTool();
+
+    //execution
+    final var result = testUnit.canSetValue(pet.name, valueToSet);
+
+    //verification
+    expect(result);
+  }
+
+  //method
+  @Test
+  void testCase_canSetValue_whenTheGivenValueIsClosed() {
+
+    //setup
+    final var pet = new Pet();
+    final var databaseAdapter = NodeDataAdapter.forTemporaryInMemoryDatabase().withName("my_database")
+      .andSchema(Schema.withEntityType(Pet.class));
+    databaseAdapter.insertEntity(pet);
+    databaseAdapter.close();
+    final String valueToSet = "Garfield";
+    final var testUnit = new ValueTool();
+
+    //setup verification
+    expect(pet.name.isClosed());
+
+    //execution
+    final var result = testUnit.canSetValue(pet.name, valueToSet);
+
+    //verification
+    expectNot(result);
+  }
+
+  //method
+  @Test
+  void testCase_canSetValue_whenTheGivenValueToSetIsNull() {
+
+    //setup
+    final var pet = new Pet();
+    final String valueToSet = null;
+    final var testUnit = new ValueTool();
+
+    //execution
+    final var result = testUnit.canSetValue(pet.name, valueToSet);
+
+    //verification
+    expectNot(result);
+  }
+}
