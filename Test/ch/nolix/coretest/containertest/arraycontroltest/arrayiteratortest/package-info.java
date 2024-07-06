84b3caf1381eb17796d8f8edcef918819a8047b6
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-06 19:51:28 +0200

    Create GetCopyMethodTest

---
+++ Test/ch/nolix/coretest/containertest/arraycontroltest/arrayiteratortest/package-info.java
@@ -0,0 +1,11 @@
+//package info
+/**
+ * The
+ * {@link ch.nolix.coretest.containertest.arraycontroltest.arrayiteratortest}
+ * package defines tests for the
+ * {@link ch.nolix.core.container.arraycontrol.ArrayIterator}.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-06
+ */
+package ch.nolix.coretest.containertest.arraycontroltest.arrayiteratortest;
