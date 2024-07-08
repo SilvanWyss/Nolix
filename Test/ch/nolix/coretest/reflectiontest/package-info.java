f2ef9295e3aaf7381a296188ed906ff2b32cba3d
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-08 21:47:38 +0200

    Create reflectiontest package

---
+++ Test/ch/nolix/coretest/reflectiontest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coretest.reflectiontest} package defines tests for the
+ * {@link ch.nolix.core.reflection} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-08
+ */
+package ch.nolix.coretest.reflectiontest;
