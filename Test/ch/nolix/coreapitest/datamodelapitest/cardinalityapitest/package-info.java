5284ea4d3b594e22be814a9fbc259b290ddeecb5
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-10 21:23:27 +0200

    Create cardinalityapitest package

---
+++ Test/ch/nolix/coreapitest/datamodelapitest/cardinalityapitest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapitest.datamodelapitest.cardinalityapitest} package
+ * defines tests for the {@link ch.nolix.coreapi.datamodelapi.cardinalityapi}
+ * package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-10
+ */
+package ch.nolix.coreapitest.datamodelapitest.cardinalityapitest;
