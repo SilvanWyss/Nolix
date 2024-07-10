f75033f316c7a408871d2a10c293178c94cf316c
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-10 22:00:18 +0200

    Create elementtest package

---
+++ Test/ch/nolix/systemtest/elementtest/basetest/elementtest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemtest.elementtest.basetest.elementtest} package
+ * defines tests for the {@link ch.nolix.system.element.base.Element}.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-10
+ */
+package ch.nolix.systemtest.elementtest.basetest.elementtest;
