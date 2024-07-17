f653d74f64d936c96e51fb993bd8ec122cad78c7
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-17 22:43:13 +0200

    Create iterabletooltest package

--- Test/ch/nolix/coretest/commontypetooltest/iterabletooltest/GlobalIterableToolTest.java
+++ Test/ch/nolix/coretest/commontypetooltest/iterabletooltest/GlobalIterableToolTest.java
@@ -1,5 +1,5 @@
 //package declaration
-package ch.nolix.coretest.commontypetooltest;
+package ch.nolix.coretest.commontypetooltest.iterabletooltest;
 
 //JUnit imports
 import org.junit.jupiter.api.Test;
---
+++ Test/ch/nolix/coretest/commontypetooltest/iterabletooltest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.coretest.commontypetooltest.iterabletooltest} package
+ * defines tests for the {@link ch.nolix.core.commontypetool.iterabletool}
+ * package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-17
+ */
+package ch.nolix.coretest.commontypetooltest.iterabletooltest;
