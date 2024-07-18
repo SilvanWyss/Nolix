aa9ebdaea2f50199d8cb3e3081ba036757b0fa5b
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-18 22:57:25 +0200

    Delete arraycontrol package

--- Source/ch/nolix/core/container/arraycontrol/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.container.arraycontrol} package defines types to
- * control arrays.
- * 
- * @author Silvan Wyss
- * @version 2023-01-02
- */
-package ch.nolix.core.container.arraycontrol;
