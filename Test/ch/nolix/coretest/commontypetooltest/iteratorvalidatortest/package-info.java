fae0e5495674bac07a400c11c55cf474f915e63f
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-19 21:18:39 +0200

    Create iteratorvalidatortest package

---
+++ Test/ch/nolix/coretest/commontypetooltest/iteratorvalidatortest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.coretest.commontypetooltest.iteratorvalidatortest}
+ * package defines tests for the
+ * {@link ch.nolix.core.commontypetool.iteratorvalidator} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-24
+ */
+package ch.nolix.coretest.commontypetooltest.iteratorvalidatortest;
