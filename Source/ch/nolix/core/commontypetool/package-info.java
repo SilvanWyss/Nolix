8a866ec1b13c835bdb2b9732112f93b51f9d060f
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-19 20:46:43 +0200

    Create commontypetoolapi package

--- Source/ch/nolix/core/commontypetool/package-info.java
+++ Source/ch/nolix/core/commontypetool/package-info.java
@@ -1,7 +1,7 @@
 //package info
 /**
- * The {@link ch.nolix.core.commontypetool} package defines tools for common
- * types.
+ * The {@link ch.nolix.core.commontypetool} package defines the types that are
+ * declared in the {@link ch.nolix.coreapi.commontypetoolapi} package.
  * 
  * @author Silvan Wyss
  * @version 2021-03-09
---
+++ Source/ch/nolix/coreapi/commontypetoolapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.core.commontypetool} package declares tools for common
+ * types.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-19
+ */
+package ch.nolix.coreapi.commontypetoolapi;
