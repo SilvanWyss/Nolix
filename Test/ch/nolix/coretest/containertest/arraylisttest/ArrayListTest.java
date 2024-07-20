bffc60d1eee562c9c4cafc15c646286dcbc0d800
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-20 23:33:41 +0200

    Extend ArrayListTest

--- Test/ch/nolix/coretest/containertest/arraylisttest/ArrayListTest.java
+++ Test/ch/nolix/coretest/containertest/arraylisttest/ArrayListTest.java
@@ -1,8 +1,13 @@
 //package declaration
 package ch.nolix.coretest.containertest.arraylisttest;
 
+//JUnit imports
+import org.junit.jupiter.api.Test;
+
 //own imports
 import ch.nolix.core.container.arraylist.ArrayList;
+import ch.nolix.core.container.linkedlist.LinkedList;
+import ch.nolix.core.errorcontrol.invalidargumentexception.NegativeArgumentException;
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.coretest.containertest.basetest.ContainerTest;
 
@@ -21,5 +26,55 @@ final class ArrayListTest extends ContainerTest {
   @Override
   protected <E> IContainer<E> createEmptyContainerForType(final Class<E> type) {
     return ArrayList.createEmpty();
+  }
+
+  //method
+  @Test
+  void testCase_clear_whenIsEmpty() {
+
+    //setup
+    final ArrayList<String> testUnit = ArrayList.createEmpty();
+
+    //execution
+    testUnit.clear();
+
+    //verification
+    expect(testUnit).isEmpty();
+  }
+
+  //method
+  @Test
+  void testCase_clear_whenContainsAny() {
+
+    //setup
+    final var testUnit = LinkedList.withElement("antelope", "elephant", "lion", "monkey", "rhino", "zebra");
+
+    //execution
+    testUnit.clear();
+
+    //verification
+    expect(testUnit).isEmpty();
+  }
+
+  //method
+  @Test
+  void testCase_withInitialCapacity() {
+
+    //execution
+    final var result = ArrayList.withInitialCapacity(10);
+
+    //verification
+    expect(result).isEmpty();
+  }
+
+  //method
+  @Test
+  void testCase_withInitialCapacity_whenTheGivenInitialCapacityIsNegative() {
+
+    //execution & verification
+    expectRunning(() -> ArrayList.withInitialCapacity(-1))
+      .throwsException()
+      .ofType(NegativeArgumentException.class)
+      .withMessage("The given initial capacity '-1' is negative.");
   }
 }
