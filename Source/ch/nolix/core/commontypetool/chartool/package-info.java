4cf2acafaa2cf06d637a88463275d43b419a80ad
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-21 22:29:33 +0200

    Create chartoolapi package

--- Source/ch/nolix/core/commontypetool/chartool/package-info.java
+++ Source/ch/nolix/core/commontypetool/chartool/package-info.java
@@ -1,9 +1,10 @@
 //package info
 /**
- * The {@link ch.nolix.core.commontypetool.chartool} package defines tools for
- * chars.
+ * The {@link ch.nolix.core.commontypetool.chartool} package defines the types
+ * that are declared in the
+ * {@link ch.nolix.coreapi.commontypetoolapi.chartoolapi} package.
  * 
  * @author Silvan Wyss
- * @version 2024-07-17
+ * @version 2024-07-21
  */
 package ch.nolix.core.commontypetool.chartool;
---
+++ Source/ch/nolix/coreapi/commontypetoolapi/chartoolapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.commontypetoolapi.chartoolapi} package declares
+ * tools for chars.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-21
+ */
+package ch.nolix.coreapi.commontypetoolapi.chartoolapi;
