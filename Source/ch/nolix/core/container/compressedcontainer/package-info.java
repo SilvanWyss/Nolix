a5874ac8c8b5ff698323dfa5db64bea1f489d827
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-21 15:07:05 +0200

    Delete CompressedList

--- Source/ch/nolix/core/container/compressedcontainer/package-info.java
+++
@@ -1,9 +0,0 @@
-//package info
-/**
- * The {@link ch.nolix.core.container.compressedcontainer} package defines
- * compressed {@link ch.nolix.core.container.base.Container}s.
- * 
- * @author Silvan Wyss
- * @version 2022-04-30
- */
-package ch.nolix.core.container.compressedcontainer;
