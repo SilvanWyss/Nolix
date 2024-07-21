5dca9b1bf9efd56e4743118af0f9af46c7bf3a88
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-21 17:03:53 +0200

    Create arraytoolapi package

--- Source/ch/nolix/core/commontypetool/arraytool/package-info.java
+++ Source/ch/nolix/core/commontypetool/arraytool/package-info.java
@@ -1,7 +1,8 @@
 //package info
 /**
- * The {@link ch.nolix.core.commontypetool.arraytool} package defines tools for
- * arrays.
+ * The {@link ch.nolix.core.commontypetool.arraytool} package defines the types
+ * that are declared in the
+ * {@link ch.nolix.coreapi.commontypetoolapi.arraytoolapi} package.
  * 
  * @author Silvan Wyss
  * @version 2024-07-17
---
+++ Source/ch/nolix/coreapi/commontypetoolapi/arraytoolapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.commontypetoolapi.arraytoolapi} package declares
+ * tools for arrays.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-21
+ */
+package ch.nolix.coreapi.commontypetoolapi.arraytoolapi;
