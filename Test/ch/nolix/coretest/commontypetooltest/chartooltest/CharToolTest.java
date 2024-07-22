8ff2ca4a23e955c5fd7ee3e4acc4c6120a46ce48
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-22 18:05:03 +0200

    Refactor CharToolTest

--- Test/ch/nolix/coretest/commontypetooltest/chartooltest/CharToolTest.java
+++ Test/ch/nolix/coretest/commontypetooltest/chartooltest/CharToolTest.java
@@ -2,7 +2,8 @@
 package ch.nolix.coretest.commontypetooltest.chartooltest;
 
 //JUnit imports
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 //own imports
 import ch.nolix.core.commontypetool.chartool.CharTool;
@@ -12,80 +13,62 @@ import ch.nolix.core.testing.standardtest.StandardTest;
 final class CharToolTest extends StandardTest {
 
   //method
-  @Test
-  void testCase_isDigit_whenGivenCharacterIsADigit() {
+  @ParameterizedTest
+  @ValueSource(chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' })
+  void testCase_isDigit_whenTheGivenCharacterIsADigit(final char character) {
 
+    //setup   
     final var testUnit = new CharTool();
 
-    expect(testUnit.isDigit('0'));
-    expect(testUnit.isDigit('1'));
-    expect(testUnit.isDigit('2'));
-    expect(testUnit.isDigit('3'));
-    expect(testUnit.isDigit('4'));
-    expect(testUnit.isDigit('5'));
-    expect(testUnit.isDigit('6'));
-    expect(testUnit.isDigit('7'));
-    expect(testUnit.isDigit('8'));
-    expect(testUnit.isDigit('9'));
+    //execution
+    final var result = testUnit.isDigit(character);
+
+    //verification
+    expect(result);
   }
 
   //method
-  @Test
-  void testCase_isDigit_whenGivenCharacterIsNotADigit() {
+  @ParameterizedTest
+  @ValueSource(chars = { 'A', 'a', '%', '&', '.', ',', '(', ')' })
+  void testCase_isDigit_whenTheGivenCharacterIsNotADigit(final char character) {
 
+    //setup   
     final var testUnit = new CharTool();
 
-    expectNot(testUnit.isDigit('A'));
-    expectNot(testUnit.isDigit('a'));
-    expectNot(testUnit.isDigit('Z'));
-    expectNot(testUnit.isDigit('z'));
-    expectNot(testUnit.isDigit('%'));
-    expectNot(testUnit.isDigit('&'));
-    expectNot(testUnit.isDigit('.'));
-    expectNot(testUnit.isDigit(','));
-    expectNot(testUnit.isDigit('('));
-    expectNot(testUnit.isDigit(')'));
+    //execution
+    final var result = testUnit.isDigit(character);
+
+    //verification
+    expectNot(result);
   }
 
   //method
-  @Test
-  void testCase_isHexadecimalDigit_whenGivenCharacterIsAHexadecimalDigit() {
+  @ParameterizedTest
+  @ValueSource(chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' })
+  void testCase_isHexadecimalDigit_whenGivenCharacterIsAHexadecimalDigit(final char character) {
 
+    //setup   
     final var testUnit = new CharTool();
 
-    expect(testUnit.isHexadecimalDigit('0'));
-    expect(testUnit.isHexadecimalDigit('1'));
-    expect(testUnit.isHexadecimalDigit('2'));
-    expect(testUnit.isHexadecimalDigit('3'));
-    expect(testUnit.isHexadecimalDigit('4'));
-    expect(testUnit.isHexadecimalDigit('5'));
-    expect(testUnit.isHexadecimalDigit('6'));
-    expect(testUnit.isHexadecimalDigit('7'));
-    expect(testUnit.isHexadecimalDigit('8'));
-    expect(testUnit.isHexadecimalDigit('9'));
-    expect(testUnit.isHexadecimalDigit('A'));
-    expect(testUnit.isHexadecimalDigit('B'));
-    expect(testUnit.isHexadecimalDigit('C'));
-    expect(testUnit.isHexadecimalDigit('D'));
-    expect(testUnit.isHexadecimalDigit('E'));
-    expect(testUnit.isHexadecimalDigit('F'));
+    //execution
+    final var result = testUnit.isHexadecimalDigit(character);
+
+    //verification
+    expect(result);
   }
 
   //method
-  @Test
-  void testCase_isHexadecimalDigit_whenGivenCharacterIsNotAHexadecimalDigit() {
+  @ParameterizedTest
+  @ValueSource(chars = { 'G', 'g', '%', '&', '.', ',', '(', ')' })
+  void testCase_isHexadecimalDigit_whenTheGivenCharacterIsNotAHexadecimalDigit(final char character) {
 
+    //setup   
     final var testUnit = new CharTool();
 
-    expectNot(testUnit.isHexadecimalDigit('G'));
-    expectNot(testUnit.isHexadecimalDigit('g'));
-    expectNot(testUnit.isHexadecimalDigit('Z'));
-    expectNot(testUnit.isHexadecimalDigit('z'));
-    expectNot(testUnit.isHexadecimalDigit('%'));
-    expectNot(testUnit.isHexadecimalDigit('&'));
-    expectNot(testUnit.isHexadecimalDigit('.'));
-    expectNot(testUnit.isHexadecimalDigit(','));
-    expectNot(testUnit.isHexadecimalDigit('('));
-    expectNot(testUnit.isHexadecimalDigit(')'));
+    //execution
+    final var result = testUnit.isHexadecimalDigit(character);
+
+    //verification
+    expectNot(result);
   }
 }
