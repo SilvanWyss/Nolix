1f63752d1a15419ab74910179b76df813bd01bb8
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-22 19:15:12 +0200

    Create IDoubleTool

---
+++ Source/ch/nolix/coreapi/commontypetoolapi/doubletoolapi/IDoubleTool.java
@@ -0,0 +1,25 @@
+//package declaration
+package ch.nolix.coreapi.commontypetoolapi.doubletoolapi;
+
+//interface
+/**
+ * A {@link IDoubleTool} provides methods to handle doubles.
+ * 
+ * @author Silvan Wyss
+ * @version 2022-07-22
+ */
+public interface IDoubleTool {
+
+  //method declaration
+  /**
+   * @param value
+   * @return a {@link String} representation of the given value. The
+   *         {@link String} representation will have the following properties:
+   * 
+   *         -The separator symbol of the decimal places is a dot.
+   * 
+   *         -The decimal places and the separator symbol are shown only when
+   *         there exist decimal places.
+   */
+  String toString(double value);
+}
