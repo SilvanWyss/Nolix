f7bb9db083fdaaa3e5f2b317651c45577fa6da82
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-22 19:11:59 +0200

    Create doubletoolapi package

--- Source/ch/nolix/core/commontypetool/doubletool/package-info.java
+++ Source/ch/nolix/core/commontypetool/doubletool/package-info.java
@@ -1,7 +1,8 @@
 //package info
 /**
- * The {@link ch.nolix.core.commontypetool.doubletool} package defines tools for
- * doubles.
+ * The {@link ch.nolix.core.commontypetool.doubletool} package defines the types
+ * that are declared in the
+ * {@link ch.nolix.coreapi.commontypetoolapi.doubletoolapi} package.
  * 
  * @author Silvan Wyss
  * @version 2024-07-17
---
+++ Source/ch/nolix/coreapi/commontypetoolapi/doubletoolapi/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coreapi.commontypetoolapi.doubletoolapi} package declares
+ * tools for doubles.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-22
+ */
+package ch.nolix.coreapi.commontypetoolapi.doubletoolapi;
