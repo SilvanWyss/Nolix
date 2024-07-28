574fee7a3e48084b79faeea1f83e3ff1ba7a64b5
Silvan Wyss <silvan.wyss@gmail.com>
2024-07-28 15:26:10 +0200

    Create iterabletooltest package

---
+++ Test/ch/nolix/coretest/commontypetooltest/iteratortooltest/iterabletooltest/package-info.java
@@ -0,0 +1,11 @@
+//package info
+/**
+ * The
+ * {@link ch.nolix.coretest.commontypetooltest.iteratortooltest.iterabletooltest}
+ * package defines tests for
+ * {@link ch.nolix.core.commontypetool.iteratortool.IterableTool}.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-07-28
+ */
+package ch.nolix.coretest.commontypetooltest.iteratortooltest.iterabletooltest;
