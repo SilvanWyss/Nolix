cd7e452e653c7885cb0ae8889442bc05701efe26
Silvan Wyss <silvan.wyss@gmail.com>
2024-08-02 00:05:52 +0200

    Extend ControlCssBuilderTest

--- Test/ch/nolix/systemtest/webguitest/basecontroltooltest/ControlCssBuilderTest.java
+++ Test/ch/nolix/systemtest/webguitest/basecontroltooltest/ControlCssBuilderTest.java
@@ -4,17 +4,44 @@ package ch.nolix.systemtest.webguitest.basecontroltooltest;
 //JUnit imports
 import org.junit.jupiter.api.Test;
 
+//own imports
 import ch.nolix.core.testing.standardtest.StandardTest;
+import ch.nolix.system.graphic.color.Color;
 import ch.nolix.systemapi.webguiapi.controltoolapi.IControlCssBuilder;
+import ch.nolix.systemapi.webguiapi.mainapi.ControlState;
 import ch.nolix.systemapi.webguiapi.mainapi.IControl;
 
 //class
 public abstract class ControlCssBuilderTest<CCB extends IControlCssBuilder<C, ?>, C extends IControl<C, ?>>
 extends StandardTest {
 
+  //method declaration
+  protected abstract C createControl();
+
+  //method declaration
+  protected abstract CCB createTestUnit();
+
   //method
   @Test
-  final void testCase_whenGivenControlIsNew() {
+  final void testCase_createCssRulesForControl_whenGivenControlHasAHoverBackgroundColor() {
+  
+    //setup
+    final var control = createControl();
+    control.editStyle(s -> s.setBackgroundColorForState(ControlState.HOVER, Color.fromString("0x102030")));
+    final var testUnit = createTestUnit();
+  
+    //execution
+    final var result = testUnit.createCssRulesForControl(control);
+  
+    //verification
+    final var controlInternalId = control.getInternalId();
+    final var hoverCssRule = result.getStoredOne(r -> r.getSelector().startsWith("#" + controlInternalId + ":hover"));
+    expect(hoverCssRule.getProperties()).containsExactlyOneWithStringRepresentation("background: #102030;");
+  }
+
+  //method
+  @Test
+  final void testCase_createCssRulesForControl_whenGivenControlIsNew() {
 
     //setup
     final var control = createControl();
@@ -35,10 +62,4 @@ extends StandardTest {
     //verification part 3
     expect(result.containsAny(r -> r.getSelector().startsWith("#" + controlInternalId + ":focus")));
   }
-
-  //method declaration
-  protected abstract C createControl();
-
-  //method declaration
-  protected abstract CCB createTestUnit();
 }
