414a28f09076983634c363544bcf39d0a7751e7b
Silvan Wyss <silvan.wyss@gmail.com>
2024-08-24 22:57:27 +0200

    Create basecontainerapitest package

---
+++ Test/ch/nolix/systemapitest/webguiapitest/basecontainerapitest/package-info.java
@@ -0,0 +1,10 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapitest.webguiapitest.basecontainerapitest} package
+ * defines tests for the {@link ch.nolix.systemapi.webguiapi.basecontainerapi}
+ * package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-08-24
+ */
+package ch.nolix.systemapitest.webguiapitest.basecontainerapitest;
