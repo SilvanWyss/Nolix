73f6962b13711483b82b6e6bf57a7251b3ded06d
Silvan Wyss <silvan.wyss@gmail.com>
2024-08-25 12:38:24 +0200

    Create guiapitest package

---
+++ Test/ch/nolix/systemapitest/guiapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapitest.guiapitest} package defines tests for the
+ * {@link ch.nolix.systemapi.guiapi} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-08-25
+ */
+package ch.nolix.systemapitest.guiapitest;
