77642aea1b753e7a52ea3c6c5360e346517c9c20
Silvan Wyss <silvan.wyss@gmail.com>
2024-08-27 22:26:01 +0200

    Create exceptiontest package

---
+++ Test/ch/nolix/coretest/errorcontroltest/exceptiontest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.coretest.errorcontroltest.exceptiontest} package defines
+ * tests for the {@link ch.nolix.core.errorcontrol.exception} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-08-27
+ */
+package ch.nolix.coretest.errorcontroltest.exceptiontest;
