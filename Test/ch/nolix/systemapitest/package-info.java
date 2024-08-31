38305564efce96d77c1d08cc5d0990fec7d15d2a
Silvan Wyss <silvan.wyss@gmail.com>
2024-08-31 17:41:30 +0200

    Add missing package infos

---
+++ Test/ch/nolix/systemapitest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.systemapitest} package defines tests for the
+ * {@link ch.nolix.systemapi} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-02-16
+ */
+package ch.nolix.systemapitest;
---
+++ Test/ch/nolix/techtest/package-info.java
@@ -0,0 +1,9 @@
+//package info
+/**
+ * The {@link ch.nolix.techtest} package defines tests for the
+ * {@link ch.nolix.tech} package.
+ * 
+ * @author Silvan Wyss
+ * @version 2022-04-03
+ */
+package ch.nolix.techtest;
