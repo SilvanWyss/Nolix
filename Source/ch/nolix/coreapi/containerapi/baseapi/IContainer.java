12acd8cf6e3310608d9a2a0b58dac2ef29c35ab6
Silvan Wyss <silvan.wyss@gmail.com>
2024-11-14 23:00:07 +0100

    Create StringMappable

--- Source/ch/nolix/coreapi/containerapi/baseapi/IContainer.java
+++ Source/ch/nolix/coreapi/containerapi/baseapi/IContainer.java
@@ -8,13 +8,14 @@ import ch.nolix.coreapi.containerapi.commoncontainerapi.IndexRequestable;
 import ch.nolix.coreapi.containerapi.commoncontainerapi.IterableWithCopyableIterator;
 import ch.nolix.coreapi.containerapi.commoncontainerapi.SingleSearchable;
 import ch.nolix.coreapi.containerapi.commoncontainerapi.StoringRequestable;
+import ch.nolix.coreapi.containerapi.commoncontainerapi.StringMappable;
 import ch.nolix.coreapi.stateapi.staterequestapi.EmptinessRequestable;
 import ch.nolix.coreapi.stateapi.staterequestapi.MaterializationRequestable;
 
 /**
  * A {@link IContainer} can store several elements of a certain type. A
  * {@link IContainer} stores its element in a linear order. There can exists
- * additionally other orders. A {@link IContainer} is iterable.
+ * additional orders. A {@link IContainer} is iterable.
  * 
  * @author Silvan Wyss
  * @version 2016-01-01
@@ -32,7 +33,8 @@ Mappable<E>,
 MaterializationRequestable,
 MultiSearchable<E>,
 SingleSearchable<E>,
-StoringRequestable<E> {
+StoringRequestable<E>,
+StringMappable {
 
   /**
    * @return a new array with the elements of the current {@link IContainer}.
@@ -40,38 +42,19 @@ StoringRequestable<E> {
   Object[] toArray();
 
   /**
-   * @return a concatenation of the {@link String} representations of the elements
-   *         of the current {@link IContainer}.
-   */
-  String toConcatenatedString();
-
-  /**
-   * @param norm
-   * @param <C>  is the type of the {@link Comparable}s the given norm returns.
+   * @param comparableMapper
+   * @param <C>              is the type of the {@link Comparable}s the given
+   *                         comparableMapper returns.
    * @return a new {@link IContainer} with the elements of the current
    *         {@link IContainer} ordered from the smallest to the biggest element
-   *         according to the given norm.
+   *         according to the {@link Comparable}s the given comparableMapper maps
+   *         from the elements of the current {@link IContainer}.
    */
-  <C extends Comparable<C>> IContainer<E> toOrderedList(Function<E, C> norm);
+  <C extends Comparable<C>> IContainer<E> toOrderedList(Function<E, C> comparableMapper);
 
   /**
    * @return a new {@link IContainer} with the elements of the current
    *         {@link IContainer} in reversed order.
    */
   IContainer<E> toReversedList();
-
-  /**
-   * @param separator
-   * @return a {@link String} representation of the current {@link IContainer}
-   *         with the given separator.
-   */
-  String toStringWithSeparator(char separator);
-
-  /**
-   * @param separator
-   * @return a {@link String} representation of the current {@link IContainer}
-   *         with the given separator.
-   * @throws RuntimeException if the given separator is null.
-   */
-  String toStringWithSeparator(String separator);
 }
