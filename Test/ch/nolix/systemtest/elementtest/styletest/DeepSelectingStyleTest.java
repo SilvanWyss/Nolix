398c890db67e760bec683972e9081d84bdfc4216
Silvan Wyss <silvan.wyss@gmail.com>
2024-11-24 13:17:31 +0100

    Create DeepSelectingStyleTest

--- Test/ch/nolix/systemtest/elementtest/styletest/DeepSelectingStyleTest.java
+++ Test/ch/nolix/systemtest/elementtest/styletest/DeepSelectingStyleTest.java
@@ -4,8 +4,45 @@ import org.junit.jupiter.api.Test;
 
 import ch.nolix.core.testing.standardtest.StandardTest;
 import ch.nolix.system.element.style.DeepSelectingStyle;
+import ch.nolix.systemapi.webguiapi.atomiccontrolapi.LabelRole;
 
 final class DeepSelectingStyleTest extends StandardTest {
+
+  @Test
+  void testCase_getSpecification_whenHasSelectorIdAndContainsAttachingeAttributes() {
+
+    //setup
+    final var testUnit = //
+    new DeepSelectingStyle()
+      .withSelectorId("selector_id")
+      .withAttachingAttribute("BaseBorderThickness(1)", "BaseBackgroundColor(Anthrazit)");
+
+    //execution
+    final var specification = testUnit.getSpecification();
+
+    //verification
+    final var expectedStringRepresentation = //
+    "DeepSelectingStyle(SelectorId(selector_id),AttachingAttribute(BaseBorderThickness(1)),AttachingAttribute(BaseBackgroundColor(Anthrazit)))";
+    expect(specification).hasStringRepresentation(expectedStringRepresentation);
+  }
+
+  @Test
+  void testCase_getSpecification_whenHasSelectorRoleAndContainsAttachingeAttributes() {
+
+    //setup
+    final var testUnit = //
+    new DeepSelectingStyle()
+      .withSelectorRole(LabelRole.TITLE)
+      .withAttachingAttribute("BaseTextSize(50)", "BaseTextColor(Black)");
+
+    //execution
+    final var specification = testUnit.getSpecification();
+
+    //verification
+    final var expectedStringRepresentation = //
+    "DeepSelectingStyle(SelectorRole(TITLE),AttachingAttribute(BaseTextSize(50)),AttachingAttribute(BaseTextColor(Black)))";
+    expect(specification).hasStringRepresentation(expectedStringRepresentation);
+  }
 
   @Test
   void testCase_selectsChildElements() {
