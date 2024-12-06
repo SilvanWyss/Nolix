78e891d68ac20a0b11942f82e02a579702f92458
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-06 19:26:30 +0100

    Create AttachingAttribute

---
+++ Source/ch/nolix/system/element/style/AttachingAttribute.java
@@ -0,0 +1,68 @@
+package ch.nolix.system.element.style;
+
+import ch.nolix.core.document.node.Node;
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.coreapi.documentapi.nodeapi.INode;
+import ch.nolix.coreapi.programatomapi.variableapi.LowerCaseVariableCatalogue;
+import ch.nolix.systemapi.elementapi.styleapi.IAttachingAttribute;
+import ch.nolix.systemapi.elementapi.styleapi.IAttachingAttributeValidator;
+
+public final class AttachingAttribute implements IAttachingAttribute {
+
+  private static final IAttachingAttributeValidator ATTACHING_ATTRIBUTE_VALIDATOR = new AttachingAttributeValidator();
+
+  private final Enum<?> optionalTag;
+
+  private final Node value;
+
+  private AttachingAttribute(final Enum<?> tag, final INode<?> value) {
+
+    GlobalValidator.assertThat(tag).thatIsNamed(LowerCaseVariableCatalogue.TAG).isNotNull();
+    GlobalValidator.assertThat(value).thatIsNamed(LowerCaseVariableCatalogue.VALUE).isNotNull();
+
+    this.optionalTag = tag;
+    this.value = Node.fromNode(value);
+  }
+
+  private AttachingAttribute(final INode<?> value) {
+
+    GlobalValidator.assertThat(value).thatIsNamed(LowerCaseVariableCatalogue.VALUE).isNotNull();
+
+    this.optionalTag = null;
+    this.value = Node.fromNode(value);
+  }
+
+  public static AttachingAttribute withTagAndValue(final Enum<?> tag, final INode<?> value) {
+    return new AttachingAttribute(tag, value);
+  }
+
+  public static AttachingAttribute withValue(final INode<?> value) {
+    return new AttachingAttribute(value);
+  }
+
+  @Override
+  public Enum<?> getTag() {
+
+    ATTACHING_ATTRIBUTE_VALIDATOR.assertHasTag(this);
+
+    return optionalTag;
+  }
+
+  @Override
+  public Node getValue() {
+    return value;
+  }
+
+  @Override
+  public boolean hasTag() {
+    return (optionalTag != null);
+  }
+
+  //For a better performance, this implementation does not use all comfortable methods.
+  @Override
+  public boolean hasTag(final Enum<?> tag) {
+    return //
+    optionalTag != null
+    && optionalTag == tag;
+  }
+}
--- Source/ch/nolix/systemapi/elementapi/styleapi/IAttachingAttribute.java
+++ Source/ch/nolix/systemapi/elementapi/styleapi/IAttachingAttribute.java
@@ -1,8 +1,9 @@
 package ch.nolix.systemapi.elementapi.styleapi;
 
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IValueHolder;
+import ch.nolix.coreapi.documentapi.nodeapi.INode;
 
-public interface IAttachingAttribute extends IValueHolder<String> {
+public interface IAttachingAttribute extends IValueHolder<INode<?>> {
 
   Enum<?> getTag();
 
