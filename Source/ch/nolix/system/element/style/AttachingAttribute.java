5acd4a7ae2042685349e8575582353e40ae44c46
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-06 20:14:41 +0100

    Make IAttachingAttribute a IElement

--- Source/ch/nolix/system/element/style/AttachingAttribute.java
+++ Source/ch/nolix/system/element/style/AttachingAttribute.java
@@ -1,13 +1,18 @@
 package ch.nolix.system.element.style;
 
+import ch.nolix.core.container.linkedlist.LinkedList;
 import ch.nolix.core.document.node.Node;
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.containerapi.listapi.ILinkedList;
 import ch.nolix.coreapi.documentapi.nodeapi.INode;
 import ch.nolix.coreapi.programatomapi.variableapi.LowerCaseVariableCatalogue;
+import ch.nolix.coreapi.programatomapi.variableapi.PascalCaseVariableCatalogue;
+import ch.nolix.system.element.base.Element;
 import ch.nolix.systemapi.elementapi.styleapi.IAttachingAttribute;
 import ch.nolix.systemapi.elementapi.styleapi.IAttachingAttributeValidator;
 
-public final class AttachingAttribute implements IAttachingAttribute {
+public final class AttachingAttribute extends Element implements IAttachingAttribute {
 
   private static final IAttachingAttributeValidator ATTACHING_ATTRIBUTE_VALIDATOR = new AttachingAttributeValidator();
 
@@ -38,6 +43,21 @@ public final class AttachingAttribute implements IAttachingAttribute {
 
   public static AttachingAttribute withValue(final INode<?> value) {
     return new AttachingAttribute(value);
+  }
+
+  //For a better performance, this implementation does not use all comfortable methods.
+  @Override
+  public IContainer<INode<?>> getAttributes() {
+
+    final ILinkedList<INode<?>> attributes = LinkedList.createEmpty();
+
+    if (optionalTag != null) {
+      attributes.addAtEnd(Node.withHeaderAndChildNode(PascalCaseVariableCatalogue.TAG, optionalTag.toString()));
+    }
+
+    attributes.addAtEnd(value);
+
+    return attributes;
   }
 
   @Override
--- Source/ch/nolix/systemapi/elementapi/styleapi/IAttachingAttribute.java
+++ Source/ch/nolix/systemapi/elementapi/styleapi/IAttachingAttribute.java
@@ -2,8 +2,9 @@ package ch.nolix.systemapi.elementapi.styleapi;
 
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IValueHolder;
 import ch.nolix.coreapi.documentapi.nodeapi.INode;
+import ch.nolix.systemapi.elementapi.baseapi.IElement;
 
-public interface IAttachingAttribute extends IValueHolder<INode<?>> {
+public interface IAttachingAttribute extends IElement, IValueHolder<INode<?>> {
 
   Enum<?> getTag();
 
