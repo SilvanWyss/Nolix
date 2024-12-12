f32afcfe849588b0944a32c391db38a8b5ad1476
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-12 12:29:43 +0100

    Suppress Sonar issue

--- Source/ch/nolix/core/testing/performancetest/PerformanceTest.java
+++ Source/ch/nolix/core/testing/performancetest/PerformanceTest.java
@@ -5,7 +5,7 @@ import java.util.function.IntFunction;
 import ch.nolix.core.errorcontrol.perfomancevalidator.ObjectSupplierMediator;
 import ch.nolix.coreapi.errorcontrolapi.performancevalidatorapi.IObjectSupplierMediator;
 
-public abstract class PerformanceTest {
+public abstract class PerformanceTest { //NOSONAR: PerformanceTest does not have abstract methods.
 
   protected final <O> IObjectSupplierMediator<O> expectOnAnObjectFrom(final IntFunction<O> objectSupplier) {
     return ObjectSupplierMediator.forObjectSupplier(objectSupplier);
