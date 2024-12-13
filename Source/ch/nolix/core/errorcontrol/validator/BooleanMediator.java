d1d5dfce9b1a5fbfd76d46c907681944426328e5
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-13 20:17:37 +0100

    Create BooleanMediator

---
+++ Source/ch/nolix/core/errorcontrol/validator/BooleanMediator.java
@@ -0,0 +1,36 @@
+package ch.nolix.core.errorcontrol.validator;
+
+import ch.nolix.core.errorcontrol.invalidargumentexception.InvalidArgumentException;
+
+public final class BooleanMediator extends Mediator {
+
+  private final boolean argument;
+
+  private BooleanMediator(final boolean argument) {
+    this.argument = argument;
+  }
+
+  public static BooleanMediator forArgument(final boolean argument) {
+    return new BooleanMediator(argument);
+  }
+
+  public void is(final boolean value) { //NOSONAR: The parameter is a boolean.
+    if (value) {
+      isTrue();
+    } else {
+      isFalse();
+    }
+  }
+
+  public void isFalse() {
+    if (argument) {
+      throw InvalidArgumentException.forArgument(argument);
+    }
+  }
+
+  public void isTrue() {
+    if (!argument) {
+      throw InvalidArgumentException.forArgument(argument);
+    }
+  }
+}
