89be627764c670210bce2b38c5b49a1e929b91fb
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-14 22:23:26 +0100

    Make IColumn a ITableComponent

--- Source/ch/nolix/systemapi/objectschemaapi/schemaapi/IColumn.java
+++ Source/ch/nolix/systemapi/objectschemaapi/schemaapi/IColumn.java
@@ -2,19 +2,22 @@ package ch.nolix.systemapi.objectschemaapi.schemaapi;
 
 import ch.nolix.coreapi.attributeapi.fluentmutablemandatoryattributeapi.IFluentMutableNameHolder;
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IIdHolder;
+import ch.nolix.coreapi.componentapi.datamodelcomponentapi.ITableComponent;
 import ch.nolix.coreapi.stateapi.staterequestapi.EmptinessRequestable;
 import ch.nolix.systemapi.databaseobjectapi.databaseobjectapi.Deletable;
 import ch.nolix.systemapi.databaseobjectapi.databaseobjectapi.IDatabaseObject;
 import ch.nolix.systemapi.rawschemaapi.schemadtoapi.IColumnDto;
 
 public interface IColumn
-extends Deletable, EmptinessRequestable, IDatabaseObject, IFluentMutableNameHolder<IColumn>, IIdHolder {
-
-  boolean belongsToTable();
+extends
+Deletable,
+EmptinessRequestable,
+IDatabaseObject,
+IFluentMutableNameHolder<IColumn>,
+IIdHolder,
+ITableComponent<ITable> {
 
   IParameterizedFieldType getParameterizedFieldType();
-
-  ITable getStoredParentTable();
 
   IColumn setParameterizedFieldType(IParameterizedFieldType parameterizedFieldType);
 
