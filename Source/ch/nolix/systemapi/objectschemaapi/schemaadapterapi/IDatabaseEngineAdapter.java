3495c01b7ac28ce77cd8ed5aac7bd52bcd9a51f3
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-15 16:40:26 +0100

    Delete IDatabaseEngine

--- Source/ch/nolix/systemapi/objectschemaapi/schemaadapterapi/IDatabaseEngineAdapter.java
+++
@@ -1,9 +0,0 @@
-package ch.nolix.systemapi.objectschemaapi.schemaadapterapi;
-
-import ch.nolix.coreapi.programcontrolapi.savecontrolapi.IChangeSaver;
-import ch.nolix.systemapi.objectschemaapi.schemaapi.IDatabaseEngine;
-
-public interface IDatabaseEngineAdapter extends IChangeSaver {
-
-  IDatabaseEngine getStoredDatabaseEngine();
-}
