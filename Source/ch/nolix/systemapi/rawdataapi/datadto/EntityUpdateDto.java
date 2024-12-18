82fc969b270cfe57042561e6116316d82df2c713
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-18 15:12:07 +0100

    Fix line length

--- Source/ch/nolix/systemapi/rawdataapi/datadto/EntityUpdateDto.java
+++ Source/ch/nolix/systemapi/rawdataapi/datadto/EntityUpdateDto.java
@@ -3,7 +3,10 @@ package ch.nolix.systemapi.rawdataapi.datadto;
 import ch.nolix.core.container.immutablelist.ImmutableList;
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 
-public record EntityUpdateDto(String id, String saveStamp, IContainer<ContentFieldWithContentAsStringDto> updatedContentFields) {
+public record EntityUpdateDto(
+String id,
+String saveStamp,
+IContainer<ContentFieldWithContentAsStringDto> updatedContentFields) {
 
   public EntityUpdateDto(String id, String saveStamp, ContentFieldWithContentAsStringDto updatedContentField) {
     this(id, saveStamp, ImmutableList.withElement(updatedContentField));
