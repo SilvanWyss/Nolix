8c676ff7d223b8a3e722e0368f3d7bb2bd975194
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-19 13:03:48 +0100

    Create DarkStyleSubStyleCatalogue

--- Source/ch/nolix/template/webgui/style/StyleCatalogue.java
+++ Source/ch/nolix/template/webgui/style/StyleCatalogue.java
@@ -1,10 +1,30 @@
 package ch.nolix.template.webgui.style;
 
+import ch.nolix.system.element.style.Style;
 import ch.nolix.systemapi.elementapi.styleapi.IStyle;
 
 public final class StyleCatalogue {
 
-  public static final IStyle DARK_STYLE = DarkStyleCreator.createDarkStyle();
+  public static final IStyle DARK_STYLE = //
+  new Style()
+    .withSubStyle(
+      DarkStyleSubStyleCatalogue.LAYER_STYLE,
+      DarkStyleSubStyleCatalogue.DIALOG_LAYER_STYLE,
+      DarkStyleSubStyleCatalogue.CONTROL_STYLE,
+      DarkStyleSubStyleCatalogue.DIALOG_CONTAINER_STYLE,
+      DarkStyleSubStyleCatalogue.FOOTER_HORIZONTAL_STACK_STYLE,
+      DarkStyleSubStyleCatalogue.GRID_STYLE,
+      DarkStyleSubStyleCatalogue.HEADER_HORIZONTAL_STACK_STYLE,
+      DarkStyleSubStyleCatalogue.LINEAR_CONTAINER_STYLE,
+      DarkStyleSubStyleCatalogue.MAIN_CONTENT_CONTAINER_STYLE,
+      DarkStyleSubStyleCatalogue.OVERALL_CONTAINER_VERTICAL_STACK_STYLE,
+      DarkStyleSubStyleCatalogue.BUTTON_STYLE,
+      DarkStyleSubStyleCatalogue.DROPDOWN_MENU_STYLE,
+      DarkStyleSubStyleCatalogue.LEVEL1_HEADER_STYLE,
+      DarkStyleSubStyleCatalogue.LINK_STYLE,
+      DarkStyleSubStyleCatalogue.TEXT_BOX_STYLE,
+      DarkStyleSubStyleCatalogue.TITLE_STYLE,
+      DarkStyleSubStyleCatalogue.VALIDATION_LABEL_STYLE);
 
   private StyleCatalogue() {
   }
