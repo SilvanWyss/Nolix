93f2c041c2c69c227482dd3181539bd0f65dec9e
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-21 10:52:39 +0100

    Make IDatabaseObject a StateRequestable

--- Source/ch/nolix/systemapi/databaseobjectapi/databaseobjectapi/IDatabaseObject.java
+++ Source/ch/nolix/systemapi/databaseobjectapi/databaseobjectapi/IDatabaseObject.java
@@ -1,16 +1,27 @@
 package ch.nolix.systemapi.databaseobjectapi.databaseobjectapi;
 
 import ch.nolix.coreapi.resourcecontrolapi.resourceclosingapi.CloseStateRequestable;
+import ch.nolix.coreapi.structureapi.typerequestapi.StateRequestable;
 import ch.nolix.systemapi.databaseobjectapi.databaseobjectproperty.DatabaseObjectState;
 import ch.nolix.systemapi.databaseobjectapi.databaseobjectrequestapi.CreationRequestable;
 import ch.nolix.systemapi.databaseobjectapi.databaseobjectrequestapi.DeletionRequestable;
 import ch.nolix.systemapi.databaseobjectapi.databaseobjectrequestapi.EditingRequestable;
 import ch.nolix.systemapi.databaseobjectapi.databaseobjectrequestapi.LoadingRequestable;
 
+/**
+ * A {@link IDatabaseObject} can be connected with a real database.
+ * 
+ * @author Silvan Wyss
+ * @version 2021-07-03
+ */
 public interface IDatabaseObject
-extends CloseStateRequestable, DeletionRequestable, EditingRequestable, LoadingRequestable, CreationRequestable {
-
-  DatabaseObjectState getState();
+extends
+CloseStateRequestable,
+CreationRequestable,
+DeletionRequestable,
+EditingRequestable,
+LoadingRequestable,
+StateRequestable<DatabaseObjectState> {
 
   boolean isConnectedWithRealDatabase();
 }
