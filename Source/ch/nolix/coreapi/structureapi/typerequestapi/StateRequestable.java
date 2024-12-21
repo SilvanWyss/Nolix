dd0606bf5d97dbf4302e445489bdfe168fbddc20
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-21 10:49:56 +0100

    Create StateRequestable

---
+++ Source/ch/nolix/coreapi/structureapi/typerequestapi/StateRequestable.java
@@ -0,0 +1,16 @@
+package ch.nolix.coreapi.structureapi.typerequestapi;
+
+/**
+ * A {@link StateRequestable} has a certain state.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-12-21
+ * @param <S> is the type of the state of a {@link StateRequestable}.
+ */
+public interface StateRequestable<S extends Enum<S>> {
+
+  /**
+   * @return the state of the current {@link StateRequestable}.
+   */
+  S getState();
+}
