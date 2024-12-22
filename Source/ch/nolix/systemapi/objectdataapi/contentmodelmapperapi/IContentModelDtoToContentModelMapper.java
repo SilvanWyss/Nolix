bb5a6a701d37c23ca3bf25a01376131046d2e8e8
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-22 14:08:14 +0100

    Edit IContentModelDtoToContentModelMapper

--- Source/ch/nolix/systemapi/objectdataapi/contentmodelmapperapi/IContentModelDtoToContentModelMapper.java
+++ Source/ch/nolix/systemapi/objectdataapi/contentmodelmapperapi/IContentModelDtoToContentModelMapper.java
@@ -6,9 +6,24 @@ import ch.nolix.systemapi.objectdataapi.dataapi.IEntity;
 import ch.nolix.systemapi.objectdataapi.dataapi.ITable;
 import ch.nolix.systemapi.rawschemaapi.schemadto.IContentModelDto;
 
+/**
+ * A {@link IContentModelDtoToContentModelMapper} maps {@link IContentModelDto}s
+ * to {@link IContentModel}s.
+ * 
+ * @author Silvan Wyss
+ * @version 2023-12-28
+ * @param <T> is the type of the {@link IContentModelDto}s a
+ *            {@link IContentModelDtoToContentModelMapper} maps.
+ */
 public interface IContentModelDtoToContentModelMapper<T extends IContentModelDto> {
 
+  /**
+   * @param contentModelDto
+   * @param referencedTables
+   * @return a new {@link IContentModel} from the given contentModelDto using the
+   *         given referencedTables.
+   */
   IContentModel mapContentModelDtoToContentModel(
     T contentModelDto,
-    IContainer<? extends ITable<IEntity>> referencableTables);
+    IContainer<? extends ITable<IEntity>> referencedTables);
 }
