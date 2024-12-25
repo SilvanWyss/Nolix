66548f8c6edd691dba214d0dd9a342c70e357afa
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-25 21:38:22 +0100

    Delete createEntityUpdateDtoForSetValue method of IValueTool

--- Source/ch/nolix/system/objectdata/fieldtool/ValueTool.java
+++ Source/ch/nolix/system/objectdata/fieldtool/ValueTool.java
@@ -2,8 +2,6 @@ package ch.nolix.system.objectdata.fieldtool;
 
 import ch.nolix.systemapi.objectdataapi.dataapi.IValue;
 import ch.nolix.systemapi.objectdataapi.fieldtoolapi.IValueTool;
-import ch.nolix.systemapi.rawdataapi.datadto.EntityUpdateDto;
-import ch.nolix.systemapi.rawdataapi.datadto.StringContentFieldDto;
 
 public final class ValueTool extends FieldTool implements IValueTool {
 
@@ -12,18 +10,6 @@ public final class ValueTool extends FieldTool implements IValueTool {
     return //
     canSetValue(value)
     && valueToSet != null;
-  }
-
-  @Override
-  public EntityUpdateDto createEntityUpdateDtoForSetValue(final IValue<?> value, final Object setValue) {
-
-    final var parentEntity = value.getStoredParentEntity();
-
-    return //
-    new EntityUpdateDto(
-      parentEntity.getId(),
-      parentEntity.getSaveStamp(),
-      StringContentFieldDto.withColumnNameAndContent(value.getName(), setValue.toString()));
   }
 
   private boolean canSetValue(final IValue<?> value) {
--- Source/ch/nolix/systemapi/objectdataapi/fieldtoolapi/IValueTool.java
+++ Source/ch/nolix/systemapi/objectdataapi/fieldtoolapi/IValueTool.java
@@ -1,11 +1,8 @@
 package ch.nolix.systemapi.objectdataapi.fieldtoolapi;
 
 import ch.nolix.systemapi.objectdataapi.dataapi.IValue;
-import ch.nolix.systemapi.rawdataapi.datadto.EntityUpdateDto;
 
 public interface IValueTool extends IFieldTool {
 
   boolean canSetValue(IValue<?> value, Object valueToSet);
-
-  EntityUpdateDto createEntityUpdateDtoForSetValue(IValue<?> value, Object valueToSet);
 }
