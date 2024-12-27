06f5ab7f26bcdc5a3ebee9c58894ce3786d60784
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-27 15:49:28 +0100

    Create IOptionalReferenceModel

--- Source/ch/nolix/system/objectschema/contentmodel/OptionalReferenceModel.java
+++ Source/ch/nolix/system/objectschema/contentmodel/OptionalReferenceModel.java
@@ -3,11 +3,12 @@ package ch.nolix.system.objectschema.contentmodel;
 import ch.nolix.core.container.containerview.ContainerView;
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.systemapi.objectdataapi.fieldproperty.ContentType;
+import ch.nolix.systemapi.objectschemaapi.schemaapi.IOptionalReferenceModel;
 import ch.nolix.systemapi.objectschemaapi.schemaapi.ITable;
 import ch.nolix.systemapi.rawschemaapi.schemadto.IContentModelDto;
 import ch.nolix.systemapi.rawschemaapi.schemadto.OptionalReferenceModelDto;
 
-public final class OptionalReferenceModel extends AbstractReferenceModel {
+public final class OptionalReferenceModel extends AbstractReferenceModel implements IOptionalReferenceModel {
 
   private OptionalReferenceModel(final IContainer<ITable> referencedTables) {
     super(referencedTables);
