4455a67ebcb7af6a71223612fea839abc6e68568
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-27 22:17:45 +0100

    Create IBasicCalculator

--- Source/ch/nolix/core/math/basic/BasicCalculator.java
+++ Source/ch/nolix/core/math/basic/BasicCalculator.java
@@ -1,43 +1,43 @@
 package ch.nolix.core.math.basic;
 
 import ch.nolix.core.errorcontrol.invalidargumentexception.EmptyArgumentException;
+import ch.nolix.coreapi.mathapi.basicapi.IBasicCalculator;
 import ch.nolix.coreapi.programatomapi.variableapi.PluralLowerCaseVariableCatalogue;
 
-public final class BasicCalculator {
+/**
+ * @author Silvan Wyss
+ * @version 2022-11-13
+ */
+public final class BasicCalculator implements IBasicCalculator {
 
   /**
-   * @param value1
-   * @param value2
-   * @return the absolute difference between the given 2 values.
+   * {@inheritDoc}
    */
+  @Override
   public double getAbsoluteDifference(final double value1, final double value2) {
     return getAbsoluteValue(value2 - value1);
   }
 
   /**
-   * @param value1
-   * @param value2
-   * @return the absolute difference between the given 2 values.
+   * {@inheritDoc}
    */
+  @Override
   public int getAbsoluteDifference(final int value1, final int value2) {
     return getAbsoluteValue(value2 - value1);
   }
 
   /**
-   * @param value1
-   * @param value2
-   * @return the absolute difference between the given 2 values.
+   * {@inheritDoc}
    */
+  @Override
   public long getAbsoluteDifference(final long value1, final long value2) {
     return getAbsoluteValue(value2 - value1);
   }
 
   /**
-   * The absolute value of a value x is -x if x is negative, x otherwise.
-   * 
-   * @param value
-   * @return the absolute value of the given value.
+   * {@inheritDoc}
    */
+  @Override
   public double getAbsoluteValue(final double value) {
 
     if (value < 0) {
@@ -48,11 +48,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * The absolute value of a value x is -x if x is negative, x otherwise.
-   * 
-   * @param value
-   * @return the absolute value of the given value.
+   * {@inheritDoc}
    */
+  @Override
   public int getAbsoluteValue(final int value) {
 
     if (value < 0) {
@@ -63,11 +61,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * The absolute value of a value x is -x if x is negative, x otherwise.
-   * 
-   * @param value
-   * @return the absolute value of the given value.
+   * {@inheritDoc}
    */
+  @Override
   public long getAbsoluteValue(final long value) {
 
     if (value < 0) {
@@ -78,10 +74,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the average of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public double getAverage(final double value, final double... values) {
 
     final var sum = getSum(value, values);
@@ -91,10 +86,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param values
-   * @return the average of the given values.
-   * @throws EmptyArgumentException if the given values is empty.
+   * {@inheritDoc}
    */
+  @Override
   public double getAverage(final Iterable<Double> values) {
 
     var count = 0;
@@ -112,10 +106,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the average of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public int getAverage(final int value, final int... values) {
 
     final var sum = getSum(value, values);
@@ -125,10 +118,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the average of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public long getAverage(final long value, final long... values) {
 
     final var sum = getSum(value, values);
@@ -138,10 +130,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the biggest value of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public double getMax(final double value, final double... values) {
 
     var max = value;
@@ -155,10 +146,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the biggest value of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public int getMax(final int value, final int... values) {
 
     var max = value;
@@ -172,10 +162,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the biggest value of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public long getMax(final long value, final long... values) {
 
     var max = value;
@@ -189,10 +178,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the smallest value of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public double getMin(final double value, final double... values) {
 
     var min = value;
@@ -206,10 +194,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the smallest value of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public int getMin(final int value, final int... values) {
 
     var min = value;
@@ -223,10 +210,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the smallest value of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public long getMin(final long value, final long... values) {
 
     var min = value;
@@ -240,34 +226,33 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @return the square of the given value.
+   * {@inheritDoc}
    */
+  @Override
   public double getSquare(final double value) {
     return (value * value);
   }
 
   /**
-   * @param value
-   * @return the square of the given value.
+   * {@inheritDoc}
    */
+  @Override
   public double getSquare(final int value) {
     return (value * value);
   }
 
   /**
-   * @param value
-   * @return the square of the given value.
+   * {@inheritDoc}
    */
+  @Override
   public double getSquare(final long value) {
     return (value * value);
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the sum of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public double getSum(final double value, final double... values) {
 
     var sum = value;
@@ -280,10 +265,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the sum of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public int getSum(final int value, final int... values) {
 
     var sum = value;
@@ -295,9 +279,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param values
-   * @return the sum of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public double getSum(final Iterable<Double> values) {
 
     var sum = 0.0;
@@ -309,10 +293,9 @@ public final class BasicCalculator {
   }
 
   /**
-   * @param value
-   * @param values
-   * @return the sum of the given values.
+   * {@inheritDoc}
    */
+  @Override
   public long getSum(final long value, final long... values) {
 
     var sum = value;
---
+++ Source/ch/nolix/coreapi/mathapi/basicapi/IBasicCalculator.java
@@ -0,0 +1,170 @@
+package ch.nolix.coreapi.mathapi.basicapi;
+
+import ch.nolix.core.errorcontrol.invalidargumentexception.EmptyArgumentException;
+
+/**
+ * @author Silvan Wyss
+ * @version 2024-12-27
+ */
+public interface IBasicCalculator {
+
+  /**
+   * @param value1
+   * @param value2
+   * @return the absolute difference between the given 2 values.
+   */
+  double getAbsoluteDifference(double value1, double value2);
+
+  /**
+   * @param value1
+   * @param value2
+   * @return the absolute difference between the given 2 values.
+   */
+  int getAbsoluteDifference(int value1, int value2);
+
+  /**
+   * @param value1
+   * @param value2
+   * @return the absolute difference between the given 2 values.
+   */
+  long getAbsoluteDifference(long value1, long value2);
+
+  /**
+   * The absolute value of a value x is -x if x is negative, x otherwise.
+   * 
+   * @param value
+   * @return the absolute value of the given value.
+   */
+  double getAbsoluteValue(double value);
+
+  /**
+   * The absolute value of a value x is -x if x is negative, x otherwise.
+   * 
+   * @param value
+   * @return the absolute value of the given value.
+   */
+  int getAbsoluteValue(int value);
+
+  /**
+   * The absolute value of a value x is -x if x is negative, x otherwise.
+   * 
+   * @param value
+   * @return the absolute value of the given value.
+   */
+  long getAbsoluteValue(long value);
+
+  /**
+   * @param value
+   * @param values
+   * @return the average of the given values.
+   */
+  double getAverage(double value, double... values);
+
+  /**
+   * @param values
+   * @return the average of the given values.
+   * @throws EmptyArgumentException if the given values is empty.
+   */
+  double getAverage(Iterable<Double> values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the average of the given values.
+   */
+  int getAverage(int value, int... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the average of the given values.
+   */
+  long getAverage(long value, long... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the biggest value of the given values.
+   */
+  double getMax(double value, double... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the biggest value of the given values.
+   */
+  int getMax(int value, int... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the biggest value of the given values.
+   */
+  long getMax(long value, long... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the smallest value of the given values.
+   */
+  double getMin(double value, double... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the smallest value of the given values.
+   */
+  int getMin(int value, int... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the smallest value of the given values.
+   */
+  long getMin(long value, long... values);
+
+  /**
+   * @param value
+   * @return the square of the given value.
+   */
+  double getSquare(double value);
+
+  /**
+   * @param value
+   * @return the square of the given value.
+   */
+  double getSquare(int value);
+
+  /**
+   * @param value
+   * @return the square of the given value.
+   */
+  double getSquare(long value);
+
+  /**
+   * @param value
+   * @param values
+   * @return the sum of the given values.
+   */
+  double getSum(double value, double... values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the sum of the given values.
+   */
+  int getSum(int value, int... values);
+
+  /**
+   * @param values
+   * @return the sum of the given values.
+   */
+  double getSum(Iterable<Double> values);
+
+  /**
+   * @param value
+   * @param values
+   * @return the sum of the given values.
+   */
+  long getSum(long value, long... values);
+}
