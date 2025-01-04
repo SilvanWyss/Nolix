ee52585611eba3f533e07dbd90160d1f0a303761
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-04 17:22:14 +0100

    Edit IResourcePool

--- Source/ch/nolix/coreapi/resourcecontrolapi/resourcepoolapi/IResourcePool.java
+++ Source/ch/nolix/coreapi/resourcecontrolapi/resourcepoolapi/IResourcePool.java
@@ -2,7 +2,20 @@ package ch.nolix.coreapi.resourcecontrolapi.resourcepoolapi;
 
 import ch.nolix.coreapi.resourcecontrolapi.resourceclosingapi.GroupCloseable;
 
-public interface IResourcePool<R extends GroupCloseable> extends GroupCloseable {
+/**
+ * A {@link IResourcePool} manages resources that are {@link AutoCloseable}s.
+ * 
+ * @author Silvan Wyss
+ * @version 2024-01-24
+ * @param <R> is the type of the resources of a {@link IResourcePool}.
+ */
+public interface IResourcePool<R extends AutoCloseable> extends GroupCloseable {
 
+  /**
+   * @return resource from the current {@link IResourcePool} which will be
+   *         borrowed. When The resource is closed its internal resource will be
+   *         given back to the current {@link IResourcePool} and will be available
+   *         again.
+   */
   R borrowResource();
 }
