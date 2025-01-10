bed724143e14b8a5239970ae0231b88c2fc571c6
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-10 22:23:17 +0100

    Edit ColumnDefinitionMapper

--- Source/ch/nolix/system/noderawdata/datareader/ColumnDefinitionMapper.java
+++ Source/ch/nolix/system/noderawdata/datareader/ColumnDefinitionMapper.java
@@ -1,9 +1,7 @@
 package ch.nolix.system.noderawdata.datareader;
 
-import ch.nolix.coreapi.datamodelapi.fieldproperty.DataType;
 import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
 import ch.nolix.system.noderawschema.nodesearcher.ColumnNodeSearcher;
-import ch.nolix.system.noderawschema.nodesearcher.ContentModelNodeSearcher;
 import ch.nolix.system.sqlrawdata.schemaview.ColumnView;
 import ch.nolix.systemapi.noderawschemaapi.nodesearcherapi.IColumnNodeSearcher;
 import ch.nolix.systemapi.rawdataapi.schemaviewapi.IColumnView;
@@ -11,8 +9,6 @@ import ch.nolix.systemapi.rawdataapi.schemaviewapi.IColumnView;
 public final class ColumnDefinitionMapper {
 
   private static final IColumnNodeSearcher COLUMN_NODE_SEARCHER = new ColumnNodeSearcher();
-
-  private static final ContentModelNodeSearcher CONTENT_MODEL_NODE_SEARCHER = new ContentModelNodeSearcher();
 
   public IColumnView createColumnDefinitionFromColumnNode(
     final IMutableNode<?> columnNode,
@@ -22,22 +18,7 @@ public final class ColumnDefinitionMapper {
       COLUMN_NODE_SEARCHER.getColumnIdFromColumnNode(columnNode),
       COLUMN_NODE_SEARCHER.getColumnNameFromColumnNode(columnNode),
       COLUMN_NODE_SEARCHER.getColumnContentTypeFromColumnNode(columnNode),
-      getColumnDataTypeFromColumnNode(columnNode),
+      COLUMN_NODE_SEARCHER.getColumnDataTypeFromColumnNode(columnNode),
       columnIndexOnEntityNode);
-  }
-
-  private DataType getColumnDataTypeFromColumnNode(final IMutableNode<?> columnNode) {
-    return getDataTypeFromContentModelNode(
-      COLUMN_NODE_SEARCHER.getStoredContentModelNodeFromColumnNode(columnNode));
-  }
-
-  private DataType getDataTypeFromDataTypeNode(final IMutableNode<?> dataTypeNode) {
-    return DataType.valueOf(dataTypeNode.getSingleChildNodeHeader());
-  }
-
-  private DataType getDataTypeFromContentModelNode(IMutableNode<?> contentModelNode) {
-    return getDataTypeFromDataTypeNode(
-      CONTENT_MODEL_NODE_SEARCHER.getStoredDataTypeNodeFromContentModelNode(
-        contentModelNode));
   }
 }
