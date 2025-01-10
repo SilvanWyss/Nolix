1a10ceb764f7c25b82ad0aaf5af36bda27451aa6
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-10 22:32:17 +0100

    Create IColumnViewDtoMapper

---
+++ Source/ch/nolix/system/noderawdata/schemaviewdtomapper/ColumnViewDtoMapper.java
@@ -0,0 +1,38 @@
+package ch.nolix.system.noderawdata.schemaviewdtomapper;
+
+import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
+import ch.nolix.system.noderawschema.nodesearcher.ColumnNodeSearcher;
+import ch.nolix.systemapi.noderawdataapi.schemaviewdtomapperapi.IColumnViewDtoMapper;
+import ch.nolix.systemapi.noderawschemaapi.nodesearcherapi.IColumnNodeSearcher;
+import ch.nolix.systemapi.rawdataapi.schemaviewdto.ColumnViewDto;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-01-10
+ */
+public final class ColumnViewDtoMapper implements IColumnViewDtoMapper {
+
+  private static final IColumnNodeSearcher COLUMN_NODE_SEARCHER = new ColumnNodeSearcher();
+
+  @Override
+  /**
+   * {@inheritDoc}
+   */
+  public ColumnViewDto mapColumnNodeToColumnViewDto(
+    final IMutableNode<?> columnNode,
+    final int oneBasedColumnOrdinalIndex) {
+
+    final var id = COLUMN_NODE_SEARCHER.getColumnIdFromColumnNode(columnNode);
+    final var name = COLUMN_NODE_SEARCHER.getColumnNameFromColumnNode(columnNode);
+    final var contentType = COLUMN_NODE_SEARCHER.getColumnContentTypeFromColumnNode(columnNode);
+    final var dataType = COLUMN_NODE_SEARCHER.getColumnDataTypeFromColumnNode(columnNode);
+
+    return //
+    new ColumnViewDto(
+      id,
+      name,
+      oneBasedColumnOrdinalIndex,
+      contentType,
+      dataType);
+  }
+}
---
+++ Source/ch/nolix/systemapi/noderawdataapi/schemaviewdtomapperapi/IColumnViewDtoMapper.java
@@ -0,0 +1,20 @@
+package ch.nolix.systemapi.noderawdataapi.schemaviewdtomapperapi;
+
+import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
+import ch.nolix.systemapi.rawdataapi.schemaviewdto.ColumnViewDto;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-01-10
+ */
+public interface IColumnViewDtoMapper {
+
+  /**
+   * @param columnNode
+   * @param oneBasedColumnOrdinalIndex
+   * @return a new {@link ColumnViewDto} from the given columnNode and
+   *         oneBasedColumnOrdinalIndex.
+   * @throws RuntimeException if the given columnNode is null.
+   */
+  ColumnViewDto mapColumnNodeToColumnViewDto(IMutableNode<?> columnNode, int oneBasedColumnOrdinalIndex);
+}
