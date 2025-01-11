9c8366bb4652205e062a6b99297420ce41ad6c72
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-11 22:57:32 +0100

    Delete TableDefinitionMapper

--- Source/ch/nolix/system/noderawdata/datareader/TableDefinitionLoader.java
+++ Source/ch/nolix/system/noderawdata/datareader/TableDefinitionLoader.java
@@ -2,19 +2,21 @@ package ch.nolix.system.noderawdata.datareader;
 
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
+import ch.nolix.system.noderawdata.schemaviewdtomapper.TableViewDtoMapper;
 import ch.nolix.system.noderawschema.nodesearcher.DatabaseNodeSearcher;
+import ch.nolix.systemapi.noderawdataapi.schemaviewdtomapperapi.ITableViewDtoMapper;
 import ch.nolix.systemapi.rawdataapi.schemaviewdto.TableViewDto;
 
 public final class TableDefinitionLoader {
 
   private static final DatabaseNodeSearcher DATABASE_NODE_SEARCHER = new DatabaseNodeSearcher();
 
-  private static final TableDefinitionMapper TABLE_DEFINITION_MAPPER = new TableDefinitionMapper();
+  private static final ITableViewDtoMapper TABLE_VIEW_DTO_MAPPER = new TableViewDtoMapper();
 
   public IContainer<TableViewDto> loadTableDefinitionsFromNodeDatabase(final IMutableNode<?> nodeDatabase) {
 
     final var tableNodes = DATABASE_NODE_SEARCHER.getStoredTableNodesFromNodeDatabase(nodeDatabase);
 
-    return tableNodes.to(TABLE_DEFINITION_MAPPER::createTableDefinitionFromTableNode);
+    return tableNodes.to(TABLE_VIEW_DTO_MAPPER::mapTableNodeToTableViewDto);
   }
 }
