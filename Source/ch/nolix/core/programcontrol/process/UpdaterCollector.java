41e68325d65ab4c13114983ca9bd97069ba8dbd7
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-12 14:16:58 +0100

    Create IUpdaterCollector

---
+++ Source/ch/nolix/core/programcontrol/process/UpdaterCollector.java
@@ -0,0 +1,77 @@
+package ch.nolix.core.programcontrol.process;
+
+import java.util.function.Consumer;
+
+import ch.nolix.core.container.linkedlist.LinkedList;
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.containerapi.listapi.ILinkedList;
+import ch.nolix.coreapi.programcontrolapi.processapi.IUpdaterCollector;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-01-12
+ * @param <O> is the type of the {@link Object}s the updaters of a
+ *            {@link UpdaterCollector} can mutate.
+ */
+public final class UpdaterCollector<O> implements IUpdaterCollector<O> {
+
+  private final ILinkedList<Consumer<O>> updaters = LinkedList.createEmpty();
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void addUpdater(final Consumer<O> updater) {
+    updaters.addAtEnd(updater);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void addUpdaters(final IContainer<Consumer<O>> updaters) {
+    this.updaters.addAtEnd(updaters);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void clear() {
+    updaters.clear();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isEmpty() {
+    return updaters.isEmpty();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void updateObjectAndClear(final O object) {
+
+    GlobalValidator.assertThat(object).thatIsNamed(Object.class).isNotNull();
+
+    updateObjectAndClearWhenObjectIsNotNull(object);
+  }
+
+  /**
+   * Updates the given object and clears the current {@link IUpdaterCollector} for
+   * the case when the given object is not null.
+   * 
+   * @param object
+   */
+  private void updateObjectAndClearWhenObjectIsNotNull(final O object) {
+    try {
+      updaters.forEach(u -> u.accept(object));
+    } finally {
+      clear();
+    }
+  }
+}
---
+++ Source/ch/nolix/coreapi/programcontrolapi/processapi/IUpdaterCollector.java
@@ -0,0 +1,40 @@
+package ch.nolix.coreapi.programcontrolapi.processapi;
+
+import java.util.function.Consumer;
+
+import ch.nolix.coreapi.containerapi.baseapi.IContainer;
+import ch.nolix.coreapi.stateapi.statemutationapi.Clearable;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-01-12
+ * @param <O> is the type of the {@link Object}s the updaters of a
+ *            {@link IUpdaterCollector} can mutate.
+ */
+public interface IUpdaterCollector<O> extends Clearable {
+
+  /**
+   * Adds the given updater to the current {@link IUpdaterCollector}.
+   * 
+   * @param updater
+   * @throws RuntimeException if the given updater is null.
+   */
+  void addUpdater(Consumer<O> updater);
+
+  /**
+   * Adds the given updaters to the current {@link IUpdaterCollector}.
+   * 
+   * @param updaters
+   * @throws RuntimeException if the given updaters is null.
+   * @throws RuntimeException if one of the given updaters is null.
+   */
+  void addUpdaters(IContainer<Consumer<O>> updaters);
+
+  /**
+   * Updates the given object and clears the current {@link IUpdaterCollector}.
+   * 
+   * @param object
+   * @throws RuntimeException if the given object is null.
+   */
+  void updateObjectAndClear(O object);
+}
