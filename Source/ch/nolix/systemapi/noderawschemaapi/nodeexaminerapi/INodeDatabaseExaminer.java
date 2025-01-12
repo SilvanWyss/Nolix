f5d265a4a9029cc443a5a79909bf4f2aedc303d1
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-12 19:19:40 +0100

    Create INodeDatabaseExaminer

---
+++ Source/ch/nolix/systemapi/noderawschemaapi/nodeexaminerapi/INodeDatabaseExaminer.java
@@ -0,0 +1,24 @@
+package ch.nolix.systemapi.noderawschemaapi.nodeexaminerapi;
+
+import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-01-12
+ */
+public interface INodeDatabaseExaminer {
+
+  /**
+   * @param nodeDatabase
+   * @return true if the given nodeDatabase is properly (!) initialized, false
+   *         otherwise.
+   */
+  boolean nodeDatabaseIsInitialized(IMutableNode<?> nodeDatabase);
+
+  /**
+   * @param nodeDatabase
+   * @return true if the given nodeDatabase is properly (!) uninitialized, false
+   *         otherwise.
+   */
+  boolean nodeDatabaseIsUninitialized(IMutableNode<?> nodeDatabase);
+}
