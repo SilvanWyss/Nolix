32e31a5f14928ab0f695d98a8dcf7236854f7199
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-14 23:46:56 +0100

    Delete AbstractSchemaAdapter

--- Source/ch/nolix/system/sqlrawschema/adapter/MsSqlSchemaAdapter.java
+++ Source/ch/nolix/system/sqlrawschema/adapter/MsSqlSchemaAdapter.java
@@ -4,19 +4,12 @@ import ch.nolix.core.sql.connectionpool.SqlConnectionPool;
 import ch.nolix.system.sqlschema.mssqlquerycreator.MsSqlQueryCreator;
 import ch.nolix.systemapi.sqlschemaapi.querycreatorapi.IQueryCreator;
 
-public final class MsSqlSchemaAdapter extends AbstractSchemaAdapter {
+public final class MsSqlSchemaAdapter extends AbstractSqlRawSchemaAdapter {
 
   private static final IQueryCreator QUERY_CREATOR = new MsSqlQueryCreator();
 
   private MsSqlSchemaAdapter(final String databaseName, final SqlConnectionPool sqlConnectionPool) {
-    super(
-      databaseName,
-      sqlConnectionPool,
-      ch.nolix.system.sqlschema.adapter.MsSqlSchemaAdapter
-        .forDatabaseWithGivenNameUsingConnectionFromGivenPool(
-          databaseName,
-          sqlConnectionPool),
-      QUERY_CREATOR);
+    super(databaseName, sqlConnectionPool.borrowResource(), QUERY_CREATOR);
   }
 
   public static MsSqlSchemaAdapter forDatabaseWithGivenNameUsingConnectionFromGivenPool(
