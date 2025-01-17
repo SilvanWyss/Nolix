5bbcedf99b2ee04428f54c30a167d8d77f177ff5
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-17 22:06:21 +0100

    Delete IDatabaseInitializer

--- Source/ch/nolix/systemapi/noderawschemaapi/databaseinitializerapi/IDatabaseInitializer.java
+++
@@ -1,8 +0,0 @@
-package ch.nolix.systemapi.noderawschemaapi.databaseinitializerapi;
-
-import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
-
-public interface IDatabaseInitializer {
-
-  void initializeDatabase(IMutableNode<?> nodeDatabase);
-}
