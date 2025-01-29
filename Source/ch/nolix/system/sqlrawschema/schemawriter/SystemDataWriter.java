1a7f04989500eb457740c48d622fda3eb58c0f22
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-29 21:42:07 +0100

    Beautify SystemDataWriter

--- Source/ch/nolix/system/sqlrawschema/schemawriter/SystemDataWriter.java
+++ Source/ch/nolix/system/sqlrawschema/schemawriter/SystemDataWriter.java
@@ -9,16 +9,15 @@ import ch.nolix.systemapi.rawschemaapi.modelapi.ColumnDto;
 import ch.nolix.systemapi.rawschemaapi.modelapi.IContentModelDto;
 import ch.nolix.systemapi.rawschemaapi.modelapi.TableDto;
 import ch.nolix.systemapi.sqlrawschemaapi.statementcreatorapi.IDatabasePropertiesStatementCreator;
+import ch.nolix.systemapi.sqlrawschemaapi.statementcreatorapi.ISchemaStatementCreator;
 import ch.nolix.systemapi.timeapi.momentapi.ITime;
 
-//TODO: Beautify
-final class SystemDataWriter implements ChangeRequestable {
+public final class SystemDataWriter implements ChangeRequestable {
 
   private static final IDatabasePropertiesStatementCreator DATABASE_PROPERTIES_STATEMENT_CREATOR = //
   new DatabasePropertiesStatementCreator();
 
-  private static final SchemaStatementCreator SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR = //
-  new SchemaStatementCreator();
+  private static final ISchemaStatementCreator SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR = new SchemaStatementCreator();
 
   private final SqlCollector sqlCollector;
 
@@ -30,21 +29,31 @@ final class SystemDataWriter implements ChangeRequestable {
   }
 
   public void addColumn(final String tableName, final ColumnDto column) {
-    sqlCollector.addSqlStatements(
-      SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementsToAddColumn(tableName, column));
+
+    final var statements = SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementsToAddColumn(tableName, column);
+
+    sqlCollector.addSqlStatements(statements);
   }
 
   public void deleteColumn(String tableName, String columnName) {
-    sqlCollector.addSqlStatement(
-      SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToDeleteColumn(tableName, columnName));
+
+    final var statement = SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToDeleteColumn(tableName, columnName);
+
+    sqlCollector.addSqlStatement(statement);
   }
 
   public void addTable(final TableDto table) {
-    sqlCollector.addSqlStatements(SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementsToAddTable(table));
+
+    final var statements = SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementsToAddTable(table);
+
+    sqlCollector.addSqlStatements(statements);
   }
 
   public void deleteTable(final String tableName) {
-    sqlCollector.addSqlStatement(SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToDeleteTable(tableName));
+
+    final var statement = SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToDeleteTable(tableName);
+
+    sqlCollector.addSqlStatement(statement);
   }
 
   @Override
@@ -53,20 +62,21 @@ final class SystemDataWriter implements ChangeRequestable {
   }
 
   public void setColumnName(final String tableName, final String columnName, final String newColumnName) {
-    sqlCollector.addSqlStatement(
-      SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToSetColumnName(
-        tableName,
-        columnName,
-        newColumnName));
+
+    final var statement = //
+    SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToSetColumnName(tableName, columnName, newColumnName);
+
+    sqlCollector.addSqlStatement(statement);
   }
 
   public void setColumnContentModel(
     final String columnId,
     final IContentModelDto contentModel) {
-    sqlCollector.addSqlStatement(
-      SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToSetColumnContentModel(
-        columnId,
-        contentModel));
+
+    final var statement = //
+    SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToSetColumnContentModel(columnId, contentModel);
+
+    sqlCollector.addSqlStatement(statement);
   }
 
   public void setSchemaTimestamp(ITime schemaTimestamp) {
@@ -77,7 +87,10 @@ final class SystemDataWriter implements ChangeRequestable {
   }
 
   public void setTableName(final String tableName, final String newTableName) {
-    sqlCollector.addSqlStatement(
-      SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToSetTableName(tableName, newTableName));
+
+    final var statement = //
+    SYSTEM_DATA_WRITER_SQL_STATEMENT_CREATOR.createStatementToSetTableName(tableName, newTableName);
+
+    sqlCollector.addSqlStatement(statement);
   }
 }
