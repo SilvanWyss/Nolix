1af2554baa6c414b16cb9323cd4a1dfd5de1aa9e
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-31 19:59:18 +0100

    Create IDatabaseSchemaViewDtoMapper

---
+++ Source/ch/nolix/system/noderawdata/schemaviewmodelmapper/DatabaseSchemaViewDtoMapper.java
@@ -0,0 +1,33 @@
+package ch.nolix.system.noderawdata.schemaviewmodelmapper;
+
+import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
+import ch.nolix.system.noderawschema.nodesearcher.DatabaseNodeSearcher;
+import ch.nolix.systemapi.noderawdataapi.schemaviewmodelmapperapi.IDatabaseSchemaViewDtoMapper;
+import ch.nolix.systemapi.noderawdataapi.schemaviewmodelmapperapi.ITableSchemaViewDtoMapper;
+import ch.nolix.systemapi.noderawschemaapi.nodesearcherapi.IDatabaseNodeSearcher;
+import ch.nolix.systemapi.rawdataapi.schemaviewmodel.DatabaseSchemaViewDto;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-01-31
+ */
+public final class DatabaseSchemaViewDtoMapper implements IDatabaseSchemaViewDtoMapper {
+
+  private static final IDatabaseNodeSearcher DATABASE_NODE_SEARCHER = new DatabaseNodeSearcher();
+
+  private static final ITableSchemaViewDtoMapper TABLE_SCHEMA_VIEW_DTO_MAPPER = new TableSchemaViewDtoMapper();
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public DatabaseSchemaViewDto mapTableNodeToTableViewDto(
+    final String databaseName,
+    final IMutableNode<?> nodeDatabase) {
+
+    final var tableNodes = DATABASE_NODE_SEARCHER.getStoredTableNodesFromNodeDatabase(nodeDatabase);
+    final var tableSchemaViews = tableNodes.to(TABLE_SCHEMA_VIEW_DTO_MAPPER::mapTableNodeToTableViewDto);
+
+    return new DatabaseSchemaViewDto(databaseName, tableSchemaViews);
+  }
+}
---
+++ Source/ch/nolix/systemapi/noderawdataapi/schemaviewmodelmapperapi/IDatabaseSchemaViewDtoMapper.java
@@ -0,0 +1,20 @@
+package ch.nolix.systemapi.noderawdataapi.schemaviewmodelmapperapi;
+
+import ch.nolix.coreapi.documentapi.nodeapi.IMutableNode;
+import ch.nolix.systemapi.rawdataapi.schemaviewmodel.DatabaseSchemaViewDto;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-01-31
+ */
+public interface IDatabaseSchemaViewDtoMapper {
+
+  /**
+   * @param databaseName
+   * @param nodeDatabase
+   * @return a new {@link DatabaseSchemaViewDto} with the given databaseName from
+   *         the given nodeDatabase.
+   * @throws RuntimeException if the given nodeDatabase is null.
+   */
+  DatabaseSchemaViewDto mapTableNodeToTableViewDto(String databaseName, IMutableNode<?> nodeDatabase);
+}
