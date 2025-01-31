86ad7511a2da2e7139b50c7702f37253d96b98fb
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-31 14:21:53 +0100

    Update ContentModelMapper

--- Source/ch/nolix/system/objectdata/modelmapper/ContentModelMapper.java
+++ Source/ch/nolix/system/objectdata/modelmapper/ContentModelMapper.java
@@ -18,32 +18,25 @@ import ch.nolix.systemapi.rawschemaapi.modelapi.ValueModelDto;
 
 public final class ContentModelMapper {
 
-  private static final ValueModelMapper PARAMETERIZED_VALUE_TYPE_MAPPER = //
-  new ValueModelMapper();
+  private static final ValueModelMapper VALUE_MODEL_MAPPER = new ValueModelMapper();
 
-  private static final OptionalValueModelMapper PARAMETERIZED_OPTIONAL_VALUE_TYPE_MAPPER = //
-  new OptionalValueModelMapper();
+  private static final OptionalValueModelMapper OPTIONAL_VALUE_MODEL_MAPPER = new OptionalValueModelMapper();
 
-  private static final MultiValueModelMapper PARAMETERIZED_MULTI_VALUE_TYPE_MAPPER = //
-  new MultiValueModelMapper();
+  private static final MultiValueModelMapper MULTI_VALUE_MODEL_MAPPER = new MultiValueModelMapper();
 
-  private static final ReferenceModelMapper PARAMETERIZED_REFERENCE_TYPE_MAPPER = //
-  new ReferenceModelMapper();
+  private static final ReferenceModelMapper REFERENCE_MODEL_MAPPER = new ReferenceModelMapper();
 
-  private static final OptionalReferenceModelMapper PARAMETERIZED_OPTIONAL_REFERENCE_TYPE_MAPPER = //
+  private static final OptionalReferenceModelMapper OPTIONAL_REFERENCE_MODEL_MAPPER = //
   new OptionalReferenceModelMapper();
 
-  private static final MultiReferenceModelMapper PARAMETERIZED_MULTI_REFERENCE_TYPE_MAPPER = //
-  new MultiReferenceModelMapper();
+  private static final MultiReferenceModelMapper MULTI_REFERENCE_MODEL_MAPPER = new MultiReferenceModelMapper();
 
-  private static final BackReferenceModelMapper PARAMETERIZED_BACK_REFERENCE_TYPE_MAPPER = //
-  new BackReferenceModelMapper();
+  private static final BackReferenceModelMapper BACK_REFERENCE_MODEL_MAPPER = new BackReferenceModelMapper();
 
-  private static final OptionalBackReferenceModelMapper //
-  PARAMETERIZED_OPTIONAL_BACK_REFERENCE_TYPE_MAPPER = //
+  private static final OptionalBackReferenceModelMapper OPTIONAL_BACK_REFERENCE_MODEL_MAPPER = //
   new OptionalBackReferenceModelMapper();
 
-  private static final MultiBackReferenceModelMapper PARAMETERIZED_MULTI_BACK_REFERENCE_TYPE_MAPPER = //
+  private static final MultiBackReferenceModelMapper MULTI_BACK_REFERENCE_MODEL_MAPPER = //
   new MultiBackReferenceModelMapper();
 
   public IContentModelView<ITable<IEntity>> mapContentModelDtoToContentModel(
@@ -51,48 +44,51 @@ public final class ContentModelMapper {
     final IContainer<? extends ITable<IEntity>> referencableTables) {
 
     if (contentModelDto instanceof ValueModelDto valueModelDto) {
-      return PARAMETERIZED_VALUE_TYPE_MAPPER.mapContentModelDtoToContentModelView(valueModelDto, referencableTables);
+      return VALUE_MODEL_MAPPER.mapContentModelDtoToContentModelView(valueModelDto, referencableTables);
     }
 
     if (contentModelDto instanceof OptionalValueModelDto optionalValueModelDto) {
-      return PARAMETERIZED_OPTIONAL_VALUE_TYPE_MAPPER.mapContentModelDtoToContentModelView(optionalValueModelDto,
-        referencableTables);
+      return //
+      OPTIONAL_VALUE_MODEL_MAPPER.mapContentModelDtoToContentModelView(optionalValueModelDto, referencableTables);
     }
 
     if (contentModelDto instanceof MultiValueModelDto multiValueModelDto) {
-      return PARAMETERIZED_MULTI_VALUE_TYPE_MAPPER.mapContentModelDtoToContentModelView(multiValueModelDto,
-        referencableTables);
+      return MULTI_VALUE_MODEL_MAPPER.mapContentModelDtoToContentModelView(multiValueModelDto, referencableTables);
     }
 
     if (contentModelDto instanceof ReferenceModelDto referenceModelDto) {
-      return PARAMETERIZED_REFERENCE_TYPE_MAPPER.mapContentModelDtoToContentModelView(referenceModelDto,
-        referencableTables);
+      return REFERENCE_MODEL_MAPPER.mapContentModelDtoToContentModelView(referenceModelDto, referencableTables);
     }
 
     if (contentModelDto instanceof OptionalReferenceModelDto optionalReferenceModelDto) {
-      return PARAMETERIZED_OPTIONAL_REFERENCE_TYPE_MAPPER.mapContentModelDtoToContentModelView(
+      return //
+      OPTIONAL_REFERENCE_MODEL_MAPPER.mapContentModelDtoToContentModelView(
         optionalReferenceModelDto,
         referencableTables);
     }
 
     if (contentModelDto instanceof MultiReferenceModelDto multiReferenceModelDto) {
-      return PARAMETERIZED_MULTI_REFERENCE_TYPE_MAPPER.mapContentModelDtoToContentModelView(multiReferenceModelDto,
-        referencableTables);
+      return //
+      MULTI_REFERENCE_MODEL_MAPPER.mapContentModelDtoToContentModelView(multiReferenceModelDto, referencableTables);
     }
 
     if (contentModelDto instanceof BackReferenceModelDto backReferenceModelDto) {
-      return PARAMETERIZED_BACK_REFERENCE_TYPE_MAPPER.mapContentModelDtoToContentModelView(backReferenceModelDto,
-        referencableTables);
+      return //
+      BACK_REFERENCE_MODEL_MAPPER.mapContentModelDtoToContentModelView(backReferenceModelDto, referencableTables);
     }
 
     if (contentModelDto instanceof OptionalBackReferenceModelDto optionalBackReferenceModelDto) {
-      return PARAMETERIZED_OPTIONAL_BACK_REFERENCE_TYPE_MAPPER
-        .mapContentModelDtoToContentModelView(optionalBackReferenceModelDto, referencableTables);
+      return //
+      OPTIONAL_BACK_REFERENCE_MODEL_MAPPER.mapContentModelDtoToContentModelView(
+        optionalBackReferenceModelDto,
+        referencableTables);
     }
 
     if (contentModelDto instanceof MultiBackReferenceModelDto multiBackReferenceModelDto) {
-      return PARAMETERIZED_MULTI_BACK_REFERENCE_TYPE_MAPPER
-        .mapContentModelDtoToContentModelView(multiBackReferenceModelDto, referencableTables);
+      return //
+      MULTI_BACK_REFERENCE_MODEL_MAPPER.mapContentModelDtoToContentModelView(
+        multiBackReferenceModelDto,
+        referencableTables);
     }
 
     throw InvalidArgumentException.forArgument(contentModelDto);
