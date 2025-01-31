9be5878d1e1fc1d6f3bc3ca7c9f3487b349c4275
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-31 14:27:12 +0100

    Update ISchemaWriter

--- Source/ch/nolix/systemapi/rawschemaapi/adapterapi/ISchemaWriter.java
+++ Source/ch/nolix/systemapi/rawschemaapi/adapterapi/ISchemaWriter.java
@@ -17,7 +17,7 @@ public interface ISchemaWriter extends IResettableChangeSaver {
 
   void setColumnName(String tableName, String columnName, String newColumnName);
 
-  void setColumnContentModel(String columnId, IContentModelDto parameterizedfieldType);
+  void setColumnContentModel(String columnId, IContentModelDto contentModelDto);
 
   void setTableName(String tableName, String newTableName);
 }
