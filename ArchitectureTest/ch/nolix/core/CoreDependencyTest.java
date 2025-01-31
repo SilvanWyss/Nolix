db95c6b96b6a4bf408bb7ec9c89fdc77838eab0f
Silvan Wyss <silvan.wyss@gmail.com>
2025-01-31 16:40:25 +0100

    Update CoreDependencyTest

--- ArchitectureTest/ch/nolix/core/CoreDependencyTest.java
+++ ArchitectureTest/ch/nolix/core/CoreDependencyTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 
-final class CoreArchitectureTest {
+final class CoreDependencyTest {
 
   @Test
   void testCase_chnolixcore_package() {
 
+    //setup
     final var rule = //
     ArchRuleDefinition
       .classes()
@@ -20,9 +21,9 @@ final class CoreArchitectureTest {
       .should()
       .onlyDependOnClassesThat()
       .resideInAnyPackage("ch.nolix.core..", "ch.nolix.coreapi..", "io.netty..", "java..", "javax..");
-
     final var testUnit = new ClassFileImporter().importPackages("ch.nolix.core..");
 
+    //execution & verification
     rule.check(testUnit);
   }
 }
