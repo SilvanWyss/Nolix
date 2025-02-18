97026d6880c9e82ccc511b599dd8b351fee8c23a
Silvan Wyss <silvan.wyss@gmail.com>
2025-02-18 21:51:36 +0100

    Fix EntityUpdateDto

--- Source/ch/nolix/systemapi/rawdataapi/modelapi/EntityUpdateDto.java
+++ Source/ch/nolix/systemapi/rawdataapi/modelapi/EntityUpdateDto.java
@@ -1,14 +1,9 @@
 package ch.nolix.systemapi.rawdataapi.modelapi;
 
-import ch.nolix.core.container.immutablelist.ImmutableList;
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
 
 public record EntityUpdateDto(
 String id,
 String saveStamp,
 IContainer<StringContentFieldDto> updatedContentFields) {
-
-  public EntityUpdateDto(String id, String saveStamp, StringContentFieldDto updatedContentField) {
-    this(id, saveStamp, ImmutableList.withElement(updatedContentField));
-  }
 }
