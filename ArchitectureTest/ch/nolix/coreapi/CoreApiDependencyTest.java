9102136b0a4dfe6976376f93b16bdba572633445
Silvan Wyss <silvan.wyss@gmail.com>
2025-02-18 22:34:42 +0100

    Create CoreApiDependencyTest

---
+++ ArchitectureTest/ch/nolix/coreapi/CoreApiDependencyTest.java
@@ -0,0 +1,29 @@
+package ch.nolix.coreapi;
+
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+
+final class CoreApiDependencyTest {
+
+  @Test
+  void testCase_chnolixcore_package() {
+
+    //setup
+    final var rule = //
+    ArchRuleDefinition
+      .classes()
+      .that()
+      .resideInAPackage("ch.nolix.coreapi..")
+      .and()
+      .haveNameNotMatching(".*Test$")
+      .should()
+      .onlyDependOnClassesThat()
+      .resideInAnyPackage("ch.nolix.coreapi..", "java..");
+    final var testUnit = new ClassFileImporter().importPackages("ch.nolix.coreapi..");
+
+    //execution & verification
+    rule.check(testUnit);
+  }
+}
