e597d1372c0886000c16e5c35ffe77c4bce107c3
Silvan Wyss <silvan.wyss@gmail.com>
2025-02-28 14:17:40 +0100

    Rename Content to AbstractContent

--- Source/ch/nolix/application/relationaldoc/backend/datamodel/AbstractContent.java
+++ Source/ch/nolix/application/relationaldoc/backend/datamodel/AbstractContent.java
@@ -3,5 +3,5 @@ package ch.nolix.application.relationaldoc.backend.datamodel;
 import ch.nolix.applicationapi.relationaldocapi.backendapi.datamodelapi.IContent;
 import ch.nolix.system.objectdata.model.Entity;
 
-public abstract class Content extends Entity implements IContent {
+public abstract class AbstractContent extends Entity implements IContent {
 }
--- Source/ch/nolix/application/relationaldoc/backend/datamodel/ReferenceContent.java
+++ Source/ch/nolix/application/relationaldoc/backend/datamodel/ReferenceContent.java
@@ -3,7 +3,7 @@ package ch.nolix.application.relationaldoc.backend.datamodel;
 import ch.nolix.applicationapi.relationaldocapi.backendapi.datamodelapi.IReferenceContent;
 import ch.nolix.coreapi.datamodelapi.fieldproperty.ContentType;
 
-public abstract class ReferenceContent extends Content implements IReferenceContent {
+public abstract class ReferenceContent extends AbstractContent implements IReferenceContent {
 
   @Override
   public final ContentType getContentType() {
