cfba26f1b8ac6b82af3a5f1d91556b2cfbd35b5c
Silvan Wyss <silvan.wyss@gmail.com>
2025-03-02 13:02:56 +0100

    Create ColumnNodeComponentMapper

--- Source/ch/nolix/system/noderawschema/nodemapper/ColumnNodeMapper.java
+++ Source/ch/nolix/system/noderawschema/nodemapper/ColumnNodeMapper.java
@@ -4,7 +4,6 @@ import ch.nolix.core.document.node.Node;
 import ch.nolix.coreapi.documentapi.nodeapi.INode;
 import ch.nolix.systemapi.noderawschemaapi.databasestructureapi.NodeHeaderCatalog;
 import ch.nolix.systemapi.noderawschemaapi.nodemapperapi.IColumnNodeMapper;
-import ch.nolix.systemapi.noderawschemaapi.nodemapperapi.IContentModelNodeMapper;
 import ch.nolix.systemapi.rawschemaapi.modelapi.ColumnDto;
 
 /**
@@ -13,30 +12,16 @@ import ch.nolix.systemapi.rawschemaapi.modelapi.ColumnDto;
  */
 public final class ColumnNodeMapper implements IColumnNodeMapper {
 
-  private static final IContentModelNodeMapper CONTENT_MODEL_NODE_MAPPER = new ContentModelNodeMapper();
-
   /**
    * {@inheritDoc}
    */
   @Override
   public INode<?> mapColumnDtoToColumnNode(final ColumnDto columnDto) {
-    return //
-    Node.withHeaderAndChildNode(
-      NodeHeaderCatalog.COLUMN,
-      mapColumnDtoToIdNode(columnDto),
-      mapColumnDtoToNameNode(columnDto),
-      mapColunDtoToContentModelNode(columnDto));
-  }
 
-  private Node mapColumnDtoToIdNode(final ColumnDto column) {
-    return Node.withHeaderAndChildNode(NodeHeaderCatalog.ID, column.id());
-  }
+    final var idNode = ColumnNodeComponentMapper.mapColumnDtoToIdNode(columnDto);
+    final var nameNode = ColumnNodeComponentMapper.mapColumnDtoToNameNode(columnDto);
+    final var contentModelNode = ColumnNodeComponentMapper.mapColunDtoToContentModelNode(columnDto);
 
-  private Node mapColumnDtoToNameNode(final ColumnDto column) {
-    return Node.withHeaderAndChildNode(NodeHeaderCatalog.NAME, column.name());
-  }
-
-  private INode<?> mapColunDtoToContentModelNode(final ColumnDto column) {
-    return CONTENT_MODEL_NODE_MAPPER.mapContentModelDtoToNode(column.contentModel());
+    return Node.withHeaderAndChildNode(NodeHeaderCatalog.COLUMN, idNode, nameNode, contentModelNode);
   }
 }
