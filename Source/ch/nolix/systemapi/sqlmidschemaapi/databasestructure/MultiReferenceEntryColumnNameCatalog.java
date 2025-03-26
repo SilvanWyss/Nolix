f34e5c02f077b63b8047ddc11db434c5c610847d
Silvan Wyss <silvan.wyss@gmail.com>
2025-03-26 22:46:39 +0100

    Create MultiReferenceEntryColumnNameCatalog

---
+++ Source/ch/nolix/systemapi/sqlmidschemaapi/databasestructure/MultiReferenceEntryColumnNameCatalog.java
@@ -0,0 +1,26 @@
+package ch.nolix.systemapi.sqlmidschemaapi.databasestructure;
+
+/**
+ * Of the {@link MultiReferenceEntryColumnNameCatalog} an instance cannot be
+ * created.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-03-26
+ */
+public final class MultiReferenceEntryColumnNameCatalog {
+
+  public static final String MULTI_REFERENCE_COLUMN_ID = "MultiReferenceColumnId";
+
+  public static final String ENTITY_ID = "EntityId";
+
+  public static final String REFERENCED_ENTITY_ID = "ReferencedEntityId";
+
+  public static final String REFERENCED_ENTITY_TABLE_ID = "ReferencedEntityTableId";
+
+  /**
+   * Prevents that an instance of the {@link MultiReferenceEntryColumnNameCatalog}
+   * can be created.
+   */
+  private MultiReferenceEntryColumnNameCatalog() {
+  }
+}
--- Source/ch/nolix/systemapi/sqlmidschemaapi/databasestructure/MultiReferenceEntryTableColumn.java
+++ Source/ch/nolix/systemapi/sqlmidschemaapi/databasestructure/MultiReferenceEntryTableColumn.java
@@ -3,10 +3,10 @@ package ch.nolix.systemapi.sqlmidschemaapi.databasestructure;
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.INameHolder;
 
 public enum MultiReferenceEntryTableColumn implements INameHolder {
-  MULTI_REFERENCE_COLUMN_ID("MultiReferenceColumnId"),
-  ENTITY_ID("EntityId"),
-  REFERENCED_ENTITY_ID("ReferencedEntityId"),
-  REFERENCED_ENTITY_TABLE_ID("ReferencedEntityTableId");
+  MULTI_REFERENCE_COLUMN_ID(MultiReferenceEntryColumnNameCatalog.MULTI_REFERENCE_COLUMN_ID),
+  ENTITY_ID(MultiReferenceEntryColumnNameCatalog.ENTITY_ID),
+  REFERENCED_ENTITY_ID(MultiReferenceEntryColumnNameCatalog.REFERENCED_ENTITY_ID),
+  REFERENCED_ENTITY_TABLE_ID(MultiReferenceEntryColumnNameCatalog.REFERENCED_ENTITY_TABLE_ID);
 
   private final String name;
 
