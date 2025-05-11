76b6c2c015e77bfbfd28e959414855ef2092dc76
Silvan Wyss <silvan.wyss@gmail.com>
2025-05-11 20:15:05 +0200

    Delete InternalSchemaWriter

--- Source/ch/nolix/system/sqlmidschema/schemawriter/SchemaWriter.java
+++ Source/ch/nolix/system/sqlmidschema/schemawriter/SchemaWriter.java
@@ -1,54 +1,51 @@
 package ch.nolix.system.sqlmidschema.schemawriter;
 
-import ch.nolix.core.errorcontrol.validator.Validator;
 import ch.nolix.core.programcontrol.closepool.CloseController;
-import ch.nolix.core.resourcecontrol.resourcevalidator.ResourceValidator;
 import ch.nolix.core.sql.sqltool.SqlCollector;
 import ch.nolix.coreapi.containerapi.baseapi.IContainer;
-import ch.nolix.coreapi.programatomapi.variableapi.LowerCaseVariableCatalog;
 import ch.nolix.coreapi.resourcecontrolapi.resourceclosingapi.ICloseController;
 import ch.nolix.coreapi.sqlapi.connectionapi.ISqlConnection;
+import ch.nolix.system.sqlmidschema.sqlschemamodelmapper.SqlSchemaColumnDtoMapper;
+import ch.nolix.system.sqlmidschema.sqlschemamodelmapper.SqlSchemaTableDtoMapper;
 import ch.nolix.system.time.moment.IncrementalCurrentTimeCreator;
 import ch.nolix.systemapi.midschemaapi.adapterapi.ISchemaWriter;
 import ch.nolix.systemapi.midschemaapi.modelapi.ColumnDto;
 import ch.nolix.systemapi.midschemaapi.modelapi.IContentModelDto;
 import ch.nolix.systemapi.midschemaapi.modelapi.TableDto;
+import ch.nolix.systemapi.sqlmidschemaapi.sqlschemamodelmapperapi.ISqlSchemaColumnDtoMapper;
+import ch.nolix.systemapi.sqlmidschemaapi.sqlschemamodelmapperapi.ISqlSchemaTableDtoMapper;
 import ch.nolix.systemapi.timeapi.momentapi.IIncrementalCurrentTimeCreator;
 
 public final class SchemaWriter implements ISchemaWriter {
 
-  private static final IIncrementalCurrentTimeCreator INCREMENTAL_CURRENT_TIME_CREATOR = //
-  new IncrementalCurrentTimeCreator();
+  private static final ISqlSchemaTableDtoMapper SQL_SCHEMA_TABLE_DTO_MAPPER = new SqlSchemaTableDtoMapper();
+
+  private static final ISqlSchemaColumnDtoMapper SQL_SCHEMA_COLUMN_DTO_MAPPER = new SqlSchemaColumnDtoMapper();
+
+  private static final IIncrementalCurrentTimeCreator INCREMENTAL_CURRENT_TIME_CREATOR = new IncrementalCurrentTimeCreator();
 
   private final ICloseController closeController = CloseController.forElement(this);
 
-  private final String databaseName;
-
-  private int saveCount;
-
   private final MetaDataWriter metaDataWriter;
 
-  private final InternalSchemaWriter internalSchemaWriter;
+  private final ch.nolix.systemapi.sqlschemaapi.adapterapi.ISchemaWriter sqlSchemaWriter;
 
   private final SqlCollector sqlCollector = new SqlCollector();
 
   private final ISqlConnection sqlConnection;
 
+  private int saveCount;
+
   public SchemaWriter(final String databaseName, final ISqlConnection sqlConnection) {
 
-    Validator.assertThat(databaseName).thatIsNamed(LowerCaseVariableCatalog.DATABASE_NAME).isNotBlank();
-    ResourceValidator.assertIsOpen(sqlConnection);
-
-    this.databaseName = databaseName;
     this.sqlConnection = sqlConnection;
     this.metaDataWriter = MetaDataWriter.forSqlCollector(sqlCollector);
 
-    final var sqlSchemaWriter = //
+    this.sqlSchemaWriter = //
     ch.nolix.system.sqlschema.adapter.SchemaWriter.forDatabasNameAndSqlConnection(databaseName, sqlConnection);
 
-    this.internalSchemaWriter = new InternalSchemaWriter(sqlSchemaWriter);
-
-    createCloseDependencyTo(sqlConnection);
+    createCloseDependencyTo(this.sqlConnection);
+    createCloseDependencyTo(this.sqlSchemaWriter);
   }
 
   public static SchemaWriter forDatabaseNameAndSqlConnection(
@@ -60,7 +57,7 @@ public final class SchemaWriter implements ISchemaWriter {
   @Override
   public void addColumn(final String tableName, final ColumnDto column) {
     metaDataWriter.addColumn(tableName, column);
-    internalSchemaWriter.addColumn(tableName, column);
+    sqlSchemaWriter.addColumn(tableName, SQL_SCHEMA_COLUMN_DTO_MAPPER.mapColumnDtoToSqlSchemaColumnDto(column));
   }
 
   @Override
@@ -73,19 +70,19 @@ public final class SchemaWriter implements ISchemaWriter {
   @Override
   public void addTable(final TableDto table) {
     metaDataWriter.addTable(table);
-    internalSchemaWriter.addTable(table);
+    sqlSchemaWriter.addTable(SQL_SCHEMA_TABLE_DTO_MAPPER.mapTableDtoSqlSchemaTableDto(table));
   }
 
   @Override
   public void deleteColumn(final String tableName, final String columnName) {
     metaDataWriter.deleteColumn(tableName, columnName);
-    internalSchemaWriter.deleteColumn(tableName, columnName);
+    sqlSchemaWriter.deleteColumn(tableName, columnName);
   }
 
   @Override
   public void deleteTable(final String tableName) {
     metaDataWriter.deleteTable(tableName);
-    internalSchemaWriter.deleteTable(tableName);
+    sqlSchemaWriter.deleteTable(tableName);
   }
 
   @Override
@@ -109,20 +106,28 @@ public final class SchemaWriter implements ISchemaWriter {
   }
 
   @Override
+  public void renameColumn(final String tableName, final String columnName, final String newColumnName) {
+    metaDataWriter.renameColumn(tableName, columnName, newColumnName);
+    sqlSchemaWriter.renameColumn(tableName, columnName, newColumnName);
+  }
+
+  @Override
+  public void renameTable(final String tableName, final String newTableName) {
+    metaDataWriter.renameTable(tableName, newTableName);
+    sqlSchemaWriter.renameTable(tableName, newTableName);
+  }
+
+  @Override
   public void reset() {
     sqlCollector.clear();
-    internalSchemaWriter.reset();
   }
 
   @Override
   public void saveChanges() {
     try {
-
-      sqlConnection.executeStatement("USE " + databaseName);
       metaDataWriter.setSchemaTimestamp(INCREMENTAL_CURRENT_TIME_CREATOR.getCurrentTime());
-      sqlCollector.addSqlStatements(internalSchemaWriter.getSqlStatements());
+      sqlSchemaWriter.addAdditionalSqlStatements(sqlCollector.getSqlStatements());
       sqlCollector.executeAndClearUsingConnection(sqlConnection);
-
       saveCount++;
     } finally {
       reset();
@@ -130,20 +135,7 @@ public final class SchemaWriter implements ISchemaWriter {
   }
 
   @Override
-  public void renameColumn(final String tableName, final String columnName, final String newColumnName) {
-    metaDataWriter.renameColumn(tableName, columnName, newColumnName);
-    internalSchemaWriter.renameColumn(tableName, columnName, newColumnName);
-  }
-
-  @Override
   public void setContentModel(final String tableName, final String columnName, final IContentModelDto contentModel) {
     metaDataWriter.setContentModel(tableName, columnName, contentModel);
-
-  }
-
-  @Override
-  public void renameTable(final String tableName, final String newTableName) {
-    metaDataWriter.renameTable(tableName, newTableName);
-    internalSchemaWriter.renameTable(tableName, newTableName);
   }
 }
