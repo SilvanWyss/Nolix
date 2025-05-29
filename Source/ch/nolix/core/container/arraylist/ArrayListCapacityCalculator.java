33f4e9c5c9f63be638ce1a44908e9265688ec72a
Silvan Wyss <silvan.wyss@gmail.com>
2025-05-29 20:44:39 +0200

    Update ArrayListCapacityCalculator

--- Source/ch/nolix/core/container/arraylist/ArrayListCapacityCalculator.java
+++ Source/ch/nolix/core/container/arraylist/ArrayListCapacityCalculator.java
@@ -3,7 +3,6 @@ package ch.nolix.core.container.arraylist;
 import ch.nolix.core.errorcontrol.invalidargumentexception.SmallerArgumentException;
 import ch.nolix.core.errorcontrol.validator.Validator;
 import ch.nolix.core.math.main.Calculator;
-import ch.nolix.coreapi.containerapi.listapi.IArrayList;
 import ch.nolix.coreapi.mathapi.numberapi.IntCatalog;
 
 /**
@@ -15,8 +14,8 @@ public final class ArrayListCapacityCalculator {
   /**
    * @param capacity
    * @param requiredCapacity
-   * @return true if a {@link IArrayList} with the given capacity needs to grow to
-   *         reach the given requiredCapacity, false otherwise.
+   * @return true if an array list with the given capacity needs to grow to reach
+   *         the given requiredCapacity, false otherwise.
    */
   public boolean arrayListNeedsToGrowForRequiredCapacity(final int capacity, final int requiredCapacity) {
     return (requiredCapacity > capacity);
@@ -25,31 +24,31 @@ public final class ArrayListCapacityCalculator {
   /**
    * @param actualCapacity
    * @param requiredCapacity
-   * @return the target capacity for an array with the given actualCapacity and
-   *         the given requiredCapacity.
+   * @return the target capacity for an array list with the given actualCapacity
+   *         and the given requiredCapacity.
    * @throws SmallerArgumentException if the given requiredCapacity is not bigger
    *                                  or does not equal the given actualCapacity.
    */
   public int calculateTargetCapacityForActualCapacityAndRequiredCapacity(
     final int actualCapacity,
     final int requiredCapacity) {
-  
+
     Validator.assertThat(actualCapacity).thatIsNamed("actial capacity").isNotNegative();
     Validator.assertThat(requiredCapacity).thatIsNamed("required capacity").isNotNegative();
-  
+
     if (actualCapacity > requiredCapacity) {
-  
+
       if (actualCapacity / 3 > requiredCapacity) {
         return (2 * requiredCapacity);
       }
-  
+
       return actualCapacity;
     }
-  
+
     if (requiredCapacity > IntCatalog.BILLION) {
       return Integer.MAX_VALUE;
     }
-  
+
     return Calculator.getMax(requiredCapacity, 2 * actualCapacity);
   }
 }
