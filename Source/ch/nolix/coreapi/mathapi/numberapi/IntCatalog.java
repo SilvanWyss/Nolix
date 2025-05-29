8d7543fc2eb85a7a8f5b35cb31f775657e78f7e6
Silvan Wyss <silvan.wyss@gmail.com>
2025-05-29 18:08:04 +0200

    Create IntCatalog

---
+++ Source/ch/nolix/coreapi/mathapi/numberapi/IntCatalog.java
@@ -0,0 +1,24 @@
+package ch.nolix.coreapi.mathapi.numberapi;
+
+/**
+ * The {@link IntCatalog} provides int constants.
+ * 
+ * Of the {@link IntCatalog} an instance cannot be created.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-05-29
+ */
+public final class IntCatalog {
+
+  public static final int THOUSAND = 1_000;
+
+  public static final int MILLION = 1_000_000;
+
+  public static final int BILLION = 1_000_000_000;
+
+  /**
+   * Prevents that an instance of the {@link IntCatalog} can be created.
+   */
+  private IntCatalog() {
+  }
+}
