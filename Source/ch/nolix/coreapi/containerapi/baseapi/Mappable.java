d0fbff076727a4011530f1c64a61c9ce92eb6366
Silvan Wyss <silvan.wyss@gmail.com>
2025-05-30 14:51:18 +0200

    Rename IMappingProvider to Mappable

--- Source/ch/nolix/coreapi/containerapi/baseapi/Mappable.java
+++ Source/ch/nolix/coreapi/containerapi/baseapi/Mappable.java
@@ -6,19 +6,19 @@ import java.util.function.Function;
 /**
  * @author Silvan Wyss
  * @version 2023-10-14
- * @param <E> is the type of the elements a {@link IMappingProvider}.
+ * @param <E> is the type of the elements a {@link Mappable}.
  */
-public interface IMappingProvider<E> {
+public interface Mappable<E> {
 
   /**
    * @param mapper
    * @param <E2>   is the type of the elements the given mapper maps from the
-   *               elements of the current {@link IMappingProvider}.
+   *               elements of the current {@link Mappable}.
    * @return a new {@link IContainer} with the elements the given mapper maps from
-   *         the elements of the current {@link IMappingProvider}.
+   *         the elements of the current {@link Mappable}.
    * @throws RuntimeException if the given mapper is null.
    * @throws RuntimeException if one of the elements of the current
-   *                          {@link IMappingProvider} is null.
+   *                          {@link Mappable} is null.
    */
   <E2> IContainer<E2> to(Function<E, E2> mapper);
 
@@ -26,22 +26,22 @@ public interface IMappingProvider<E> {
    * @param multipleMapper
    * @param <E2>           is the type of the elements of the {@link IContainer}s
    *                       the given multipleMapper maps from the elements of the
-   *                       current {@link IMappingProvider}.
+   *                       current {@link Mappable}.
    * @return a new {@link IContainer} with the elements of the {@link IContainer}s
    *         the given multipleMapper maps from the elements of the current
-   *         {@link IMappingProvider}.
+   *         {@link Mappable}.
    * @throws RuntimeException if the given multipleMapper is null.
    * @throws RuntimeException if one of the elements of the current
-   *                          {@link IMappingProvider} is null.
+   *                          {@link Mappable} is null.
    */
   <E2> IContainer<E2> toMultiples(Function<E, IContainer<E2>> multipleMapper);
 
   /**
    * @param numberMapper
    * @param <N>          is the type of the {@link Number}s the given numberMapper
-   *                     maps from the elements of the current {@link IMappingProvider}.
+   *                     maps from the elements of the current {@link Mappable}.
    * @return a new {@link IContainer} with the {@link Number}s the given
-   *         numberMapper maps from the elements of the current {@link IMappingProvider}.
+   *         numberMapper maps from the elements of the current {@link Mappable}.
    *         Maps null elements to 0.0.
    * @throws RuntimeException if the given numberMapper is null.
    */
@@ -56,14 +56,14 @@ public interface IMappingProvider<E> {
   /**
    * @param mapper
    * @param <E2>   is the type of the elements the given mapper maps from the
-   *               elements of the current {@link IMappingProvider} and from the one-based
+   *               elements of the current {@link Mappable} and from the one-based
    *               index of these elements.
    * @return a new {@link IContainer} with the elements the given mapper maps from
-   *         the elements of the current {@link IMappingProvider} and from the one-based
+   *         the elements of the current {@link Mappable} and from the one-based
    *         index of these elements.
    * @throws RuntimeException if the given mapper is null.
    * @throws RuntimeException if one of the elements of the current
-   *                          {@link IMappingProvider} is null.
+   *                          {@link Mappable} is null.
    */
   <E2> IContainer<E2> toWithOneBasedIndex(BiFunction<Integer, E, E2> mapper);
 }
