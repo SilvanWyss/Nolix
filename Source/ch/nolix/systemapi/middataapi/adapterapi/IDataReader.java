c4c5de07e36da663380a628529b3be588cef440c
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-01 15:28:38 +0200

    Edit IDataReader

--- Source/ch/nolix/systemapi/middataapi/adapterapi/IDataReader.java
+++ Source/ch/nolix/systemapi/middataapi/adapterapi/IDataReader.java
@@ -6,12 +6,33 @@ import ch.nolix.coreapi.resourcecontrolapi.resourceclosingapi.GroupCloseable;
 import ch.nolix.systemapi.middataapi.modelapi.EntityLoadingDto;
 import ch.nolix.systemapi.timeapi.momentapi.ITime;
 
+/**
+ * @author Silvan Wyss
+ * @version 2021-09-18
+ */
 public interface IDataReader extends GroupCloseable, IDatabaseNameHolder {
 
+  /**
+   * @return the schema timestamp from the database.
+   * @throws RuntimeException if the current {@link IDataReader} is closed.
+   */
   ITime getSchemaTimestamp();
 
+  /**
+   * @param tableName
+   * @return the entities, that are in the table with the given tableName, from
+   *         the database.
+   * @throws RuntimeException if the current {@link IDataReader} is closed.
+   */
   IContainer<EntityLoadingDto> loadEntities(String tableName);
 
+  /**
+   * @param tableName
+   * @param id
+   * @return the entity, that is in the table with the given tableName and has the
+   *         given id, from the database.
+   * @throws RuntimeException if the current {@link IDataReader} is closed.
+   */
   EntityLoadingDto loadEntity(String tableName, String id);
 
   IContainer<String> loadMultiBackReferenceEntries(
