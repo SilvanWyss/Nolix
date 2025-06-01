82be0664f8af21d764102c36976970b5d7f5dbdf
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-01 14:06:27 +0200

    Make IDataAdapter a IDatabaseNameHolder

--- Source/ch/nolix/systemapi/objectdataapi/adapterapi/IDataAdapter.java
+++ Source/ch/nolix/systemapi/objectdataapi/adapterapi/IDataAdapter.java
@@ -1,13 +1,12 @@
 package ch.nolix.systemapi.objectdataapi.adapterapi;
 
+import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.IDatabaseNameHolder;
 import ch.nolix.coreapi.programcontrolapi.savecontrolapi.IResettableChangeSaver;
 import ch.nolix.coreapi.structurecontrolapi.copierapi.EmptyCopyable;
 import ch.nolix.systemapi.objectdataapi.modelapi.IEntity;
 import ch.nolix.systemapi.objectdataapi.modelapi.ITable;
 
-public interface IDataAdapter extends EmptyCopyable<IDataAdapter>, IResettableChangeSaver {
-
-  String getDatabaseName();
+public interface IDataAdapter extends EmptyCopyable<IDataAdapter>, IDatabaseNameHolder, IResettableChangeSaver {
 
   <E extends IEntity> ITable<E> getStoredTableByEntityType(Class<E> entityType);
 
