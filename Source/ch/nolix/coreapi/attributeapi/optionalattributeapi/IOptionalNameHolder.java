c0f7792f8a8bfc9906fb63b10f504b4e321ef5e7
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-01 13:08:19 +0200

    Update IOptionalNameHolder

--- Source/ch/nolix/coreapi/attributeapi/optionalattributeapi/IOptionalNameHolder.java
+++ Source/ch/nolix/coreapi/attributeapi/optionalattributeapi/IOptionalNameHolder.java
@@ -13,27 +13,34 @@ public interface IOptionalNameHolder {
 
   /**
    * @return the name of the current {@link IOptionalNameHolder}.
+   * @throws RuntimeException if the current {@link IOptionalNameHolder} does not
+   *                          have a name.
    */
   String getName();
 
   /**
    * @return the name of the current {@link IOptionalNameHolder} in quotes.
+   * @throws RuntimeException if the current {@link IOptionalNameHolder} does not
+   *                          have a name.
    */
   default String getNameInQuotes() {
     return ("'" + getName() + "'");
   }
 
   /**
-   * @return true if the current {@link IOptionalNameHolder} has a name.
+   * @return true if the current {@link IOptionalNameHolder} has a name, false
+   *         otherwise.
    */
   boolean hasName();
 
   /**
    * @param name
-   * @return true if the current {@link IOptionalNameHolder} has the given name.
+   * @return true if the current {@link IOptionalNameHolder} has the given name,
+   *         false otherwise.
    */
   default boolean hasName(final String name) {
-    return hasName()
+    return //
+    hasName()
     && getName().equals(name);
   }
 }
