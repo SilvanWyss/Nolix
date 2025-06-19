c43e75f11df768d05a61c37dbbf7ed66a6280827
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-19 16:30:20 +0200

    Edit MultiBackReferenceEntryColumn enum

--- Source/ch/nolix/systemapi/sqlmidschemaapi/databasestructure/MultiBackReferenceEntryColumn.java
+++ Source/ch/nolix/systemapi/sqlmidschemaapi/databasestructure/MultiBackReferenceEntryColumn.java
@@ -2,10 +2,15 @@ package ch.nolix.systemapi.sqlmidschemaapi.databasestructure;
 
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.INameHolder;
 
+/**
+ * @author Silvan Wyss
+ * @version 2024-02-13
+ */
 public enum MultiBackReferenceEntryColumn implements INameHolder {
   ENTITY_ID("EntityId"),
   MULTI_BACK_REFERENCE_COLUMN_ID("MultiBackReferenceColumnId"),
-  BACK_REFERENCED_ENTITY_ID("BackReferencedEntityId");
+  BACK_REFERENCED_ENTITY_ID("BackReferencedEntityId"),
+  BACK_REFERENCED_ENTITY_TABLE_ID("BackReferencedEntityTableId");
 
   private final String name;
 
