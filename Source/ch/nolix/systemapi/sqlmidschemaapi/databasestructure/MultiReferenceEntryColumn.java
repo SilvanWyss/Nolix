001983acc30c2627eb8eedc1a7d7cbadd1d17d90
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-19 16:26:07 +0200

    Edit MultiReferenceEntryColumn enum

--- Source/ch/nolix/systemapi/sqlmidschemaapi/databasestructure/MultiReferenceEntryColumn.java
+++ Source/ch/nolix/systemapi/sqlmidschemaapi/databasestructure/MultiReferenceEntryColumn.java
@@ -2,9 +2,13 @@ package ch.nolix.systemapi.sqlmidschemaapi.databasestructure;
 
 import ch.nolix.coreapi.attributeapi.mandatoryattributeapi.INameHolder;
 
+/**
+ * @author Silvan Wyss
+ * @version 2022-01-01
+ */
 public enum MultiReferenceEntryColumn implements INameHolder {
-  MULTI_REFERENCE_COLUMN_ID(MultiReferenceEntryColumnNameCatalog.MULTI_REFERENCE_COLUMN_ID),
   ENTITY_ID(MultiReferenceEntryColumnNameCatalog.ENTITY_ID),
+  MULTI_REFERENCE_COLUMN_ID(MultiReferenceEntryColumnNameCatalog.MULTI_REFERENCE_COLUMN_ID),
   REFERENCED_ENTITY_ID(MultiReferenceEntryColumnNameCatalog.REFERENCED_ENTITY_ID),
   REFERENCED_ENTITY_TABLE_ID(MultiReferenceEntryColumnNameCatalog.REFERENCED_ENTITY_TABLE_ID);
 
