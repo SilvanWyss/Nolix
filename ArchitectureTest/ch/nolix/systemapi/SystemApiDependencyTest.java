496e34ecd90c80cdcf233314606618cef2e257b4
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-22 14:54:59 +0200

    Extend SystemApiDependencyTest

--- ArchitectureTest/ch/nolix/systemapi/SystemApiDependencyTest.java
+++ ArchitectureTest/ch/nolix/systemapi/SystemApiDependencyTest.java
@@ -4,11 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 
 final class SystemApiDependencyTest {
 
   @Test
-  void testCase_chnolixsystemapi_package() {
+  void testCase_cycles() {
+
+    //setup
+    final var rule = SlicesRuleDefinition.slices().matching("ch.nolix.systemapi.(*)..").should().beFreeOfCycles();
+    final var testUnit = new ClassFileImporter().importPackages("ch.nolix.systemapi..");
+
+    //execution & verification
+    rule.check(testUnit);
+  }
+
+  @Test
+  void testCase_dependencies() {
 
     //setup
     final var rule = //
