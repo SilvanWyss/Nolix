c7fb33d51f2f4e7c4e1999bd52d088da973730c6
Silvan Wyss <silvan.wyss@gmail.com>
2025-07-11 16:44:30 +0200

    Create ConnectionSideRequestable

---
+++ Source/ch/nolix/coreapi/netapi/staterequestapi/ConnectionSideRequestable.java
@@ -0,0 +1,28 @@
+package ch.nolix.coreapi.netapi.staterequestapi;
+
+import ch.nolix.coreapi.structureapi.typemarkerapi.AllowDefaultMethodsAsDesignPattern;
+
+/**
+ * A {@link ConnectionSideRequestable} can be requested if it is on back-end or
+ * front-end.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-07-11
+ */
+@AllowDefaultMethodsAsDesignPattern
+public interface ConnectionSideRequestable {
+
+  /**
+   * @return true if the current {@link ConnectionSideRequestable} is on the
+   *         back-end, false otherwise.
+   */
+  boolean isOnBackend();
+
+  /**
+   * @return true if the current {@link ConnectionSideRequestable} is on the
+   *         front-end, false otherwise.
+   */
+  default boolean isOnFrontend() {
+    return !isOnBackend();
+  }
+}
