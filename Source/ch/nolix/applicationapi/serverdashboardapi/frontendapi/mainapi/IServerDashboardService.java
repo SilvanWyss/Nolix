71a908caf41199280e5a06892c918d340d0c1e7e
Silvan Wyss <silvan.wyss@gmail.com>
2025-07-21 21:57:15 +0200

    Delete serverdashboard package

--- Source/ch/nolix/applicationapi/serverdashboardapi/frontendapi/mainapi/IServerDashboardService.java
+++
@@ -1,9 +0,0 @@
-package ch.nolix.applicationapi.serverdashboardapi.frontendapi.mainapi;
-
-import ch.nolix.applicationapi.serverdashboardapi.backendapi.datamodelapi.IWebApplicationInfo;
-import ch.nolix.coreapi.containerapi.baseapi.IContainer;
-
-public interface IServerDashboardService {
-
-  IContainer<IWebApplicationInfo> getWebApplicationInfos();
-}
