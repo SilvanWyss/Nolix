f807138f61205945a75cab43e11e0eb5dd47220a
Silvan Wyss <silvan.wyss@gmail.com>
2025-07-21 22:46:17 +0200

    Delete applicationapi package and application package

--- Source/ch/nolix/application/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The {@link ch.nolix.application} package defines the types that are declared
- * in the {@link ch.nolix.applicationapi} package.
- * 
- * @author Silvan Wyss
- * @version 2024-12-02
- */
-package ch.nolix.application;
--- Source/ch/nolix/applicationapi/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * The {@link ch.nolix.applicationapi} package declares applications.
- * 
- * @author Silvan Wyss
- * @version 2024-12-02
- */
-package ch.nolix.applicationapi;
