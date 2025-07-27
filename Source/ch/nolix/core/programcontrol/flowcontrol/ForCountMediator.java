6c335ad198ea98039f8a52716ad07a56041326a0
Silvan Wyss <silvan.wyss@gmail.com>
2025-07-27 22:10:14 +0200

    Create IForCountMediator

--- Source/ch/nolix/core/programcontrol/flowcontrol/ForCountMediator.java
+++ Source/ch/nolix/core/programcontrol/flowcontrol/ForCountMediator.java
@@ -2,10 +2,10 @@ package ch.nolix.core.programcontrol.flowcontrol;
 
 import java.util.function.IntConsumer;
 
-import ch.nolix.core.errorcontrol.invalidargumentexception.ArgumentIsNullException;
 import ch.nolix.core.errorcontrol.invalidargumentexception.NegativeArgumentException;
 import ch.nolix.core.errorcontrol.validator.Validator;
 import ch.nolix.coreapi.programatom.variable.LowerCaseVariableCatalog;
+import ch.nolix.coreapi.programcontrol.flowcontrol.IForCountMediator;
 import ch.nolix.coreapi.programcontrol.future.IFuture;
 
 /**
@@ -14,7 +14,7 @@ import ch.nolix.coreapi.programcontrol.future.IFuture;
  * @author Silvan Wyss
  * @version 2016-06-01
  */
-public final class ForCountMediator {
+public final class ForCountMediator implements IForCountMediator {
 
   private final int maxRunCount;
 
@@ -41,11 +41,9 @@ public final class ForCountMediator {
   }
 
   /**
-   * Lets the current {@link ForCountMediator} run the given job.
-   * 
-   * @param job
-   * @throws ArgumentIsNullException if the given job is null.
+   * {@inheritDoc}
    */
+  @Override
   public void run(final Runnable job) {
 
     Validator.assertThat(job).thatIsNamed(LowerCaseVariableCatalog.JOB).isNotNull();
@@ -56,11 +54,9 @@ public final class ForCountMediator {
   }
 
   /**
-   * Lets the current {@link ForCountMediator} run the given job.
-   * 
-   * @param job
-   * @throws ArgumentIsNullException if the given job is null.
+   * {@inheritDoc}
    */
+  @Override
   public void run(final IntConsumer job) {
 
     Validator.assertThat(job).thatIsNamed(LowerCaseVariableCatalog.JOB).isNotNull();
@@ -71,12 +67,9 @@ public final class ForCountMediator {
   }
 
   /**
-   * Lets the current {@link ForCountMediator} run the given job in background.
-   * 
-   * @param job
-   * @return a new {@link IFuture} for the job running.
-   * @throws ArgumentIsNullException if the given job is null.
+   * {@inheritDoc}
    */
+  @Override
   public IFuture runInBackground(final Runnable job) {
     return new Future(new JobRunner(job, maxRunCount));
   }
---
+++ Source/ch/nolix/coreapi/programcontrol/flowcontrol/IForCountMediator.java
@@ -0,0 +1,37 @@
+package ch.nolix.coreapi.programcontrol.flowcontrol;
+
+import java.util.function.IntConsumer;
+
+import ch.nolix.coreapi.programcontrol.future.IFuture;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-07-27
+ */
+public interface IForCountMediator {
+
+  /**
+   * Lets the current {@link IForCountMediator} run the given job.
+   * 
+   * @param job
+   * @throws RuntimeException if the given job is null.
+   */
+  void run(Runnable job);
+
+  /**
+   * Lets the current {@link IForCountMediator} run the given job.
+   * 
+   * @param job
+   * @throws RuntimeException if the given job is null.
+   */
+  void run(IntConsumer job);
+
+  /**
+   * Lets the current {@link IForCountMediator} run the given job in background.
+   * 
+   * @param job
+   * @return a new {@link IFuture} for the job execution.
+   * @throws RuntimeException if the given job is null.
+   */
+  IFuture runInBackground(Runnable job);
+}
