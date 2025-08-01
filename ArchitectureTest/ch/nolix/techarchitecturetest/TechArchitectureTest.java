37da8a4ca27542f11850e35c171cfd41a9433a3b
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-01 22:26:04 +0200

    Create TechArchitectureTest

---
+++ ArchitectureTest/ch/nolix/techarchitecturetest/TechArchitectureTest.java
@@ -0,0 +1,44 @@
+package ch.nolix.techarchitecturetest;
+
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+
+final class TechArchitectureTest {
+
+  private static final JavaClasses TEST_UNIT = new ClassFileImporter().importPackages("ch.nolix.tech..");
+
+  void testCase_cycles() {
+
+    //setup
+    final var rule = SlicesRuleDefinition.slices().matching("ch.nolix.tech.(*)..").should().beFreeOfCycles();
+
+    //execution & verification
+    rule.check(TEST_UNIT);
+  }
+
+  @Test
+  void testCase_dependencies() {
+
+    //setup
+    final var rule = //
+    ArchRuleDefinition
+      .classes()
+      .should()
+      .onlyDependOnClassesThat()
+      .resideInAnyPackage(
+        "ch.nolix.core..",
+        "ch.nolix.coreapi..",
+        "ch.nolix.system..",
+        "ch.nolix.systemapi..",
+        "ch.nolix.tech..",
+        "ch.nolix.techapi..",
+        "java..");
+
+    //execution & verification
+    rule.check(TEST_UNIT);
+  }
+}
