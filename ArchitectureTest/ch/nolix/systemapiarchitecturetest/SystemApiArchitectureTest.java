45adcc031c9f993720bfc2a139bce729a1dc42be
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-01 20:36:09 +0200

    Create SystemApiArchitectureTest

--- ArchitectureTest/ch/nolix/systemapiarchitecturetest/SystemApiArchitectureTest.java
+++ ArchitectureTest/ch/nolix/systemapiarchitecturetest/SystemApiArchitectureTest.java
@@ -2,21 +2,23 @@ package ch.nolix.systemapiarchitecturetest;
 
 import org.junit.jupiter.api.Test;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 
-final class SystemApiDependencyTest {
+final class SystemApiArchitectureTest {
+
+  private static final JavaClasses TEST_UNIT = new ClassFileImporter().importPackages("ch.nolix.systemapi..");
 
   @Test
   void testCase_cycles() {
 
     //setup
     final var rule = SlicesRuleDefinition.slices().matching("ch.nolix.systemapi.(*)..").should().beFreeOfCycles();
-    final var testUnit = new ClassFileImporter().importPackages("ch.nolix.systemapi..");
 
     //execution & verification
-    rule.check(testUnit);
+    rule.check(TEST_UNIT);
   }
 
   @Test
@@ -26,16 +28,11 @@ final class SystemApiDependencyTest {
     final var rule = //
     ArchRuleDefinition
       .classes()
-      .that()
-      .resideInAPackage("ch.nolix.systemapi..")
-      .and()
-      .haveNameNotMatching(".*Test$")
       .should()
       .onlyDependOnClassesThat()
       .resideInAnyPackage("ch.nolix.coreapi..", "ch.nolix.systemapi..", "java..");
-    final var testUnit = new ClassFileImporter().importPackages("ch.nolix.systemapi..");
 
     //execution & verification
-    rule.check(testUnit);
+    rule.check(TEST_UNIT);
   }
 }
