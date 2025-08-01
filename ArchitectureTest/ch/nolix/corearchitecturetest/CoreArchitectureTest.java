6db133c34efd3052f3d921b13cca14e4b4a0abb2
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-01 21:12:41 +0200

    Create CoreApiArchitectureTest

---
+++ ArchitectureTest/ch/nolix/corearchitecturetest/CoreArchitectureTest.java
@@ -0,0 +1,40 @@
+package ch.nolix.corearchitecturetest;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+
+final class CoreArchitectureTest {
+
+  private static final JavaClasses TEST_UNIT = new ClassFileImporter().importPackages("ch.nolix.core..");
+
+  @Test
+  @Disabled
+  void testCase_cycles() {
+
+    //setup
+    final var rule = SlicesRuleDefinition.slices().matching("ch.nolix.core.(*)..").should().beFreeOfCycles();
+
+    //execution & verification
+    rule.check(TEST_UNIT);
+  }
+
+  @Test
+  void testCase_dependencies() {
+
+    //setup
+    final var rule = //
+    ArchRuleDefinition
+      .classes()
+      .should()
+      .onlyDependOnClassesThat()
+      .resideInAnyPackage("ch.nolix.core..", "ch.nolix.coreapi..", "io.netty..", "java..", "javax..");
+
+    //execution & verification
+    rule.check(TEST_UNIT);
+  }
+}
