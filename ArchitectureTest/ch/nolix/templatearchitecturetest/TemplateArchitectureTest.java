8e47b6767a9068b4d8c433c36c91733bbe501565
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-01 22:33:55 +0200

    Create TemplateArchitectureTest

---
+++ ArchitectureTest/ch/nolix/templatearchitecturetest/TemplateArchitectureTest.java
@@ -0,0 +1,46 @@
+package ch.nolix.templatearchitecturetest;
+
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+
+final class TemplateArchitectureTest {
+
+  private static final JavaClasses TEST_UNIT = new ClassFileImporter().importPackages("ch.nolix.template..");
+
+  @Test
+  void testCase_cycles() {
+
+    //setup
+    final var rule = SlicesRuleDefinition.slices().matching("ch.nolix.template.(*)..").should().beFreeOfCycles();
+
+    //execution & verification
+    rule.check(TEST_UNIT);
+  }
+
+  @Test
+  void testCase_dependencies() {
+
+    //setup
+    final var rule = //
+    ArchRuleDefinition
+      .classes()
+      .should()
+      .onlyDependOnClassesThat()
+      .resideInAnyPackage(
+        "ch.nolix.core..",
+        "ch.nolix.coreapi..",
+        "ch.nolix.system..",
+        "ch.nolix.systemapi..",
+        "ch.nolix.tech..",
+        "ch.nolix.techapi..",
+        "ch.nolix.template..",
+        "java..");
+
+    //execution & verification
+    rule.check(TEST_UNIT);
+  }
+}
