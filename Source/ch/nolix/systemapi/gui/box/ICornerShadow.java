33b9aff6f145da4b138367bf971da1beabb0a1c7
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-09 21:02:11 +0200

    Create ICornerShadow

---
+++ Source/ch/nolix/systemapi/gui/box/ICornerShadow.java
@@ -0,0 +1,41 @@
+package ch.nolix.systemapi.gui.box;
+
+import ch.nolix.systemapi.graphic.color.IColor;
+import ch.nolix.systemapi.gui.location.Location;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-08-09
+ */
+public interface ICornerShadow {
+
+  /**
+   * @return the blur radius of the current {ICornerShadow}.
+   */
+  int getBlurRadius();
+
+  /**
+   * @return the color of the current {ICornerShadow}.
+   */
+  IColor getColor();
+
+  /**
+   * @return the corner of the current {ICornerShadow}.
+   */
+  Corner getCorner();
+
+  /**
+   * @return the location of the current {ICornerShadow}.
+   */
+  Location getLocation();
+
+  /**
+   * @return the side 1 thickness of the current {ICornerShadow}.
+   */
+  int getSide1Thickness();
+
+  /**
+   * @return the side 2 thickness of the current {ICornerShadow}.
+   */
+  int getSide2Thickness();
+}
