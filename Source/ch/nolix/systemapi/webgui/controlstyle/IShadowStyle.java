b5f2aea9c616b2525fcd3b571f547f1ac6b5acf6
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-10 15:29:33 +0200

    Create IShadowStyle

---
+++ Source/ch/nolix/systemapi/webgui/controlstyle/IShadowStyle.java
@@ -0,0 +1,16 @@
+package ch.nolix.systemapi.webgui.controlstyle;
+
+import ch.nolix.coreapi.container.base.IContainer;
+import ch.nolix.systemapi.gui.box.ICornerShadow;
+import ch.nolix.systemapi.webgui.main.ControlState;
+
+public interface IShadowStyle<S extends IShadowStyle<S>> {
+
+  IContainer<? extends ICornerShadow> getCornerShadowsWhenHasState(ControlState state);
+
+  void removeCustomCornerShadows();
+
+  S setCornerShadowForState(ControlState state, ICornerShadow cornerShadow, ICornerShadow... cornerShadows);
+
+  S setCornerShadowsForState(ControlState state, IContainer<? extends ICornerShadow> cornerShadows);
+}
