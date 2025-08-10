998647fdc07a59575e6d3fc6edf1e299281a3c0f
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-10 19:11:29 +0200

    Add mapCornerShadowsToOptionalCssProperty method to ICornerShadowToCssMapper

--- Source/ch/nolix/system/gui/cssmapper/CornerShadowToCssMapper.java
+++ Source/ch/nolix/system/gui/cssmapper/CornerShadowToCssMapper.java
@@ -1,5 +1,10 @@
 package ch.nolix.system.gui.cssmapper;
 
+import java.util.Optional;
+
+import ch.nolix.core.web.cssmodel.CssProperty;
+import ch.nolix.coreapi.container.base.IContainer;
+import ch.nolix.coreapi.web.css.CssPropertyNameCatalog;
 import ch.nolix.coreapi.web.cssmodel.ICssProperty;
 import ch.nolix.systemapi.gui.box.ICornerShadow;
 import ch.nolix.systemapi.gui.cssmapper.ICornerShadowToCssMapper;
@@ -24,5 +29,23 @@ public final class CornerShadowToCssMapper implements ICornerShadowToCssMapper {
       case INSIDE ->
         CornerShadowToCssMapperHelper.mapCornerShadowToCssPropertyWhenShadowIsInsideTheBox(cornerShadow);
     };
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Optional<ICssProperty> mapCornerShadowsToOptionalCssProperty(
+    final IContainer<? extends ICornerShadow> cornerShadows) {
+
+    if (cornerShadows.containsAny()) {
+      return //
+      Optional.of(
+        CssProperty.withNameAndValue(
+          CssPropertyNameCatalog.BOX_SHADOW,
+          cornerShadows.getViewOf(s -> mapCornerShadowToCssProperty(s).getValue()).toString()));
+    }
+
+    return Optional.empty();
   }
 }
--- Source/ch/nolix/systemapi/gui/cssmapper/ICornerShadowToCssMapper.java
+++ Source/ch/nolix/systemapi/gui/cssmapper/ICornerShadowToCssMapper.java
@@ -1,5 +1,8 @@
 package ch.nolix.systemapi.gui.cssmapper;
 
+import java.util.Optional;
+
+import ch.nolix.coreapi.container.base.IContainer;
 import ch.nolix.coreapi.web.cssmodel.ICssProperty;
 import ch.nolix.systemapi.gui.box.ICornerShadow;
 
@@ -15,4 +18,14 @@ public interface ICornerShadowToCssMapper {
    * @throws RuntimeException if the given cornerShadow is null.
    */
   ICssProperty mapCornerShadowToCssProperty(ICornerShadow cornerShadow);
+
+  /**
+   * @param cornerShadows
+   * @return a {@link Optional} with a new {@link ICssProperty} from the given
+   *         cornerShadows if the given cornerShadows is not empty, an empty
+   *         {@link Optional} otherwise.
+   * @throws RuntimeException if the given cornerShadows is null or one of the
+   *                          given cornerShadows is null.
+   */
+  Optional<ICssProperty> mapCornerShadowsToOptionalCssProperty(IContainer<? extends ICornerShadow> cornerShadows);
 }
