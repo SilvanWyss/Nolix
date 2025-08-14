99dd02d83e3f9b954ac49877d041b219f988ad15
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-14 20:14:44 +0200

    Create ContentModelNodeMapperHelper

--- Source/ch/nolix/system/nodemidschema/nodemapper/ContentModelNodeMapper.java
+++ Source/ch/nolix/system/nodemidschema/nodemapper/ContentModelNodeMapper.java
@@ -1,6 +1,5 @@
 package ch.nolix.system.nodemidschema.nodemapper;
 
-import ch.nolix.core.document.node.Node;
 import ch.nolix.core.errorcontrol.invalidargumentexception.InvalidArgumentException;
 import ch.nolix.coreapi.document.node.INode;
 import ch.nolix.systemapi.midschema.model.BackReferenceModelDto;
@@ -14,7 +13,6 @@ import ch.nolix.systemapi.midschema.model.OptionalReferenceModelDto;
 import ch.nolix.systemapi.midschema.model.OptionalValueModelDto;
 import ch.nolix.systemapi.midschema.model.ReferenceModelDto;
 import ch.nolix.systemapi.midschema.model.ValueModelDto;
-import ch.nolix.systemapi.nodemidschema.databasestructure.NodeHeaderCatalog;
 import ch.nolix.systemapi.nodemidschema.nodemapper.IContentModelNodeMapper;
 
 /**
@@ -41,129 +39,39 @@ public final class ContentModelNodeMapper implements IContentModelNodeMapper {
   public INode<?> mapContentModelDtoToNode(final IContentModelDto contentModelDto) {
 
     if (contentModelDto instanceof ValueModelDto valueModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          valueModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          valueModelDto.dataType().name()));
+      return ContentModelNodeMapperHelper.mapValueModelDtoToNode(valueModelDto);
     }
 
     if (contentModelDto instanceof OptionalValueModelDto optionalValueModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          optionalValueModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          optionalValueModelDto.dataType().name()));
+      return ContentModelNodeMapperHelper.mapOptionalValueModelDtoToNode(optionalValueModelDto);
     }
 
     if (contentModelDto instanceof MultiValueModelDto multiValueModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          multiValueModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          multiValueModelDto.dataType().name()));
+      return ContentModelNodeMapperHelper.mapMultiValueModelDtoToNode(multiValueModelDto);
     }
 
     if (contentModelDto instanceof ReferenceModelDto referenceModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          referenceModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          referenceModelDto.dataType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.REFERENCED_TABLE_ID,
-          referenceModelDto.referencedTableId()));
+      return ContentModelNodeMapperHelper.mapRefernceModelDtoToNode(referenceModelDto);
     }
 
     if (contentModelDto instanceof OptionalReferenceModelDto optionalReferenceModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          optionalReferenceModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          optionalReferenceModelDto.dataType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.REFERENCED_TABLE_ID,
-          optionalReferenceModelDto.referencedTableId()));
+      return ContentModelNodeMapperHelper.mapOptionalRefernceModelDtoToNode(optionalReferenceModelDto);
     }
 
     if (contentModelDto instanceof MultiReferenceModelDto multiReferenceModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          multiReferenceModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          multiReferenceModelDto.dataType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.REFERENCED_TABLE_ID,
-          multiReferenceModelDto.referencedTableId()));
+      return ContentModelNodeMapperHelper.mapMultiRefernceModelDtoToNode(multiReferenceModelDto);
     }
 
     if (contentModelDto instanceof BackReferenceModelDto backReferenceModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          backReferenceModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          backReferenceModelDto.dataType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.BACK_REFERENCED_COLUMN_ID,
-          backReferenceModelDto.backReferencedColumnId()));
+      return ContentModelNodeMapperHelper.mapBackRefernceModelDtoToNode(backReferenceModelDto);
     }
 
     if (contentModelDto instanceof OptionalBackReferenceModelDto optionalBackReferenceModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          optionalBackReferenceModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          optionalBackReferenceModelDto.dataType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.BACK_REFERENCED_COLUMN_ID,
-          optionalBackReferenceModelDto.backReferencedColumnId()));
+      return ContentModelNodeMapperHelper.mapOptionalBackRefernceModelDtoToNode(optionalBackReferenceModelDto);
     }
 
     if (contentModelDto instanceof MultiBackReferenceModelDto multiBackReferenceModelDto) {
-      return //
-      Node.withHeaderAndChildNode(
-        NodeHeaderCatalog.CONTENT_MODEL,
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.CONTENT_TYPE,
-          multiBackReferenceModelDto.getContentType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.DATA_TYPE,
-          multiBackReferenceModelDto.dataType().name()),
-        Node.withHeaderAndChildNode(
-          NodeHeaderCatalog.BACK_REFERENCED_COLUMN_ID,
-          multiBackReferenceModelDto.backReferencedColumnId()));
+      return ContentModelNodeMapperHelper.mapMultiBackRefernceModelDtoToNode(multiBackReferenceModelDto);
     }
 
     throw InvalidArgumentException.forArgument(contentModelDto);
---
+++ Source/ch/nolix/system/nodemidschema/nodemapper/ContentModelNodeMapperHelper.java
@@ -0,0 +1,146 @@
+package ch.nolix.system.nodemidschema.nodemapper;
+
+import ch.nolix.core.document.node.Node;
+import ch.nolix.systemapi.midschema.model.BackReferenceModelDto;
+import ch.nolix.systemapi.midschema.model.MultiBackReferenceModelDto;
+import ch.nolix.systemapi.midschema.model.MultiReferenceModelDto;
+import ch.nolix.systemapi.midschema.model.MultiValueModelDto;
+import ch.nolix.systemapi.midschema.model.OptionalBackReferenceModelDto;
+import ch.nolix.systemapi.midschema.model.OptionalReferenceModelDto;
+import ch.nolix.systemapi.midschema.model.OptionalValueModelDto;
+import ch.nolix.systemapi.midschema.model.ReferenceModelDto;
+import ch.nolix.systemapi.midschema.model.ValueModelDto;
+import ch.nolix.systemapi.nodemidschema.databasestructure.NodeHeaderCatalog;
+
+public final class ContentModelNodeMapperHelper {
+
+  private ContentModelNodeMapperHelper() {
+  }
+
+  public static Node mapBackRefernceModelDtoToNode(final BackReferenceModelDto backReferenceModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        backReferenceModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        backReferenceModelDto.dataType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.BACK_REFERENCED_COLUMN_ID,
+        backReferenceModelDto.backReferencedColumnId()));
+  }
+
+  public static Node mapMultiBackRefernceModelDtoToNode(final MultiBackReferenceModelDto multiBackReferenceModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        multiBackReferenceModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        multiBackReferenceModelDto.dataType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.BACK_REFERENCED_COLUMN_ID,
+        multiBackReferenceModelDto.backReferencedColumnId()));
+  }
+
+  public static Node mapMultiRefernceModelDtoToNode(final MultiReferenceModelDto multiReferenceModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        multiReferenceModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        multiReferenceModelDto.dataType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.REFERENCED_TABLE_ID,
+        multiReferenceModelDto.referencedTableId()));
+  }
+
+  public static Node mapMultiValueModelDtoToNode(final MultiValueModelDto multiValueModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        multiValueModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        multiValueModelDto.dataType().name()));
+  }
+
+  public static Node mapOptionalBackRefernceModelDtoToNode(
+    final OptionalBackReferenceModelDto optionalBackReferenceModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        optionalBackReferenceModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        optionalBackReferenceModelDto.dataType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.BACK_REFERENCED_COLUMN_ID,
+        optionalBackReferenceModelDto.backReferencedColumnId()));
+  }
+
+  public static Node mapOptionalRefernceModelDtoToNode(final OptionalReferenceModelDto optionalReferenceModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        optionalReferenceModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        optionalReferenceModelDto.dataType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.REFERENCED_TABLE_ID,
+        optionalReferenceModelDto.referencedTableId()));
+  }
+
+  public static Node mapOptionalValueModelDtoToNode(final OptionalValueModelDto optionalValueModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        optionalValueModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        optionalValueModelDto.dataType().name()));
+  }
+
+  public static Node mapRefernceModelDtoToNode(final ReferenceModelDto referenceModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        referenceModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        referenceModelDto.dataType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.REFERENCED_TABLE_ID,
+        referenceModelDto.referencedTableId()));
+  }
+
+  public static Node mapValueModelDtoToNode(final ValueModelDto valueModelDto) {
+    return //
+    Node.withHeaderAndChildNode(
+      NodeHeaderCatalog.CONTENT_MODEL,
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.CONTENT_TYPE,
+        valueModelDto.getContentType().name()),
+      Node.withHeaderAndChildNode(
+        NodeHeaderCatalog.DATA_TYPE,
+        valueModelDto.dataType().name()));
+  }
+}
