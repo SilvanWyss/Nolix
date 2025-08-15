52b2dfe11df5d442e8f680b62c76c73f2c795990
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-15 16:32:41 +0200

    Create IShortDescriptionHolder

---
+++ Source/ch/nolix/coreapi/attribute/mandatoryattribute/IShortDescriptionHolder.java
@@ -0,0 +1,23 @@
+package ch.nolix.coreapi.attribute.mandatoryattribute;
+
+/**
+ * A {@link IShortDescriptionHolder} has a short description.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-08-15
+ */
+public interface IShortDescriptionHolder {
+
+  /**
+   * @return the short description of the current {@link IShortDescriptionHolder}.
+   */
+  String getShortDescription();
+
+  /**
+   * @return the short description of the current {@link IShortDescriptionHolder}
+   *         in quotes.
+   */
+  default String getShortDescriptionInQuotes() {
+    return ("'" + getShortDescription() + "'");
+  }
+}
