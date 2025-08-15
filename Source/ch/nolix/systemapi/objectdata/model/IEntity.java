522fcf0159eb6f50bb9ca4aaff019a8096cad574
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-15 16:33:32 +0200

    Make IEntity a IShortDescriptionHolder

--- Source/ch/nolix/systemapi/objectdata/model/IEntity.java
+++ Source/ch/nolix/systemapi/objectdata/model/IEntity.java
@@ -1,6 +1,7 @@
 package ch.nolix.systemapi.objectdata.model;
 
 import ch.nolix.coreapi.attribute.mandatoryattribute.IIdHolder;
+import ch.nolix.coreapi.attribute.mandatoryattribute.IShortDescriptionHolder;
 import ch.nolix.coreapi.attribute.optionalattribute.IOptionalSaveStampHolder;
 import ch.nolix.coreapi.component.datamodelcomponent.ITableComponent;
 import ch.nolix.coreapi.container.base.IContainer;
@@ -8,11 +9,15 @@ import ch.nolix.systemapi.databaseobject.model.Deletable;
 import ch.nolix.systemapi.databaseobject.model.IDatabaseObject;
 
 public interface IEntity
-extends Deletable, IDatabaseObject, IIdHolder, IOptionalSaveStampHolder, ITableComponent<ITable<? extends IEntity>> {
+extends
+Deletable,
+IDatabaseObject,
+IIdHolder,
+IOptionalSaveStampHolder,
+IShortDescriptionHolder,
+ITableComponent<ITable<? extends IEntity>> {
 
   String getParentTableName();
-
-  String getShortDescription();
 
   IDatabase getStoredParentDatabase();
 
