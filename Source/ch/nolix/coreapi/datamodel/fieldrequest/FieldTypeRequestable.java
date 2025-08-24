b1f3249b28a34d3659066eedf102891b148e515d
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-24 21:58:43 +0200

    Rename ContentTypeRequestable to FieldTypeRequestable

---
+++ Source/ch/nolix/coreapi/datamodel/fieldrequest/FieldTypeRequestable.java
@@ -0,0 +1,32 @@
+package ch.nolix.coreapi.datamodel.fieldrequest;
+
+import ch.nolix.coreapi.datamodel.fieldproperty.FieldType;
+
+/**
+ * A {@link FieldTypeRequestable} can be asked if it either is for holding
+ * values or references.
+ * 
+ * @author Silvan Wyss
+ * @version 2023-09-08
+ */
+public interface FieldTypeRequestable {
+
+  /**
+   * @return the field type of the current {@link FieldTypeRequestable}.
+   */
+  FieldType getFieldType();
+
+  /**
+   * @return true if the current {@link FieldTypeRequestable} is for references,
+   *         false otherwise.
+   */
+  default boolean isForReferences() {
+    return !isForValues();
+  }
+
+  /**
+   * @return true if the current {@link FieldTypeRequestable} is for values, false
+   *         otherwise.
+   */
+  boolean isForValues();
+}
