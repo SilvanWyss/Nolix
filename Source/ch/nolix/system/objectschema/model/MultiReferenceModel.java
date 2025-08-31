72cd40a782186f97dc352d468effafaec5c11dc6
Silvan Wyss <silvan.wyss@gmail.com>
2025-08-31 22:23:10 +0200

    Add forReferenceableTable static method to MultiReferenceModel

--- Source/ch/nolix/system/objectschema/model/MultiReferenceModel.java
+++ Source/ch/nolix/system/objectschema/model/MultiReferenceModel.java
@@ -1,5 +1,6 @@
 package ch.nolix.system.objectschema.model;
 
+import ch.nolix.core.container.containerview.ContainerView;
 import ch.nolix.coreapi.container.base.IContainer;
 import ch.nolix.systemapi.midschema.fieldproperty.FieldType;
 import ch.nolix.systemapi.objectschema.model.IMultiReferenceModel;
@@ -9,6 +10,15 @@ public final class MultiReferenceModel extends AbstractBaseReferenceModel implem
 
   private MultiReferenceModel(final IContainer<? extends ITable> referenceableTables) {
     super(referenceableTables);
+  }
+
+  public static MultiReferenceModel forReferenceableTable(
+    final ITable referenceableTable,
+    final ITable... referenceableTables) {
+
+    final var allReferenceableTables = ContainerView.forElementAndArray(referenceableTable, referenceableTables);
+
+    return new MultiReferenceModel(allReferenceableTables);
   }
 
   public static MultiReferenceModel forReferenceableTables(final IContainer<? extends ITable> referenceableTables) {
