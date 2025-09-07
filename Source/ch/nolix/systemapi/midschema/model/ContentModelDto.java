bfbecfb2f91106b06faff9b6902b0d6960118a1a
Silvan Wyss <silvan.wyss@gmail.com>
2025-09-07 10:40:12 +0200

    Create ContentModelDto

---
+++ Source/ch/nolix/systemapi/midschema/model/ContentModelDto.java
@@ -0,0 +1,12 @@
+package ch.nolix.systemapi.midschema.model;
+
+import ch.nolix.coreapi.container.base.IContainer;
+import ch.nolix.coreapi.datamodel.fieldproperty.DataType;
+import ch.nolix.systemapi.midschema.fieldproperty.FieldType;
+
+public record ContentModelDto(
+FieldType fieldType,
+DataType dataType,
+IContainer<String> referenceableTableIds,
+IContainer<String> backReferenceableColumnIds) {
+}
