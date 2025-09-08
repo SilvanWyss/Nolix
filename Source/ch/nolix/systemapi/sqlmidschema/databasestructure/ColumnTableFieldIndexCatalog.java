d6982e9d88e56d53443f7e01e8467750d4af9b30
Silvan Wyss <silvan.wyss@gmail.com>
2025-09-08 22:06:23 +0200

    Update ColumnTableFieldIndexCatalog

--- Source/ch/nolix/systemapi/sqlmidschema/databasestructure/ColumnTableFieldIndexCatalog.java
+++ Source/ch/nolix/systemapi/sqlmidschema/databasestructure/ColumnTableFieldIndexCatalog.java
@@ -15,10 +15,6 @@ public final class ColumnTableFieldIndexCatalog {
 
   public static final int DATA_TYPE_INDEX = 5;
 
-  public static final int REFERENCED_TABLE_ID_INDEX = 6;
-
-  public static final int BACK_REFERENCED_TABLE_ID_INDEX = 7;
-
   /**
    * Prevents that an instance of the {@link ColumnTableFieldIndexCatalog} can be
    * created.
