bae0717e9809aca7d81b1ca65b9d3d687557da0d
Silvan Wyss <silvan.wyss@gmail.com>
2025-09-13 11:38:35 +0200

    Create IColumn

---
+++ Source/ch/nolix/systemapi/objectdata/model/IColumn.java
@@ -0,0 +1,25 @@
+package ch.nolix.systemapi.objectdata.model;
+
+import ch.nolix.coreapi.attribute.mandatoryattribute.IIdHolder;
+import ch.nolix.coreapi.attribute.mandatoryattribute.INameHolder;
+import ch.nolix.coreapi.container.base.IContainer;
+import ch.nolix.systemapi.databaseobject.model.IDatabaseObject;
+import ch.nolix.systemapi.midschema.fieldproperty.FieldType;
+
+public interface IColumn extends IDatabaseObject, IIdHolder, INameHolder {
+  boolean containsValueInPersistedData(String value);
+
+  boolean containsValueInPersistedDataIgnoringGivenEntities(
+    String value,
+    IContainer<String> entitiesToIgnoreIds);
+
+  Class<Object> getDataTypeClass();
+
+  FieldType getFieldType();
+
+  IContainer<? extends IColumn> getStoredBackReferenceableColumns();
+
+  ITable<IEntity> getStoredParentTable();
+
+  IContainer<? extends ITable<IEntity>> getStoredReferenceableTables();
+}
