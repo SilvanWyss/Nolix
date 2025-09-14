5e56b8a471c0ab2c6d0a45a731122b9725ee3af7
Silvan Wyss <silvan.wyss@gmail.com>
2025-09-14 19:55:31 +0200

    Beautify StandardConsoleLogHandler

--- Source/ch/nolix/core/errorcontrol/logging/StandardConsoleLogHandler.java
+++ Source/ch/nolix/core/errorcontrol/logging/StandardConsoleLogHandler.java
@@ -5,8 +5,8 @@ public final class StandardConsoleLogHandler extends AbstractLogHandler {
   protected void log(final LogEntry logEntry) {
     System.out.println(logEntry.toString()); //NOSONAR: This is a logger.
 
-    for (final var ail : logEntry.getAdditionalInfoLines()) {
-      System.out.println("  " + ail); //NOSONAR: This is a logger.
+    for (final var l : logEntry.getAdditionalInfoLines()) {
+      System.out.println("  " + l); //NOSONAR: This is a logger.
     }
   }
 }
