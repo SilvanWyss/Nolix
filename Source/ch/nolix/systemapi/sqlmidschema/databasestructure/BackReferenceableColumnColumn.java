43d06bf3e6cdc68ebf70d0311e79f2a342110eab
Silvan Wyss <silvan.wyss@gmail.com>
2025-09-20 09:28:34 +0200

    Create BackReferenceableColumnColumn enum

---
+++ Source/ch/nolix/systemapi/sqlmidschema/databasestructure/BackReferenceableColumnColumn.java
@@ -0,0 +1,31 @@
+package ch.nolix.systemapi.sqlmidschema.databasestructure;
+
+import ch.nolix.coreapi.attribute.mandatoryattribute.INameHolder;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-09-20
+ */
+public enum BackReferenceableColumnColumn implements INameHolder {
+  PARENT_BASE_BACK_REFERENCE_COLUMN_ID(BackReferenceableColumnColumnNameCatalog.PARENT_BASE_BACK_REFERENCE_COLUMN_ID),
+  BACK_REFERENCEABLE_COLUMN_ID(BackReferenceableColumnColumnNameCatalog.BACK_REFERENCEABLE_COLUMN_ID);
+
+  private final String name;
+
+  /**
+   * Creates a new {@link BackReferenceableColumnColumn} with the given name.
+   * 
+   * @param name
+   */
+  BackReferenceableColumnColumn(final String name) {
+    this.name = name;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getName() {
+    return name;
+  }
+}
