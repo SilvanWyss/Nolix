979b41b883184f2fdf99043fce584110972a58fd
Silvan Wyss <silvan.wyss@gmail.com>
2025-09-26 17:23:43 +0200

    Update ColumnDtoMapper

--- Source/ch/nolix/system/sqlmidschema/modelmapper/ColumnDtoMapper.java
+++ Source/ch/nolix/system/sqlmidschema/modelmapper/ColumnDtoMapper.java
@@ -1,17 +1,18 @@
 package ch.nolix.system.sqlmidschema.modelmapper;
 
+import ch.nolix.core.container.immutablelist.ImmutableList;
+import ch.nolix.coreapi.datamodel.fieldproperty.DataType;
 import ch.nolix.coreapi.sql.model.ISqlRecord;
+import ch.nolix.systemapi.midschema.fieldproperty.FieldType;
 import ch.nolix.systemapi.midschema.model.ColumnDto;
 import ch.nolix.systemapi.sqlmidschema.databasestructure.ColumnTableFieldIndexCatalog;
 import ch.nolix.systemapi.sqlmidschema.modelmapper.IColumnDtoMapper;
-import ch.nolix.systemapi.sqlmidschema.modelmapper.IContentModelDtoMapper;
 
 /**
  * @author Silvan Wyss
  * @version 2021-09-02
  */
 public final class ColumnDtoMapper implements IColumnDtoMapper {
-  private static final IContentModelDtoMapper CONTENT_MODEL_DTO_MAPPER = new ContentModelDtoMapper();
 
   /**
    * {@inheritDoc}
@@ -20,11 +21,33 @@ public final class ColumnDtoMapper implements IColumnDtoMapper {
   public ColumnDto mapJoinedColumnSqlRecordToColumnDto(final ISqlRecord joinedColumnSqlRecord) {
     final var id = joinedColumnSqlRecord.getStoredAtOneBasedIndex(ColumnTableFieldIndexCatalog.ID_INDEX);
     final var name = joinedColumnSqlRecord.getStoredAtOneBasedIndex(ColumnTableFieldIndexCatalog.NAME_INDEX);
-    final var contentModel = CONTENT_MODEL_DTO_MAPPER.mapJoinedColumnSqlRecordToColumnDto(joinedColumnSqlRecord);
-    final var fieldType = contentModel.fieldType();
-    final var dataType = contentModel.dataType();
-    final var referenceableTableIds = contentModel.referenceableTableIds();
-    final var backReferenceableColumnIds = contentModel.backReferenceableColumnIds();
+
+    final var fieldTypeString = //
+    joinedColumnSqlRecord.getStoredAtOneBasedIndex(ColumnTableFieldIndexCatalog.FIELD_TYPE_INDEX);
+
+    final var fieldType = FieldType.valueOf(fieldTypeString);
+
+    final var dataTypeString = //
+    joinedColumnSqlRecord.getStoredAtOneBasedIndex(ColumnTableFieldIndexCatalog.DATA_TYPE_INDEX);
+
+    final var dataType = DataType.valueOf(dataTypeString);
+    final var referenceableTableIdsString = joinedColumnSqlRecord.getStoredAtOneBasedIndex(6);
+    ImmutableList<String> referenceableTableIds;
+
+    if (referenceableTableIdsString != null) {
+      referenceableTableIds = ImmutableList.forArray(referenceableTableIdsString.split(","));
+    } else {
+      referenceableTableIds = ImmutableList.createEmpty();
+    }
+
+    final var backReferenceableColumnIdsString = joinedColumnSqlRecord.getStoredAtOneBasedIndex(7);
+    ImmutableList<String> backReferenceableColumnIds;
+
+    if (backReferenceableColumnIdsString != null) {
+      backReferenceableColumnIds = ImmutableList.forArray(backReferenceableColumnIdsString.split(","));
+    } else {
+      backReferenceableColumnIds = ImmutableList.createEmpty();
+    }
 
     return new ColumnDto(id, name, fieldType, dataType, referenceableTableIds, backReferenceableColumnIds);
   }
