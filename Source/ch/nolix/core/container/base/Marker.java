7972988e53227b5979ebde65ca689988f090fccf
Silvan Wyss <silvan.wyss@gmail.com>
2025-10-16 20:42:43 +0200

    Edit Marker

--- Source/ch/nolix/core/container/base/Marker.java
+++ Source/ch/nolix/core/container/base/Marker.java
@@ -1,4 +1,9 @@
 package ch.nolix.core.container.base;
 
+/**
+ * @author Silvan Wyss
+ * @version 2023-01-29
+ * @param <T> is the type a {@link Marker} is for.
+ */
 public final class Marker<T> { //NOSONAR: A Marker needs to be instantiable.
 }
