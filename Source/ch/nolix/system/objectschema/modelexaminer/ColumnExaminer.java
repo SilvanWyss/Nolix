896ea19482461ebd6619cf04a964f5247097ca74
Silvan Wyss <silvan.wyss@gmail.com>
2025-10-20 21:47:18 +0200

    Add canSetContentModel method to IColumnExaminer

--- Source/ch/nolix/system/objectschema/modelexaminer/ColumnExaminer.java
+++ Source/ch/nolix/system/objectschema/modelexaminer/ColumnExaminer.java
@@ -1,7 +1,11 @@
 package ch.nolix.system.objectschema.modelexaminer;
 
+import ch.nolix.coreapi.container.base.IContainer;
+import ch.nolix.coreapi.datamodel.fieldproperty.DataType;
 import ch.nolix.systemapi.midschema.fieldproperty.BaseFieldType;
+import ch.nolix.systemapi.midschema.fieldproperty.FieldType;
 import ch.nolix.systemapi.objectschema.model.IColumn;
+import ch.nolix.systemapi.objectschema.model.ITable;
 import ch.nolix.systemapi.objectschema.modelexaminer.IColumnExaminer;
 
 /**
@@ -13,7 +17,26 @@ public final class ColumnExaminer implements IColumnExaminer {
    * {@inheritDoc}
    */
   @Override
-  public boolean isBaseReferenceColumn(IColumn column) {
+  public boolean canSetContentModel(
+    final IColumn column,
+    final FieldType fieldType,
+    final DataType dataType,
+    final IContainer<? extends ITable> referenceableTables,
+    final IContainer<? extends IColumn> backReferenceableColumns) {
+    return //
+    column != null
+    && column.isEmpty()
+    && fieldType != null
+    && dataType != null
+    && referenceableTables != null
+    && backReferenceableColumns != null;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isBaseReferenceColumn(final IColumn column) {
     return //
     column != null
     && column.getFieldType().getBaseType() == BaseFieldType.BASE_REFERENCE;
--- Source/ch/nolix/systemapi/objectschema/modelexaminer/IColumnExaminer.java
+++ Source/ch/nolix/systemapi/objectschema/modelexaminer/IColumnExaminer.java
@@ -1,12 +1,32 @@
 package ch.nolix.systemapi.objectschema.modelexaminer;
 
+import ch.nolix.coreapi.container.base.IContainer;
+import ch.nolix.coreapi.datamodel.fieldproperty.DataType;
+import ch.nolix.systemapi.midschema.fieldproperty.FieldType;
 import ch.nolix.systemapi.objectschema.model.IColumn;
+import ch.nolix.systemapi.objectschema.model.ITable;
 
 /**
  * @author Silvan Wyss
  * @version 2024-12-30
  */
 public interface IColumnExaminer {
+  /**
+   * @param column
+   * @param fieldType
+   * @param dataType
+   * @param referenceableTables
+   * @param backReferenceableColumns
+   * @return true if the given column can set the given content model, false
+   *         otherwise.
+   */
+  boolean canSetContentModel(
+    IColumn column,
+    FieldType fieldType,
+    DataType dataType,
+    IContainer<? extends ITable> referenceableTables,
+    IContainer<? extends IColumn> backReferenceableColumns);
+
   /**
    * @param column
    * @return true if the given column is a base reference column, false otherwise.
