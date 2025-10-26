0abdd46ea720c014a1867418b59b4405b98624db
Silvan Wyss <silvan.wyss@gmail.com>
2025-10-26 16:38:01 +0100

    Create FieldMapperTest

---
+++ Test/ch/nolix/systemtest/objectdata/model/FieldMapperTest.java
@@ -0,0 +1,51 @@
+package ch.nolix.systemtest.objectdata.model;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import ch.nolix.core.container.immutablelist.ImmutableList;
+import ch.nolix.core.testing.standardtest.StandardTest;
+import ch.nolix.system.objectdata.model.Column;
+import ch.nolix.system.objectdata.model.FieldMapper;
+import ch.nolix.system.objectdata.model.Table;
+import ch.nolix.systemapi.databaseobject.property.DatabaseObjectState;
+import ch.nolix.systemapi.midschema.fieldproperty.FieldType;
+
+final class FieldMapperTest extends StandardTest {
+
+  @Test
+  void testCase_mapColumnToField() {
+    //setup part 1: Creates tableMock.
+    final var tableMock = mock(Table.class);
+
+    //setup part 1: Creates column.
+    @SuppressWarnings("unchecked")
+    final var column = //
+    Column.withParentTableAndIdAndNameAndFieldTypeAndDataTypeClassAndReferenceableTablesAndBackReferenceableColumns(
+      tableMock,
+      "id",
+      "name",
+      FieldType.VALUE_FIELD,
+      Integer.class,
+      ImmutableList.createEmpty(),
+      ImmutableList.createEmpty());
+
+    //execution
+    final var result = FieldMapper.mapColumnToField(column);
+
+    //verification part 1: Verifies parents.
+    expect(result.belongsToDatabase()).isFalse();
+    expect(result.belongsToTable()).isFalse();
+    expect(result.belongsToEntity()).isFalse();
+    expect(result.getStoredParentColumn()).is(column);
+
+    //verification part 2: Verifies attributes.
+    expect(result.getName()).isEqualTo("name");
+    expect(result.getType()).is(FieldType.VALUE_FIELD);
+
+    //verification part 3: Verifies states.
+    expect(result.getState()).is(DatabaseObjectState.NEW);
+    expect(result.isEmpty());
+  }
+}
