0c39be9d51b96e9808045714ea8a3f736cc6452a
Silvan Wyss <silvan.wyss@gmail.com>
2025-10-28 19:07:57 +0100

    Extend DeepSelectingStyleTest

--- Test/ch/nolix/systemtest/style/model/DeepSelectingStyleTest.java
+++ Test/ch/nolix/systemtest/style/model/DeepSelectingStyleTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import ch.nolix.core.testing.standardtest.StandardTest;
 import ch.nolix.system.style.model.DeepSelectingStyle;
+import ch.nolix.system.webgui.atomiccontrol.button.Button;
+import ch.nolix.systemapi.webgui.atomiccontrol.buttonapi.ButtonRole;
 import ch.nolix.systemapi.webgui.atomiccontrol.labelapi.LabelRole;
 
 final class DeepSelectingStyleTest extends StandardTest {
@@ -63,5 +65,26 @@ final class DeepSelectingStyleTest extends StandardTest {
 
     //verification
     expect(result).isFalse();
+  }
+
+  @Test
+  void testCase_withSelectorTokens() {
+    //setup
+    final var testUnit = //
+    new DeepSelectingStyle()
+      .withSelectorId("id")
+      .withSelectorType(Button.class)
+      .withSelectorRole(ButtonRole.CREATE_BUTTON, ButtonRole.DELETE_BUTTON)
+      .withSelectorToken("token1", "token2")
+      .withAttachingAttribute("BaseTextSize(20)", "BaseTextColor(Black)");
+
+    //execution
+    final var result = testUnit.withSelectorToken("token3", "token4");
+
+    //verification
+    expect(result.getSelectorId()).isEqualTo("id");
+    expect(result.getSelectorType()).isEqualTo("Button");
+    expect(result.getSelectorRoles()).containsExactly("CREATE_BUTTON", "DELETE_BUTTON");
+    expect(result.getSelectorTokens()).containsExactlyEqualing("token1", "token2", "token3", "token4");
   }
 }
