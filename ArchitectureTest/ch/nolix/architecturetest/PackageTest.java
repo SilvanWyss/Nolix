5a32df192ef6047284ccfac5d08f5b185e640378
Silvan Wyss <silvan.wyss@gmail.com>
2025-10-31 17:33:08 +0100

    Create PackageTest

---
+++ ArchitectureTest/ch/nolix/architecturetest/PackageTest.java
@@ -0,0 +1,31 @@
+package ch.nolix.architecturetest;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+
+final class PackageTest {
+  @Test
+  @Disabled
+  void testCase_packagesHaveAMaxHierarchyDepthOf5() {
+    //parameter definition
+    final var maxPackageHierarchyDepth = 5;
+
+    //setup
+    final var rule = //
+    ArchRuleDefinition
+      .classes()
+      .that()
+      .arePublic()
+      .and()
+      .areNotNestedClasses()
+      .should()
+      .haveNameMatching("ch[.]nolix([.][0-9a-zA-Z[-]]*){0," + (maxPackageHierarchyDepth - 1) + "}");
+    final var testUnit = new ClassFileImporter().importPackages("ch.nolix...");
+
+    //execution & verification
+    rule.check(testUnit);
+  }
+}
