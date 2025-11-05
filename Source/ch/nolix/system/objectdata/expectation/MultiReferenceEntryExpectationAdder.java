259838ff1ec1853f9451e699bb5523e995c61bc5
Silvan Wyss <silvan.wyss@gmail.com>
2025-11-05 22:04:05 +0100

    Create IMultiReferenceEntryExpectationAdder

---
+++ Source/ch/nolix/system/objectdata/expectation/MultiReferenceEntryExpectationAdder.java
@@ -0,0 +1,38 @@
+package ch.nolix.system.objectdata.expectation;
+
+import ch.nolix.systemapi.middata.adapter.IDataAdapterAndSchemaReader;
+import ch.nolix.systemapi.objectdata.expectation.IMultiReferenceEntryExpectationAdder;
+import ch.nolix.systemapi.objectdata.model.IEntity;
+import ch.nolix.systemapi.objectdata.model.IMultiReferenceEntry;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-11-05
+ */
+public final class MultiReferenceEntryExpectationAdder implements IMultiReferenceEntryExpectationAdder {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void addExpectationThatNewlyReferencedEntitiesExist(
+    final IMultiReferenceEntry<IEntity> multiReferenceEntry,
+    final IDataAdapterAndSchemaReader dataAndSchemaAdapter) {
+    final var referencedTableName = multiReferenceEntry.getReferencedTableName();
+    final var referencedEntityId = multiReferenceEntry.getReferencedEntityId();
+
+    dataAndSchemaAdapter.expectTableContainsEntity(referencedTableName, referencedEntityId);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void addExpectationThatNewlyReferencedEntitiesExistIfMultiReferenceEntryIsNew(
+    final IMultiReferenceEntry<IEntity> multiReferenceEntry,
+    final IDataAdapterAndSchemaReader dataAndSchemaAdapter) {
+    if (multiReferenceEntry.isNew()) {
+      addExpectationThatNewlyReferencedEntitiesExist(multiReferenceEntry, dataAndSchemaAdapter);
+    }
+  }
+}
---
+++ Source/ch/nolix/systemapi/objectdata/expectation/IMultiReferenceEntryExpectationAdder.java
@@ -0,0 +1,33 @@
+package ch.nolix.systemapi.objectdata.expectation;
+
+import ch.nolix.systemapi.middata.adapter.IDataAdapterAndSchemaReader;
+import ch.nolix.systemapi.objectdata.model.IEntity;
+import ch.nolix.systemapi.objectdata.model.IMultiReferenceEntry;
+
+/**
+ * @author Silvan Wyss
+ * @version 2025-11-05
+ */
+public interface IMultiReferenceEntryExpectationAdder {
+  /**
+   * Adds expectation to the given multiReferenceEntry that its newly referenced
+   * entities actually exist.
+   * 
+   * @param multiReferenceEntry
+   * @param dataAndSchemaAdapter
+   */
+  void addExpectationThatNewlyReferencedEntitiesExist(
+    IMultiReferenceEntry<IEntity> multiReferenceEntry,
+    IDataAdapterAndSchemaReader dataAndSchemaAdapter);
+
+  /**
+   * Adds expectation to the given multiReferenceEntry that its newly referenced
+   * entities actually exist if the given multiReferenceEntry is new.
+   * 
+   * @param multiReferenceEntry
+   * @param dataAndSchemaAdapter
+   */
+  void addExpectationThatNewlyReferencedEntitiesExistIfMultiReferenceEntryIsNew(
+    IMultiReferenceEntry<IEntity> multiReferenceEntry,
+    IDataAdapterAndSchemaReader dataAndSchemaAdapter);
+}
