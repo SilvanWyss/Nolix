c7b06d662204ea6f2dab235de43a6d75b8b9b2e7
Silvan Wyss <silvan.wyss@gmail.com>
2025-11-07 10:35:12 +0100

    Create IFluentMutableCardinalityHolder

---
+++ Source/ch/nolix/coreapi/attribute/fluentmutablemandatoryattribute/IFluentMutableCardinalityHolder.java
@@ -0,0 +1,24 @@
+package ch.nolix.coreapi.attribute.fluentmutablemandatoryattribute;
+
+import ch.nolix.coreapi.datamodel.cardinality.Cardinality;
+import ch.nolix.coreapi.datamodel.cardinality.ICardinalityHolder;
+
+/**
+ * A {@link IFluentMutableCardinalityHolder} is a {@link ICardinalityHolder}
+ * whose cardinality can be set programmatically and fluently.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-11-07
+ * @param <H> is the type of a {@link IFluentMutableCardinalityHolder}.
+ */
+public interface IFluentMutableCardinalityHolder<H extends IFluentMutableCardinalityHolder<H>>
+extends ICardinalityHolder {
+  /**
+   * Sets the cardinality of the current {@link IFluentMutableCardinalityHolder}.
+   * 
+   * @param cardinality
+   * @return the current {@link IFluentMutableCardinalityHolder}.
+   * @throws RuntimeException if the given cardinality is null.
+   */
+  H setCardinality(Cardinality cardinality);
+}
