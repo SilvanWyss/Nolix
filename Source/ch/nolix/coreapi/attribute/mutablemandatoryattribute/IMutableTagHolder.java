ea87b043bc6748623233400b397cf37254c09aec
Silvan Wyss <silvan.wyss@gmail.com>
2025-11-07 10:27:42 +0100

    Create IMutableTagHolder

---
+++ Source/ch/nolix/coreapi/attribute/mutablemandatoryattribute/IMutableTagHolder.java
@@ -0,0 +1,20 @@
+package ch.nolix.coreapi.attribute.mutablemandatoryattribute;
+
+import ch.nolix.coreapi.attribute.mandatoryattribute.ITagHolder;
+
+/**
+ * A {@link IMutableTagHolder} is a {@link ITagHolder} whose tag can be set
+ * programmatically.
+ * 
+ * @author Silvan Wyss
+ * @version 2023-02-09
+ */
+public interface IMutableTagHolder extends ITagHolder {
+  /**
+   * Sets the tag of the current {@link IMutableTagHolder}.
+   * 
+   * @param tag
+   * @throws RuntimeException if the given tag is null or blank.
+   */
+  void setTag(String tag);
+}
