c6fd923cb3cad00fd1bcfbe44c97c788067ff5d2
Silvan Wyss <silvan.wyss@gmail.com>
2025-11-07 10:46:11 +0100

    Create IMutableMultiTagHolder

---
+++ Source/ch/nolix/coreapi/attribute/mutablemultiattribute/IMutableMultiTagHolder.java
@@ -0,0 +1,34 @@
+package ch.nolix.coreapi.attribute.mutablemultiattribute;
+
+import ch.nolix.coreapi.attribute.multiattribute.IMultiTagHolder;
+
+/**
+ * A {@link IMutableMultiTagHolder} is a {@link IMultiTagHolder} whose tags can
+ * be added and removed programmatically.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-11-07
+ */
+public interface IMutableMultiTagHolder extends IMultiTagHolder {
+  /**
+   * Adds the given tag to the current {@link IMutableMultiTagHolder} if the
+   * current {@link IMutableMultiTagHolder} does not contain already such a tag.
+   * 
+   * @param tag
+   * @throws RuntimeException if the given tag is null or blank.
+   */
+  void addTag(String tag);
+
+  /**
+   * Removes the given tag from the current {@link IMutableMultiTagHolder} if the
+   * current {@link IMutableMultiTagHolder} contains such a tag.
+   * 
+   * @param tag
+   */
+  void removeTag(String tag);
+
+  /**
+   * Removes all tags from the current {@link IMutableMultiTagHolder}.
+   */
+  void removeTags();
+}
