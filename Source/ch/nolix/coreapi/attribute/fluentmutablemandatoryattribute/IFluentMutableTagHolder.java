90674fe76fb58642c3fe59218cab8178c6d6c766
Silvan Wyss <silvan.wyss@gmail.com>
2025-11-07 10:32:37 +0100

    Create IFluentMutableTagHolder

---
+++ Source/ch/nolix/coreapi/attribute/fluentmutablemandatoryattribute/IFluentMutableTagHolder.java
@@ -0,0 +1,22 @@
+package ch.nolix.coreapi.attribute.fluentmutablemandatoryattribute;
+
+import ch.nolix.coreapi.attribute.mandatoryattribute.ITagHolder;
+
+/**
+ * A {@link IFluentMutableTagHolder} is a {@link ITagHolder} whose tag can be
+ * set programmatically and fluently.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-11-07
+ * @param <H> is the type of a {@link IFluentMutableTagHolder}.
+ */
+public interface IFluentMutableTagHolder<H extends IFluentMutableTagHolder<H>> extends ITagHolder {
+  /**
+   * Sets the tag of the current {@link IFluentMutableTagHolder}.
+   * 
+   * @param tag
+   * @return the current {@link IFluentMutableTagHolder}.
+   * @throws RuntimeException if the given tag is null or blank.
+   */
+  H setTag(String tag);
+}
