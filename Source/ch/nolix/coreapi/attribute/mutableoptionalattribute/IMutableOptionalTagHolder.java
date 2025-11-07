26f5fd4ef23b9414105664035be5a3fc3356db01
Silvan Wyss <silvan.wyss@gmail.com>
2025-11-07 10:30:02 +0100

    Create IMutableOptionalTagHolder

---
+++ Source/ch/nolix/coreapi/attribute/mutableoptionalattribute/IMutableOptionalTagHolder.java
@@ -0,0 +1,25 @@
+package ch.nolix.coreapi.attribute.mutableoptionalattribute;
+
+import ch.nolix.coreapi.attribute.optionalattribute.IOptionalTagHolder;
+
+/**
+ * A {@link IMutableOptionalTagHolder} is a {@link IOptionalTagHolder} whose tag
+ * can be set and removed programmatically.
+ * 
+ * @author Silvan Wyss
+ * @version 2025-11-07
+ */
+public interface IMutableOptionalTagHolder extends IOptionalTagHolder {
+  /**
+   * Removes the tag of the current {@link IMutableOptionalTagHolder}.
+   */
+  void removeTag();
+
+  /**
+   * Sets the tag of the current {@link IMutableOptionalTagHolder}.
+   * 
+   * @param tag
+   * @throws RuntimeException if the given tag is null or blank.
+   */
+  void setTag(String tag);
+}
