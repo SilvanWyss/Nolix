ccf587fc3a347c554f89aea14852e682fa123d69
Silvan Wyss <silvan.wyss@gmail.com>
2025-11-14 18:11:54 +0100

    Create VarargsTest

---
+++ ArchitectureTest/ch/nolix/architecturetest/VarargsTest.java
@@ -0,0 +1,37 @@
+package ch.nolix.architecturetest;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.domain.JavaMethod;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+
+final class VarargsTest {
+  @Disabled
+  @Test
+  void testCase_packagesHaveAMaxHierarchyDepthOf5() {
+    //setup
+    final var rule = //
+    ArchRuleDefinition
+      .methods()
+      .should(new ArchCondition<JavaMethod>("Methods with varargs parameters do not have other parameters.") {
+        @Override
+        public void check(final JavaMethod item, final ConditionEvents events) {
+          if (item.getParameters().size() > 1 && item.reflect().isVarArgs()) {
+            final var message = "The method" + item.getFullName() + " has varargs parameters and other parameters.";
+
+            events.add(new SimpleConditionEvent(item, false, message));
+          }
+        }
+      });
+
+    final var testUnit = new ClassFileImporter().importPackages("ch.nolix...");
+
+    //execution & verification
+    rule.check(testUnit);
+  }
+}
