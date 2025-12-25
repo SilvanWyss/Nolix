ea8a78fd77dccd3bd8d6694e5a133e20e96b9666
Silvan Wyss <silvan.wyss@gmail.com>
2025-12-25 22:32:14 +0100

    Edit DropdownMenuHtmlBuilder

--- Source/ch/nolix/system/webatomiccontrol/dropdownmenu/DropdownMenuHtmlBuilder.java
+++ Source/ch/nolix/system/webatomiccontrol/dropdownmenu/DropdownMenuHtmlBuilder.java
@@ -1,11 +1,8 @@
 package ch.nolix.system.webatomiccontrol.dropdownmenu;
 
 import ch.nolix.core.web.htmlelementmodel.HtmlElement;
-import ch.nolix.coreapi.container.base.IContainer;
 import ch.nolix.coreapi.web.html.HtmlElementTypeCatalog;
-import ch.nolix.coreapi.web.htmlelementmodel.IHtmlElement;
 import ch.nolix.systemapi.webatomiccontrol.dropdownmenu.IDropdownMenu;
-import ch.nolix.systemapi.webatomiccontrol.itemmenu.IItemMenuItem;
 import ch.nolix.systemapi.webgui.controltool.IControlHtmlBuilder;
 
 /**
@@ -14,25 +11,9 @@ import ch.nolix.systemapi.webgui.controltool.IControlHtmlBuilder;
 public final class DropdownMenuHtmlBuilder implements IControlHtmlBuilder<IDropdownMenu> {
   @Override
   public HtmlElement createHtmlElementForControl(final IDropdownMenu dropdownMenu) {
-    return HtmlElement.withTypeAndChildElements(
-      HtmlElementTypeCatalog.SELECT,
-      createHtmlChildElementsForDropdownMenu(dropdownMenu));
-  }
+    final var type = HtmlElementTypeCatalog.SELECT;
+    final var childElements = DropdownMenuHtmlBuilderHelper.createHtmlChildElementsForDropdownMenu(dropdownMenu);
 
-  private IContainer<IHtmlElement> createHtmlChildElementsForDropdownMenu(
-    final IDropdownMenu dropdownMenu) {
-    return createHtmlElementsFromItems(dropdownMenu.getStoredItems());
-  }
-
-  private IContainer<IHtmlElement> createHtmlElementsFromItems(
-    final IContainer<? extends IItemMenuItem<?>> items) {
-    return items.to(this::createHtmlElementForItem);
-  }
-
-  private IHtmlElement createHtmlElementForItem(final IItemMenuItem<?> item) {
-    return HtmlElement.withTypeAndAttributesAndInnerText(
-      HtmlElementTypeCatalog.OPTION,
-      DropdownMenuHtmlBuilderHelper.createHtmlAttributesForDropdownMenuItem(item),
-      item.getText());
+    return HtmlElement.withTypeAndChildElements(type, childElements);
   }
 }
--- Source/ch/nolix/system/webatomiccontrol/dropdownmenu/DropdownMenuHtmlBuilderHelper.java
+++ Source/ch/nolix/system/webatomiccontrol/dropdownmenu/DropdownMenuHtmlBuilderHelper.java
@@ -2,9 +2,13 @@ package ch.nolix.system.webatomiccontrol.dropdownmenu;
 
 import ch.nolix.core.container.linkedlist.LinkedList;
 import ch.nolix.core.web.htmlelementmodel.HtmlAttribute;
+import ch.nolix.core.web.htmlelementmodel.HtmlElement;
 import ch.nolix.coreapi.container.base.IContainer;
 import ch.nolix.coreapi.container.list.ILinkedList;
+import ch.nolix.coreapi.web.html.HtmlElementTypeCatalog;
 import ch.nolix.coreapi.web.htmlelementmodel.IHtmlAttribute;
+import ch.nolix.coreapi.web.htmlelementmodel.IHtmlElement;
+import ch.nolix.systemapi.webatomiccontrol.dropdownmenu.IDropdownMenu;
 import ch.nolix.systemapi.webatomiccontrol.itemmenu.IItemMenuItem;
 
 /**
@@ -14,7 +18,24 @@ public final class DropdownMenuHtmlBuilderHelper {
   private DropdownMenuHtmlBuilderHelper() {
   }
 
-  public static IContainer<IHtmlAttribute> createHtmlAttributesForDropdownMenuItem(
+  public static IContainer<IHtmlElement> createHtmlChildElementsForDropdownMenu(
+    final IDropdownMenu dropdownMenu) {
+    return createHtmlElementsFromItems(dropdownMenu.getStoredItems());
+  }
+
+  private static IContainer<IHtmlElement> createHtmlElementsFromItems(
+    final IContainer<? extends IItemMenuItem<?>> items) {
+    return items.to(DropdownMenuHtmlBuilderHelper::createHtmlElementForItem);
+  }
+
+  private static IHtmlElement createHtmlElementForItem(final IItemMenuItem<?> item) {
+    return HtmlElement.withTypeAndAttributesAndInnerText(
+      HtmlElementTypeCatalog.OPTION,
+      DropdownMenuHtmlBuilderHelper.createHtmlAttributesForDropdownMenuItem(item),
+      item.getText());
+  }
+
+  private static IContainer<IHtmlAttribute> createHtmlAttributesForDropdownMenuItem(
     final IItemMenuItem<?> dropdownMenuItem) {
     final ILinkedList<IHtmlAttribute> htmlAttributes = LinkedList.createEmpty();
 
