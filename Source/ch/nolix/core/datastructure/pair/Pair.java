a1a38c62143df9bc87a54baaf605d6227583ab44
Silvan Wyss <silvan.wyss@gmail.com>
2026-02-02 22:14:47 +0100

    Beautify Pair

--- Source/ch/nolix/core/datastructure/pair/Pair.java
+++ Source/ch/nolix/core/datastructure/pair/Pair.java
@@ -12,12 +12,12 @@ import ch.nolix.coreapi.datastructure.pair.IPair;
  * 
  * @author Silvan Wyss
  * @param <E1> is the type of the element 1 of a {@link Pair}.
- * @param <T>  is the type of the element 2 of a {@link Pair}.
+ * @param <E2> is the type of the element 2 of a {@link Pair}.
  */
-public final class Pair<E1, T> implements IPair<E1, T> {
+public final class Pair<E1, E2> implements IPair<E1, E2> {
   private final E1 element1;
 
-  private final T element2;
+  private final E2 element2;
 
   /**
    * Creates a new {@link Pair} with the given elements.
@@ -27,17 +27,11 @@ public final class Pair<E1, T> implements IPair<E1, T> {
    * @throws ArgumentIsNullException if the given element 1 is null.
    * @throws ArgumentIsNullException if the given element 2 is null.
    */
-  public Pair(final E1 element1, final T element2) {
-    //Asserts that the given element 1 is not null.
+  public Pair(final E1 element1, final E2 element2) {
     Validator.assertThat(element1).thatIsNamed("element 1").isNotNull();
-
-    //Asserts that the given element 2 is not null.
     Validator.assertThat(element2).thatIsNamed("element 2").isNotNull();
 
-    //Sets the element 1 of the current {@link Pair}.
     this.element1 = element1;
-
-    //Sets the element 2 of the current {@link Pair}.
     this.element2 = element2;
   }
 
@@ -53,7 +47,7 @@ public final class Pair<E1, T> implements IPair<E1, T> {
    * {@inheritDoc}
    */
   @Override
-  public T getStoredElement2() {
+  public E2 getStoredElement2() {
     return element2;
   }
 
@@ -62,7 +56,7 @@ public final class Pair<E1, T> implements IPair<E1, T> {
    */
   @Override
   public boolean hasElement1(final Object object) {
-    return (element1 == object);
+    return element1 == object;
   }
 
   /**
@@ -70,7 +64,7 @@ public final class Pair<E1, T> implements IPair<E1, T> {
    */
   @Override
   public boolean hasElement2(final Object object) {
-    return (element2 == object);
+    return element2 == object;
   }
 
   /**
@@ -78,6 +72,6 @@ public final class Pair<E1, T> implements IPair<E1, T> {
    */
   @Override
   public String toString() {
-    return ("(" + getStoredElement1() + "," + getStoredElement2() + ")");
+    return "(" + getStoredElement1() + "," + getStoredElement2() + ")";
   }
 }
