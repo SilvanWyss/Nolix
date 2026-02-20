bacb035726e47f221369c30b41d62f04c851f158
Silvan Wyss <silvan.wyss@gmail.com>
2026-02-20 11:17:19 +0100

    Rename commonarchitecturetest package to unionarchitecturetest

--- ArchitectureTest/ch/nolix/unionarchitecturetest/VarargsTest.java
+++ ArchitectureTest/ch/nolix/unionarchitecturetest/VarargsTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright © by Silvan Wyss. All rights reserved.
  */
-package ch.nolix.commonarchitecturetest;
+package ch.nolix.unionarchitecturetest;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
