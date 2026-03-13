11cafc53a47558930e118c92887c616f71f87990
Silvan Wyss <silvan.wyss@gmail.com>
2026-03-13 14:09:07 +0100

    Create ClassTest

---
+++ ArchitectureTest/ch/nolix/unionarchitecturetest/ClassTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.unionarchitecturetest;
+
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+
+import ch.nolix.base.testing.archunit.ArchUnitRuleCatalog;
+
+/**
+ * @author Silvan Wyss
+ */
+final class ClassTest {
+  @Test
+  void testCase_publicClassesDoNotContainNestedClasses() {
+    //setup
+    final var testUnit = new ClassFileImporter().importPackages("ch.nolix..");
+
+    //execution & verification
+    ArchUnitRuleCatalog.PUBLIC_CLASSES_DO_NOT_CONTAIN_NESTED_CLASSES.check(testUnit);
+  }
+}
