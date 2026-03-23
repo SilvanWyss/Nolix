38889cf206d13880606548cd9aeff624740b8ea0
Silvan Wyss <silvan.wyss@gmail.com>
2026-03-23 21:46:18 +0100

    Beautify AbstractMediator

--- Source/ch/nolix/base/validation/base/AbstractMediator.java
+++ Source/ch/nolix/base/validation/base/AbstractMediator.java
@@ -7,12 +7,9 @@ import ch.nolix.baseapi.errorcontrol.invalidargumentexception.ArgumentIsNullExce
 import ch.nolix.baseapi.errorcontrol.invalidargumentexception.InvalidArgumentException;
 
 /**
- * A {@link AbstractMediator} is not mutable.
- * 
  * @author Silvan Wyss
  */
-public abstract class AbstractMediator { //NOSONAR: Mediator does not have abstract methods.
-
+public abstract class AbstractMediator { //NOSONAR: A AbstractMediator does not have abstract methods.
   public static final String DEFAULT_ARGUMENT_NAME = "argument";
 
   private final String argumentName;
@@ -21,32 +18,25 @@ public abstract class AbstractMediator { //NOSONAR: Mediator does not have abstr
    * Creates a new {@link AbstractMediator} with a default argument name.
    */
   protected AbstractMediator() {
-    //Calls other constructor.
-    this(DEFAULT_ARGUMENT_NAME);
+    argumentName = DEFAULT_ARGUMENT_NAME;
   }
 
   /**
    * Creates a new argument {@link AbstractMediator} with the given argumentName.
    * 
    * @param argumentName
-   * @throws RuntimeException if the given argumentName is null.
-   * @throws RuntimeException if the given argumentName is blank.
+   * @throws RuntimeException if the given argumentName is null or blank.
    */
   protected AbstractMediator(final String argumentName) {
-    //Asserts that the given argumentName is not null.
     if (argumentName == null) {
       throw ArgumentIsNullException.forArgumentName("argument name");
     }
 
-    //Asserts that the given argumentName is not blank.
     if (argumentName.isBlank()) {
-      throw InvalidArgumentException.forArgumentAndArgumentNameAndErrorPredicate(
-        "argument name",
-        argumentName,
-        "is blank");
+      throw //
+      InvalidArgumentException.forArgumentAndArgumentNameAndErrorPredicate(argumentName, "argument name", "is blank");
     }
 
-    //Sets the argumentName of the current Mediator.
     this.argumentName = argumentName;
   }
 
