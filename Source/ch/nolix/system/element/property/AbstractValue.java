4a3ed6e9082d89c68745f6d439db851a5b054d89
Silvan Wyss <silvan.wyss@gmail.com>
2026-04-10 11:32:24 +0200

    Make IBaseValueProperty a MutabilityRequestable

--- Source/ch/nolix/system/element/property/AbstractValue.java
+++ Source/ch/nolix/system/element/property/AbstractValue.java
@@ -9,14 +9,13 @@ import ch.nolix.base.validation.validator.Validator;
 import ch.nolix.baseapi.attribute.mandatoryattribute.INameHolder;
 import ch.nolix.baseapi.document.node.INode;
 import ch.nolix.baseapi.misc.variable.LowerCaseVariableCatalog;
-import ch.nolix.baseapi.state.staterequest.MutabilityRequestable;
 import ch.nolix.systemapi.property.value.IBaseValueProperty;
 
 /**
  * @author Silvan Wyss
  * @param <V> is the type of the values of a {@link AbstractValue}.
  */
-public abstract class AbstractValue<V> implements IBaseValueProperty, MutabilityRequestable, INameHolder {
+public abstract class AbstractValue<V> implements IBaseValueProperty, INameHolder {
   private final String name;
 
   private final Function<INode<?>, V> valueCreator;
--- Source/ch/nolix/systemapi/property/value/IBaseValueProperty.java
+++ Source/ch/nolix/systemapi/property/value/IBaseValueProperty.java
@@ -4,11 +4,12 @@
 package ch.nolix.systemapi.property.value;
 
 import ch.nolix.baseapi.state.staterequest.EmptinessRequestable;
+import ch.nolix.baseapi.state.staterequest.MutabilityRequestable;
 import ch.nolix.systemapi.property.base.IProperty;
 
 /**
  * @author Silvan Wyss
  */
-public interface IBaseValueProperty extends EmptinessRequestable, IProperty {
+public interface IBaseValueProperty extends EmptinessRequestable, IProperty, MutabilityRequestable {
   //This interface is just an union of other interfaces.
 }
