5bb0c8a1a5460da377396865dd28b7be9420e5bc
Silvan Wyss <silvan.wyss@gmail.com>
2026-04-13 16:36:52 +0200

    Delete createArrayWithElement method of IArrayTool

--- Source/ch/nolix/base/commontypetool/arraytool/ArrayTool.java
+++ Source/ch/nolix/base/commontypetool/arraytool/ArrayTool.java
@@ -14,18 +14,6 @@ public final class ArrayTool implements IArrayTool {
    * {@inheritDoc}
    */
   @Override
-  public <E> E[] createArrayWithElement(final E element, final @SuppressWarnings("unchecked") E... elements) {
-    final @SuppressWarnings("unchecked") var array = (E[]) new Object[elements.length + 1];
-    array[0] = element;
-    System.arraycopy(elements, 0, array, 1, elements.length);
-
-    return array;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
   public double[] createArrayWithValue(final double value, final double... values) {
     final var array = new double[1 + values.length];
     array[0] = value;
--- Source/ch/nolix/baseapi/commontypetool/arraytool/IArrayTool.java
+++ Source/ch/nolix/baseapi/commontypetool/arraytool/IArrayTool.java
@@ -7,8 +7,6 @@ package ch.nolix.baseapi.commontypetool.arraytool;
  * @author Silvan Wyss
  */
 public interface IArrayTool {
-  <E> E[] createArrayWithElement(E element, @SuppressWarnings("unchecked") E... elements);
-
   double[] createArrayWithValue(double value, double... values);
 
   IByteArrayMediator onArray(byte[] byteArray);
