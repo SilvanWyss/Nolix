c8afdcc3abad4bebec6cda74819429b28e090bba
Silvan Wyss <silvan.wyss@gmail.com>
2026-04-16 16:02:01 +0200

    Create TabContainerStyle

---
+++ Source/ch/nolix/system/containercontrol/tabcontainer/TabContainerStyle.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.system.containercontrol.tabcontainer;
+
+import ch.nolix.system.webgui.controlstyle.AbstractControlStyle;
+import ch.nolix.systemapi.containercontrol.tabcontainer.ITabContainerStyle;
+
+/**
+ * @author Silvan Wyss
+ */
+public class TabContainerStyle extends AbstractControlStyle<ITabContainerStyle> implements ITabContainerStyle {
+  /**
+   * Creates a new {@link TabContainerStyle}.
+   */
+  public TabContainerStyle() {
+    initialize();
+  }
+}
