03136f7c5ee264e7742e7dfe677713efd8bd7bfd
Silvan Wyss <silvan.wyss@gmail.com>
2026-05-02 11:21:06 +0200

    Rename FixTableNameCatalog to MetaTableNameCatalog

--- Source/ch/nolix/systemapi/sqlmidschema/databasestructure/MetaTable.java
+++ Source/ch/nolix/systemapi/sqlmidschema/databasestructure/MetaTable.java
@@ -7,15 +7,15 @@ package ch.nolix.systemapi.sqlmidschema.databasestructure;
  * @author Silvan Wyss
  */
 public enum MetaTable {
-  DATABASE_PROPERTY(FixTableNameCatalog.DATABASE_PROPERTY),
-  TABLE(FixTableNameCatalog.TABLE),
-  COLUMN(FixTableNameCatalog.COLUMN),
-  REFERENCEABLE_TABLE(FixTableNameCatalog.REFERENCEABLE_TABLE),
-  BACK_REFERENCEABLE_COLUMN(FixTableNameCatalog.BACK_REFERENCEABLE_COLUMN),
-  ENTITY_INDEX(FixTableNameCatalog.ENTITY_INDEX),
-  MULTI_VALUE_ENTRY(FixTableNameCatalog.MULTI_VALUE_ENTRY),
-  MULTI_REFERENCE_ENTRY(FixTableNameCatalog.MULTI_REFERENCE_ENTRY),
-  MULTI_BACK_REFERENCE_ENTRY(FixTableNameCatalog.MULTI_BACK_REFERENCE_ENTRY);
+  DATABASE_PROPERTY(MetaTableNameCatalog.DATABASE_PROPERTY),
+  TABLE(MetaTableNameCatalog.TABLE),
+  COLUMN(MetaTableNameCatalog.COLUMN),
+  REFERENCEABLE_TABLE(MetaTableNameCatalog.REFERENCEABLE_TABLE),
+  BACK_REFERENCEABLE_COLUMN(MetaTableNameCatalog.BACK_REFERENCEABLE_COLUMN),
+  ENTITY_INDEX(MetaTableNameCatalog.ENTITY_INDEX),
+  MULTI_VALUE_ENTRY(MetaTableNameCatalog.MULTI_VALUE_ENTRY),
+  MULTI_REFERENCE_ENTRY(MetaTableNameCatalog.MULTI_REFERENCE_ENTRY),
+  MULTI_BACK_REFERENCE_ENTRY(MetaTableNameCatalog.MULTI_BACK_REFERENCE_ENTRY);
 
   private final String stringRepresentation;
 
