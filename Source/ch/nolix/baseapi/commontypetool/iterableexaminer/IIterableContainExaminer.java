96058305252267c9e729b185e43b5dae9e3024a5
Silvan Wyss <silvan.wyss@gmail.com>
2026-05-08 15:09:26 +0200

    Create IIterableContainExaminer

---
+++ Source/ch/nolix/baseapi/commontypetool/iterableexaminer/IIterableContainExaminer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.baseapi.commontypetool.iterableexaminer;
+
+/**
+ * @author Silvan Wyss
+ */
+public interface IIterableContainExaminer {
+  /**
+   * @param iterable the searched {@link Iterable}, is considered to be empty when
+   *                 is null
+   * @return true if the given iterable contains an element, false otherwise
+   */
+  boolean containsAny(Iterable<Object> iterable);
+
+  /**
+   * @param iterable the searched {@link Iterable}, is considered to be empty when
+   *                 is null
+   * @return true if the given iterable contains a non-null element, false
+   *         otherwise.
+   */
+  boolean containsNonNull(Iterable<Object> iterable);
+
+  /**
+   * @param iterable the searched {@link Iterable}, is considered to be empty when
+   *                 is null
+   * @return true if the given iterable is empty, false otherwise.
+   */
+  boolean isEmpty(Iterable<Object> iterable);
+}
