eff058f45da314147746969b35c081bfc6d618ec
Silvan Wyss <silvan.wyss@gmail.com>
2026-05-24 17:29:10 +0200

    Make IElement a AttributesRepresentable and a SpecificationRepresentable

--- Source/ch/nolix/systemapi/element/base/IElement.java
+++ Source/ch/nolix/systemapi/element/base/IElement.java
@@ -3,28 +3,9 @@
  */
 package ch.nolix.systemapi.element.base;
 
-import ch.nolix.baseapi.container.base.IContainer;
-import ch.nolix.baseapi.document.node.INode;
-
 /**
  * @author Silvan Wyss
  */
-public interface IElement {
-  /**
-   * @return the attributes of the current {@link IElement}.
-   */
-  IContainer<INode<?>> getAttributes();
-
-  /**
-   * @return the specification of the current {@link IElement}.
-   */
-  INode<?> getSpecification();
-
-  /**
-   * @return a formated {@link String} representation of the current
-   *         {@link IElement}.
-   */
-  default String toFormatedString() {
-    return getSpecification().toFormattedString();
-  }
+public interface IElement extends AttributesRepresentable, SpecificationRepresentable {
+  //This interface is a dedicated union of other interfaces.
 }
