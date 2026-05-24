8d0188d252c9b4a848eca4bc3023229d383ea965
Silvan Wyss <silvan.wyss@gmail.com>
2026-05-24 17:07:40 +0200

    Create SpecificationRepresentable

---
+++ Source/ch/nolix/systemapi/element/base/SpecificationRepresentable.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.systemapi.element.base;
+
+import ch.nolix.baseapi.document.node.INode;
+
+/**
+ * A {@link SpecificationRepresentable} can be represented by a specification.
+ * 
+ * @author Silvan Wyss
+ */
+public interface SpecificationRepresentable {
+  /**
+   * @return the specification of the current {@link SpecificationRepresentable}
+   */
+  INode<?> getSpecification();
+
+  /**
+   * @param element
+   * @return true if the current {@link SpecificationRepresentable} has an equal
+   *         specification as the given element, false otherwise
+   */
+  default boolean hasEqualSpecificationAsElement(final IElement element) {
+    return element != null && getSpecification().equals(element.getSpecification());
+  }
+
+  /**
+   * @return a formated {@link String} representation of the current
+   *         {@link SpecificationRepresentable}
+   */
+  default String toFormatedString() {
+    return getSpecification().toFormattedString();
+  }
+}
