219b59c261b9819363c8a4c2b97c69e31d96cc10
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-09 18:59:09 +0200

    Edit IValueHolder

--- Source/ch/nolix/baseapi/attribute/mandatoryattribute/IValueHolder.java
+++ Source/ch/nolix/baseapi/attribute/mandatoryattribute/IValueHolder.java
@@ -7,11 +7,11 @@ package ch.nolix.baseapi.attribute.mandatoryattribute;
  * A {@link IValueHolder} contains a value.
  * 
  * @author Silvan Wyss
- * @param <V> is the type of the value of a {@link IValueHolder}.
+ * @param <V> the type of the value of a {@link IValueHolder}
  */
 public interface IValueHolder<V> {
   /**
-   * @return the value of the current {@link IValueHolder}.
+   * @return the value of the current {@link IValueHolder}
    */
   V getStoredValue();
 }
