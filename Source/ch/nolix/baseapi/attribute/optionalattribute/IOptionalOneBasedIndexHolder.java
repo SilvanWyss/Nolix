7bf098ec6011e86989996c6cbfb5eb42082f72ff
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-11 15:27:42 +0200

    Edit IOptionalOneBasedIndexHolder

--- Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalOneBasedIndexHolder.java
+++ Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalOneBasedIndexHolder.java
@@ -11,15 +11,15 @@ package ch.nolix.baseapi.attribute.optionalattribute;
 public interface IOptionalOneBasedIndexHolder {
   /**
    * @return the one-based index of the current
-   *         {@link IOptionalOneBasedIndexHolder}.
+   *         {@link IOptionalOneBasedIndexHolder}
    * @throws RuntimeException if the current {@link IOptionalOneBasedIndexHolder}
-   *                          does not have a one-based index.
+   *                          does not have a one-based index
    */
   int getOneBasedIndex();
 
   /**
    * @return true if the current {@link IOptionalOneBasedIndexHolder} has a
-   *         one-based index, false otherwise.
+   *         one-based index, false otherwise
    */
   boolean hasOneBasedIndex();
 }
