06f47228532a66c01eb2c9c39d11112bba4b0f03
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-11 15:17:56 +0200

    Edit IOptionalIdHolder

--- Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalIdHolder.java
+++ Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalIdHolder.java
@@ -3,6 +3,8 @@
  */
 package ch.nolix.baseapi.attribute.optionalattribute;
 
+import ch.nolix.baseapi.commontypetool.stringtool.StringCatalog;
+
 /**
  * A {@link IOptionalIdHolder} can have an id.
  * 
@@ -10,28 +12,33 @@ package ch.nolix.baseapi.attribute.optionalattribute;
  */
 public interface IOptionalIdHolder {
   /**
-   * @return the id of the current {@link IOptionalIdHolder}.
+   * @return the id of the current {@link IOptionalIdHolder}
+   * @throws RuntimeException if the current {@link IOptionalIdHolder} does not
+   *                          have an id
    */
   String getId();
 
   /**
-   * @return the id of the current {@link IOptionalIdHolder} in quotes.
+   * @return the id of the current {@link IOptionalIdHolder} in single quotes
+   * @throws RuntimeException if the current {@link IOptionalIdHolder} does not
+   *                          have an id
    */
-  default String getIdInQuotes() {
-    return ("'" + getId() + "'");
+  default String getIdInSingleQuotes() {
+    return StringCatalog.SINGLE_QUOTE + getId() + StringCatalog.SINGLE_QUOTE;
   }
 
   /**
-   * @return true if the current {@link IOptionalIdHolder} has an id.
+   * @return true if the current {@link IOptionalIdHolder} has an id, false
+   *         otherwise
    */
   boolean hasId();
 
   /**
    * @param id
-   * @return true if the current {@link IOptionalIdHolder} has the given id.
+   * @return true if the current {@link IOptionalIdHolder} has the given id, false
+   *         otherwise
    */
   default boolean hasId(final String id) {
-    return hasId()
-    && getId().equals(id);
+    return hasId() && getId().equals(id);
   }
 }
