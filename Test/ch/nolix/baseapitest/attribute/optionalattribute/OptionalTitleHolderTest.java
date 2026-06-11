f4dcd7316e633efcc743cd7dd49f41537accd97c
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-11 22:23:56 +0200

    Create OptionalTitleHolderTest

---
+++ Test/ch/nolix/baseapitest/attribute/optionalattribute/OptionalTitleHolderTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.baseapitest.attribute.optionalattribute;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import ch.nolix.base.testing.standardtest.StandardTest;
+import ch.nolix.baseapi.attribute.optionalattribute.IOptionalTitleHolder;
+
+/**
+ * @author Silvan Wyss
+ */
+final class OptionalTitleHolderTest extends StandardTest {
+  @Test
+  void testCase_getTitleInSingleQuotes() {
+    //setup
+    final var testUnit = Mockito.mock(IOptionalTitleHolder.class);
+    Mockito.when(testUnit.getTitleInSingleQuotes()).thenCallRealMethod();
+    Mockito.when(testUnit.hasTitle()).thenReturn(true);
+    Mockito.when(testUnit.getTitle()).thenReturn("title");
+
+    //execution
+    final var result = testUnit.getTitleInSingleQuotes();
+
+    //verification
+    expect(result).isEqualTo("'title'");
+  }
+}
