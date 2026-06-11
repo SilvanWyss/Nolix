a7c3c87332705d2e24a11f2287e4a9128b19d96d
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-11 22:12:07 +0200

    Create OptionalLabelHolderTest

---
+++ Test/ch/nolix/baseapitest/attribute/optionalattribute/OptionalLabelHolderTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.baseapitest.attribute.optionalattribute;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import ch.nolix.base.testing.standardtest.StandardTest;
+import ch.nolix.baseapi.attribute.optionalattribute.IOptionalLabelHolder;
+
+/**
+ * @author Silvan Wyss
+ */
+final class OptionalLabelHolderTest extends StandardTest {
+  @Test
+  void testCase_getLabelInSingleQuotes() {
+    //setup
+    final var testUnit = Mockito.mock(IOptionalLabelHolder.class);
+    Mockito.when(testUnit.getLabelInSingleQuotes()).thenCallRealMethod();
+    Mockito.when(testUnit.hasLabel()).thenReturn(true);
+    Mockito.when(testUnit.getLabel()).thenReturn("label");
+
+    //execution
+    final var result = testUnit.getLabelInSingleQuotes();
+
+    //verification
+    expect(result).isEqualTo("'label'");
+  }
+}
