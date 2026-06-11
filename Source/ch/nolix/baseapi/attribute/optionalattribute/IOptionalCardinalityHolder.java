e80a584fc99a5c2ee5976772e6771fbd428cedf1
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-11 15:06:44 +0200

    Edit IOptionalCardinalityHolder

--- Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalCardinalityHolder.java
+++ Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalCardinalityHolder.java
@@ -10,15 +10,15 @@ package ch.nolix.baseapi.attribute.optionalattribute;
  */
 public interface IOptionalCardinalityHolder {
   /**
-   * @return the cardinality of the current {@link IOptionalCardinalityHolder}.
+   * @return the cardinality of the current {@link IOptionalCardinalityHolder}
    * @throws RuntimeException if the current {@link IOptionalCardinalityHolder}
-   *                          does not have a cardinality.
+   *                          does not have a cardinality
    */
   String getCardinality();
 
   /**
    * @return true if the current {@link IOptionalCardinalityHolder} has a
-   *         cardinality, false otherwise.
+   *         cardinality, false otherwise
    */
   boolean hasCardinality();
 }
