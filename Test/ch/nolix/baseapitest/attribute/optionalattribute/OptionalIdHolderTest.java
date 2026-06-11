b1e95b72ae5ea8ea72201a7cf16b7590f2f2e0fc
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-11 17:12:51 +0200

    Create OptionalIdHolderTest

---
+++ Test/ch/nolix/baseapitest/attribute/optionalattribute/OptionalIdHolderTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.baseapitest.attribute.optionalattribute;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+import ch.nolix.base.testing.standardtest.StandardTest;
+import ch.nolix.baseapi.attribute.optionalattribute.IOptionalIdHolder;
+
+/**
+ * @author Silvan Wyss
+ */
+final class OptionalIdHolderTest extends StandardTest {
+  @Test
+  void testCase_getIdInSingleQuotes() {
+    //setup
+    final var testUnit = Mockito.mock(IOptionalIdHolder.class);
+    Mockito.when(testUnit.getIdInSingleQuotes()).thenCallRealMethod();
+    Mockito.when(testUnit.hasId()).thenReturn(true);
+    Mockito.when(testUnit.getId()).thenReturn("id");
+
+    //execution
+    final var result = testUnit.getIdInSingleQuotes();
+
+    //verification
+    expect(result).isEqualTo("'id'");
+  }
+
+  @Test
+  void testCase_hasId_whenHasTheGivenId() {
+    //setup
+    final var testUnit = Mockito.mock(IOptionalIdHolder.class);
+    Mockito.when(testUnit.hasId(ArgumentMatchers.any())).thenCallRealMethod();
+    Mockito.when(testUnit.hasId()).thenReturn(true);
+    Mockito.when(testUnit.getId()).thenReturn("id");
+
+    //execution
+    final var result = testUnit.hasId("id");
+
+    //verification
+    expect(result).isTrue();
+  }
+
+  @Test
+  void testCase_hasId_whenHasAnotherId() {
+    //setup
+    final var testUnit = Mockito.mock(IOptionalIdHolder.class);
+    Mockito.when(testUnit.hasId(ArgumentMatchers.any())).thenCallRealMethod();
+    Mockito.when(testUnit.hasId()).thenReturn(true);
+    Mockito.when(testUnit.getId()).thenReturn("id");
+
+    //execution
+    final var result = testUnit.hasId("Id");
+
+    //verification
+    expect(result).isFalse();
+  }
+
+  @Test
+  void testCase_hasId_whenDoesNotHaveAId() {
+    //setup
+    final var testUnit = Mockito.mock(IOptionalIdHolder.class);
+    Mockito.when(testUnit.hasId(ArgumentMatchers.any())).thenCallRealMethod();
+    Mockito.when(testUnit.hasId()).thenReturn(false);
+
+    //execution
+    final var result = testUnit.hasId("id");
+
+    //verification
+    expect(result).isFalse();
+  }
+}
