243ab4d78f7bf3484eded6978b206b6c4740d147
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-11 15:33:58 +0200

    Edit IOptionalValueHolder

--- Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalValueHolder.java
+++ Source/ch/nolix/baseapi/attribute/optionalattribute/IOptionalValueHolder.java
@@ -7,19 +7,19 @@ package ch.nolix.baseapi.attribute.optionalattribute;
  * A {@link IOptionalValueHolder} can contain a value.
  * 
  * @author Silvan Wyss
- * @param <V> is the type of the value of a {@link IOptionalValueHolder}.
+ * @param <V> the type of the value of a {@link IOptionalValueHolder}
  */
 public interface IOptionalValueHolder<V> {
   /**
-   * @return the value of the current {@link IOptionalValueHolder}.
+   * @return the value of the current {@link IOptionalValueHolder}
    * @throws RuntimeException if the current {@link IOptionalValueHolder} does not
-   *                          contain a value.
+   *                          contain a value
    */
   V getStoredValue();
 
   /**
    * @return true if the current {@link IOptionalValueHolder} contains a value,
-   *         false otherwise.
+   *         false otherwise
    */
   boolean hasValue();
 }
