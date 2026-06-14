3e8d3021cc2e077c44bf2448ab357b7f8274d477
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-14 20:25:04 +0200

    Create ExaminerTest

---
+++ ArchitectureTest/ch/nolix/unionarchitecturetest/ExaminerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.unionarchitecturetest;
+
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+
+import ch.nolix.base.testing.archunit.ArchUnitRuleCatalog;
+
+/**
+ * @author Silvan Wyss
+ */
+final class ExaminerTest {
+  @Test
+  void testCase() {
+    //setup
+    final var testUnit = //
+    new ClassFileImporter()
+      .importPackages("ch.nolix..")
+      .that(
+        new DescribedPredicate<JavaClass>("examiner classes") {
+          @Override
+          public boolean test(final JavaClass javaClass) {
+            return javaClass.getName().matches(".*Examiner");
+          }
+        });
+
+    //execution & verification
+    ArchUnitRuleCatalog.PUBLIC_NON_STATIC_METHODS_RETURN_A_PRIMITIVE_BOOLEAN.check(testUnit);
+  }
+}
