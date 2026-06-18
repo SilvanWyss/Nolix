2946cd3919d3cfa4a54927c69ce06997d9e9d0e0
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-18 16:30:17 +0200

    Update Javadoc comment

--- Source/ch/nolix/baseapi/commontype/inputstreamtool/IInputStreamTool.java
+++ Source/ch/nolix/baseapi/commontype/inputstreamtool/IInputStreamTool.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 public interface IInputStreamTool {
   /**
    * @param inputStream
-   * @return the next line from the given inputStream or null.
+   * @return the next line from the given inputStream or null
    */
   String readLineFromInputStreamOrNull(InputStream inputStream);
 }
