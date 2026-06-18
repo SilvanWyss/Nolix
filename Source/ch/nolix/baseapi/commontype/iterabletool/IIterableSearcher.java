05219cb0caa6cddcc7ae3aac275fee1d91d6883c
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-18 16:33:46 +0200

    Edit IIterableSearcher

--- Source/ch/nolix/baseapi/commontype/iterabletool/IIterableSearcher.java
+++ Source/ch/nolix/baseapi/commontype/iterabletool/IIterableSearcher.java
@@ -7,5 +7,14 @@ package ch.nolix.baseapi.commontype.iterabletool;
  * @author Silvan Wyss
  */
 public interface IIterableSearcher extends IIterableCountSearcher {
+  /**
+   * @param iterable      the searched {@link Iterable}, is considered to be empty
+   *                      when is null
+   * @param oneBasedIndex the one-based index at which an element is requested
+   * @param <E>           the type of the elements of the given iterable
+   * @return the element at the given oneBasedIndexed from the given iterable
+   * @throws RuntimeException if the given iterable does not contain an element at
+   *                          the given oneBasedIndex
+   */
   <E> E getStoredAtOneBasedIndex(Iterable<E> iterable, int oneBasedIndex);
 }
