0cc392b6e098f80613f3fe392cb1f77a33ddab4d
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-20 16:44:45 +0200

    Create DocumentComponent

---
+++ Source/ch/nolix/baseapi/component/documentcomponent/DocumentComponent.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.baseapi.component.documentcomponent;
+
+/**
+ * A {@link DocumentComponent} can belong to a document.
+ * 
+ * @author Silvan Wyss
+ * @param <D> the type of the document a {@link DocumentComponent} can belong to
+ */
+public interface DocumentComponent<D> {
+  /**
+   * @return true if the current {@link DocumentComponent} belongs to a document,
+   *         false otherwise
+   */
+  boolean belongsToDocument();
+
+  /**
+   * @return the parent document of the current {@link DocumentComponent}
+   * @throws RuntimeException if the current {@link DocumentComponent} does not
+   *                          belong to a document
+   */
+  D getStoredParentDocument();
+}
