fe8e1cfcf35f60442d4b2c2f83d6009627f65838
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-20 16:19:12 +0200

    Create OneBasedYDownGridPositioned

---
+++ Source/ch/nolix/techapi/geometry/OneBasedYDownGridPositioned.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.techapi.geometry;
+
+/**
+ * @author Silvan Wyss
+ */
+public interface OneBasedYDownGridPositioned {
+  int getOneBasedXGridPosition();
+
+  int getOneBasedYDownGridPosition();
+}
