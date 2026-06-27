ce810c700fee4b660ce636c28772f2ebac82a42a
Silvan Wyss <silvan.wyss@gmail.com>
2026-06-27 11:44:16 +0200

    Rename variable package to variablenamecatalog

--- Test/ch/nolix/baseapitest/misc/variablenamecatalog/PascalCaseVariableCatalogTest.java
+++ Test/ch/nolix/baseapitest/misc/variablenamecatalog/PascalCaseVariableCatalogTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright © by Silvan Wyss. All rights reserved.
  */
-package ch.nolix.baseapitest.misc.variable;
+package ch.nolix.baseapitest.misc.variablenamecatalog;
 
 import org.junit.jupiter.api.Test;
 
--- Test/ch/nolix/baseapitest/misc/variablenamecatalog/PluralLowerCaseVariableCatalogTest.java
+++ Test/ch/nolix/baseapitest/misc/variablenamecatalog/PluralLowerCaseVariableCatalogTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright © by Silvan Wyss. All rights reserved.
  */
-package ch.nolix.baseapitest.misc.variable;
+package ch.nolix.baseapitest.misc.variablenamecatalog;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,21 +10,23 @@ import ch.nolix.base.datastructure.linkedlist.LinkedList;
 import ch.nolix.base.reflection.reflectiontool.ReflectionTool;
 import ch.nolix.base.testing.standardtest.StandardTest;
 import ch.nolix.baseapi.commontype.stringexaminer.IStringExaminer;
-import ch.nolix.baseapi.misc.variablenamecatalog.LowerCaseVariableNameCatalog;
+import ch.nolix.baseapi.misc.variablenamecatalog.PluralLowerCaseVariableNameCatalog;
 
 /**
  * @author Silvan Wyss
  */
-final class LowerCaseVariableCatalogTest extends StandardTest {
+final class PluralLowerCaseVariableCatalogTest extends StandardTest {
   private static final IStringExaminer STRING_EXAMINER = new StringExaminer();
 
   @Test
   void testCase_constants() {
     //setup
-    final var exceptions = LinkedList.withElement(LowerCaseVariableNameCatalog.GUI, LowerCaseVariableNameCatalog.URL);
+    final var exceptions = //
+    LinkedList.withElement(PluralLowerCaseVariableNameCatalog.GUIS, PluralLowerCaseVariableNameCatalog.URLS);
 
     //verification
-    for (final var c : ReflectionTool.getStoredPublicStaticFieldValuesOfClass(LowerCaseVariableNameCatalog.class)) {
+    for (final var c : ReflectionTool
+      .getStoredPublicStaticFieldValuesOfClass(PluralLowerCaseVariableNameCatalog.class)) {
       expect(c).isOfType(String.class);
 
       final var stringValue = c.toString();
