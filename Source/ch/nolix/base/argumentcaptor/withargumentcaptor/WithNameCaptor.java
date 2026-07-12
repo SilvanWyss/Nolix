7ed5bdf73cb6112ff5e0ab74eef0923a7121496f
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-12 16:10:20 +0200

    Create IWithNameCaptor

--- Source/ch/nolix/base/argumentcaptor/withargumentcaptor/WithNameCaptor.java
+++ Source/ch/nolix/base/argumentcaptor/withargumentcaptor/WithNameCaptor.java
@@ -5,25 +5,34 @@ package ch.nolix.base.argumentcaptor.withargumentcaptor;
 
 import ch.nolix.base.argumentcaptor.base.AbstractArgumentCaptor;
 import ch.nolix.base.validation.validator.Validator;
+import ch.nolix.baseapi.argumentcaptor.withargumentcaptor.IWithNameCaptor;
 import ch.nolix.baseapi.generalcatalog.variablenamecatalog.LowerCaseVariableNameCatalog;
 
 /**
  * @author Silvan Wyss
- * @param <N> the type of the next thing of a {@link WithNameCaptor}.
+ * @param <S> the type of the successor of a {@link WithNameCaptor}.
  */
-public class WithNameCaptor<N> extends AbstractArgumentCaptor<String, N> {
+public class WithNameCaptor<S> extends AbstractArgumentCaptor<String, S> implements IWithNameCaptor<S> {
   public WithNameCaptor() {
   }
 
-  public WithNameCaptor(final N nextArgumentCaptor) {
+  public WithNameCaptor(final S nextArgumentCaptor) {
     super(nextArgumentCaptor);
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
   public final String getName() {
     return getStoredArgument();
   }
 
-  public final N withName(final String name) {
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public final S withName(final String name) {
     Validator.assertThat(name).thatIsNamed(LowerCaseVariableNameCatalog.NAME).isNotBlank();
 
     return setArgumentAndGetStoredSuccessor(name);
