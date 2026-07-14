ba327f5e225f76997f4d8d565d4d67bd61918af4
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-14 22:40:30 +0200

    Create ExtensionPropertyTest

---
+++ Test/ch/nolix/systemtest/property/extension/ExtensionPropertyTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.systemtest.property.extension;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import ch.nolix.base.testing.standardtest.StandardTest;
+import ch.nolix.system.property.extension.ExtensionProperty;
+
+/**
+ * @author Silvan Wyss
+ */
+final class ExtensionPropertyTest extends StandardTest {
+  @Test
+  void testCase_isMaterialized() {
+    // setup
+    final var testUnit = ExtensionProperty.withExtension(mock());
+
+    // execution
+    final var result = testUnit.isMaterialized();
+
+    // verification
+    expect(result).isTrue();
+  }
+}
