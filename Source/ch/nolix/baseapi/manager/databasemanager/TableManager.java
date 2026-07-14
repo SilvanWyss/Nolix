2e8339b17bec5b66eec8d870b0fbc60c95f0b7c3
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-14 21:37:02 +0200

    Create TableManager

---
+++ Source/ch/nolix/baseapi/manager/databasemanager/TableManager.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.baseapi.manager.databasemanager;
+
+import ch.nolix.baseapi.datastructure.extendediterable.ExtendedIterable;
+
+/**
+ * @author Silvan Wyss
+ * @param <T> the type of the tables of a {@link TableManager}
+ */
+public interface TableManager<T> {
+  /**
+   * @return the tables of the current {@link TableManager}
+   */
+  ExtendedIterable<? extends T> getStoredTables();
+}
