fda1de5c8aaf831fa8c70c6ec6e7b4725d7e472c
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-18 18:11:47 +0200

    Create ToFormattedStringTest

---
+++ Test/ch/nolix/basetest/document/node/abstractnode/ToFormattedStringTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.basetest.document.node.abstractnode;
+
+import org.junit.jupiter.api.Test;
+
+import ch.nolix.base.document.node.ImmutableNode;
+import ch.nolix.base.environment.filesystem.FileSystemAccessor;
+import ch.nolix.base.testing.standardtest.StandardTest;
+
+/**
+ * @author Silvan Wyss
+ */
+final class ToFormattedStringTest extends StandardTest {
+
+  @Test
+  void testCase_toFormattedString() {
+    // setup
+    final var testUnit = ImmutableNode.fromString("Cat(Name(Garfield),Sex(Male),AgeInYears(5),WeightInGrams(6500))");
+
+    // execute
+    final var result = testUnit.toFormattedString();
+
+    // verify
+    final var expectedResult = FileSystemAccessor.readFile("./././././TestResource/sample_node/garfield.node");
+
+    System.out.println(result);
+    System.out.println(expectedResult);
+
+    expect(result).isEqualTo(expectedResult);
+  }
+}
