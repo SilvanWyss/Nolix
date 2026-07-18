e486635ed1001b20a934e9637dbe221fc4fd91ee
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-18 15:40:43 +0200

    Make ILabel a FluentMutableTextHolder

--- Source/ch/nolix/systemapi/control/label/ILabel.java
+++ Source/ch/nolix/systemapi/control/label/ILabel.java
@@ -3,21 +3,18 @@
  */
 package ch.nolix.systemapi.control.label;
 
+import ch.nolix.baseapi.attribute.fluentmutablemandatoryattribute.FluentMutableTextHolder;
 import ch.nolix.systemapi.webgui.main.Control;
 
 /**
  * @author Silvan Wyss
  */
-public interface ILabel extends Control<ILabel, ILabelStyle> {
+public interface ILabel extends Control<ILabel, ILabelStyle>, FluentMutableTextHolder<ILabel> {
   LabelRole getRole();
-
-  String getText();
 
   boolean hasRole();
 
   void removeRole();
 
   ILabel setRole(LabelRole role);
-
-  ILabel setText(String text);
 }
