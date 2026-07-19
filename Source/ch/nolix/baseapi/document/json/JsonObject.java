ec26c5f36118d28b6df4ff5c387fe9e141332735
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-19 17:21:55 +0200

    Make JsonObject a EmptinessRequestable

--- Source/ch/nolix/baseapi/document/json/JsonObject.java
+++ Source/ch/nolix/baseapi/document/json/JsonObject.java
@@ -4,11 +4,12 @@
 package ch.nolix.baseapi.document.json;
 
 import ch.nolix.baseapi.datastructure.extendediterable.ExtendedIterable;
+import ch.nolix.baseapi.state.staterequest.EmptinessRequestable;
 
 /**
  * @author Silvan Wyss
  */
-public interface JsonObject extends JsonValue {
+public interface JsonObject extends EmptinessRequestable, JsonValue {
   ExtendedIterable<JsonNameValuePair> getStoredNameValuePairs();
 
   JsonObject toAlphabeticallyOrdered();
