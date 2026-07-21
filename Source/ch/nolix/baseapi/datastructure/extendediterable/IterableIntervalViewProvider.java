b7e8004942ab8695db5cd7d056383ee06c3f1a9f
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-21 19:54:22 +0200

    Rename IntervallContainerViewProvider to IterableIntervalViewProvider

--- Source/ch/nolix/baseapi/datastructure/extendediterable/IterableIntervalViewProvider.java
+++ Source/ch/nolix/baseapi/datastructure/extendediterable/IterableIntervalViewProvider.java
@@ -6,12 +6,12 @@ package ch.nolix.baseapi.datastructure.extendediterable;
 /**
  * @author Silvan Wyss
  * @param <E> the type of the elements of a
- *            {@link IntervallContainerViewProvider}
+ *            {@link IterableIntervalViewProvider}
  */
-public interface IntervallContainerViewProvider<E> {
+public interface IterableIntervalViewProvider<E> {
   /**
    * @param oneBasedStartIndex
-   * @return a new view of the current {@link IntervallContainerViewProvider} from
+   * @return a new view of the current {@link IterableIntervalViewProvider} from
    *         the given oneBasedStartIndex
    * @throws RuntimeException if the given oneBasedStartIndex is bigger than the
    *                          number of elements of the current
@@ -22,7 +22,7 @@ public interface IntervallContainerViewProvider<E> {
   /**
    * @param oneBasedStartIndex
    * @param oneBasedEndIndex
-   * @return a new view of the current {@link IntervallContainerViewProvider} from
+   * @return a new view of the current {@link IterableIntervalViewProvider} from
    *         the given oneBasedStartIndex to the given oneBasedEndIndex
    * @throws RuntimeException if the given oneBasedStartIndex is not positive
    * @throws RuntimeException if the given oneBasedStartIndex is smaller than the
@@ -36,7 +36,7 @@ public interface IntervallContainerViewProvider<E> {
   /**
    * @param oneBasedEndIndex
    * @return a new view {@link ExtendedIterable} of the current
-   *         {@link IntervallContainerViewProvider} to the given oneBasedEndIndex
+   *         {@link IterableIntervalViewProvider} to the given oneBasedEndIndex
    * @throws RuntimeException if the given oneBasedEndIndex is not positive
    * @throws RuntimeException if the given oneBasedEndIndex is bigger than the
    *                          number of the elements of the current
@@ -46,32 +46,32 @@ public interface IntervallContainerViewProvider<E> {
 
   /**
    * @return a new view {@link ExtendedIterable} view of the current
-   *         {@link IntervallContainerViewProvider} without the first element
+   *         {@link IterableIntervalViewProvider} without the first element
    * @throws RuntimeException if the current
-   *                          {@link IntervallContainerViewProvider} is empty
+   *                          {@link IterableIntervalViewProvider} is empty
    */
   ExtendedIterable<E> getViewWithoutFirst();
 
   /**
    * @param n
    * @return a new view {@link ExtendedIterable} view of the current
-   *         {@link IntervallContainerViewProvider} without the first n elements
+   *         {@link IterableIntervalViewProvider} without the first n elements
    * @throws RuntimeException if the given n is negative
    */
   ExtendedIterable<E> getViewWithoutFirst(int n);
 
   /**
    * @return a new view {@link ExtendedIterable} view of the current
-   *         {@link IntervallContainerViewProvider} without the last element
+   *         {@link IterableIntervalViewProvider} without the last element
    * @throws RuntimeException if the current
-   *                          {@link IntervallContainerViewProvider} is empty
+   *                          {@link IterableIntervalViewProvider} is empty
    */
   ExtendedIterable<E> getViewWithoutLast();
 
   /**
    * @param n
    * @return a new view {@link ExtendedIterable} of the current
-   *         {@link IntervallContainerViewProvider} without the last n elements
+   *         {@link IterableIntervalViewProvider} without the last n elements
    * @throws RuntimeException if the given n is negative
    */
   ExtendedIterable<E> getViewWithoutLast(int n);
