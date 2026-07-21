d30d80724de6e14440d348e73cca0a64fcafc529
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-21 19:52:33 +0200

    Rename Groupable to IterableGroupProvider

--- Source/ch/nolix/baseapi/datastructure/extendediterable/IterableGroupProvider.java
+++ Source/ch/nolix/baseapi/datastructure/extendediterable/IterableGroupProvider.java
@@ -7,13 +7,13 @@ import java.util.function.Function;
 
 /**
  * @author Silvan Wyss
- * @param <E> the type of the elements a {@link Groupable}
+ * @param <E> the type of the elements a {@link IterableGroupProvider}
  */
-public interface Groupable<E> {
+public interface IterableGroupProvider<E> {
   /**
    * @param norm
    * @return a new {@link ExtendedIterable} with the elements of the current
-   *         {@link Groupable} grouped by the given norm, ignoring null elements
+   *         {@link IterableGroupProvider} grouped by the given norm, ignoring null elements
    * @throws RuntimeException if the given norm is null
    */
   ExtendedIterable<? extends ExtendedIterable<E>> getStoredInGroups(Function<E, ?> norm);
