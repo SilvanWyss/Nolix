ca2870e0ba622dd9f4712883b0a642c8b3a8dc0f
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-22 11:20:05 +0200

    Create ColumnMapperTest

---
+++ Test/ch/nolix/systemtest/objectdata/schemamapper/ColumnMapperTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.systemtest.objectdata.schemamapper;
+
+import org.junit.jupiter.api.Test;
+
+import ch.nolix.base.datastructure.immutablelist.ImmutableList;
+import ch.nolix.base.testing.standardtest.StandardTest;
+import ch.nolix.baseapi.errorcontrol.invalidargumentexception.InvalidArgumentException;
+import ch.nolix.system.objectdata.schemamapper.ColumnMapper;
+
+/**
+ * @author Silvan Wyss
+ */
+final class ColumnMapperTest extends StandardTest {
+  @Test
+  void testCase_whenTheGivenFieldIsNull() {
+    // setup
+    final var testUnit = new ColumnMapper();
+
+    // execute & verify
+    expectRunning(() -> testUnit.mapFieldToColumn(null, "column_id", ImmutableList.createEmpty()))
+      .throwsException()
+      .ofType(InvalidArgumentException.class);
+  }
+}
