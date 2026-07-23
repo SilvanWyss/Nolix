b8f5fac4204f5b9bf0fa0aa2e2de712c19c00f1d
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-23 20:27:34 +0200

    Move CharacterTypeTest to right package

--- Test/ch/nolix/baseapitest/commontype/charactertool/CharacterTypeTest.java
+++ Test/ch/nolix/baseapitest/commontype/charactertool/CharacterTypeTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright © by Silvan Wyss. All rights reserved.
  */
-package ch.nolix.baseapitest.commontypetool.charactertool;
+package ch.nolix.baseapitest.commontype.charactertool;
 
 import org.junit.jupiter.api.Test;
 
