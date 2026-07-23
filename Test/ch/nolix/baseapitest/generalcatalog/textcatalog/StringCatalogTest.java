85f80fbe36fa2a2b7b65ceb5a178e4525edc9e6a
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-23 20:26:22 +0200

    Move StringCatalogTest to right package

--- Test/ch/nolix/baseapitest/generalcatalog/textcatalog/StringCatalogTest.java
+++ Test/ch/nolix/baseapitest/generalcatalog/textcatalog/StringCatalogTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright © by Silvan Wyss. All rights reserved.
  */
-package ch.nolix.baseapitest.commontypetool.stringtool;
+package ch.nolix.baseapitest.generalcatalog.textcatalog;
 
 import org.junit.jupiter.api.Test;
 
