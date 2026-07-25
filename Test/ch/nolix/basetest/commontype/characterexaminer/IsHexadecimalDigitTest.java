a5f30e540de81a72f999a900bd8020025b5c85d0
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-25 18:00:28 +0200

    Create IsHexadecimalDigitTest

--- Test/ch/nolix/basetest/commontype/characterexaminer/IsHexadecimalDigitTest.java
+++ Test/ch/nolix/basetest/commontype/characterexaminer/IsHexadecimalDigitTest.java
@@ -12,7 +12,7 @@ import ch.nolix.base.testing.standardtest.StandardTest;
 /**
  * @author Silvan Wyss
  */
-final class CharacterExaminerTest extends StandardTest {
+final class IsHexadecimalDigitTest extends StandardTest {
   @ParameterizedTest
   @ValueSource(chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' })
   void testCase_isHexadecimalDigit_whenGivenCharacterIsAHexadecimalDigit(final char character) {
