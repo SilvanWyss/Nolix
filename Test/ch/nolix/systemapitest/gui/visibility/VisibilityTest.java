d30699b6052a41728cd85d17151541ede30c9b00
Silvan Wyss <silvan.wyss@gmail.com>
2026-07-30 22:12:50 +0200

    Create VisibilityTest

---
+++ Test/ch/nolix/systemapitest/gui/visibility/VisibilityTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.systemapitest.gui.visibility;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import ch.nolix.base.datastructure.immutablelist.ImmutableList;
+import ch.nolix.base.document.node.ImmutableNode;
+import ch.nolix.base.testing.standardtest.StandardTest;
+import ch.nolix.baseapi.datastructure.extendediterable.ExtendedIterable;
+import ch.nolix.baseapi.document.node.INode;
+import ch.nolix.systemapi.gui.visibility.Visibility;
+
+/**
+ * @author Silvan Wyss
+ */
+final class VisibilityTest extends StandardTest {
+  @MethodSource
+  private static ExtendedIterable<Arguments> getSpecificationsAndTheirVisibility() {
+    return //
+    ImmutableList.withElements(
+      Arguments.of(ImmutableNode.withChildNodes("VISIBLE"), Visibility.VISIBLE),
+      Arguments.of(ImmutableNode.withChildNodes("INVISIBLE"), Visibility.INVISIBLE));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getSpecificationsAndTheirVisibility")
+  void testCase_fromSpecification(final INode<?> specification, final Visibility expectedVisibility) {
+    // execute
+    final var result = Visibility.fromSpecification(specification);
+
+    // verify
+    expect(result).is(expectedVisibility);
+  }
+}
