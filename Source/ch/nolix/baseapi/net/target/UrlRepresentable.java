f1adf27873f22a8d98a0f44c7330b5e5600dd9b3
Silvan Wyss <silvan.wyss@gmail.com>
2026-08-11 09:59:45 +0200

    Create UrlRepresentable

---
+++ Source/ch/nolix/baseapi/net/target/UrlRepresentable.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © by Silvan Wyss. All rights reserved.
+ */
+package ch.nolix.baseapi.net.target;
+
+/**
+ * @author Silvan Wyss
+ */
+public interface UrlRepresentable {
+  String toUrl();
+}
