8e7c24736beef385cc56719ef1562f647d5d6c25
Silvan Wyss <silvan.wyss@gmail.com>
2026-08-22 12:52:03 +0200

    Create ToFormattedStringTest

--- Test/ch/nolix/basetest/document/json/immutablejsonobject/ToFormattedStringTest.java
+++ Test/ch/nolix/basetest/document/json/immutablejsonobject/ToFormattedStringTest.java
@@ -5,6 +5,7 @@ package ch.nolix.basetest.document.json.immutablejsonobject;
 
 import org.junit.jupiter.api.Test;
 
+import ch.nolix.base.document.json.ImmutableJsonArray;
 import ch.nolix.base.document.json.ImmutableJsonNameValuePair;
 import ch.nolix.base.document.json.ImmutableJsonNumber;
 import ch.nolix.base.document.json.ImmutableJsonObject;
@@ -17,7 +18,7 @@ import ch.nolix.base.testing.standardtest.StandardTest;
  */
 final class ToFormattedStringTest extends StandardTest {
   @Test
-  void testCase_toFormattedString() {
+  void testCase_toFormattedString_1() {
     // setup
     final var testUnit = ImmutableJsonObject.withNameValuePairs(
       ImmutableJsonNameValuePair.withNameAndValue("name", ImmutableJsonString.withString("Garfield")),
@@ -30,6 +31,46 @@ final class ToFormattedStringTest extends StandardTest {
 
     // verify
     final var expectedResult = FileSystemAccessor.readFile("./././././TestResource/sample_json/garfield.json");
+    expect(result).isEqualTo(expectedResult);
+  }
+
+  @Test
+  void testCase_toFormattedString_2() {
+    // setup
+    final var testUnit = ImmutableJsonObject.withNameValuePairs(
+      ImmutableJsonNameValuePair.withNameAndValue("date", ImmutableJsonString.withString("2030-01-01")),
+      ImmutableJsonNameValuePair.withNameAndValue(
+        "customer",
+        ImmutableJsonObject.withNameValuePairs(
+          ImmutableJsonNameValuePair.withNameAndValue("firstName", ImmutableJsonString.withString("Donald")),
+          ImmutableJsonNameValuePair.withNameAndValue("lastName", ImmutableJsonString.withString("Duck")),
+          ImmutableJsonNameValuePair.withNameAndValue(
+            "deliveryAddress",
+            ImmutableJsonObject.withNameValuePairs(
+              ImmutableJsonNameValuePair.withNameAndValue("housenumber", ImmutableJsonString.withString("13")),
+              ImmutableJsonNameValuePair.withNameAndValue("street", ImmutableJsonString.withString("Quack Street")),
+              ImmutableJsonNameValuePair.withNameAndValue("city", ImmutableJsonString.withString("Duckburg")))))),
+      ImmutableJsonNameValuePair.withNameAndValue(
+        "positions",
+        ImmutableJsonArray.withObjects(
+          ImmutableJsonObject.withNameValuePairs(
+            ImmutableJsonNameValuePair.withNameAndValue("product", ImmutableJsonString.withString("desk")),
+            ImmutableJsonNameValuePair.withNameAndValue("amount", ImmutableJsonNumber.withNumber(1)),
+            ImmutableJsonNameValuePair.withNameAndValue("pricePerPiece", ImmutableJsonNumber.withNumber(513))),
+          ImmutableJsonObject.withNameValuePairs(
+            ImmutableJsonNameValuePair.withNameAndValue("product", ImmutableJsonString.withString("chair")),
+            ImmutableJsonNameValuePair.withNameAndValue("amount", ImmutableJsonNumber.withNumber(4)),
+            ImmutableJsonNameValuePair.withNameAndValue("pricePerPiece", ImmutableJsonNumber.withNumber(113))),
+          ImmutableJsonObject.withNameValuePairs(
+            ImmutableJsonNameValuePair.withNameAndValue("product", ImmutableJsonString.withString("lamp")),
+            ImmutableJsonNameValuePair.withNameAndValue("amount", ImmutableJsonNumber.withNumber(1)),
+            ImmutableJsonNameValuePair.withNameAndValue("pricePerPiece", ImmutableJsonNumber.withNumber(213))))));
+
+    // execute
+    final var result = testUnit.toFormattedString();
+
+    // verify
+    final var expectedResult = FileSystemAccessor.readFile("./././././TestResource/sample_json/order.json");
     expect(result).isEqualTo(expectedResult);
   }
 }
